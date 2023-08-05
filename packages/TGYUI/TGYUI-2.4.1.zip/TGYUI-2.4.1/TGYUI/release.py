7c725ac05ae1196074487d55dae999db72b78eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package TGYUI/2.4.1/TGYUI-2.4.1.zip

---
+++ packages/TGYUI/TGYUI-2.4.1.zip/TGYUI-2.4.1/TGYUI/release.py
@@ -0,0 +1,14 @@
+# Release information about TGYUI
+
+version = "2.4.1"
+
+description = "The Yahoo! YUI packaged as collection of TurboGears widgets."
+# long_description = "More description about your plan"
+author = "W-Mark Kubacki, Marco Mariani"
+email = "wmark.tgyui@hurrikane.de"
+copyright = "Authors; for static/yui: Copyright (c) 2006-2007, Yahoo! Inc."
+
+# if it's open source, you might want to specify these
+url = "http://tgwidgets.ossdl.de/"
+# download_url = "http://yourcool.site/download"
+license = "BSD"
