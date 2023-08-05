5bb31fe5a5097457886bf0f63525b9f9fb0ea99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package cssutils/0.9.4a3/cssutils-0.9.4a3-py2.5.egg

---
+++ packages/cssutils/cssutils-0.9.4a3-py2.5.egg/cssutils/css/__init__.py
@@ -0,0 +1,65 @@
+"""
+Document Object Model Level 2 CSS
+http://www.w3.org/TR/2000/PR-DOM-Level-2-Style-20000927/css.html
+
+currently implemented
+    - CSSStyleSheet
+    - CSSRuleList
+    - CSSRule
+    - CSSComment (cssutils addon)
+    - CSSCharsetRule
+    - CSSImportRule
+    - CSSMediaRule
+    - CSSNamespaceRule (WD)
+    - CSSPageRule
+    - CSSStyleRule
+    - CSSUnkownRule
+    - CSSStyleDeclaration
+    - CSS2Properties
+    - CSSValue
+    - CSSPrimitiveValue
+    - CSSValueList
+
+todo
+    - RGBColor, Rect, Counter
+"""
+__all__ = [
+    'CSSStyleSheet',
+    'CSSRuleList',
+    'CSSRule',
+    'CSSComment',
+    'CSSCharsetRule',
+    'CSSImportRule',
+    'CSSMediaRule',
+    'CSSNamespaceRule',
+    'CSSPageRule',
+    'CSSStyleRule',
+    'CSSUnknownRule',
+    'CSSStyleDeclaration', 'Property',
+    'CSSValue', 'CSSPrimitiveValue', 'CSSValueList',
+    ]
+__docformat__ = 'restructuredtext'
+__author__ = '$LastChangedBy: cthedot $'
+__date__ = '$LastChangedDate: 2007-10-16 21:41:17 +0200 (Di, 16 Okt 2007) $'
+__version__ = '$LastChangedRevision: 484 $'
+
+from cssstylesheet import *
+from cssrulelist import *
+from cssrule import *
+from csscomment import *
+from csscharsetrule import *
+from cssimportrule import *
+from cssmediarule import *
+from cssnamespacerule import *
+from csspagerule import *
+from cssstylerule import *
+from cssunknownrule import *
+from cssstyledeclaration import *
+from cssvalue import *
+from selectorlist import *
+from selector import *
+
+
+if __name__ == '__main__':
+    for x in __all__:
+        print x
