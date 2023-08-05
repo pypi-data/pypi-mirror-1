863fc184ef3085558bd51d0a249b8a756c9d4c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:56 +0000

    Add package zope.copypastemove/3.4.0/zope.copypastemove-3.4.0.tar.gz

---
+++ packages/zope.copypastemove/zope.copypastemove-3.4.0.tar.gz/zope.copypastemove-3.4.0/setup.py
@@ -0,0 +1,57 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.copypastemove package
+
+$Id: setup.py 80320 2007-09-28 12:21:19Z philikon $
+"""
+import os
+from setuptools import setup, find_packages
+
+long_description = (open('README.txt').read() +
+                    '\n\n' +
+                    open('CHANGES.txt').read())
+
+setup(name='zope.copypastemove',
+      version = '3.4.0',
+      url='http://pypi.python.org/pypi/zope.copypastemove',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description="Copy, Paste and Move support for content components.",
+      long_description=long_description,
+      classifiers=['Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Zope Public License',
+                   'Programming Language :: Python',
+                   'Framework :: Zope3',
+                   ],
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope',],
+      install_requires=['setuptools',
+                        'zope.interface',
+                        'zope.exceptions',
+                        'zope.component',
+                        'zope.event',
+                        'zope.location',
+                        'zope.annotation',
+                        'zope.lifecycleevent',
+                        # testing dependencies:
+                        'zope.app.testing',
+                        'zope.app.principalannotation',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
