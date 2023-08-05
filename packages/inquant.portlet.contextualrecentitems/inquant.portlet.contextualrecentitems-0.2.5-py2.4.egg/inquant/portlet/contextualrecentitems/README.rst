69e1d10cc7600b70cb1ec98f1f7f1963b6115b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:47 +0000

    Add package inquant.portlet.contextualrecentitems/0.2.5/inquant.portlet.contextualrecentitems-0.2.5-py2.4.egg

---
+++ packages/inquant.portlet.contextualrecentitems/inquant.portlet.contextualrecentitems-0.2.5-py2.4.egg/inquant/portlet/contextualrecentitems/README.rst
@@ -0,0 +1,24 @@
+Contextual Recent Items Portlet
+===============================
+
+:Author:    $Author: ramonski $
+:Date:      $Date: 2008-02-12 10:56:57 +0100 (Di, 12 Feb 2008) $
+:Revision:  $Revision: 58591 $
+
+Abstract
+--------
+A Portlet wich displays recent changed items of a custom type.
+
+
+Introduction
+------------
+This Portlet behaves in almost the same manner as the standard recent changed
+items Portlet. The difference is, that a custom Content Type can be selected so
+that only recent changed items of this portal type are displayed.  Also the
+"more changes" link is modified that a custum view will be displayed with all
+recent changes of exactly this type.
+
+
+::
+
+ vim: set ft=rst tw=75 nocin nosi ai sw=4 ts=4 expandtab:
---
+++ packages/inquant.portlet.contextualrecentitems/inquant.portlet.contextualrecentitems-0.2.5-py2.4.egg/inquant/portlet/contextualrecentitems/interfaces.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# File: .py
+#
+# Copyright (c) InQuant GmbH
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+__author__    = """Stefan Eletzhofer <stefan.eletzhofer@inquant.de>"""
+__docformat__ = 'plaintext'
+__revision__  = "$Revision: 58891 $"
+__version__   = '$Revision: 58891 $'[11:-2]
+
+
+from zope import interface
+
+class ITypeNameProvider(interface.Interface):
+    """ a thing which is able to proveide us with a name of a
+        portal type """
+    type = interface.Attribute(u"the type name")
+
+# vim: set ft=python ts=4 sw=4 expandtab :
