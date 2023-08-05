acb3779e998b48f6385e6a3ce23416435fd58c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package zope.datetime/3.4.0/zope.datetime-3.4.0.tar.gz

---
+++ packages/zope.datetime/zope.datetime-3.4.0.tar.gz/zope.datetime-3.4.0/setup.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.datetime package
+
+$Id: setup.py 78224 2007-07-20 16:54:08Z fdrake $
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.datetime',
+      version = '3.4.0',
+      url='http://svn.zope.org/zope.datetime',
+      license='ZPL 2.1',
+      description='Zope datetime',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="Commonly used date and time related utility functions.",
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['setuptools'],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
