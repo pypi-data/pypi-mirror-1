3068a622c82b5d52a62802c156dc17cb5357028e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:56 +0000

    Add package ZSI/2.0-rc1/ZSI-2.0_rc1-py2.3.egg

---
+++ packages/ZSI/ZSI-2.0_rc1-py2.3.egg/ZSI/generate/__init__.py
@@ -0,0 +1,18 @@
+# $Id: __init__.py,v 1.2 2006/01/03 23:45:29 boverhof Exp $
+
+__all__ = ['wsdl2python', 'utility', 'containers']
+
+class WsdlGeneratorError(Exception):
+    pass
+
+class Wsdl2PythonError(Exception):
+    pass
+
+class WSInteropError(Exception):
+   '''Conformance to WS-I Basic-Profile 1.0 specification
+   '''
+
+class WSISpec:
+    R2203 = 'An rpc-literal binding in a DESCRIPTION MUST refer, in its soapbind:body element(s), only to wsdl:part element(s) that have been defined using the type attribute.'
+    R2710 = 'The operations in a wsdl:binding in a DESCRIPTION MUST result in wire signatures that are different from one another.'
+    R2717 = 'An rpc-literal binding in a DESCRIPTION MUST have the namespace attribute specified, the value of which MUST be an absolute URI, on contained  soapbind:body elements.'
