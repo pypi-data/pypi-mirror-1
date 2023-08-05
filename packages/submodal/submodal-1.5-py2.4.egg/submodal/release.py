011d4b19fcbdc1b604fe19ec6ea719b7eb14ab8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:49 +0000

    Add package submodal/1.5/submodal-1.5-py2.4.egg

---
+++ packages/submodal/submodal-1.5-py2.4.egg/submodal/release.py
@@ -0,0 +1,19 @@
+# Release information about submodal
+
+version = "1.5"
+
+description = "Cross Browser DHTML Modal Dialogs For Web Apps"
+long_description = """\
+The subModal works by placing a semi-transparent div over the browser, blocking \
+access to the content below while still providing visibility. This maintains \
+state and doesn't make someone feel disoriented or lost by moving them \
+completely to another page. Their frame of reference is kept while allowing them \
+to perform a new task (usually closely associated with the content below). \
+\
+This version was based on the enhanced subModal provided by Todd Huss.\
+"""
+author = "Jorge Godoy (packager)"
+email = "jgodoy@gmail.com"
+
+url = "http://gabrito.com/post/enhancements-to-submodal"
+license = "Free for any use - See included files"
