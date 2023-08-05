b163a674e34628145453e564c9c57e02f9b50800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:25 +0000

    Add package Scriptaculous/1.8.1.1/Scriptaculous-1.8.1.1-py2.5.egg

---
+++ packages/Scriptaculous/Scriptaculous-1.8.1.1-py2.5.egg/scriptaculous/release.py
@@ -0,0 +1,12 @@
+# Release information about Scriptaculous
+
+version = "1.8.1.1"
+
+description = "Scriptaculous packaged as TurboGears widgets."
+author = "Thomas Fuchs, Sam Stephenson, Kevin Dangoor, Fred Lin, W-Mark Kubacki"
+email = "mark@ossdl.de"
+copyright = "Copyright 2007-2008"
+
+url = "http://tgwidgets.ossdl.de/"
+download_url = "http://static.ossdl.de/tgwidgets/downloads/"
+license = "MIT"
