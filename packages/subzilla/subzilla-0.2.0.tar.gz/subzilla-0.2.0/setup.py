f73396dba3aaa58a8cd5ccfbbe0f3acebcd04a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package subzilla/0.2.0/subzilla-0.2.0.tar.gz

---
+++ packages/subzilla/subzilla-0.2.0.tar.gz/subzilla-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+setup(
+    name = "subzilla",
+    version = "0.2.0",
+    packages = find_packages(),
+    install_requires = ['twill>=0.9.0'],
+    entry_points = { 'console_scripts': ['subzilla = subzilla.subzilla:main'] },
+    url = 'http://svn.osafoundation.org/sandbox/jeffrey/subzilla/README.txt',
+    author = 'Jeffrey Harris',
+    description = 'Subzilla will post a patch from a Subversion tree to a ' \
+                  'Bugzilla 3.0 bug, or apply a patch from a Bugzilla bug to a ' \
+                  'Subversion tree.',
+    long_description = """Development has focused on connecting to OSAF's
+                          Bugzilla instance, but it ought to work with
+                          reasonable Bugzilla instances.""",
+    classifiers = [
+      'Development Status :: 3 - Alpha',
+      'Environment :: Console',
+      'Environment :: Web Environment',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: Python Software Foundation License',
+      'Programming Language :: Python',
+      'Topic :: Software Development :: Bug Tracking',
+      ],
+)
