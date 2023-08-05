b108b6396787ae7a7216fa664475fec4bde2d61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package zope.dublincore/3.4.0/zope.dublincore-3.4.0.tar.gz

---
+++ packages/zope.dublincore/zope.dublincore-3.4.0.tar.gz/zope.dublincore-3.4.0/setup.py
@@ -0,0 +1,48 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.dublincore package
+
+$Id: setup.py 80286 2007-09-28 10:51:02Z philikon $
+"""
+from setuptools import setup, find_packages
+
+long_description = (open('README.txt').read() +
+                    '\n\n' +
+                    open('CHANGES.txt').read())
+
+setup(
+    name="zope.dublincore",
+    version = '3.4.0',
+    url='http://pypi.python.org/pypi/zope.dublincore',
+    license='ZPL 2.1',
+    description='Zope Dublin Core implementation',
+    long_description=long_description,
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['zope'],
+    include_package_data=True,
+    install_requires = ['setuptools',
+                        'zope.annotation',
+                        'zope.component',
+                        'zope.interface',
+                        # testing dependencies
+                        'zope.testing',
+                        'zope.security',
+                        'zope.app.testing',
+                        ],
+    zip_safe = False
+    )
