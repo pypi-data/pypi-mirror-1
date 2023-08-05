4f1ac528ec66c25fdfc15ff37ad47bc6701aa2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:17 +0000

    Add package crablfs/0.1/crablfs-0.1.tar.gz

---
+++ packages/crablfs/crablfs-0.1.tar.gz/crablfs-0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+# Author: ÷‹≈Ù
+# Date: 2006-10-02
+# Email: chowroc.z@gmail.com
+
+from distutils.core import setup
+
+setup(
+	name = 'crablfs',
+	version = '0.1',
+	author = 'Zhou Peng(chowroc)',
+	author_email = 'chowroc.z@gmail.com',
+	description = 'User Based Package Management System',
+	# classifier = [
+	#	'Environment :: Console',
+	#	'Operating System :: POSIX',
+	#	'Programming Language :: Python',
+	#	'Topic :: Communications :: Email',
+	# ],
+	py_modules = ['userpack'],
+	package_dir = {'crablfs' : 'lib'},
+	packages = ['crablfs'],
+	scripts = ['userpack', 'upm', 'crablfs'],
+	data_files = [('/etc', ['userpack.dirs'])],
+)
