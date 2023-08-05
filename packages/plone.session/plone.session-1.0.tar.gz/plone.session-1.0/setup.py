2bf234747d31239452a1659945cdce56c8038153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package plone.session/1.0/plone.session-1.0.tar.gz

---
+++ packages/plone.session/plone.session-1.0.tar.gz/plone.session-1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+setup(name='plone.session',
+      version=version,
+      description="Session based authentication for Zope",
+      long_description=open("README.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+	"Topic :: System :: Systems Administration :: Authentication/Directory",
+        ],
+      keywords='PAS session authentication Zope',
+      author='Wichert Akkerman - Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.session',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      download_url='http://code.google.com/p/plone/downloads/list',
+      install_requires=[
+        'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
