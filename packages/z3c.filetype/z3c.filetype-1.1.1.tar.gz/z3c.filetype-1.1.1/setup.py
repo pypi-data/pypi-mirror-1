4978de5698e0907d9c9b47f20af40a958cf49b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:58 +0000

    Add package z3c.filetype/1.1.1/z3c.filetype-1.1.1.tar.gz

---
+++ packages/z3c.filetype/z3c.filetype-1.1.1.tar.gz/z3c.filetype-1.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="z3c.filetype",
+    version="1.1.1",
+    namespace_packages=["z3c"],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "setuptools",
+        "zope.cachedescriptors",
+        "zope.component",
+        "zope.contenttype",
+        "zope.event",
+        "zope.i18nmessageid",
+        "zope.interface",
+        "zope.lifecycleevent",
+        "zope.proxy",
+        "zope.schema",
+        "zope.size",
+        ],
+    extras_require={
+        "test": ["zope.testing"],
+        },
+    zip_safe=False,
+    )
