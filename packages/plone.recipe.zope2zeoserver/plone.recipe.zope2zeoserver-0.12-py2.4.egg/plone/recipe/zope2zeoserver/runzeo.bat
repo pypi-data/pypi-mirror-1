ef9eb0e0ef8513822bc6881ce2020bfb31b4461a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package plone.recipe.zope2zeoserver/0.12/plone.recipe.zope2zeoserver-0.12-py2.4.egg

---
+++ packages/plone.recipe.zope2zeoserver/plone.recipe.zope2zeoserver-0.12-py2.4.egg/plone/recipe/zope2zeoserver/runzeo.bat
@@ -0,0 +1,8 @@
+@rem %(PACKAGE)s instance start script
+@set PYTHON=%(PYTHON)s
+@set ZODB3_HOME=%(zodb3_home)s
+@set CONFIG_FILE=%(INSTANCE_HOME)s/etc/%(PACKAGE)s.conf
+@set PYTHONPATH=%%ZODB3_HOME%%
+@set RUNZEO=%%ZODB3_HOME%%/ZEO/runzeo.py
+"%%PYTHON%%" "%%RUNZEO%%" -C "%%CONFIG_FILE%%" %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9
+
