7b4da74ace6776b57ec1748d96ff7a7002a31868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:16 +0000

    Add package setuptools/0.6c2/setuptools-0.6c2.zip

---
+++ packages/setuptools/setuptools-0.6c2.zip/setuptools-0.6c2/setuptools/command/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    'alias', 'bdist_egg', 'bdist_rpm', 'build_ext', 'build_py', 'develop',
+    'easy_install', 'egg_info', 'install', 'install_lib', 'rotate', 'saveopts',
+    'sdist', 'setopt', 'test', 'upload', 'install_egg_info', 'install_scripts',
+    'register',
+]
+
+
+from distutils.command.bdist import bdist
+
+if 'egg' not in bdist.format_commands:
+    bdist.format_command['egg'] = ('bdist_egg', "Python .egg file")
+    bdist.format_commands.append('egg')
+
+del bdist
