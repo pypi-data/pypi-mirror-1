6ddfc457d2a88b530c03532f652c3696ac1e0e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package zope.app.file/3.4.0a1/zope.app.file-3.4.0a1.tar.gz

---
+++ packages/zope.app.file/zope.app.file-3.4.0a1.tar.gz/zope.app.file-3.4.0a1/setup.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.file package
+
+$Id: setup.py 74411 2007-04-22 10:33:40Z ctheune $
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.file',
+      version = '3.4.0a1',
+      url='http://svn.zope.org/zope.app.file',
+      license='ZPL 2.1',
+      description='Zope file',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=['zope.app.testing',
+                                  'zope.app.securitypolicy',
+                                  'zope.app.zcmlfiles',]),
+      install_requires=['setuptools',
+                        'ZODB3',
+                        'zope.app.publication',
+                        'zope.contenttype',
+                        'zope.datetime',
+                        'zope.dublincore',
+                        'zope.event',
+                        'zope.exceptions',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.publisher',
+                        'zope.schema',
+                        'zope.size',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
