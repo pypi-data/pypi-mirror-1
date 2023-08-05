e1180d88212876b6d8b4a95f5270016427a405cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package pIDLy/0.2.3/pIDLy-0.2.3.tar.gz

---
+++ packages/pIDLy/pIDLy-0.2.3.tar.gz/pIDLy-0.2.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(name='pIDLy',
+      version='0.2.3',
+      description='IDL within Python',
+      long_description='Control ITT\'s IDL (Interactive Data Language) from within Python',
+      author='Anthony Smith',
+      author_email='A.J.Smith@sussex.ac.uk',
+      url='http://astronomy.sussex.ac.uk/~anthonys/pidly/',
+      py_modules=['pidly'],
+      license='MIT',
+      requires=['NumPy', 'Pexpect'],
+      install_requires=['numpy', 'pexpect'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Science/Research',
+                   'Programming Language :: Other',
+                   'License :: OSI Approved :: MIT License']
+      )
