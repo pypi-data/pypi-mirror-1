7c1435312ae4dcd3b052cfaaec73205372d71332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:11 +0000

    Add package FuXi/0.86b.dev-r24/FuXi-0.86b.dev-r24.tar.gz

---
+++ packages/FuXi/FuXi-0.86b.dev-r24.tar.gz/FuXi-0.86b.dev-r24/setup.py
@@ -0,0 +1,27 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools  import setup
+setup(name="FuXi",
+      version="0.86b",
+      description="A N3-based forward-chaining, DL reasoner for RDFLib",
+      author="Chime Ogbuji",
+      author_email="chimezie@ogbuji.net",
+      package_dir = {
+        'FuXi': 'lib',
+      },
+      packages=[
+        "FuXi",
+        "FuXi.Rete"
+      ],
+      install_requires = ['rdflib>=2.3.3'],
+      scripts = ['Fuxi.py'],
+      license = "BSD",
+      keywords = "python logic owl rdf dlp n3 rule reasoner",
+      url = "http://code.google.com/p/python-dlp/wiki/FuXi",
+      entry_points = {
+       'console_scripts': [
+           'Fuxi = FuXi.Rete.CommandLine:main',
+        ],
+      },
+      zip_safe=False
+)
