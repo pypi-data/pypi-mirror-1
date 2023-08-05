158ccc24dc70a394412b7f8c4d8b1dce62cec6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:27 +0000

    Add package ZSI/2.0-rc2/ZSI-2.0_rc2-py2.4.egg

---
+++ packages/ZSI/ZSI-2.0_rc2-py2.4.egg/ZSI/wstools/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+"""WSDL parsing services package for Web Services for Python."""
+
+ident = "$Id: __init__.py 840 2004-12-07 10:54:53 -0500 (Tue, 07 Dec 2004) blunck2 $"
+
+import WSDLTools
+import XMLname
+import logging
+
