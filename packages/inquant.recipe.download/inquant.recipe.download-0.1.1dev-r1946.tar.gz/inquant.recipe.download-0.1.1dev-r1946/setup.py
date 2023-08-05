8a993ba52f50df4645c7d3793f6454627cdfe835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:02 +0000

    Add package inquant.recipe.download/0.1.1dev-r1946/inquant.recipe.download-0.1.1dev-r1946.tar.gz

---
+++ packages/inquant.recipe.download/inquant.recipe.download-0.1.1dev-r1946.tar.gz/inquant.recipe.download-0.1.1dev-r1946/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.1'
+name ='inquant.recipe.download'
+
+setup(name=name,
+      version=version,
+      description="A buildout recipe based on the gocept.download recipe which allows to clean out the destination.",
+      long_description=file("README.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Buildout",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='buildout recipe',
+      author='Stefan Eletzhofer',
+      author_email='stefan.eletzhofer@inquant.de',
+      url='https://svn.inquant.de/inquant/develop/plone/recipes/'+name,
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['inquant', 'inquant.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          "gocept.download", "zc.buildout", "setuptools"
+      ],
+      entry_points = {'zc.buildout': ['default = %s:Recipe' % name]},
+      )
