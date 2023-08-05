9bbfd5d34fc7526367689d9ad0b0d41cef112210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package z3c.offlinepack/0.1.3/z3c.offlinepack-0.1.3.tar.gz

---
+++ packages/z3c.offlinepack/z3c.offlinepack-0.1.3.tar.gz/z3c.offlinepack-0.1.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.3'
+
+setup(name='z3c.offlinepack',
+      version=version,
+      description="Pack ZODB databases without running Zope or ZEO",
+      long_description=open(os.path.join(os.path.dirname(__file__),
+                                         'README.txt')).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Ross Patterson',
+      author_email='me@rpatterson.net',
+      url='http://svn.zope.org/z3c.offlinepack',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['z3c'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'ZODB3', 'zope.dottedname'
+      ],
+      extras_require=dict(test=['zc.buildout', 'zc.recipe.egg']),
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      offlinepack = z3c.offlinepack:main
+      """,
+      )
