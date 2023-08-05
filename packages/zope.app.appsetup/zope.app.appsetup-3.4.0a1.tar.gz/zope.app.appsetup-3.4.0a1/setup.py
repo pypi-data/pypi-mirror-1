19ff19140e9bf0b1a7ad26498765ae0dbf697628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package zope.app.appsetup/3.4.0a1/zope.app.appsetup-3.4.0a1.tar.gz

---
+++ packages/zope.app.appsetup/zope.app.appsetup-3.4.0a1.tar.gz/zope.app.appsetup-3.4.0a1/setup.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.appsetup package
+
+$Id: setup.py 74347 2007-04-22 10:20:31Z ctheune $
+"""
+
+import os
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.appsetup',
+      version = '3.4.0a1',
+      url='http://svn.zope.org/zope.app.appsetup',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+	  packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+      extras_require=dict(test=['zope.app.testing']),
+      namespace_packages=['zope', 'zope.app'],
+      install_requires=['setuptools',
+                        'zope.interface',
+                        'zope.security',
+                        'zope.component',
+                        'zope.app.component',
+                        'zope.app.folder',
+                        'zope.configuration',
+                        'zope.event',
+                        'zope.traversing',
+                        'zope.app.container',
+                        'zope.app.publication',
+                        'ZODB3',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
