abf6f479fddedeee7025f3d446fe678e11fef9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package z3c.menu/0.2.0/z3c.menu-0.2.0.tar.gz

---
+++ packages/z3c.menu/z3c.menu-0.2.0.tar.gz/z3c.menu-0.2.0/README.txt
@@ -0,0 +1,2 @@
+The z3c.menu package provides a simple menu implementation which allows you to
+implement simply menus based on content providers and viewlets.
---
+++ packages/z3c.menu/z3c.menu-0.2.0.tar.gz/z3c.menu-0.2.0/setup.py
@@ -0,0 +1,78 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for z3c.menu package
+
+$Id: setup.py 81038 2007-10-24 14:34:17Z srichter $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='z3c.menu',
+      version = '0.2.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Collection of Viewlet-based Menus',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          'Detailed Dcoumentation\n' +
+          '======================\n'
+          + '\n\n' +
+          read('src', 'z3c', 'menu', 'simple', 'README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 menu viewlet simple",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/z3c.menu',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['z3c'],
+      install_requires = [
+          'setuptools',
+          'z3c.i18n',
+          'z3c.viewlet',
+          'zope.app.component',
+          'zope.app.pagetemplate',
+          'zope.app.publisher',
+          'zope.app.zapi',
+          'zope.component',
+          'zope.contentprovider',
+          'zope.interface',
+          'zope.schema',
+          'zope.viewlet',
+          ],
+      extras_require = {
+          "test": ['zope.app.testing',
+                   'zope.security',
+                   'zope.testing',
+                   'zope.traversing',
+                   ],
+          },
+      include_package_data = True,
+      zip_safe = False,
+      )
