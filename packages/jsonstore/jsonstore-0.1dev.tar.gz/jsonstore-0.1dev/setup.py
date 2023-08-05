590881db55be3307c9f2ca86ff46598247857424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:05 +0000

    Add package jsonstore/0.1dev/jsonstore-0.1dev.tar.gz

---
+++ packages/jsonstore/jsonstore-0.1dev.tar.gz/jsonstore-0.1dev/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='jsonstore',
+      version=version,
+      description="A RESTful atom store based using JSON representation.",
+      long_description="""\
+This package contains a WSGI app implementing an atom store based on the JSON representation.
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Roberto De Almeida',
+      author_email='roberto@dealmeida.net',
+      url='http://dealmeida.net/projects/jsonstore',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          "Paste",
+          "atomstorage",
+          "simplejson",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [paste.app_factory]
+      main = jsonstore.jsonstore:make_app
+      """,
+      )
+      
