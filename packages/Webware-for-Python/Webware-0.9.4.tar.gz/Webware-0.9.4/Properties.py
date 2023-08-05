715a7566256cf33b743d2a4cd37a9e86af475f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package Webware-for-Python/0.9.4/Webware-0.9.4.tar.gz

---
+++ packages/Webware-for-Python/Webware-0.9.4.tar.gz/Webware-0.9.4/Properties.py
@@ -0,0 +1,11 @@
+name = 'Webware for Python'
+
+version = (0, 9, 4)
+
+status = 'beta'
+
+releaseDate = '07/07/07'
+
+requiredPyVersion = (2, 0, 0)
+
+synopsis = """Webware provides a suite of Python components for developing Web applications. Included are an app server, servlets, Python Server Pages (PSP), Kid template plug-in (KidKit), object-relational mapping and task scheduling. The architecture is very modular, and you can provide your own extensions."""
---
+++ packages/Webware-for-Python/Webware-0.9.4.tar.gz/Webware-0.9.4/WebUtils/Properties.py
@@ -0,0 +1,13 @@
+name = 'WebUtils'
+
+version = (0, 9, 4)
+
+docs = [
+	{'name': "User's Guide", 'file': 'UsersGuide.html'},
+]
+
+status = 'beta'
+
+requiredPyVersion = (2, 0, 0)
+
+synopsis = """WebUtils contains functions for common web related programming tasks such as encoding/decoding HTML, etc."""
