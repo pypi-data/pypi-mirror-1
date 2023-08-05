ef21143fc69eb7006620648ed7db66094c816d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:56 +0000

    Add package FibraNet/9/FibraNet-9.tar.gz

---
+++ packages/FibraNet/FibraNet-9.tar.gz/FibraNet-9/nanothreads.py
@@ -0,0 +1,340 @@
+"""
+
+The nanothreads module simulates concurrency using Python generators to 
+implement cooperative threads.
+
+"""
+
+__author__ = "simonwittber@gmail.com"
+
+#import the best timing function possible (platform dependent)
+import platform
+if platform.system() == "Windows": 
+    from time import clock as time_func
+else: 
+    from time import time as time_func
+del platform
+
+from collections import deque
+from itertools import chain as chain_iterators
+from threading import Thread, Lock
+
+import time
+
+import eventnet.driver
+
+
+#core functions and classes
+
+def throw(e):
+    """
+    Raise an exception. This function exists, so that lambda functions can 
+    raise Exceptions.
+    """
+    raise e
+
+def synchronize(func):
+    """
+    A decorator which adds threading.Lock functionality around func.
+    """
+    lock = Lock()
+    def f(*args, **kw):
+        lock.acquire()
+        try:
+            r = func(*args, **kw)
+        finally:
+            lock.release()
+        return r
+    return f
+
+
+class _KillFibra(Exception): pass
+
+
+class NanoEvent(object): pass
+
+
+class UNBLOCK(NanoEvent):
+    """
+    Yield UNBLOCK() to spawn the next iteration into a seperate, OS level 
+    thread.
+    """
+    pass
+
+
+class CONTINUE(NanoEvent):
+    """
+    Yield CONTINUE() or None, to allow the next task in the schedule to 
+    iterate.
+    """
+    pass
+
+
+class SLEEP(NanoEvent):
+    """
+    Yield SLEEP() to make the task pause iterations for a period.
+    """
+    def __init__(self, seconds):
+        self.seconds = seconds
+
+
+class RESUME_ON_EVENT(NanoEvent):
+    """
+    Yield RESUME_ON_EVENT(event_name) to pause the task, and resume when 
+    event_name is posted.
+    """
+    def __init__(self, name):
+        self.name = name
+
+
+class Fibra(object):
+    """
+    Fibra is a Latin word, meaning fiber. Fibra instances are very light 
+    cooperatives threads, which are iterated by the Pool.
+    To create a Fibra instance, pass a generator function the the Pool().
+    register method, which will return a Fibra instance.
+    A Fibra instance can be .pause(d), .resume(d), .kill(ed), and .end(ed).
+    When a Fibra instance ends, it calls all functions which were registered 
+    with the Fibra.call_on_exit method.
+    When a Fibra instance is killed, it does not call any exit functions.
+    """
+    __slots__ = 'task', 'next', 'thread', 'exit_funcs'
+    
+    def __init__(self, task):
+        self.task = task
+        try:
+            self.next = self.task.next
+        except AttributeError, e:
+            raise AttributeError("Cannot install a function as a nanothread, use a generator function.")
+            
+        self.exit_funcs = []
+
+    def __repr__(self):
+        return "<%s object at 0x%X >" % (self.__class__.__name__, id(self))
+
+    def pause(self):
+        """
+        Stops the execution of the fibra, until the resume method is called.
+        """
+        self.next = lambda: None
+
+    def resume(self):
+        """
+        Resumes the fibra after it has been paused.
+        """
+        self.next = self.task.next
+        
+    def kill(self):
+        """
+        Stop the fibra, and do not call any registered exit functions.
+        """
+        self.next = lambda: throw(_KillFibra)
+
+    def end(self):
+        """
+        Stop the fibra, and call any registered exit functions.
+        """
+        self.next = lambda: throw(StopIteration)
+
+    def call_on_exit(self, func, *args, **kw):
+        """
+        Add a function which is called when the fibra terminates.
+        """
+        self.exit_funcs.append((func, list(args), dict(kw)))
+        
+    def call_exit_funcs(self):
+        """
+        Calls exit functions registered for this instance. This method is 
+        called automatically by the scheduler when the task finishes.
+        """
+        for func, arg, kw in self.exit_funcs:
+            func(*arg, **kw)
+
+    def spawn_thread(self, onThreadFinish):
+        """
+        Run the next iteration step of the task in a seperate thread.
+        """
+        def yield_one_step():
+            try:
+                self.task.next()
+            except Exception, e:
+                self.next = lambda: throw(e)
+            onThreadFinish(self)
+
+        self.thread = Thread(target=yield_one_step)
+        self.thread.start()
+        
+
+class Pool(object):
+    """
+    A Cooperative/Preemptive thread scheduler, implemented using generators.
+    """
+    __slots__ = ["pool", "running"]
+    def __init__(self):
+        self.pool = deque()
+        self.running = False
+
+    @synchronize
+    def install(self, generator):
+        """
+        Add a new generator (task) to the pool. Returns a fibra instance, 
+        which can control the task.
+        """
+        fibra = Fibra(generator)
+        self.pool.append(fibra)
+        return fibra
+        
+    @synchronize
+    def install_fibra(self, fibra):
+        """
+        Add a fibra to the pool. Returns the same Fibra.
+        """
+        self.pool.append(fibra)
+        return fibra
+        
+    def chain(self, *args):
+        """
+        Chain the execution of multiple generators.
+        """
+        return self.install(chain_iterators(*args))
+
+    def defer(self, func, *args, **kw):
+        """
+        Defer a function call until the next iteration of the pool.
+        """
+        def deferred(*args, **kw):
+            func(*args, **kw)
+            yield None
+            
+        return self.install(deferred(*args, **kw))
+
+    def defer_for(self, seconds, func, *args, **kw):
+        """
+        Defer a function call for a number of seconds.
+        """
+        def deferred(seconds, func, *args, **kw):
+            start = time_func()
+            while (time_func() - start) <= seconds: 
+                yield None
+            func(*args, **kw)
+            yield None
+            
+        return self.install(deferred(seconds, func, *args, **kw))
+        
+    def poll(self):
+        """
+        Iterate the execution queue. Used for integrating nanothreads with 
+        other mainloop constructs.
+        """
+        try:
+            task = self.pool.popleft()
+        except IndexError:
+            return
+        try:
+            r = task.next()
+        except StopIteration:
+            task.call_exit_funcs()
+            return
+        except _KillFibra:
+            return
+        if  isinstance(r, UNBLOCK):
+            task.spawn_thread(self.resume_from_thread)
+        elif isinstance(r, RESUME_ON_EVENT):
+            self.resume_on_event(r.name, task)
+        elif isinstance(r, SLEEP):
+            self.resume_later(r.seconds, task)
+        else:
+            self.pool.append(task)
+
+    def loop(self):
+        """
+        Start the scheduler, and keep running until all tasks have finished.
+        """
+        #The below assignments are simple optimisations, which help avoid multiple attribute lookups per loop.
+        self_pool_popleft = self.pool.popleft
+        self_pool_append = self.pool.append
+        self_pool = self.pool
+        self.running = True
+        while self.running:
+            try:
+                task = self_pool_popleft()
+            except IndexError:
+                continue
+            try:
+                r = task.next()
+            except StopIteration:
+                task.call_exit_funcs()
+                continue
+            except _KillFibra:
+                continue
+            if  isinstance(r, UNBLOCK):
+                task.spawn_thread(self.resume_from_thread)
+            elif isinstance(r, RESUME_ON_EVENT):
+                self.resume_on_event(r.name, task)
+            elif isinstance(r, SLEEP):
+                self.resume_later(r.seconds, task)
+            else:
+                self_pool_append(task)
+                
+    def shutdown(self):
+        """
+        Shutdown the looping scheduler cleanly.
+        """
+        self.running = False
+    
+    def resume_later(self, seconds, task):
+        def resume_task():
+            self.pool.append(task)
+        self.defer_for(seconds, resume_task)
+
+    def resume_on_event(self, event_name, task):
+        @eventnet.driver.subscribe(event_name)
+        def resume_task():
+            self.pool.append(task)
+            resume_task.release()
+            
+    def resume_from_thread(self, fibra):
+        def reinstall():
+            fibra.thread.join()
+            self.pool.append(fibra)
+        self.defer(reinstall)
+
+
+# This code generates global names which point to method names of a global 
+# Pool instance. This is so the user can use these names to access a default
+# global nanothread scheduler.
+
+__pool = Pool()
+pool = __pool.pool
+for name in dir(__pool):
+    if name[:2] != "__":
+        attr = getattr(__pool, name)
+        if callable(attr):
+            globals()[name] = attr
+
+
+#user functions and classes
+        
+class TaskQueue(object):
+    def __init__(self):
+        self.tasks = []
+        self.current_thread = None
+        
+    def _update(self):
+        if len(self.tasks) > 0 and self.current_thread is None:
+            self.current_thread = install(self.tasks.pop(0))
+            self.current_thread.call_on_exit(self._finish_task)
+    
+    def _finish_task(self):
+        self.current_thread = None
+        self._update()
+        
+    def install(self, task):
+        self.tasks.append(task)
+        self._update()
+        
+    def flush(self):
+        self.tasks[:] = []
+        if hasattr(self, 'current_thread'):
+            self.current_thread.kill()
+        self.current_thread = None
---
+++ packages/FibraNet/FibraNet-9.tar.gz/FibraNet-9/nanotubes.py
@@ -0,0 +1,337 @@
+"""
+
+The nanotubes module implements asynchronous socket handling, and provides
+protocol classes which simplify event passing over sockets.
+
+"""
+
+__author__ = "simonwittber@gmail.com"
+
+import socket
+import select
+import struct
+
+import gherkin as encoder
+
+from errno import EALREADY, EINPROGRESS, EWOULDBLOCK, ECONNRESET, ENOTCONN, ESHUTDOWN, EINTR, EISCONN
+
+
+#---------------------------------------------------------------------#
+#Utility classes
+
+class StringQueue(object):
+    """
+    A simple character buffer which is used for queing network bytes.
+    """
+    def __init__(self):
+        self.l_buffer = []
+        self.s_buffer = ""
+
+    def write(self, data):
+        """
+        Append's data to the buffer.
+        """
+        self.l_buffer.append(data)
+
+    def read(self, count=None):
+        """
+        Returns and removes a number of bytes from the buffer. If count is
+        None, all bytes are returned.
+        """
+        result = self.peek(count)
+        self.discard(count)
+        return result
+    
+    def discard(self, count):
+        """
+        Removes a number of bytes from the buffer.
+        """
+        self.s_buffer = self.s_buffer[count:]
+            
+    def peek(self, count=None):
+        """
+        Returns a number of bytes from the buffer, but does not remove them.
+        If count is None, all bytes are returned.
+        """
+        if count > len(self.s_buffer) or count==None:
+            self._build_s_buffer()
+        result = self.s_buffer[:count]
+        return result
+
+    def _build_s_buffer(self):
+        new_string = "".join(self.l_buffer)
+        self.s_buffer = "".join((self.s_buffer, new_string))
+        self.l_buffer = []
+
+    def __len__(self):
+        self._build_s_buffer()
+        return len(self.s_buffer)
+        
+
+#---------------------------------------------------------------------#
+#Base classes
+
+class SocketHandler(object):
+    """
+    The SocketHandler class registers all socket connections, and uses the
+    select function to trigger read, write and close calls on Connection 
+    objects.
+    """
+    sockets = []
+    socket_objects = {}
+    dead_sockets = []
+    
+    @classmethod
+    def poll(cls):
+        """
+        The poll method removes dead sockets, and triggers method calls on 
+        any Connection objects which are waiting for attention. This method
+        needs to be called often.
+        """
+        for s in cls.dead_sockets:
+            cls.sockets.remove(s)
+            del cls.socket_objects[s]
+        cls.dead_sockets[:] = []
+        if cls.sockets:
+            readable,writable,exceptional = select.select(cls.sockets,cls.sockets,cls.sockets,0)
+            for s in readable:
+                cls.socket_objects[s].handle_read()
+            for s in writable:
+                cls.socket_objects[s].handle_write()
+            for s in exceptional:
+                cls.socket_objects[s].handle_exception()
+            
+    @classmethod
+    def nanothread(cls):
+        """
+        The nanothread method returns a generator which can be installed into
+        the nanothread scheduler which will iterate the poll method.
+        """
+        while True:
+            cls.poll()
+            yield None
+    
+    @classmethod
+    def remove(cls, s):
+        """
+        Schedules a socket for deletion.
+        """
+        cls.dead_sockets.append(s)
+    
+    @classmethod
+    def add_socket(cls, s, s_o):
+        """
+        Adds a socket and socket_object to the handler. The socket_object 
+        will have handler methods called when activity occurs on the socket.
+        """
+        cls.sockets.append(s)
+        cls.socket_objects[s] = s_o
+
+
+class Connection(object):
+    def handle_close(self):
+        pass
+        
+    def handle_read(self):
+        pass
+
+    def handle_write(self):
+        pass
+    
+    def handle_exception(self):
+        pass
+        
+    
+class TCPConnection(Connection):
+    def __init__(self, tcp_socket, protocol):
+        self.protocol = protocol
+        self.tcp_socket = tcp_socket
+        self.tcp_socket.setblocking(0)
+        SocketHandler.add_socket(self.tcp_socket, self)
+        
+    def channel(self, port):
+        """
+        Opens a UDP channel on a port.
+        """
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return UDPConnection(udp_socket, (self.host, port))
+        
+    def handle_read(self):
+        try:
+            data = self.tcp_socket.recv(4096)
+        except socket.error:
+            self.handle_exception()
+        else:
+            if len(data) == 0:
+                self.handle_close()
+            self.protocol.input_buffer.write(data)
+            self.protocol.handle_read()
+
+    def handle_write(self):
+        data = self.protocol.output_buffer.peek(4096)
+        if data:
+            try:
+                count = self.tcp_socket.send(data)
+            except socket.error:
+                self.handle_exception()
+            else:
+                self.protocol.output_buffer.discard(count)
+    
+    def handle_exception(self):
+        self.protocol.handle_exception()
+        
+
+class UDPConnection(Connection):
+    def __init__(self, udp_socket, (host, port)):
+        self.udp_socket = udp_socket
+        self.udp_socket.setblocking(0)
+        self.host = host
+        self.port = port
+        self.transmissions = []
+        SocketHandler.add_socket(self.udp_socket, self)
+        
+    def listen(self, receiver):
+        """
+        Listen on this channel for incoming traffic.
+        receiver is a callback function which takes (address, data) as args.
+        """
+        self.receiver = receiver
+        self.udp_socket.bind((self.host, self.port))
+        self.handle_read = self._handle_read
+    
+    def handle_read(self):
+        #Do nothing by default.
+        pass
+        
+    def _handle_read(self):
+        try:
+            data, address = self.udp_socket.recvfrom(4096)
+        except socket.error, e:
+            self.handle_exception()
+        else:
+            self.receiver(address, data)
+            
+    def transmit(self, address, data):
+        """
+        Transmit data to address on this channel's port.
+        """
+        self.transmissions.append((address, data))
+    
+    def handle_write(self):
+        try:
+            address, data = self.transmissions.pop(-1)
+        except IndexError:
+            pass
+        else:
+            try:
+                count = self.udp_socket.sendto(data, (address, self.port))
+            except socket.error, e:
+                self.handle_exception()
+            
+    def handle_exception(self):
+        print 'UDP Exception.'
+        pass
+
+
+class Protocol(object):
+    def __init__(self):
+        self.input_buffer = StringQueue()
+        self.output_buffer = StringQueue()
+        
+    def handle_close(self):
+        pass
+    
+    def handle_read(self):
+        pass
+    
+    def handle_exception(self):
+        pass
+
+
+class Listener(TCPConnection):
+    """
+    Opens a socket and starts listening for connections.
+    """
+    def __init__(self, (host, port), protocol_factory):
+        TCPConnection.__init__(self, socket.socket(socket.AF_INET, socket.SOCK_STREAM), self)
+        self.host = host
+        self.port = port
+        self.protocol_factory = protocol_factory
+        self.tcp_socket.bind((host, port))
+        self.tcp_socket.listen(5)
+        
+    def handle_new(self, tcp_connection):
+        pass
+        
+    def handle_read(self):
+        new_socket, address = self.tcp_socket.accept()
+        c = TCPConnection(new_socket, self.protocol_factory())
+        self.handle_new(c)
+
+
+class Connector(TCPConnection):
+    """
+    Connects to a socket on a remote host.
+    """
+    def __init__(self, (host, port), protocol_factory):
+        TCPConnection.__init__(self, socket.socket(socket.AF_INET, socket.SOCK_STREAM), protocol_factory())
+        self.host = host
+        self.port = port
+        self.connect()
+        
+    def connect(self):
+        err = self.tcp_socket.connect_ex((self.host, self.port))
+        if err in (EINPROGRESS, EALREADY, EWOULDBLOCK):
+            pass
+        else:
+            raise socket.error, err
+    
+
+#---------------------------------------------------------------------#
+#User/Derived classes
+
+class MessageProtocol(Protocol):
+    """
+    The message protocol fires and forgets events over a connections.
+    Events are (name, dict) pairs. Recieved events are queued up in the
+    messages attribute.
+    """
+    def __init__(self):
+        Protocol.__init__(self)
+        self.unpacker = self._create_unpacker(self.input_buffer)
+        self.messages = []
+    
+    def _create_unpacker(self, buffer):
+        """
+        This generator yields new messages from the incoming buffer.
+        """
+        while True:
+            if len(buffer) >= 4:
+                size = struct.unpack("!l", buffer.read(4))[0]
+                while len(buffer) < size:
+                    yield None
+                message = buffer.read(size)
+                yield message
+            else:
+                yield None
+
+    def pack_message(self, name, kw):
+        data = encoder.dumps((name, kw))
+        size = struct.pack("!l", len(data))
+        return "".join((size, data))
+    
+    def post(self, name, **kw):
+        """
+        Post an event over the connection. The name argument is the event
+        name, kw arguments are the event attributes.
+        """
+        data = self.pack_message(name, kw)
+        self.output_buffer.write(data)
+
+    def handle_read(self):
+        for message in self.unpacker:
+            if message is not None:
+                self.messages.append(encoder.loads(message))
+            else: 
+                break
+
