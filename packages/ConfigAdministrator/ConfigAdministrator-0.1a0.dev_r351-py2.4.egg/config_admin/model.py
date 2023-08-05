09fcab5766e2d3b13aec89bf6c6eee43ab10da32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:35 +0000

    Add package ConfigAdministrator/0.1a0.dev-r351/ConfigAdministrator-0.1a0.dev_r351-py2.4.egg

---
+++ packages/ConfigAdministrator/ConfigAdministrator-0.1a0.dev_r351-py2.4.egg/config_admin/model.py
@@ -0,0 +1,21 @@
+from sqlobject import *
+
+from turbogears.database import PackageHub
+
+hub = PackageHub("turbogears.config_admin")
+__connection__ = hub
+
+def create_models():
+    hub.begin()
+    ConfigOption.createTable(ifNotExists=True)
+    hub.commit()
+    hub.end()
+
+class ConfigOption(SQLObject):
+    class sqlmeta:
+        table = "configuration"
+    name = StringCol(length=255)
+    panel = StringCol(length=255)
+    value = BLOBCol(length=16*1024**2)
+    
+
