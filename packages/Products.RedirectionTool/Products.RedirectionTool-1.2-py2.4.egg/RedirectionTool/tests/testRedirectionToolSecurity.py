320531453f50749735a32bc30555bb6d0ff212b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package Products.RedirectionTool/1.2/Products.RedirectionTool-1.2-py2.4.egg

---
+++ packages/Products.RedirectionTool/Products.RedirectionTool-1.2-py2.4.egg/RedirectionTool/tests/testRedirectionToolSecurity.py
@@ -0,0 +1,54 @@
+#
+# Skeleton PloneTestCase
+#
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from Testing import ZopeTestCase
+import RedirectionToolTestCase
+import utils
+
+class TestRedirectionToolSecurity(RedirectionToolTestCase.RedirectionToolTestCase):
+
+    def afterSetUp(self):
+        RedirectionToolTestCase.RedirectionToolTestCase.afterSetUp(self)
+        self.portal.acl_users._doAddUser('user1', 'secret', ['Member'], [])
+        self.portal.acl_users._doAddUser('user2', 'secret', ['Member'], [])
+        self.membership = self.portal.portal_membership
+        self.createMemberarea('user1')
+        self.createMemberarea('user2')
+
+    def testCheckPermission(self):
+        portal_path = self.portal.getPhysicalPath()
+        self.login('user1')
+        folder = self.membership.getHomeFolder('user1')
+        testobj = utils.makeContent(folder, 'Document', 'testobj')
+        folder_path = folder.getPhysicalPath()[len(portal_path):]
+        testurl = '/%s/foo' % '/'.join(folder_path)
+        # user should be able to add redirects to his own objects
+        self.failUnless(self.rt.addRedirect(testurl, testobj))
+        self.failUnlessEqual(self.rt.getRedirectObject(testurl), testobj)
+
+        folder = self.membership.getHomeFolder('user2')
+        folder_path = folder.getPhysicalPath()[len(portal_path):]
+        testurl = '/%s/foo' % '/'.join(folder_path)
+        # but only if the alias is in a place which belongs to him
+        self.failIf(self.rt.addRedirect(testurl, testobj))
+        self.failUnlessEqual(self.rt.getRedirectObject(testurl), None)
+        testurl = '/bar'
+        self.failIf(self.rt.addRedirect(testurl, testobj))
+        self.failUnlessEqual(self.rt.getRedirectObject(testurl), None)
+
+
+if __name__ == '__main__':
+    framework()
+else:
+    # While framework.py provides its own test_suite()
+    # method the testrunner utility does not.
+    from unittest import TestSuite, makeSuite
+    def test_suite():
+        suite = TestSuite()
+        suite.addTest(makeSuite(TestRedirectionToolSecurity))
+        return suite
