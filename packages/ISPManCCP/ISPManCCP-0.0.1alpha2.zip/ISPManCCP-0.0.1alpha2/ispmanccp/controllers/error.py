6a671834e6d4fca56c06923cffdc73819fb7599c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:15 +0000

    Add package ISPManCCP/0.0.1alpha2/ISPManCCP-0.0.1alpha2.zip

---
+++ packages/ISPManCCP/ISPManCCP-0.0.1alpha2.zip/ISPManCCP-0.0.1alpha2/ispmanccp/controllers/error.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4 ts=4 fenc=utf-8
+# =============================================================================
+# $Id: error.py 2 2006-08-26 17:51:50Z s0undt3ch $
+# =============================================================================
+#             $URL: http://ccp.ufsoft.org/svn/tags/0.0.1alpha2/ispmanccp/controllers/error.py $
+# $LastChangedDate: 2006-08-26 18:51:50 +0100 (Sat, 26 Aug 2006) $
+#             $Rev: 2 $
+#   $LastChangedBy: s0undt3ch $
+# =============================================================================
+# Copyright (C) 2006 Ufsoft.org - Pedro Algarvio <ufs@ufsoft.org>
+#
+# Please view LICENSE for additional licensing information.
+# =============================================================================
+
+import os.path
+from paste import fileapp
+from pylons.middleware import media_path, error_document_template
+from pylons.util import get_prefix
+from ispmanccp.lib.base import *
+
+class ErrorController(BaseController):
+    """
+    Class to generate error documents as and when they are required. This behaviour of this
+    class can be altered by changing the parameters to the ErrorDocuments middleware in 
+    your config/middleware.py file.
+    """
+
+    def document(self):
+        """
+        Change this method to change how error documents are displayed
+        """
+        page = error_document_template % {
+            'prefix': get_prefix(request.environ),
+            'code': request.params.get('code', ''),
+            'message': request.params.get('message', ''),
+        }
+        return Response(page)
+
+    def img(self, id):
+        return self._serve_file(os.path.join(media_path, 'img', id))
+        
+    def style(self, id):
+        return self._serve_file(os.path.join(media_path, 'style', id))
+
+    def _serve_file(self, path):
+        fapp = fileapp.FileApp(path)
+        return fapp(request.environ, self.start_response)
---
+++ packages/ISPManCCP/ISPManCCP-0.0.1alpha2.zip/ISPManCCP-0.0.1alpha2/setup.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: sw=4 ts=4 fenc=utf-8
+# =============================================================================
+# $Id: setup.py 92 2006-12-09 22:30:03Z s0undt3ch $
+# =============================================================================
+#             $URL: http://ccp.ufsoft.org/svn/tags/0.0.1alpha2/setup.py $
+# $LastChangedDate: 2006-12-09 22:30:03 +0000 (Sat, 09 Dec 2006) $
+#             $Rev: 92 $
+#   $LastChangedBy: s0undt3ch $
+# =============================================================================
+# Copyright (C) 2006 Ufsoft.org - Pedro Algarvio <ufs@ufsoft.org>
+#
+# Please view LICENSE for additional licensing information.
+# =============================================================================
+
+from sys import exit
+from setuptools import setup, find_packages
+
+try:
+    # Let's find out if we have python-ldap
+    import ldap
+except ImportError:
+    # We don't have python-ldap, exit nicely
+    print
+    print "You must have the python-ldap module instaled."
+    print "Most distributions already provide it, just install it."
+    print "As an alternative, you can get it from:"
+    print "   http://python-ldap.sourceforge.net/"
+    print
+    exit(1)
+
+
+try:
+    # Let's find out if we have PyPerl installed
+    import perl
+    if perl.MULTI_PERL:
+        print
+        print "You already have PyPerl installed but it's thread enabled and that"
+        print "ill make ISPMan CCP not work correclty."
+        print "You must either recompile your PyPerl without the MULTI_PERL file,"
+        print "or, completely remove PyPerl from your system and allow ISPMan CCP"
+        print "install it's own."
+        print
+        print "A simple way to remove PyPerl from you system is:"
+        print "  rm -rf /usr/lib/python2.4/site-packages/perl* && \ "
+        print "  rm -rf /usr/local/lib/perl/5.8.8/auto/Python && \ "
+        print "  rm -rf /usr/lib/perl/5.8.8/auto/Python"
+        print "That shoud take care of PyPerl. Of course adapt to your system's"
+        print "Python and Perl versions"
+        print
+        exit(1)
+except ImportError:
+    # We don't have PyPerl, so, install it
+    import os, subprocess
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(os.path.join(cur_dir, 'extra-packages', 'pyperl-1.0.1d'))
+    retcode = subprocess.call(['python', './setup.py', 'install'])
+    os.chdir(cur_dir)
+
+
+
+# We now resume normal setup operation
+
+
+VERSION = "0.0.1alpha2"
+
+readme_file = open('README.txt')
+
+setup(
+    name = 'ISPManCCP',
+    version = VERSION,
+    description = "Customer Control Panel for ISPMan",
+    long_description = readme_file.read(),
+    license = 'BSD',
+    platforms = "Anywhere you've got ISPMan working.",
+    author = "Pedro Algarvio",
+    author_email = "ufs@ufsoft.org",
+    url = "http://ccp.ufsoft.org/",
+    #download_url = "http://ccp.ufsoft.org/download/%s/" % VERSION,
+    zip_safe = False,
+    install_requires = [
+        "Pylons>=0.9.3",
+        "Genshi>=0.3.4",
+        "formencode>=0.6",
+    ],
+    packages = find_packages(),
+    include_package_data = True,
+    test_suite = 'nose.collector',
+    package_data = {'ispmanccp': ['i18n/*/LC_MESSAGES/*.mo']},
+    entry_points = """
+    [paste.app_factory]
+    main=ispmanccp:make_app
+    [paste.app_install]
+    main=paste.script.appinstall:Installer
+    """,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Customer Service',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Natural Language :: Portuguese',
+        'Programming Language :: Python',
+        'Programming Language :: Perl',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Utilities',
+    ],
+    keywords = "ISPMan PyPerl Python Customer Control Pannel"
+
+)
