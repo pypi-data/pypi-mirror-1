f16cddbbb7307c7caa40f1e68e469dc5fd2bdc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package pygnet/0.1/pygnet-0.1-py2.5.egg

---
+++ packages/pygnet/pygnet-0.1-py2.5.egg/pygnet/protocol.py
@@ -0,0 +1,27 @@
+import marshal
+
+from twisted.protocols import basic
+
+
+
+class GameProtocol(basic.Int32StringReceiver):
+    def connectionMade(self):
+        self.factory.new_connection(self)
+
+    def connectionLost(self, reason):
+        self.factory.lost_connection(self)
+
+    def stringReceived(self, data):
+        obj = marshal.loads(data)
+        self.factory.receive_object(self, obj)
+
+    def send_object(self, obj):
+        data = marshal.dumps(obj)
+        self.sendString(data)
+
+
+
+
+
+    
+
