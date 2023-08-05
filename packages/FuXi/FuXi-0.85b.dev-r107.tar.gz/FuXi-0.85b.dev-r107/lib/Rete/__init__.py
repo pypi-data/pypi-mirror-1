3d479982ab5175bd15e57fd3974e6c34c6d684c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:22 +0000

    Add package FuXi/0.85b.dev-r107/FuXi-0.85b.dev-r107.tar.gz

---
+++ packages/FuXi/FuXi-0.85b.dev-r107.tar.gz/FuXi-0.85b.dev-r107/lib/Rete/__init__.py
@@ -0,0 +1,5 @@
+from Network import ReteNetwork
+from BetaNode import BetaNode
+from AlphaNode import AlphaNode,ReteToken,BuiltInAlphaNode
+from ReteVocabulary import RETE_NS
+from DescriptionHornLogic import T
