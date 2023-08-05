add78e5ba656e24858092ecd8e39e38118d4a261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:47 +0000

    Add package rwpopen/1.0/rwpopen-1.0.tar.gz

---
+++ packages/rwpopen/rwpopen-1.0.tar.gz/rwpopen-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='rwpopen',
+      version='1.0',
+      py_modules=['rwpopen'],
+      author="Hrvoje Niksic",
+      author_email="hniksic@srce.hr",
+      maintainer="Asheesh Laroia",
+      maintainer_email="asheesh@asheesh.org",
+      description="A module for passing strings to programs and reading the output without a temporary file or deadlocks.",
+#      url="http://www.asheesh.org/something",
+      )
+# source = http://mail.python.org/pipermail/python-list/1999-October/013897.html
