219fa03d4b8fa973b7c6c495ed0c57581c907141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:37 +0000

    Add package pytnef/0.1/pytnef-0.1.zip

---
+++ packages/pytnef/pytnef-0.1.zip/pytnef-0.1/setup.py
@@ -0,0 +1,39 @@
+"""pytnef library: access MS TNEF-coded files (winmail.dat)
+"""
+
+classifiers = """\
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Software Development :: Libraries
+Topic :: Communications :: Email
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Operating System :: POSIX
+"""
+
+import sys
+from distutils.core import setup
+
+if sys.version_info < (2, 3):
+    _setup = setup
+    def setup(**kwargs):
+        if kwargs.has_key("classifiers"):
+            del kwargs["classifiers"]
+        _setup(**kwargs)
+
+doclines = __doc__.split("\n")
+
+setup(name="pytnef",
+      version="0.1",
+      maintainer="Petri Savolainen",
+      maintainer_email="petri.savolainen@iki.fi",
+      license = "http://www.fsf.org/licensing/licenses/lgpl.txt",
+      platforms = ["any"],
+      py_modules = ("tnef",),
+      description = doclines[0],
+      classifiers = filter(None, classifiers.split("\n")),
+      long_description = "\n".join(doclines[2:]),
+)
