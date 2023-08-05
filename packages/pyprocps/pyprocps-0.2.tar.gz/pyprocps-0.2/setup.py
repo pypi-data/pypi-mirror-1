9642fdf715c8e018dcc25b6c49f306a262463169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:04 +0000

    Add package pyprocps/0.2/pyprocps-0.2.tar.gz

---
+++ packages/pyprocps/pyprocps-0.2.tar.gz/pyprocps-0.2/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+setup (name='pyprocps',
+    version='0.2',
+    py_modules=['pyprocps'],
+    description='a python interface to the infomation found in the /proc filesystem',
+    author='Eli Criffield',
+    author_email='pyprocps@criffield.net',
+    url='http://eli.criffield.net/pyprocps/',
+    license='Python Foundation License',
+    platforms='UNIX',
+)
