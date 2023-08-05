d35152a05e9d939932a02e4ba4aef6a4c9a36c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:26 +0000

    Add package collective.formlib.link/0.1/collective.formlib.link-0.1.tar.gz

---
+++ packages/collective.formlib.link/collective.formlib.link-0.1.tar.gz/collective.formlib.link-0.1/collective/formlib/link/widget.py
@@ -0,0 +1,19 @@
+from zope.app.form.browser.textwidgets import TextWidget
+from zope.schema.interfaces import IFromUnicode
+
+from field import Link
+
+class LinkWidget(TextWidget):
+    type = 'Link'
+    
+    def _toFieldValue(self, input):
+        if self.convert_missing_value and input == self._missing:
+            value = self.context.missing_value
+        else:
+            assert IFromUnicode.providedBy(self.context)
+
+            try:
+                value = self.context.fromUnicode(input)
+            except ValueError, v:
+                raise ConversionError(_("Invalid link"), v)
+        return value
