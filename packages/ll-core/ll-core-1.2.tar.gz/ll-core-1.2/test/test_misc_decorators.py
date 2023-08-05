d74635d4daf6e4d54fd37f08a15d8fd6de6e3b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:31 +0000

    Add package ll-core/1.2/ll-core-1.2.tar.gz

---
+++ packages/ll-core/ll-core-1.2.tar.gz/ll-core-1.2/test/test_misc_decorators.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env/python
+# -*- coding: iso-8859-1 -*-
+
+## Copyright 2005 by LivingLogic AG, Bayreuth/Germany.
+## Copyright 2005 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See __init__.py for the license
+
+
+import py.test
+
+from ll import misc
+
+
+def test_notimplemented():
+	class Bad(object):
+		@misc.notimplemented
+		def bad(self):
+			pass
+
+	py.test.raises(NotImplementedError, Bad().bad)
