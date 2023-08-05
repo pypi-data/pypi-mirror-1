a2e1813cef738fa99b6319b7138bc6c0ec87e118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package zopyx.convert/0.4.4/zopyx.convert-0.4.4-py2.4.egg

---
+++ packages/zopyx.convert/zopyx.convert-0.4.4-py2.4.egg/zopyx/convert/util.py
@@ -0,0 +1,45 @@
+##########################################################################
+# zopyx.convert - XSL-FO related functionalities
+#
+# (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+import sys
+import tempfile
+import logging
+from subprocess import Popen, PIPE
+
+win32 = (sys.platform=='win32')
+LOG = logging.getLogger()
+
+def newTempfile(suffix=''):
+    return tempfile.mktemp(suffix=suffix)
+
+
+def runcmd(cmd):                
+    """ Execute a command using the subprocess module """
+
+    if win32:
+        raise NotImplementedError('No windows support so far')
+
+    else:      
+
+        stdin = open('/dev/null')
+        stdout = stderr = PIPE
+
+        p = Popen(cmd, 
+                  shell=True,
+                  stdin=stdin,
+                  stdout=stdout,
+                  stderr=stderr,
+                  )
+
+        status = p.wait()
+        stdout_ = p.stdout.read().strip()
+        stderr_ = p.stderr.read().strip()
+        if stdout_:
+            LOG.info(stdout_)
+        if stderr_:
+            LOG.info(stderr_)
+        return status, stdout_ + stderr_
+
