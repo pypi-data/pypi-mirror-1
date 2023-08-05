bb4ee93dcfc4da3363f6b2aab104172f1836d8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package huffman-encoder-decoder/0.1.2/huffman-0.1.2.tar.gz

---
+++ packages/huffman-encoder-decoder/huffman-0.1.2.tar.gz/huffman-0.1.2/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup, Extension
+
+setup(name="huffman encoder/decoder", version="0.1.2",
+      author="Guilherme Polo", author_email="ggpolo@gmail.com",
+      license="GPL", url="http://gpolo.ath.cx:81/texts/opc",
+      packages=["huffman"])
