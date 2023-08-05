a87f03fba33239d27aff32e02754e81d54b7d951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:03 +0000

    Add package plone.portlet.collection/0.1/plone.portlet.collection-0.1.tar.gz

---
+++ packages/plone.portlet.collection/plone.portlet.collection-0.1.tar.gz/plone.portlet.collection-0.1/plone/portlet/collection/__init__.py
@@ -0,0 +1,3 @@
+from zope.i18nmessageid import MessageFactory
+CollectionMessageFactory = MessageFactory('plone.portlet.collection')
+
---
+++ packages/plone.portlet.collection/plone.portlet.collection-0.1.tar.gz/plone.portlet.collection-0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='plone.portlet.collection',
+      version=version,
+      description="A portlet that fetches results from a collection",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Geir Baekholt - Jarn',
+      author_email='baekholt@jarn.com',
+      url='http://svn.plone.org/svn/plone/plone.portlet.collection',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          'plone.memoize',
+          'plone.portlets',
+          'plone.app.portlets',
+          'plone.app.vocabularies',
+          'plone.app.form',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
