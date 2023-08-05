fde62d74603a9e404e60e49971e347b464097a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package cns.recipe.zmysqlda/2.0.8/cns.recipe.zmysqlda-2.0.8.tar.gz

---
+++ packages/cns.recipe.zmysqlda/cns.recipe.zmysqlda-2.0.8.tar.gz/cns.recipe.zmysqlda-2.0.8/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '2.0.8'
+name='cns.recipe.zmysqlda'
+setup(name=name,
+      version=version,
+      description="Recipe for installing ZMySQLDA",
+      long_description="""\
+cns.recipe.zmysqlda is simple recipe used for installing ZMySQLDA from tarball. ZMySQLDA 2.0.8 tarball
+has structure lib/python/Products/ZMySQLDA/.... and has to be extraceted manually.
+
+Example::
+
+  [zmysqlda]
+  recipe = cns.recipe.zmysqlda
+  target = ${buildout:directory}/products
+
+**target** is optional. Defaults to *${buildout:directory}/products* 
+""",
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='buildout',
+      author='Radim Novotny',
+      author_email='radim.novotny@corenet.cz',
+      url='http://corenet-int.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cns.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+	  install_requires = ['zc.buildout', 'setuptools'],
+      entry_points = {'zc.buildout':
+                    ['default = %s:Recipe' % name]},
+      )
