ab7fbcebb05464d771e19bee05012c2bb031b92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:57 +0000

    Add package cssutils/0.9.5a4/cssutils-0.9.5a4.zip

---
+++ packages/cssutils/cssutils-0.9.5a4.zip/cssutils-0.9.5a4/README.txt
@@ -0,0 +1,62 @@
+=============================================
+                 cssutils
+=============================================
+---------------------------------------------
+CSS Cascading Style Sheets library for Python
+---------------------------------------------
+:Author: $LastChangedBy: cthedot $
+:Copyright: 2004-2008 Christof Hoeke
+:Date: $LastChangedDate: 2008-02-10 22:40:40 +0100 (So, 10 Feb 2008) $
+:Version: (rev $LastChangedRevision: 994 $)
+
+Overview
+========
+A Python package to parse and build CSS Cascading Style Sheets.
+
+Based upon and partly implements the following specifications (DOM only, not any rendering facilities):
+
+`DOM Level 2 Style CSS <http://www.w3.org/TR/DOM-Level-2-Style/css.html>`__
+    DOM for package css
+`DOM Level 2 Style Stylesheets <http://www.w3.org/TR/DOM-Level-2-Style/stylesheets.html>`__
+    DOM for package stylesheets
+`CSSOM <http://dev.w3.org/csswg/cssom/>`__
+    A few details (mainly the NamespaceRule DOM) is taken from here. Plan is to move implementation to the stuff defined here which is newer but still no REC so might change anytime...
+
+`CSS 2.1 <http://www.w3.org/TR/CSS21/>`__
+    Rules and properties are defined here
+`CSS 2.1 Errata  <http://www.w3.org/Style/css2-updates/CR-CSS21-20070719-errata.html>`__
+    A few erratas, mainly the definition of CHARSET_SYM tokens
+`CSS3 module: Syntax <http://www.w3.org/TR/css3-syntax/>`__
+    Used in parts since cssutils 0.9.4 which basically tries to use the features from CSS 2.1 and CSS 3.
+`MediaQueries <http://www.w3.org/TR/css3-mediaqueries/>`__
+    MediaQueries are part of ``stylesheets.MediaList`` since v0.9.4, used in @import and @media rules.
+`Namespaces <http://dev.w3.org/csswg/css3-namespace/>`__
+    Added in v0.9.1, updated to definition in CSSOM in v0.9.4, updated in 0.9.5 for dev version
+`Selectors <http://www.w3.org/TR/css3-selectors/>`__
+    The selector syntax defined here (and not in CSS 2.1) should be parsable with cssutils (*should* mind though ;) )
+
+Please visit http://cthedot.de/cssutils/ for full details.
+
+
+license
+=======
+Published under the LGPL, see http://cthedot.de/cssutils/license.html
+
+
+installation
+============
+From 0.9 cssutils uses EasyInstall. Please find installation instructions and more information about EasyInstall from http://peak.telecommunity.com/DevCenter/EasyInstall#installation-instructions.
+
+After installing EasyInstall simple use::
+
+    > easy_install cssutils
+
+to install the latest version of cssutils.
+
+Alternatively download the provided source distribution. Expand the file and from a command line install with::
+
+    > python setup.py install
+
+Before using EasyInstall the first time or using the sdist please remove any old version which should be installed at PYTHONDIR/Lib/site-packages/cssutils.
+
+
---
+++ packages/cssutils/cssutils-0.9.5a4.zip/cssutils-0.9.5a4/src/cssutils/tests/test_cssunknownrule.py
@@ -0,0 +1,121 @@
+"""testcases for cssutils.css.CSSUnkownRule"""
+__author__ = '$LastChangedBy: cthedot $'
+__date__ = '$LastChangedDate: 2008-02-12 23:43:05 +0100 (Di, 12 Feb 2008) $'
+__version__ = '$LastChangedRevision: 1049 $'
+
+import xml.dom
+import test_cssrule
+import cssutils
+
+class CSSUnknownRuleTestCase(test_cssrule.CSSRuleTestCase):
+
+    def setUp(self):
+        super(CSSUnknownRuleTestCase, self).setUp()
+        self.r = cssutils.css.CSSUnknownRule()
+        self.rRO = cssutils.css.CSSUnknownRule(readonly=True)
+        self.r_type = cssutils.css.CSSUnknownRule.UNKNOWN_RULE
+        self.r_typeString = 'UNKNOWN_RULE'
+
+    def test_init(self):
+        "CSSUnknownRule.type and init"
+        super(CSSUnknownRuleTestCase, self).test_init()
+
+        self.assertFalse(self.r.wellformed)
+
+        # only name
+        r = cssutils.css.CSSUnknownRule(cssText=u'@init;')
+        self.assertEqual(u'@init', r.atkeyword)
+        self.assertEqual(u'@init;', r.cssText)
+        self.assertTrue(r.wellformed)
+
+        # @-... not allowed?
+        r = cssutils.css.CSSUnknownRule(cssText=u'@-init;')
+        self.assertEqual(u'@-init;', r.cssText)
+        self.assertEqual(u'@-init', r.atkeyword)
+        self.assertTrue(r.wellformed)
+
+        r = cssutils.css.CSSUnknownRule(cssText=u'@_w-h-a-012;')
+        self.assertEqual(u'@_w-h-a-012;', r.cssText)
+        self.assertEqual(u'@_w-h-a-012', r.atkeyword)
+        self.assertTrue(r.wellformed)
+
+        # name and content
+        r = cssutils.css.CSSUnknownRule(cssText=u'@init xxx;')
+        self.assertEqual(u'@init', r.atkeyword)
+        self.assertEqual(u'@init xxx;', r.cssText)
+        self.assertTrue(r.wellformed)
+
+        # name and block
+        r = cssutils.css.CSSUnknownRule(cssText=u'@init { xxx }')
+        self.assertEqual(u'@init', r.atkeyword)
+        self.assertEqual(u'@init {\n    xxx\n    }', r.cssText)
+        self.assertTrue(r.wellformed)
+
+        # name and content and block
+        r = cssutils.css.CSSUnknownRule(cssText=u'@init xxx { yyy }')
+        self.assertEqual(u'@init', r.atkeyword)
+        self.assertEqual(u'@init xxx {\n    yyy\n    }', r.cssText)
+        self.assertTrue(r.wellformed)
+
+    def test_InvalidModificationErr(self):
+        "CSSUnknownRule.cssText InvalidModificationErr"
+        self._test_InvalidModificationErr(u'@unknown')
+
+    def test_cssText(self):
+        "CSSUnknownRule.cssText"
+        tests = {
+            '@x;': None,
+            '@x {}': u'@x {\n}',
+            '@x{ \n \t \f\r}': u'@x {\n}',
+            '@x {\n    [()]([ {\n    }]) {\n    }\n    }': None,
+            '@a {\n    @b;\n    }': None,
+            '''@a {
+    @b {
+        x: 1x;
+        y: 2y;
+        }
+    }''': None,
+            '@x "string" url(x);': None,
+            "@x'string'url('x');": '@x "string" url(x);',
+        }
+        self.do_equal_p(tests)
+        self.do_equal_r(tests)
+
+    def test_SyntaxErr(self):
+        "CSSUnknownRule.cssText"
+        # at keyword 
+        self.assertRaises(xml.dom.InvalidModificationErr,
+                          self.r._setCssText, '@;')
+        self.assertRaises(xml.dom.InvalidModificationErr,
+                          self.r._setCssText, '@{}')
+        self.assertRaises(xml.dom.InvalidModificationErr,
+                          self.r._setCssText, '@ ;')
+        self.assertRaises(xml.dom.InvalidModificationErr,
+                          self.r._setCssText, '@ {}')
+        # rule end
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x {}{}')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x {};')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x ;{}')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x ;;')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x }  ')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x }  ;')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x {  ')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x {  ;')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x ')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x (;')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x );')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x [;')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x ];')
+        self.assertRaises(xml.dom.SyntaxErr, self.r._setCssText, '@x {[(]()}')
+
+    def test_reprANDstr(self):
+        "CSSUnknownRule.__repr__(), .__str__()"        
+        s = cssutils.css.CSSUnknownRule(cssText='@x;')
+        
+        s2 = eval(repr(s))
+        self.assert_(isinstance(s2, s.__class__))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
