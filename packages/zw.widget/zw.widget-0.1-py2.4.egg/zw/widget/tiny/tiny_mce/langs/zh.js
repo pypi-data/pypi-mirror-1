a9ccb9418246502c07f5c93ddd35e829873bf800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:45 +0000

    Add package zw.widget/0.1/zw.widget-0.1-py2.4.egg

---
+++ packages/zw.widget/zw.widget-0.1-py2.4.egg/zw/widget/tiny/tiny_mce/langs/zh.js
@@ -0,0 +1,154 @@
+tinyMCE.addI18n({zh:{
+common:{
+edit_confirm:" \u662F\u5426\u5728\u6B64\u6587\u672C\u57DF\u555F\u7528 \u300C \u6240\u898B\u5373\u6240\u5F97 \u300D \u7DE8\u8F2F\u6A21\u5F0F\uFF1F ",
+apply:" \u5957\u7528 ",
+insert:" \u63D2\u5165 ",
+update:" \u66F4\u65B0 ",
+cancel:" \u53D6\u6D88 ",
+close:" \u95DC\u9589 ",
+browse:" \u700F\u89BD ",
+class_name:" \u6A23\u5F0F Class",
+not_set:"-- \u672A\u8A2D\u7F6E --",
+clipboard_msg:" \u8907\u88FD\u3001\u526A\u4E0B\u3001\u8CBC\u4E0A\u529F\u80FD\u5728 Mozilla \u548C Firefox \u4E2D\u4E0D\u53EF\u7528\u3002 \n \u662F\u5426\u67E5\u770B\u66F4\u591A\u8AAA\u660E\u4FE1\u606F\uFF1F ",
+clipboard_no_support:" \u5C1A\u4E0D\u652F\u6301\u60A8\u6240\u4F7F\u7528\u7684\u700F\u89BD\u5668\uFF0C\u8ACB\u4F7F\u7528\u9375\u76E4\u5FEB\u901F\u9375\u3002 ",
+popup_blocked:" \u62B1\u6B49\uFF0C\u7121\u6CD5\u6253\u958B\u65B0\u7A97\u53E3\uFF0C\u8ACB\u8A2D\u7F6E\u60A8\u7684\u700F\u89BD\u5668\u6216\u76F8\u95DC\u5DE5\u5177\uFF0C\u5141\u8A31\u672C\u7DB2\u7AD9\u5F48\u51FA\u65B0\u7A97\u53E3\uFF0C\u4EE5\u4FBF\u7E7C\u7E8C\u4F7F\u7528\u6B64\u529F\u80FD\u3002 ",
+invalid_data:" \u932F\u8AA4 : \u8F38\u5165\u503C\u7121\u6548 , \u5DF2\u7528\u7D05\u8272\u6A19\u51FA .",
+more_colors:" \u66F4\u591A\u984F\u8272 "
+},
+contextmenu:{
+align:" \u5C0D\u9F4A\u65B9\u5F0F ",
+left:" \u9760\u5DE6\u5C0D\u9F4A ",
+center:" \u7F6E\u4E2D ",
+right:" \u9760\u53F3\u5C0D\u9F4A ",
+full:" \u5DE6\u53F3\u5C0D\u9F4A "
+},
+insertdatetime:{
+date_fmt:"%Y-%m-%d",
+time_fmt:"%H:%M:%S",
+insertdate_desc:" \u63D2\u5165\u7576\u524D\u65E5\u671F ",
+inserttime_desc:" \u63D2\u5165\u7576\u524D\u6642\u9593 ",
+months_long:" \u4E00\u6708 , \u4E8C\u6708 , \u4E09\u6708 , \u56DB\u6708 , \u4E94\u6708 , \u516D\u6708 , \u4E03\u6708 , \u516B\u6708 , \u4E5D\u6708 , \u5341\u6708 , \u5341\u4E00\u6708 , \u5341\u4E8C\u6708 ",
+months_short:" \u4E00\u6708 , \u4E8C\u6708 , \u4E09\u6708 , \u56DB\u6708 , \u4E94\u6708 , \u516D\u6708 , \u4E03\u6708 , \u516B\u6708 , \u4E5D\u6708 , \u5341\u6708 , \u5341\u4E00\u6708 , \u5341\u4E8C\u6708 ",
+day_long:" \u661F\u671F\u65E5 , \u661F\u671F\u4E00 , \u661F\u671F\u4E8C , \u661F\u671F\u4E09 , \u661F\u671F\u56DB , \u661F\u671F\u4E94 , \u661F\u671F\u516D , \u661F\u671F\u65E5 ",
+day_short:" \u9031\u65E5 , \u9031\u4E00 , \u9031\u4E8C , \u9031\u4E09 , \u9031\u56DB , \u9031\u4E94 , \u9031\u516D , \u9031\u65E5 "
+},
+print:{
+print_desc:" \u6253\u5370 "
+},
+preview:{
+preview_desc:" \u9810\u89BD "
+},
+directionality:{
+ltr_desc:" \u5F9E\u5DE6\u81F3\u53F3 ",
+rtl_desc:" \u5F9E\u53F3\u81F3\u5DE6 "
+},
+layer:{
+insertlayer_desc:" \u63D2\u5165\u65B0\u5C64 ",
+forward_desc:" \u5411\u524D\u79FB\u52D5 ",
+backward_desc:" \u5411\u5F8C\u79FB\u52D5 ",
+absolute_desc:" \u7D55\u5C0D\u5C0B\u5740 ",
+content:" \u65B0\u5C64 ..."
+},
+save:{
+save_desc:" \u4FDD\u5B58 ",
+cancel_desc:" \u53D6\u6D88\u6240\u6709\u66F4\u6539 "
+},
+nonbreaking:{
+nonbreaking_desc:" \u63D2\u5165\u7A7A\u683C "
+},
+iespell:{
+iespell_desc:" \u9032\u884C\u62FC\u5BEB\u6AA2\u67E5 ",
+download:"ieSpell \u672A\u5B89\u88DD . \u662F\u5426\u73FE\u5728\u5B89\u88DD ?"
+},
+advhr:{
+advhr_desc:" \u6C34\u5E73\u7DDA "
+},
+emotions:{
+emotions_desc:" \u8868\u60C5 "
+},
+searchreplace:{
+search_desc:" \u5C0B\u627E ",
+replace_desc:" \u5C0B\u627E / \u53D6\u4EE3 "
+},
+advimage:{
+image_desc:" \u63D2\u5165 / \u7DE8\u8F2F \u5716\u7247 "
+},
+advlink:{
+link_desc:" \u63D2\u5165 / \u7DE8\u8F2F \u9023\u7D50 "
+},
+xhtmlxtras:{
+cite_desc:" \u5F15\u7528 ",
+abbr_desc:" \u7E2E\u5BEB ",
+acronym_desc:" \u9996\u5B57\u6BCD\u7E2E\u5BEB ",
+del_desc:" \u522A\u9664 ",
+ins_desc:" \u63D2\u5165 ",
+attribs_desc:" \u63D2\u5165 / \u7DE8\u8F2F \u5C6C\u6027 "
+},
+style:{
+desc:" \u7DE8\u8F2F CSS \u6A23\u5F0F "
+},
+paste:{
+paste_text_desc:" \u7D14\u6587\u5B57\u8CBC\u4E0A ",
+paste_word_desc:" \u5F9E Word \u8CBC\u4E0A ",
+selectall_desc:" \u5168\u9078 "
+},
+paste_dlg:{
+text_title:" \u4F7F\u7528\u9375\u76E4 CTRL+V \u4EE5\u8CBC\u4E0A\u5167\u5BB9\u81F3\u7A97\u53E3 .",
+text_linebreaks:" \u4FDD\u6301\u5206\u884C\u7B26\u865F ",
+word_title:" \u4F7F\u7528\u9375\u76E4 CTRL+V \u4EE5\u8CBC\u4E0A\u5167\u5BB9\u81F3\u7A97\u53E3 ."
+},
+table:{
+desc:" \u63D2\u5165\u65B0\u8868\u683C ",
+row_before_desc:" \u4E0A\u65B9\u63D2\u5165\u4E00\u884C ",
+row_after_desc:" \u4E0B\u65B9\u63D2\u5165\u4E00\u884C ",
+delete_row_desc:" \u522A\u9664\u884C ",
+col_before_desc:" \u5DE6\u5074\u63D2\u5165\u4E00\u5217 ",
+col_after_desc:" \u53F3\u5074\u63D2\u5165\u4E00\u5217 ",
+delete_col_desc:" \u522A\u9664\u5217 ",
+split_cells_desc:" \u5206\u5272\u5132\u5B58\u683C ",
+merge_cells_desc:" \u5408\u4F75\u5132\u5B58\u683C ",
+row_desc:" \u884C\u5C6C\u6027 ",
+cell_desc:" \u5132\u5B58\u683C\u5C6C\u6027 ",
+props_desc:" \u8868\u683C\u5C6C\u6027 ",
+paste_row_before_desc:" \u4E0A\u65B9\u8CBC\u4E0A ",
+paste_row_after_desc:" \u4E0B\u65B9\u8CBC\u4E0A ",
+cut_row_desc:" \u526A\u4E0B\u884C ",
+copy_row_desc:" \u8907\u88FD\u884C ",
+del:" \u522A\u9664\u8868\u683C ",
+row:" \u884C ",
+col:" \u5217 ",
+cell:" \u5132\u5B58\u683C "
+},
+autosave:{
+unload_msg:" \u96E2\u958B\u6B64\u9801\u9762\u5C07\u5C0E\u81F4\u6240\u505A\u66F4\u6539\u5168\u90E8\u4E1F\u5931 ."
+},
+fullscreen:{
+desc:" \u5168\u5C4F\u6A21\u5F0F "
+},
+media:{
+desc:" \u63D2\u5165 / \u7DE8\u8F2F \u5A92\u9AD4 ",
+edit:" \u7DE8\u8F2F\u5A92\u9AD4 "
+},
+fullpage:{
+desc:" \u6587\u4EF6\u5C6C\u6027 "
+},
+template:{
+desc:" \u63D2\u5165\u9810\u5B9A\u7FA9\u7BC4\u672C\u5167\u5BB9 "
+},
+visualchars:{
+desc:" \u8996\u89BA\u6A19\u8B58\u7B26 "
+},
+spellchecker:{
+desc:" \u62FC\u5BEB\u6AA2\u67E5 ",
+menu:" \u62FC\u5BEB\u6AA2\u67E5\u8A2D\u7F6E ",
+ignore_word:" \u5FFD\u7565\u55AE\u8A5E ",
+ignore_words:" \u5FFD\u7565\u5168\u90E8 ",
+langs:" \u8A9E\u8A00 ",
+wait:" \u8ACB\u7A0D\u5019 ...",
+sug:" \u5EFA\u8B70 ",
+no_sug:" \u7121\u5EFA\u8B70 ",
+no_mpell:" \u672A\u767C\u73FE\u932F\u8AA4 ."
+},
+pagebreak:{
+desc:" \u63D2\u5165\u5206\u9801\u7B26 "
+}}});
