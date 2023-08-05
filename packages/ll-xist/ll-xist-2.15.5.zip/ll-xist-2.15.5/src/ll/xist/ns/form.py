5829718f98f7628f2d7383e26f81e858c9682ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package ll-xist/2.15.5/ll-xist-2.15.5.zip

---
+++ packages/ll-xist/ll-xist-2.15.5.zip/ll-xist-2.15.5/src/ll/xist/ns/form.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+## Copyright 1999-2007 by LivingLogic AG, Bayreuth/Germany.
+## Copyright 1999-2007 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See xist/__init__.py for the license
+
+
+"""
+<par>An &xist; module that contains elements that are useful for
+forms. These are just abbreviations for the various
+<lit>&lt;input type=<rep>...</rep>&gt;</lit> elements.</par>
+"""
+
+__version__ = "$Revision: 1.2.2.1 $".split()[1]
+# $Source: /data/cvsroot/LivingLogic/Python/xist/src/ll/xist/ns/form.py,v $
+
+from ll.xist import xsc
+from ll.xist.ns import html
+
+
+class checkbox(html.input):
+	class Attrs(html.input.Attrs):
+		type = None
+
+	def convert(self, converter):
+		e = html.input(self.attrs, type=u"checkbox")
+		return e.convert(converter)
+
+
+class edit(html.input):
+	class Attrs(html.input.Attrs):
+		type = None
+
+	def convert(self, converter):
+		e = html.input(self.attrs, type=u"text")
+		return e.convert(converter)
+
+
+class radio(html.input):
+	class Attrs(html.input.Attrs):
+		type = None
+
+	def convert(self, converter):
+		e = html.input(self.attrs, type=u"radio")
+		return e.convert(converter)
+
+
+class submit(html.input):
+	class Attrs(html.input.Attrs):
+		type = None
+
+	def convert(self, converter):
+		e = html.input(self.attrs, type=u"submit")
+		return e.convert(converter)
+
+
+class memo(html.textarea):
+	class Attrs(html.textarea.Attrs):
+		class value(xsc.TextAttr): pass
+
+	def convert(self, converter):
+		e = html.textarea(self[u"value"], self.attrs.withoutnames([u"value"]))
+		return e.convert(converter)
+
+
+class hidden(html.input):
+	class Attrs(html.input.Attrs):
+		type = None
+
+	def __unicode__(self):
+		return u""
+
+	def convert(self, converter):
+		e = html.input(self.attrs, type=u"hidden")
+		return e.convert(converter)
+
+
+class __ns__(xsc.Namespace):
+	xmlname = "form"
+	xmlurl = "http://xmlns.livinglogic.de/xist/ns/form"
+__ns__.makemod(vars())
