e2ccbaa4d6d2c6354c25ee58dc3a6b162685efad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:30 +0000

    Add package InPlace/0.1a0.dev-r501/InPlace-0.1a0.dev-r501.zip

---
+++ packages/InPlace/InPlace-0.1a0.dev-r501.zip/InPlace-0.1a0.dev-r501/inplace/release.py
@@ -0,0 +1,21 @@
+# Release information about InPlace
+
+version = "0.1a0"
+
+description = "Wrapper for the InPlace javascript library"
+
+long_description = """Still a work in progress.
+This is a javascript library I've been developing to allow in place editions
+of arbitrary DOM objects. It doesn't do any AJAX by itself but allows 
+the attachment of hooks that can do it.
+For the input fields it uses a javascript widget system that will allow
+complex ones to develop. Right now there are available the text field and 
+single selector widgets.
+Uses Prototype right now, but I might do it "library" agnostic.
+Works with Opera and Firefox, fails with IE (I don't know when I'll fix it, it's
+far from being a priority on the project I'm currently working).
+Check the widget browser!"""
+
+author = "Claudio Martinez"
+email = "claudio.s.martinez@gmail.com"
+license = "MIT"
