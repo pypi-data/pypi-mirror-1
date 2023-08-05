cf639bd3f171008258d1768e6b93fd2ca9e7673e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:30 +0000

    Add package plone.app.i18n/1.0b2/plone.app.i18n-1.0b2-py2.4.egg

---
+++ packages/plone.app.i18n/plone.app.i18n-1.0b2-py2.4.egg/plone/app/i18n/locales/browser/selector.py
@@ -0,0 +1,54 @@
+from zope.component import queryUtility
+from zope.interface import implements
+from zope.viewlet.interfaces import IViewlet
+
+from Products.CMFCore.interfaces import IURLTool
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from Products.PloneLanguageTool.interfaces import ILanguageTool
+
+
+class LanguageSelector(BrowserView):
+    """Language selector.
+    """
+    implements(IViewlet)
+
+    render = ZopeTwoPageTemplateFile('languageselector.pt')
+
+    def __init__(self, context, request, view, manager):
+        super(BrowserView, self).__init__(context, request)
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+        self.tool = queryUtility(ILanguageTool)
+        portal_tool = queryUtility(IURLTool)
+        self.portal_url = portal_tool.getPortalObject().absolute_url()
+
+    def update(self):
+        pass
+
+    def languages(self):
+        """Returns list of languages."""
+        bound = self.tool.getLanguageBindings()
+        current = bound[0]
+
+        supported = self.tool.getSupportedLanguages()
+        languages = []
+        for lang in supported:
+            data = {}
+            data['code'] = lang
+            if lang == current:
+                data['selected'] = True
+            else:
+                data['selected'] = False
+            data['flag'] = self.tool.getFlagForLanguageCode(lang)
+            data['name'] = self.tool.getNameForLanguageCode(lang)
+            languages.append(data)
+
+        return languages
+
+    def showFlags(self):
+        """Do we use flags?."""
+        return self.tool.showFlags()
