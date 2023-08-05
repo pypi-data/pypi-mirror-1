123baacbe65352855187215cfad0bf5a3f372b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:50 +0000

    Add package zgeo.geographer/0.2dev-r1157/zgeo.geographer-0.2dev-r1157.tar.gz

---
+++ packages/zgeo.geographer/zgeo.geographer-0.2dev-r1157.tar.gz/zgeo.geographer-0.2dev-r1157/zgeo/geographer/geo.py
@@ -0,0 +1,53 @@
+
+from zope.interface import implements
+from zope.component import adapts
+from zope.event import notify
+from zope.annotation.interfaces import IAnnotations
+from zope.lifecycleevent import modified, Attributes
+from persistent.dict import PersistentDict
+from zgeo.geographer.interfaces import IGeoreferenced, IGeoreferenceable
+
+import logging
+
+logger = logging.getLogger('zgeo.geographer.geo')
+
+KEY = 'zgeo.geographer.georeference'
+
+
+class GeoreferencingAnnotator(object):
+    
+    """Geographically annotate objects with metadata modeled after GeoJSON.
+    See http://geojson.org.
+    """
+    implements(IGeoreferenced)
+    adapts(IGeoreferenceable)
+
+    def __init__(self, context):
+        self.context = context
+        annotations = IAnnotations(context)
+        self.geo = annotations.get(KEY, None)
+        if not self.geo:
+            annotations[KEY] = PersistentDict()
+            self.geo = annotations[KEY]
+            self.geo['type'] = None
+            self.geo['coordinates'] = None
+            self.geo['crs'] = None
+
+    @property
+    def type(self):
+        return self.geo['type']
+
+    @property
+    def coordinates(self):
+        return self.geo['coordinates']
+
+    @property
+    def crs(self):
+        return self.geo['crs']
+
+    def setGeoInterface(self, type, coordinates, crs=None):
+        self.geo['type'] = type
+        self.geo['coordinates'] = coordinates
+        self.geo['crs'] = crs
+        modified(self.context, IGeoreferenced)
+
