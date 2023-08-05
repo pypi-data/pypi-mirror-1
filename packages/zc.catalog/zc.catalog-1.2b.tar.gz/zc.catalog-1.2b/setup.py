b8c1f683089e6991436759aa159e66512184a97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package zc.catalog/1.2b/zc.catalog-1.2b.tar.gz

---
+++ packages/zc.catalog/zc.catalog-1.2b.tar.gz/zc.catalog-1.2b/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="zc.catalog",
+    version="1.2b",
+    packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
+    
+    package_dir= {'':'src'},
+    
+    namespace_packages=['zc'],
+    package_data = {
+    '': ['*.txt', '*.zcml'],
+    },
+    url='http://svn.zope.org/zc.catalog',
+    zip_safe=False,
+    author='Zope Project',
+    author_email='zope3-dev@zope.org',
+    description="zc.catalog contains a number of extensions to the Zope 3 catalog",
+    long_description=(
+        open('README.txt').read() + '\n' +
+        open('src/zc/catalog/CHANGES.txt').read()),
+    license='ZPL',
+    keywords="zope zope3 indexing",
+    classifiers = ['Framework :: Zope3'],
+    install_requires=['setuptools',
+                      'zope.component',
+                      'zope.testing',
+                      'ZODB3 >=3.8.0a1.dev-r74856', # required BTrees.family*
+                      'zope.schema',
+                      'zope.interface',
+                      'zope.app.catalog >=3.4.0a2',
+                      'pytz',
+                      'zope.security',
+                      'zope.publisher',
+                      'zope.i18nmessageid',
+                      'zope.app.container',
+                      ],
+    )
