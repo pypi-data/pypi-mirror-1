d254a8778c3ed464f3b0dc081039c6ea568586af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package archgenxml/1.6.b1/archgenxml-1.6.b1.tar.gz

---
+++ packages/archgenxml/archgenxml-1.6.b1.tar.gz/archgenxml-1.6.b1/src/archgenxml/tests/testDoctests.py
@@ -0,0 +1,32 @@
+import doctest
+import unittest
+
+import archgenxml
+import archgenxml.utility
+import archgenxml.plone.config_py
+
+modules = [
+    archgenxml.utility,
+    archgenxml.plone.config_py,
+    ]
+
+textfiles = [
+    'uml/README.txt',
+    ]
+
+def test_suite():
+    optionflags = (doctest.ELLIPSIS |
+                   doctest.NORMALIZE_WHITESPACE |
+                   doctest.REPORT_ONLY_FIRST_FAILURE)
+    suite = unittest.TestSuite()
+    for module in modules:
+        suite.addTest(doctest.DocTestSuite(
+            module,
+            optionflags=optionflags))
+    for textfile in textfiles:
+        suite.addTest(doctest.DocFileSuite(
+            textfile,
+            module_relative=True,
+            package=archgenxml,
+            optionflags=optionflags))
+    return suite
