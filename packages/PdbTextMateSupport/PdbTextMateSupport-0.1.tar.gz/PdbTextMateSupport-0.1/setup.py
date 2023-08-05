d5cadc626b3da166994fe611adf04e2bf336c57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:59 +0000

    Add package PdbTextMateSupport/0.1/PdbTextMateSupport-0.1.tar.gz

---
+++ packages/PdbTextMateSupport/PdbTextMateSupport-0.1.tar.gz/PdbTextMateSupport-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PdbTextMateSupport',
+      version='0.1',
+      description='Display source code in TextMate while debugging with pdb',
+      author='Andreas Zeidler',
+      author_email='az@zitc.de',
+      url='https://svn.zitc.de/trac/pdbtextmatesupport',
+      py_modules=['PdbTextMateSupport'],
+)
