5bb58b8c08304d5925360eeed667dc5a11dc3880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package cogen/0.1.1/cogen-0.1.1-py2.5.egg

---
+++ packages/cogen/cogen-0.1.1-py2.5.egg/cogen/common.py
@@ -0,0 +1,8 @@
+"""
+A module for quick importing the essential core stuff.
+"""
+from cogen.core.coroutine import coroutine
+from cogen.core.schedulers import Scheduler
+from cogen.core import events
+from cogen.core import sockets
+from cogen.core.events import priority
