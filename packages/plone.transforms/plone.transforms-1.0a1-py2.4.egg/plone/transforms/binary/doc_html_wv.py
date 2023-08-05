b2c8a5519d208e2cb54f58de1c16cd2b726440e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:15 +0000

    Add package plone.transforms/1.0a1/plone.transforms-1.0a1-py2.4.egg

---
+++ packages/plone.transforms/plone.transforms-1.0a1-py2.4.egg/plone/transforms/binary/doc_html_wv.py
@@ -0,0 +1,28 @@
+from plone.transforms.command import CommandTransform
+from plone.transforms.message import PloneMessageFactory as _
+from plone.transforms.stringiter import StringIter
+from plone.transforms.utils import html_bodyfinder
+
+
+class DocHtmlWvCommandTransform(CommandTransform):
+    """A transform which transforms doc into HTML including the images
+    as subobjects.
+    """
+
+    title = _(u'title_doc_html_wv_transform',
+        default=u'DOC to HTML transform including images.')
+
+    inputs = ("application/msword", )
+    output = "text/html"
+
+    command = 'wvHtml'
+    args = "%(infile)s %(infile)s.html --charset=utf-8"
+
+    def transform(self, data, options=None):
+        if self._validate(data) is None:
+            return None
+
+        result = self.prepare_transform(data, infile_data_suffix='.html')
+        text = ''.join(result.data).decode('utf-8', 'ignore')
+        result.data = StringIter(html_bodyfinder(text))
+        return result
