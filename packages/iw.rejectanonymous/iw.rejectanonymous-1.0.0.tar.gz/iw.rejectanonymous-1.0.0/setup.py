96a360c674578b9426e053acfd384da49049faaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:12 +0000

    Add package iw.rejectanonymous/1.0.0/iw.rejectanonymous-1.0.0.tar.gz

---
+++ packages/iw.rejectanonymous/iw.rejectanonymous-1.0.0.tar.gz/iw.rejectanonymous-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+
+setup(name='iw.rejectanonymous',
+      version=version,
+      description="Disallow access to a folder and its children if user is anonymous",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Ingeniweb',
+      author_email='support@ingeniweb.com',
+      url='http://ingeniweb.svn.sourceforge.net/svnroot/ingeniweb/iw.rejectanonymous',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['iw'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
