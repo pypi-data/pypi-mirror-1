38f0c118b456a140e71ed179297ce8598e301695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:49 +0000

    Add package gocept.cache/0.2.1/gocept.cache-0.2.1.tar.gz

---
+++ packages/gocept.cache/gocept.cache-0.2.1.tar.gz/gocept.cache-0.2.1/src/gocept/cache/test.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id: test.py 5176 2007-09-18 10:34:58Z zagy $
+
+import unittest
+
+from zope.testing import doctest
+
+
+def test_suite():
+    return doctest.DocFileSuite(
+        'README.txt',
+        optionflags=doctest.ELLIPSIS)
