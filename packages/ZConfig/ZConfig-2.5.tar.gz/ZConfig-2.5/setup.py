3736c8809364c167b6c60fb62192ffcedd15c587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package ZConfig/2.5/ZConfig-2.5.tar.gz

---
+++ packages/ZConfig/ZConfig-2.5.tar.gz/ZConfig-2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ZConfig",
+    version="2.5",
+    author="Fred L. Drake, Jr.",
+    author_email="fred@zope.com",
+    description="Structured Configuration Library",
+    long_description=open("README.txt").read(),
+    license="ZPL 2.1",
+    url='http://www.zope.org/Members/fdrake/zconfig/',
+
+    packages=find_packages("."),
+    scripts=["scripts/zconfig", "scripts/zconfig_schema2html"],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+      'Intended Audience :: Developers',
+      'Intended Audience :: System Administrators',
+      'License :: OSI Approved :: Zope Public License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+    )
