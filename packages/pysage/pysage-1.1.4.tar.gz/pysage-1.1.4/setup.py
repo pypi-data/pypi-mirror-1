44447071e9def2aebec2eb3b61a8cb056ba74d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:07 +0000

    Add package pysage/1.1.4/pysage-1.1.4.tar.gz

---
+++ packages/pysage/pysage-1.1.4.tar.gz/pysage-1.1.4/setup.py
@@ -0,0 +1,16 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from distutils.core import setup
+setup(name='pysage',
+      version='1.1.4',
+      packages=['pysage'],
+      
+    # metadata for upload to PyPI
+    author = "John Yang",
+    author_email = "bigjhnny@gmail.com",
+    description = "pySage",
+    license = "creative commons attribution share-alike 1.0",
+    keywords = "python message publisher subscriber",
+    url = "http://code.google.com/p/pysage/"
+      )
