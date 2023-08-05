5b4c01c166ef165fe9cf2091228272e676816bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:26 +0000

    Add package TGSociable/0.1.0/TGSociable-0.1.0-py2.4.egg

---
+++ packages/TGSociable/TGSociable-0.1.0-py2.4.egg/tgsociable/release.py
@@ -0,0 +1,14 @@
+# Release information about TGSociable
+
+version = "0.1.0"
+
+description = "A port of the sociable Wordpress plugin"
+long_description = """TurboGears Widget to add links to post pages to social bookmarking sites (Digg, Reddit, del.icio.us, &c.)"""
+author = "Neil Blakey-Milner, Peter Harkins (original sociable plugin for wordpress)"
+email = "nbm@nxsy.org"
+copyright = "2007"
+
+# if it's open source, you might want to specify these
+url = "http://nxsy.org/code/tgsociable/"
+# download_url = "http://yourcool.site/download"
+license = "GPL"
