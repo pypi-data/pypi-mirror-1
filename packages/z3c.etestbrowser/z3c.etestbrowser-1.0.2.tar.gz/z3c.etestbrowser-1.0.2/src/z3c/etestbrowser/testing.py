99e80b981ee2cebdf82269cf4e89c4b0ab0f7bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package z3c.etestbrowser/1.0.2/z3c.etestbrowser-1.0.2.tar.gz

---
+++ packages/z3c.etestbrowser/z3c.etestbrowser-1.0.2.tar.gz/z3c.etestbrowser-1.0.2/src/z3c/etestbrowser/testing.py
@@ -0,0 +1,67 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Extensions for z3c.etestbrowser
+
+$Id: testing.py 79569 2007-09-11 13:56:09Z ctheune $
+"""
+
+import re
+import StringIO
+import lxml.etree
+
+import zope.testbrowser.testing
+
+
+RE_CHARSET = re.compile('.*;charset=(.*)')
+
+
+class ExtendedTestBrowser(zope.testbrowser.testing.Browser):
+    """An extended testbrowser implementation.
+
+    Features:
+
+        - offers the content also as parsed etree
+
+    """
+
+    xml_strict = False
+
+    _etree = None
+
+    @property
+    def etree(self):
+        if self._etree is not None:
+            return self._etree
+        # I'm not using any internal knowledge about testbrowser
+        # here, to avoid breakage. Memory usage won't be a problem.
+        if self.xml_strict:
+            self._etree = lxml.etree.XML(self.contents)
+        else:
+            # This is a workaround against the broken fallback for 
+            # encoding detection of libxml2.
+            # We have a chance of knowing the encoding as Zope states this in
+            # the content-type response header.
+            content = self.contents
+            content_type = self.headers['content-type']
+            match = RE_CHARSET.match(content_type)
+            if match is not None:
+                charset = match.groups()[0]
+                content = content.decode(charset)
+            self._etree = lxml.etree.HTML(content)
+
+        return self._etree
+
+    def _changed(self):
+        super(ExtendedTestBrowser, self)._changed()
+        self._etree = None
