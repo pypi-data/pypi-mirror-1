b6b1675f982111e145fe5f78b2845c0b1f02e397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:44 +0000

    Add package context_tools/0.1/context_tools-0.1.tar.gz

---
+++ packages/context_tools/context_tools-0.1.tar.gz/context_tools-0.1/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name = 'context_tools',
+    version = '0.1',
+    description = "Utility functions for context managers",
+
+    long_description = """Python 2.5's contextlib provides some handy functions for dealing with context managers, but it's pretty limited. The code in context_tools allows you to use context managers as decorators, as setUp and tearDown functions in unittest tests, and as decorators for generators.""",
+
+    author = 'Collin Winter',
+    author_email = 'collinw@gmail.com',
+    url = 'http://oakwinter.com/code/context_tools',
+    license = 'MIT License',
+    classifiers = [     
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',       
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords = 'context managers decorators generators tests',
+    py_modules = ['context_tools'],
+    zip_safe = True,
+)
