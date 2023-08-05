1b24e755fc569a705a6ef0a22dc33df6de4a861f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:23 +0000

    Add package zhpy/0.3/zhpy-0.3.zip

---
+++ packages/zhpy/zhpy-0.3.zip/zhpy-0.3/setup.py
@@ -0,0 +1,42 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+from pkg_resources import DistributionNotFound
+
+import sys
+import os
+import glob
+
+execfile(os.path.join('zhpy', 'release.py'))
+
+# setup params
+install_requires = ["pyparsing >=1.4.7"]
+
+
+setup(
+    name="zhpy",
+    version=version,
+    author=author,
+    author_email=email,
+    license=license,
+    description=description,
+    long_description=long_description,
+    url=url,
+    zip_safe=False,
+    install_requires = install_requires,
+    packages=find_packages(),
+    entry_points = """
+    [console_scripts]
+    zhpy = zhpy.zhpy:commandtool
+    """,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    test_suite = 'nose.collector',
+    )
+
