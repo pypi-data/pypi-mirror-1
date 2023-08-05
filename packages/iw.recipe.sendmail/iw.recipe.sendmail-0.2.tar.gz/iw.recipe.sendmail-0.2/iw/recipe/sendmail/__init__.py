3e91e57f564933e0586ed7e7ba81757113fb2340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:12 +0000

    Add package iw.recipe.sendmail/0.2/iw.recipe.sendmail-0.2.tar.gz

---
+++ packages/iw.recipe.sendmail/iw.recipe.sendmail-0.2.tar.gz/iw.recipe.sendmail-0.2/iw/recipe/sendmail/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Recipe sendmail"""
+
+import os
+from zc.buildout import UserError
+from iw.recipe.template import Template
+
+class Recipe(Template):
+    """zc.buildout recipe"""
+    template_dir = os.path.dirname(os.path.abspath(__file__))
+
+    def __init__(self, buildout, name, options):
+        self.buildout, self.name, self.options = buildout, name, options
+        self.name = 'iw.sendmail-configure.zcml'
+        self.source = os.path.join(self.template_dir, 'template.zcml_tmpl')
+        zope2location = options.get('zope2location')
+        if not zope2location:
+            raise UserError('zope2location is required for iw.recipe.mailer')
+        self.destination = os.path.join(options.get('zope2location'),
+                                        'etc', 'package-includes')
+
+        options.setdefault('name', 'iw.mailer')
+        options.setdefault('host', 'localhost')
+        options.setdefault('port', '25')
+        mailqueue = options.get('mailqueue', None)
+        if mailqueue is not None:
+            options['mailqueue'] = os.path.join(mailqueue, 'mailqueue')
+        else:
+            options.setdefault('mailqueue', os.path.join(
+                    buildout['buildout']['directory'],'var', 'mailqueue'))
+
+        self.kwargs = dict(name=name,
+                           options=options,
+                           buildout=buildout)
+
