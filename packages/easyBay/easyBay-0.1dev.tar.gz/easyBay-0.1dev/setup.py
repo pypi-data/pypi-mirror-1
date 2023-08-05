68ef190bd5906236d0332973fde3f15051696d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:22 +0000

    Add package easyBay/0.1dev/easyBay-0.1dev.tar.gz

---
+++ packages/easyBay/easyBay-0.1dev.tar.gz/easyBay-0.1dev/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='easyBay',
+      version="0.1dev",
+      packages=find_packages(),
+
+      author="Brian Beck",
+      author_email="exogen@gmail.com",
+      description="A simple way to talk to eBay (manage listings, gather data) with Python.",
+      license="PSF",
+      keywords="ebay",
+      url="http://exogen.case.edu/easyBay/",
+)
+
