40c54c97e6fb41eb73264619f5f3ee81ad8f9bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:34 +0000

    Add package MochiInterpreter/1.0/MochiInterpreter-1.0.tar.gz

---
+++ packages/MochiInterpreter/MochiInterpreter-1.0.tar.gz/MochiInterpreter-1.0/README.txt
@@ -0,0 +1,7 @@
+MochiInterpreter
+
+This is a TurboGears (http://www.turbogears.org) widget project.
+You can view the widgets in the Toolbox.
+
+MochiInterpreter is basically a wrapper for the MochiKit Interactive
+Interpreter/Debugger example.
---
+++ packages/MochiInterpreter/MochiInterpreter-1.0.tar.gz/MochiInterpreter-1.0/mochiinterpreter/release.py
@@ -0,0 +1,17 @@
+# Release information about MochiInterpreter
+
+version = "1.0"
+
+description = """This widget is an implementation of the interpreter example from MochiKit
+code.  It is provided as a facility to develop TurboGears applications that
+needs JavaScript.
+
+It is based on code provided at http://trac.turbogears.org/turbogears/ticket/255
+"""
+
+author = "Jorge Godoy"
+email = "jgodoy@gmail.org"
+
+url = "http://www.g2ctech.com/"
+download_url = "http://www.g2ctech.com/widgets/MochiInterpreter-%s-py2.4.egg" % version
+license = "MIT"
