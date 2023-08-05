477a41ed73c3d931c28fffbe7eaae78d06a20255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package Scriptaculous/1.6/Scriptaculous-1.6.tar.gz

---
+++ packages/Scriptaculous/Scriptaculous-1.6.tar.gz/Scriptaculous-1.6/scriptaculous/__init__.py
@@ -0,0 +1,3 @@
+from scriptaculous.widgets import prototype, scriptaculous, scriptaculous_js
+
+__all__ = ["prototype", "scriptaculous", "scriptaculous_js"]
---
+++ packages/Scriptaculous/Scriptaculous-1.6.tar.gz/Scriptaculous-1.6/scriptaculous/release.py
@@ -0,0 +1,12 @@
+# Release information about Scriptaculous
+
+version = "1.6"
+
+description = "Scriptaculous packaged as TurboGears widgets."
+author = "Thomas Fuchs, Sam Stephenson, Kevin Dangoor"
+email = "dangoor+scriptaculous@gmail.com"
+copyright = "Copyright 2006"
+
+url = "http://www.turbogears.org/widgets/"
+download_url = "http://www.turbogears.org/widgets/"
+license = "MIT"
