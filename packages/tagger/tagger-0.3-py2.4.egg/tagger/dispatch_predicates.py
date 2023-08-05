b66ee4945f4c35e571f08899852441258ee7d80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:32 +0000

    Add package tagger/0.3/tagger-0.3-py2.4.egg

---
+++ packages/tagger/tagger-0.3-py2.4.egg/tagger/dispatch_predicates.py
@@ -0,0 +1,9 @@
+"""
+tagger.Tagger depends on a number of string predicates used by
+RuleDispatch to determine identity of string arguments. These
+predicates have been collected here for cleanliness.
+"""
+IS_TAG = "self.is_rdf_type(%s, self.taggedby)"
+IS_USER = "self.is_rdf_type(%s, self.authoredby)"
+IS_ITEM = "self.is_rdf_type(%s, self.annotates)"
+ITEM_USER = '%s and %s' %(IS_ITEM, IS_USER)
