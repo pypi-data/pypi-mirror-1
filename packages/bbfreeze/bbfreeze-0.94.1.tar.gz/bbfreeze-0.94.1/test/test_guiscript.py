fe322806125bea99765ca4145048dd09b8dbb55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package bbfreeze/0.94.1/bbfreeze-0.94.1.tar.gz

---
+++ packages/bbfreeze/bbfreeze-0.94.1.tar.gz/bbfreeze-0.94.1/test/test_guiscript.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env py.test
+try:
+    import win32ui
+except:
+    win32ui = None
+
+import os
+import bbfreeze
+
+if win32ui:
+    def test_guiscript():
+        f = bbfreeze.Freezer()
+        f.addScript("ex-mbox.py", True)
+        f()
+        err=os.system("dist\\ex-mbox")
+        assert err==0
+
+def test_guiscript2():
+    f = bbfreeze.Freezer()
+    f.addScript("hello-world.py", True)
+    f()
+    err=os.system("dist/hello-world")
+    assert err==0
+    
+    
