a5bdc6df251cd07b777ea91c33ebd57a92e48467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package pyephem/3.7.2.1/pyephem-3.7.2.1.tar.gz

---
+++ packages/pyephem/pyephem-3.7.2.1.tar.gz/pyephem-3.7.2.1/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup, Extension
+except:
+    from distutils.core import setup, Extension
+from glob import glob
+
+# The `pyephem' module is built from every .c file in the libastro
+# directory plus ...
+
+libastro_version = '3.7.2'
+libastro_files = glob('libastro-%s/*.c' % libastro_version)
+
+setup(name = 'pyephem',
+      version = '3.7.2.1',
+      description = 'computational astronomy routines for Python',
+      license = 'LGPL',
+      author = 'Brandon Craig Rhodes',
+      author_email = 'brandon@rhodesmill.org',
+      url = 'http://rhodesmill.org/pyephem/',
+      packages = [ 'ephem' ],
+      package_dir = { 'ephem': 'src' },
+      ext_modules = [
+    Extension('ephem._libastro', ['src/_libastro.c'] + libastro_files,
+              include_dirs=['libastro-' + libastro_version],
+              )],
+      )
