b242cb792e203b879a976699419a638b9c911bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:43 +0000

    Add package cclib/0.6b/cclib-0.6b.zip

---
+++ packages/cclib/cclib-0.6b.zip/cclib-0.6b/src/cclib/method/cspa.py
@@ -0,0 +1,105 @@
+"""
+cclib (http://cclib.sf.net) is (c) 2006, the cclib development team
+and licensed under the LGPL (http://www.gnu.org/copyleft/lgpl.html).
+"""
+
+__revision__ = "$Revision: 238 $"
+
+import Numeric
+import random # For sometimes running the progress updater
+from population import Population
+
+class CSPA(Population):
+    """The C-squared population analysis"""
+    def __init__(self, *args):
+
+        # Call the __init__ method of the superclass
+        super(CSPA, self).__init__(logname="CSPA", *args)
+        
+    def __str__(self):
+        """Return a string representation of the object."""
+        return "CSPA of" % (self.parser)
+
+    def __repr__(self):
+        """Return a representation of the object."""
+        return 'CSPA("%s")' % (self.parser)
+    
+    def calculate(self, indices=None, fupdate=0.05):
+        """Perform a C^2 population analysis given the results of a parser"""
+    
+        if not self.parser.parsed:
+            self.parser.parse()
+
+#do we have the needed info in the parser?
+        if not hasattr(self.parser, "mocoeffs"):
+            self.logger.error("Missing mocoeffs")
+            return False
+        if not hasattr(self.parser, "nbasis"):
+            self.logger.error("Missing nbasis")
+            return False
+        if not hasattr(self.parser, "homos"):
+            self.logger.error("Missing homos")
+            return False
+#end attributes check
+
+        self.logger.info("Creating attribute aoresults: array[3]")
+        unrestricted = (len(self.parser.mocoeffs)==2)
+        nmocoeffs = len(self.parser.mocoeffs[0])
+        nbasis = self.parser.nbasis
+
+        #determine number of steps, and whether process involves beta orbitals
+        nstep = nmocoeffs
+        if unrestricted:
+            self.aoresults = Numeric.zeros([2, nmocoeffs, nbasis], "f")
+            nstep += nmocoeffs
+        else:
+            self.aoresults = Numeric.zeros([1, nmocoeffs, nbasis], "f")
+
+        #intialize progress if available
+        if self.progress:
+            self.progress.initialize(nstep)
+
+        step = 0
+        for spin in range(len(self.parser.mocoeffs)):
+
+            for i in range(nmocoeffs):
+
+                if self.progress and random.random() < fupdate:
+                    self.progress.update(step, "C^2 Population Analysis")
+
+                submocoeffs = self.parser.mocoeffs[spin][i]
+                scale = Numeric.innerproduct(submocoeffs, submocoeffs)
+                tempcoeffs = Numeric.multiply(submocoeffs, submocoeffs)
+                self.aoresults[spin][i] = Numeric.divide(tempcoeffs, scale)
+
+                step += 1
+
+        if self.progress:
+            self.progress.update(nstep, "Done")
+
+        retval = super(CSPA, self).partition(indices)
+
+        if not retval:
+            self.logger.error("Error in partitioning results")
+            return False
+
+#create array for mulliken charges
+        self.logger.info("Creating fragcharges: array[1]")
+        size = len(self.fragresults[0][0])
+        self.fragcharges = Numeric.zeros([size], "f")
+        
+        for spin in range(len(self.fragresults)):
+
+            for i in range(self.parser.homos[spin] + 1):
+
+                temp = Numeric.reshape(self.fragresults[spin][i], (size,))
+                self.fragcharges = Numeric.add(self.fragcharges, temp)
+        
+        if not unrestricted:
+            self.fragcharges = Numeric.multiply(self.fragcharges, 2)
+
+        return True
+
+if __name__ == "__main__":
+    import doctest, cspa
+    doctest.testmod(cspa, verbose=False)
---
+++ packages/cclib/cclib-0.6b.zip/cclib-0.6b/test/testSP.py
@@ -0,0 +1,67 @@
+import os, unittest
+from Numeric import array
+from testall import getfile
+from cclib.parser import ADF, GAMESS, Gaussian, GAMESSUK
+
+class GenericSPTest(unittest.TestCase):
+    """Restricted single point calculations with MO coeffs and overlap info."""
+    def testdimaooverlaps(self):
+        """Are the dims of the overlap matrix consistent with nbasis?"""
+        self.assertEquals(self.data.aooverlaps.shape,(self.data.nbasis,self.data.nbasis))
+
+    def testatomcoords(self):
+        """Are the dimensions of atomcoords 1 x natom x 3?"""
+        self.assertEquals(self.data.atomcoords.shape,(1,self.data.natom,3))
+    
+    def testdimmocoeffs(self):
+        """Are the dimensions of mocoeffs equal to 1 x nmo x nbasis?"""
+        self.assertEquals(self.data.mocoeffs.shape,(1,self.data.nmo,self.data.nbasis))
+
+class GaussianSPTest(GenericSPTest):
+    def setUp(self):
+        self.data = data[0]
+
+class GamessUSSPTest(GenericSPTest):
+    def setUp(self):
+        self.data = data[1]
+
+class PCGamessSPTest(GenericSPTest):
+    def setUp(self):
+        self.data = data[2]
+
+class ADFSPTest(GenericSPTest):
+    def setUp(self):
+        self.data = data[3]
+    
+    def testdimaooverlaps(self):
+        """Are the dims of the overlap matrix consistent with nbasis?"""
+        #ADF uses fooverlaps
+        self.assertEquals(self.data.fooverlaps.shape,(self.data.nbasis,self.data.nbasis))
+
+class GamessUKSPTest(GenericSPTest):
+    def setUp(self):
+        self.data = data[4]
+
+names = [ "Gaussian", "PCGamess", "GAMESS", "ADF", "GAMESS UK"]
+tests = [ GaussianSPTest, PCGamessSPTest,
+          GamessUSSPTest, ADFSPTest,
+          GamessUKSPTest]
+data = [getfile(Gaussian, "basicGaussian03","dvb_sp.out"),
+        getfile(GAMESS, "basicGAMESS-US","dvb_sp.out"),
+        getfile(GAMESS, "basicPCGAMESS","dvb_sp.out"),
+        getfile(ADF, "basicADF2004.01","dvb_sp_b.adfout"),
+        getfile(GAMESSUK, "basicGAMESS-UK", "dvb_sp_b.out")]
+              
+if __name__=="__main__":
+    total = errors = failures = 0
+
+    for name,test in zip(names,tests):
+        print "\n**** Testing %s SP ****" % name
+        myunittest = unittest.makeSuite(test)
+        a = unittest.TextTestRunner(verbosity=2).run(myunittest)
+        total += a.testsRun
+        errors += len(a.errors)
+        failures += len(a.failures)
+
+    print "\n\n********* SUMMARY OF SP **************"
+    print "TOTAL: %d\tPASSED: %d\tFAILED: %d\tERRORS: %d" % (total,total-(errors+failures),failures,errors)
