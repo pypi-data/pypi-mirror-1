3ecf5483f105938a9bf0b32a5bd67524fa02ec40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:12 +0000

    Add package zope.exceptions/3.4dev/zope.exceptions-3.4dev-r73107.tar.gz

---
+++ packages/zope.exceptions/zope.exceptions-3.4dev-r73107.tar.gz/zope.exceptions-3.4dev-r73107/setup.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.exceptions package
+
+$Id: setup.py 73107 2007-03-09 10:53:49Z baijum $
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.exceptions',
+      version='3.4dev_r73107',
+      url='http://svn.zope.org/zope.exceptions',
+      license='ZPL 2.1',
+      description='Zope Exceptions',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="This package contains exception interfaces "
+                       "and implementations which are so general purpose "
+                       "that they don't belong in a Zope application-specific "
+                       "package.",
+      
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['setuptools',
+                        'zope.deprecation',
+                        'zope.interface',
+                       ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
