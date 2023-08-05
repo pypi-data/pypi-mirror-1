3a460d547994c0fccf02c946a3ef36f29fa77412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:08 +0000

    Add package dublincore/1.0/dublincore-1.0.zip

---
+++ packages/dublincore/dublincore-1.0.zip/dublincore-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dublincore',
+	version = '1.0',
+	 description = 'Dublin Core metadata representation and generation',
+	  author = 'Jonathan E. Magen',
+	   author_email = 'yonkeltron@gmail.com',
+	    url = 'http://www.bluefacemonkey.com/code/dublincore',
+	     py_modules = ['dublincore']
+	      )
