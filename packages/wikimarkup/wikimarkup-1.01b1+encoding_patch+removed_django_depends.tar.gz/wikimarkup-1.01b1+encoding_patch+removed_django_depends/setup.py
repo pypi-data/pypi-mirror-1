b8995e8aef5be045aff09224884a8c58abbcf346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:11 +0000

    Add package wikimarkup/1.01b1+encoding_patch+removed_django_depends/wikimarkup-1.01b1+encoding_patch+removed_django_depends.tar.gz

---
+++ packages/wikimarkup/wikimarkup-1.01b1+encoding_patch+removed_django_depends.tar.gz/wikimarkup-1.01b1+encoding_patch+removed_django_depends/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wikimarkup',
+      version='1.01b1+encoding_patch+removed_django_depends',
+      description='Wiki Markup (MediaWiki)',
+      author='Curse, Inc.',
+      author_email='dcramer@curse.com',
+      url='http://code.curse.com/wikimarkup/',
+      packages=['wikimarkup'],
+     )
