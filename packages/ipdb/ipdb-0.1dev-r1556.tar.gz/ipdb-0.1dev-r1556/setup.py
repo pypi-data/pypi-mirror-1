b84f4b6543321802fa62165e3b7b2c56737ae586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:10 +0000

    Add package ipdb/0.1dev-r1556/ipdb-0.1dev-r1556.tar.gz

---
+++ packages/ipdb/ipdb-0.1dev-r1556.tar.gz/ipdb-0.1dev-r1556/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='ipdb',
+      version=version,
+      description="IPython-enabled pdb",
+      long_description=file('README.txt').read(),
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='pdb ipython',
+      author='Godefroid Chapelle',
+      author_email='gotcha@bubblenet.be',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
