b6ad8630a9c47bc0a1315f4d8b97cca85f242c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:51 +0000

    Add package libxml2dom/0.3.2/libxml2dom-0.3.2.tar.gz

---
+++ packages/libxml2dom/libxml2dom-0.3.2.tar.gz/libxml2dom-0.3.2/libxml2dom/macrolib/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"DOM macro library for libxml2."
+
+__version__ = "0.3.2"
+
+# Expose all functions here.
+
+from libxml2dom.macrolib.macrolib import *
+
+# vim: tabstop=4 expandtab shiftwidth=4
