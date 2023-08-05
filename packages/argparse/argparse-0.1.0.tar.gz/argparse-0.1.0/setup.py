2e8cffa087843b9bbbd35cdb2e637ce7109d7c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:47 +0000

    Add package argparse/0.1.0/argparse-0.1.0.tar.gz

---
+++ packages/argparse/argparse-0.1.0.tar.gz/argparse-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import textwrap
+import distutils.core
+
+distutils.core.setup(
+    name='argparse',
+    version='0.1.0',
+    author='Steven Bethard',
+    author_email='steven.bethard@gmail.com',
+    url='http://argparse.python-hosting.com/',
+    description='An optparse-inspired command-line parsing library',
+    long_description = textwrap.dedent("""\
+        Argparse takes the best of the optparse command-line parsing module
+        and brings it new life. Argparse adds positional as well as optional
+        arguments, the ability to create parsers for sub-commands, more
+        informative help and usage messages, and much more. At the same time,
+        it retains the ease and flexibility of use that made optparse so
+        popular."""),
+    license='BSD',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    py_modules=['argparse'],
+)
