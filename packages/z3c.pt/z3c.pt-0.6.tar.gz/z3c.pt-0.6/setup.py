13dbb16e8f3645216239473872d127d111b1e9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:03 +0000

    Add package z3c.pt/0.6/z3c.pt-0.6.tar.gz

---
+++ packages/z3c.pt/z3c.pt-0.6.tar.gz/z3c.pt-0.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '0.6'
+
+setup(name='z3c.pt',
+      version=version,
+      description="An implementation of the TAL template language.",
+      long_description=open("README.txt").read() + open("docs/HISTORY.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Text Processing :: Markup :: XML",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Malthe Borch',
+      author_email='mborch@gmail.com',
+      url='',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['z3c'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'lxml<=1.3.6',
+          'zope.i18n',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ packages/z3c.pt/z3c.pt-0.6.tar.gz/z3c.pt-0.6/z3c/pt/pagetemplate.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import translation
+import template
+
+class PageTemplate(template.BaseTemplate):
+    @property
+    def translate(self):
+        return translation.translate_xml
+
+class PageTemplateFile(template.BaseTemplateFile):
+    @property
+    def translate(self):
+        return translation.translate_xml
+
+class ViewPageTemplate(template.BaseViewTemplate):
+    def __init__(self, body):
+        super(ViewPageTemplate, self).__init__(body)
+        self.template = PageTemplate(body)
+    
+class ViewPageTemplateFile(template.BaseViewTemplateFile):
+    def __init__(self, filename):
+        super(ViewPageTemplateFile, self).__init__(filename)
+        self.template = PageTemplateFile(self.filename)
