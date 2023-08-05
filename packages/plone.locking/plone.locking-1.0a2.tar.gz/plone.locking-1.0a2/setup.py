f57b184151b9eb7b6ed3f811b93e329e110f3778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:17 +0000

    Add package plone.locking/1.0a2/plone.locking-1.0a2.tar.gz

---
+++ packages/plone.locking/plone.locking-1.0a2.tar.gz/plone.locking-1.0a2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0a2'
+
+setup(name='plone.locking',
+      version=version,
+      description="Webdav locking support",
+      long_description="""\
+plone.locking provides WebDAV locking support, with useful abstractions and
+views to assist a user interface. By default, it provides "stealable" locks,
+but can support other lock types. It is used by Plone, Archetypes and
+plone.app.iterate.
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='locking webdav plone archetypes',
+      author='Raphael Ritz, Jeff Roche, Martin Aspeli and others',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.locking',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
