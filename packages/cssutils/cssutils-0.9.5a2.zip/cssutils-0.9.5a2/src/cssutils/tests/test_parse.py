1068ba0d2f9033b699406a45030cc874a99b9e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package cssutils/0.9.5a2/cssutils-0.9.5a2.zip

---
+++ packages/cssutils/cssutils-0.9.5a2.zip/cssutils-0.9.5a2/src/cssutils/tests/test_parse.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""tests for parsing which does not raise Exceptions normally
+"""
+__author__ = '$LastChangedBy: cthedot $'
+__date__ = '$LastChangedDate: 2008-01-12 22:36:38 +0100 (Sa, 12 Jan 2008) $'
+__version__ = '$LastChangedRevision: 833 $'
+
+import xml.dom
+import basetest
+import cssutils
+
+class CSSStyleSheetTestCase(basetest.BaseTestCase):
+
+    def test_roundtrip(self):
+        "cssutils encodings"
+        css1 = ur'''@charset "utf-8";
+/* ä */'''
+        s = cssutils.parseString(css1)
+        css2 = unicode(s.cssText, 'utf-8')
+        self.assertEqual(css1, css2)
+
+        s = cssutils.parseString(css2)
+        s.cssRules[0].encoding='ascii'
+        css3 = ur'''@charset "ascii";
+/* \E4  */'''
+        self.assertEqual(css3, unicode(s.cssText, 'utf-8'))
+
+    def test_escapes(self):
+        "cssutils escapes"
+        css = ur'\43\x { \43\x: \43\x !import\41nt }'
+        sheet = cssutils.parseString(css)
+        self.assertEqual(sheet.cssText, ur'''C\x {
+    c\x: C\x !important
+    }''')
+
+        css = ur'\ x{\ x :\ x ;y:1} '
+        sheet = cssutils.parseString(css)
+        self.assertEqual(sheet.cssText, ur'''\ x {
+    \ x: \ x;
+    y: 1
+    }''')
+
+    def test_invalidstring(self):
+        "cssutils.parseString(INVALID_STRING)"
+        validfromhere = '@namespace "x";'
+        csss = (
+            u'''@charset "ascii
+                ;''' + validfromhere,
+            u'''@charset 'ascii
+                ;''' + validfromhere,
+            u'''@namespace "y
+                ;''' + validfromhere,
+            u'''@import "y
+                ;''' + validfromhere,
+            u'''@import url('a
+                );''' + validfromhere,
+            u'''@unknown "y
+                ;''' + validfromhere)
+        for css in csss:
+            s = cssutils.parseString(css)
+            self.assertEqual(validfromhere, s.cssText)
+
+        css = u'''a { font-family: "Courier
+                ; }'''
+        s = cssutils.parseString(css)
+        self.assertEqual(u'', s.cssText)
+
+    def test_invalid(self):
+        "cssutils.parseString(INVALID_CSS)"
+        tests = {
+            u'a {color: blue}} a{color: red} a{color: green}':
+                u'''a {
+    color: blue
+    }
+a {
+    color: green
+    }'''
+            }
+
+        for css in tests:
+            exp = tests[css] 
+            if exp == None:
+                exp = css
+            s = cssutils.parseString(css)
+            self.assertEqual(exp, s.cssText)
+
+    def test_specialcases(self):
+        "cssutils.parseString(special_case)"
+        tests = {
+            u'''
+    a[title="a not s\
+o very long title"] {/*...*/}''': u'''a[title="a not so very long title"] {
+    /*...*/
+    }'''
+        }
+        for css in tests:
+            exp = tests[css] 
+            if exp == None:
+                exp = css
+            s = cssutils.parseString(css)
+            self.assertEqual(exp, s.cssText)
+
+    def test_iehack(self):
+        "IEhack: $property"
+        # $color is not color!
+        css = 'a { color: green; $color: red; }'
+        s = cssutils.parseString(css)
+
+        p1 = s.cssRules[0].style.getProperty('color')
+        self.assertEqual('color', p1.name)
+        self.assertEqual('color', p1.literalname)
+        self.assertEqual('color', p1.normalname) # DEPRECATED
+        self.assertEqual('red', s.cssRules[0].style.getPropertyValue('$color'))
+
+        p2 = s.cssRules[0].style.getProperty('$color')
+        self.assertEqual('$color', p2.name)
+        self.assertEqual('$color', p2.literalname)
+        self.assertEqual('$color', p2.normalname) # DEPRECATED
+        self.assertEqual('green', s.cssRules[0].style.getPropertyValue('color'))
+        self.assertEqual('green', s.cssRules[0].style.color)
+
+
+    def test_attributes(self):
+        "cssutils.parseString(href, media)"
+        s = cssutils.parseString("a{}", href="file:foo.css", media="screen, projection, tv")
+        self.assertEqual(s.href, "file:foo.css")
+        self.assertEqual(s.media.mediaText, "screen, projection, tv")
+
+        s = cssutils.parseString("a{}", href="file:foo.css", media=["screen", "projection", "tv"])
+        self.assertEqual(s.media.mediaText, "screen, projection, tv")
+
+    def tearDown(self):
+        # needs to be reenabled here for other tests
+        cssutils.log.raiseExceptions = True
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
