977b82af882272800644e66090d70030ec057448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:44 +0000

    Add package FormEncode/0.3/FormEncode-0.3.tar.gz

---
+++ packages/FormEncode/FormEncode-0.3.tar.gz/FormEncode-0.3/setup.py
@@ -0,0 +1,30 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+setup(name="FormEncode",
+      version="0.3",
+      description="HTML form validation, generation, and conversion package",
+      long_description="""\
+FormEncode validates and converts nested structures.  It allows for
+a declarative form of defining the validation, and decoupled processes
+for filling and generating forms.
+""",
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: Python Software Foundation License",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      author="Ian Bicking",
+      author_email="ianb@colorstudy.com",
+      url="http://formencode.org",
+      license="PSF",
+      packages=["formencode", "formencode.util"],
+      package_data={'formencode': ['javascript/*']},
+      extras_require={'testing': ['elementtree']},
+      )
+
+# Send announce to:
+#   python-announce@python.org
+#   python-list@python.org
