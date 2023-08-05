d761c873da8c0b536cb639cbc0732abcfce4a22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package zope.app.security/3.4.0a1/zope.app.security-3.4.0a1.tar.gz

---
+++ packages/zope.app.security/zope.app.security-3.4.0a1.tar.gz/zope.app.security-3.4.0a1/setup.py
@@ -0,0 +1,57 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.security package
+
+$Id: setup.py 74453 2007-04-22 10:45:16Z ctheune $
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.security',
+      version = '3.4.0a1',
+      url='http://svn.zope.org/zope.app.security',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+	  packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+      extras_require=dict(test=['zope.app.testing']),
+      namespace_packages=['zope', 'zope.app'],
+      install_requires=['setuptools',
+                        'zope.app.authentication',
+                        'zope.app.component',
+                        'zope.app.container',
+                        'zope.app.form',
+                        'zope.app.i18n',
+                        'zope.app.pagetemplate',
+                        'zope.app.publisher',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.deferredimport',
+                        'zope.deprecation',
+                        'zope.i18n',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.location',
+                        'zope.publisher',
+                        'zope.schema',
+                        'zope.security',
+                        'ZODB3',
+                        ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
