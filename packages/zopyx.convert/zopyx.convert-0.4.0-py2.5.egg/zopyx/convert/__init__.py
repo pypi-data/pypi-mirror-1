c729efe540b5db10506e355ae1855f086429f250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:44 +0000

    Add package zopyx.convert/0.4.0/zopyx.convert-0.4.0-py2.5.egg

---
+++ packages/zopyx.convert/zopyx.convert-0.4.0-py2.5.egg/zopyx/convert/__init__.py
@@ -0,0 +1,8 @@
+##########################################################################
+# zopyx.convert - XSL-FO related functionalities
+#
+# (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+from convert import Converter
+from interfaces import IFOConverter, IHTML2FOConverter
