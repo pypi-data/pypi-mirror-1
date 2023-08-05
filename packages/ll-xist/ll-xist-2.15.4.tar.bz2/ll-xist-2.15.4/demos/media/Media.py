dff919676ac1d9357e3bb14d9ea14b3feb06df66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package ll-xist/2.15.4/ll-xist-2.15.4.tar.bz2

---
+++ packages/ll-xist/ll-xist-2.15.4.tar.bz2/ll-xist-2.15.4/demos/media/Media.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from ll.xist import xsc, sims, parsers
+from ll.xist.ns import html, htmlspecials, meta, xml, chars
+
+
+class name(xsc.Element):
+	model = sims.NoElements()
+
+	def convert(self, converter):
+		return self.content.convert(converter)
+
+
+class rc(xsc.Element):
+	model = sims.NoElements()
+
+	def convert(self, converter):
+		return self.content.convert(converter)
+
+
+class duration(xsc.Element):
+	model = sims.NoElements()
+
+	def convert(self, converter):
+		return xsc.Frag(self.content.convert(converter), " min")
+
+
+class place(xsc.Element):
+	model = sims.NoElements()
+
+	def convert(self, converter):
+		return self.content.convert(converter)
+
+
+class date(xsc.Element):
+	model = sims.NoElements()
+
+	def convert(self, converter):
+		return self.content.convert(converter)
+
+
+class price(xsc.Element):
+	model = sims.NoElements()
+	class Attrs(xsc.Element.Attrs):
+		class currency(xsc.TextAttr): pass
+
+	def convert(self, converter):
+		return xsc.Frag(self.content, " ", self["currency"]).convert(converter)
+
+
+class purchase(xsc.Element):
+	model = sims.Elements(place, date, price)
+
+	def convert(self, converter):
+		e = html.div(self[place], class_="purchase")
+		for e2 in self[price]:
+			e.append(": ", e2)
+		e.append(" ")
+		for e2 in self[date]:
+			e.append("(", e2, ")")
+		return e.convert(converter)
+
+
+class ld(xsc.Element):
+	model = sims.Elements(name, duration, purchase)
+
+	def convert(self, converter):
+		e = html.li(
+			html.span(self[name], class_="name")
+		)
+		for e2 in self[duration]:
+			e.append(" (", e2, ")")
+		e.append(self[purchase])
+		return e.convert(converter)
+
+
+class dvd(xsc.Element):
+	model = sims.Elements(name, rc, duration, purchase)
+
+	def convert(self, converter):
+		e = html.li(
+			html.span(self[name], class_="name")
+		)
+		durations = xsc.Frag(self[duration])
+		rcs = xsc.Frag(self[rc])
+		if len(durations) or len(rcs):
+			e.append(" (")
+			if len(durations):
+				e.append(durations[0])
+				if len(rcs):
+					e.append("; ")
+			if len(rcs):
+				e.append("RC ", rcs.withsep(", "))
+			e.append(")")
+		e.append(self[purchase])
+		return e.convert(converter)
+
+
+class media(xsc.Element):
+	model = sims.Elements(ld, dvd)
+
+	def convert(self, converter):
+		def namekey(node):
+			return unicode(node[name][0].content)
+
+		dvds = xsc.Frag(self[dvd]).sorted(key=namekey)
+		lds = xsc.Frag(self[ld]).sorted(key=namekey)
+
+		e = xsc.Frag(
+			xml.XML10(), "\n",
+			html.DocTypeXHTML10transitional(), "\n",
+			html.html(
+				html.head(
+					meta.contenttype(),
+					html.title("Media"),
+					meta.stylesheet(href="Media.css")
+				),
+				htmlspecials.plainbody(
+					html.h1("Media")
+				)
+			)
+		)
+		if lds:
+			e[-1][-1].append(html.h2(len(lds), " LDs"), html.ol(lds))
+		if dvds:
+			e[-1][-1].append(html.h2(len(dvds), " DVDs"), html.ol(dvds))
+		return e.convert(converter)
+
+
+class __ns__(xsc.Namespace):
+	xmlname = "media"
+	xmlurl = "http://xmlns.livinglogic.de/xist/demo/media"
+__ns__.update(vars())
+
+
+if __name__ == "__main__":
+	prefixes = xsc.Prefixes([__ns__, chars], xml=xml)
+	node = parsers.parseFile("Media.xml", prefixes=prefixes)
+	node = node[media][0]
+	node = node.conv()
+	print node.asBytes(encoding="us-ascii")
---
+++ packages/ll-xist/ll-xist-2.15.4.tar.bz2/ll-xist-2.15.4/src/ll/xist/options.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+## Copyright 1999-2007 by LivingLogic AG, Bayreuth/Germany.
+## Copyright 1999-2007 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See xist/__init__.py for the license
+
+
+"""
+Contains everthing related to options in &xist; (apart for syntax highlighting
+which can be found in presenters.py).
+"""
+
+__version__ = "$Revision: 1.1.2.1 $".split()[1]
+# $Source: /data/cvsroot/LivingLogic/Python/xist/src/ll/xist/options.py,v $
+
+import sys, os
+
+
+def getenvstr(name, default):
+	try:
+		return os.environ[name]
+	except:
+		return default
+
+
+def getenvint(name, default):
+	try:
+		return int(os.environ[name])
+	except:
+		return default
+
+
+repransi = getenvint("LL_XIST_REPRANSI", 0)  # should ANSI escape sequences be used for dumping the DOM tree and which ones? (0=off, 1=dark background, 2=light background)
+reprtab = getenvstr("LL_XIST_REPR_TAB", u"  ") # how to represent an indentation in the DOM tree?
+reprencoding = sys.getdefaultencoding()
+
+server = "localhost" # Host for server relative URLs
