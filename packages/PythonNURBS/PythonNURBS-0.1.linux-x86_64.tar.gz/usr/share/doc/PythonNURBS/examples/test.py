eb9d9499fdf130d1845f524c30683f253b1fbd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:02 +0000

    Add package PythonNURBS/0.1/PythonNURBS-0.1.linux-x86_64.tar.gz

---
+++ packages/PythonNURBS/PythonNURBS-0.1.linux-x86_64.tar.gz/usr/share/doc/PythonNURBS/examples/test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#***************************************************************************
+#*   Copyright (C) 2008-2008 by Oliver Borm                                *
+#*   oli.borm@web.de                                                       *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU General Public License as published by  *
+#*   the Free Software Foundation; either version 3 of the License, or     *
+#*   (at your option) any later version.                                   *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU General Public License for more details.                          *
+#*                                                                         *
+#*   You should have received a copy of the GNU General Public License     *
+#*   along with this program; if not, write to the                         *
+#*   Free Software Foundation, Inc.,                                       *
+#*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+#***************************************************************************
+
+# Author: Oliver Borm
+# Date: February 2008
+
+import NurbsCurveD
+import NurbsPointD
+import NurbsHPointD
+import NurbsVectorD
+import NurbsMatrixRTD
+import NurbsMatrixD
+import NurbsSurfaceD
+import NurbsColor
+
+print "Members from NurbsCurveD: "
+print dir(NurbsCurveD)
+print 
+print "Members from NurbsPointD: "
+print dir(NurbsPointD)
+print 
+print "Members from NurbsHPointD: "
+print dir(NurbsHPointD)
+print 
+print "Members from NurbsVectorD: "
+print dir(NurbsVectorD)
+print 
+print "Members from NurbsMatrixRTD: "
+print dir(NurbsMatrixRTD)
+print 
+print "Members from NurbsMatrixD: "
+print dir(NurbsMatrixD)
+print 
+print "Members from NurbsSurfaceD: "
+print dir(NurbsSurfaceD)
+print 
+print "Members from NurbsColor: "
+print dir(NurbsColor)
