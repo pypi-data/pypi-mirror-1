c8a0f2359412875f0357c63ec0ff024dd3530f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package zope.sendmail/3.5.0a1/zope.sendmail-3.5.0a1.tar.gz

---
+++ packages/zope.sendmail/zope.sendmail-3.5.0a1.tar.gz/zope.sendmail-3.5.0a1/setup.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.sendmail package
+
+$Id: setup.py 79016 2007-08-20 07:27:04Z philikon $
+"""
+from setuptools import setup, find_packages
+
+setup(name='zope.sendmail',
+      version = '3.5.0a1',
+      url='http://cheeseshop.python.org/pypi/zope.sendmail',
+      license='ZPL 2.1',
+      description='Zope sendmail',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description=open('README.txt').read(),
+
+      packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['setuptools',
+                        'ZODB3',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.schema',
+                        'zope.security',
+                        'zope.app.component',
+                       ],
+      include_package_data = True,
+      zip_safe = False,
+      )
