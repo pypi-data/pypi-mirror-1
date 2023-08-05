f387d1437d93b098b54a8b087defe28834b45b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package plone.recipe.varnish/1.0b3/plone.recipe.varnish-1.0b3.tar.gz

---
+++ packages/plone.recipe.varnish/plone.recipe.varnish-1.0b3.tar.gz/plone.recipe.varnish-1.0b3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+version = '1.0b3'
+
+setup(name='plone.recipe.varnish',
+      version=version,
+      description="Buildout recipe to install varnish",
+      long_description=open("README.txt").read(),
+      classifiers=[
+        "Framework :: Buildout",
+        "Framework :: Zope2",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX",
+        "Topic :: Internet :: Proxy Servers",
+        ],
+      keywords='buildout varnish cache proxy',
+      author='Wichert Akkerman',
+      author_email='wichert@wiggy.net',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+          "zc.buildout" : [
+              "build = plone.recipe.varnish:BuildRecipe",
+              "instance = plone.recipe.varnish:ConfigureRecipe",
+              ],
+      }
+      )
+
