c7d7b44ea48a1440170bf55c16cb6cc08e4a4d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:03 +0000

    Add package NoseXML/0.15/NoseXML-0.15.tar.gz

---
+++ packages/NoseXML/NoseXML-0.15.tar.gz/NoseXML-0.15/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="NoseXML",
+	version="0.15",
+	description="""Nose XML Output plugin""",
+	long_description="""A plugin for nose/nosetests that produces an XML report of the result of a test. The XML can then be post-processed using XSLT or additional code into readily viewable forms, and/or stored in a database for long-term analysis of test result trends.""",
+	author="Richard Clark",
+	author_email="richard@onesquared.net",
+	packages=['nosexml'],
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python',
+		'Topic :: Communications :: Email',
+		'Topic :: Office/Business',
+		'Topic :: Software Development :: Testing',
+		],
+	entry_points = {	'nose.plugins': [ 'nosexml = nosexml.plugin:NoseXML' ] }
+	)
