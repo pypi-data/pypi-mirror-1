23f8ff3f0adf384953a1213b67568eec4759e66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package zhpy/0.8.1/zhpy-0.8.1-py2.5.egg

---
+++ packages/zhpy/zhpy-0.8.1-py2.5.egg/tests/test_buildin_methods_cn.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+"""
+test build-in methods
+"""
+from zhpy import convertor
+
+def test_type():
+    """
+    test type method
+    """
+    assert convertor('类型("sting") == 类型("abc")') == \
+                    'type("sting") == type("abc")'
+    
+def test_len():
+    """
+    test len method
+    """
+    assert convertor("长度('hello')") == "len('hello')"
