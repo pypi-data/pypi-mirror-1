711b030718e25f272f82b42a82af78e3844e8aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package pytils/0.2.2/pytils-0.2.2.tar.gz

---
+++ packages/pytils/pytils-0.2.2.tar.gz/pytils-0.2.2/pytils/templatetags/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# pytils - simple processing for russian strings
+# Copyright (C) 2006-2007  Yury Yurevich
+#
+# http://www.pyobject.ru/pythy/projects/pytils/
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+"""
+Pytils templatetags for Django web-framework
+"""
+
+__id__ = __revision__ = "$Id: __init__.py 102 2007-07-12 12:33:36Z the.pythy $"
+__url__ = "$URL: https://pythy.googlecode.com/svn/tags/pytils/0_2_2/pytils/templatetags/__init__.py $"
