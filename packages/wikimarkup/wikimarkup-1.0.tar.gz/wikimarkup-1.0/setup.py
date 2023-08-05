8841dee4e9832c57b7fdb1d0f823d172ccd4c59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:11 +0000

    Add package wikimarkup/1.0/wikimarkup-1.0.tar.gz

---
+++ packages/wikimarkup/wikimarkup-1.0.tar.gz/wikimarkup-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wikimarkup',
+      version='1.0',
+      description='Wiki Markup (MediaWiki)',
+      author='Curse, Inc.',
+      author_email='dcramer@curse.com',
+      url='http://code.curse.com/wikimarkup/',
+      packages=['wikimarkup'],
+     )
