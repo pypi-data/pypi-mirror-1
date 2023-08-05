903827ea45e3cd91a262a8e9ec3512ac3dd48ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package plone.app.openid/1.0rc1/plone.app.openid-1.0rc1.tar.gz

---
+++ packages/plone.app.openid/plone.app.openid-1.0rc1.tar.gz/plone.app.openid-1.0rc1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0rc1'
+
+setup(name='plone.app.openid',
+      version=version,
+      description="Plone OpenID authentication support",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.app.openid',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      download_url='http://code.google.com/p/plone/downloads/list',
+      install_requires=[
+        'plone.openid',
+        'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
