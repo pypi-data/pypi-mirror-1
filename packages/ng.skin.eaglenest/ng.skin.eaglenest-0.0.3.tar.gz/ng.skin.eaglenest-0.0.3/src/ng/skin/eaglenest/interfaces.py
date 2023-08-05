3102fa3009aad1352239908f43c03d8b6f64c499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:54 +0000

    Add package ng.skin.eaglenest/0.0.3/ng.skin.eaglenest-0.0.3.tar.gz

---
+++ packages/ng.skin.eaglenest/ng.skin.eaglenest-0.0.3.tar.gz/ng.skin.eaglenest-0.0.3/src/ng/skin/eaglenest/interfaces.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""The definitions of new skin interface
+
+$Id: interfaces.py 50811 2008-02-21 12:01:55Z cray $
+"""
+__author__  = "Andrey Orlov, 2008"
+__license__ = "GPL"
+__version__ = "$Revision: 50811 $"
+
+from ng.skin.base.interfaces import NGBaseSkin
+
+class EagleNestSkin(NGBaseSkin):
+    """Skin for EagleNest Ru"""
