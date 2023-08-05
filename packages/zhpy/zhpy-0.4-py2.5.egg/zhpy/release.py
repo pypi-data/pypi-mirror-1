7aac64069f02ee5b7fe8dc6c184542dfc9111a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package zhpy/0.4/zhpy-0.4-py2.5.egg

---
+++ packages/zhpy/zhpy-0.4-py2.5.egg/zhpy/release.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+version = "0.4"
+author = "Fred Lin"
+email = "gasolin+zhpy@gmail.com"
+copyright = "Copyright 2007 Fred Lin and contributors"
+license = "MIT <http://www.opensource.org/licenses/mit-license.php>"
+url = "http://code.google.com/p/zhpy/"
+download_url="http://code.google.com/p/zhpy/"
+description="Write python language in chinese"
+long_description = """zhpy is python on Chinese, which good for Taiwan and China beginners to 
+learn python in your native language.
+
+zhpy is a lightweight python module and a source convertor, which provides a command line 
+tool to translate python code. The python code written by traditional and simplified 
+chinese could be translated to nature python code (english).
+
+zhpy use pyparsing module to detect chinese keywords, class name, methods, arguments, 
+variables and translate them back to python.
+
+It's possible for developers to port zhpy to python on korean or python on japenese.
+
+Check examples_ here.
+ 
+.. _examples: "http://code.google.com/p/zhpy/wiki/ZhpyExample"
+
+Install zhpy
+--------------
+
+You could use easy_install command to install zhpy::
+
+    $ easy_install zhpy
+
+or check instructions_ for detail.
+
+.. _instructions: "http://code.google.com/p/zhpy/wiki/DownloadInstall"
+
+Usage
+-----
+
+You could use 'zhpy' command instead of "python" in command line to 
+execute source code mixed in Chinese and English.::
+
+    $ zhpy hello.py
+    hello, world!
+
+You could assign a file name to export the zhpy source to the normal python source (english)::
+
+    $ zhpy hello.py nhello.py
+
+Then run the exported file as normal python source::
+
+    $ python nhello.py
+    hello, world!
+
+check the BasicUsage_ for detail.
+
+.. _BasicUsage: "http://code.google.com/p/zhpy/wiki/BasicUsage"
+
+Programming
+-----------
+
+You could mix original english keywords and Chinese keywords in your zhpy source.
+
+Reserved keywords are listed here_
+
+.. _here: http://code.google.com/p/zhpy/wiki/KeyWords
+
+You could view the ChangeLog_ to see what's new in these version
+
+.. _ChangeLog: http://code.google.com/p/zhpy/wiki/ChangeLog
+
+"""
