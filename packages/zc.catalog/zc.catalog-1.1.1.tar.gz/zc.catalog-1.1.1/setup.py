b67a9906398998415a11f848d6bf36fd07a07671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:13 +0000

    Add package zc.catalog/1.1.1/zc.catalog-1.1.1.tar.gz

---
+++ packages/zc.catalog/zc.catalog-1.1.1.tar.gz/zc.catalog-1.1.1/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="zc.catalog",
+    version="1.1.1",
+    packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
+    
+    package_dir= {'':'src'},
+    
+    namespace_packages=['zc'],
+    package_data = {
+    '': ['*.txt', '*.zcml'],
+    },
+    url='http://svn.zope.org/zc.catalog',
+    zip_safe=False,
+    author='Zope Project',
+    author_email='zope3-dev@zope.org',
+    description="zc.catalog contains a number of extensions to the Zope 3 catalog",
+    long_description=(
+        open('README.txt').read() + '\n' + open('CHANGES.txt').read()),
+    license='ZPL',
+    keywords="zope zope3 indexing",
+    classifiers = ['Framework :: Zope3'],
+    install_requires=['setuptools'],
+    )
---
+++ packages/zc.catalog/zc.catalog-1.1.1.tar.gz/zc.catalog-1.1.1/src/zc/catalog/tests.py
@@ -0,0 +1,48 @@
+#############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""catalog package test runner
+
+$Id: tests.py 2918 2005-07-19 22:12:38Z jim $
+"""
+
+import unittest
+from zope.testing import doctest, module
+import zope.component.testing
+
+def modSetUp(test):
+    zope.component.testing.setUp(test)
+    module.setUp(test, 'zc.catalog.doctest_test')
+
+def modTearDown(test):
+    module.tearDown(test)
+    zope.component.testing.tearDown(test)
+
+def test_suite():
+    tests = unittest.TestSuite((
+        doctest.DocFileSuite(
+            'extentcatalog.txt', setUp=modSetUp, tearDown=modTearDown,
+            optionflags=doctest.INTERPRET_FOOTNOTES),
+        doctest.DocFileSuite(
+            'setindex.txt', optionflags=doctest.INTERPRET_FOOTNOTES),
+        doctest.DocFileSuite('valueindex.txt'),
+        doctest.DocFileSuite('normalizedindex.txt'),
+        doctest.DocFileSuite('globber.txt'),
+        ))
+    import zc.catalog.stemmer
+    if not zc.catalog.stemmer.broken:
+        tests.addTest(doctest.DocFileSuite('stemmer.txt'))
+    return tests
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
