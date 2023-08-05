2266c04c8f80a4f8b32a782808b5d6c3ee033056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:22 +0000

    Add package Products.ATReferenceBrowserWidget/2.0b2/Products.ATReferenceBrowserWidget-2.0b2.tar.gz

---
+++ packages/Products.ATReferenceBrowserWidget/Products.ATReferenceBrowserWidget-2.0b2.tar.gz/Products.ATReferenceBrowserWidget-2.0b2/Products/ATReferenceBrowserWidget/Extensions/Install.py
@@ -0,0 +1,23 @@
+from zope.component import getUtility
+
+from StringIO import StringIO
+from Products.Archetypes.Extensions.utils import install_subskin
+from Products.CMFCore.interfaces import IPropertiesTool
+
+from Products.ATReferenceBrowserWidget.config import *
+        
+def install(self):
+    out = StringIO()
+    
+    # remove comments if you whish to install the demo type
+    # installTypes(self, out, listTypes(PROJECTNAME), PROJECTNAME)
+    
+    # install property for startup_directory
+    props = getUtility(IPropertiesTool).site_properties
+    if not props.hasProperty('refwidget_startupdirectories'):
+        props._setProperty('refwidget_startupdirectories', [], 'lines')
+        out.write('Registered property refwidget_startupdirectories in site_properties - see readme.txt')
+
+    install_subskin(self, out, GLOBALS)
+    out.write("Successfully installed %s." % PROJECTNAME)
+    return out.getvalue()
