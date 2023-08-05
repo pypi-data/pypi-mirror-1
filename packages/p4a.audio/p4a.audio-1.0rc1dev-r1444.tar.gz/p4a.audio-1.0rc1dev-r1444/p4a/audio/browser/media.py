8d617d3b79bdf7e9575cee8bd6c810c8dadb49c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:35 +0000

    Add package p4a.audio/1.0rc1dev-r1444/p4a.audio-1.0rc1dev-r1444.tar.gz

---
+++ packages/p4a.audio/p4a.audio-1.0rc1dev-r1444.tar.gz/p4a.audio-1.0rc1dev-r1444/p4a/audio/browser/media.py
@@ -0,0 +1,52 @@
+from zope import interface
+from zope.formlib import form
+try:
+    from zope.app.annotation import interfaces as annointerfaces
+except ImportError, err:
+    # Zope 2.10 support
+    from zope.annotation import interfaces as annointerfaces
+from p4a.audio import interfaces
+from p4a.audio import media
+from p4a.audio.browser import widget
+from p4a.common import feature
+
+class ToggleEnhancementsView(media.MediaActivator):
+    """
+    """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        was_activated = self.media_activated
+        self.media_activated = not was_activated
+        response = self.request.response
+        
+        if was_activated:
+            activated = 'Media+deactivated'
+        else:
+            activated = 'Media+activated'
+        
+        response.redirect(self.context.absolute_url()+'/view?portal_status_message='+activated)
+
+class BaseMediaDisplayView(form.PageDisplayForm):
+    """Base view for displaying media.
+    """
+
+    adapted_interface = None
+    media_field = None
+
+    def _media_player(self):
+        audio = self.adapters.get(self.adapted_interface,
+                                  self.adapted_interface(self.context))
+        field = self.adapted_interface[self.media_field].bind(audio)
+        player_widget = widget.MediaPlayerWidget(field, self.request)
+        player_widget.name = self.prefix + 'media_player'
+        player_widget._data = field.get(audio)
+        return player_widget
+    
+    def update(self):
+        super(BaseMediaDisplayView, self).update()
+        player_widget = self._media_player()
+        self.widgets += form.Widgets([(None, player_widget)], len(self.prefix))
