61424c011b6b4dd1e3ca8511d4a7f47d5306fd0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:45 +0000

    Add package zc.sshtunnel/1.1/zc.sshtunnel-1.1.tar.gz

---
+++ packages/zc.sshtunnel/zc.sshtunnel-1.1.tar.gz/zc.sshtunnel-1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+entry_points = """\
+
+[zc.buildout]
+default=zc.sshtunnel.recipe:Recipe
+
+"""
+
+setup(
+    name="zc.sshtunnel",
+    version="1.1",
+    description="zc.buildout recipe to manage and SSH tunnel",
+    author="Zope Corporation and contributors",
+    author_email="zope3-dev@zope.org",
+    packages=["zc", "zc.sshtunnel"],
+    package_dir={"": "src"},
+    namespace_packages=["zc"],
+    install_requires=["setuptools"],
+    entry_points=entry_points,
+    extras_require={"test": "zc.buildout"},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=["Framework :: Buildout"],
+    )
