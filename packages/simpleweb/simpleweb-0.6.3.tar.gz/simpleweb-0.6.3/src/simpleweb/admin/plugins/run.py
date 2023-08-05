2a742cb9526d88f85400fb07be4964b72a08b8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:54 +0000

    Add package simpleweb/0.6.3/simpleweb-0.6.3.tar.gz

---
+++ packages/simpleweb/simpleweb-0.6.3.tar.gz/simpleweb-0.6.3/src/simpleweb/admin/plugins/run.py
@@ -0,0 +1,23 @@
+import sys
+import simpleweb
+
+def run(name, args):
+	"""Usage: simpleweb-admin run 
+
+Start the simpleweb application in the current directory, 
+in the internal development web server
+	"""
+
+	if len(args) > 0:
+		sys.stderr.write("RUN takes no arguments\n")
+		sys.exit(0)
+
+	try:
+		__import__('urls')
+	except ImportError:
+		sys.stderr.write("Cannot find valid urls.py. Is this a simpleweb project directory?")
+	else:
+		simpleweb.run()
+	
+	
+
