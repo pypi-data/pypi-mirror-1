341bc4b9aee47291b3debdf0f030a5492b135c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:30 +0000

    Add package selector/0.8.8/selector-0.8.8.tar.gz

---
+++ packages/selector/selector-0.8.8.tar.gz/selector-0.8.8/setup.py
@@ -0,0 +1,64 @@
+"""setup - setuptools based setup for selector
+
+Copyright (C) 2006 Luke Arno - http://lukearno.com/
+
+This program is free software; you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by the 
+Free Software Foundation; either version 2 of the License, or (at your 
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to:
+
+The Free Software Foundation, Inc., 
+51 Franklin Street, Fifth Floor, 
+Boston, MA  02110-1301, USA.
+
+Luke Arno can be found at http://lukearno.com/
+
+"""
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(name='selector',
+      version='0.8.8',
+      description='WSGI delegation based on URL path and method.',
+      long_description="""\
+This distribution provides WSGI middleware
+for "RESTful" mapping of URL paths to WSGI applications.
+Selector now also comes with components for environ based
+dispatch and on-the-fly middleware composition.
+There is a very simple optional mini-language for
+path expressions. Alternately we can easily use
+regular expressions directly or even create our own
+mini-language. There is a simple "mapping file" format
+that can be used. There are no architecture specific
+features (to MVC or whatever). Neither are there any
+framework specific features. Best of all, selector is the
+simplest thing that will work well (IMHO).""",
+      author='Luke Arno',
+      author_email='luke.arno@gmail.com',
+      url='http://lukearno.com/projects/selector/',
+      license="GPL2",
+      py_modules=['selector'],
+      packages = [],
+      install_requires="resolver",
+      keywords="wsgi delegation web http rest webapps",
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Utilities'])
+
---
+++ packages/selector/selector-0.8.8.tar.gz/selector-0.8.8/tryme.py
@@ -0,0 +1,94 @@
+"""tryme - try out selector with flup's scgi server
+
+Copyright (C) 2006 Luke Arno - http://lukearno.com/
+
+This program is free software; you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by the 
+Free Software Foundation; either version 2 of the License, or (at your 
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to:
+
+The Free Software Foundation, Inc., 
+51 Franklin Street, Fifth Floor, 
+Boston, MA  02110-1301, USA.
+
+Luke Arno can be found at http://lukearno.com/
+
+"""
+from string import Template
+from selector import Selector, \
+                     EnvironDispatcher, MiddlewareComposer
+
+t = Template("Hello $name, \n\tWelcome to the selector.")
+
+def say_hello(environ, start_response):
+    """Say hello based on the name in a path like /hello/(name)"""
+    start_response("200 OK", [('Content-type', 'text/plain')])
+    return [t.safe_substitute(environ['selector.vars'])]
+
+
+def var_equ(val):
+    def fn(env):
+        if env['selector.vars']['val'] == val:
+            return True
+    return fn
+
+def status_app(status, message):
+    def theapp(environ, start_response):
+        start_response(status, [('Content-type', 'text/plain')])
+        return message
+    return theapp
+
+erules = [(var_equ('one'), status_app('200 Ok', 'hello one')),
+          (var_equ('two'), status_app('200 Ok', 'hello two')),
+          (lambda x: True, status_app('200 Ok', 'hello blarg'))]
+
+envdis = EnvironDispatcher(erules)
+
+def middle_say(word):
+    def m(fn):
+        def a(env, sr):
+            ret = list(fn(env, sr))
+            ret.insert(0, word+'(')
+            ret.append(')')
+            return ret
+        return a
+    return m
+
+mrules = [(var_equ('one'), middle_say("one")),
+          (var_equ('two'), middle_say("two")),
+          (lambda x: True, middle_say("always"))]
+
+app = status_app('200 Ok', 'app-to-wrap')
+
+composer = MiddlewareComposer(app, mrules)
+
+structure = [(r'/foo[/]?$', {'GET': status_app('200 OK', 'hello foo')}),
+             (r'/bar[/]?$', {'GET': status_app('200 OK', 'hello bar')}),
+             (r'/baz[/]?$', {'GET': status_app('200 OK', 'hello baz')})]
+
+structure2 = [('/yo[/[{name}[/]]]', {'GET': say_hello}),
+              ('/envdis/{val}[/]', {'GET': envdis}),
+              ('/compose/{val}[/]', {'GET': composer}),
+              ('/whatup/{name:number}[/]', {'GET': say_hello})]
+
+s = Selector(structure2, prefix='/selector')
+s.slurp(structure, prefix='^/selector', parser=lambda x: x)
+s.add('/hello/{name}', GET=say_hello)
+
+s.slurp_file('test.map')
+
+if __name__ == '__main__':
+    """Run the app with the Selector with wsgiref."""
+    from wsgiref.simple_server import make_server
+    try:
+        make_server('localhost', 9999, s).serve_forever()
+    except KeyboardInterrupt, ki:
+        print 'Thanks for playing!'
