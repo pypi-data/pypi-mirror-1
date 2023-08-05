bf3a7bba7ab840cd015b6907f3adc8782efe9ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package ll-xist/2.15.5/ll-xist-2.15.5.tar.gz

---
+++ packages/ll-xist/ll-xist-2.15.5.tar.gz/ll-xist-2.15.5/src/ll/xist/ns/tal.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+## Copyright 1999-2007 by LivingLogic AG, Bayreuth/Germany.
+## Copyright 1999-2007 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See xist/__init__.py for the license
+
+
+"""
+<par>An &xist; module that contains the global attributes from
+<app>Zope</app>s <z>Template Attribute Language</z>.</par>
+"""
+
+__version__ = "$Revision: 1.1.2.1 $".split()[1]
+# $Source: /data/cvsroot/LivingLogic/Python/xist/src/ll/xist/ns/tal.py,v $
+
+from ll.xist import xsc
+
+
+class __ns__(xsc.Namespace):
+	xmlname = "tal"
+	xmlurl = "http://xml.zope.org/namespaces/tal"
+
+	class Attrs(xsc.Namespace.Attrs):
+		class define(xsc.TextAttr): pass
+		class attributes(xsc.TextAttr): pass
+		class condition(xsc.TextAttr): pass
+		class replace(xsc.TextAttr): pass
+		class repeat(xsc.TextAttr): pass
+		class on_error(xsc.TextAttr): xmlname = "on-error"
+		class omit_tag(xsc.TextAttr): xmlname = "omit-tag"
+
+__ns__.makemod(vars())
