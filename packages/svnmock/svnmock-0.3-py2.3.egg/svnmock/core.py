856c8947d5524d087fb40391a4a89954f0cc4e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:34 +0000

    Add package svnmock/0.3/svnmock-0.3-py2.3.egg

---
+++ packages/svnmock/svnmock-0.3-py2.3.egg/svnmock/core.py
@@ -0,0 +1,4 @@
+import svnmock.common as __common
+import svn.core as __core
+
+__common.__build_mock_api('__core', globals())
---
+++ packages/svnmock/svnmock-0.3-py2.3.egg/svnmock/ra.py
@@ -0,0 +1,4 @@
+import svnmock.common as __common
+import svn.ra as __ra
+
+__common.__build_mock_api('__ra', globals())
