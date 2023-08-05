ab44610d322d7389d1e0d05ef9877752def4309d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:32 +0000

    Add package QuickWiki/0.1.2/QuickWiki-0.1.2-py2.4.egg

---
+++ packages/QuickWiki/QuickWiki-0.1.2-py2.4.egg/quickwiki/models/__init__.py
@@ -0,0 +1,29 @@
+import re
+import quickwiki.lib.helpers as h
+wikiwords = re.compile(r"\b([A-Z]\w+[A-Z]+\w+)")
+from docutils.core import publish_parts
+
+from sqlalchemy import *
+meta = DynamicMetaData()
+
+pages_table = Table('pages', meta,
+    Column('title', String(40), primary_key=True),
+    Column('content', String(), default='')
+)
+
+class Page(object):
+    def __str__(self):
+        return self.title
+        
+    content = None
+
+    def get_wiki_content(self):
+        content = publish_parts(self.content, writer_name="html")["html_body"]
+        titles = wikiwords.findall(content)
+        for title in titles:
+            content = content.replace(title, h.link_to(title, 
+                    h.url_for(controller='page', action='index', 
+                        title=title)))
+        return content
+
+page_mapper = mapper(Page, pages_table)
