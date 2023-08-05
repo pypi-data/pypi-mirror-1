53b9aa060c8558ac677835bce14263e434d49541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:20 +0000

    Add package plone.app.workflow/1.0a2/plone.app.workflow-1.0a2-py2.4.egg

---
+++ packages/plone.app.workflow/plone.app.workflow-1.0a2-py2.4.egg/plone/app/workflow/interfaces.py
@@ -0,0 +1,15 @@
+from zope.interface import Interface
+from zope import schema
+
+class ISharingPageRole(Interface):
+    """A named utility providing information about roles that are managed
+    by the sharing page.
+    
+    Utility names should correspond to the role name.
+    """
+    
+    title = schema.TextLine(title=u"A friendly name for the role")
+    
+    required_permission = schema.TextLine(title=u"Permission required to manag this local role",
+                                          required=False)
+    
