84b810b935f0d69de85b51c26bd5f92a071ae37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:35 +0000

    Add package Migration/0.2a4.dev-r642/Migration-0.2a4.dev_r642-py2.4.egg

---
+++ packages/Migration/Migration-0.2a4.dev_r642-py2.4.egg/migration/base_classes.py
@@ -0,0 +1,33 @@
+migrations = dict()
+
+class MigrationMeta(type):
+    def __init__(cls, name, *args, **kw):
+        if name not in ['Migration', 'SOMigration', 'SAMigration'] and \
+           name.startswith('Migration'):
+            version = int(name[-3:])
+            migrations[version] = cls
+        super(MigrationMeta, cls).__init__(*args, **kw)
+
+class Migration(object):
+    __metaclass__ = MigrationMeta
+    def add_column(self, *args, **kw):
+        assert 0, 'This has to be overriden'
+    def del_column(self, *args, **kw):
+        assert 0, 'This has to be overriden'
+    def create_table(self, *args, **kw):
+        assert 0, 'This has to be overriden'
+    def drop_table(self, *args, **kw):
+        assert 0, 'This has to be overriden'
+    def up(self):
+        assert 0, 'This has to be overriden'
+    def down(self):
+        assert 0, 'This has to be overriden'
+    def query(self, query):
+        assert 0, 'This has to be overriden'
+       
+class PackageVersion(object):
+    def get_version(self):
+        assert 0, 'This has to be overriden'
+    
+    def set_version(self, version):
+        assert 0, 'This has to be overriden'
---
+++ packages/Migration/Migration-0.2a4.dev_r642-py2.4.egg/migration/package.py
@@ -0,0 +1,5 @@
+def get_migrations_module(package):
+    #migrations = getattr(__import__(package, {}, {}, ["migrations"]), "migrations", None)
+    migrations = __import__(package, {}, {})
+    return migrations
+    
