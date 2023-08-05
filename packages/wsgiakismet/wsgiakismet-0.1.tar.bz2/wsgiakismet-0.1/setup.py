13135e7946de59e0c839eeb4421302282a8e0f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:43 +0000

    Add package wsgiakismet/0.1/wsgiakismet-0.1.tar.bz2

---
+++ packages/wsgiakismet/wsgiakismet-0.1.tar.bz2/wsgiakismet-0.1/setup.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2006 L. C. Rees.  All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1.  Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3.  Neither the name of the Portable Site Information Project nor the names
+# of its contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+'''setup - setuptools based setup for wsgiakismet.'''
+
+import ez_setup
+ez_setup.use_setuptools()
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(name='wsgiakismet',
+      version='0.1',
+      description='''Akismet SPAM blocking WSGI middleware.''',
+      long_description='''Validates form submissions against the Akismet service
+to verify that they are not spam.
+
+Simple usage example:
+
+import cgi
+from wsgiakismet import akismet
+
+# Wordpress API Key and website name are required arguments
+@akisimet('3489012ab121', 'http://blog.example.com/')
+def app(env, start_response):
+    usersub = cgi.parse(fp=env['wsgi.input'], environ=env)
+    start_response('200 OK', [('Content-type', 'text/plain')])
+    return ['Comment is %s' % usersub['comment'][0]]
+''',
+      author='L. C. Rees',
+      author_email='lcrees@gmail.com',
+      license='BSD',
+      py_modules=['wsgiakismet'],
+      packages = ['tests'],
+      zip_safe = True,
+      keywords='WSGI comment spam blocking blog middleware web HTTP',
+      classifiers=['Development Status :: 4 - Beta',
+                    'Environment :: Web Environment',
+                    'License :: OSI Approved :: BSD License',
+                    'Natural Language :: English',
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python',
+                    'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware'])
