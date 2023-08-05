d32dc26068851a1d3c684787f113645c32c7bea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:08 +0000

    Add package LyricSearch/0.1.9/LyricSearch-0.1.9.tar.gz

---
+++ packages/LyricSearch/LyricSearch-0.1.9.tar.gz/LyricSearch-0.1.9/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+PACKAGE = 'LyricSearch'
+VERSION = '0.1.9'
+
+setup(
+    name = PACKAGE,
+    version = VERSION,
+    author = 'Claudio Torcato',
+    author_email = 'clauditorcato@gmail.com',
+    description = 'script for letter of music in web',
+    classifiers = [
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Natural Language :: Portuguese (Brazilian)',
+    ],
+    packages = ['lyricsearch'],
+    install_requires = [
+        "BeautifulSoup >= 3.0.3"],    
+    entry_points = """
+    [lyricsearch.search]
+    terra = lyricsearch.lyricterra:LyricsTerra
+    [console_scripts]
+    lyse = lyricsearch.lyse:command
+    """
+)
