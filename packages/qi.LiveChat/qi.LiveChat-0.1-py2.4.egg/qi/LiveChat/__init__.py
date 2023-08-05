641cdff0a4f6c0cdd5fd6d75bb95a952c9895344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package qi.LiveChat/0.1/qi.LiveChat-0.1-py2.4.egg

---
+++ packages/qi.LiveChat/qi.LiveChat-0.1-py2.4.egg/qi/LiveChat/__init__.py
@@ -0,0 +1,20 @@
+from config import *
+from zope.i18nmessageid import MessageFactory
+from Products.CMFCore import utils
+from Products.Archetypes import atapi
+
+LiveChatMessageFactory = MessageFactory('qi.LiveChat')
+
+def initialize(context):
+	import content.LiveChat
+
+	content_types, constructors, ftis = atapi.process_types(
+		atapi.listTypes(config.PROJECTNAME),
+		config.PROJECTNAME)
+
+	for atype, constructor in zip(content_types, constructors):
+		utils.ContentInit("%s: %s" % (config.PROJECTNAME, atype.portal_type),
+			content_types	   = (atype,),
+			permission		   = config.ADD_PERMISSIONS[atype.portal_type],
+			extra_constructors = (constructor,),
+			).initialize(context)
