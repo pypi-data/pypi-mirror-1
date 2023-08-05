8ca0711c93200ccb18444be78ea3d3c2aa0e064b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:28 +0000

    Add package plone.app.contentrules/1.0b1.1/plone.app.contentrules-1.0b1.1.tar.gz

---
+++ packages/plone.app.contentrules/plone.app.contentrules-1.0b1.1.tar.gz/plone.app.contentrules-1.0b1.1/plone/app/contentrules/browser/interfaces.py
@@ -0,0 +1,24 @@
+from zope.interface import Interface
+from zope.app.container.interfaces import IAdding
+
+class IRuleAdding(IAdding):
+    """Marker interface for rule add views.
+    
+    Rules' addviews should be registered for this.
+    """
+    
+class IRuleElementAdding(IAdding):
+    """Marker interface for rule element (actions/conditions) add views.
+    
+    Rules' addviews should be registered for this.
+    """
+    
+class IRuleConditionAdding(IRuleElementAdding):
+    pass
+    
+class IRuleActionAdding(IRuleElementAdding):
+    pass
+
+class IContentRulesForm(Interface):
+    """Marker interface for forms that need content rules layout
+    """
