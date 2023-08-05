4440f575d13fa0bbe154262e3e9ca442196054d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:16 +0000

    Add package email/3.0.1/email-3.0.1.tar.gz

---
+++ packages/email/email-3.0.1.tar.gz/email-3.0.1/docs/internals.pl
@@ -0,0 +1,50 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/module-email.Utils/;
+$ref_files{$key} = "$dir".q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Parser/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Errors/;
+$ref_files{$key} = "$dir".q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Generator/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Header/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Encoders/;
+$ref_files{$key} = "$dir".q|node14.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Iterators/;
+$ref_files{$key} = "$dir".q|node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/about/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Charset/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Message/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ packages/email/email-3.0.1.tar.gz/email-3.0.1/docs/labels.pl
@@ -0,0 +1,97 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/module-email.Utils/;
+$external_labels{$key} = "$URL/" . q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Parser/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Errors/;
+$external_labels{$key} = "$URL/" . q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Generator/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Header/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Encoders/;
+$external_labels{$key} = "$URL/" . q|node14.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Iterators/;
+$external_labels{$key} = "$URL/" . q|node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/about/;
+$external_labels{$key} = "$URL/" . q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Charset/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Message/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/module-email.Utils/;
+$external_latex_labels{$key} = q|2.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Parser/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Errors/;
+$external_latex_labels{$key} = q|2.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Generator/;
+$external_latex_labels{$key} = q|2.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Header/;
+$external_latex_labels{$key} = q|2.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Encoders/;
+$external_latex_labels{$key} = q|2.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Iterators/;
+$external_latex_labels{$key} = q|2.10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Charset/;
+$external_latex_labels{$key} = q|2.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/module-email.Message/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+1;
+
