6e7fe9f1fdcc0fc27264321892415e501c8af8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:41 +0000

    Add package TheMingServer/0.7.2/TheMingServer-0.7.2.tar.gz

---
+++ packages/TheMingServer/TheMingServer-0.7.2.tar.gz/TheMingServer-0.7.2/TheMingServer/__init__.py
@@ -0,0 +1,7 @@
+# __init__ for The Ming Server package
+
+from Constants import Constants
+from Incoming import Incoming
+from Logger import Logger
+from PageMaker import PageMaker
+from SiteWalker import SiteWalker
---
+++ packages/TheMingServer/TheMingServer-0.7.2.tar.gz/TheMingServer-0.7.2/TheMingServer/writers/StaticWriter.py
@@ -0,0 +1,41 @@
+
+# StaticWriter.py
+# billy_bob_ming@yahoo.com
+
+# Copyright (C) 2005 by Richard Harris
+# Released under the GNU General Public License
+# (See the included COPYING file)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+import os
+
+class StaticWriter:
+	NAME = "StaticWriter"
+
+	def __init__(self, debug):
+		self.debug = debug
+		return
+
+	def write(self, fname, page):
+		if (self.debug): print "Static: "+fname
+		dir = os.path.dirname(fname)
+		if not os.path.isdir(dir):
+			os.makedirs(dir)
+		page = "".join(page)
+		f = open(fname,'w')
+		f.write(page)
+		f.close()
+		return 
