8a3313a1d642805058d38eae5917880e59de6c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:59 +0000

    Add package ng.skin.dreambot/0.0.2/ng.skin.dreambot-0.0.2.tar.gz

---
+++ packages/ng.skin.dreambot/ng.skin.dreambot-0.0.2.tar.gz/ng.skin.dreambot-0.0.2/src/ng/skin/dreambot/interfaces.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""The definitions of new skin interface
+
+$Id: interfaces.py 49957 2008-02-06 20:32:00Z cray $
+"""
+__author__  = "Andrey Orlov, 2008"
+__license__ = "GPL"
+__version__ = "$Revision: 49957 $"
+
+from ng.skin.base.interfaces import NGBaseSkin
+
+class DreamBotSkin(NGBaseSkin):
+    """Skin for DreamBot Ru"""
