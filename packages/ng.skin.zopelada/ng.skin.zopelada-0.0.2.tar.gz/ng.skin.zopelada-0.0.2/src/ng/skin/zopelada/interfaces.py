2580ea5cc4f37732ad7911c0a42775dd13499fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package ng.skin.zopelada/0.0.2/ng.skin.zopelada-0.0.2.tar.gz

---
+++ packages/ng.skin.zopelada/ng.skin.zopelada-0.0.2.tar.gz/ng.skin.zopelada-0.0.2/src/ng/skin/zopelada/interfaces.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""The definitions of new skin interface
+
+$Id: interfaces.py 50730 2008-02-18 22:01:39Z cray $
+"""
+__author__  = "Andrey Orlov, 2008"
+__license__ = "GPL"
+__version__ = "$Revision: 50730 $"
+
+from ng.skin.base.interfaces import NGBaseSkin
+
+class ZopeLadaSkin(NGBaseSkin):
+    """Skin for ZopeLada Ru"""
