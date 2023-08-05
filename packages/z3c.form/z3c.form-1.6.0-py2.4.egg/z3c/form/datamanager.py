7408ab9c8cc32ae176f3add4ea500285b0b27044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package z3c.form/1.6.0/z3c.form-1.6.0-py2.4.egg

---
+++ packages/z3c.form/z3c.form-1.6.0-py2.4.egg/z3c/form/datamanager.py
@@ -0,0 +1,114 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Widget Framework Implementation
+
+$Id: datamanager.py 76848 2007-06-20 17:46:18Z srichter $
+"""
+__docformat__ = "reStructuredText"
+
+import zope.interface
+import zope.component
+import zope.schema
+from zope.security.checker import canAccess, canWrite, Proxy
+
+from z3c.form import interfaces
+
+
+class DataManager(object):
+    """Data manager base class."""
+    zope.interface.implements(interfaces.IDataManager)
+
+
+class AttributeField(DataManager):
+    """Attribute field."""
+    zope.component.adapts(
+        zope.interface.Interface, zope.schema.interfaces.IField)
+
+    def __init__(self, context, field):
+        self.context = context
+        self.field = field
+
+    def get(self):
+        """See z3c.form.interfaces.IDataManager"""
+        # get the right adapter or context
+        context = self.context
+        if self.field.interface is not None:
+            context = self.field.interface(context)
+        return getattr(context, self.field.__name__)
+
+    def query(self, default=interfaces.NOVALUE):
+        """See z3c.form.interfaces.IDataManager"""
+        try:
+            return self.get()
+        except AttributeError:
+            return default
+
+    def set(self, value):
+        """See z3c.form.interfaces.IDataManager"""
+        if self.field.readonly:
+            raise TypeError("Can't set values on read-only fields "
+                            "(name=%s, class=%s.%s)"
+                            % (self.field.__name__,
+                               self.context.__class__.__module__,
+                               self.context.__class__.__name__))
+        # get the right adapter or context
+        context = self.field.interface(self.context)
+        setattr(context, self.field.__name__, value)
+
+    def canAccess(self):
+        """See z3c.form.interfaces.IDataManager"""
+        if isinstance(self.context, Proxy):
+            return canAccess(self.context, self.field.__name__)
+        return True
+
+    def canWrite(self):
+        """See z3c.form.interfaces.IDataManager"""
+        if isinstance(self.context, Proxy):
+            return canWrite(self.context, self.field.__name__)
+        return True
+
+class DictionaryField(DataManager):
+    """Dictionary field."""
+    zope.component.adapts(
+        dict, zope.schema.interfaces.IField)
+
+    def __init__(self, data, field):
+        if not isinstance(data, dict):
+            raise ValueError("Data are not a dictionary: %s" %type(data))
+        self.data = data
+        self.field = field
+
+    def get(self):
+        """See z3c.form.interfaces.IDataManager"""
+        return self.data[self.field.__name__]
+
+    def query(self, default=interfaces.NOVALUE):
+        """See z3c.form.interfaces.IDataManager"""
+        return self.data.get(self.field.__name__, default)
+
+    def set(self, value):
+        """See z3c.form.interfaces.IDataManager"""
+        if self.field.readonly:
+            raise TypeError("Can't set values on read-only fields name=%s"
+                            % self.field.__name__)
+        self.data[self.field.__name__] = value
+
+    def canAccess(self):
+        """See z3c.form.interfaces.IDataManager"""
+        return True
+
+    def canWrite(self):
+        """See z3c.form.interfaces.IDataManager"""
+        return True
+
