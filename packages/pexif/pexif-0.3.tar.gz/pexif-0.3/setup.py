bd36c9c9a6da1b449828095e6e34f630bfd1e771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:09 +0000

    Add package pexif/0.3/pexif-0.3.tar.gz

---
+++ packages/pexif/pexif-0.3.tar.gz/pexif-0.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+"""Setup script for pexif"""
+
+setup (
+    name = "pexif",
+    version = "0.3",
+    description = "A module for editing JPEG EXIF data",
+    long_description = "This module allows you to parse and edit the EXIF data tags in a JPEG image.",
+    author = "Ben Leslie",
+    author_email = "benno@benno.id.au",
+    url = "http://www.benno.id.au/code/pexif/",
+    license = "python",
+    py_modules = ["pexif"],
+    scripts = ["scripts/dump_exif.py",
+               "scripts/setgps.py",
+               "scripts/getgps.py"],
+    platforms = ["any"],
+    classifiers = ["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "License :: OSI Approved :: Python Software Foundation License",
+                   "Topic :: Multimedia :: Graphics"]
+    )
+    
+    
