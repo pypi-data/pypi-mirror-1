7e43eb39b41fc57a9dc5addb5fef330d5944c050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package zhpy/1.1/zhpy-1.1-py2.5.egg

---
+++ packages/zhpy/zhpy-1.1-py2.5.egg/zhpy/release.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+"""Release information"""
+
+
+version = "1.1"
+author = "Fred Lin"
+email = "gasolin+zhpy@gmail.com"
+copyright = "Copyright 2007 Fred Lin and contributors"
+license = "MIT <http://www.opensource.org/licenses/mit-license.php>"
+url = "http://code.google.com/p/zhpy/"
+download_url="http://code.google.com/p/zhpy/"
+description="Write python language in chinese"
+long_description = """zhpy is the full feature python language with 
+fully tested chinese keywords, variables, and parameters support, 
+independent on python version,
+bundle with command line tool, interpreter, pluggable keyword system and 
+great document.
+
+zhpy on python is good for Taiwan and China beginners to learn python in 
+our native language.
+
+The core of zhpy is a lightweight python module and a chinese source convertor
+based on python, 
+which provides interpreter and command line tool to translate zhpy code to
+python. zhpy integrated a setuptools-based plugin system for keyword reuse.
+The zhpy code written in traditional and simplified
+chinese could be translated and converted to natual python code.
+Thus it could be execute as nature python code and be used in normal
+python programs.
+
+Use 'zhpy' command instead of "python" in command line to execute source
+code wrote in Chinese and English.
+
+zhpy provide a method 'zh_exec' that allow to embed
+chinese script in python, and zhpy could be used as the chinese script in
+shell as well.
+
+Check examples here.
+http://code.google.com/p/zhpy/wiki/ZhpyExample
+
+Install zhpy
+--------------
+
+You could use easy_install command to install zhpy::
+
+    $ easy_install zhpy
+
+or check instructions for detail.
+  * http://code.google.com/p/zhpy/wiki/DownloadInstall
+
+Usage
+-------
+
+You could use zhpy interpreter to test zhpy::
+
+    $ zhpy
+    >>> print 'hello in chinese'
+    hello in chinese
+
+Browse project homepage to get examples in chinese.
+  * http://code.google.com/p/zhpy/
+
+check the BasicUsage for detail.
+http://code.google.com/p/zhpy/wiki/BasicUsage
+
+Programming Guide
+-------------------
+
+An C.C licensed zhpy book "A Byte of Zhpy" is in progress.
+You could freely view it online.
+The book is based on "A Byte of python".
+
+  * http://code.google.com/p/zhpy/wiki/ByteOfZhpy
+  * http://swaroopch.info/text/Byte_of_Python:Main_Page
+
+There's the API document available in zhpy download list
+  * http://code.google.com/p/zhpy/downloads/list
+
+Change Log
+-------------
+
+You could view the ChangeLog to see what's new in these version.
+http://zhpy.googlecode.com/svn/trunk/CHANGELOG.txt
+
+"""
