aa9f3ae7c912810f3acefb110a274a66ad323693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:24 +0000

    Add package checkselect/1.0/checkselect-1.0-py2.5.egg

---
+++ packages/checkselect/checkselect-1.0-py2.5.egg/checkselect/release.py
@@ -0,0 +1,22 @@
+# Release information about checkselect
+
+version = "1.0"
+
+description = "CheckSelect TurboGears Widget"
+long_description = """\
+CheckSelect aims to solve a problem found with Multiple Select fields
+where it isn't possible to specify their widths, messing up with some
+layouts.  It also aims to make selecting multiple choices easier and
+more accessible for users of your application.
+
+This is a widgetized version of Nicholas Rougeux code at
+http://c82.net/article.php?ID=25 that extends TurboGears' default
+CheckBoxList code.
+"""
+author = "Jorge Godoy"
+email = "jgodoy@gmail.com"
+copyright = "Copyright 2007 by Jorge Godoy"
+
+url = "http://public-trac.godoy.homeip.net/trac/checkselect/"
+
+license = "MIT"
---
+++ packages/checkselect/checkselect-1.0-py2.5.egg/checkselect/widgets.py
@@ -0,0 +1,57 @@
+import pkg_resources
+
+from turbogears.widgets import CSSLink, JSLink, \
+                               WidgetDescription, register_static_directory, \
+                               CheckBoxList, mochikit, js_location
+
+static_dir = pkg_resources.resource_filename("checkselect", "static")
+register_static_directory("checkselect", static_dir)
+
+js_dir = pkg_resources.resource_filename("checkselect", "static/javascript")
+
+checkselect_css = [CSSLink("checkselect", "css/checkselect.css")]
+checkselect_js = [
+    mochikit,
+    JSLink("checkselect", "javascript/checkselect.js",
+        location = js_location.bodytop),
+    ]
+
+__all__ = [
+    'CheckSelect',
+    ]
+
+
+class CheckSelect(CheckBoxList):
+    """
+CheckSelect aims to solve a problem found with Multiple Select fields
+where it isn't possible to specify their widths, messing up with some
+layouts.  It also aims to make selecting multiple choices easier and
+more accessible for users of your application.
+
+This is a widgetized version of Nicholas Rougeux code at
+http://c82.net/article.php?ID=25 that extends TurboGears' default
+CheckBoxList code.
+    """
+    template = "checkselect.templates.checkselect"
+    css = checkselect_css
+    javascript = checkselect_js
+
+
+class CheckSelectDesc(WidgetDescription):
+    name = "CheckSelect"
+    for_widget = CheckSelect("your_checkselect",
+                              options=[(1, "Python"),
+                                       (2, "Java"),
+                                       (3, "Pascal"),
+                                       (4, "Ruby"),
+                                       (5, "JavaScript"),
+                                       (6, "C"),
+                                       (7, "C++"),
+                                       (8, "Eiffel"),
+                                       (9, "Haskell"),
+                                       (10, "SmallTalk"),
+                                       (11, "ADA"),
+                                       (12, "APL"),
+                                       (13, "Perl"),
+                                       (14, "A very long entry to see what happens when this exceeds the horizontal size of the list")],
+                              default=[1,5])
