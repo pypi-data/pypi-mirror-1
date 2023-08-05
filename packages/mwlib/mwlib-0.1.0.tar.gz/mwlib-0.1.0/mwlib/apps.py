4e405b9c0896aba5ccb8989607682ba10cce3276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package mwlib/0.1.0/mwlib-0.1.0.tar.gz

---
+++ packages/mwlib/mwlib-0.1.0.tar.gz/mwlib-0.1.0/mwlib/apps.py
@@ -0,0 +1,236 @@
+
+# Copyright (c) 2007, PediaPress GmbH
+# See README.txt for additional licensing information.
+
+"""main programs - installed via setuptools' entry_points"""
+
+import optparse
+
+def buildcdb():
+    parser = optparse.OptionParser(usage="%prog --input XMLDUMP --output OUTPUT")
+    parser.add_option("-i", "--input", help="input file")
+    parser.add_option("-o", "--output", help="write output to OUTPUT")
+    options, args = parser.parse_args()
+    
+    if args:
+        parser.error("too many arguments.")
+
+    
+    input = options.input
+    output = options.output
+
+    if not (input and output):
+        parser.error("missing argument.")
+        
+    import os
+    from mwlib import cdbwiki
+
+    cdbwiki.BuildWiki(input, output)()
+    open(os.path.join(output, "wikiconf.txt"), "w").write("""
+[wiki]
+type = cdb
+path = %s
+
+[images]
+type = download
+url = http://upload.wikimedia.org/wikipedia/commons/
+localpath = ~/images
+""" % (os.path.abspath(output),))
+
+def show():
+    parser = optparse.OptionParser(usage="%prog [-e|--expand] --conf CONF ARTICLE [...]")
+    parser.add_option("-c", "--conf", help="config file")
+    parser.add_option("-e", "--expand", action="store_true", help="expand templates")
+    parser.add_option("-t", "--template", action="store_true", help="show template")
+    
+    options, args = parser.parse_args()
+    
+    if not args:
+        parser.error("missing ARTICLE argument")
+        
+    articles = [unicode(x, 'utf-8') for x in args]
+
+    conf = options.conf
+    if not conf:
+        parser.error("missing --conf argument")
+
+    from mwlib import wiki, expander
+    
+    db = wiki.makewiki(conf)['wiki']
+    
+    for a in articles:
+        if options.template:
+            raw=db.getTemplate(a)
+        else:
+            raw=db.getRawArticle(a)
+
+        if raw:
+            if options.expand:
+                te = expander.Expander(raw, pagename=a, wikidb=db)
+                raw = te.expandTemplates()
+
+            print raw.encode("utf-8")
+
+
+def buildzip():
+    parser = optparse.OptionParser(usage="%prog --conf CONF -o OUTPUT ARTICLE [...]")
+    parser.add_option("-c", "--conf", help="config file")
+    parser.add_option("-x", "--noimages", action="store_true", help="exclude images")
+
+    parser.add_option("-o", "--output", help="write output to OUTPUT")
+    options, args = parser.parse_args()
+    
+    if not args:
+        parser.error("missing ARTICLE argument")
+        
+    articles = [unicode(x, 'utf-8') for x in args]
+
+    conf = options.conf
+    if not options.conf:
+        parser.error("missing --conf argument")
+
+    output = options.output
+    if not output:
+        parser.error("missing -o/--output argument.")
+
+    from mwlib import wiki, recorddb
+
+    w = wiki.makewiki(conf)
+    if options.noimages:
+        w['images'] = None
+
+    z = recorddb.ZipfileCreator(w['wiki'], w['images'])
+
+    for x in articles:
+        z.addArticle(x)
+
+    z.createArchive(output)
+
+
+def parse():
+    parser = optparse.OptionParser(usage="%prog [-a|--all] --conf CONF [ARTICLE1 ...]")
+    parser.add_option("-a", "--all", action="store_true", help="parse all articles")
+    parser.add_option("--tb", action="store_true", help="show traceback on error")
+
+    parser.add_option("-c", "--conf", help="config file")
+
+    options, args = parser.parse_args()
+                                   
+    if not args and not options.all:
+        parser.error("missing option.")
+        
+    if not options.conf:
+        parser.error("missing --conf argument")
+
+    articles = [unicode(x, 'utf-8') for x in args]
+
+    conf = options.conf
+    
+    import traceback
+    from mwlib import wiki, uparser
+    
+    w = wiki.makewiki(conf)
+    
+    db = w['wiki']
+
+    if options.all:
+        if not hasattr(db, "articles"):
+            raise RuntimeError("%s does not support iterating over all articles" % (db, ))
+        articles = db.articles()
+
+
+    
+    for x in articles:
+        try:
+            raw = db.getRawArticle(x)
+            # yes, raw can be None, when we have a redirect to a non-existing article.
+            if raw is None: 
+                continue
+            a=uparser.parseString(x, raw=raw, wikidb=db)
+        except Exception, err:
+            print "-", repr(x), err
+            if options.tb:
+                traceback.print_exc()
+        else:
+            print "+", repr(x)
+
+def serve():
+    parser = optparse.OptionParser(usage="%prog --conf CONF ARTICLE [...]")
+    parser.add_option("-c", "--conf", help="config file")
+
+    options, args = parser.parse_args()
+    
+
+    conf = options.conf
+    if not options.conf:
+        parser.error("missing --conf argument")
+    
+    from mwlib import wiki, web
+    
+    res = wiki.makewiki(conf)
+    db = res['wiki']
+    images = res['images']
+    from wsgiref.simple_server import make_server, WSGIServer
+
+    from SocketServer import ThreadingMixIn, ForkingMixIn
+    class MyServer(ForkingMixIn, WSGIServer):
+        pass
+
+    iface, port = '0.0.0.0', 8080
+    print "serving on %s:%s" % (iface, port)
+    http = make_server(iface, port, web.Serve(db, res['images']), server_class=MyServer)
+    http.serve_forever()
+    
+
+    
+def html():
+    parser = optparse.OptionParser(usage="%prog --conf CONF ARTICLE [...]")
+    parser.add_option("-c", "--conf", help="config file")
+
+    options, args = parser.parse_args()
+    
+    if not args:
+        parser.error("missing ARTICLE argument")
+        
+    articles = [unicode(x, 'utf-8') for x in args]
+
+    conf = options.conf
+    if not options.conf:
+        parser.error("missing --conf argument")
+    
+    import StringIO
+    import tempfile
+    import os
+    import webbrowser
+    from mwlib import wiki, uparser, htmlwriter
+    
+    res = wiki.makewiki(conf)
+    db = res['wiki']
+    images = res['images']
+
+    for a in articles:
+        raw=db.getRawArticle(a)
+        if not raw:
+            continue
+
+        out=StringIO.StringIO()
+        out.write("""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset="utf-8"></meta>
+<link rel="stylesheet" href="pedia.css" />
+</head>
+<body>
+
+""")
+
+        a=uparser.parseString(x, raw=raw, wikidb=db)
+        w=htmlwriter.HTMLWriter(out, images)
+        w.write(a)
+
+        fd, htmlfile = tempfile.mkstemp(".html")
+        os.close(fd)
+        open(htmlfile, "wb").write(out.getvalue().encode('utf-8'))
+        webbrowser.open("file://"+htmlfile)
+
---
+++ packages/mwlib/mwlib-0.1.0.tar.gz/mwlib-0.1.0/setup.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2007, PediaPress GmbH
+# See README.txt for additional licensing information.
+
+import sys
+import os
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+
+install_requires=["simplejson>=1.7"]
+if sys.version_info[:2] < (2,5):
+    install_requires.append("wsgiref>=0.1.2")
+
+
+def read_long_description():
+    fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.txt")
+    return open(fn).read()
+
+setup(
+    name="mwlib",
+    version="0.1.0",
+    entry_points = dict(console_scripts=['mw-buildcdb = mwlib.apps:buildcdb',
+                                         'mw-zip = mwlib.apps:buildzip',
+                                         'mw-parse = mwlib.apps:parse',
+                                         'mw-show = mwlib.apps:show',
+                                         'mw-html = mwlib.apps:html',
+                                         'mw-serve = mwlib.apps:serve',
+                                         ]),
+    install_requires=install_requires,
+
+    packages=["mwlib", "mwlib.Plex", "mwlib.resources"],
+    include_package_data = True,
+    zip_safe = False,
+    url = "http://code.pediapress.com/",
+    description="mediawiki parser and utility library",
+    license="BSD License",
+    maintainer="pediapress.com",
+    maintainer_email="info@pediapress.com",
+    long_description = read_long_description()
+)
