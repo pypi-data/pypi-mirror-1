35e3776e8ea952e9bcec19b1ac246c323f1f4165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package gibe/0.10.12/gibe-0.10.12-py2.4.egg

---
+++ packages/gibe/gibe-0.10.12-py2.4.egg/gibe/release.py
@@ -0,0 +1,14 @@
+# Release information about gibe
+
+version = "0.10.12"
+
+description = "Blog using TurboGears with popular replacement components"
+long_description = """TurboGears-based web log supporting multiple web logs on a single instance, using Genshi, SQLAlchemy, and Routes, and a plugin architecture."""
+author = "Neil Blakey-Milner"
+email = "nbm@nxsy.org"
+copyright = "2006-2007, Neil Blakey-Milner"
+
+# if it's open source, you might want to specify these
+url = "http://gibe-blog.org/"
+download_url = "http://gibe-blog.org/download/"
+license = "BSD"
