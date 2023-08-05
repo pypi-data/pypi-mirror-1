b886b6410b68e7e66f95e8b870020550dba3f4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package Editra/0.2.15/Editra-0.2.15-py2.5.egg

---
+++ packages/Editra/Editra-0.2.15-py2.5.egg/Editra/src/__init__.py
@@ -0,0 +1,19 @@
+###############################################################################
+# Name: __init__.py                                                           #
+# Purpose: Import the required base modules needed for launching Editra into  #
+#          into the namespace.                                                #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2007 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
+"""Main package module
+@summary: Makes ed_glob, util, and Editra available in the package namespace
+
+"""
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id: __init__.py 49250 2007-10-20 02:40:49Z CJP $"
+__revision__ = "$Revision: 49250 $"
+
+import ed_glob
+import util
+import Editra
