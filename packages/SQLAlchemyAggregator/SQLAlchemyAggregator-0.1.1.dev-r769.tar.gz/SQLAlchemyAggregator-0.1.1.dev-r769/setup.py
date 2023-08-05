26c15dc66fd802e07e2d1324ca095fc86fcf556f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package SQLAlchemyAggregator/0.1.1.dev-r769/SQLAlchemyAggregator-0.1.1.dev-r769.tar.gz

---
+++ packages/SQLAlchemyAggregator/SQLAlchemyAggregator-0.1.1.dev-r769.tar.gz/SQLAlchemyAggregator-0.1.1.dev-r769/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name = "SQLAlchemyAggregator",
+    version = "0.1.1",
+    packages = find_packages(),
+    test_suite = "tests/simpletest",
+
+    install_requires = ['SQLAlchemy>=0.4dev'],
+
+    author = "Paul Colomiets",
+    author_email = "pc@gafol.net",
+    description = "SQLAlchemy's mapper extension which can"
+        "automatically track changes in mapped classes and"
+        "calculate aggregations based on them",
+    license = "MIT",
+    keywords = "sql sqlalchemy aggragation database",
+    url = "http://www.mr-pc.kiev.ua/en/projects/SQLAlchemyAggregator",
+)
