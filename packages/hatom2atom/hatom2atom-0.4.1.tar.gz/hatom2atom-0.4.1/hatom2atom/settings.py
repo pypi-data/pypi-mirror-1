a5d6854444334eacbb3856d604c5fdda1a160ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:49 +0000

    Add package hatom2atom/0.4.1/hatom2atom-0.4.1.tar.gz

---
+++ packages/hatom2atom/hatom2atom-0.4.1.tar.gz/hatom2atom-0.4.1/hatom2atom/settings.py
@@ -0,0 +1,60 @@
+"""settings - settings for hatom2atom
+
+this file is part of the hatom2atom package.
+
+created and maintained by luke arno <luke.arno@gmail.com>
+
+copyright (c) 2006  Luke Arno  <luke.arno@gmail.com>
+
+this program is free software; you can redistribute it and/or
+modify it under the terms of the gnu general public license
+as published by the free software foundation; either version 2
+of the license, or (at your option) any later version.
+
+this program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with this program; if not, write to:
+
+the free software foundation, inc., 
+51 franklin street, fifth floor, 
+boston, ma  02110-1301, usa.
+
+luke arno can be found at http://lukearno.com/
+"""
+
+import os
+import logging
+import urllib2
+
+from pkg_resources import resource_filename, Requirement
+from flup.server.scgi import WSGIServer
+
+
+host = 'localhost'
+port = 4000
+log_level = logging.INFO
+
+stylesheet = 'http://rbach.priv.at/hAtom2Atom/hAtom2Atom.xsl'
+
+kid_template = 'proxypage.kid'
+kid_dir = 'kid'
+app_name = 'hatom2atom'
+app_resource = Requirement.parse(app_name)
+kid_dir = resource_filename(app_resource, kid_dir)
+kid_template = os.sep.join([kid_dir, kid_template])
+
+default_ctype = 'application/atom+xml'
+
+tidy_options = dict(output_xhtml=1, numeric_entities=1, doctype="omit")
+
+url_opener = urllib2.build_opener()
+url_opener.addheaders = [('User-Agent',
+                          'hAtom2Atom proxy '
+                            '(http://lukearno.com/projects/hatom2atom/)'), 
+                         ('Accept',
+                          'application/xhtml+xml,application/xml,'
+                            'text/xml,text/html;q=0.9,*/*;q=0.8')]
---
+++ packages/hatom2atom/hatom2atom-0.4.1.tar.gz/hatom2atom-0.4.1/setup.py
@@ -0,0 +1,56 @@
+"""setup - setuptools based setup for hatom2atom
+
+Created and maintained by Luke Arno <luke.arno@gmail.com>
+
+Copyright (C) 2006 Luke Arno <luke.arno@gmail.com> 
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to:
+
+The Free Software Foundation, Inc., 
+51 Franklin Street, Fifth Floor, 
+Boston, MA  02110-1301, USA.
+
+Luke Arno can be found at http://lukearno.com/
+"""
+
+from setuptools import setup, find_packages
+from glob import glob
+
+desc = 'WSGI proxy for transforming hAtom to Atom via hAtom2Atom.xsl.'
+
+setup(name='hatom2atom',
+      version='0.4.1',
+      description=desc,
+      long_description=desc,
+      author='Luke Arno',
+      author_email='luke.arno@gmail.com',
+      url='http://lukearno.com/projects/hatom2atom',
+      license="GPL2",
+      install_requires="flup\nkid >= 0.8",
+      packages = find_packages(),
+      data_files=[('kid', glob('kid/*.kid'))],
+      entry_points={'console_scripts':['h2aserve = hatom2atom.h2aproxy:run']},
+      keywords="xml html micorformats atom hatom wsgi proxy",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Text Processing :: Markup :: XML',
+          'Topic :: Text Processing :: Markup :: HTML',])
+
