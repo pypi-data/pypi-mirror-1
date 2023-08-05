89d7b55cee7135c751c07a29308e40f9aa9f097d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:24 +0000

    Add package plone.app.blob/1.0b2/plone.app.blob-1.0b2-py2.4.egg

---
+++ packages/plone.app.blob/plone.app.blob-1.0b2-py2.4.egg/plone/app/blob/db/custom_zodb.py
@@ -0,0 +1,7 @@
+import ZODB; ZODB   # make pyflakes happy...
+from ZODB.DemoStorage import DemoStorage
+from ZODB.blob import BlobStorage
+from tempfile import mkdtemp
+
+print 'Setting up blob-aware ZODB storage ...'
+Storage = BlobStorage(mkdtemp(), DemoStorage(quota=(1<<20)))
