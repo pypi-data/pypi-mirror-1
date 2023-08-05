4f48bf2b3123d2151e73611bf95de0441aac55bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:28 +0000

    Add package dm.zdoc/1.1/dm.zdoc-1.1.tar.gz

---
+++ packages/dm.zdoc/dm.zdoc-1.1.tar.gz/dm.zdoc-1.1/dm/zdoc/__init__.py
@@ -0,0 +1,24 @@
+import App.FindHomes # activate Zope's import magic
+
+# rebind 'pydoc.locate' to prevent reloading -- as this deactivates Zope's import magic
+from pydoc import *
+import pydoc
+
+org_locate = locate
+def locate(path, *args, **kw):
+ if args: args = (False,) + args[1:]
+ if 'forceload' in kw: kw['forceload'] = False
+ return org_locate(path, *args, **kw)
+
+pydoc.locate = locate
+
+# work around a bug in 'zope.interface'
+import inspect
+from dm.reuse import rebindFunction
+def _patched_dir(obj):
+  return [d for d in dir(obj) if hasattr(obj, d)]
+inspect.getmembers = rebindFunction(inspect.getmembers, dir=_patched_dir)
+inspect.classify_class_attrs = rebindFunction(inspect.classify_class_attrs, dir=_patched_dir)
+
+if __name__ == '__main__': cli()
+                              
