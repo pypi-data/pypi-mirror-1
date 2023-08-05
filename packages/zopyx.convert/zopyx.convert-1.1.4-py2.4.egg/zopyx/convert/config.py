0065f3aa96c753100830ee06eb218be4d83e4343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package zopyx.convert/1.1.4/zopyx.convert-1.1.4-py2.4.egg

---
+++ packages/zopyx.convert/zopyx.convert-1.1.4-py2.4.egg/zopyx/convert/config.py
@@ -0,0 +1,8 @@
+##########################################################################
+# zopyx.convert - XSL-FO related functionalities
+#
+# (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+java = 'java'
+supported_formats = ('pdf', 'pdf2', 'rtf', 'wml', 'docx', 'odt')
