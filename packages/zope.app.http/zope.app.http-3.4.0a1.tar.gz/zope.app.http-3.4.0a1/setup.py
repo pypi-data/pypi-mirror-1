59f4e68c662c9bc9d21fc6d310e48a429e3b74a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package zope.app.http/3.4.0a1/zope.app.http-3.4.0a1.tar.gz

---
+++ packages/zope.app.http/zope.app.http-3.4.0a1.tar.gz/zope.app.http-3.4.0a1/setup.py
@@ -0,0 +1,44 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.http package
+
+$Id: setup.py 74419 2007-04-22 10:35:33Z ctheune $
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.http',
+      version = '3.4.0a1',
+      url='http://svn.zope.org/zope.app.http',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+	  packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require=dict(test=['zope.app.testing']),
+      install_requires=['setuptools',
+                        'zope.interface',
+                        'zope.app.zapi',
+                        'zope.publisher',
+                        'zope.app.container',
+                        'zope.app.publication',
+                        'zope.filerepresentation',
+                        ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
