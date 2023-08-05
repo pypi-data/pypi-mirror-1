54ba4ec26e25a5bb84dceea220bece446653383b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package collective.allowsearch/0.1/collective.allowsearch-0.1.tar.gz

---
+++ packages/collective.allowsearch/collective.allowsearch-0.1.tar.gz/collective.allowsearch-0.1/collective/allowsearch/__init__.py
@@ -0,0 +1,29 @@
+import logging
+
+from zope import component
+
+from Products.CMFPlone.CatalogTool import registerIndexableAttribute
+from collective.allowsearch.interfaces import IAllowedRolesAndUsers
+
+def allowedRolesAndUsers(obj, portal, **kwargs):
+    """
+    We reregister the 'allowedRolesAndUsers' attribute here to
+    have a chance to extend the list the original returns. We
+    have added a utility which provides the default (original)
+    behavior, and use it here. Then we try to adapt the object
+    to be indexed to IAllowedRolesAndUsers and try to use that.
+    If we cannot adapt, then we use the original.
+    """
+    adapted = IAllowedRolesAndUsers(obj, None)
+    if adapted is None:
+        allowed = component.getUtility(IAllowedRolesAndUsers,u"collective.allowsearch.default")(obj,portal,**kwargs)
+        return allowed
+    
+    logging.debug("collective.allowsearch: found adapter for %s" % obj )
+    return adapted(obj,portal,**kwargs)
+
+registerIndexableAttribute( 'allowedRolesAndUsers', allowedRolesAndUsers)
+
+def initialize(context):
+    """Intializer called when used as a Zope 2 product."""
+    pass
---
+++ packages/collective.allowsearch/collective.allowsearch-0.1.tar.gz/collective.allowsearch-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='collective.allowsearch',
+      version=version,
+      description="Provides an interface and a default adapter to allow users to search the catalog independent of the View permission.",
+      long_description=file("README.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Stefan Eletzhofer',
+      author_email='stefan.eletzhofer@inquant.de',
+      url='https://svn.plone.org/svn/collective/collective.allowsearch/branches/release-0-1',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
