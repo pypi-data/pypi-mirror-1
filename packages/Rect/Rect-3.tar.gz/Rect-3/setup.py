3382b5392a9faa3820b0cbc8098b395a9ed7a7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package Rect/3/Rect-3.tar.gz

---
+++ packages/Rect/Rect-3.tar.gz/Rect-3/setup.py
@@ -0,0 +1,29 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages, Extension
+
+packages = find_packages()
+
+setup(
+    name = "Rect",
+    version = "3",
+    url="http://code.google.com/p/corsair-redux/wiki/Rect",
+    packages = packages,
+    author='Simon Wittber',
+    author_email='simonwittber@gmail.com',
+    license='MIT',
+    platforms=['Any'],
+    description="A class for handling rectangle regions.",
+    long_description="""The Rect class is similar to the pygame.Rect class, except it works with
+float precision in a left handed coordinate system.
+
+The rect package also provides a spatial index and a rect packing function.
+
+Changes:
+* 20070723: Quadtree bug fixes
+* 20070718: Added rect packing algorithm. (rect.packer)
+* 20070717: Added QuadTree spatial index. (rect.quadtree)
+
+"""
+)
+
