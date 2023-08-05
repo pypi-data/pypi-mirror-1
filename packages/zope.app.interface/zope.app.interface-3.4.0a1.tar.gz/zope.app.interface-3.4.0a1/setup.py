f70718b9862c31fd69332103dc8dfcc5cc2532db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package zope.app.interface/3.4.0a1/zope.app.interface-3.4.0a1.tar.gz

---
+++ packages/zope.app.interface/zope.app.interface-3.4.0a1.tar.gz/zope.app.interface-3.4.0a1/setup.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.interface package
+
+$Id: setup.py 74425 2007-04-22 10:36:59Z ctheune $
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.interface',
+      version = '3.4.0a1',
+      url='http://svn.zope.org/zope.app.interface',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description='The Security framework provides a generic mechanism '
+                       'to implement security policies on Python objects.',
+      
+	  packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+
+      namespace_packages=['zope', 'zope.app'],
+      extras_require=dict(test=['zope.app.testing',
+                                'zope.app.content']),
+      install_requires=['setuptools',
+                        'ZODB3',
+                        'zodbcode',
+                        'zope.security',
+                       ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
