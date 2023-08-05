81256d144a608c83b0d4c5ffffe2c44eed3de7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:57 +0000

    Add package plone.i18n/1.0b1/plone.i18n-1.0b1.tar.gz

---
+++ packages/plone.i18n/plone.i18n-1.0b1.tar.gz/plone.i18n-1.0b1/plone/i18n/locales/interfaces.py
@@ -0,0 +1,49 @@
+from zope.interface import Interface
+from zope.i18n.interfaces import ILanguageAvailability as \
+    IBaseLanguageAvailability
+
+class ICountryAvailability(Interface):
+    """A list of available coutries."""
+
+    def getAvailableCountries():
+        """Return a sequence of country tags for available countries.
+        """
+
+    def getCountries():
+        """Return a sequence of Country objects for available countries.
+        """
+
+    def getCountryListing():
+        """Return a sequence of country code and country name tuples.
+        """
+
+class ILanguageAvailability(IBaseLanguageAvailability):
+    """A list of available languages."""
+
+    def getLanguages(combined=False):
+        """Return a sequence of Language objects for available languages.
+        """
+
+    def getLanguageListing(combined=False):
+        """Return a sequence of language code and language name tuples.
+        """
+
+class IContentLanguageAvailability(ILanguageAvailability):
+    """A list of available content languages."""
+
+class IMetadataLanguageAvailability(ILanguageAvailability):
+    """A list of available metadata languages."""
+
+class IModifiableLanguageAvailability(ILanguageAvailability):
+    """A modifiable list of available languages."""
+
+    def setAvailableLanguages(languages, combined=False):
+        """Set a list of available language tags.
+        """
+
+class IModifiableCountryAvailability(ICountryAvailability):
+    """A modifiable list of available countries."""
+
+    def setAvailableCountries(countries):
+        """Set a list of available country tags.
+        """
