831d5fd77f9d56d46f675e19b1ed97b6947f73ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package plone.app.controlpanel/1.0b5/plone.app.controlpanel-1.0b5.tar.gz

---
+++ packages/plone.app.controlpanel/plone.app.controlpanel-1.0b5.tar.gz/plone.app.controlpanel-1.0b5/plone/app/controlpanel/tests/test_doctests.py
@@ -0,0 +1,32 @@
+from zope.testing import doctest
+from unittest import TestSuite
+
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+from Products.PloneTestCase.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.PloneTestCase import setupPloneSite
+
+from plone.app.controlpanel.tests.cptc import ControlPanelTestCase
+
+setupPloneSite()
+
+OPTIONFLAGS = (doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
+def test_suite():
+    tests = ['calendar.txt',
+             'filter.txt',
+             'mail.txt',
+             'maintenance.txt',
+             'search.txt',
+             'site.txt',
+             'skins.txt',
+             'markup.txt',
+             'types.txt',
+             ]
+    suite = TestSuite()
+    for test in tests:
+        suite.addTest(FunctionalDocFileSuite(test,
+            optionflags=OPTIONFLAGS,
+            package="plone.app.controlpanel.tests",
+            test_class=ControlPanelTestCase))
+    return suite
