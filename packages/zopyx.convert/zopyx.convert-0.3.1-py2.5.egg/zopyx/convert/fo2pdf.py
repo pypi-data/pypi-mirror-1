3810720ef5325c661ef51155254cdee8eb1edebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package zopyx.convert/0.3.1/zopyx.convert-0.3.1-py2.5.egg

---
+++ packages/zopyx.convert/zopyx.convert-0.3.1-py2.5.egg/zopyx/convert/fo2pdf.py
@@ -0,0 +1,41 @@
+##########################################################################
+# zopyx.convert - XSL-FO related functionalities
+#
+# (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+import os
+import sys
+
+from config import java
+from util import newTempfile, runcmd
+from logger import LOG
+
+xinc_home = os.environ.get('XINC_HOME')
+
+def _check_xinc():
+    if not xinc_home:
+        LOG.error('$XINC_HOME not set')
+        return False
+
+    if not os.path.exists(xinc_home):
+        LOG.error('$XINC_HOME=%s does not exist' % xinc_home)
+        return False
+
+    return True        
+
+xinc_available = _check_xinc()
+
+def fo2pdf(fo_filename, output_filename=None):
+    """ Convert a FO file to PDF using XINC """
+
+    if not output_filename:
+        output_filename = newTempfile(suffix='.pdf')
+
+    cmd = '"%s/bin/unix/xinc" -fo "%s" -pdf "%s"' % (xinc_home, fo_filename, output_filename)
+    status, output = runcmd(cmd)
+    if status != 0:
+        raise RuntimeError('Error executing: %s' % cmd)
+
+    return output_filename
+
