ea152dd67746fb0e68100d3728db73a6aa25cb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:36 +0000

    Add package PasteDeploy/0.3/PasteDeploy-0.3-py2.3.egg

---
+++ packages/PasteDeploy/PasteDeploy-0.3-py2.3.egg/paste/deploy/paster_templates.py
@@ -0,0 +1,10 @@
+from paste.script.templates import Template
+
+class PasteDeploy(Template):
+
+    _template_dir = 'paster_templates/paste_deploy'
+    summary = "A web application deployed through paste.deploy"
+    
+    egg_plugins = ['PasteDeploy']
+    
+    required_templates = ['PasteScript#basic_package']
