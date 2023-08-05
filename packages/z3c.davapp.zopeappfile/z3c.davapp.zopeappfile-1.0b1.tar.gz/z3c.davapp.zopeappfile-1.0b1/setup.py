61fc6491cf68fe90fdf1dc6b1e03aee1d5261b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package z3c.davapp.zopeappfile/1.0b1/z3c.davapp.zopeappfile-1.0b1.tar.gz

---
+++ packages/z3c.davapp.zopeappfile/z3c.davapp.zopeappfile-1.0b1.tar.gz/z3c.davapp.zopeappfile-1.0b1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(name = "z3c.davapp.zopeappfile",
+      version = "1.0b1",
+      author = "Michael Kerrin",
+      author_email = "michael.kerrin@openapp.ie",
+      url = "http://launchpad.net/z3c.dav",
+      description = open("README.txt").read(),
+      long_description = (
+          open("src/z3c/davapp/zopeappfile/properties.txt").read() +
+          "\n\n" +
+          open("CHANGES.txt").read()),
+      license = "ZPL",
+      classifiers = ["Environment :: Web Environment",
+                     "Intended Audience :: Developers",
+                     "License :: OSI Approved :: Zope Public License",
+                     "Programming Language :: Python",
+                     "Framework :: Zope3",
+                     ],
+
+      packages = find_packages("src"),
+      package_dir = {"": "src"},
+      namespace_packages = ["z3c", "z3c.davapp"],
+      install_requires = ["setuptools",
+                          "z3c.dav",
+                          "zope.app.file",
+                          ],
+
+      extras_require = dict(test = ["cElementTree"]),
+
+      include_package_data = True,
+      zip_safe = False)
