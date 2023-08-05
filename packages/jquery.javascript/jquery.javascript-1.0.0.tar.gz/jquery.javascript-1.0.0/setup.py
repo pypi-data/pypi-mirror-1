3320561eba11cb42336bfb4797c1b023aca458b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package jquery.javascript/1.0.0/jquery.javascript-1.0.0.tar.gz

---
+++ packages/jquery.javascript/jquery.javascript-1.0.0.tar.gz/jquery.javascript-1.0.0/setup.py
@@ -0,0 +1,59 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 81253 2007-10-31 00:52:33Z srichter $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='jquery.javascript',
+      version='1.0.0',
+      author = "Roger Ineichen and the Zope Community",
+      author_email = "zope3-dev@zope.org",
+      description = "JQuery integration into Zope 3 via Viewlets",
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 javascript jquery",
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/jquery.javascript',
+      license = "ZPL 2.1",
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      namespace_packages = ['jquery'],
+      extras_require = dict(
+          test = ['zope.testing'],
+          ),
+      install_requires = [
+          'setuptools',
+          'zope.viewlet',
+          ],
+      include_package_data = True,
+      zip_safe = False,
+      )
