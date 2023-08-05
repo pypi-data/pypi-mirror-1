4c8410f68fb0a1db5208b831342345a3590a0d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:36 +0000

    Add package zope.xmlpickle/3.4.0/zope.xmlpickle-3.4.0.tar.gz

---
+++ packages/zope.xmlpickle/zope.xmlpickle-3.4.0.tar.gz/zope.xmlpickle-3.4.0/setup.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.xmlpickle package
+
+$Id: setup.py 81503 2007-11-05 00:09:37Z srichter $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.xmlpickle',
+      version = '3.4.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='XML-based Pickling',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 pickle xml",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP'],
+      url='http://cheeseshop.python.org/pypi/zope.xmlpickle',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=[
+          'setuptools',
+          ],
+      include_package_data = True,
+      zip_safe = False,
+      )
