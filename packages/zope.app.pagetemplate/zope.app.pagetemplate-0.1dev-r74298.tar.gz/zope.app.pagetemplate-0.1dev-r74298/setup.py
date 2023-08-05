c0633c55c5c6001407b39df45b48aa69c85c6412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package zope.app.pagetemplate/0.1dev-r74298/zope.app.pagetemplate-0.1dev-r74298.tar.gz

---
+++ packages/zope.app.pagetemplate/zope.app.pagetemplate-0.1dev-r74298.tar.gz/zope.app.pagetemplate-0.1dev-r74298/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(name='zope.app.pagetemplate',
+      version='0.1',
+      url='http://svn.zope.org/zope.app.pagetemplate',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+
+      namespace_packages=['zope', 'zope.app'],
+      include_package_data=True,
+      install_requires=[
+          'setuptools',
+          'zope.app.interpreter',
+          'zope.component [hook]',
+          'zope.configuration',
+          'zope.dublincore',
+          'zope.i18n',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.pagetemplate',
+          'zope.publisher',
+          'zope.schema',
+          'zope.security [untrustedpython]',
+          'zope.size',
+          'zope.tales',
+          'zope.traversing',
+          ],
+      extras_require={
+          "test": ['zope.app.testing',
+                   'zope.app.securitypolicy',
+                   'zope.app.zcmlfiles'],
+          },
+      zip_safe=False,
+      )
