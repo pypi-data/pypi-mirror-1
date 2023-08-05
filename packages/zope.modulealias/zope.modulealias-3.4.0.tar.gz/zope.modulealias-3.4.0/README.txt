d83bba06ebed9c4198d5d4a2b00d30ed65c60ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package zope.modulealias/3.4.0/zope.modulealias-3.4.0.tar.gz

---
+++ packages/zope.modulealias/zope.modulealias-3.4.0.tar.gz/zope.modulealias-3.4.0/README.txt
@@ -0,0 +1,6 @@
+===============
+Module Aliasing
+===============
+
+This package enables the developer to make one module available under a
+different path.
---
+++ packages/zope.modulealias/zope.modulealias-3.4.0.tar.gz/zope.modulealias-3.4.0/setup.py
@@ -0,0 +1,59 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.modulealias package
+
+$Id: setup.py 80524 2007-10-03 02:46:31Z srichter $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name="zope.modulealias",
+      version = '3.4.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Zope modulealias',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 module alias",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.modulealias',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.testing',
+                ]),
+    install_requires = ['setuptools',
+                        'zope.interface',
+                        'zope.configuration'],
+    include_package_data=True,
+    zip_safe = False
+    )
+
