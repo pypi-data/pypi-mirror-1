4ef33e6da73ff449c854ab113656ee623f8bdf47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:25 +0000

    Add package RDFCalendar/0.2.6/RDFCalendar-0.2.6.tar.gz

---
+++ packages/RDFCalendar/RDFCalendar-0.2.6.tar.gz/RDFCalendar-0.2.6/RDFCalendar/FormatWriters.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+
+"""
+Format writer classes.
+
+Copyright (C) 2005, 2006 Paul Boddie <paul@boddie.org.uk>
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from RDFCalendar.Format import *
+import RDFFormats.FormatWriters
+
+class iCalendarWriter:
+
+    "An iCalendar writer class."
+
+    def __init__(self, f, compact=0, use_quoted_printable=0, fold_incorrectly=0, do_not_fold=0):
+
+        """
+        Initialise the writer with a stream 'f', configuring the writer with the
+        following optional parameters:
+
+          * compact              - use a compact representation (popular with
+                                   many applications, but not enabled by
+                                   default)
+          * use_quoted_printable - use the quoted-printable representation for
+                                   text (not enabled by default)
+          * fold_incorrectly     - produce incorrectly folded text to satisfy
+                                   non-compliant applications (not enabled by
+                                   default)
+          * do_not_fold          - do not fold text but place long text on a
+                                   single line (not enabled by default)
+        """
+
+        self.f = f
+        self.compact = compact
+        self.use_quoted_printable = use_quoted_printable
+        self.fold_incorrectly = fold_incorrectly
+        self.do_not_fold = do_not_fold
+        self.line_length = 76
+
+    def start_element(self, label):
+        self.f.write("BEGIN:%s\r\n" % label)
+
+    def end_element(self, label):
+        self.f.write("END:%s\r\n" % label)
+
+    def write_attribute(self, name, modifiers=None, value=None, subvalues=None, multivalues=None):
+        if self.compact:
+            self.f.write(name)
+        else:
+            self.f.write(name + "\r\n")
+
+        # Where quoted printable is used, sneak in a modifier.
+        # NOTE: Only supported for single value attributes.
+        # NOTE: Assume that the value should be ISO 8859-1.
+        # NOTE: Also assume that the quopri stuff uses only LF and not CRLF.
+
+        modifiers = modifiers or []
+        if value is not None:
+            if self.use_quoted_printable:
+                try:
+                    value = value.encode("US-ASCII").replace("\n", "=0A")
+                except UnicodeError:
+                    modifiers.append(("ENCODING", "QUOTED-PRINTABLE"))
+
+                    # NOTE: Nasty recoding of newlines to avoid confusion with
+                    # NOTE: those introduced by encodestring.
+
+                    value = value.replace("\n", "\\n")
+                    value = quopri.encodestring(value.encode("iso-8859-1"))
+
+                    # NOTE: Explicit conversions.
+
+                    value = value.replace("\\n", "=0A")
+                    value = value.replace("\n", "\r\n")
+            else:
+                # NOTE: Introducing newline conversions.
+
+                value = value.replace("\n", "\\n")
+
+        if modifiers is not None:
+            for modifier_name, modifier_value in modifiers:
+                if self.compact:
+                    self.f.write(";" + modifier_name + "=" + modifier_value)
+                else:
+                    self.f.write(" ;" + modifier_name + "=" + modifier_value + "\r\n")
+
+        if value is not None:
+            if self.compact:
+                self.f.write(":")
+            else:
+                self.f.write(" :")
+            self.f.write(self.get_folded_lines(value) + "\r\n")
+
+        elif subvalues is not None:
+            assignments = [(name + "=" + value) for (name, value) in subvalues]
+            if self.compact:
+                self.f.write(":")
+            else:
+                self.f.write(" :")
+            self.f.write(";".join(assignments) + "\r\n")
+
+        elif multivalues is not None:
+            if self.compact:
+                self.f.write(":")
+            else:
+                self.f.write(" :")
+            l = []
+            for multivalue in multivalues:
+                if multivalue.has_key("END"):
+                    l.append("%s/%s" % (multivalue["START"], multivalue["END"]))
+                elif multivalue.has_key("DURATION"):
+                    l.append("%s/%s" % (multivalue["START"], multivalue["DURATION"]))
+                else:
+                    l.append(multivalue["DETAILS"])
+            self.f.write(",".join(l) + "\r\n")
+
+    def close(self):
+        self.f.close()
+
+    def get_folded_lines(self, value):
+        if self.do_not_fold:
+            return value
+        if not self.fold_incorrectly:
+            value = value.replace("\r\n", "\r\n ")
+        new_lines = []
+        for line in value.split("\r\n"):
+            self.add_folded_lines(line, new_lines)
+        return "\r\n".join(new_lines)
+
+    def add_folded_lines(self, line, lines):
+        lines.append(line[:self.line_length])
+        for i in range(self.line_length, len(line), self.line_length):
+            limit = min(i + self.line_length, len(line))
+            new_line = line[i:limit]
+            if not self.fold_incorrectly:
+                lines.append(" " + new_line)
+            else:
+                lines.append(new_line)
+
+class DOMWriter(RDFFormats.FormatWriters.DOMWriter):
+
+    "A DOM document writer."
+
+    def get_item_type_name(self, label):
+        return element_names[label]
+
+    def get_property_name(self, attribute_name):
+        return attribute_name.lower()
+
+    def get_property_label(self, property_name):
+        return get_property_label(property_name)
+
+# vim: tabstop=4 expandtab shiftwidth=4
---
+++ packages/RDFCalendar/RDFCalendar-0.2.6.tar.gz/RDFCalendar-0.2.6/tools/iCalendar.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+"""
+Import or export items from data stores.
+
+Copyright (C) 2005, 2006, 2007 Paul Boddie <paul@boddie.org.uk>
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import RDFCalendar.Store
+import RDFCalendar.Parsers
+import RDFCalendar.Writers
+import RDFCalendar.FormatWriters
+
+# Main program.
+
+if __name__ == "__main__":
+    import cmdsyntax
+    import sys
+    import codecs
+    import libxml2dom
+
+    # Find the documents.
+
+    syntax = cmdsyntax.Syntax("""
+        --store=STORE_NAME
+        --store-type=STORE_TYPE
+        [--module=MODULE]
+        [(--contexts | --context=NAMESPACE)]
+        [--name=NAME]
+        [--uriref=URIREF]
+        [--input-ics=INPUT_FILE]
+        [--input-encoding=ENCODING]
+        [--input-xml=INPUT_FILE]
+        [--output-rdf=OUTPUT_FILE]
+        [--output-ics=OUTPUT_FILE]
+        [--output-xml=OUTPUT_FILE]
+        [--output-xml-value-attr]
+        [--compact]
+        [--quopri]
+        [--badfold]
+        [--cr]
+        [--remove]
+        [--remove-context]
+        """)
+
+    syntax_matches = syntax.get_args(sys.argv[1:])
+
+    try:
+        args = syntax_matches[0]
+    except IndexError:
+        print "Syntax:"
+        print syntax.syntax
+        print "Terms:"
+        print
+        print "  STORE_NAME         The store location or identifier."
+        print "  STORE_TYPE         The store type: sqltriples"
+        print "  MODULE             The sqltriples module to be used: PgSQL or pysqlite2"
+        print "  NAMESPACE          A context namespace."
+        print "  NAME               A name property to be assigned to the resource."
+        print "  URIREF             The URI representing the resource."
+        print "  INPUT_FILE         An input file in one of the supported formats."
+        print "  OUTPUT_FILE        An output file in one of the supported formats."
+        print "  ENCODING           A character encoding such as utf-8 or iso-8859-1."
+        print
+        print "Examples:"
+        print
+        print "  python tools/iCalendar.py --store=testdb --store-type=sqltriples --module=PgSQL \\"
+        print "    --uriref=MyCalendar --input-ics=calendar.ics"
+        sys.exit(1)
+
+    # Open a store.
+
+    store = RDFCalendar.Store.open(args["store"], args["store-type"], args.get("context"), database_module_name=args.get("module"))
+
+    # Perform the requested operations.
+
+    try:
+        # Handle different store contexts, if appropriate.
+
+        if args.has_key("contexts"):
+            print "Contexts:"
+            for context in store.contexts():
+                print context
+            sys.exit(0)
+
+        # Parse an iCalendar input file.
+
+        if args.has_key("input-ics") and args.has_key("uriref"):
+            if args.has_key("encoding"):
+                encoding = args["encoding"]
+            else:
+                encoding = "utf-8"
+            f = codecs.open(args["input-ics"], encoding=encoding)
+            print "Added", RDFCalendar.Parsers.parse(
+                f, store, name=args.get("name"), uriref=store.URIRef(args["uriref"]),
+                non_standard_newline=args.has_key("cr")), "as", args["uriref"]
+
+        # Parse an XML input file.
+
+        if args.has_key("input-xml") and args.has_key("uriref"):
+            doc = libxml2dom.parseFile(args["input-xml"])
+            print "Added", RDFCalendar.Parsers.parse_document(doc, store, uriref=store.URIRef(args["uriref"])), "as", args["uriref"]
+
+        # Save as RDF if requested.
+
+        if args.has_key("output-rdf"):
+            print "RDF/XML output not supported by this store type."
+
+        # Save as iCalendar/vCard if requested.
+
+        if args.has_key("output-ics") and args.has_key("uriref"):
+            f = codecs.open(args["output-ics"], "wb", encoding="utf-8")
+            RDFCalendar.Writers.write_to_stream(
+                f, store, main_node=store.URIRef(args["uriref"]),
+                compact=args.has_key("compact"),
+                use_quoted_printable=args.has_key("quopri"),
+                fold_incorrectly=args.has_key("badfold")
+                )
+            f.close()
+
+        # Save as XML if requested.
+
+        if args.has_key("output-xml") and args.has_key("uriref"):
+            if args.has_key("output-xml-value-attr"):
+                doc = RDFCalendar.Writers.get_document(store, store.URIRef(args["uriref"]), value_as_attribute=1)
+            else:
+                doc = RDFCalendar.Writers.get_document(store, store.URIRef(args["uriref"]))
+            f = open(args["output-xml"], "wb")
+            doc.toStream(f, encoding="utf-8")
+            f.close()
+
+        # Remove from the information store if requested.
+
+        if args.has_key("remove"):
+            store.remove_node(store.URIRef(args["uriref"]), deep=1)
+
+        # Remove entire contexts if requested.
+
+        if args.has_key("remove-context"):
+            store.remove_context(args["context"])
+
+        store.commit()
+
+    finally:
+        store.close()
+
+# vim: tabstop=4 expandtab shiftwidth=4
