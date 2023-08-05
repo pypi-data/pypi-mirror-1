76491609ec3bb923fb4bbfaf2202ae410d7aec50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package pyorbited/0.1.1/pyorbited-0.1.1.zip

---
+++ packages/pyorbited/pyorbited-0.1.1.zip/pyorbited-0.1.1/pyorbited/distribute.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+class ClientPool(object):
+
+    def __init__(self, servers):
+        pass
+        
+        
+        
+    def event(self, recipients, data):
+        # Sort recipients into servers based on the hash key
+        # Send each set of events to the appropriate server
+        # Combined the results
+        
+        pass
+        
+    
