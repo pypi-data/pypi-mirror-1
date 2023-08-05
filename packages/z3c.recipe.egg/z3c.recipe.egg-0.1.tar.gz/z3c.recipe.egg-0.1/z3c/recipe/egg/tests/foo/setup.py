087bc4d8894e70028074aeca491bd326be7ee640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package z3c.recipe.egg/0.1/z3c.recipe.egg-0.1.tar.gz

---
+++ packages/z3c.recipe.egg/z3c.recipe.egg-0.1.tar.gz/z3c.recipe.egg-0.1/z3c/recipe/egg/tests/foo/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='foo',
+      author='foo',
+      author_email='foo@foo.com',
+      url='http://nohost/foo',
+      packages=find_packages())
