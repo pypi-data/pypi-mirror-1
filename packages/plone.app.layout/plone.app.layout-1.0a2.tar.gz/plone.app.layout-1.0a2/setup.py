f932e628d7418fd37691b7b099d995400d64db8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:19 +0000

    Add package plone.app.layout/1.0a2/plone.app.layout-1.0a2.tar.gz

---
+++ packages/plone.app.layout/plone.app.layout-1.0a2.tar.gz/plone.app.layout-1.0a2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0a2'
+
+setup(name='plone.app.layout',
+      version=version,
+      description="Layout mechanisms for Plone",
+      long_description="""\
+plone.app.layout contains various visual components for Plone, such as 
+viewlets and general views.
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='plone layout viewlet',
+      author='Martin Aspeli',
+      author_email='optilude@gmx.net',
+      url='http://svn.plone.org/svn/plone/plone.app.layout',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
