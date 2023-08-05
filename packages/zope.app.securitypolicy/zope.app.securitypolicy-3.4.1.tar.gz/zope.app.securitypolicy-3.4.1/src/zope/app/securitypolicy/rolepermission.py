67f1ef8a5e743a4d56628da92ba0a2e9dac1ee8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package zope.app.securitypolicy/3.4.1/zope.app.securitypolicy-3.4.1.tar.gz

---
+++ packages/zope.app.securitypolicy/zope.app.securitypolicy-3.4.1.tar.gz/zope.app.securitypolicy-3.4.1/src/zope/app/securitypolicy/rolepermission.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Permission to Roles Manager (Adapter)
+
+$Id: rolepermission.py 80149 2007-09-26 22:00:18Z rogerineichen $
+"""
+
+import zope.deferredimport
+
+zope.deferredimport.deprecated(
+    "It has moved to zope.securitypolicy.rolepermission  This reference will be "
+    "removed somedays",
+    AnnotationRolePermissionManager = 'zope.securitypolicy.grantinfo:AnnotationRolePermissionManager',
+    RolePermissionManager = 'zope.securitypolicy.grantinfo:RolePermissionManager',
+    rolePermissionManager = 'zope.securitypolicy.grantinfo:rolePermissionManager',
+    )
