3e6c439d8c054230ea27ed52afaed90b0ed3373b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package antigravity/0.1/antigravity-0.1.zip

---
+++ packages/antigravity/antigravity-0.1.zip/antigravity-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='antigravity',
+    version='0.1',
+    description='A really simple module that allow everyone to do "import antigravity"',
+    author='Fabien Schwob',
+    author_email='antigravity@x-phuture.com',
+    url='http://fabien.schwob.org/antigravity/',
+    packages=['antigravity'],
+)
