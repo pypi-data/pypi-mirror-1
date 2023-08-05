c49c1ba54f999ca06fac5c463b7a2a92a8e2769c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:17 +0000

    Add package dbstore/0.3/dbstore-0.3.tar.gz

---
+++ packages/dbstore/dbstore-0.3.tar.gz/dbstore-0.3/dbstore/__init__.py
@@ -0,0 +1,27 @@
+"""__init__ - dbstore package initialization module.
+
+This module is part of the dbstore package.
+
+Copyright (C) 2006 Luke Arno - http://lukearno.com/
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to 
+the Free Software Foundation, Inc., 51 Franklin Street, 
+Fifth Floor, Boston, MA  02110-1301  USA
+
+Luke Arno can be found at http://lukearno.com/
+
+"""
+
+
+__all__ = ['storage', 'mysqlstore', 'psycopgstore']
