a7bff6260326a818d584391d6bcf14a0a89c706c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:14 +0000

    Add package FibraNet/7/FibraNet-7.tar.gz

---
+++ packages/FibraNet/FibraNet-7.tar.gz/FibraNet-7/setup.py
@@ -0,0 +1,26 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+    name = "FibraNet",
+    version = "7",
+    packages = find_packages(),
+    py_modules=['nanothreads','gherkin'],
+    description='A cooperative threading and event driven framework.',
+    author='Simon Wittber',
+    author_email='simonwittber@gmail.com',
+    license='BSD',
+    platforms=['Any'],
+    long_description="""
+FibraNet includes a global event dispatcher, and a concurrency 
+simulator (using Python generator functions) / cooperative threading 
+system with pause/resume, kill/end functions, and some facilities
+to handle CPU blocking operations. 
+
+It is the successor to the EventNet package and the
+nanothreads module.
+""" 
+)
+
+
