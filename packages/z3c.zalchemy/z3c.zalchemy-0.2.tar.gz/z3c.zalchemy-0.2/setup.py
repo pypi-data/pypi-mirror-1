02997f9f79c953d45fa5d03d35cbb11e297f29ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package z3c.zalchemy/0.2/z3c.zalchemy-0.2.tar.gz

---
+++ packages/z3c.zalchemy/z3c.zalchemy-0.2.tar.gz/z3c.zalchemy-0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(name='z3c.zalchemy',
+      version='0.2',
+      author='Juergen Kartnaller and the Zope Community',
+      author_email='zope3-dev@zope.org',
+      url='http://pypi.python.org/pypi/z3c.sqlalchemy',
+      description="""SQLAlchemy integration into Zope 3""",
+      license='ZPL 2.1',
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      include_package_data = True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'SQLAlchemy>=0.3.9,<0.4dev',
+                        'ZODB3',
+                        'zope.component',
+                        'zope.interface',
+                        'zope.schema',
+                        'zope.app.testing',
+                        'zope.app.component',
+                        'zope.app.keyreference',
+                        'zope.app.container',
+                        'zope.app.pagetemplate',
+                       ],
+      extras_require = dict(test=['pysqlite']))
---
+++ packages/z3c.zalchemy/z3c.zalchemy-0.2.tar.gz/z3c.zalchemy-0.2/src/z3c/__init__.py
@@ -0,0 +1,19 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""z3c.zalchemy integrates sqlalchemy into zope 3
+
+$Id: __init__.py 67202 2006-04-21 10:25:35Z dobe $
+"""
+__docformat__ = "reStructuredText"
+
