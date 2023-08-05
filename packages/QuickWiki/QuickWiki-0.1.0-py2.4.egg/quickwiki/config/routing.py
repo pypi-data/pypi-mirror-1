9bfd983f82c6f19920c36032c5665ed2fc69ce0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:48 +0000

    Add package QuickWiki/0.1.0/QuickWiki-0.1.0-py2.4.egg

---
+++ packages/QuickWiki/QuickWiki-0.1.0-py2.4.egg/quickwiki/config/routing.py
@@ -0,0 +1,21 @@
+"""
+Setup your Routes options here
+"""
+import sys, os
+from routes import Mapper
+
+def make_map():
+    root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    map = Mapper(directory=os.path.join(root_path, 'controllers'))
+    
+    # Define your routes. The more specific and detailed routes should be defined first,
+    # so they may take precedent over the more generic routes. For more information, refer
+    # to the routes manual @ http://routes.groovie.org/docs/
+
+    map.connect('error/:action/:id', controller='error')
+    map.connect(':controller/:action/:title', controller='page', action='index', title='FrontPage')
+    map.connect(':title', controller='page', action='index', title='FrontPage')
+    map.connect('*url', controller='template', action='view')
+
+    return map
