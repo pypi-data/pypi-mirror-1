f71e30985e6f50f18f525560a3d8ddbed02a9c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:19 +0000

    Add package zc.sourcefactory/0.1DEV-r73411/zc.sourcefactory-0.1DEV-r73411.tar.gz

---
+++ packages/zc.sourcefactory/zc.sourcefactory-0.1DEV-r73411.tar.gz/zc.sourcefactory-0.1DEV-r73411/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zc.sourcefactory",
+    version="0.1",
+    author="Zope Corporation and Contributors",
+    author_email="zope3-dev@zope.org",
+    url="http://svn.zope.org/zc.sourcefactory",
+
+    long_description="An easy way to create custom Zope 3 sources.",
+
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+
+    include_package_data=True,
+    install_requires=["setuptools"],
+    namespace_packages=['zc'],
+
+    zip_safe=False
+    )
