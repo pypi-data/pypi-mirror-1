875142987d39aed3d6947c53d25db0c50b0a3f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package mixed/0.2/mixed-0.2.tar.gz

---
+++ packages/mixed/mixed-0.2.tar.gz/mixed-0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name="mixed",
+    version="0.2",
+    py_modules = ['mixed',],
+    
+    # metadata for upload to PyPI
+    author = 'Catherine Devlin',
+    author_email = 'catherine.devlin@gmail.com',
+    description = 'Class for fractions and mixed numbers',
+    license = 'MIT',
+    keywords = 'fraction arithmetic',
+    url = 'http://trac-hg.assembla.com/mixed_python/',
+    
+    long_description = """Class that parses strings representing fractions
+and mixed numbers and handles arithmetic using them properly.""",
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    )
+
