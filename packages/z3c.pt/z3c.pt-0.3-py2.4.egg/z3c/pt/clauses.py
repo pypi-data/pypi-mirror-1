dbf361c9fbc65e363ae87ca7dcf81729f0d7e14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:00 +0000

    Add package z3c.pt/0.3/z3c.pt-0.3-py2.4.egg

---
+++ packages/z3c.pt/z3c.pt-0.3-py2.4.egg/z3c/pt/clauses.py
@@ -0,0 +1,512 @@
+from attributes import expression
+
+class Assign(object):
+    """
+      >>> from z3c.pt.io import CodeIO; stream = CodeIO()
+
+    Simple assignment:
+
+      >>> assign = Assign(expression("1"))
+      >>> assign.begin(stream, 'a')
+      >>> exec stream.getvalue()
+      >>> a == 1
+      True
+      >>> assign.end(stream)
+
+    Try-except chain:
+
+      >>> assign = Assign(expression("float('abc') | 1"))
+      >>> assign.begin(stream, 'b')
+      >>> exec stream.getvalue()
+      >>> b == 1
+      True
+      >>> assign.end(stream)
+
+    Try-except chain part 2: 
+
+      >>> assign = Assign(expression("'abc' | 1"))
+      >>> assign.begin(stream, 'b')
+      >>> exec stream.getvalue()
+      >>> b == 'abc'
+      True
+      >>> assign.end(stream)
+     """
+    
+    def __init__(self, expressions, variable=None):
+        self.expressions = expressions
+        self.variable = variable
+
+    def begin(self, stream, variable=None):
+        """First n - 1 expressions must be try-except wrapped."""
+
+        variable = variable or self.variable
+            
+        for expression in self.expressions[:-1]:
+            stream.write("try:")
+            stream.indent()
+            stream.write("%s = %s" % (variable, expression))
+            stream.outdent()
+            stream.write("except Exception, e:")
+            stream.indent()
+
+        expression = self.expressions[-1]
+        stream.write("%s = %s" % (variable, expression))
+
+        stream.outdent(len(self.expressions)-1)
+        
+    def end(self, stream):
+        pass
+        
+class Define(object):
+    """
+      >>> from z3c.pt.io import CodeIO; stream = CodeIO()
+
+    Variable scope:
+
+      >>> define = Define("a", expression("b"))
+      >>> b = object()
+      >>> define.begin(stream)
+      >>> exec stream.getvalue()
+      >>> a is b
+      True
+      >>> del a
+      >>> define.end(stream)
+      >>> exec stream.getvalue()
+      >>> a
+      Traceback (most recent call last):
+          ...
+      NameError: name 'a' is not defined
+      >>> b is not None
+      True
+
+    Multiple defines:
+
+      >>> stream = CodeIO()
+      >>> define1 = Define("a", expression("b"))
+      >>> define2 = Define("c", expression("d"))
+      >>> d = object()
+      >>> define1.begin(stream)
+      >>> define2.begin(stream)
+      >>> exec stream.getvalue()
+      >>> a is b and c is d
+      True
+      >>> define2.end(stream)
+      >>> define1.end(stream)
+      >>> del a; del c
+      >>> stream.scope[-1].remove('a'); stream.scope[-1].remove('c')
+      >>> exec stream.getvalue()
+      >>> a
+      Traceback (most recent call last):
+          ...
+      NameError: name 'a' is not defined
+      >>> c
+      Traceback (most recent call last):
+          ...
+      NameError: name 'c' is not defined
+      >>> b is not None and d is not None
+      True
+
+    Tuple assignments:
+
+      >>> stream = CodeIO()
+      >>> define = Define(['e', 'f'], expression("[1, 2]"))
+      >>> define.begin(stream)
+      >>> exec stream.getvalue()
+      >>> e == 1 and f == 2
+      True
+      >>> define.end(stream)
+
+    Verify scope is preserved on tuple assignment:
+
+      >>> stream = CodeIO()
+      >>> e = None; f = None
+      >>> stream.scope[-1].add('e'); stream.scope[-1].add('f')
+      >>> stream.scope.append(set())
+      >>> define.begin(stream)
+      >>> define.end(stream)
+      >>> exec stream.getvalue()
+      >>> e is None and f is None
+      True
+
+    Using semicolons in expressions within a define:
+
+      >>> stream = CodeIO()
+      >>> define = Define("a", expression("';'"))
+      >>> define.begin(stream)
+      >>> exec stream.getvalue()
+      >>> a
+      ';'
+      >>> define.end(stream)
+
+    Scope:
+
+      >>> stream = CodeIO()
+      >>> a = 1
+      >>> stream.scope[-1].add('a')
+      >>> stream.scope.append(set())
+      >>> define = Define("a", expression("2"))
+      >>> define.begin(stream)
+      >>> define.end(stream)
+      >>> exec stream.getvalue()
+      >>> a
+      1
+    
+    """
+    def __init__(self, definition, expression):
+        if not isinstance(definition, (list, tuple)):
+            definition = (definition,)
+
+        if len(definition) == 1:
+            variable = definition[0]
+        else:
+            variable = u"(%s,)" % ", ".join(definition)
+
+        self.assign = Assign(expression, variable)        
+        self.definitions = definition
+        
+    def begin(self, stream):
+        # save local variables already in in scope
+        for var in self.definitions:
+            temp = stream.save()
+
+            # If we didn't set the variable in this scope already
+            if var not in stream.scope[-1]:
+
+                # we'll check if it's set in one of the older scopes
+                for scope in stream.scope[:-1]:
+                    if var in scope:
+                        # in which case we back it up
+                        stream.write('%s = %s' % (temp, var))
+
+                stream.scope[-1].add(var)
+                   
+        self.assign.begin(stream)
+
+    def end(self, stream):
+        self.assign.end(stream)
+
+        # back come the variables that were already in scope in the
+        # first place
+        for var in reversed(self.definitions):
+            temp = stream.restore()
+
+            # If we set the variable in this scope already
+            if var in stream.scope[-1]:
+
+                # we'll check if it's set in one of the older scopes
+                for scope in stream.scope[:-1]:
+                    if var in scope:
+                        # in which case we restore it
+                        stream.write('%s = %s' % (var, temp))
+                        stream.scope[-1].remove(var)
+                        break
+                else:
+                    stream.write("del %s" % var)
+
+class Condition(object):
+    """
+      >>> from z3c.pt.io import CodeIO
+
+    Unlimited scope:
+    
+      >>> stream = CodeIO()
+      >>> true = Condition(expression("True"))
+      >>> false = Condition(expression("False"))
+      >>> true.begin(stream)
+      >>> stream.write("print 'Hello'")
+      >>> true.end(stream)
+      >>> false.begin(stream)
+      >>> stream.write("print 'Universe!'")
+      >>> false.end(stream)
+      >>> stream.write("print 'World!'")
+      >>> exec stream.getvalue()
+      Hello
+      World!
+
+    Finalized limited scope:
+
+      >>> stream = CodeIO()
+      >>> from StringIO import StringIO
+      >>> _out = StringIO()
+      >>> true = Condition(expression("True"), [Write(expression("'Hello'"))])
+      >>> false = Condition(expression("False"), [Write(expression("'Hallo'"))])
+      >>> true.begin(stream)
+      >>> true.end(stream)
+      >>> false.begin(stream)
+      >>> false.end(stream)
+      >>> exec stream.getvalue()
+      >>> _out.getvalue()
+      'Hello'
+
+    Open limited scope:
+
+      >>> stream = CodeIO()
+      >>> from StringIO import StringIO
+      >>> _out = StringIO()
+      >>> true = Condition(expression("True"), [Tag('div')], finalize=False)
+      >>> false = Condition(expression("False"), [Tag('span')], finalize=False)
+      >>> true.begin(stream)
+      >>> stream.out("Hello World!")
+      >>> true.end(stream)
+      >>> false.begin(stream)
+      >>> false.end(stream)
+      >>> exec stream.getvalue()
+      >>> _out.getvalue()
+      '<div>Hello World!</div>'
+          
+    """
+      
+    def __init__(self, expression, clauses=None, finalize=True):
+        self.assign = Assign(expression)
+        self.clauses = clauses
+        self.finalize = finalize
+        
+    def begin(self, stream):
+        temp = stream.save()
+        self.assign.begin(stream, temp)
+        stream.write("if %s:" % temp)
+        stream.indent()
+        if self.clauses:
+            for clause in self.clauses:
+                clause.begin(stream)
+            if self.finalize:
+                for clause in reversed(self.clauses):
+                    clause.end(stream)
+            stream.outdent()
+        
+    def end(self, stream):
+        temp = stream.restore()
+
+        if self.clauses:
+            if not self.finalize:
+                stream.write("if %s:" % temp)
+                stream.indent()
+                for clause in reversed(self.clauses):
+                    clause.end(stream)
+                    stream.outdent()
+        else:
+            stream.outdent()
+        self.assign.end(stream)
+
+class Else(object):
+    def __init__(self, clauses=None):
+        self.clauses = clauses
+        
+    def begin(self, stream):
+        stream.write("else:")
+        stream.indent()
+        if self.clauses:
+            for clause in self.clauses:
+                clause.begin(stream)
+            for clause in reversed(self.clauses):
+                clause.end(stream)
+            stream.outdent()
+        
+    def end(self, stream):
+        if not self.clauses:
+            stream.outdent()
+
+class Group(object):
+    def __init__(self, clauses):
+        self.clauses = clauses
+        
+    def begin(self, stream):
+        for clause in self.clauses:
+            clause.begin(stream)
+        for clause in reversed(self.clauses):
+            clause.end(stream)
+        
+    def end(self, stream):
+        pass
+    
+class Tag(object):
+    """
+      >>> from z3c.pt.io import CodeIO
+      >>> from StringIO import StringIO
+
+      >>> _out = StringIO(); stream = CodeIO()
+      >>> tag = Tag('div', dict(alt=expression(repr('Hello World!'))))
+      >>> tag.begin(stream)
+      >>> stream.out('Hello Universe!')
+      >>> tag.end(stream)
+      >>> exec stream.getvalue()
+      >>> _out.getvalue()
+      '<div alt="Hello World!">Hello Universe!</div>'
+
+      >>> _out = StringIO(); stream = CodeIO()
+      >>> tag = Tag('br', {}, True)
+      >>> tag.begin(stream)
+      >>> tag.end(stream)
+      >>> exec stream.getvalue()
+      >>> _out.getvalue()
+      '<br />'
+      
+    """
+
+    def __init__(self, tag, attributes={}, selfclosing=False):
+        i = tag.find('}')
+        if i != -1:
+            self.tag = tag[i+1:]
+        else:
+            self.tag = tag
+
+        self.selfclosing = selfclosing
+        self.attributes = attributes
+        
+    def begin(self, stream):
+        stream.out('<%s' % self.tag)
+
+        # attributes
+        for attribute, expression in self.attributes.items():
+            stream.out(' %s="' % attribute)
+
+            if isinstance(expression, (tuple, list)):
+                write = Write(expression)
+                write.begin(stream)
+                write.end(stream)
+            else:
+                stream.out(expression.replace("'", "\\'"))
+            stream.out('"')
+
+        if self.selfclosing:
+            stream.out(" />")
+        else:
+            stream.out(">")
+
+    def end(self, stream):
+        if not self.selfclosing:
+            stream.out('</%s>' % self.tag)
+
+class Repeat(object):
+    """
+      >>> from z3c.pt.io import CodeIO; stream = CodeIO()
+
+    We need to set up the repeat object.
+
+      >>> from z3c.pt import utils
+      >>> repeat = utils.repeatdict()
+
+    Simple repeat loop and repeat data structure:
+
+      >>> _repeat = Repeat("i", expression("range(5)"))
+      >>> _repeat.begin(stream)
+      >>> stream.write("r = repeat['i']")
+      >>> stream.write("print (i, r.index, r.start, r.end, r.number(), r.odd(), r.even())")
+      >>> exec stream.getvalue()
+      (0, 0, True, False, 1, False, True)
+      (1, 1, False, False, 2, True, False)
+      (2, 2, False, False, 3, False, True)
+      (3, 3, False, False, 4, True, False)
+      (4, 4, False, True, 5, False, True)
+      >>> _repeat.end(stream)
+      
+    """
+        
+    def __init__(self, v, e, scope=()):
+        self.variable = v
+        self.define = Define(v, expression("None"))
+        self.assign = Assign(e)
+
+    def begin(self, stream):
+        variable = self.variable
+        iterator = stream.save()
+
+        # assign iterator
+        self.assign.begin(stream, iterator)
+
+        # initialize variable scope
+        self.define.begin(stream)
+
+        # initialize iterator
+        stream.write("repeat['%s'] = %s = %s.__iter__()" % (variable, iterator, iterator))
+
+        # loop
+        stream.write("while %s:" % iterator)
+        stream.indent()
+        stream.write("%s = %s.next()" % (variable, iterator))
+
+    def end(self, stream):
+        # cook before leaving loop
+        stream.cook()
+        
+        stream.outdent()
+        self.define.end(stream)
+        self.assign.end(stream)
+        stream.restore()
+
+class Write(object):
+    """
+      >>> from z3c.pt.io import CodeIO; stream = CodeIO()
+      >>> from StringIO import StringIO
+
+      >>> _out = StringIO()
+      >>> write = Write(expression("'New York'"))
+      >>> write.begin(stream)
+      >>> write.end(stream)
+      >>> exec stream.getvalue()
+      >>> _out.getvalue()
+      'New York'
+      >>> _out = StringIO()
+      >>> write = Write(expression("undefined | ', New York!'"))
+      >>> write.begin(stream)
+      >>> write.end(stream)
+      >>> exec stream.getvalue()
+      >>> _out.getvalue()
+      'New York, New York!'
+    """
+    
+    def __init__(self, expressions):
+        self.assign = Assign(expressions)
+        self.expressions = expressions
+        self.count = len(expressions)
+        
+    def begin(self, stream):
+        temp = stream.save()
+                
+        if self.count == 1:
+            stream.write("_out.write(%s)" % self.expressions[0])
+        else:
+            self.assign.begin(stream, temp)
+            stream.write("_out.write(%s)" % temp)
+        
+    def end(self, stream):
+        if self.count != 1:
+            self.assign.end(stream)
+        stream.restore()
+
+class Out(object):
+    """
+      >>> from z3c.pt.io import CodeIO; stream = CodeIO()
+      >>> from StringIO import StringIO
+      >>> _out = StringIO()
+      
+      >>> out = Out('Hello World!')
+      >>> out.begin(stream)
+      >>> out.end(stream)
+      >>> exec stream.getvalue()
+      >>> _out.getvalue()
+      'Hello World!'      
+    """
+    
+    def __init__(self, string, defer=False):
+        self.string = string
+        self.defer = defer
+        
+    def begin(self, stream):
+        if not self.defer:
+            stream.out(self.string)
+
+    def end(self, stream):
+        if self.defer:
+            stream.out(self.string)
+
+class Translate(object):
+    """
+    The translate clause works retrospectively.
+    """
+
+    def begin(self, stream):
+        raise
+
+    def end(self, stream):
+        raise
