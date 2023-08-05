77955861f2dd19fc916af3384785e200752289b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:47 +0000

    Add package gocept.mochikit/1.3.1/gocept.mochikit-1.3.1.tar.gz

---
+++ packages/gocept.mochikit/gocept.mochikit-1.3.1.tar.gz/gocept.mochikit-1.3.1/src/gocept/mochikit/interfaces.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import zope.viewlet.interfaces
+
+
+class IJavascriptViewletManager(zope.viewlet.interfaces.IViewletManager):
+    """Viewletmanager for javascripts."""
