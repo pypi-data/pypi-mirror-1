745d2000172da68a63142750e71c1f6dcebf7f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:09 +0000

    Add package Rabbyt/0.0.2/Rabbyt-0.0.2.tar.gz

---
+++ packages/Rabbyt/Rabbyt-0.0.2.tar.gz/Rabbyt-0.0.2/examples/basic2.py
@@ -0,0 +1,21 @@
+import rabbyt
+import pygame
+
+import os.path
+# We do this so that you don't have to be in the same directory as the script
+# to use it.
+rabbyt.data_directory = os.path.dirname(__file__)
+
+# Make sure that you allways have a valid OpenGL context before creating a
+# sprite.  This init function will do that for you:
+rabbyt.init_display((640, 480))
+
+# Creating a sprite is easy.  Just give it an image filename.
+car = rabbyt.Sprite("car.png")
+
+while True:
+    rabbyt.clear((1,1,1))
+
+    car.render()
+
+    pygame.display.flip()
---
+++ packages/Rabbyt/Rabbyt-0.0.2.tar.gz/Rabbyt-0.0.2/examples/collision.py
@@ -0,0 +1,52 @@
+from __future__ import division
+
+import random
+
+import rabbyt, rabbyt.collisions
+from rabbyt import lerp, wrap
+
+import pygame
+
+rabbyt.init_display((800, 600))
+
+
+sprites = []
+
+r = lambda: random.random()-.5
+
+for i in range(400):
+    s = rabbyt.Sprite(shape=(-2,2,2,-2))
+
+    s.x = wrap([-400,400], lerp(r()*800, r()*800, dt=4000, extend="extrapolate"))
+    s.y = wrap([-300,300], lerp(r()*600, r()*600, dt=4000, extend="extrapolate"))
+
+    sprites.append(s)
+
+collision_times = []
+c = pygame.time.Clock()
+last_fps = 0
+while True:
+    c.tick()
+    if pygame.time.get_ticks() - last_fps > 1000:
+        print "FPS: ", c.get_fps()
+        last_fps = pygame.time.get_ticks()
+        average = sum(collision_times)/len(collision_times)
+        print "Average ms to find collisions:", average
+        collision_times = []
+    rabbyt.clear()
+    rabbyt.set_time(pygame.time.get_ticks())
+
+    start = pygame.time.get_ticks()
+    # With the sprites so close together, just using brute_force ends up being
+    # faster.  If you have more sprites spread further apart, collide should
+    # work better.
+    collisions = rabbyt.collisions.brute_force(sprites)
+    #collisions = rabbyt.collisions.collide(sprites)
+    collision_times.append(pygame.time.get_ticks()-start)
+
+    for s1, s2 in collisions:
+        s1.rgb = lerp((1,0,0),(1,1,1), dt=400)
+        s2.rgb = lerp((1,0,0),(1,1,1), dt=400)
+
+    rabbyt.render_unsorted(sprites)
+    pygame.display.flip()
