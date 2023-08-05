28d0c77ee90cf0385f15ecab69aae777f610b854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:02 +0000

    Add package huTools/0.3.dev-r3164/huTools-0.3.dev-r3164.tar.gz

---
+++ packages/huTools/huTools-0.3.dev-r3164.tar.gz/huTools-0.3.dev-r3164/printing.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+printing.py simple minded toolkit for printing on CUPS.
+
+Created by Maximillian Dornseif on 2006-11-19. BSD Licensed.
+"""
+
+import os
+
+__revision__ = "$Revision: 2916 $"
+
+def print_file(filename, jobname=None, printer=None, copies=1):
+    """Print a file."""
+    args = [] # '-n %d' % copies]
+    if printer:
+        args.append('-P %r' % str(printer))
+    for c in range(copies):
+        os.system('/usr/local/bin/lpr %s %r' % (' '.join(args), filename))
+
+def print_data(data, jobname=None, printer=None, copies=1, printserver='printserver.local.hudora.biz'):
+    """Print a file."""
+    args = [] # ['-n %d' % copies]
+    if printer:
+        args.append('-P %r' % str(printer))
+    #if printserver:
+    #    args.append('-H %r' % str(printserver))
+    #if jobname:
+    #    args.append('-J %r' % jobname.replace("'\";./ ", "_"))
+    os.system('echo /usr/local/bin/lpr %s >> /tmp/huTools_lplog' % (' '.join(args),))
+    for c in range(copies):
+        fd = os.popen('/usr/local/bin/lpr %s >> /tmp/huTools_lplog' % (' '.join(args),), 'w')
+        fd.write(data)
+        fd.close()
---
+++ packages/huTools/huTools-0.3.dev-r3164.tar.gz/huTools-0.3.dev-r3164/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(name='huTools',
+      maintainer='Maximillian Dornseif',
+      maintainer_email='md@hudora.de',
+      url='https://cybernetics.hudora.biz/projects/wiki/huTools',
+      version='0.3',
+      description='Various tiny tools and toys to make Python coding less work more fun.',
+      long_description='''huTools is a collection of many totally non earth shattering modules:
+      Various tiny tools and toys to make Python coding less work more fun:
+      
+      * calendar              - date based calculations and format conversions for HTTP and ATOM.
+      * checksummming         - calculate various checksums including EAN/GTIN/NVE/SSCC and DPD GeoPost
+      * luids                 - locally unique user-ids with strong guarantees of beeing unique.
+      * printing              - access printers (very primitive)
+      * ReReadingConfigParser - ConfigParser which detects changes in config files
+      * unicode               - real-world unicode handling (very primitive)
+      * world                 - country codes (very primitive)''',
+      license='BSD',
+      classifiers=['Intended Audience :: Developers',
+                   'Programming Language :: Python'],
+      
+      zip_safe=False, 
+      package_dir = {'huTools': ''},
+      packages = ['huTools', 'huTools.calendar'],
+      zip_safe = False,      
+      # This seems to be broken with certain Python versions:
+      #package_data = {
+      #      # If any package contains *.txt or *.rst files, include them:
+      #      '': ['*.txt', '*.rst'],
+      #},
+)
