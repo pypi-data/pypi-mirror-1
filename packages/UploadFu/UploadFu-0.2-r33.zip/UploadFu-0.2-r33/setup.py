5ff3712600f54f172156f10bb81561020b4dcaab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:37 +0000

    Add package UploadFu/0.2-r33/UploadFu-0.2-r33.zip

---
+++ packages/UploadFu/UploadFu-0.2-r33.zip/UploadFu-0.2-r33/setup.py
@@ -0,0 +1,35 @@
+import os
+os.environ['HOME'] = os.getcwd()
+print os.environ['HOME']
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "UploadFu",
+    version = "0.2",
+    author="Leonord",
+    author_email="allegory.bucket@gmail.com",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+    ],      
+    description="Bittorrent upload tools for win32",
+    scripts = [
+        'scripts/UploadFu_Upload.py', 
+        'scripts/UploadFu_Watch.py', 
+        'scripts/UploadFu_StartWatchJobs.py',
+        'scripts/UploadFu_ContextMenu.py', 
+        'scripts/UploadFu_example.ini', 
+        'scripts/UploadFu_Setup.bat'
+    ],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['mechanize>=0.1.7b', 'configobj>=4.4.0'],
+    packages=['uploadfu', 'uploadfu.BitTorrent'],
+    
+    zip_safe=False,
+  
+)
