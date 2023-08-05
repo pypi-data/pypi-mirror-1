790f6ef4b5e8275b4cdc9d0036b0d773b6542ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:59 +0000

    Add package GFX/3/GFX-3-py2.5-macosx-10.3-fat.egg

---
+++ packages/GFX/GFX-3-py2.5-macosx-10.3-fat.egg/gfx/mvc/view.py
@@ -0,0 +1,27 @@
+from dag import Node, Composite, traverse
+
+from gfx.ext import GLSurface, Rect
+
+
+
+class Image(Node):
+    def __init__(self, surface):
+        assert isinstance(surface, GLSurface)
+        self.surface = surface
+        self.rect = Rect((0,0) + surface.size)
+
+class Line(Node):
+    pass
+
+class Group(Composite):
+    def get_rect(self):
+        return Rect.union(i.rect for i in self.children)
+    rect = property(get_rect)
+
+class Root(Composite):
+    pass
+
+
+root = Root()
+
+
