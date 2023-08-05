59d4b78dd8afebecd9c9d6d1b98e2c78698f85ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:49 +0000

    Add package rwproperty/1.0/rwproperty-1.0.tar.gz

---
+++ packages/rwproperty/rwproperty-1.0.tar.gz/rwproperty-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rwproperty',
+    version='1.0',
+    author='Philipp von Weitershausen',
+    author_email='grok-dev@zope.org',
+    url='http://www.z3lab.org/sections/blogs/philipp-weitershausen/2006_05_29_pycon-06-lightning-talk',
+    download_url='http://codespeak.net/svn/user/philikon/rwproperty#egg=rwproperty-dev',
+    description='Read & write properties',
+    long_description=open('rwproperty.txt').read(),
+    license='ZPL',
+    
+    py_modules=['rwproperty']
+)
