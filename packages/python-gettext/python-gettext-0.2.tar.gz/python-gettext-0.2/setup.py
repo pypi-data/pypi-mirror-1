5bd53d371fba363f9eefe7f2b4db7afae2792c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:46 +0000

    Add package python-gettext/0.2/python-gettext-0.2.tar.gz

---
+++ packages/python-gettext/python-gettext-0.2.tar.gz/python-gettext-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '0.2'
+
+setup(name='python-gettext',
+      version=version,
+      description="Python Gettext implementation.",
+      long_description="""\
+This implementation of Gettext for Python includes a Msgfmt class which can be
+used to generate compiled mo files from Gettext po files and includes support
+for the newer msgctxt keyword.""",
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Localization',
+      ],
+      keywords='Python Gettext Msgctxt',
+      author='Hanno Schlichting',
+      author_email='hanno@hannosch.info',
+      url='http://svn.plone.org/svn/collective/python-gettext/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      )
