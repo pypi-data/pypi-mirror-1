f0368fc5f187ade039a0a19eff8bae35d1cb0e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package plone.app.portlets/1.0.1/plone.app.portlets-1.0.1.tar.gz

---
+++ packages/plone.app.portlets/plone.app.portlets-1.0.1.tar.gz/plone.app.portlets-1.0.1/plone/app/portlets/assignable.py
@@ -0,0 +1,27 @@
+from zope.interface import implementer
+from zope.component import adapter
+from zope.annotation.interfaces import IAnnotations
+
+from plone.portlets.interfaces import IPortletAssignmentMapping
+from plone.portlets.interfaces import ILocalPortletAssignable
+from plone.portlets.interfaces import IPortletManager
+
+from plone.portlets.constants import CONTEXT_ASSIGNMENT_KEY
+
+from BTrees.OOBTree import OOBTree
+
+from plone.app.portlets.storage import PortletAssignmentMapping
+
+@adapter(ILocalPortletAssignable, IPortletManager)
+@implementer(IPortletAssignmentMapping)
+def localPortletAssignmentMappingAdapter(context, manager):
+    """Zope 2 version of the localPortletAssignmentMappingAdapter factory.
+    """
+    annotations = IAnnotations(context)
+    local = annotations.get(CONTEXT_ASSIGNMENT_KEY, None)
+    if local is None:
+        local = annotations[CONTEXT_ASSIGNMENT_KEY] = OOBTree()
+    portlets = local.get(manager.__name__, None)
+    if portlets is None:
+        portlets = local[manager.__name__] = PortletAssignmentMapping()
+    return portlets
