9739f978a833cad6956fdf7ad646d4128fd0efa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package haufe.releaser/0.1.4/haufe.releaser-0.1.4.tar.gz

---
+++ packages/haufe.releaser/haufe.releaser-0.1.4.tar.gz/haufe.releaser-0.1.4/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.1.4'
+changes = open(os.path.join(os.path.dirname(__file__), 'CHANGES.txt')).read()
+long_desc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+long_desc += "\nCHANGES\n=======\n" + changes
+
+setup(name='haufe.releaser',
+      version=version,
+      description="Performs local upload of distribution files to a haufe.eggserver instance",
+      long_description=long_desc,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Andreas Jung',
+      author_email='andreas.jung@haufe.de',
+      url='',
+      license='ZPL V2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['haufe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+
+      [distutils.commands]
+      local_upload = haufe.releaser.commands:local_upload
+      check_description = haufe.releaser.commands:check_description
+      """
+      )
