5a7d59aa5db86f90c491f0158364c0cd86bf1248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:42 +0000

    Add package qtxmldom/0.2.1/qtxmldom-0.2.1.tar.gz

---
+++ packages/qtxmldom/qtxmldom-0.2.1.tar.gz/qtxmldom-0.2.1/examples/uppercase.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"Convert text nodes to upper case."
+
+import sys
+import qtxmldom
+from qtxmldom.khtmlutils import import_doc, export_doc
+
+def main(part):
+    html_doc = qtxmldom.fromNode(part.document())
+    doc = import_doc(html_doc)
+    query = "/h:HTML/h:BODY//text()"
+    text = doc.xpath(query, namespaces={"h" : ""}) or doc.xpath(query.lower(), namespaces={"h" : "http://www.w3.org/1999/xhtml"})
+    for node in text:
+        node.nodeValue = node.nodeValue.upper()
+    export_doc(html_doc, doc)
+
+# vim: tabstop=4 expandtab shiftwidth=4
