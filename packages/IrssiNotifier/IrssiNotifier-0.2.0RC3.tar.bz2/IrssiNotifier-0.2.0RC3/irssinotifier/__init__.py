9ce97ec93b27db39ea2c7ff51fadf282a9dce1ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package IrssiNotifier/0.2.0RC3/IrssiNotifier-0.2.0RC3.tar.bz2

---
+++ packages/IrssiNotifier/IrssiNotifier-0.2.0RC3.tar.bz2/IrssiNotifier-0.2.0RC3/irssinotifier/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4 ts=4 fenc=utf-8
+# =============================================================================
+# $Id: __init__.py 48 2007-09-04 08:28:43Z s0undt3ch $
+# =============================================================================
+#             $URL: http://irssinotifier.ufsoft.org/svn/branches/0.2.x/irssinotifier/__init__.py $
+# $LastChangedDate: 2007-09-04 09:28:43 +0100 (Tue, 04 Sep 2007) $
+#             $Rev: 48 $
+#   $LastChangedBy: s0undt3ch $
+# =============================================================================
+# Copyright (C) 2007 UfSoft.org - Pedro Algarvio <ufs@ufsoft.org>
+#
+# Please view LICENSE for additional licensing information.
+# =============================================================================
+
+__version__ = '0.2.0RC3'
+__author__  = 'Pedro Algarvio'
+__email__   = 'ufs@ufsoft.org'
+__package__ = 'IrssiNotifier'
+__license__ = 'BSD'
+__url__     = 'http://irssinotifier.ufsoft.org'
+
