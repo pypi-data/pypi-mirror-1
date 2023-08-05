bce837edb06913393e66b87245ca7636948fd66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:13 +0000

    Add package motmot.wxglvideo/0.6.1.dev-r481/motmot.wxglvideo-0.6.1.dev-r481.tar.gz

---
+++ packages/motmot.wxglvideo/motmot.wxglvideo-0.6.1.dev-r481.tar.gz/motmot.wxglvideo-0.6.1.dev-r481/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(name='motmot.wxglvideo',
+      description='wx/OpenGL viewer of image sequences',
+      long_description = \
+"""Allows for rapid display and resizing/rotation of images by
+offloading the image operations to OpenGL.
+
+This is a subpackage of the motmot family of digital image utilities.
+""",
+      version='0.6.1',
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
+      install_requires = ['numpy>=1.0.4',
+                          'motmot.imops>=0.5.2.dev',
+                          'pyglet>=1.0',
+                          'wxPython>=2.8',
+                          'pygarrayimage>=0.0.2',
+                          ],
+      url='http://code.astraw.com/projects/motmot',
+      license='BSD',
+      zip_safe=True,
+      packages = find_packages(),
+      namespace_packages = ['motmot'],
+      package_data = {'motmot.wxglvideo':['demo.xrc']},
+      entry_points = {'gui_scripts': ['wxglvideo_demo=motmot.wxglvideo.demo:main',
+                                      ]}
+      )
