d5e296d355173d703479443f4a6f0623c97fc43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package zope.app.form/0.1.dev-r73787/zope.app.form-0.1.dev-r73787.tar.gz

---
+++ packages/zope.app.form/zope.app.form-0.1.dev-r73787.tar.gz/zope.app.form-0.1.dev-r73787/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(name='zope.app.form',
+      version='0.1',
+      url='http://svn.zope.org/zope.app.form',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+
+      namespace_packages=["zope", "zope.app"],
+      include_package_data=True,
+      install_requires=[
+          "setuptools",
+          "ZODB3",
+          "zope.app.container",
+          "zope.app.publisher",
+          "zope.cachedescriptors",
+          "zope.component",
+          "zope.configuration",
+          "zope.deprecation",
+          "zope.exceptions",
+          "zope.i18n",
+          "zope.interface",
+          "zope.proxy",
+          "zope.publisher",
+          "zope.schema",
+          "zope.security",
+          'zope.app.basicskin',
+          ],
+      extras_require={"test": ['zope.app.testing',
+                               'zope.app.securitypolicy',
+                               'zope.app.zcmlfiles']},
+      zip_safe=False,
+      )
