cae3281419dfef532f41a864007824f7e7b837e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package zc.sourcerelease/0.2/zc.sourcerelease-0.2.tar.gz

---
+++ packages/zc.sourcerelease/zc.sourcerelease-0.2.tar.gz/zc.sourcerelease-0.2/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+entry_points = """
+[console_scripts]
+buildout-source-release = zc.sourcerelease:source_release
+"""
+
+long_description=(
+    '.. contents::\n\n'
+    +
+    read('src', 'zc', 'sourcerelease', 'README.txt')
+    + '\n' +
+    read('src', 'zc', 'sourcerelease', 'CHANGES.txt')
+    + '\n' +
+    'Download\n'
+    '========\n'
+    )
+
+open('doc.txt', 'w').write(long_description)
+
+
+setup(
+    name = "zc.sourcerelease",
+    description = "Utility script to create source releases from buildouts",
+    version = "0.2",
+    license = "ZPL 1.0",
+    url='http://www.python.org/pypi/zc.sourcerelease',
+    author='Jim Fulton', author_email='jim@zope.com',
+    long_description=long_description,
+    
+    entry_points = entry_points,
+    packages = find_packages('src'),
+    include_package_data = True,
+    zip_safe = False,
+    package_dir = {'':'src'},
+    namespace_packages = ['zc'],
+    install_requires = [
+        'setuptools',
+        'zc.buildout',
+        'zc.recipe.egg',
+        ],
+    )
