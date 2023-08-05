b465278a5e3dc5130317d811daa97dd0c7aef12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:40 +0000

    Add package ll-xist/2.14.2/ll_xist-2.14.2-py2.4-linux-i686.egg

---
+++ packages/ll-xist/ll_xist-2.14.2-py2.4-linux-i686.egg/ll/xist/ns/xlink.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+## Copyright 1999-2006 by LivingLogic AG, Bayreuth/Germany.
+## Copyright 1999-2006 by Walter Dörwald
+##
+## All Rights Reserved
+##
+## See xist/__init__.py for the license
+
+"""
+<par>Contains the global attributes for the xlink namespace
+(<lit>http://www.w3.org/1999/xlink</lit>).</par>
+"""
+
+__version__ = "$Revision: 1.1 $".split()[1]
+# $Source: /data/cvsroot/LivingLogic/Python/xist/src/ll/xist/ns/xlink.py,v $
+
+from ll.xist import xsc, utils
+
+
+class __ns__(xsc.Namespace):
+	xmlname = "xlink"
+	xmlurl  = "http://www.w3.org/1999/xlink"
+
+	class Attrs(xsc.Namespace.Attrs):
+		class type(xsc.TextAttr): values = (u"simple", u"extended", u"locator", u"arc", u"resource", u"title")
+		class href(xsc.URLAttr): pass
+		class role(xsc.URLAttr): pass
+		class arcrole(xsc.URLAttr): pass
+		class title(xsc.TextAttr): pass
+		class show(xsc.TextAttr): values = (u"new", u"replace", u"embed", u"other", u"none")
+		class label(xsc.TextAttr): pass
+		class actuate(xsc.TextAttr): values = (u"onLoad", u"onRequest", u"other", u"none")
+		class from_(xsc.TextAttr): xmlname = "from"
+		class to(xsc.TextAttr): pass
+__ns__.makemod(vars())
