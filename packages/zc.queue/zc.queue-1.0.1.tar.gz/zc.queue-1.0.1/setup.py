24912640ca6fccad8c85d89766f7c5678227120c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:58 +0000

    Add package zc.queue/1.0.1/zc.queue-1.0.1.tar.gz

---
+++ packages/zc.queue/zc.queue-1.0.1.tar.gz/zc.queue-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="zc.queue",
+    version="1.0.1",
+    license="ZPL 2.1",
+    author="Zope Project",
+    author_email="zope3-dev@zope.org",
+
+    namespace_packages=["zc"],
+    packages=["zc", "zc.queue"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=["zope.interface", "ZODB3"],
+    tests_require=["zope.testing"],
+    description=open('README.txt').read(),
+    long_description=open("src/zc/queue/queue.txt").read(),
+    keywords="zope zope3",
+    zip_safe=False
+    )
