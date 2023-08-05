64d8e779023bc87d990fc8ef8c281a3e9daff601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:18 +0000

    Add package motmot.imops/0.5.2/motmot.imops-0.5.2.zip

---
+++ packages/motmot.imops/motmot.imops-0.5.2.zip/motmot.imops-0.5.2/setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import setup, Extension, find_packages
+
+import numpy
+
+setup(name="motmot.imops",
+      description="image format conversion (e.g. between MONO8, YUV422, and RGB)",
+      long_description = """
+This is a subpackage of the motmot family of digital image utilities.
+""",
+      version="0.5.2",
+      license="BSD",
+      maintainer="Andrew Straw",
+      maintainer_email="strawman@astraw.com",
+      url="http://code.astraw.com/projects/motmot",
+      packages = find_packages(),
+      namespace_packages = ['motmot'],
+      install_requires = ['numpy>=1.0.4'],
+      ext_modules=[Extension(name="motmot.imops.imops",
+                             sources=['src/imops.pyx','src/color_convert.c',],
+                             include_dirs=[numpy.get_include()],
+                             ),
+                   ],
+      zip_safe = True,
+     )
