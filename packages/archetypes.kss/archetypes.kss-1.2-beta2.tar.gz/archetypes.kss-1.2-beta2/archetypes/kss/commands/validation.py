e08fa3460c7c7b4c44c036a9e0e921d72a256d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package archetypes.kss/1.2-beta2/archetypes.kss-1.2-beta2.tar.gz

---
+++ packages/archetypes.kss/archetypes.kss-1.2-beta2.tar.gz/archetypes.kss-1.2-beta2/archetypes/kss/commands/validation.py
@@ -0,0 +1,24 @@
+
+from kss.core.azaxview import AzaxViewAdapter
+
+class ValidationCommands(AzaxViewAdapter):
+    __allow_access_to_unprotected_subobjects__ = 1
+    
+    def issueFieldError(self, fieldname, error):
+        'Issue this error message for the field'
+        ksscore = self.getCommandSet('core')
+        selector = ksscore.getCssSelector('div#archetypes-fieldname-%s div.fieldErrorBox' % fieldname)
+        if error:
+            ksscore.replaceInnerHTML(selector, error)
+            errorklass = ' error'
+        else:
+            ksscore.clearChildNodes(selector)
+            errorklass = ''
+        klass = "field%s Archetypes%sfield" % (errorklass, fieldname)
+        kssattr = self.context.restrictedTraverse('kss_field_decorator_view').getKssClasses(fieldname)
+        if kssattr:
+            klass += ' ' + kssattr
+        # set the field style in the required way
+        ksscore.setAttribute(
+            ksscore.getHtmlIdSelector('archetypes-fieldname-%s' % fieldname),
+            'class', klass)
