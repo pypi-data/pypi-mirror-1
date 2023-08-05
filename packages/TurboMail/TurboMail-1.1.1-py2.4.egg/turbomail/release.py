0a193dcf8ddd7f31c43af24e756fb04abaefcfdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:48 +0000

    Add package TurboMail/1.1.1/TurboMail-1.1.1-py2.4.egg

---
+++ packages/TurboMail/TurboMail-1.1.1-py2.4.egg/turbomail/release.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+"""Release information about the Turbo-Mail mail queue."""
+
+version = "1.1.1"
+description = "Multi-threaded mail queue manager for TurboGears applications."
+author = "Top Floor Computer Systems Ltd."
+email = "webmaster@topfloor.ca"
+url = "http://www.topfloor.ca/turbomail/"
+download_url = "http://www.topfloor.ca/turbomail/"
+copyright = "© 2006 Top Floor Computer Systems Ltd."
+license="MIT"
