9c7b1307256a4952385a4ce2e0dce3bce4ee81d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:35 +0000

    Add package zope.component/3.4dev-r72747/zope.component-3.4dev-r72747.tar.gz

---
+++ packages/zope.component/zope.component-3.4dev-r72747.tar.gz/zope.component-3.4dev-r72747/README.txt
@@ -0,0 +1,48 @@
+*****************************
+zope.component Package Readme
+*****************************
+
+This package represents the core of the Zope Component Architecture.
+Together with the 'zope.interface' package, it provides facilities for
+defining, registering and looking up components.
+
+.. contents::
+
+Releases
+********
+
+3.2.0.2 (2006/04/15)
+====================
+
+Fix packaging bug:  'package_dir' must be a *relative* path.
+
+zope.component version 3.2.0.1 (2006/04/14)
+----------------------------------------------
+
+Packaging change:  suppress inclusion of 'setup.cfg' in 'sdist' builds.
+
+3.2.0 (2006/01/05)
+==================
+
+Corresponds to the verison of the zope.component package shipped as part of
+the Zope 3.2.0 release.
+
+Deprecated services and related APIS. The adapter and utility registries
+are now available directly via the site manager's 'adapters' and 'utilities'
+attributes, respectively.  Services are accessible, but deprecated, and
+will be removed in Zope 3.3.
+
+Deprectaed all presentation-related APIS, including all view-related
+API functions. Use the adapter API functions instead.
+See http://dev.zope.org/Zope3/ImplementViewsAsAdapters`
+
+Deprecated 'contextdependent' package:  site managers are now looked up
+via a thread global, set during URL traversal.  The 'context' argument
+is now always optional, and should no longer be passed.
+
+3.0.0 (2004/11/07)
+==================
+
+Corresponds to the verison of the zope.component package shipped as part of
+the Zope X3.0.0 release.
+
---
+++ packages/zope.component/zope.component-3.4dev-r72747.tar.gz/zope.component-3.4dev-r72747/setup.py
@@ -0,0 +1,81 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.i18nmessageid package
+
+$Id: setup.py 72747 2007-02-21 16:04:04Z jim $
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+name = 'zope.component'
+setup(
+    name=name,
+    version='3.4dev',
+    url='http://www.python.org/pypi/'+name,
+    license='ZPL 2.1',
+    description='Core of the Zope Component Architecture',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+    long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'zope', 'component', 'README.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'event.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'registry.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'socketexample.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+    namespace_packages=['zope',],
+    tests_require = ['zope.testing'],
+    install_requires=['zope.deprecation',
+                      'zope.interface',
+                      'zope.deferredimport',
+                      'zope.event',
+                      'setuptools',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    extras_require = dict(
+        service = ['zope.exceptions'],
+        zcml = ['zope.configuration', 'zope.security', 'zope.proxy',
+                'zope.i18nmessageid',
+                #'zope.location', # should be depenency of zope.security
+                ],
+        test = ['zope.testing',
+                'ZODB3',
+                #'zope.configuration', 'zope.security', 'zope.proxy',
+                #'zope.i18nmessageid',
+                #'zope.location', # should be depenency of zope.security
+                ],
+        hook = ['zope.hookable'],
+        persistentregistry = ['ZODB3'],
+        ),
+    )
