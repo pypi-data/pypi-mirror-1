1f9018730c798d2e494677e096da25fb99f40b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:53 +0000

    Add package simpleweb/0.6.2/simpleweb-0.6.2.tar.gz

---
+++ packages/simpleweb/simpleweb-0.6.2.tar.gz/simpleweb-0.6.2/src/simpleweb/admin/plugins/createtables.py
@@ -0,0 +1,17 @@
+import sys
+
+def createtables(name, args):
+	"""Usage: simpleweb-admin createtables
+
+Create all the tables defined in the models for a simpleweb project.
+config.db_plugin has to be configured first.
+"""
+	if len(args) > 0:
+		sys.stderr.write("command 'createtables' takes no arguments\n")
+		sys.exit(0)
+
+	try:
+		config = __import__('config')
+		config.db_plugin.createtables()
+	except ImportError:
+		sys.stderr.write("Can not find config.py. Is this a valid simpleweb project directory?\n")
---
+++ packages/simpleweb/simpleweb-0.6.2.tar.gz/simpleweb-0.6.2/src/simpleweb/settings.py
@@ -0,0 +1,10 @@
+class Config(object):
+	"""
+	Grabs config.py information and makes it and any other information available conveniently
+	"""
+
+	def __init__(self, conf_file='config'):
+		module = __import__(conf_file)
+		var_list = (v for v in dir(module)) # if not v.startswith('__'))
+		for attr in var_list:
+			setattr(self, attr, getattr(module, attr))
