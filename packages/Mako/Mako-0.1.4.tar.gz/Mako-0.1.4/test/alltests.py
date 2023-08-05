814edcdad60f1a75d301a0288205b6fed7135325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:06 +0000

    Add package Mako/0.1.4/Mako-0.1.4.tar.gz

---
+++ packages/Mako/Mako-0.1.4.tar.gz/Mako-0.1.4/test/alltests.py
@@ -0,0 +1,27 @@
+import unittest
+
+def suite():
+    modules_to_test = (
+        'lru',
+        'ast',
+        'pygen',
+        'lexer',
+        'template',
+        'lookup',
+        'def',
+        'namespace',
+        'filters',
+        'inheritance',
+        'call',
+	'cache'
+        )
+    alltests = unittest.TestSuite()
+    for name in modules_to_test:
+        mod = __import__(name)
+        for token in name.split('.')[1:]:
+            mod = getattr(mod, token)
+        alltests.addTest(unittest.findTestCases(mod, suiteClass=None))
+    return alltests
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
