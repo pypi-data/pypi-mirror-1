587c712244cd7b693b3b96e503961f488a7c753e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package p4a.video/1.0/p4a.video-1.0-py2.4.egg

---
+++ packages/p4a.video/p4a.video-1.0-py2.4.egg/p4a/video/wmv/_player.py
@@ -0,0 +1,35 @@
+from zope import interface
+from zope import component
+from p4a.video import interfaces
+
+from Products.CMFCore import utils as cmfutils
+
+class WMVVideoPlayer(object):
+    interface.implements(interfaces.IMediaPlayer)
+    component.adapts(object)
+    
+    def __init__(self, context):
+        self.context = context
+    
+    def __call__(self, downloadurl, imageurl):
+        contentobj = self.context.context.context
+        url = contentobj.absolute_url()
+        
+        videoobj = interfaces.IVideo(contentobj)
+        
+        width = videoobj.width
+        height = videoobj.height
+        duration = videoobj.duration
+
+        
+        return """
+        <div class="hVlog">
+          <a href="%(url)s" class="hVlogTarget" type="video/x-ms-wmv" onclick="vPIPPlay(this, '', '', ''); return false;">
+              <img src="%(imageurl)s" />
+              </a>
+        <br />
+          <a href="%(url)s" type="video/x-ms-wmv" onclick="vPIPPlay(this, '', '', ''); return false;">
+        Play WindowsMedia version</a>
+        </div>        
+        """ % {'url': url, 
+               'imageurl': imageurl}
