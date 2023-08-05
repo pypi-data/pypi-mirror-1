82e64fbaf884509b2868292abf6889802797b971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package z3c.form/1.2.0/z3c.form-1.2.0.tar.gz

---
+++ packages/z3c.form/z3c.form-1.2.0.tar.gz/z3c.form-1.2.0/setup.py
@@ -0,0 +1,94 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 76038 2007-05-31 04:40:05Z srichter $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+chapters = '\n'.join(
+    [read('src', 'z3c', 'form', name)
+    for name in ('README.txt',
+                 'form.txt',
+                 'subform.txt',
+                 'field.txt',
+                 'button.txt',
+                 'zcml.txt',
+                 'validator.txt',
+                 'widget.txt',
+                 'action.txt',
+                 'value.txt',
+                 'datamanager.txt',
+                 'converter.txt',
+                 'term.txt',
+                 'util.txt')])
+
+setup (
+    name='z3c.form',
+    version='1.2.0',
+    author = "Stephan Richter, Roger Ineichen and the Zope Community",
+    author_email = "zope3-dev@zope.org",
+    description = "An advanced form and widget framework for Zope 3",
+    long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' + chapters
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 form widget",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://svn.zope.org/z3c.form',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = ['zope.testing', 'z3c.coverage', 'z3c.template'],
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.app.pagetemplate',
+        'zope.app.testing',
+        'zope.component',
+        'zope.configuration',
+        'zope.event',
+        'zope.formlib',
+        'zope.i18n',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.lifecycleevent',
+        'zope.location',
+        'zope.pagetemplate',
+        'zope.publisher',
+        'zope.schema',
+        'zope.security',
+        ],
+    dependency_links = ['http://download.zope.org/distribution'],
+    zip_safe = False,
+    )
