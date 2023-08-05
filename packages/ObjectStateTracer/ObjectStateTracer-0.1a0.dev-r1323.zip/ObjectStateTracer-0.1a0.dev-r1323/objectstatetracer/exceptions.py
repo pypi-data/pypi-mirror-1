7a56d6d36d2bf9ba6c573ba381eb5d46bc1782c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package ObjectStateTracer/0.1a0.dev-r1323/ObjectStateTracer-0.1a0.dev-r1323.zip

---
+++ packages/ObjectStateTracer/ObjectStateTracer-0.1a0.dev-r1323.zip/ObjectStateTracer-0.1a0.dev-r1323/objectstatetracer/exceptions.py
@@ -0,0 +1,12 @@
+class PendingChange(Exception):
+    pass
+
+class PermissionError(Exception):
+    pass
+
+class PendingCreation(Exception):
+    pass
+
+class ClassAlreadyRegistered(Exception):
+    pass
+
