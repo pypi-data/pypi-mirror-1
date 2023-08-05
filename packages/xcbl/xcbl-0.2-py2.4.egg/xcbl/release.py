3315c55a59acc8393988540bcb30358d4e4932f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package xcbl/0.2/xcbl-0.2-py2.4.egg

---
+++ packages/xcbl/xcbl-0.2-py2.4.egg/xcbl/release.py
@@ -0,0 +1,17 @@
+# Release information about xcbl
+
+version = "0.2"
+
+description = "Extensible CheckBoxList widget for TurboGears"
+long_description = """List ends with an 'Other:' element that can be filled in by hand.
+Any number of new options can be specified.
+MochiKit 1.4 required for optimal appearance."""
+author = "Catherine Devlin"
+maintainer = author
+email = "catherine.devlin@gmail.com"
+# copyright = "Vintage 2006 - a good year indeed"
+
+# if it's open source, you might want to specify these
+url = "http://catherine.devlin.googlepages.com/xcbl.html"
+download_url = "http://catherine.devlin.googlepages.com/xcbl.egg"
+license = "MIT"
