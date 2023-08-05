85602da584532ff782c3d519361667cd34b424da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:59 +0000

    Add package Behaviour/0.1a4/Behaviour-0.1a4-py2.5.egg

---
+++ packages/Behaviour/Behaviour-0.1a4-py2.5.egg/src/specnamer.py
@@ -0,0 +1,144 @@
+﻿#!/usr/bin/env python
+# coding=utf-8
+"""
+
+
+"""
+__docformat__ = "restructuredtext en"
+
+import sys
+import dequeue
+
+
+class EmptyPhraseError( Exception ):
+	pass
+
+
+class SpecNamer():
+	"""
+	Converts a behaviour name from lower_case_with_underscores or from CamelCase
+	to a Capitalised english phrase.
+
+	Handles acronyms and numerics embedded in the words.
+
+	"""
+
+	def __init__( self, name ):
+		"""
+		Stores the name and converts it to a phrase for retrieve when required.
+		"""
+
+		self.name = name
+		self.phrase = self._nameToPhrase( self.name )
+
+
+	def _nameToPhrase( self, name ):
+		"""
+		Convert the name to a phrase by breaking it into chunks (separated by
+		underscores), processing each chunk, adding it to the result, and changing
+		the first letter to uppercase.
+
+		Parameters
+		----------
+
+		name : *string*
+			The name to convert into an English phrase.
+
+		Returns
+		-------
+
+		string
+			The phrase.
+
+		"""
+
+		def _processChunk( dq ):
+			"""
+			Pops the first chunk off the dq. It it is all one case, returns it,
+			otherwise it breaks it into smaller chunks, pushing them back onto
+			the dq, and returning just the first small chunk.
+			"""
+
+			def _lower( char ):
+				return char == char.lower()
+
+			def _upper( char ):
+				return char == char.upper()
+
+			def _numeric( char ):
+				return char in ( '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' )
+
+			chunk = dq.pop()
+			if chunk == chunk.upper() or chunk == chunk.lower():
+				return chunk
+
+			while chunk:
+				hasLower = False
+				token = ''
+				while chunk and _numeric( chunk[-1] ):
+					token = chunk[-1] + token
+					chunk = chunk[:-1]
+				while chunk and ( _lower( chunk[-1] ) or _numeric( chunk[-1] ) ):
+					token = chunk[-1] + token
+					chunk = chunk[:-1]
+					hasLower = True
+				if chunk and _upper( chunk[-1] ):
+					if hasLower:
+						token = chunk[-1].lower() + token
+						chunk = chunk[:-1]
+					else:
+						while chunk and ( _upper( chunk[-1] ) or _numeric( chunk[-1] ) ):
+							token = chunk[-1] + token
+							chunk = chunk[:-1]
+				dq.push( token )
+
+			return dq.pop()
+
+		chunks = name.split( "_" )
+		dq = dequeue.DeQueue( chunks )
+		chunk = chunks[0]
+		phrase = _processChunk( dq )
+
+		while not dq.isEmpty():
+			phrase = phrase + ' ' + _processChunk( dq )
+
+		phrase = self._stripPrefixAndSuffix( phrase )
+
+		return phrase[0].upper() + phrase[1:]
+
+
+	def _stripPrefixAndSuffix( self, phrase ):
+		"""
+		Strips the words test, spec, and specificaton from the ends of the name.
+
+		Parameters
+		----------
+
+		phrase : *string*
+			The phrase to remove the prefixes and suffixes from.
+
+		Returns
+		-------
+
+		string
+			The phrase with the prefixes and suffixes removed.
+
+		Raises
+		------
+
+		EmptyPhrase
+			The phrase with the prefixes and suffixes removed is empty.
+
+		"""
+
+		words = phrase.split( " " )
+		if words[0] in ( "test", "spec", "specification" ):
+			words = words[1:]
+		if words[-1] in ( "test", "spec", "specification" ):
+			words = words[:-1]
+		if not words:
+			raise EmptyPhraseError, """Phrase '%s' consists of prefixes and suffixes only.""" % phrase
+		phrase = words[0]
+		for word in words[1:]:
+			phrase = phrase + ' ' + word
+		return phrase
---
+++ packages/Behaviour/Behaviour-0.1a4-py2.5.egg/test/test_specnamer.py
@@ -0,0 +1,118 @@
+﻿#!/usr/bin/env python
+# coding=utf-8
+'''
+Unit tests for specnamer.py
+'''
+__docformat__ = "restructuredtext en"
+
+import sys
+import unittest
+
+import specnamer
+	
+
+class TestStripPrefixAndSuffix( unittest.TestCase ):
+	"""
+	Tests the SpecNamer class
+	"""
+
+	def test_ShouldStripLeadingUnderscoredTest( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "test_trial" ).phrase, "Trial" )
+
+	def test_ShouldStripLeadingCamelCaseTest( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "TestTrial" ).phrase, "Trial" )
+
+	def test_ShouldStripTrailingUnderscoredTest( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "trial_test" ).phrase, "Trial" )
+
+	def test_ShouldStripTrailingCamelCaseTest( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "TrialTest" ).phrase, "Trial" )
+
+	def test_ShouldStripLeadingUnderscoredSpec( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "spec_trial" ).phrase, "Trial" )
+
+	def test_ShouldStripLeadingCamelCaseSpec( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "SpecTrial" ).phrase, "Trial" )
+
+	def test_ShouldStripTrailingUnderscoredSpec( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "trial_spec" ).phrase, "Trial" )
+
+	def test_ShouldStripTrailingCamelCaseSpec( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "TrialSpec" ).phrase, "Trial" )
+
+	def test_ShouldStripLeadingUnderscoredSpecification( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "specification_trial" ).phrase, "Trial" )
+
+	def test_ShouldStripLeadingCamelCaseSpecification( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "SpecificationTrial" ).phrase, "Trial" )
+
+	def test_ShouldStripTrailingUnderscoredSpecification( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "trial_specification" ).phrase, "Trial" )
+
+	def test_ShouldStripTrailingCamelCaseSpecification( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "TrialSpecification" ).phrase, "Trial" )
+
+	def test_ShouldStripBothEndsWhenAffixes( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "test_trial_spec" ).phrase, "Trial" )
+
+	def test_ShouldRaiseEmptyPhraseErrorWhenStrippedToEmpty( self ):
+		self.failUnlessRaises( specnamer.EmptyPhraseError, specnamer.SpecNamer, "test_spec" )
+	
+
+class TestNametoPhrase( unittest.TestCase ):
+	"""
+	Tests the SpecNamer class
+	"""
+
+	def test_ShouldReturnCapitalCaseFromCapitalCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "Word" ).phrase, "Word" )
+
+	def test_ShouldReturnCapitalCaseFromLowerCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "word" ).phrase, "Word" )
+
+	def test_ShouldReturnUpperCaseFromUpperCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "ASCII" ).phrase, "ASCII" )
+
+	def test_ShouldReturnCapitalCasePlusLowerCaseFromCapitalCasePlusCapitalCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "TwoWords" ).phrase, "Two words" )
+
+	def test_ShouldReturnCapitalCasePlusLowerCaseFromLowerCasePlusLowerCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "two_words" ).phrase, "Two words" )
+
+	def test_ShouldReturnCapitalCasePlusUpperCaseFromCapitalCasePlusUpperCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "OnlyASCII" ).phrase, "Only ASCII" )
+
+	def test_ShouldReturnUpperCasePlusUpperCaseFromLowerCasePlusUpperCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "only_ASCII" ).phrase, "Only ASCII" )
+
+	def test_ShouldReturnUpperCasePlusLowerCaseFromUpperCasePlusCapitalCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "ASCIIOnly" ).phrase, "ASCII only" )
+
+	def test_ShouldReturnUpperCasePlusLowerCaseFromUpperCasePlusLowerCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "ASCII_only" ).phrase, "ASCII only" )
+
+	def test_ShouldReturnTwoUpperCasesFromTwoUpperCases( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "ASCII_ANSI" ).phrase, "ASCII ANSI" )
+
+	def test_ShouldReturnUpperCaseWithEmbeddedNosFromUpperCaseWithEmbeddedNos( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "H2O" ).phrase, "H2O" )
+
+	def test_ShouldReturnCapitalCasePlusUpperCaseTrailingNosFromCapitalCasePlusUpperCaseTrailingNos( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "FormUB40" ).phrase, "Form UB40" )
+
+	def test_ShouldReturnUpperCaseTrailingNosPlusLowerCaseFromUpperCaseTrailingNosPlusCapitalCase( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "HAL9000Computer" ).phrase, "HAL9000 computer" )
+
+	def test_ShouldReturnCapitalCasePlusUpperCaseLeadingNosFromCapitalCasePlusUpperCaseLeadingNos( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "Learn123ABC" ).phrase, "Learn 123ABC" )
+
+	def test_ShouldReturnManyWordsFromManyWords( self ):
+		self.failUnlessEqual( specnamer.SpecNamer( "one_two_ThreeFOURFive_six7eight_nine" ).phrase, "One two three FOUR five six7eight nine" )
+
+
+if __name__ == '__main__':
+	suite = []
+	suite += unittest.TestLoader().loadTestsFromTestCase( TestStripPrefixAndSuffix )
+	suite += unittest.TestLoader().loadTestsFromTestCase( TestNametoPhrase )
+	alltests = unittest.TestSuite( suite )
+	unittest.TextTestRunner( verbosity=2 ).run( alltests )
