3b9f37bc396bf2e33a595c70b8886609db561663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:33 +0000

    Add package Detextile/0.0.2/Detextile-0.0.2.tar.gz

---
+++ packages/Detextile/Detextile-0.0.2.tar.gz/Detextile-0.0.2/setup.py
@@ -0,0 +1,21 @@
+"""Convert HTML to Textile syntax using BeautifulSoup."""
+
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+doclines = __doc__.split("\n")
+
+setup(
+      name = "Detextile",
+      version = "0.0.2",
+      packages = find_packages(),
+
+      author = "Roberto De Almeida",
+      author_email = "roberto@dealmeida.net",
+      description = doclines[0],
+      #long_description = "\n".join(doclines[2:]),
+      license = "MIT",
+      keywords = "textile html",
+      #url = "http://example.com/HelloWorld/", 
+     )
