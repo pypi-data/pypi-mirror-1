5841d2ef509797a9418f72f15ff11f56ec4829d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:57 +0000

    Add package collective.portlet.tal/1.0rc1/collective.portlet.tal-1.0rc1.tar.gz

---
+++ packages/collective.portlet.tal/collective.portlet.tal-1.0rc1.tar.gz/collective.portlet.tal-1.0rc1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '1.0rc1'
+
+setup(name='collective.portlet.tal',
+      version=version,
+      description="TAL portlet for Plone 3",
+      long_description="""\
+This portlet allows you to enter TAL into a text area, which is then executed 
+as if it came from a page template""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone portlet tal',
+      author='Martin Aspeli',
+      author_email='optilude@gmx.net',
+      url='http://plone.org',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
