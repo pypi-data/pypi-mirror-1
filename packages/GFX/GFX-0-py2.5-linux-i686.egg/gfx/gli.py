6a867928e4e8420e1ee2abf1b1dd3e15ef3be8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package GFX/0/GFX-0-py2.5-linux-i686.egg

---
+++ packages/GFX/GFX-0-py2.5-linux-i686.egg/gfx/gli.py
@@ -0,0 +1,22 @@
+def load_image(data):
+    """
+    Returns OpenGL texture from data.
+    """
+    pass
+
+def transform(translate, angle=0.0, scale=(1.0,1.0)):
+    pass
+
+def draw_quad(vertices, texture_coords=(0.0,0.0,0.0,1.0,1.0,1.0,1.0,0.0), texture_id=0, rgba=(1.0,1.0,1.0,1.0)):
+    """
+    Draws a quad.
+    """
+
+def draw_line(points, rgba=(1.0,1.0,1.0,1.0)):
+    pass
+
+def set_viewport(bottom, left, top, bottom):
+    pass
+
+def clear(rgba=(0.0,0.0,0.0,1.0)):
+    pass
