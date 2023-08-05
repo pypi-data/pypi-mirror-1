aedea82aeaa739b6a5893d1f04c846496afaf673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:33:00 +0000

    Add package Mako/0.1.2/Mako-0.1.2.tar.gz

---
+++ packages/Mako/Mako-0.1.2.tar.gz/Mako-0.1.2/test/call.py
@@ -0,0 +1,257 @@
+from mako.template import Template
+from mako import util
+import unittest
+from util import result_lines
+
+class CallTest(unittest.TestCase):
+    def test_call(self):
+        t = Template("""
+        <%def name="foo()">
+            hi im foo ${caller.body(y=5)}
+        </%def>
+        
+        <%call expr="foo()" args="y, **kwargs">
+            this is the body, y is ${y}
+        </%call>
+""")
+        assert result_lines(t.render()) == ['hi im foo', 'this is the body, y is 5']
+
+
+    def test_compound_call(self):
+        t = Template("""
+
+        <%def name="bar()">
+            this is bar
+        </%def>
+        
+        <%def name="comp1()">
+            this comp1 should not be called
+        </%def>
+        
+        <%def name="foo()">
+            foo calling comp1: ${caller.comp1(x=5)}
+            foo calling body: ${caller.body()}
+        </%def>
+        
+        <%call expr="foo()">
+            <%def name="comp1(x)">
+                this is comp1, ${x}
+            </%def>
+            this is the body, ${comp1(6)}
+        </%call>
+        ${bar()}
+
+""")
+        assert result_lines(t.render()) == ['foo calling comp1:', 'this is comp1, 5', 'foo calling body:', 'this is the body,', 'this is comp1, 6', 'this is bar']
+
+    def test_compound_call_revset(self):
+        # monkeypatch Set to return items in reverse
+        oldset = util.Set
+        class goofyset(oldset):
+            def __iter__(self):
+                x = list(oldset.__iter__(self))
+                x.reverse()
+                return iter(x)
+        util.Set = goofyset
+        
+        try:
+            self.test_compound_call()
+        finally:
+            util.Set = oldset
+            
+    def test_chained_call(self):
+        """test %calls that are chained through their targets"""
+        t = Template("""
+            <%def name="a()">
+                this is a. 
+                <%call expr="b()">
+                    this is a's ccall.  heres my body: ${caller.body()}
+                </%call>
+            </%def>
+            <%def name="b()">
+                this is b.  heres  my body: ${caller.body()}
+                whats in the body's caller's body ?
+                ${context.caller_stack[-2].body()}
+            </%def>
+            
+            <%call expr="a()">
+                heres the main templ call
+            </%call>
+            
+""")
+        assert result_lines(t.render()) == [
+            'this is a.',
+            'this is b. heres my body:',
+            "this is a's ccall. heres my body:",
+            'heres the main templ call',
+            "whats in the body's caller's body ?",
+            'heres the main templ call'
+        ]
+
+    def test_nested_call(self):
+        """test %calls that are nested inside each other"""
+        t = Template("""
+            <%def name="foo()">
+                ${caller.body(x=10)}
+            </%def>
+
+            x is ${x}
+            <%def name="bar()">
+                bar: ${caller.body()}
+            </%def>
+
+            <%call expr="foo()" args="x">
+                this is foo body: ${x}
+
+                <%call expr="bar()">
+                    this is bar body: ${x}
+                </%call>
+            </%call>
+""")
+        assert result_lines(t.render(x=5)) == [
+            "x is 5",
+            "this is foo body: 10",
+            "bar:",
+            "this is bar body: 10"
+        ]
+        
+    def test_nested_call_2(self):
+        t = Template("""
+            x is ${x}
+            <%def name="foo()">
+                ${caller.foosub(x=10)}
+            </%def>
+
+            <%def name="bar()">
+                bar: ${caller.barsub()}
+            </%def>
+
+            <%call expr="foo()">
+                <%def name="foosub(x)">
+                this is foo body: ${x}
+                
+                <%call expr="bar()">
+                    <%def name="barsub()">
+                    this is bar body: ${x}
+                    </%def>
+                </%call>
+                
+                </%def>
+
+            </%call>
+""")
+        assert result_lines(t.render(x=5)) == [
+            "x is 5",
+            "this is foo body: 10",
+            "bar:",
+            "this is bar body: 10"
+        ]
+        
+    def test_chained_call_in_nested(self):
+        t = Template("""
+            <%def name="embedded()">
+            <%def name="a()">
+                this is a. 
+                <%call expr="b()">
+                    this is a's ccall.  heres my body: ${caller.body()}
+                </%call>
+            </%def>
+            <%def name="b()">
+                this is b.  heres  my body: ${caller.body()}
+                whats in the body's caller's body ? ${context.caller_stack[-2].body()}
+            </%def>
+
+            <%call expr="a()">
+                heres the main templ call
+            </%call>
+            </%def>
+            ${embedded()}
+""")
+        assert result_lines(t.render()) == [
+            'this is a.',
+            'this is b. heres my body:',
+            "this is a's ccall. heres my body:",
+            'heres the main templ call',
+            "whats in the body's caller's body ?",
+            'heres the main templ call'
+        ]
+        
+    def test_call_in_nested(self):
+        t = Template("""
+            <%def name="a()">
+                this is a ${b()}
+                <%def name="b()">
+                    this is b
+                    <%call expr="c()">
+                        this is the body in b's call
+                    </%call>
+                </%def>
+                <%def name="c()">
+                    this is c: ${caller.body()}
+                </%def>
+            </%def>
+        ${a()}
+""")
+        assert result_lines(t.render()) == ['this is a', 'this is b', 'this is c:', "this is the body in b's call"]
+
+        
+    def test_call_in_nested_2(self):
+        t = Template("""
+            <%def name="a()">
+                <%def name="d()">
+                    not this d
+                </%def>
+                this is a ${b()}
+                <%def name="b()">
+                    <%def name="d()">
+                        not this d either
+                    </%def>
+                    this is b
+                    <%call expr="c()">
+                        <%def name="d()">
+                            this is d
+                        </%def>
+                        this is the body in b's call
+                    </%call>
+                </%def>
+                <%def name="c()">
+                    this is c: ${caller.body()}
+                    the embedded "d" is: ${caller.d()}
+                </%def>
+            </%def>
+        ${a()}
+""")
+        assert result_lines(t.render()) == ['this is a', 'this is b', 'this is c:', "this is the body in b's call", 'the embedded "d" is:', 'this is d']
+
+class SelfCacheTest(unittest.TestCase):
+    def test_basic(self):
+        t = Template("""
+        <%!
+            cached = None
+        %>
+        <%def name="foo()">
+            <% 
+                global cached
+                if cached:
+                    return "cached: " + cached
+                context.push_buffer()
+            %>
+            this is foo
+            <%
+                buf = context.pop_buffer()
+                cached = buf.getvalue()
+                return cached
+            %>
+        </%def>
+        
+        ${foo()}
+        ${foo()}
+""")
+        assert result_lines(t.render()) == [
+            "this is foo",
+            "cached:",
+            "this is foo"
+        ]
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/Mako/Mako-0.1.2.tar.gz/Mako-0.1.2/test/template.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+
+from mako.template import Template
+from mako.lookup import TemplateLookup
+import unittest, re, os
+from util import flatten_result, result_lines
+
+if not os.access('./test_htdocs', os.F_OK):
+    os.mkdir('./test_htdocs')
+if not os.access('./test_htdocs/subdir', os.F_OK):
+    os.mkdir('./test_htdocs/subdir')
+file('./test_htdocs/unicode.html', 'w').write("""# -*- coding: utf-8 -*-
+Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »""")
+    
+class EncodingTest(unittest.TestCase):
+    def test_unicode(self):
+        template = Template(u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »""")
+        assert template.render_unicode() == u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+        
+    def test_unicode_arg(self):
+        val = u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+        template = Template("${val}")
+        assert template.render_unicode(val=val) == u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+
+    def test_unicode_file(self):
+        template = Template(filename='./test_htdocs/unicode.html', module_directory='./test_htdocs')
+        assert template.render_unicode() == u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+
+    def test_unicode_memory(self):
+        val = u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+        val = "# -*- coding: utf-8 -*-\n" + val.encode('utf-8')
+        template = Template(val)
+        #print template.code
+        assert template.render_unicode() == u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+    
+    def test_unicode_text(self):
+        val = u"""<%text>Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »</%text>"""
+        val = "# -*- coding: utf-8 -*-\n" + val.encode('utf-8')
+        template = Template(val)
+        print template.code
+        assert template.render_unicode() == u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+
+    def test_unicode_text_ccall(self):
+        val = u"""
+        <%def name="foo()">
+            ${capture(caller.body)}
+        </%def>
+        <%call expr="foo()">
+        <%text>Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »</%text>
+        </%call>"""
+        val = "# -*- coding: utf-8 -*-\n" + val.encode('utf-8')
+        template = Template(val)
+        #print template.code
+        assert flatten_result(template.render_unicode()) == u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+        
+    def test_unicode_literal_in_expr(self):
+        template = Template(u"""# -*- coding: utf-8 -*-
+        ${u"Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"}
+        """.encode('utf-8'))
+        assert template.render_unicode().strip() == u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+
+    def test_unicode_literal_in_code(self):
+        template = Template(u"""# -*- coding: utf-8 -*-
+        <%
+            context.write(u"Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »")
+        %>
+        """.encode('utf-8'))
+        assert template.render_unicode().strip() == u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+
+    def test_unicode_literal_in_controlline(self):
+        template = Template(u"""# -*- coding: utf-8 -*-
+        <%
+            x = u"drôle de petit voix m’a réveillé."
+        %>
+        % if x==u"drôle de petit voix m’a réveillé.":
+            hi, ${x}
+        % endif
+        """.encode('utf-8'))
+        assert template.render_unicode().strip() == u"""hi, drôle de petit voix m’a réveillé."""
+    
+        
+    def test_input_encoding(self):
+        """test the 'input_encoding' flag on Template, and that unicode objects arent double-decoded"""
+        s2 = u"hello ${f(u'śląsk')}"
+        res = Template(s2, input_encoding='utf-8').render_unicode(f=lambda x:x)
+        assert res == u"hello śląsk"
+
+        s2 = u"# -*- coding: utf-8 -*-\nhello ${f(u'śląsk')}"
+        res = Template(s2).render_unicode(f=lambda x:x)
+        assert res == u"hello śląsk"
+
+    def test_raw_strings(self):
+        """test that raw strings go straight thru with default_filters turned off"""
+        g = 'śląsk'
+        s = u"# -*- coding: utf-8 -*-\nhello ${x}"
+        t = Template(s, default_filters=[])
+        y = t.render(x=g)
+        assert y == "hello śląsk"
+
+        # now, the way you *should* be doing it....
+        q = g.decode('utf-8')
+        y = t.render_unicode(x=q)
+        assert y == u"hello śląsk"
+        
+    def test_encoding(self):
+        val = u"""Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petit voix m’a réveillé. Elle disait: « S’il vous plaît… dessine-moi un mouton! »"""
+        template = Template(val, output_encoding='utf-8')
+        assert template.render() == val.encode('utf-8')
+    
+    def test_read_unicode(self):
+        lookup = TemplateLookup(directories=['./test_htdocs'], filesystem_checks=True, output_encoding='utf-8')
+        template = lookup.get_template('/read_unicode.html')
+        data = template.render(path=os.path.join('./test_htdocs', 'internationalization.html'))
+        
+
+class PageArgsTest(unittest.TestCase):
+    def test_basic(self):
+        template = Template("""
+            <%page args="x, y, z=7"/>
+            
+            this is page, ${x}, ${y}, ${z}
+""")
+
+        assert flatten_result(template.render(x=5, y=10)) == "this is page, 5, 10, 7"
+        assert flatten_result(template.render(x=5, y=10, z=32)) == "this is page, 5, 10, 32"
+        try:
+            template.render(y=10)
+            assert False
+        except TypeError, e:
+            assert True
+
+    def test_with_context(self):
+        template = Template("""
+            <%page args="x, y, z=7"/>
+
+            this is page, ${x}, ${y}, ${z}, ${w}
+""")
+        #print template.code
+        assert flatten_result(template.render(x=5, y=10, w=17)) == "this is page, 5, 10, 7, 17"
+
+    def test_overrides_builtins(self):
+        template = Template("""
+            <%page args="id"/>
+            
+            this is page, id is ${id}
+        """)
+        
+        assert flatten_result(template.render(id="im the id")) == "this is page, id is im the id"
+        
+
+        
+class ControlTest(unittest.TestCase):
+    def test_control(self):
+        t = Template("""
+    # this is a template.
+    % for x in y:
+    %   if x.has_key('test'):
+        yes x has test
+    %   else:
+        no x does not have test
+    %endif
+    %endfor
+""")
+        assert result_lines(t.render(y=[{'test':'one'}, {'foo':'bar'}, {'foo':'bar', 'test':'two'}])) == [
+            "yes x has test",
+            "no x does not have test",
+            "yes x has test"
+        ]
+
+class GlobalsTest(unittest.TestCase):
+    def test_globals(self):
+        t= Template("""
+            <%!
+                y = "hi"
+            %>
+        y is ${y}
+""")
+        assert t.render().strip() == "y is hi"
+
+
+class ModuleDirTest(unittest.TestCase):
+    def test_basic(self):
+        file('./test_htdocs/modtest.html', 'w').write("""this is a test""")
+        file('./test_htdocs/subdir/modtest.html', 'w').write("""this is a test""")
+        t = Template(filename='./test_htdocs/modtest.html', module_directory='./test_htdocs/modules')
+        t2 = Template(filename='./test_htdocs/subdir/modtest.html', module_directory='./test_htdocs/modules')
+        assert t.module.__file__ == os.path.normpath('./test_htdocs/modules/test_htdocs/modtest.html.py')
+        assert t2.module.__file__ == os.path.normpath('./test_htdocs/modules/test_htdocs/subdir/modtest.html.py')
+    def test_callable(self):
+        file('./test_htdocs/modtest.html', 'w').write("""this is a test""")
+        file('./test_htdocs/subdir/modtest.html', 'w').write("""this is a test""")
+        def get_modname(filename, uri):
+            return os.path.dirname(filename) + "/foo/" + os.path.basename(filename) + ".py"
+        lookup = TemplateLookup('./test_htdocs', modulename_callable=get_modname)
+        t = lookup.get_template('/modtest.html')
+        t2 = lookup.get_template('/subdir/modtest.html')
+        assert t.module.__file__ == 'test_htdocs/foo/modtest.html.py'
+        assert t2.module.__file__ == 'test_htdocs/subdir/foo/modtest.html.py'
+        
+            
+if __name__ == '__main__':
+    unittest.main()
