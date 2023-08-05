97ea1f7336fb34d2351f6d62192ae0fe57b47ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package environ/1.0/environ-1.0.tar.gz

---
+++ packages/environ/environ-1.0.tar.gz/environ-1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='environ',
+    version='1.0',
+    description='Python Stack Based Globals Management',
+    author='Rich Harkins',
+    author_email='rich@worldsinfinite.com',
+    py_modules=['environ'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Topic :: Software Development :: Libraries',
+    ],
+    )
+
