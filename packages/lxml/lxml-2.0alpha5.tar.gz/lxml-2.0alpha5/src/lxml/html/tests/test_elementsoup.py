bc4894535dc2e02122507495a6ac7484db2b7072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:16 +0000

    Add package lxml/2.0alpha5/lxml-2.0alpha5.tar.gz

---
+++ packages/lxml/lxml-2.0alpha5.tar.gz/lxml-2.0alpha5/src/lxml/html/tests/test_elementsoup.py
@@ -0,0 +1,18 @@
+import unittest
+from lxml.tests.common_imports import doctest
+
+try:
+    import BeautifulSoup
+    BS_INSTALLED = True
+except ImportError:
+    BS_INSTALLED = False
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    if BS_INSTALLED:
+        suite.addTests([doctest.DocFileSuite('../../../../doc/elementsoup.txt')])
+    return suite
+
+if __name__ == '__main__':
+    unittest.main()
