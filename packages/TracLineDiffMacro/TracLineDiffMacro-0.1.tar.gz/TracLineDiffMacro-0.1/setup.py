b3da29df2530b57623c40df1bd87faf09d01b179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:34 +0000

    Add package TracLineDiffMacro/0.1/TracLineDiffMacro-0.1.tar.gz

---
+++ packages/TracLineDiffMacro/TracLineDiffMacro-0.1.tar.gz/TracLineDiffMacro-0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracLineDiffMacro',
+    version = '0.1',
+    packages = ['linediff'],
+    #package_data={ 'linediff' : [ 'templates/*.cs' , 'htdocs/js/*.js' ] },
+    author = "Noah Kantrowitz",
+    author_email = "coderanger@yahoo.com",
+    description = "A small macro to diff two lines of text.",
+    long_description = "A small macro to diff two lines of text, and render the output nicely.",
+    license = "BSD",
+    keywords = "trac plugin macro diff",
+    url = "http://trac-hacks.org/wiki/",
+
+    entry_points = {
+        'trac.plugins': [
+            'linediff.macro = linediff.macro',
+        ],
+    },
+
+    install_requires = [  ],
+)
