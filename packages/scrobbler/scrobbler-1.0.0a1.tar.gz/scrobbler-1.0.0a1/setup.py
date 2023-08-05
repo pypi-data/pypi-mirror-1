eeb4f71480d5f87d0962506160a96be7f31a8d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package scrobbler/1.0.0a1/scrobbler-1.0.0a1.tar.gz

---
+++ packages/scrobbler/scrobbler-1.0.0a1.tar.gz/scrobbler-1.0.0a1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='scrobbler',
+      version='1.0.0a1',
+      author='Michel Albert',
+      author_email='exhuma@users.sourceforge.net',
+      py_modules=['scrobbler'],
+      url='http://exhuma.wicked.lu/projects/python/scrobbler',
+      license='LGPL',
+      description="Submit tracks to audioscrobbler (last.fm)",
+      )
