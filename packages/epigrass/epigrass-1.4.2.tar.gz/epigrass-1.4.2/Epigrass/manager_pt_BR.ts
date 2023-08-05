2de7c2e9290fb813774e1a10ad5bc65210c4ee49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:38 +0000

    Add package epigrass/1.4.2/epigrass-1.4.2.tar.gz

---
+++ packages/epigrass/epigrass-1.4.2.tar.gz/epigrass-1.4.2/Epigrass/manager_pt_BR.ts
@@ -0,0 +1,371 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>MainPanel</name>
+    <message>
+        <source>Epigrass Control Panel</source>
+        <translation>Painel de controle do Epigrass</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Ajuda</translation>
+    </message>
+    <message>
+        <source>F1</source>
+        <translation>F1</translation>
+    </message>
+    <message>
+        <source>Click here to open the userguide in the web browser</source>
+        <translation>Clique aqui para abrir o guia do usuário no navegador</translation>
+    </message>
+    <message>
+        <source>&amp;Run</source>
+        <translation>&amp;Iniciar</translation>
+    </message>
+    <message>
+        <source>Alt+R</source>
+        <translation>Alt+I</translation>
+    </message>
+    <message>
+        <source>Click here to start your simulation</source>
+        <translation>Clique aqui para iniciar a simulação</translation>
+    </message>
+    <message>
+        <source>&amp;Exit</source>
+        <translation>&amp;Sair</translation>
+    </message>
+    <message>
+        <source>Alt+E</source>
+        <translation>Alt+S</translation>
+    </message>
+    <message>
+        <source>Click here to leave Epigrass</source>
+        <translation>Clique aqui para sair do EpiGrass</translation>
+    </message>
+    <message>
+        <source>Model Specification</source>
+        <translation>Especificação do Modelo</translation>
+    </message>
+    <message>
+        <source>Script Name:</source>
+        <translation>Nome do Script:</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <source>write the name of the your script or press the choose button on the right to select one.</source>
+        <translation>Escreva o nome do seu script ou pressione o botão escolher à direita para selecionar um.</translation>
+    </message>
+    <message>
+        <source>Choose</source>
+        <translation>Escolher</translation>
+    </message>
+    <message>
+        <source>Database Specification</source>
+        <translation>Especificação do Banco de Dados</translation>
+    </message>
+    <message>
+        <source>Userid</source>
+        <translation>Usuário</translation>
+    </message>
+    <message>
+        <source>This is the userid for accessing the database server</source>
+        <translation>Este é o usuário usado para acessar o banco de dados</translation>
+    </message>
+    <message>
+        <source>localhost</source>
+        <translation>localhost</translation>
+    </message>
+    <message>
+        <source>This is the url of your database server.</source>
+        <translation>Esta é a url do seu servidor de banco de dados.</translation>
+    </message>
+    <message>
+        <source>Host</source>
+        <translation>Servidor</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Senha</translation>
+    </message>
+    <message>
+        <source>Database password for the userid entered</source>
+        <translation>Senha do banco de dados para o usuário indicado</translation>
+    </message>
+    <message>
+        <source>Port</source>
+        <translation>Porta</translation>
+    </message>
+    <message>
+        <source>3306</source>
+        <translation>3306</translation>
+    </message>
+    <message>
+        <source>Enter the port  the server listens to.</source>
+        <translation>Entre a porta à qual servidor responde.</translation>
+    </message>
+    <message>
+        <source>Run Options</source>
+        <translation>Opções de Simulação</translation>
+    </message>
+    <message>
+        <source>Editor</source>
+        <translation>Editor</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Enter your full name. This will be added to the report.</source>
+        <translation>Escreva o seu nome completo. Ele será usado no relatório.</translation>
+    </message>
+    <message>
+        <source>Enter your preferred text editor</source>
+        <translation>Indique seu editor de texto preferido</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Configurações</translation>
+    </message>
+    <message>
+        <source>Visualize Graph</source>
+        <translation>Visualizar Grafo</translation>
+    </message>
+    <message>
+        <source>Check this box if you have vpython intalled</source>
+        <translation>Marque esta opção se o VPython estiver instalado</translation>
+    </message>
+    <message>
+        <source>Database</source>
+        <translation>Banco de dados</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Informação</translation>
+    </message>
+    <message>
+        <source>Click here for a short description of the epigrass database</source>
+        <translation>Clique aqui para uma breve descrição do banco epigrass</translation>
+    </message>
+    <message>
+        <source>Backup</source>
+        <translation>Backup</translation>
+    </message>
+    <message>
+        <source>Click here to backup the epigrass database </source>
+        <translation>Clique aqui para fazer backup do banco epigrass</translation>
+    </message>
+    <message>
+        <source>Utilities</source>
+        <translation>Utilitários</translation>
+    </message>
+    <message>
+        <source>PDF Viewer</source>
+        <translation>Leitor de PDFs</translation>
+    </message>
+    <message>
+        <source>Enter the name of your preferred PDF viewer</source>
+        <translation>Entre com o nome do seu visualizador de PDFs preferencial</translation>
+    </message>
+    <message>
+        <source>Simulation Status</source>
+        <translation>Status da Simulação</translation>
+    </message>
+    <message>
+        <source>Report</source>
+        <translation>Relatório</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Abrir</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Linguagem</translation>
+    </message>
+    <message>
+        <source>english</source>
+        <translation>english</translation>
+    </message>
+    <message>
+        <source>Brazilian portuguese</source>
+        <translation>Brazilian portuguese</translation>
+    </message>
+    <message>
+        <source>Select the language for the GUI</source>
+        <translation>Selecione a linguagem da interface gráfica</translation>
+    </message>
+    <message>
+        <source>Visualization tools</source>
+        <translation>Ferramentas de Visualização</translation>
+    </message>
+    <message>
+        <source>Visualization</source>
+        <translation>Visualização</translation>
+    </message>
+    <message>
+        <source>Simulation step</source>
+        <translation>Passo da simulação</translation>
+    </message>
+    <message>
+        <source>Progress:</source>
+        <translation>Progresso:</translation>
+    </message>
+    <message>
+        <source>Start animation</source>
+        <translation>Animar</translation>
+    </message>
+    <message>
+        <source>Simulations stored:</source>
+        <translation>Simulações armazenadas:</translation>
+    </message>
+    <message>
+        <source>Select a database stored simulation</source>
+        <translation>Selecione uma simulação armazenada no banco de dados</translation>
+    </message>
+    <message>
+        <source>No map</source>
+        <translation>Nenhum mapa</translation>
+    </message>
+    <message>
+        <source>Select a map</source>
+        <translation>Selecione um mapa</translation>
+    </message>
+    <message>
+        <source>Scan DB</source>
+        <translation>Ler BD</translation>
+    </message>
+    <message>
+        <source>Animation
+rate</source>
+        <translation>Taxa de 
+animação</translation>
+    </message>
+    <message>
+        <source>Maps available:</source>
+        <translation>Mapas disponíveis:</translation>
+    </message>
+    <message>
+        <source>Spread Trees</source>
+        <translation>Árvores de espalhamento</translation>
+    </message>
+    <message>
+        <source>Consensus Tree</source>
+        <translation>Árvores de Consenso</translation>
+    </message>
+    <message>
+        <source>Select a directory with tree-files to build consensus on.</source>
+        <translation>Selecione um diretório com arquivos de árvore para ser utilizados no consenso.</translation>
+    </message>
+    <message>
+        <source>Variable to display:</source>
+        <translation>Variável a ser mostrada:</translation>
+    </message>
+    <message>
+        <source>Select a variable to display in the animation</source>
+        <translation>Selecione uma variável para a animação</translation>
+    </message>
+</context>
+<context>
+    <name>MainPanel_Impl</name>
+    <message>
+        <source>Syntax Error</source>
+        <translation>Erro de Sintaxe</translation>
+    </message>
+    <message>
+        <source>Incomplete Configuration</source>
+        <translation>Configuração Incompleta</translation>
+    </message>
+    <message>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <source>Editor or Script not selected</source>
+        <translation>Editor ou Script não selecionados</translation>
+    </message>
+    <message>
+        <source>Please make sure you have selected both
+an editor and your model&apos;s script.</source>
+        <translation>Por favor certifique-se de haver selecionado
+um editor e o script do modelo.</translation>
+    </message>
+    <message>
+        <source>*.epg</source>
+        <translation>*.epg</translation>
+    </message>
+    <message>
+        <source>Select your Model&apos;s Script</source>
+        <translation>Selecione o script do seu modelo</translation>
+    </message>
+    <message>
+        <source>Missing database password</source>
+        <translation>Falta a senha do banco de dados</translation>
+    </message>
+    <message>
+        <source>Please enter password for the Mysql Database.</source>
+        <translation>Por favor, entre com a senha do banco de dados MySQL.</translation>
+    </message>
+    <message>
+        <source>Restore Instructions:</source>
+        <translation>Instruções de Recuperação:</translation>
+    </message>
+    <message>
+        <source>Help not available</source>
+        <translation>Ajuda não disponível</translation>
+    </message>
+    <message>
+        <source>No Report</source>
+        <translation>Relatório não disponível</translation>
+    </message>
+    <message>
+        <source>Could not open the report.
+Make sure you have generated it.</source>
+        <translation>Não foi possível abrir o relatório.
+Certifique-se de tê-lo gerado.</translation>
+    </message>
+    <message>
+        <source>Non-local Database Server</source>
+        <translation>Banco de Dados Remoto</translation>
+    </message>
+    <message>
+        <source>The epigrass database has been Backed up
+to a file named &quot;epigrass.sql&quot;.
+This file is in you current working directory.
+To restore the database type:
+epigrass.sql &gt; mysql</source>
+        <translation>O banco de dados epigrass foi arquivado
+no arquivo &quot;epigrass.sql&quot;
+Este arquivo está em seu diretório de trabalho atual.
+Para restaurar o banco de dados digite:
+epigrass.sql &gt; mysql
+</translation>
+    </message>
+    <message>
+        <source>Currently, the database backup function only
+                works on local database servers.</source>
+        <translation>Atualmente, a função de backup do banco de dados somente funciona em
+                bancos de dados locais.</translation>
+    </message>
+    <message>
+        <source>Could not open user guide.
+Please make sure your chosen PDF viewer is installed.</source>
+        <translation>Não foi possível abrir o guia do usuário.
+Por favor verifique se o leitor de PDF escolhido está instalado.</translation>
+    </message>
+    <message>
+        <source>Select Directory with Tree Files (epipath*.csv)</source>
+        <translation>Selecione Diretório com Arquivos de árvores (epipath*.csv)</translation>
+    </message>
+    <message>
+        <source>Branch Support</source>
+        <translation>Suporte do ramo</translation>
+    </message>
+    <message>
+        <source>Enter minimum branch support level to display:</source>
+        <translation>Enter com o nível de suporte mínimo a ser mostrado:</translation>
+    </message>
+</context>
+</TS>
---
+++ packages/epigrass/epigrass-1.4.2.tar.gz/epigrass-1.4.2/setup.py
@@ -0,0 +1,49 @@
+# -*- coding:utf8 -*-
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+#from distutils.core import setup 
+import os 
+
+print """If this is your first installation please edit setup.py
+as documented in it, and re-run the installation"""
+# Edit the lines below if you already have a MySQL server installed 
+# with root user and password defined.
+sqluser = 'root'
+sqlpass = ''
+host = 'localhost'
+# Is this installation an upgrade?  1 if yes, 0 if no.
+print 'If this installation is not an upgrade to a previous one,\nand you plan to use the MySQL database backend,\nPlease edit setup.py and set upgrade=0'
+upgrade = 1
+#===========================================================================
+if not upgrade:
+    import MySQLdb
+    con = None
+    try:
+        os.system('mysqladmin -u %s create epigrass -p%s'%(sqluser,sqlpass))
+        con = MySQLdb.connect(host=host, port=3306, user=sqluser,passwd=sqlpass, db='epigrass')
+        Cursor = con.cursor()
+        Cursor.execute("GRANT ALL ON epigrass.* TO 'epigrass'@'localhost'IDENTIFIED BY 'epigrass';")
+        print Cursor.fetchall()
+    finally:
+        if con:
+            con.close()
+
+setup(name='epigrass',
+        version='1.4.2',
+        author = 'Flavio Codeco Coelho, Claudia Torres Codeco',
+        author_email = 'fccoelho@fiocruz.br',
+	maintainer = 'Flavio Codeco Coelho',
+	maintainer_email = 'fccoelho@fiocruz.br',
+	url = 'http://epigrass.sourceforge.net',
+	description = 'Epidemiological Geo-Referenced Analysis and Simulation System',
+	long_description = 'EpiGrass is a simulator of epidemics over networks.  Its is a scientific tool created for simulations and scenario analysis in Network epidemiology.',
+	download_url = 'http://sourceforge.net/project/showfiles.php?group_id=128000',
+        license = 'GPL',
+	install_requires=['SQLObject>=0.7','matplotlib>=0.87','pysqlite>=2.2'],
+        packages = ['Epigrass'],
+        scripts = ['epigrass.py'],
+        data_files = [('share/epigrass/demos',['demos/script.epg', 'demos/mesh.epg', 'demos/star.epg','demos/flu.epg','demos/sitios3.csv','demos/edgesout.csv', 'demos/nodes.csv','demos/mesh.csv','demos/star.csv','demos/limites.map']),
+                    ('share/epigrass/docs',['docs/userguide.pdf'])
+                    ]
+        )
