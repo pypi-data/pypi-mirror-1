890eddf6d3bc945d26104cabaabff2aec7d793cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:48 +0000

    Add package DisplayShelf/1.0/DisplayShelf-1.0.tar.gz

---
+++ packages/DisplayShelf/DisplayShelf-1.0.tar.gz/DisplayShelf-1.0/displayshelf/release.py
@@ -0,0 +1,19 @@
+# Release information about DisplayShelf
+
+version = "1.0"
+
+description = "The Flex Display Shelf"
+long_description = '''The Display Shelf renders images in a fancy shelf similar to the iTunes 7 album art display.  One image is in front with a flat perspective, while the other images are slanted to the sides of the selected image.
+The Display Shelf was written by Ely Greenfield, licensed BSD, and built with the free Flex SDK.  The Display Shelf is compiled to Flash byte code and requires the client to have Flash 9.
+
+You can find the source code and more information about the Display Shelf at:
+http://www.quietlyscheming.com/blog/components/tutorial-displayshelf-component/
+'''
+author = "James Ward and Kevin Dangoor"
+email = "james@jamesward.org kid@blazingthings.com"
+copyright = "Copyright 2007"
+
+# if it's open source, you might want to specify these
+# url = "http://yourcool.site/"
+# download_url = "http://yourcool.site/download"
+license = "MIT"
