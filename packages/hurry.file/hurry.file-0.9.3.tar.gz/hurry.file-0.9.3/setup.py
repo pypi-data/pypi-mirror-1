75c0b72021fa070bede74da854308be18c3330dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:15 +0000

    Add package hurry.file/0.9.3/hurry.file-0.9.3.tar.gz

---
+++ packages/hurry.file/hurry.file-0.9.3.tar.gz/hurry.file-0.9.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hurry.file",
+    version="0.9.3",
+    packages=find_packages('src'),
+    
+    package_dir= {'':'src'},
+    
+    namespace_packages=['hurry'],
+    package_data = {
+    '': ['*.txt', '*.zcml'],
+    },
+
+    zip_safe=False,
+    author='Infrae',
+    author_email='faassen@infrae.com',
+    description="""\
+hurry.file is an advanced Zope 3 file widget which tries its best to behave
+like other widgets, even when the form is redisplayed due to a validation
+error.
+""",
+    license='BSD',
+    keywords="zope zope3",
+    classifiers = ['Framework :: Zope3'],
+    install_requires=['setuptools'],
+    )
