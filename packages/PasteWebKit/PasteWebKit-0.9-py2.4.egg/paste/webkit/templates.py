61fbbc48bdb022929b047763c8de18f70a0dce8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:33 +0000

    Add package PasteWebKit/0.9/PasteWebKit-0.9-py2.4.egg

---
+++ packages/PasteWebKit/PasteWebKit-0.9-py2.4.egg/paste/webkit/templates.py
@@ -0,0 +1,12 @@
+import os
+from paste.script.templates import Template
+
+class WebKit(Template):
+
+    _template_dir = 'paster_templates/webkit'
+    summary = "A Paste WebKit web application"
+
+    egg_plugins = ['PasteWebKit']
+
+    required_templates = ['PasteDeploy#paste_deploy']
+        
