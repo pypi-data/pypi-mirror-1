71f3e8fc73cbdc2a151be6fb93c6035672d4eb91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:43 +0000

    Add package empywiz/0.6/empywiz-0.6-py2.5.egg

---
+++ packages/empywiz/empywiz-0.6-py2.5.egg/empywiz/empywizcfg.py
@@ -0,0 +1,13 @@
+# Config file for pywiz (prjwiz, expandtemplate)
+# This should be edited by the user to customize behaviour
+
+# change this to reflect the location where you keep your templates
+# Note that all the templates for prjwiz.py should be in the same dir
+
+#project_templates_dir = "/home/ville/templates"
+
+project_templates_dir = "c:/prj/templates"
+
+preview_changes = False
+
+
