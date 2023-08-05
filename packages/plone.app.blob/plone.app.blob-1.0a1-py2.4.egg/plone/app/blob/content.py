b05c752c7e1f5c77ac79e195771c64cbad2600d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:46 +0000

    Add package plone.app.blob/1.0a1/plone.app.blob-1.0a1-py2.4.egg

---
+++ packages/plone.app.blob/plone.app.blob-1.0a1-py2.4.egg/plone/app/blob/content.py
@@ -0,0 +1,97 @@
+from zope.interface import implements
+
+from AccessControl import ClassSecurityInfo
+from ComputedAttribute import ComputedAttribute
+from Products.Archetypes.atapi import Schema
+from Products.Archetypes.atapi import AnnotationStorage
+from Products.Archetypes.atapi import ATFieldProperty
+from Products.Archetypes.atapi import registerType
+from Products.CMFCore.permissions import View, ModifyPortalContent
+from Products.ATContentTypes.interfaces import IATFile
+from Products.ATContentTypes.content.base import ATCTFileContent
+from Products.ATContentTypes.content.schemata import ATContentTypeSchema
+from Products.ATContentTypes.content.schemata import finalizeATCTSchema
+
+from plone.app.blob.interfaces import IATBlob
+from plone.app.blob.config import packageName
+from plone.app.blob.field import BlobMarshaller
+
+
+ATBlobSchema = ATContentTypeSchema.copy()
+ATBlobSchema['title'].storage = AnnotationStorage()
+
+finalizeATCTSchema(ATBlobSchema, folderish=False, moveDiscussion=False)
+ATBlobSchema.registerLayer('marshall', BlobMarshaller())
+
+
+class ATBlob(ATCTFileContent):
+    """ a chunk of binary data """
+    implements(IATBlob)
+
+    __implements__ = ATCTFileContent.__implements__, IATFile
+
+    portal_type = 'Blob'
+    _at_rename_after_creation = True
+    schema = ATBlobSchema
+
+    title = ATFieldProperty('title')
+    summary = ATFieldProperty('description')
+
+    security  = ClassSecurityInfo()
+
+    security.declareProtected(View, 'index_html')
+    def index_html(self, REQUEST, RESPONSE):
+        """ download the file inline """
+        field = self.getPrimaryField()
+        return field.download(self, REQUEST, RESPONSE)
+
+    # helper & explicit accessor and mutator methods
+
+    security.declarePrivate('getBlobWrapper')
+    def getBlobWrapper(self):
+        """ return wrapper class containing the actual blob """
+        accessor = self.getField('file').getAccessor(self)
+        return accessor()
+
+    security.declareProtected(View, 'getFile')
+    def getFile(self, **kwargs):
+        """ archetypes.schemaextender (wisely) doesn't mess with classes,
+            so we have to provide our own accessor """
+        return self.getBlobWrapper()
+
+    security.declareProtected(ModifyPortalContent, 'setFile')
+    def setFile(self, value, **kwargs):
+        """ set the file contents and possibly also the id """
+        mutator = self.getField('file').getMutator(self)
+        mutator(value, **kwargs)
+
+    # compatibility methods when used as ATFile replacement
+
+    security.declareProtected(View, 'get_data')
+    def get_data(self):
+        """ return data as a string;  this is highly inefficient as it
+            loads the complete blob content into memory, but the method
+            is unfortunately still used here and there... """
+        return str(self.getBlobWrapper())
+
+    data = ComputedAttribute(get_data, 1)
+
+    def __str__(self):
+        """ return data as a string;  this is highly inefficient as it
+            loads the complete blob content into memory, but the method
+            is unfortunately still used here and there... """
+        return self.get_data()
+
+    security.declareProtected(ModifyPortalContent, 'setFilename')
+    def setFilename(self, value, key=None):
+        """ convenience method to set the file name on the field """
+        self.getBlobWrapper().setFilename(value)
+
+    security.declareProtected(ModifyPortalContent, 'setFormat')
+    def setFormat(self, value):
+        """ convenience method to set the mime-type """
+        self.getBlobWrapper().setContentType(value)
+
+
+registerType(ATBlob, packageName)
+
---
+++ packages/plone.app.blob/plone.app.blob-1.0a1-py2.4.egg/plone/app/blob/tests.py
@@ -0,0 +1,74 @@
+from plone.app.blob import db   # needs to be imported first to set up ZODB
+
+from unittest import TestSuite, makeSuite
+from Testing.ZopeTestCase import installPackage, ZopeDocFileSuite
+from ZPublisher.HTTPRequest import HTTPRequest
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+from Products.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.layer import onsetup
+
+from plone.app.blob.utils import guessMimetype
+
+from StringIO import StringIO
+from base64 import decodestring
+
+
+@onsetup
+def setupPackage():
+    """ set up the package and its dependencies """
+    fiveconfigure.debug_mode = True
+    import plone.app.blob
+    zcml.load_config('configure.zcml', plone.app.blob)
+    fiveconfigure.debug_mode = False
+    installPackage('plone.app.blob')
+
+setupPackage()
+PloneTestCase.setupPloneSite(products=(
+    'plone.app.blob',
+))
+
+
+test_environment = {
+    'CONTENT_TYPE': 'multipart/form-data; boundary=12345',
+    'REQUEST_METHOD': 'POST',
+    'SERVER_NAME': 'localhost',
+    'SERVER_PORT': '80',
+}
+
+largefile_data = '''
+--12345
+Content-Disposition: form-data; name="file"; filename="file"
+Content-Type: application/octet-stream
+
+test %s
+
+''' % ('test' * 1000)
+
+
+class BlobTestCase(PloneTestCase.PloneTestCase):
+
+    def testFileName(self):
+        """ checks fileupload object supports the filename """
+        req = HTTPRequest(StringIO(largefile_data), test_environment.copy(), None)
+        req.processInputs()
+        f = req.form.get('file')
+        self.assert_(f.name)
+
+    def testMimetypeGuessing(self):
+        gif = 'R0lGODlhAQABAPAAAPj8+AAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
+        gif = StringIO(decodestring(gif))
+        self.assertEqual(guessMimetype(gif), 'image/gif')
+        self.assertEqual(guessMimetype(gif, 'image.jpg'), 'image/jpeg')
+        self.assertEqual(guessMimetype(StringIO(), 'image.jpg'), 'image/jpeg')
+        self.assertEqual(guessMimetype(StringIO('foo')), 'text/plain')
+
+
+def test_suite():
+    return TestSuite((
+        makeSuite(BlobTestCase),
+        ZopeDocFileSuite(
+           'README.txt', package='plone.app.blob',
+           test_class=PloneTestCase.FunctionalTestCase),
+    ))
+
