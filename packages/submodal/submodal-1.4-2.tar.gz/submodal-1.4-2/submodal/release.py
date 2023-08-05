387da2bc998c3ab9a8e4d7e74ed3e1c01fd9b115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:49 +0000

    Add package submodal/1.4-2/submodal-1.4-2.tar.gz

---
+++ packages/submodal/submodal-1.4-2.tar.gz/submodal-1.4-2/submodal/release.py
@@ -0,0 +1,20 @@
+# Release information about submodal
+
+version = "1.4-2"
+
+description = "Cross Browser DHTML Modal Dialogs For Web Apps"
+long_description = """\
+The subModal works by placing a semi-transparent div over the browser, blocking \
+access to the content below while still providing visibility. This maintains \
+state and doesn't make someone feel disoriented or lost by moving them \
+completely to another page. Their frame of reference is kept while allowing them \
+to perform a new task (usually closely associated with the content below). \
+"""
+author = "Jorge Godoy (packager)"
+email = "jgodoy@gmail.com"
+# copyright = "Vintage 2006 - a good year indeed"
+
+# if it's open source, you might want to specify these
+url = "http://sublog.subimage.com/articles/2006/01/01/subModal"
+# download_url = "http://yourcool.site/download"
+license = "Free for any use - See included files"
