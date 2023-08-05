57ef75e26a9a74149e00215c56a5604891f26d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:26 +0000

    Add package py2tex/4.3/py2tex-4.3.zip

---
+++ packages/py2tex/py2tex-4.3.zip/py2tex-4.3/doc/source/test.py
@@ -0,0 +1,10 @@
+# test.py
+
+"""This module contains examples of tricky code that "<py2tex">
+should be able to handle"""
+
+def first():
+	""""Uberal"""
+	s = "'Uberal^^A"
+	s = '"Uberal'
+	# "Uberal
