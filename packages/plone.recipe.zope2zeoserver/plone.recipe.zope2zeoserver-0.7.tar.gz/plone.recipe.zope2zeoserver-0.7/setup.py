48b2ef085850204ffca75b344b76b1ab491bf4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package plone.recipe.zope2zeoserver/0.7/plone.recipe.zope2zeoserver-0.7.tar.gz

---
+++ packages/plone.recipe.zope2zeoserver/plone.recipe.zope2zeoserver-0.7.tar.gz/plone.recipe.zope2zeoserver-0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+name = "plone.recipe.zope2zeoserver"
+version = '0.7'
+
+setup(
+    name = name,
+    version = version,
+    author = "Hanno Schlichting",
+    author_email = "plone@hannosch.info",
+    description = "ZC Buildout recipe for installing a Zope 2 ZEO server",
+    long_description = open("README.txt").read(),
+    license = "ZPL 2.1",
+    keywords = "zope2 buildout",
+    url='http://svn.plone.org/svn/collective/buildout/'+name,
+    classifiers=[
+      "License :: OSI Approved :: Zope Public License",
+      "Framework :: Buildout",
+      "Framework :: Zope2",
+      "Programming Language :: Python",
+      ],
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['plone', 'plone.recipe'],
+    install_requires = ['zc.buildout', 'setuptools', 'zc.recipe.egg'],
+    zip_safe=False,
+    entry_points = {'zc.buildout': ['default = %s:Recipe' % name]},
+    )
