f4482764d6e348c03b7b3d2e64144a9abc68413a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:25 +0000

    Add package plone.app.kss/1.2-beta1.2/plone.app.kss-1.2-beta1.2.tar.gz

---
+++ packages/plone.app.kss/plone.app.kss-1.2-beta1.2.tar.gz/plone.app.kss-1.2-beta1.2/plone/app/kss/plugins/legacy/reload/initializeCollapsible.js
@@ -0,0 +1,10 @@
+
+// bind collapsible sections on load
+
+
+kukit.actionsGlobalRegistry.register("initializeCollapsible", function (oper) {
+        activateCollapsibles();
+    });
+
+kukit.log('actions for Plone legacy js registered (initialize collapsible)');
+
