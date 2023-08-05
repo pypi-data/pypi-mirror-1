61c9b2af56883af5ad2c7e658b6affaf4c3c3756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package iwm.recipe.bebopinstance/0.0.2/iwm.recipe.bebopinstance-0.0.2-py2.4.egg

---
+++ packages/iwm.recipe.bebopinstance/iwm.recipe.bebopinstance-0.0.2-py2.4.egg/iwm/recipe/bebopinstance/tests.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os, re, shutil, sys, tempfile
+import pkg_resources
+
+
+
+import zc.buildout.testing
+
+import unittest
+import zope.testing
+from zope.testing import doctest, renormalizing
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install_develop('zc.recipe.zope3instance', test)
+    zc.buildout.testing.install('zope.testing', test)
+    zc.buildout.testing.install('zc.recipe.egg', test)
+    sample_zope3 = test.globs['tmpdir']()
+    test.globs['sample_zope3'] = sample_zope3
+    test.globs['mkdir'](sample_zope3, 'zopeskel')
+    test.globs['mkdir'](sample_zope3, 'zopeskel', 'etc')
+
+    test.globs['write'](sample_zope3, 'zopeskel', 'etc', 
+                        'ftesting.zcml', 'This is ftesting')
+    test.globs['write'](sample_zope3, 'zopeskel', 'etc', 
+                        'site.zcml', 'This is site')
+    test.globs['mkdir'](sample_zope3, 'zopeskel', 'package-includes')
+
+def test_suite():
+    return unittest.TestSuite((
+        #doctest.DocTestSuite(),
+        doctest.DocFileSuite(
+            'README.txt',
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            checker=renormalizing.RENormalizing([
+               ])
+            ),
+        
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
---
+++ packages/iwm.recipe.bebopinstance/iwm.recipe.bebopinstance-0.0.2-py2.4.egg/iwm/recipe/bebopinstance/zope3scripts.py
@@ -0,0 +1,58 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Zope 3 program entry points
+
+The Zope 3 scripts, scriptzope, and debugzope are distributed
+as templates, rather than as entry points. Here we provide entry-point
+versions.
+
+$Id: zope3scripts.py 70995 2006-10-30 12:13:58Z jim $
+"""
+
+import os, sys
+import zope.app.debug
+import zope.app.twisted.main
+
+def zglobals(args):
+    db = zope.app.twisted.main.debug(args)
+    if "PYTHONSTARTUP" in os.environ:
+        execfile(os.environ["PYTHONSTARTUP"])
+    
+    app = zope.app.debug.Debugger.fromDatabase(db)
+    return dict(
+        app = app,
+        debugger = app,
+        root = app.root(),
+        __name__ = '__main__',
+        )
+
+
+def script(args):
+    globs = zglobals(args[:2])
+    sys.argv[:] = args[2:]
+    globs['__file__'] = sys.argv[0]
+    execfile(sys.argv[0], globs)
+    sys.exit()
+
+banner = """Welcome to the Zope 3 "debugger".
+The application root object is available as the root variable.
+A Zope debugger instance is available as the debugger (aka app) variable.
+"""
+
+def debug(args):
+    globs = zglobals(args)
+    import code
+    code.interact(banner=banner, local=globs)
+
+    
