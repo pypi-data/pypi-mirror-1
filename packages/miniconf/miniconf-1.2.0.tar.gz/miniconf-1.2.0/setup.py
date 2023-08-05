70e6feafbc64db34a255221f2d78db583438b2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:52 +0000

    Add package miniconf/1.2.0/miniconf-1.2.0.tar.gz

---
+++ packages/miniconf/miniconf-1.2.0.tar.gz/miniconf-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='miniconf',
+      version='1.2.0',
+      description='Plain text Python built-in objects persistence',
+      author='Sylvain Fourmanoit',
+      author_email='syfou@users.sourceforge.net',
+      classifiers = [
+    	'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Topic :: Software Development',
+        ],
+      py_modules=['miniconf'])
