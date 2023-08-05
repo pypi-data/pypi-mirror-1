b51c2be9dd87aa5ef6bc3fb61dafa6cc91063c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package zope.app.securitypolicy/3.4.2/zope.app.securitypolicy-3.4.2.tar.gz

---
+++ packages/zope.app.securitypolicy/zope.app.securitypolicy-3.4.2.tar.gz/zope.app.securitypolicy-3.4.2/setup.py
@@ -0,0 +1,70 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.securitypolicy package
+
+$Id: setup.py 80230 2007-09-27 15:16:52Z fdrake $
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.app.securitypolicy',
+    version = '3.4.2',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+    description='Zope securitypolicy',
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    keywords = "zope3 security policy",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://pypi.python.org/pypi/zope.app.securitypolicy',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['zope', 'zope.app'],
+    extras_require = dict(test=['zope.app.testing']),
+    install_requires=['setuptools',
+                      'zope.annotation',
+                      'zope.app.form',
+                      'zope.app.security',
+                      'zope.component [hook]',
+                      'zope.configuration',
+                      'zope.exceptions',
+                      'zope.i18n',
+                      'zope.i18nmessageid',
+                      'zope.interface',
+                      'zope.location',
+                      'zope.schema',
+                      'zope.security',
+                      'zope.securitypolicy',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    )
---
+++ packages/zope.app.securitypolicy/zope.app.securitypolicy-3.4.2.tar.gz/zope.app.securitypolicy-3.4.2/src/zope/app/securitypolicy/securitymap.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Generic two-dimensional array type (in context of security)
+
+$Id: securitymap.py 80149 2007-09-26 22:00:18Z rogerineichen $
+"""
+
+import zope.deferredimport
+
+zope.deferredimport.deprecated(
+    "It has moved to zope.securitypolicy.securitymap  This reference will be "
+    "removed somedays",
+    SecurityMap = 'zope.securitypolicy.grantinfo:SecurityMap',
+    PersistentSecurityMap = 'zope.securitypolicy.grantinfo:PersistentSecurityMap',
+    AnnotationSecurityMap = 'zope.securitypolicy.grantinfo:AnnotationSecurityMap',
+    )
