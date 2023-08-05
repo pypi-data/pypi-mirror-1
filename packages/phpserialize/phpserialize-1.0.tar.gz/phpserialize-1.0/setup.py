a695d9c4cfc9884922148e88989c01237e384cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:37 +0000

    Add package phpserialize/1.0/phpserialize-1.0.tar.gz

---
+++ packages/phpserialize/phpserialize-1.0.tar.gz/phpserialize-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='phpserialize',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    version='1.0',
+    url='http://trac.pocoo.org/repos/sandbox/phpserialize',
+    py_modules=['phpserialize'],
+    license='BSD'
+)
