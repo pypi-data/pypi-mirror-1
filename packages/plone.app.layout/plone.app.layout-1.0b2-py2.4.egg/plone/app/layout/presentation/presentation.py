3c79a462da08e226d1202f03044190b1668d992c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:18 +0000

    Add package plone.app.layout/1.0b2/plone.app.layout-1.0b2-py2.4.egg

---
+++ packages/plone.app.layout/plone.app.layout-1.0b2-py2.4.egg/plone/app/layout/presentation/presentation.py
@@ -0,0 +1,80 @@
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFPlone import PloneMessageFactory as _
+
+from plone.memoize.view import memoize
+from plone.app.layout.viewlets.common import ViewletBase
+
+class PresentationView(BrowserView):
+    template = ViewPageTemplateFile('presentation.pt')
+
+    def __call__(self):
+        return self.template()
+
+    @memoize
+    def body(self):
+        return self.context.CookedBody()
+
+    @memoize
+    def enabled(self):
+        getPresentation = getattr(self.context.aq_base, "getPresentation", None)
+        if getPresentation is None or getPresentation() == False:
+            return False
+
+        body = self.body()
+
+        # this should be a regex too, but hey this works
+        tags = ["h1", "H1", "h2", "H2"]
+        tag = None
+        for t in tags:
+            if body.find("<%s>" % t) > -1:
+                tag = t
+                break
+
+        if tag is None:
+            return False
+
+        return True
+
+    def content(self):
+        # ugly, ugly, ugly code, that basically changes the way the slide is put
+        # together this should be a HTML parser or XSLT or even JS
+
+        body = self.body()
+
+        # this should be a regex too, but hey this works
+        tags = ["h1", "H1", "h2", "H2"]
+        tag = None
+        for t in tags:
+            if body.find("<%s>" % t) > -1:
+                tag = t
+                break
+
+        num = int(tag[1])
+        if num > 1:
+            new = "%s1" % (tag[0])
+            body = body.replace("<%s>" % tag, "<%s>" % new)
+            body = body.replace("</%s>" % tag, "</%s>" % new)
+            tag = new
+
+        body = body.split('<%s>' % tag)
+        body = ('</div>\n<div class="slide">\n<%s>' % tag).join(body)
+        body = body.split('</%s>' % tag)
+        body = ('</%s>\n' % tag).join(body)
+
+        return body
+
+class PresentationViewlet(ViewletBase):
+    def update(self):
+        getPresentation = getattr(self.context.aq_base, "getPresentation", None)
+        if getPresentation is not None and getPresentation():
+            self.presentation_enabled = True
+        else:
+            self.presentation_enabled = False
+
+    def render(self):
+        if self.presentation_enabled:
+            url = "%s/presentation_view" % self.context.absolute_url()
+            msg = _(u'Also available in presentation mode\u2026')
+            return u'<p><a href="%s" rel="nofollow" class="link-presentation">%s</a></p>' % (url, msg)
+        return u''
