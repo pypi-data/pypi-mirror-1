eafa7aab06c557d8105e84528dd1e334205b6386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:09 +0000

    Add package emailfilter/0.2.1/emailfilter-0.2.1.tar.gz

---
+++ packages/emailfilter/emailfilter-0.2.1.tar.gz/emailfilter-0.2.1/setup_vars.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+"""
+Setup variables for EMailFilter
+Copyright (C) 2008 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+"""
+
+__progname__ = "emailfilter"
+__version__ = "0.2.1"
+__short_description__ = "A customizable e-mail content filter."
+__start_line__ = "The EMailFilter Program"
+__end_line__ = "Copyright and License"
+__url_base__ = "http://www.peterdonis.net/computers"
+__url_type__ = "unix"
+__author__ = "Peter A. Donis"
+__author_email__ = "peterdonis@alum.mit.edu"
+__url__ = "http://www.peterdonis.net"
+
+__classifiers__ = """
+Development Status :: 3 - Alpha
+Environment :: Console
+Intended Audience :: Developers
+Intended Audience :: End Users/Desktop
+License :: OSI Approved :: GNU General Public License (GPL)
+Operating System :: POSIX
+Operating System :: POSIX :: Linux
+Programming Language :: Python
+Topic :: Communications :: Email :: Filters
+"""
