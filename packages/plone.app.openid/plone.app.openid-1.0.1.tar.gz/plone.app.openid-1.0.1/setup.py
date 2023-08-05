9dfdf2c7bc2f23dfbf12fdca80595f1ed360ee7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package plone.app.openid/1.0.1/plone.app.openid-1.0.1.tar.gz

---
+++ packages/plone.app.openid/plone.app.openid-1.0.1.tar.gz/plone.app.openid-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '1.0.1'
+
+setup(name='plone.app.openid',
+      version=version,
+      description="Plone OpenID authentication support",
+      long_description=open("README.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Plone OpenID authentication consumer',
+      author='Wichert AkkermanPlone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.app.openid',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      download_url='http://code.google.com/p/plone/downloads/list',
+      install_requires=[
+        'plone.openid',
+        'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
