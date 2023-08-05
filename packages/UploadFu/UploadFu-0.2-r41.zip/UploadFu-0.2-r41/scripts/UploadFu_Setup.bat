115551d60b97724781e8c14d87fe239969f952a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:43 +0000

    Add package UploadFu/0.2-r41/UploadFu-0.2-r41.zip

---
+++ packages/UploadFu/UploadFu-0.2-r41.zip/UploadFu-0.2-r41/scripts/UploadFu_Setup.bat
@@ -0,0 +1,8 @@
+ez_setup.py mechanize configobj pywin32
+pywin32_postinstall.py -install
+ContextMenu.py --unregister
+UploadFu_ContextMenu.py --unregister
+UploadFu_ContextMenu.py
+UploadFu_Upload.py
+UploadFu_StartWatchJobs.py register
+REM ~ pause
