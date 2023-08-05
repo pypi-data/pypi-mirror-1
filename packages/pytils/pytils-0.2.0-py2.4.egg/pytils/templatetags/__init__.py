26a24ce0869877bd9cbe38223e13ecebf6a16ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:20 +0000

    Add package pytils/0.2.0/pytils-0.2.0-py2.4.egg

---
+++ packages/pytils/pytils-0.2.0-py2.4.egg/pytils/templatetags/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# License: GNU GPL2
+# Author: Pythy <the.pythy@gmail.com>
+"""
+Pytils templatetags for Django web-framework
+"""
+
+__id__ = __revision__ = "$Id: __init__.py 11 2006-09-28 17:45:00Z the.pythy $"
+__url__ = "$URL: https://pythy.googlecode.com/svn/trunk/pytils/pytils/templatetags/__init__.py $"
