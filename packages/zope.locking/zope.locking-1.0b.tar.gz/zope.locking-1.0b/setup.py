4bd8f44885a76371c68abf42db41d44d91a13191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package zope.locking/1.0b/zope.locking-1.0b.tar.gz

---
+++ packages/zope.locking/zope.locking-1.0b.tar.gz/zope.locking-1.0b/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zope.locking",
+    version="1.0b",
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['zope'],
+    include_package_data=True,
+    install_requires = ['setuptools'],
+    zip_safe = False,
+    description=open("README.txt").read(),
+    long_description=(
+        open('src/zope/locking/CHANGES.txt').read() +
+        '\n\n' +
+        open("src/zope/locking/README.txt").read()),
+    )
