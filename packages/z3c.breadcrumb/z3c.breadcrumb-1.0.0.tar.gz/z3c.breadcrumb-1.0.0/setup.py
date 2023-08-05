2a66774e559a94b10566ddfae4bda7e672c761f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:09 +0000

    Add package z3c.breadcrumb/1.0.0/z3c.breadcrumb-1.0.0.tar.gz

---
+++ packages/z3c.breadcrumb/z3c.breadcrumb-1.0.0.tar.gz/z3c.breadcrumb-1.0.0/setup.py
@@ -0,0 +1,74 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 77684 2007-07-10 19:41:15Z srichter $
+"""
+import os
+import xml.sax.saxutils
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    text = open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    return xml.sax.saxutils.escape(text)
+
+setup (
+    name='z3c.breadcrumb',
+    version='1.0.0',
+    author = "Roger Ineichen,Stephan Richter and the Zope Community",
+    author_email = "zope3-dev@zope.org",
+    description = "A pluggable breadcrumbs implementation based on adapters.",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'z3c', 'breadcrumb', 'README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 breadcrumb adapter",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://svn.zope.org/z3c.breadcrumb',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = ['zope.testing'],
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.app.component',
+        'zope.component',
+        'zope.interface',
+        'zope.location',
+        'zope.publisher',
+        'zope.schema',
+        'zope.traversing',
+        ],
+    dependency_links = ['http://download.zope.org/distribution'],
+    zip_safe = False,
+    )
