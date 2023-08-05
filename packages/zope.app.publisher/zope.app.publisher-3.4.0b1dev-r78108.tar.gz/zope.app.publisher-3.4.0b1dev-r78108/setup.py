88a7e4445eb330aea04c8ee23e7c6032ad1a0e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:33 +0000

    Add package zope.app.publisher/3.4.0b1dev-r78108/zope.app.publisher-3.4.0b1dev-r78108.tar.gz

---
+++ packages/zope.app.publisher/zope.app.publisher-3.4.0b1dev-r78108.tar.gz/zope.app.publisher-3.4.0b1dev-r78108/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.publisher',
+      version = '3.4.0b1',
+      url='http://svn.zope.org/zope.app.publisher',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+
+      namespace_packages=['zope', 'zope.app'],
+      include_package_data=True,
+      install_requires=['setuptools',
+                        'zope.app.component',
+                        'zope.app.container',
+                        'zope.app.pagetemplate',
+                        'zope.app.publication',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.contenttype',
+                        'zope.datetime',
+                        'zope.deferredimport',
+                        'zope.deprecation',
+                        'zope.i18n',
+                        'zope.interface',
+                        'zope.location',
+                        'zope.pagetemplate',
+                        'zope.publisher',
+                        'zope.schema',
+                        'zope.security',
+                        'zope.traversing',
+                        ],
+      extras_require={
+          'test': ['zope.testing',
+                   'zope.app.testing',
+                   'zope.app.securitypolicy',
+                   'zope.app.zcmlfiles'],
+          'back35': ['zope.app.skins',
+                     'zope.app.layers'],
+          },
+
+      zip_safe = False,
+      )
