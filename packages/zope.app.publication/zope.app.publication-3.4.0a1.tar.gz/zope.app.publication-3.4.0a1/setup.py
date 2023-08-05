d79b2ede694bc8d8abe516e131a698fd387803d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package zope.app.publication/3.4.0a1/zope.app.publication-3.4.0a1.tar.gz

---
+++ packages/zope.app.publication/zope.app.publication-3.4.0a1.tar.gz/zope.app.publication-3.4.0a1/setup.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.publication package
+
+$Id: setup.py 74445 2007-04-22 10:43:17Z ctheune $
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.publication',
+      version = '3.4.0a1',
+      url='http://svn.zope.org/zope.app.publication',
+      license='ZPL 2.1',
+      description='Zope publication',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=['zope.app.testing',
+                                  'zope.app.securitypolicy',
+                                  'zope.app.zcmlfiles',
+                                  'zope.app.dav',
+                                  'zope.app.zptpage',
+                                  ]),
+      install_requires=['zope.interface',
+                        'ZODB3',
+                        'zope.component',
+                        'zope.i18n',
+                        'zope.app.http',
+                        'zope.app.applicationcontrol',
+                        'zope.app.error',
+                        'zope.app.exception',
+                        'zope.app.security'
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
