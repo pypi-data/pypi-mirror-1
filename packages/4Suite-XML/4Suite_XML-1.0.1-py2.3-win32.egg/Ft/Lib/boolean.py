5dd7de08e0bc69a6a96eee3a7dce856012ba8c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:11 +0000

    Add package 4Suite-XML/1.0.1/4Suite_XML-1.0.1-py2.3-win32.egg

---
+++ packages/4Suite-XML/4Suite_XML-1.0.1-py2.3-win32.egg/Ft/Lib/boolean.py
@@ -0,0 +1,8 @@
+# dynamic module loader stub for .egg zipfiles
+def __bootstrap__():
+    global __bootstrap__, __loader__, __file__
+    import imp, pkg_resources
+    __file__ = pkg_resources.resource_filename(__name__, 'boolean.pyd')
+    del __bootstrap__, __loader__
+    imp.load_dynamic(__name__, __file__)
+__bootstrap__()
