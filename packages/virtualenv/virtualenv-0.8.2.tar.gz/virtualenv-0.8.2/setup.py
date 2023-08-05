5816e83198913d8669680c77e3070765ad3fe268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package virtualenv/0.8.2/virtualenv-0.8.2.tar.gz

---
+++ packages/virtualenv/virtualenv-0.8.2.tar.gz/virtualenv-0.8.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.8.2'
+
+f = open(os.path.join(os.path.dirname(__file__), 'docs', 'index.txt'))
+long_description = f.read().strip()
+f.close()
+
+setup(name='virtualenv',
+      version=version,
+      description="Virtual Python Environment builder",
+      long_description=long_description,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+      ],
+      keywords='setuptools deployment installation distutils',
+      author='Ian Bicking',
+      author_email='ianb@colorstudy.com',
+      url='http://pypi.python.org/pypi/virtualenv',
+      license='MIT',
+      py_modules=['virtualenv'],
+      ## Hacks to get the package data installed:
+      packages=[''],
+      package_dir={'': '.'},
+      package_data={'': ['support-files/*-py%s.egg' % sys.version[:3]]},
+      zip_safe=False,
+      entry_points="""
+      [console_scripts]
+      virtualenv = virtualenv:main
+      """,
+      )
