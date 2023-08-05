57028992d03169b7e6bda91afe4d0739d9112b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:28 +0000

    Add package twPrototype/1.5dev-r632/twPrototype-1.5dev_r632-py2.4.egg

---
+++ packages/twPrototype/twPrototype-1.5dev_r632-py2.4.egg/toscawidgets/widgets/prototype/js.py
@@ -0,0 +1,8 @@
+from toscawidgets.api import JSLink, js_function
+
+__all__ = ['prototype_js', 'S']
+
+prototype_js = JSLink(modname=__name__, filename='static/prototype.js')
+
+S = js_function('$')
+
