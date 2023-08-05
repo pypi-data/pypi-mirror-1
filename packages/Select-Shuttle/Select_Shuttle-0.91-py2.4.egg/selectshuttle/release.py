4f7e51ed517c3f4666bf420606f51b206ff403eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:32 +0000

    Add package Select-Shuttle/0.91/Select_Shuttle-0.91-py2.4.egg

---
+++ packages/Select-Shuttle/Select_Shuttle-0.91-py2.4.egg/selectshuttle/release.py
@@ -0,0 +1,21 @@
+# Release information about Select-Shuttle
+
+version = "0.91"
+
+description = """The SelectShuttle widget provides a mechanism for selecting
+multiple values from a list of values by allowing the user to move items
+between two lists. On modern browsers you can also double click an item to
+move it from one list to the other. After the first "move", all entries
+will be sorted automatically accordingly to its "description" on both
+lists. Take a look at the code for SelectShuttleDesc for an example of how
+to use this widget in your code."""
+
+author = "Jorge Godoy"
+
+email = "jgodoy@gmail.com"
+# copyright = "Vintage 2006 - a good year indeed"
+
+# if it's open source, you might want to specify these
+url = "http://www.g2ctech.com/"
+download_url = "http://www.g2ctech.com/widgets/Select_Shuttle-0.9-py2.4.egg"
+license = "MIT"
