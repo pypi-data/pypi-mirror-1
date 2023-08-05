1af68b14cd485b89576b7fb3c3caff9ab1ad005c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:25 +0000

    Add package FileFields/0.1a0.dev-r295/FileFields-0.1a0.dev_r295-py2.4.egg

---
+++ packages/FileFields/FileFields-0.1a0.dev_r295-py2.4.egg/file_fields/widgets/file_field.py
@@ -0,0 +1,194 @@
+﻿# -*- coding: utf-8 -*-
+
+# Es al cohete convertir hacia y de base64. Hay que tener los filehandles 
+#   abiertos y listo...
+# Limitar el tamaño del upload.
+# Deberia poder recibir todos los datos para no tener que calcular nada y
+#   y hacerlo super liviano.
+# Tambien tendria que en data recibir una funcion o algo por el estilo
+#   para hacerlo lazy.
+
+import cherrypy
+
+import md5
+
+import logging
+
+from os.path import basename
+
+from turbogears import widgets, validators, url
+from turbogears.util import Bunch
+
+from tempfile import TemporaryFile
+
+log = logging.getLogger("file_fields.widgets.file_field")
+
+class FileValidator(validators.FancyValidator):
+    messages = {'file_needed': 'A file is needed'}
+    
+    def get_md5(self, data):
+        hash = md5.new()
+        hash.update(data)
+        return hash.hexdigest()
+        
+    def get_md5_file(self, file):
+        file.seek(0)
+        return self.get_md5(file.read())
+    
+    def make_temp_file(self, data):
+        file = TemporaryFile()
+        file.write(data)
+        file.seek(0)
+        return file
+        
+    def get_file_size(self, file):
+        file.seek(0, 2)
+        size = file.tell()
+        file.seek(0)
+        return size
+    
+    def b64encode_file(self, file):
+        file.seek(0)
+        file = self.make_temp_file(file.read().encode('base64'))
+        return file
+    
+    def b64decode_file(self, file):
+        file.seek(0)
+        file = self.make_temp_file(file.read().decode('base64'))
+        return file
+    
+    def basename(self, name):
+        name = basename(name)
+        if name.split('\\'): # IE bug fix (sends comlpete path as filename) and
+                             # basename won't clear it if its on a linux server
+            return name.split('\\')[-1]
+        return name
+    
+    def _to_python(self, value, state):
+        file = None
+        if value['file'].filename:
+            # nuevo archivo desde el formulario
+            info = value['file'] # FieldStorage()
+            # inicializamos la sesion
+            value.clear()
+            value['id'] = value['md5'] = self.get_md5_file(info.file)
+            value['type'] = info.type
+            value['name'] = self.basename(info.filename)
+            value['size'] = self.get_file_size(info.file)
+            value['file'] = info.file
+            cherrypy.session[value['id']] = value
+        else:
+            if value['id']:
+                # hay que sacar el diccionario de la session
+                id = value['id']
+                value.clear()
+                value.update(cherrypy.session[id])
+            else:
+                # no hay archivo en sesion ni llego ninguno en el formulario
+                value.clear()
+                if self.not_empty:
+                    raise validators.Invalid(self.message('file_needed', 
+                                                          state),
+                                             value, state)
+                return ''
+        if not value.get('base64') and self.base64:
+            value['file'] = self.b64encode_file(value['file'])
+            value['base64'] = True
+        value['file'].seek(0)
+        return value
+        
+    def _from_python(self, value, state):
+        # TODO: refactorear esto
+        if isinstance(value, dict):
+            type = value['type']
+            name = value['name']
+            value = value['data'] # a lo ultimo
+        else:
+            # if don't get a hint we can't guess this data
+            type = ''
+            name = ''
+            
+        if self.base64:
+            value = value.decode('base64')
+            
+        file = self.make_temp_file(value)
+        hash = md5.new()
+        hash.update(value)
+        size = len(value)
+        
+        # inicializamos la sesion
+        value = dict()
+        value['id'] = value['md5'] = hash.hexdigest()
+        value['size'] = size
+        value['file'] = file
+        value['type'] = type
+        value['name'] = name
+        cherrypy.session[value['id']] = value
+        
+        return value
+
+class FileField(widgets.FormField):
+    template = """
+    <div xmlns:py="http://purl.org/kid/ns#">
+        <input type="hidden" id="${field_id}.id" name="${name}.id"
+            value="${isinstance(value, dict) and value.get('id') or ''}"/>
+        <input type="file" id="${field_id}.file" name="${name}.file"/>
+        <div py:if="value_of('value')" id="${field_id}_info">
+            <div style="float: right; font-size: small">
+                <a href="${file_url}" style="font-size: small">
+                    Download
+                </a> | 
+                <span py:if="scriptaculous"> 
+                    <a href="#" onclick="document.getElementById('${field_id}.id').value=''; new Effect.BlindUp(document.getElementById('${field_id}_info'));">
+                        Delete File
+                    </a>
+                </span>
+                <span py:if="not scriptaculous"> 
+                    <a href="#" onclick="document.getElementById('${field_id}.id').value=''; document.getElementById('${field_id}_info').style.display='none';">
+                        Delete file
+                    </a>
+                </span>
+            </div>
+            <div py:if="value['name']">
+                <b>Name:</b> ${value['name']}
+                
+            </div>
+            <div py:if="value['size']">
+                <b>Size:</b> ${value['size']}
+            </div>
+            <div py:if="value['type']">
+                <b>Type:</b> ${value['type']}
+            </div>
+            <div py:if="value['md5']">
+                <b>MD5:</b> ${value['md5']}
+            </div>
+        </div>
+    </div>
+    """
+    
+    file_upload = True
+    scriptaculous = None
+    css = []
+    javascript = []
+    
+    def __init__(self, name='', base64=False, use_scriptaculous=True, 
+                 file_server_url='/file_server', *args, **kw):
+        super(FileField, self).__init__(name=name, *args, **kw)
+        self.base64 = base64
+        self.validator = FileValidator(base64=base64)
+        self.file_server_url = file_server_url
+        
+        if use_scriptaculous:
+            try:
+                from scriptaculous import scriptaculous
+                self.scriptaculous = scriptaculous
+                self.javascript += self.scriptaculous.javascript
+            except ImportError:
+                log.warning('Scriptaculous is not installed')
+    
+    def update_params(self, d):
+        super(FileField, self).update_params(d)
+        if d['value']:
+            d['scriptaculous'] = self.scriptaculous
+            d['file_url'] = url(self.file_server_url, id=d['value']['id'])
+
---
+++ packages/FileFields/FileFields-0.1a0.dev_r295-py2.4.egg/file_fields/widgets/image_field.py
@@ -0,0 +1,157 @@
+﻿# -*- coding: utf-8 -*-
+
+import Image
+import cherrypy
+import logging
+
+from turbogears import validators, widgets, url
+
+from tempfile import TemporaryFile
+
+from file_field import FileField, FileValidator
+
+log = logging.getLogger("file_fields.widgets.image_field")
+
+class ImageValidator(FileValidator):
+    messages = {'unsupported': 'Unsupported image format',
+                'too_big': 'Image dimensions are too large',
+                'file_needed': 'An image is needed'}
+    
+    def _to_python(self, value, state):
+        value = super(ImageValidator, self)._to_python(value, state)
+        if value and not value.get('image_processed'):
+            if self.base64:
+                value['file'] = self.b64decode_file(value['file'])
+                value['base64'] = False
+            
+            value['file'].seek(0)
+            try: # verificamos que PIL reconozca el formato de la imagen
+                image = Image.open(value['file'])
+            except:
+                value.clear()
+                raise validators.Invalid(self.message('unsupported', state),
+                                         value, state)
+            
+            #verificamos el tamaño de la imagen
+            if self.max_dimensions != (0, 0) and \
+               (self.max_dimensions[0] < image.size[0] or \
+                self.max_dimensions[1] < image.size[1]):
+                if not self.resize_if_bigger:
+                    value.clear()
+                    raise validators.Invalid(self.message('too_big', state),
+                                             value, state)
+                value['file'] = self.resize(value['file'], self.max_dimensions)
+                
+                old_id = value['id']
+                cherrypy.session.pop(value['id'])
+               
+                value['id'] = value['md5'] = self.get_md5_file(value['file'])
+                value['size'] = self.get_file_size(value['file'])
+                
+            cherrypy.session[value['id'] + '_thumb'] = \
+                self.get_thumbnail(value['file'])
+                
+            value['image_processed'] = True
+        elif not value:
+            return ''
+        if not value.get('base64') and self.base64:
+            value['file'] = self.b64encode_file(value['file'])
+            value['base64'] = True
+        value['file'].seek(0)
+        return value
+    
+    def _from_python(self, value, state):
+        value = super(ImageValidator, self)._from_python(value, state)    
+        cherrypy.session[value['id'] + '_thumb'] = \
+            self.get_thumbnail(value['file'])
+        return value
+        
+    def resize(self, file, dimensions):
+        file.seek(0)
+        image = Image.open(file)
+        image.thumbnail(dimensions)
+        file = TemporaryFile()
+        try:
+            image.save(file, format=image.format)
+        except IOError:
+            log.warning('There was a problem resizing the image')
+        file.seek(0)
+        return file
+        
+    def get_thumbnail(self, file):
+        file = self.resize(file, self.thumb_dimensions)
+        info = dict()
+        file.seek(0, 2)
+        info['size'] = file.tell()
+        info['file'] = file
+        return info
+
+# Como mostrar una imagen inline
+#<img src="data:${thumbnail.type};base64,${thumbnail.data}" />
+
+class ImageField(FileField):
+    template = """
+    <div xmlns:py="http://purl.org/kid/ns#">
+        <input type="hidden" id="${field_id}.id" name="${name}.id"
+            value="${isinstance(value, dict) and value.get('id') or ''}"/>
+        <input type="file" id="${field_id}.file" name="${name}.file"/>
+        <div py:if="value_of('value')" id="${field_id}_pic">
+            <span py:if="scriptaculous"> 
+                <a href="#" onclick="document.getElementById('${field_id}.id').value=''; new Effect.BlindUp(document.getElementById('${field_id}_pic'));" 
+                   style="float: right; font-size: small">
+                    Delete Image
+                </a>
+            </span>
+            <span py:if="not scriptaculous"> 
+                <a href="#" onclick="document.getElementById('${field_id}.id').value=''; document.getElementById('${field_id}_pic').style.display='none';" 
+                   style="float: right; font-size: small">
+                    Delete Image
+                </a>
+            </span>
+            <div style="display: block; text-align: center">
+                <span py:if="lightbox">
+                    ${lightbox.display(img_url, thumb_url=thumb_url)}
+                </span>
+                <span py:if="not lightbox">
+                    <a href="${img_url}" target="_new">
+                        <img src="${thumb_url}" border="0" />
+                    </a>
+                </span>
+            </div>
+        </div>
+    </div>
+    """
+    
+    file_upload = True
+    lightbox = None
+    scriptaculous = None
+    css = []
+    javascript = []
+    
+    def __init__(self, name='', thumb_dimensions=(220, 220), base64=False, 
+                 max_dimensions=(0, 0), resize_if_bigger=False,
+                 use_lightbox=True, image_server_url='/pic_server', *args, 
+                 **kw):
+        super(ImageField, self).__init__(name=name, *args, **kw)
+        self.image_server_url = image_server_url
+        self.validator = ImageValidator(thumb_dimensions=thumb_dimensions,
+                                        max_dimensions=max_dimensions,
+                                        resize_if_bigger=resize_if_bigger,
+                                        base64=base64)
+        if use_lightbox:
+            try:
+                from lightbox.widgets import Lightbox
+                self.lightbox = Lightbox()
+                self.javascript += self.lightbox.javascript
+                self.css += self.lightbox.css
+            except ImportError:
+                log.warning('Lightbox is not installed')
+    
+    def update_params(self, d):
+        super(ImageField, self).update_params(d)
+        if d['value']:
+            d['lightbox'] = self.lightbox
+            d['img_url'] = url(self.image_server_url, id=d['value']['id'])
+            d['thumb_url'] = url(self.image_server_url, 
+                                 id=(d['value']['id'] + '_thumb'))
+
