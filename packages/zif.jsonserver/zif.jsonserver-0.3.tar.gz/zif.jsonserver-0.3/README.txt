428201e5b6534996ff98630ccec5078a31470888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:58 +0000

    Add package zif.jsonserver/0.3/zif.jsonserver-0.3.tar.gz

---
+++ packages/zif.jsonserver/zif.jsonserver-0.3.tar.gz/zif.jsonserver-0.3/README.txt
@@ -0,0 +1,30 @@
+****************
+zif.jsonserver
+****************
+
+See 'src/zif/jsonserver/README.txt' for more information.
+
+
+Releases
+********
+
+================
+0.3 (2007/05/24)
+================
+
+Repackaging to ensure zcml files are included for zope 3.
+
+
+================
+0.2 (2007/04/18)
+================
+
+Corrected setup configuration file to include zif namespace.
+
+
+================
+0.1 (2007/04/13)
+================
+
+Initial release.
+
---
+++ packages/zif.jsonserver/zif.jsonserver-0.3.tar.gz/zif.jsonserver-0.3/setup.py
@@ -0,0 +1,46 @@
+"""
+$Id: setup.py 77 2007-05-25 01:44:12Z fairwinds $
+
+zif.jsonserver
+Copyright (c) 2006, Virginia Polytechnic Institute and State University
+All rights reserved. Refer to LICENSE.txt for details of distribution and use.
+
+Distutils setup
+ 
+"""
+
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name = 'zif.jsonserver',
+    version = '0.3',
+    license = 'ZPL2.1',
+    description = 'A json server for zope 3.',
+    long_description = (
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'zif', 'jsonserver', 'README.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+    keywords = 'jsonserver json zope3',
+    author = 'Jim Washington',
+    author_email = 'jwashin@users.sourceforge.net',
+    url = 'http://zif.svn.sourceforge.net/viewvc/zif/zif.jsonserver',
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    namespace_packages = ['zif'],
+    install_requires = ['setuptools'],
+    dependency_links = ['http://download.zope.org/distribution/'],   
+    include_package_data = True,
+    zip_safe = False,
+    )
