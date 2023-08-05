30a35c5493a5b3d32dbd1988aad4d3cbea395dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:55 +0000

    Add package zope.schema/3.4.0/zope.schema-3.4.0.tar.gz

---
+++ packages/zope.schema/zope.schema-3.4.0.tar.gz/zope.schema-3.4.0/setup.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.schema package
+
+$Id: setup.py 80309 2007-09-28 11:49:51Z philikon $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.schema',
+      version = '3.4.0',
+      url='http://pypi.python.org/pypi/zope.schema',
+      license='ZPL 2.1',
+      description='Zope3 Data Schemas',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description=(read('src', 'zope', 'schema', 'README.txt')
+                        + '\n\n' +
+                        read('CHANGES.txt')),
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope',],
+      install_requires=['setuptools',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.event',
+                        # testing dependencies
+                        'zope.testing',
+                       ],
+      include_package_data = True,
+      zip_safe = False,
+      )
