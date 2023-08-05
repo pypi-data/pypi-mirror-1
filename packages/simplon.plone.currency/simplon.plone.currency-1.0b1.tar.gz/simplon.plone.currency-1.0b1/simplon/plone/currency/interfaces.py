d516a68edc30677c433b0576f863e5367d7d6004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:46 +0000

    Add package simplon.plone.currency/1.0b1/simplon.plone.currency-1.0b1.tar.gz

---
+++ packages/simplon.plone.currency/simplon.plone.currency-1.0b1.tar.gz/simplon.plone.currency-1.0b1/simplon/plone/currency/interfaces.py
@@ -0,0 +1,75 @@
+from zope.interface import Attribute
+from zope.interface import Interface
+from zope.schema import Choice
+from zope.schema import Container
+from zope.schema import Float
+from zope.app.container.interfaces import IContained
+from zope.app.container.interfaces import IContainer
+from zope.app.container.interfaces import IContainerNamesContainer
+from zope.app.container.constraints import contains
+from Products.CMFPlone import PloneMessageFactory as _
+
+class ICurrencyInformation(Interface):
+    code = Choice(
+            title=_(u"label_curren_currency",
+                default=u"Currency"),
+            description=_(u"help_currency_currency",
+                default=u"The main currency is used as the default currency "
+                        u"as well as the base for currency rates. If you "
+                        u"change the main currency all rates will be "
+                        u"recalculated automatically."),
+            vocabulary="simplon.plone.currency.currencies",
+            required=True)
+
+    rate = Float(
+            title=_(u"label_currency_rate",
+                default=u"Rate"),
+            description=_(u"help_currency_date",
+                default=u"This is the conversion rate from this currency to a "
+                        u"'system' currency you configure."),
+            min=0.0,
+            default=1.0,
+            required=True)
+
+    symbol = Attribute("symbol",
+            u"The symbol used to identify this currency. Since not all"
+            u"currencies have a symbol this can be an empty string.")
+
+    description = Attribute("description",
+            u"A short description of the currency.")
+
+
+class ICurrency(IContained, ICurrencyInformation):
+    """Currency information."""
+
+
+class ICurrencyStorage(IContainer, IContainerNamesContainer):
+    contains("simplon.plone.currency.interfaces.ICurrency")
+
+
+class IGlobalCurrencySettings(Interface):
+    currency = Choice(
+            title=_(u"label_currencymanager_currency",
+                default=u"Main currency"),
+            description=_(u"help_currencymanager_currency",
+                default=u"The main currency is used as the default currency "
+                        u"as well as the base for currency rates."),
+            vocabulary="simplon.plone.currency.sitecurrencies",
+            default="EUR",
+            required=True)
+
+    def SwitchCurrency(code):
+        """Switch the main currency.
+
+        This will take care of recalculating all conversion rates.
+        """
+
+
+class ICurrencyManager(IGlobalCurrencySettings):
+    currencies = Container(
+            title=_(u"label_currencymanager_currencies",
+                default=u"Currencies"),
+            description=_(u"help_currencymanager_currencies",
+                default=u""),
+            required=True)
+
---
+++ packages/simplon.plone.currency/simplon.plone.currency-1.0b1.tar.gz/simplon.plone.currency-1.0b1/simplon/plone/currency/manager.py
@@ -0,0 +1,20 @@
+from Persistence import Persistent
+from zope.interface import implements
+from simplon.plone.currency.interfaces import ICurrencyManager
+from simplon.plone.currency.currency import CurrencyStorage
+from simplon.plone.currency.currency import Currency
+
+class CurrencyManager(Persistent):
+    implements(ICurrencyManager)
+    
+    def __init__(self):
+        self.currencies=CurrencyStorage()
+        self.currencies.addItem(Currency(code="EUR", rate=1.0))
+        self.currency="EUR"
+
+    def SwitchCurrency(self, code):
+        factor=self.currencies[code].rate
+        for cur in self.currencies.values():
+            cur.rate/=factor
+        self.currency=code
+
