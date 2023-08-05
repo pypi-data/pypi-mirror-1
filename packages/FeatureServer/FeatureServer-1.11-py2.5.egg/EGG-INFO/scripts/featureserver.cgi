68715c4a96926e65a6fcdec01fac2e9d8944ae54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:09 +0000

    Add package FeatureServer/1.11/FeatureServer-1.11-py2.5.egg

---
+++ packages/FeatureServer/FeatureServer-1.11-py2.5.egg/EGG-INFO/scripts/featureserver.cgi
@@ -0,0 +1,9 @@
+#!/usr/bin/python2.5
+
+import sys
+
+from FeatureServer.Server import Server, cgiHandler, cfgfiles
+
+if __name__ == '__main__':
+    cgiHandler()
+
---
+++ packages/FeatureServer/FeatureServer-1.11-py2.5.egg/FeatureServer/Feature/__init__.py
@@ -0,0 +1,6 @@
+__author__  = "MetaCarta"
+__copyright__ = "Copyright (c) 2006-2007 MetaCarta"
+__license__ = "Clear BSD" 
+__version__ = "$Id: __init__.py 396 2007-12-31 07:24:33Z crschmidt $"
+
+from Feature import Feature
