c469447f116b96bd0a6c2b45f8091a0748c1eeb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:29 +0000

    Add package gsquickstart/1.0Beta/gsquickstart-1.0Beta-py2.5.egg

---
+++ packages/gsquickstart/gsquickstart-1.0Beta-py2.5.egg/gsquickstart/release.py
@@ -0,0 +1,45 @@
+# Release information about gsquickstart
+
+version = "1.0Beta"
+
+description = "TurboGears quickstart with genshi template"
+long_description = """
+
+gsquickstart is a Turbogears template plugin for genshi template.
+It will install a paste template for turbogears quickstart usage.
+
+Install
+-----------
+Install with setuptools::
+
+    $easy_install gsquickstart
+
+Usage
+-----------
+
+gsquickstart Just as normal tg-admin quickstart, 
+but you've to specify the tggenshi(TurboGears Genshi) template instead of the default kid template::
+
+    $tg-admin quickstart -t tggenshi [project name]
+
+
+Notice
+---------
+
+TurboGears 1.0 documents are written for the default setting of kid+sqlobject.
+
+But genshi template has some difference between kid template. 
+
+You may expect problems when using genshi template instead.
+
+Take widgets for example, you've to use '${ET(myform())}' instead of '${myform()}' in template.
+
+""" 
+author = "Fred Lin"
+email = "gasolin+tg@gmail.com"
+copyright = "Fred Lin 2007"
+
+# if it's open source, you might want to specify these
+url = "http://trac.turbogears.org/browser/projects/GenshiQuickStart"
+download_url = "http://www.python.org/pypi/gsquickstart/"
+license = "MIT"
