0d11528efcb91fceb8017d2f333bf95a8e5d8cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package mailinglogger/3.2.1/mailinglogger-3.2.1.tar.gz

---
+++ packages/mailinglogger/mailinglogger-3.2.1.tar.gz/mailinglogger-3.2.1/mailinglogger/common.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2007 Simplistix Ltd
+#
+# This Software is released under the MIT License:
+# http://www.opensource.org/licenses/mit-license.html
+# See license.txt for more details.
+
+from logging import Formatter
+from socket import gethostname
+from ZConfig.datatypes import RegularExpressionConversion
+
+class SubjectFormatter(Formatter):
+    
+    def format(self,record):
+        record.message = record.getMessage()
+        if self._fmt.find('%(line)') >= 0:
+            record.line = record.message.split('\n')[0]
+        if self._fmt.find("%(asctime)") >= 0:
+            record.asctime = self.formatTime(record, self.datefmt)
+        if self._fmt.find("%(hostname)") >= 0:
+            record.hostname = gethostname()
+        return self._fmt % record.__dict__
+    
+class RegexConversion(RegularExpressionConversion):
+    def __call__(self, value):
+        return bool(self._rx.search(value))
+
+def process_ignore(ignore):
+    if isinstance(ignore,basestring):
+        ignore = [ignore]
+    result = []
+    for i in ignore:
+        if not isinstance(i,RegexConversion):
+            i = RegexConversion(i)
+        result.append(i)
+    return result
