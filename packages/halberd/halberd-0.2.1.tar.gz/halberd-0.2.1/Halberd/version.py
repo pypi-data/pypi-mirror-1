9ed8f59f66854a3e3505ef0df48dd2e4ac9050a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:25:09 +0000

    Add package halberd/0.2.1/halberd-0.2.1.tar.gz

---
+++ packages/halberd/halberd-0.2.1.tar.gz/halberd-0.2.1/Halberd/version.py
@@ -0,0 +1,15 @@
+##
+##  ./Halberd/version.py -- Version Information for halberd (syntax: Python)
+##  [automatically generated and maintained by GNU shtool]
+##
+
+class version:
+    v_hex       = 0x002201
+    v_short     = "0.2.1"
+    v_long      = "0.2.1 (24-Aug-2006)"
+    v_tex       = "This is halberd, Version 0.2.1 (24-Aug-2006)"
+    v_gnu       = "halberd 0.2.1 (24-Aug-2006)"
+    v_web       = "halberd/0.2.1"
+    v_sccs      = "@(#)halberd 0.2.1 (24-Aug-2006)"
+    v_rcs       = "$Id: halberd 0.2.1 (24-Aug-2006) $"
+
