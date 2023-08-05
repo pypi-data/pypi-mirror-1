f97392996b1bb1f24b654919cdb0e44c07dff436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:56 +0000

    Add package ConfigAdministrator/0.1a0.dev-r457/ConfigAdministrator-0.1a0.dev_r457-py2.4.egg

---
+++ packages/ConfigAdministrator/ConfigAdministrator-0.1a0.dev_r457-py2.4.egg/config_admin/__init__.py
@@ -0,0 +1,33 @@
+import cherrypy
+
+import widgets
+
+import logging
+
+from controllers import ConfigServer
+
+from turbogears import config
+
+from model import create_models
+
+log = logging.getLogger("config_admin")
+
+mounted = False
+
+def start_extension():
+    global mounted
+    if not config.get("config_server.on", False):
+        return
+    if hasattr(cherrypy.root, 'config_admin'):
+        raise 'The path /conf_admin'
+    create_models()
+    cherrypy.root.config_admin = ConfigServer()
+    mounted = True
+
+def stop_extension():
+    global mounted
+    if not config.get("config_server.on", False):
+        return
+    if mounted:
+        delattr(cherrypy.root, 'config_admin')
+
