46fd9a206bddd619610bf79e74805fc33bea7b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:42 +0000

    Add package zope.size/3.4.0/zope.size-3.4.0.tar.gz

---
+++ packages/zope.size/zope.size-3.4.0.tar.gz/zope.size-3.4.0/setup.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.size package
+
+$Id: setup.py 78210 2007-07-20 03:11:25Z fdrake $
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.size',
+      version = '3.4.0',
+      url='http://svn.zope.org/zope.size',
+      license='ZPL 2.1',
+      description='Zope size',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="Interfaces and adapters that give the size of an object.",
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      tests_require = [],
+      install_requires=['setuptools',
+                        'zope.interface',
+                        'zope.i18nmessageid'],
+      include_package_data = True,
+      zip_safe = False,
+      )
