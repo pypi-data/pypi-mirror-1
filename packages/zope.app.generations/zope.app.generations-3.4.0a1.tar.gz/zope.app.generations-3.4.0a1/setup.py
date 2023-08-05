2facf01139de3f436fab6f37c6ecb25352b60406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package zope.app.generations/3.4.0a1/zope.app.generations-3.4.0a1.tar.gz

---
+++ packages/zope.app.generations/zope.app.generations-3.4.0a1.tar.gz/zope.app.generations-3.4.0a1/setup.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.generations package
+
+$Id: setup.py 74417 2007-04-22 10:35:04Z ctheune $
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.generations',
+      version = '3.4.0a1',
+      url='http://svn.zope.org/zope.app.generations',
+      license='ZPL 2.1',
+      description='Zope generations',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=['zope.app.testing',
+                                  'zope.app.securitypolicy',
+                                  'zope.app.zcmlfiles',
+                                  ]),
+      install_requires=['setuptools',
+                        'zope.app.renderer',
+                        'zope.app.testing',
+                        'zope.interface',
+                        'ZODB3'
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
