f80e5b3c4418cf5d40d58b25c1bd7417ef0436f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package plonetheme.andreas01/0.1dev-r56191/plonetheme.andreas01-0.1dev-r56191.tar.bz2

---
+++ packages/plonetheme.andreas01/plonetheme.andreas01-0.1dev-r56191.tar.bz2/plonetheme.andreas01-0.1dev-r56191/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='plonetheme.andreas01',
+      version=version,
+      description="An installable theme for Plone 3.0.",
+      long_description="""\
+This is a theme for Plone 3.0.  It is based on the Andreas 01 web template by Andreas Viklund""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Plone Collective',
+      author_email='product-developers@lists.plone.org',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
