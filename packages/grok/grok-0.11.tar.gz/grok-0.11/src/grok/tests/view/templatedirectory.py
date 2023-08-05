dc6d827dd33e1ec9e14f5aad0f9910c7f7ff0483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:45 +0000

    Add package grok/0.11/grok-0.11.tar.gz

---
+++ packages/grok/grok-0.11.tar.gz/grok-0.11/src/grok/tests/view/templatedirectory.py
@@ -0,0 +1,28 @@
+"""
+You can explicitly specify the template directory using grok.templatedir on module level:
+
+  >>> grok.testing.grok('grok.templatereg')
+  >>> grok.testing.grok(__name__)
+
+  >>> manfred = Mammoth()
+  >>> from zope.publisher.browser import TestRequest
+  >>> request = TestRequest()
+  >>> from zope import component
+  >>> view = component.getMultiAdapter((manfred, request), name='food')
+  >>> print view()
+  <html>
+  <body>
+  ME GROK EAT MAMMOTH!
+  </body>
+  </html>
+
+"""
+import grok
+
+grok.templatedir('templatedirectoryname')
+
+class Mammoth(grok.Model):
+    pass
+
+class Food(grok.View):
+    pass
