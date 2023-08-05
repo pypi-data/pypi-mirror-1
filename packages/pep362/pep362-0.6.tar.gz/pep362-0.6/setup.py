4fd00552bb215ee7d31c90ab7961b7bb2797b552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package pep362/0.6/pep362-0.6.tar.gz

---
+++ packages/pep362/pep362-0.6.tar.gz/pep362-0.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        # Package metadata.
+        name='pep362',
+        version='0.6',
+        description='Implementation of PEP 362 (Function Signature objects)',
+        author='Brett Cannon',
+        author_email='brett@python.org',
+        url='http://svn.python.org/view/sandbox/trunk/pep362/',
+        # Files.
+        py_modules=['pep362'],
+        data_files=['README'],
+    )
