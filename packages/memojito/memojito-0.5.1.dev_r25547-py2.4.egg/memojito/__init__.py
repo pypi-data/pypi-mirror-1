e8f42f0463b0d57d9e15251f3bd7e836316ba769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:24:36 +0000

    Add package memojito/0.5.1.dev-r25547/memojito-0.5.1.dev_r25547-py2.4.egg

---
+++ packages/memojito/memojito-0.5.1.dev_r25547-py2.4.egg/memojito/__init__.py
@@ -0,0 +1,61 @@
+"""
+see README.txt
+"""
+
+
+class version(object):
+    v_short     = "0.5.1"
+
+from StringIO import StringIO
+
+out = StringIO()
+
+_marker = object()
+class Memojito(object):
+    propname = '_memojito_'
+    def clear(self, inst):
+        if hasattr(inst, self.propname):
+            delattr(inst, self.propname)
+        
+    def clearbefore(self, func):
+        def clear(*args, **kwargs):
+            inst=args[0]
+            self.clear(inst)
+            return func(*args, **kwargs)
+        return clear
+
+    def clearafter(self, func):
+        def clear(*args, **kwargs):
+            inst=args[0]
+            val = func(*args, **kwargs)
+            self.clear(inst)
+            return val 
+        return clear
+
+    def memoizedproperty(self, func):
+        return property(self.memoize(func))
+
+    def memoize(self, func):
+        def memogetter(*args, **kwargs):
+            inst = args[0]
+            cache = getattr(inst, self.propname, dict())
+
+            # XXX this could be improved to unfold unhashables
+            # and optimized with pyrex
+            
+            key = (func.__name__, args, frozenset(kwargs.items()))
+            print >> out, key
+            key=hash(key)
+            val = cache.get(key, _marker)
+            if val is _marker:
+                val=func(*args, **kwargs)
+                cache[key]=val
+                setattr(inst, self.propname, cache)
+            return val
+        return memogetter
+
+_m = Memojito()
+memoize = _m.memoize
+memoizedproperty = _m.memoizedproperty
+clearbefore = _m.clearbefore
+clearafter = _m.clearafter
