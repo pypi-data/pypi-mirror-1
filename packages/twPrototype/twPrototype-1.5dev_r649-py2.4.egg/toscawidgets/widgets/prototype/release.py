4cbffd687e1f44441fc4cf3bbcc852826898f856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:43 +0000

    Add package twPrototype/1.5dev-r649/twPrototype-1.5dev_r649-py2.4.egg

---
+++ packages/twPrototype/twPrototype-1.5dev_r649-py2.4.egg/toscawidgets/widgets/prototype/release.py
@@ -0,0 +1,11 @@
+# Metadata for the widget EGG. Should leave it for the (unimplemented) widget
+# tracker to track it
+
+__PROJECT__ = 'twPrototype'
+__DESCRIPTION__ = 'Wrapper for the Prototype Javascript Library for ToscaWidgets'
+__URL__ = ''
+__VERSION__ = '1.5'
+__AUTHOR__ = 'Claudio Martinez'
+__EMAIL__ = 'claudio.s.martinez@gmail.com'
+__COPYRIGHT__ = "Copyright 2007 Claudio Mart¡nez"
+__LICENSE__ = ''
