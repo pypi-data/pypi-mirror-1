86b0b0c50f038d83500fbb16796eb13980e56e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:18 +0000

    Add package plone.app.iterate/1.0a2/plone.app.iterate-1.0a2.tar.gz

---
+++ packages/plone.app.iterate/plone.app.iterate-1.0a2.tar.gz/plone.app.iterate-1.0a2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0a2'
+
+setup(name='plone.app.iterate',
+      version=version,
+      description="check-out/check-in staging for Plone",
+      long_description="""\
+plone.app.iterate is a check-out/check-in staging solution for Plone, 
+initially released as a Zope 2 product called "iterate". It uses CMFEditions,
+CMFDiffTool and other Plone 3 features.
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Kapil Thangavelu',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.app.iterate',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
