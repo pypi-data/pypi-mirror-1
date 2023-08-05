e58d993e532f8d5f3007c33226a207c567280ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package si/0.1/si-0.1.tar.gz

---
+++ packages/si/si-0.1.tar.gz/si-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name = "si",
+        version = "0.1",
+        packages = ["si"],
+        author = "chrysn",
+        author_email = "chrysn@fsfe.org",
+        description = "Module to represent SI units",
+        license = "GPL",
+        zip_safe = True,
+        test_suite = "test.all",
+        )
