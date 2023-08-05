e4af9f87c7c446efd7a6430ae346bd2dc6f99822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:13 +0000

    Add package 4Suite-XML/1.0/4Suite_XML-1.0-py2.3-win32.egg

---
+++ packages/4Suite-XML/4Suite_XML-1.0-py2.3-win32.egg/Ft/__config__.py
@@ -0,0 +1,28 @@
+# Configuration variables
+NAME     = '4Suite-XML'
+VERSION  = '1.0'
+FULLNAME = '4Suite-XML-1.0'
+URL      = 'http://4suite.org/'
+
+import sys
+if getattr(sys, 'frozen', False):
+    # "bundled" installation locations (e.g., py2exe, cx_Freeze)
+    RESOURCEBUNDLE = True
+    PYTHONLIBDIR   = '\\'
+    BINDIR         = None
+    DATADIR        = '\\Share'
+    SYSCONFDIR     = None
+    LOCALSTATEDIR  = None
+    LIBDIR         = None
+    LOCALEDIR      = '\\Share\\Locale'
+else:
+    # standard distutils installation directories
+    RESOURCEBUNDLE = True
+    PYTHONLIBDIR   = '\\'
+    BINDIR         = None
+    DATADIR        = '\\Share'
+    SYSCONFDIR     = None
+    LOCALSTATEDIR  = None
+    LIBDIR         = None
+    LOCALEDIR      = '\\Share\\Locale'
+del sys
