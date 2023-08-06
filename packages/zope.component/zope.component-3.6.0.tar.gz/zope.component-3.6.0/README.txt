b010050a64a7acd9c3bb113477c77b0f02bd41e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:22 +0000

    Add package zope.component/3.6.0/zope.component-3.6.0.tar.gz

---
+++ packages/zope.component/zope.component-3.6.0.tar.gz/zope.component-3.6.0/README.txt
@@ -0,0 +1,9 @@
+*****************************
+zope.component Package Readme
+*****************************
+
+This package represents the core of the Zope Component Architecture.
+Together with the 'zope.interface' package, it provides facilities for
+defining, registering and looking up components.
+
+.. contents::
---
+++ packages/zope.component/zope.component-3.6.0.tar.gz/zope.component-3.6.0/setup.py
@@ -0,0 +1,83 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.component package
+
+$Id: setup.py 97993 2009-03-12 16:09:51Z nadako $
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='zope.component',
+    version = '3.6.0',
+    url='http://pypi.python.org/pypi/zope.component',
+    license='ZPL 2.1',
+    description='Zope Component Architecture',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'zope', 'component', 'README.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'event.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'factory.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'registry.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'persistentregistry.txt')
+        + '\n' +
+        read('src', 'zope', 'component', 'socketexample.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Download\n'
+        '********\n'
+        ),
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+    namespace_packages=['zope',],
+    tests_require = ['zope.testing'],
+    install_requires=['setuptools',
+                      'zope.interface',
+                      'zope.event',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    extras_require = dict(
+        hook = ['zope.hookable'],
+        persistentregistry = ['ZODB3'],
+        zcml = ['zope.configuration',
+                'zope.security',
+                'zope.proxy',
+                'zope.i18nmessageid',
+                ],
+        test = ['ZODB3',
+                'zope.testing',
+                'zope.hookable',
+                'zope.location',
+                ],
+        docs = ['z3c.recipe.sphinxdoc'],
+        ),
+    )
