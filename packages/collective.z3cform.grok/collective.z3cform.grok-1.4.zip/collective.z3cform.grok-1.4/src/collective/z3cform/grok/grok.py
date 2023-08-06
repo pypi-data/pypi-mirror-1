37a67177f708f0f33ee9326d7b355b4798dcb1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:56 +0000

    Add package collective.z3cform.grok/1.4/collective.z3cform.grok-1.4.zip

---
+++ packages/collective.z3cform.grok/collective.z3cform.grok-1.4.zip/collective.z3cform.grok-1.4/src/collective/z3cform/grok/grok.py
@@ -0,0 +1,114 @@
+from five  import grok
+
+import z3c.form
+import plone.z3cform
+
+from zope.interface import alsoProvides
+from zope.interface.interfaces import IInterface
+from Acquisition import aq_inner
+
+import zope
+import Products
+class FormWrapper(grok.View):
+    """
+    Base wrapper to z3c.form, just register a class which set the form attribute.
+    You have a classical grok.View instance with an attribute ``form_instance``
+    which is the wrapped z3c.form.form.Form instance.
+    >>> class Klass(FormWrapper):
+    ...      context = grok.context(someinterface)
+    ...      form = FormKlass
+    You can also override the layer attribute.
+    """
+    grok.baseclass()
+    grok.context(zope.interface.Interface)
+    grok.implements(plone.z3cform.interfaces.IFormWrapper)
+    form               = None # z3c.form class
+    index              = None # template
+    view_request_layer = None # wrapper view layer
+    form_request_layer = None # form instance layer
+
+    def __init__(self,  *args, **kwargs):
+        grok.View.__init__(self, *args, **kwargs)
+        if self.view_request_layer: self.addLayer(self.view_request_layer)
+        self.switch_on_form_layer()
+        self.switch_on_form_layer(plone.z3cform.interfaces.IFormWrapper)
+        self.form_instance = self.form(aq_inner(self.context), self.request)
+        self.form_instance.__name__ = self.__name__
+        if getattr(self, 'template', None):
+            self.index = self.template
+
+    def switch_on_form_layer(self, layer=None):
+        """
+        we need that to render forms in zope2 !
+        """
+        if not layer:
+            layer = z3c.form.interfaces.IFormLayer
+            if self.form_request_layer:
+                layer = self.form_request_layer
+        # check that the layer is interface:
+        if IInterface.providedBy(layer):
+            if not layer.providedBy(self.request):
+                # request must be patched for forms to be rendered in Zope2
+                if plone.z3cform.z2.IFixedUpRequest.providedBy(self.request):
+                    self.addLayer(layer)
+                else:
+                    plone.z3cform.z2.switch_on(self, layer)
+
+    def addLayer(self, layer):
+        alsoProvides(self.request, layer)
+
+    def update(self, *args, **kwargs):
+        self.compute_widgets()
+        grok.View.update(self, *args, **kwargs)
+
+    def compute_widgets(self):
+        """
+        shortcut 'widget' dictionary for all fieldsets
+        stolen from plone.autoform_instance/plone/autoform_instance/view.py
+        """
+        self.form_instance.update()
+        self.form_instance.w = {}
+        for k, v in self.form_instance.widgets.items():
+            self.form_instance.w[k] = v
+        groups = []
+        self.form_instance.fieldsets = {}
+        if getattr(self.form_instance, 'groups', None):
+            for idx, group in enumerate(self.form_instance.groups):
+                #group = groupFactory(self.form_instance.context,
+                #                     self.form_instance.request,
+                #                     self.form_instance)
+                group.update()
+                for k, v in group.widgets.items():
+                    self.form_instance.w[k] = v
+                groups.append(group)
+                group_name = getattr(group, '__name__', str(idx))
+                self.form_instance.fieldsets[group_name] = group
+            self.form_instance.groups = tuple(groups)
+
+    def render_form(self):
+        """
+        call this method in templates to have the full html rendered form
+        """
+        self.compute_widgets()
+        return self.form_instance.render()
+
+    # stolen from plone.directives.form.form.DisplayForm
+    def render(self, *args, **kwargs):
+        template = getattr(self, 'template', None)
+        if template is not None:
+            return self.template.render()
+        return zope.publisher.publish.mapply(self.render, (), self.request)
+    render.base_method = True
+
+class PloneFormWrapper(FormWrapper):
+    """
+    Use that nice plone with fieldsets and kss template 
+    explicitly rather than the basic z3c.form template
+    """
+    grok.baseclass()
+    view_request_layer = Products.CMFDefault.interfaces.ICMFDefaultSkin
+
+class PloneForm(plone.z3cform.fieldsets.extensible.ExtensibleForm, z3c.form.form.Form):
+    grok.baseclass()
+
+
---
+++ packages/collective.z3cform.grok/collective.z3cform.grok-1.4.zip/collective.z3cform.grok-1.4/src/collective/z3cform/grok/tests/base.py
@@ -0,0 +1,74 @@
+import zope
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import onsetup
+
+@onsetup
+def setup_collective_z3cform_grok():
+    """Set up the additional products required for the nmd.sugar) site policy.
+
+    The @onsetup decorator causes the execution of this body to be deferred
+    until the setup of the Plone site testing layer.
+    """
+    # ------------------------------------------------------------------------------------
+    # Get five errors if any for making debug easy.
+    # ------------------------------------------------------------------------------------
+    fiveconfigure.debug_mode = True
+
+    # ------------------------------------------------------------------------------------
+    # Import all our python modules required by our packages
+    # ------------------------------------------------------------------------------------
+#with_ploneproduct_pz3cform
+    import five.grok
+    import plone.z3cform
+    import plone.app.z3cform
+
+    # ------------------------------------------------------------------------------------
+    # - Load the ZCML configuration for the collective.z3cform.grok package.
+    # ------------------------------------------------------------------------------------
+
+
+#with_ploneproduct_pz3cform
+    zcml.load_config('configure.zcml', five.grok)
+#with_ploneproduct_facultystaff
+    zcml.load_config('configure.zcml', plone.z3cform)
+    zcml.load_config('configure.zcml', plone.app.z3cform)
+
+    # ------------------------------------------------------------------------------------
+    # - Load the python packages that are registered as Zope2 Products via Five
+    #   which can't happen until we have loaded the package ZCML.
+    # ------------------------------------------------------------------------------------
+
+
+    # ------------------------------------------------------------------------------------
+    # Load our own policy
+    # ------------------------------------------------------------------------------------
+    import collective.z3cform.grok
+    zcml.load_config('configure.zcml', collective.z3cform.grok)
+
+    # ------------------------------------------------------------------------------------
+    # Reset five debug mode as we do not use it anymore
+    # ------------------------------------------------------------------------------------
+    fiveconfigure.debug_mode = False
+
+# The order here is important: We first call the (deferred) function which
+# installs the products we need for the nmd.sugar package. Then, we let
+# PloneTestCase set up this product on installation.
+
+setup_collective_z3cform_grok()
+ptc.setupPloneSite(products=[\
+# if we have csvreplicata, just say that a plone site can't live without :)
+    'csvreplicata',
+    'collective.z3cform.grok']
+)
+
+class collective_z3cform_grok_PolicyTestCase(ptc.PloneTestCase):
+    """We use this base class for all the tests in this package. If necessary,
+    we can put common utility or setup code in here.
+    """
+
+class collective_z3cform_grok_PolicyFunctionalTestCase(ptc.FunctionalTestCase):
+    """
+    """
