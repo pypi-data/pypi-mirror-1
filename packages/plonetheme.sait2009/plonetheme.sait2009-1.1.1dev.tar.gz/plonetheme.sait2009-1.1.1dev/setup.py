4b44e022e9e84dccbfb96af7f39ea89b4aa90ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:47 +0000

    Add package plonetheme.sait2009/1.1.1dev/plonetheme.sait2009-1.1.1dev.tar.gz

---
+++ packages/plonetheme.sait2009/plonetheme.sait2009-1.1.1dev.tar.gz/plonetheme.sait2009-1.1.1dev/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.1'
+
+setup(name='plonetheme.sait2009',
+      version=version,
+      description="SAIT-2009 installable Plone theme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme sait 2009',
+      author='makukha',
+      author_email='m.makukha@gmail.com',
+      url='http://sait.org.ua',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
