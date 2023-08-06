ac2c0726cfe116e38dd61c6b6bf3ed4debe34f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:52 +0000

    Add package timetric/1.3/timetric-1.3.tar.gz

---
+++ packages/timetric/timetric-1.3.tar.gz/timetric-1.3/setup.py
@@ -0,0 +1,31 @@
+import os
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name = "timetric",
+    version = "1.3",
+    description = "Client library for Timetric (http://timetric.com/)",
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = 'Jacob Kaplan-Moss',
+    author_email = 'jacob@jaobian.org',
+    url = 'http://github.com/jacobian/timetric',
+    packages = ['timetric'],
+    install_requires = [
+        'httplib2',
+        'python-dateutil',
+        'simplejson',
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+    ]
+)
