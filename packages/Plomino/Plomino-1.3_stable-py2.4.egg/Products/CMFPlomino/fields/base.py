ab138aac455c3f98abc03b0dda36e97cc69d01bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:51 +0000

    Add package Plomino/1.3-stable/Plomino-1.3_stable-py2.4.egg

---
+++ packages/Plomino/Plomino-1.3_stable-py2.4.egg/Products/CMFPlomino/fields/base.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# File: base.py
+#
+# Copyright (c) 2008 by ['Eric BREHAULT']
+#
+# Zope Public License (ZPL)
+#
+
+__author__ = """Eric BREHAULT <eric.brehault@makina-corpus.org>"""
+__docformat__ = 'plaintext'
+
+from zope.interface import Interface, implements
+from zope.annotation.interfaces import IAnnotations
+from persistent.dict import PersistentDict
+
+class IBaseField(Interface):
+    """
+    """
+
+class BaseField(object):
+    """
+    """
+    implements(IBaseField)
+    
+    def __init__(self, context):
+        """Initialize adapter."""
+        self.context = context
+        # get annotation to store param values
+        annotations = IAnnotations(context)
+        self.parameters = annotations.get("PLOMINOFIELDCONFIG", None)
+        if not self.parameters:
+            annotations["PLOMINOFIELDCONFIG"] = PersistentDict()
+            self.parameters = annotations["PLOMINOFIELDCONFIG"]
+            
+    def validate(self, strValue):
+        """
+        """
+        errors=[]
+        return errors
+    
+    def processInput(self, strValue):
+        """
+        """
+        return strValue
