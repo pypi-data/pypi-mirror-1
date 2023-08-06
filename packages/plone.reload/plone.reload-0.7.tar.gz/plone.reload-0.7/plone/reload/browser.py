834e0340fa03e3bb9d156d70743a26bddd68a015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:53 +0000

    Add package plone.reload/0.7/plone.reload-0.7.tar.gz

---
+++ packages/plone.reload/plone.reload-0.7.tar.gz/plone.reload-0.7/plone/reload/browser.py
@@ -0,0 +1,59 @@
+from zope.interface import implements
+
+from Acquisition import aq_base
+from Products.Five.browser import BrowserView
+
+from plone.reload.code import reload_code
+from plone.reload.interfaces import IReload
+from plone.reload.zcml import reload_zcml
+
+
+class Reload(BrowserView):
+    """Reload view.
+    """
+    implements(IReload)
+
+    def __init__(self, context, request):
+        BrowserView.__init__(self, context, request)
+        self.message = None
+
+    def __call__(self):
+        action = self.request.form.get('action')
+        if action is not None:
+            if action == 'code':
+                self.message = self.code_reload()
+            elif action == 'zcml':
+                self.message = self.zcml_reload()
+        return self.index()
+
+    def status(self):
+        return self.message
+
+    def code_reload(self):
+        reloaded = reload_code()
+
+        result = ''
+        if reloaded:
+            result += 'Code reloaded:\n\n'
+            result += '\n'.join(reloaded)
+        else:
+            result = 'No code reloaded!'
+        return result
+
+    def zcml_reload(self):
+        # We always do an implicit code reload so we can register all newly
+        # added classes.
+        reloaded = reload_code()
+        reload_zcml()
+
+        # TODO Minimize all caches, we only really want to invalidate the
+        # local site manager from all caches
+        aq_base(self.context)._p_jar.db().cacheMinimize()
+        result = ''
+        if reloaded:
+            result += 'Code reloaded:\n\n'
+            result += '\n'.join(reloaded)
+        else:
+            result = 'No code reloaded!'
+        result += '\n\nGlobal ZCML reloaded.'
+        return result
