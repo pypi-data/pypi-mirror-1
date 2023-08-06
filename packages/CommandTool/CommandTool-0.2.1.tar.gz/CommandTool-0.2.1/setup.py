21a6561dea5c5dd280e74ba2c7f848a7f31241c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:22 +0000

    Add package CommandTool/0.2.1/CommandTool-0.2.1.tar.gz

---
+++ packages/CommandTool/CommandTool-0.2.1.tar.gz/CommandTool-0.2.1/setup.py
@@ -0,0 +1,48 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import sys, os
+
+version = '0.2.1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    "\n"+read('doc/index.txt')
+    + '\n'
+    + read('CHANGELOG.txt')
+    + '\n'
+    'License\n'
+    '=======\n'
+    + read('LICENSE.txt')
+    + '\n'
+    'Download\n'
+    '========\n'
+)
+
+setup(
+    name='CommandTool',
+    version=version,
+    description="Tools for creating command line interfaces involving sub-commands.",
+    long_description=long_description,
+    # Get classifiers from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
+    keywords='',
+    author='James Gardner',
+    author_email='',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "docutils>=0.5.0",
+    ],
+    entry_points="""
+    """,
+)
