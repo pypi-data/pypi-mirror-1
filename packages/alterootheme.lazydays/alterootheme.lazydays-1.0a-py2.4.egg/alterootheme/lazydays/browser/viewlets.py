8c1f3cdad68b0a6dd87d71cbd7696e741f2eba19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:29 +0000

    Add package alterootheme.lazydays/1.0a/alterootheme.lazydays-1.0a-py2.4.egg

---
+++ packages/alterootheme.lazydays/alterootheme.lazydays-1.0a-py2.4.egg/alterootheme/lazydays/browser/viewlets.py
@@ -0,0 +1,7 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets import common
+
+class GlobalSectionsViewlet(common.GlobalSectionsViewlet):
+    render = ViewPageTemplateFile("templates/sections.pt")
+
