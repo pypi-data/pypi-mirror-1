12a8e283593d3567dd22861511fa983500b728b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:37 +0000

    Add package zc.z3monitor/0.5/zc.z3monitor-0.5.tar.gz

---
+++ packages/zc.z3monitor/zc.z3monitor-0.5.tar.gz/zc.z3monitor-0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+name = 'zc.z3monitor'
+
+long_description = (open('src/zc/z3monitor/README.txt').read() +
+                    "\n\n" +
+                    open('src/zc/z3monitor/CHANGES.txt').read())
+
+setup(
+    name = name,
+    version = '0.5',
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    license = 'ZPL 2.1',
+    keywords = 'zope3',
+    description=open('README.txt').read(),
+    long_description=long_description,
+
+    packages = find_packages('src'),
+    namespace_packages = ['zc'],
+    package_dir = {'': 'src'},
+    install_requires = [
+        'setuptools', 'zc.ngi', 'ZODB3', 'zope.component',
+        'zope.publisher', 'zope.app.appsetup',
+        ],
+    include_package_data = True,
+    zip_safe = False,
+    )
