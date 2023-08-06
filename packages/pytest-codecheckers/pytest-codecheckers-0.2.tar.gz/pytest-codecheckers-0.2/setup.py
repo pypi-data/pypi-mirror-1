869586f4d3ade6fdc1bf3524ac26779445a073ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:01 +0000

    Add package pytest-codecheckers/0.2/pytest-codecheckers-0.2.tar.gz

---
+++ packages/pytest-codecheckers/pytest-codecheckers-0.2.tar.gz/pytest-codecheckers-0.2/setup.py
@@ -0,0 +1,42 @@
+"""
+py.test plugin for checking PEP8 source code compliance using pyflakes.
+
+Usage
+---------
+
+after installation (e.g. via ``pip install pytest-codecheckers``) you can type::
+
+    py.test [path/to/mypkg]
+
+which will automatically perform source code sanity checks.  If you have
+further questions please send them to the `pytest-dev`_ mailing list.
+
+.. _`pytest-dev`: http://codespeak.net/mailman/listinfo/py-dev
+"""
+from setuptools import setup
+
+setup(
+    name='pytest-codecheckers',
+    description='pytest plugin to add source code sanity checks (pep8 and friends)',
+    long_description=__doc__,
+    version='0.2',
+    author='Ronny Pfannschmidt',
+    author_email='Ronny.Pfannschmidt@gmx.de',
+    packages=[
+        'codecheckers',
+        ],
+    entry_points={
+        'pytest11': [
+            'codechecker = codecheckers.plugin',
+            ],
+        'codechecker': [
+            'pep8 = codecheckers.pep',
+            'pyflakes = codecheckers.flakes',
+            ],
+        },
+    install_requires=[
+        'py>=1.2.0',
+        'pyflakes>=0.4',
+        'pep8',
+        ],
+    )
