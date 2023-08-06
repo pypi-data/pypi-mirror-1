2116388f62a8cb8db64b54222354bd8e0c190a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:30 +0000

    Add package sqlbean/0.37/sqlbean-0.37.tar.gz

---
+++ packages/sqlbean/sqlbean-0.37.tar.gz/sqlbean-0.37/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+version = '0.37'
+
+setup(name="sqlbean",
+        version=version,
+        description="A auto maping orm .",
+        long_description=""" """,
+        author="zsp",
+        author_email="zsp007@gmail.com",
+        packages=find_packages(),
+        install_requires = ['dbutils','decorator'],
+)
