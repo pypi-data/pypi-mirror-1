9983f5cc3bc5d43728a6c9b4edcd314776e4f8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:45 +0000

    Add package grokcore.component/1.7/grokcore.component-1.7.tar.gz

---
+++ packages/grokcore.component/grokcore.component-1.7.tar.gz/grokcore.component-1.7/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+setup(
+    name='grokcore.component',
+    version = '1.7',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://pypi.python.org/pypi/grokcore.component',
+    description='Grok-like configuration for basic components '
+                '(adapters, utilities, subscribers)',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=['Intended Audience :: Developers',
+                 'License :: OSI Approved :: Zope Public License',
+                 'Programming Language :: Python',
+                 ],
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools',
+                      'martian >= 0.10',
+                      'zope.component',
+                      'zope.configuration',
+                      'zope.interface',
+                      'zope.event', 
+                      'zope.testing',
+                      ],
+)
