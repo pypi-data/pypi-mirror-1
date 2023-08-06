72b24a4a2938cbf62adac9d1923e15806cbecbf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:01 +0000

    Add package schemaish/0.5.3/schemaish-0.5.3.tar.gz

---
+++ packages/schemaish/schemaish-0.5.3.tar.gz/schemaish-0.5.3/schemaish/__init__.py
@@ -0,0 +1,8 @@
+"""
+A high-level schema definition package, using formencode for validation.
+"""
+
+# Import all public (or at least all frequently used) names from the schemaish
+# package.
+from schemaish.attr import *
+
