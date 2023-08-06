6bc780075d056b32cc2819d34d846c776f4d34f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:35 +0000

    Add package texttree/1.0.2/texttree-1.0.2.zip

---
+++ packages/texttree/texttree-1.0.2.zip/texttree-1.0.2/setup.py
@@ -0,0 +1,21 @@
+import texttree
+
+from distutils.core import setup
+setup(name='texttree',
+    version='1.0.2',
+    description="Parse textual representations of strings using indentation for nesting",
+    long_description="""
+texttree is a basic python module for parsing textual representations of trees
+of strings which use indentation to indicate nesting - like the following:
+
+Root
+     non-leaf
+         leaf1
+          leaf2
+     leaf3
+""",
+    author='Tom Wright',
+    author_email="tat.wright@googlemail.com",
+    url="http://tat.wright.name/texttree/",
+    classifiers=["License :: OSI Approved :: BSD License"],
+    py_modules=['texttree'])
