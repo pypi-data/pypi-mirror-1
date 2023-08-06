c3f35fc7f5f170db6cdd4613994e33d795334c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:09 +0000

    Add package chiffre/1.0/chiffre-1.0.tar.gz

---
+++ packages/chiffre/chiffre-1.0.tar.gz/chiffre-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name = "chiffre",
+      version = "1.0",
+      author = "ibot",
+      author_email = "ibot@keckeisen.net",
+      packages = ['chiffre']
+)
+
