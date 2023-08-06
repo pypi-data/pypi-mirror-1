1bb4268124e4814af5b737bc6a253bca6766fc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:55 +0000

    Add package Products.LinguaPlone/3.0c4/Products.LinguaPlone-3.0c4.zip

---
+++ packages/Products.LinguaPlone/Products.LinguaPlone-3.0c4.zip/Products.LinguaPlone-3.0c4/Products/LinguaPlone/I18NBaseBTreeFolder.py
@@ -0,0 +1,35 @@
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import BaseBTreeFolder
+
+try:
+    from App.class_init import InitializeClass
+except ImportError:
+    from Globals import InitializeClass
+
+from Products.LinguaPlone.I18NBaseObject import I18NBaseObject
+
+
+class I18NOnlyBaseBTreeFolder(I18NBaseObject):
+    """Base class for translatable objects."""
+
+    security = ClassSecurityInfo()
+
+    def __nonzero__(self):
+        return 1
+
+    def manage_beforeDelete(self, item, container):
+        I18NBaseObject.manage_beforeDelete(self, item, container)
+
+InitializeClass(I18NOnlyBaseBTreeFolder)
+
+
+class I18NBaseBTreeFolder(I18NOnlyBaseBTreeFolder, BaseBTreeFolder):
+    """Base class for translatable objects."""
+
+    security = ClassSecurityInfo()
+
+    def manage_beforeDelete(self, item, container):
+        I18NOnlyBaseBTreeFolder.manage_beforeDelete(self, item, container)
+        BaseBTreeFolder.manage_beforeDelete(self, item, container)
+
+InitializeClass(I18NBaseBTreeFolder)
