a71416e6ed82eef50a79f0871c7378fa4c0e687e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:40 +0000

    Add package collective.solr/1.0b2/collective.solr-1.0b2.zip

---
+++ packages/collective.solr/collective.solr-1.0b2.zip/collective.solr-1.0b2/collective/solr/tests/test_doctests.py
@@ -0,0 +1,35 @@
+from unittest import TestSuite
+from zope.testing import doctest
+from Testing import ZopeTestCase as ztc
+from plone.app.controlpanel.tests.cptc import ControlPanelTestCase
+from collective.solr.tests.base import SolrFunctionalTestCase
+from collective.solr.tests.utils import pingSolr
+
+optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
+               doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+
+
+def test_suite():
+    suite = TestSuite([
+        ztc.FunctionalDocFileSuite(
+           'configlet.txt', package='collective.solr.tests',
+           test_class=ControlPanelTestCase, optionflags=optionflags),
+    ])
+    if pingSolr():
+        suite.addTest(
+            ztc.FunctionalDocFileSuite(
+               'errors.txt', package='collective.solr.tests',
+               test_class=SolrFunctionalTestCase, optionflags=optionflags),
+        )
+        suite.addTest(
+            ztc.FunctionalDocFileSuite(
+               'search.txt', package='collective.solr.tests',
+               test_class=SolrFunctionalTestCase, optionflags=optionflags),
+        )
+        suite.addTest(
+            ztc.FunctionalDocFileSuite(
+               'conflicts.txt', package='collective.solr.tests',
+               test_class=SolrFunctionalTestCase, optionflags=optionflags),
+        )
+    return suite
+
---
+++ packages/collective.solr/collective.solr-1.0b2.zip/collective.solr-1.0b2/collective/solr/tests/test_exportimport.py
@@ -0,0 +1,75 @@
+from unittest import defaultTestLoader
+from zope.component import getUtility
+from Products.GenericSetup.tests.common import TarballTester
+from StringIO import StringIO
+
+from collective.solr.tests.base import SolrTestCase
+from collective.solr.interfaces import ISolrConnectionConfig
+
+
+class SetupToolTests(SolrTestCase, TarballTester):
+
+    def afterSetUp(self):
+        config = getUtility(ISolrConnectionConfig)
+        config.active = False
+        config.host = 'foo'
+        config.port = 23
+        config.base = '/bar'
+        config.async = False
+        config.index_timeout = 7
+        config.search_timeout = 3.1415
+        config.max_results = 42
+        config.required = ('foo', 'bar')
+
+    def testImportStep(self):
+        tool = self.portal.portal_setup
+        result = tool.runImportStepFromProfile('profile-collective.solr:default', 'solr')
+        self.assertEqual(result['steps'], [u'componentregistry', 'solr'])
+        self.failUnless(result['messages']['solr'].endswith('collective.solr: settings imported.'))
+        config = getUtility(ISolrConnectionConfig)
+        self.assertEqual(config.active, False)
+        self.assertEqual(config.host, '127.0.0.1')
+        self.assertEqual(config.port, 8983)
+        self.assertEqual(config.base, '/solr')
+        self.assertEqual(config.async, False)
+        self.assertEqual(config.index_timeout, 0)
+        self.assertEqual(config.search_timeout, 0)
+        self.assertEqual(config.max_results, 0)
+        self.assertEqual(config.required, ('SearchableText',))
+
+    def testExportStep(self):
+        tool = self.portal.portal_setup
+        result = tool.runExportStep('solr')
+        self.assertEqual(result['steps'], ['solr'])
+        self.assertEqual(result['messages']['solr'], None)
+        tarball = StringIO(result['tarball'])
+        self._verifyTarballContents(tarball, ['solr.xml'])
+        self._verifyTarballEntryXML(tarball, 'solr.xml', SOLR_XML)
+
+
+SOLR_XML = """\
+<?xml version="1.0"?>
+<object name="solr">
+  <connection>
+    <active value="False" />
+    <host value="foo" />
+    <port value="23" />
+    <base value="/bar" />
+  </connection>
+  <settings>
+    <async value="False" />
+    <index-timeout value="7" />
+    <search-timeout value="3.1415" />
+    <max-results value="42" />
+    <required-query-parameters>
+      <parameter name="foo" />
+      <parameter name="bar" />
+    </required-query-parameters>
+  </settings>
+</object>
+"""
+
+
+def test_suite():
+    return defaultTestLoader.loadTestsFromName(__name__)
+
