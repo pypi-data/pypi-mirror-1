4c49f9378914a383ce0a71507283f8cdeacc4876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:01 +0000

    Add package ConceptNet/4.0b6/ConceptNet-4.0b6.tar.gz

---
+++ packages/ConceptNet/ConceptNet-4.0b6.tar.gz/ConceptNet-4.0b6/csc/nl/nl/__init__.py
@@ -0,0 +1,5 @@
+from csc.nl.euro import EuroNL
+
+class NL(EuroNL):
+    def __init__(self):
+        super(NL, self).__init__('nl')
---
+++ packages/ConceptNet/ConceptNet-4.0b6.tar.gz/ConceptNet-4.0b6/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+packages = find_packages(exclude=['maint'])
+version_str = '4.0b6'
+
+setup(  name='ConceptNet',
+        version=version_str,
+        description='A Python API to a Semantic Network Representation of the Open Mind Common Sense Project',
+        author='Catherine Havasi, Robert Speer, Jason Alonso, and Kenneth Arnold',
+        author_email='conceptnet@media.mit.edu',
+        url='http://conceptnet.media.mit.edu/',
+        packages=packages,
+        include_package_data=False,
+        namespace_packages = ['csc'],
+        install_requires=['PyStemmer', 'csc-utils', 'django'],
+        package_data={'csc.nl': ['mblem/*.pickle']},
+
+        # Metadata
+        license = "GPL v2",
+        )
