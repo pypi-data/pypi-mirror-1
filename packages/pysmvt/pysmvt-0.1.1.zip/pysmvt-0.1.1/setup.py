893b12d6b0ac94acde30fb6bd1d7ab28212cb2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:57 +0000

    Add package pysmvt/0.1.1/pysmvt-0.1.1.zip

---
+++ packages/pysmvt/pysmvt-0.1.1.zip/pysmvt-0.1.1/setup.py
@@ -0,0 +1,106 @@
+"""
+Introduction
+---------------
+
+pysmvt is a wsgi web framework library designed in the spirit of Pylons but with
+Django modularity (i.e. what they would call "apps").  If you want to try it out
+it would be best to start with
+`our example application <http://pypi.python.org/pypi/PysAppExample/>`_.
+
+Steps for Installation
+----------------------
+
+#. Install Python
+#. install setuptools (includes easy_install)
+#. install virtualenv `easy_install virtualenv`
+#. Create a new virtual environement `virtualenv myproj-staging --no-site-packages`
+#. `Activate the virtual environment (os dependent) <http://pypi.python.org/pypi/virtualenv#activate-script>`_
+#. install pysmvt & dependencies `easy_install pysmvt`
+
+Steps for creating a working application
+-----------------------------------------
+#. `cd myproj-staging`
+#. `mkdir src`
+#. `cd src`
+#. `pysmvt project myapp`
+#. answer the questions that come up.  Note what you put for
+    "Enter author (your name)" as <user>.  If you forget, look in myapp/settings.py.
+#. `cd myapp-dist`
+#. `python setup.py -q develop`
+#.  `nosetests` you should get three succesful tests
+#. `cd myapp`
+#. `pysmvt serve <user>` run a development http server with the user's settings 
+   profile
+#. point your browser at http://localhost:5000/
+    
+Creating a New Application Module
+---------------------------------
+This step creates a Application Module directory structure in myapp/modules/<mymod>:
+
+`pysmvt module <mymod>`
+
+where <mymod> is the name of the module you want to create
+
+Questions & Comments
+---------------------
+
+Please visit: http://groups.google.com/group/pyslibs
+
+Current Status
+---------------
+
+The code for 0.1 is pretty stable.  API, however, will be changing in 0.2.
+
+The unstable `development version
+<https://svn.rcslocal.com:8443/svn/pysmvt/pysmvt/trunk#egg=pysmvt-dev>`_.
+"""
+import sys
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name = "pysmvt",
+    version = "0.1.1",
+    description = "A wsgi web framework with a pylons spirit and django modularity",
+    long_description = __doc__,
+    author = "Randy Syring",
+    author_email = "randy@rcs-comp.com",
+    url='http://pypi.python.org/pypi/pysmvt/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+      ],
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    install_requires = [
+        "Beaker>=1.1.3",
+        "decorator>=3.0.1",
+        "FormEncode>=1.2.2",
+        "html2text>=2.35",
+        "jinja2>=2.1.1",
+        "markdown2>=1.0.1.11",
+        "nose>=0.10.4",
+        "Paste>=1.7.2",
+        "PasteScript>=1.7.3",
+        "pysutils>=0.1",
+        "Werkzeug>=0.5"
+    ],
+    entry_points="""
+    [console_scripts]
+    pysmvt = pysmvt.script:main
+    
+    [pysmvt.pysmvt_project_template]
+    pysmvt = pysmvt.paster_tpl:ProjectTemplate
+    
+    [pysmvt.pysmvt_module_template]
+    pysmvt = pysmvt.paster_tpl:ModuleTemplate
+
+    """,
+    zip_safe=False
+)
---
+++ packages/pysmvt/pysmvt-0.1.1.zip/pysmvt-0.1.1/tests/test_routing.py
@@ -0,0 +1,125 @@
+import config
+import unittest
+
+from pysmvt.routing import *
+from pysmvt.exceptions import SettingsError
+from pysmvttestapp.applications import make_wsgi
+from werkzeug import Client, BaseResponse, create_environ
+
+class RoutingSettings(config.Testruns):
+    def __init__(self):
+        config.Testruns.__init__(self)
+    
+        self.routing.routes.extend([
+            Rule('/<file>', endpoint='static', build_only=True),
+            Rule('/c/<file>', endpoint='styles', build_only=True),
+            Rule('/js/<file>', endpoint='javascript', build_only=True),
+            Rule('/', endpoint='mod:Index'),
+            Rule('/url1', endpoint='mod:Url1'),
+        ])
+
+class Prefixsettings(RoutingSettings):
+    def __init__(self):
+        RoutingSettings.__init__(self)
+        
+        self.routing.prefix = '/prefix'
+
+class TestRouting(unittest.TestCase):
+    def setUp(self):
+        self.app = config.make_console(RoutingSettings)
+        self.app.start_request()
+    
+    def tearDown(self):
+        self.app.end_request()
+        self.app = None
+    
+    def test_routes(self):
+        self.assertEqual( '/url1', url_for('mod:Url1'))
+        self.assertEqual('/url1?foo=bar', url_for('mod:Url1', foo='bar'))
+        self.assertEqual('http://localhost/url1', url_for('mod:Url1', True))
+        self.assertEqual('/c/test.css', style_url('test.css'))
+        self.assertEqual('/c/test.css', style_url('test.css', app='foo'))
+        self.assertEqual('/js/test.js', js_url('test.js'))
+        self.assertEqual('/js/test.js', js_url('test.js', app='foo'))
+        self.assertEqual('https://localhost/url1', url_for('mod:Url1', _https=True))
+        self.assertEqual('http://localhost/url1', url_for('mod:Url1', _https=False, _external=True))
+        
+
+class TestPrefix(unittest.TestCase):
+    def setUp(self):
+        self.app = config.make_console(Prefixsettings)
+        self.app.start_request()
+    
+    def tearDown(self):
+        self.app.end_request()
+        self.app = None
+    
+    def test_routes(self):
+        self.assertEqual('/prefix/url1', url_for('mod:Url1'))
+        self.assertEqual('/prefix/url1?foo=bar', url_for('mod:Url1', foo='bar'))
+        self.assertEqual('http://localhost/prefix/url1', url_for('mod:Url1', True))
+        self.assertEqual('/prefix/c/test.css', style_url('test.css'))
+        self.assertEqual('/prefix/c/test.css', style_url('test.css', app='foo'))
+        self.assertEqual('/prefix/js/test.js', js_url('test.js'))
+        self.assertEqual('/prefix/js/test.js', js_url('test.js', app='foo'))
+        self.assertEqual('https://localhost/prefix/url1', url_for('mod:Url1', _https=True))
+        self.assertEqual('http://localhost/prefix/url1', url_for('mod:Url1', _https=False, _external=True))
+
+class TestCurrentUrl(unittest.TestCase):
+
+    def setUp(self):
+        self.app = make_wsgi('Testruns')
+        self.client = Client(self.app, BaseResponse)
+
+    def tearDown(self):
+        self.client = None
+        self.app = None
+
+    def test_in_view(self):
+        r = self.client.get('routingtests/currenturl?foo=bar')
+
+        self.assertEqual(r.status, '200 OK')
+        
+        self.assertEqual(r.data, 'http://localhost/routingtests/currenturl?foo=bar')
+
+    def test_arguments(self):
+        env = create_environ("/news/list?param=foo", "http://localhost:8080/script")
+        self.assertEqual('http://localhost:8080/script/news/list?param=foo', current_url(environ=env))
+        self.assertEqual('http://localhost:8080/script/', current_url(environ=env, root_only=True))
+        self.assertEqual('http://localhost:8080/', current_url(environ=env, host_only=True))
+        self.assertEqual('http://localhost:8080/script/news/list', current_url(environ=env, strip_querystring=True))
+        self.assertEqual('/script/news/list?param=foo', current_url(environ=env, strip_host=True))
+        self.assertEqual('http://localhost:8080/script/news/list?param=foo', current_url(environ=env, https=False))
+        self.assertEqual('http://localhost:8080/script/', current_url(environ=env, root_only=True, https=False))
+        self.assertEqual('http://localhost:8080/', current_url(environ=env, host_only=True, https=False))
+        self.assertEqual('http://localhost:8080/script/news/list', current_url(environ=env, strip_querystring=True, https=False))
+        self.assertEqual('/script/news/list?param=foo', current_url(environ=env, strip_host=True, https=False))
+
+        env = create_environ("/news/list?param=foo", "https://localhost:8080/script")
+        self.assertEqual('https://localhost:8080/script/news/list?param=foo', current_url(environ=env))
+        self.assertEqual('https://localhost:8080/script/', current_url(environ=env, root_only=True))
+        self.assertEqual('https://localhost:8080/', current_url(environ=env, host_only=True))
+        self.assertEqual('https://localhost:8080/script/news/list', current_url(environ=env, strip_querystring=True))
+        self.assertEqual('/script/news/list?param=foo', current_url(environ=env, strip_host=True))
+        self.assertEqual('https://localhost:8080/script/news/list?param=foo', current_url(environ=env, https=True))
+        self.assertEqual('https://localhost:8080/script/', current_url(environ=env, root_only=True, https=True))
+        self.assertEqual('https://localhost:8080/', current_url(environ=env, host_only=True, https=True))
+        self.assertEqual('https://localhost:8080/script/news/list', current_url(environ=env, strip_querystring=True, https=True))
+        self.assertEqual('/script/news/list?param=foo', current_url(environ=env, strip_host=True, https=True))
+        self.assertEqual('/script/', current_url(root_only=True, strip_host=True, environ=env))
+
+        env = create_environ("/news/list?param=foo", "http://localhost:8080/")
+        self.assertEqual('http://localhost:8080/news/list?param=foo', current_url(environ=env))
+        self.assertEqual('http://localhost:8080/', current_url(environ=env, root_only=True))
+        self.assertEqual('http://localhost:8080/', current_url(environ=env, host_only=True))
+        self.assertEqual('http://localhost:8080/news/list', current_url(environ=env, strip_querystring=True))
+        self.assertEqual('/news/list?param=foo', current_url(environ=env, strip_host=True))
+        self.assertEqual('http://localhost:8080/news/list?param=foo', current_url(environ=env, https=False))
+        self.assertEqual('http://localhost:8080/', current_url(environ=env, root_only=True, https=False))
+        self.assertEqual('http://localhost:8080/', current_url(environ=env, host_only=True, https=False))
+        self.assertEqual('http://localhost:8080/news/list', current_url(environ=env, strip_querystring=True, https=False))
+        self.assertEqual('/news/list?param=foo', current_url(environ=env, strip_host=True, https=False))
+        self.assertEqual('/', current_url(root_only=True, strip_host=True, environ=env))
+
+if __name__ == '__main__':
+    unittest.main()
