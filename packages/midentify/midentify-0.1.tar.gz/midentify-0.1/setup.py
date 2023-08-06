8798d621b1be1c928fcc221919dc06f83e7de14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:08 +0000

    Add package midentify/0.1/midentify-0.1.tar.gz

---
+++ packages/midentify/midentify-0.1.tar.gz/midentify-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(  name='midentify',
+        version='0.1',
+        description="Identification of a file using MPlayer's midentify.sh script",
+        author="Abhishek Mukherjee",
+        author_email="linkinpark342@gmail.com",
+        py_modules=['midentify'],
+        url='http://github.com/linkinpark342/midentify-py/',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: Public Domain',
+            'Topic :: Multimedia :: Video',
+            ]
+        )
