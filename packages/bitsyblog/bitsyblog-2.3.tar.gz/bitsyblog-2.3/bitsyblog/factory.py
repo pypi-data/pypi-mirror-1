2d841c284361413b322a56a1500407522fef1a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:58 +0000

    Add package bitsyblog/2.3/bitsyblog-2.3.tar.gz

---
+++ packages/bitsyblog/bitsyblog-2.3.tar.gz/bitsyblog-2.3/bitsyblog/factory.py
@@ -0,0 +1,32 @@
+from bitsyauth import BitsyAuth
+from bitsyblog import BitsyBlog, BitsierBlog
+from getpass import getpass 
+from paste.httpexceptions import HTTPExceptionHandler
+
+# accepted configuration keys, e.g. 'bitsyblog.file_dir'
+config = [ 'file_dir', 'date_format', 'subject', 'n_links', 'help_file', 'header', 'template_directories' ]
+
+def factory(global_conf, **app_conf):
+    """make bitsyauth app and wrap it in middleware"""
+    key_str = 'bitsyblog.%s'
+    args = dict([ (key, app_conf[ key_str % key]) for key in config
+                  if app_conf.has_key(key_str % key) ])
+    app = BitsyBlog(**args)
+    secret = app_conf.get('secret', 'secret')
+    return BitsyAuth(HTTPExceptionHandler(app), global_conf, app.passwords, app.newuser, 'bitsyblog', secret)
+
+
+def bitsierfactory(global_conf, **app_conf):
+    """make single-user bitsyblog"""
+    key_str = 'bitsyblog.%s'
+    args = dict([ (key, app_conf[ key_str % key]) for key in config
+                  if app_conf.has_key(key_str % key) ])
+    user = app_conf['bitsyblog.user']
+    app = BitsierBlog(**args)
+    app.user = user
+    secret = app_conf.get('secret', 'secret')
+    auth = BitsyAuth(HTTPExceptionHandler(app), global_conf, app.passwords, newuser=None, site='bitsyblog', secret=secret)
+    if not user in app.users:
+        pw = getpass('Enter password for %s: ' % user)
+        app.newuser(user, auth.hash(app.user, pw))
+    return auth
