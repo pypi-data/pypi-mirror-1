4ce6cf5c702426915d1d6eb6c4ff899e6ab08261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:29 +0000

    Add package getpaid.core/0.7.3/getpaid.core-0.7.3.tar.gz

---
+++ packages/getpaid.core/getpaid.core-0.7.3.tar.gz/getpaid.core-0.7.3/setup.py
@@ -0,0 +1,62 @@
+"""
+$Id: setup.py 2376 2009-03-13 18:43:31Z lucielejard $
+
+Copyright (c) 2007 - 2008 ifPeople, Kapil Thangavelu, and Contributors
+All rights reserved. Refer to LICENSE.txt for details of distribution and use.
+
+Distutils setup
+
+"""
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.7.3'
+
+setup(
+    name='getpaid.core',
+    version=version,
+    license = 'ZPL2.1',
+    author='Getpaid Community',
+    author_email='getpaid-dev@googlegroups.com',
+    description='Core ecommerce functionality for zope and python projects',
+    long_description = (
+        read('README.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'getpaid', 'core', 'order.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+    classifiers = [
+        "Framework :: Plone",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Software Development :: Libraries",
+        ],
+    url='http://code.google.com/p/getpaid',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['getpaid'],
+    include_package_data=True,
+    install_requires = [ 'hurry.workflow',
+                         'setuptools',
+                         'zope.app.annotation',
+                         'zope.interface',
+                         'zope.event',
+                         'zope.schema',
+                       ],
+    zip_safe = False,
+    )
