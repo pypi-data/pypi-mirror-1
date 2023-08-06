79e81a7bf268f49509dadf1dabfde437d374e56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:35 +0000

    Add package Couchapp/0.1.14/Couchapp-0.1.14.tar.gz

---
+++ packages/Couchapp/Couchapp-0.1.14.tar.gz/Couchapp-0.1.14/INSTALLING.md
@@ -0,0 +1,17 @@
+# Installing
+
+## Python
+
+If `sudo easy_install couchapp` doesn't work for you, you can install from source.
+
+    git clone git://github.com/jchris/couchapp.git
+    cd couchapp
+    sudo python setup.py install
+
+You may need to satisfy some dependencies. We hope this will happen automatically but... you never know.
+
+## Questions and Comments
+
+If you can improve this documentation, please send pull requests using Github or hit the [CouchApp mailing list](http://groups.google.com/group/couchapp). 
+
+We want the install experience to go smoothly for **everyone**, so please send feedback and let us help you make this documentation cover troubleshooting problems.
---
+++ packages/Couchapp/Couchapp-0.1.14.tar.gz/Couchapp-0.1.14/python/couchapp/_external/git.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+GIT=`which git` 
+tempfile=`mktemp /tmp/couchappXXXXXXX`
+
+
+rm $tempfile
+
+# get vendor files
+$GIT clone --depth=1 $2 $tempfile
+
+
+# delete existing files
+if [ $1 = "update" ]; then
+    if [ -r $3 ]; then
+        rm -rvf $3
+    fi
+
+    if [ -r $tempfile/vendor ]; then
+        for f in $(find $tempfile/vendor -type d); do
+            if [ $f != "$tempfile/vendor" ]; then
+                cp -vr $f $4
+            fi
+        done
+        
+    fi
+else
+    if [ -r $tempfile/vendor ]; then
+        for f in $(find $tempfile/vendor -type d); do
+            if [ $f != "$tempfile/vendor" ]; then
+                echo "$2" > $f/.new
+                cp -vr $f $3
+                
+            fi
+        done
+    fi
+fi
