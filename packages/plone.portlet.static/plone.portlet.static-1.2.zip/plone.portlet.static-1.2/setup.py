e852e26ae6a979fbd6bd7d3036fc6f2cf9820475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:25 +0000

    Add package plone.portlet.static/1.2/plone.portlet.static-1.2.zip

---
+++ packages/plone.portlet.static/plone.portlet.static-1.2.zip/plone.portlet.static-1.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2'
+
+setup(name='plone.portlet.static',
+      version=version,
+      description="A simple static HTML portlet for Plone 3",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone portlet',
+      author='Martin Aspeli',
+      author_email='optilude@gmx.net',
+      url='http://plone.org',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=["plone", 'plone.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'Products.PloneTestCase',
+        ]
+      ),
+      install_requires=[
+          'setuptools',
+          "plone.portlets",
+          "plone.app.portlets",
+          "plone.app.form>=1.1",
+          "plone.i18n",
+          'zope.component',
+          'zope.formlib',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
+          'Products.kupu',
+          # 'Zope2',
+      ],
+      )
