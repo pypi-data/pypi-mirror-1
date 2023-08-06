042052565eee71f255847dd574b727a943adf67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:42 +0000

    Add package django-filer/0.1.3a/django-filer-0.1.3a.tar.gz

---
+++ packages/django-filer/django-filer-0.1.3a.tar.gz/django-filer-0.1.3a/filer/__init__.py
@@ -0,0 +1,10 @@
+"""
+django-filer - file and image management for django projects
+"""
+
+__version__ = "0.1.3a"
+__authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
+
+
+
+#from fields.image import FilerImageField, AdminImageWidget, AdminImageFormField
