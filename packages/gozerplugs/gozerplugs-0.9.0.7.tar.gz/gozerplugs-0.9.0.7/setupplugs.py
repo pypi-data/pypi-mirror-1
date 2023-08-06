daac9cf484fb9563f08e6b28d68391b094b949d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:53 +0000

    Add package gozerplugs/0.9.0.7/gozerplugs-0.9.0.7.tar.gz

---
+++ packages/gozerplugs/gozerplugs-0.9.0.7.tar.gz/gozerplugs-0.9.0.7/setupplugs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+#
+
+__copyright__ = 'this file is in the public domain'
+__revision__ = '$Id: setup.py 71 2005-11-10 13:37:50Z bart $'
+
+from setuptools import setup
+
+setup(
+    name='gozerplugs',
+    version='0.9.0.7',
+    url='http://gozerbot.org',
+    author='Bart Thate',
+    author_email='bthate@gmail.com',
+    description='plugin package for gozerbot ',
+    license='BSD',
+    packages = ['gozerplugs', 'gozerplugs.webserver'],
+    install_requires = ['gozerbot >= 0.9.0.7']
+)
