c0c66e9bc5be0a2a9958ef1145c6f7276140df5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:08 +0000

    Add package ciutils/0.0.1/ciutils-0.0.1.tar.gz

---
+++ packages/ciutils/ciutils-0.0.1.tar.gz/ciutils-0.0.1/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+
+setup(name="ciutils",
+      author="Olemis Lang",
+      version="0.0.1",
+      description="Helpers to use in CI environments (e.g. use xmlrunner together with `test` command) ",
+      py_modules=['ciutils'])
+
