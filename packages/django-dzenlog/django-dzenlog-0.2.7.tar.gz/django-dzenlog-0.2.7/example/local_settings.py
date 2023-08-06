bb25a3fdd1fb1028e2c94955b630edea1b7ef5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:59 +0000

    Add package django-dzenlog/0.2.7/django-dzenlog-0.2.7.tar.gz

---
+++ packages/django-dzenlog/django-dzenlog-0.2.7.tar.gz/django-dzenlog-0.2.7/example/local_settings.py
@@ -0,0 +1,6 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'dzenlog'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'root'             # Not used with sqlite3.
