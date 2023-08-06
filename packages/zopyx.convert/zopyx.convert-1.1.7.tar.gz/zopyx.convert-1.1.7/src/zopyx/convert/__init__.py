8199e4b49a25a4b630139fe682d0e01ff2c23ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package zopyx.convert/1.1.7/zopyx.convert-1.1.7.tar.gz

---
+++ packages/zopyx.convert/zopyx.convert-1.1.7.tar.gz/zopyx.convert-1.1.7/src/zopyx/convert/__init__.py
@@ -0,0 +1,26 @@
+##########################################################################
+# zopyx.convert - XSL-FO related functionalities
+#
+# (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+from interfaces import IFOConverter, IHTML2FOConverter, IXSLFOConverter
+
+from convert import Converter
+from html2fo import HTML2FOConverter
+from fo2pdf import PDFConverter, xinc_available
+from fo2fop import FOPPDFConverter, fop_available
+from fo2xfc import DOCXConverter, RTFConverter, ODTConverter, WMLConverter, xfc_available
+
+
+def availableFormats():
+
+    formats = []
+    if xinc_available:
+        formats.append('pdf')
+    if fop_available:
+        formats.append('pdf2')
+    if xfc_available:
+        formats.extend(['rtf', 'docx', 'odt', 'wml'])
+
+    return formats
