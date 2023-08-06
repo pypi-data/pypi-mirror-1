80c0396ac25ff537f5d209118f96b7f6e0b0d857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:24 +0000

    Add package git-svn-helpers/0.2b/git-svn-helpers-0.2b.zip

---
+++ packages/git-svn-helpers/git-svn-helpers-0.2b.zip/git-svn-helpers-0.2b/gitsvnhelpers/gitify.py
@@ -0,0 +1,70 @@
+import sys
+import os
+from os.path import exists
+from jarn.mkrelease.tee import popen
+
+from config import GIT_CACHE
+from utils import basename
+from utils import is_git
+from utils import is_svn
+from utils import svn_type
+from utils import svn_branch
+from clone import clone
+
+
+def main(args=None):
+
+    if is_git():
+        print "This seems to be a local git repository!"
+        sys.exit(1)
+
+    if not is_svn():
+        print "This only works on svn checkouts!"
+        sys.exit(1)
+
+    package_name = basename()
+    svntype = svn_type()
+
+    if svntype == 'tags':
+        print "Can't work on tags!"
+        sys.exit(1)
+    elif svntype == 'unrecognized':
+        print "Unrecognized svn structure!"
+        sys.exit(1)
+
+    if not exists(GIT_CACHE + package_name):
+        print "No git repository found in %s." % GIT_CACHE
+        print "Initiating cloning into cache."
+        clone()
+
+    # get the branch svn is on
+    remote_branch = svn_branch()
+    # the following is just convention:
+    local_branch = "local/%s" % remote_branch
+
+    cwd = os.getcwd()
+    # perform all index updates in the cache to avoid conflicts
+    os.chdir(GIT_CACHE + package_name)
+
+    dummy, existing_branches = popen('git b', False, False)
+    existing_branches = [b.strip() for b in existing_branches]
+    if local_branch in existing_branches:
+        popen('git co -f %s' % local_branch, False, False)
+    else:
+        popen('git co -f -b %s %s' % (local_branch, remote_branch), False,
+            False)
+
+    os.chdir(cwd)
+    if not exists('.git'):
+        popen('ln -s %s%s/.git' % (GIT_CACHE, package_name), False, False)
+
+    print "Git branch '%s' is now following svn branch '%s':" % (
+        local_branch, remote_branch)
+    popen('svn st')
+    popen('git st')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/git-svn-helpers/git-svn-helpers-0.2b.zip/git-svn-helpers-0.2b/gitsvnhelpers/utils.py
@@ -0,0 +1,61 @@
+from os.path import exists, isdir, islink
+from elementtree import ElementTree
+from jarn.mkrelease.tee import popen
+
+
+def basename():
+    return popen('basename $PWD', False, False)[1][0]
+
+
+def is_git():
+    """ Is the current directory a git local repository?
+        (as opposed to no git repository at all or just a symlink)
+    """
+    return exists('.git') and not islink('.git')
+
+
+def is_svn():
+    """is the current directory a svn checkout?"""
+    return (exists('.svn') and isdir('.svn'))
+
+
+def svn_info():
+    """Returns the svn info as XML element"""
+    code, result = popen('svn info --xml .', False, False)
+    return ElementTree.fromstring(''.join(result))
+
+
+def svn_log():
+    """Returns the svn log of the base url as XML element"""
+    code, result = popen('svn log --stop-on-copy --xml %s' % base_url(),
+        False, False)
+    return ElementTree.fromstring(''.join(result))
+
+
+def svn_url(svninfo=None):
+    """ returns the URL of the svn checkout"""
+    if svninfo is None:
+        svninfo = svn_info()
+    return svninfo.find('entry/url').text
+
+
+def svn_branch():
+    """ returns the name of the branch that this svn checkout
+        is on"""
+    return svn_url().split('/')[-1]
+
+
+def base_url():
+    return svn_url().split(svn_type())[0]
+
+
+def svn_type():
+    url = svn_url()
+    if '/trunk' in url:
+        return 'trunk'
+    elif '/tags' in url:
+        return 'tags'
+    elif '/branches' in url:
+        return 'branches'
+    else:
+        return 'unrecognized'
