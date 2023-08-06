88a5f20b352e66b69523e9aa96f887da8e298103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:09 +0000

    Add package ContextPy/1.1/ContextPy-1.1.zip

---
+++ packages/ContextPy/ContextPy-1.1.zip/ContextPy-1.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="ContextPy",
+      version="1.1",
+      description="Context-oriented Programming for Python",
+      author="Christian Schubert & Michael Perscheid",
+      author_email="michael.perscheid@hpi.uni-potsdam.de",
+      py_modules=['contextpy'])
