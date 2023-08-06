b930795c6d99f636cfbc4c0261af1f08b1384060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:19 +0000

    Add package django-generic-images/0.28/django-generic-images-0.28.tar.gz

---
+++ packages/django-generic-images/django-generic-images-0.28.tar.gz/django-generic-images-0.28/generic_images/models.py
@@ -0,0 +1,146 @@
+#coding: utf-8
+import os
+import random
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.files.storage import default_storage
+from django.db.models import Max
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.generic import GenericTabularInline, GenericForeignKey
+from django.utils.translation import ugettext_lazy as _
+
+from generic_images.signals import image_saved, image_deleted
+from generic_images.managers import AttachedImageManager
+from generic_utils.models import GenericModelBase
+
+
+class BaseImageModel(models.Model):
+    ''' Simple abstract Model class with image field.
+    '''
+    
+    def get_upload_path(self, filename):
+        ''' Override this to customize upload path
+        '''
+        raise NotImplementedError
+
+    def _upload_path_wrapper(self, filename):
+        return self.get_upload_path(filename)
+
+    image = models.ImageField(_('Image'), upload_to=_upload_path_wrapper)
+    
+    class Meta:
+        abstract = True
+
+
+
+class ReplaceOldImageModel(BaseImageModel):
+    '''
+        Abstract Model class with image field.
+        If the file for image is re-uploaded, old file is deleted.
+    '''
+    
+    def _replace_old_image(self):
+        ''' Override this in subclass if you don't want
+            image replacing or want to customize image replacing
+        '''
+        try:
+            old_obj = self.__class__.objects.get(pk=self.pk)
+            if old_obj.image.path != self.image.path:
+                path = old_obj.image.path
+                default_storage.delete(path)
+        except self.__class__.DoesNotExist:
+            pass
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self._replace_old_image()
+        super(ReplaceOldImageModel, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+        
+                            
+
+class AbstractAttachedImage(ReplaceOldImageModel, GenericModelBase):
+    '''
+        Abstract Image model that can be attached to any other Django model using 
+        generic relations.
+    '''    
+    user = models.ForeignKey(User, blank=True, null=True, verbose_name=_('User'))
+    
+    caption = models.TextField(_('Caption'), null=True, blank=True)
+    is_main = models.BooleanField(_('Main image'), default=False)
+    
+    order = models.IntegerField(_('Order'), default=0)
+
+    objects = AttachedImageManager()    
+    
+    def _get_next_pk(self):
+        max_pk = self.__class__.objects.aggregate(max_pk=Max('pk'))['max_pk'] or 0
+        return max_pk+1
+
+
+    def get_file_name(self, filename):
+#        alphabet = "1234567890abcdefghijklmnopqrstuvwxyz"        
+#        return ''.join([random.choice(alphabet) for i in xrange(16)]) # 1e25 variants
+        return str(self._get_next_pk()) # anyway _get_next_pk is needed for setting `order` field
+    
+        
+    def get_upload_path(self, filename):
+        ''' Override this in proxy subclass to customize upload path.
+            Default upload path is "/media/images/<user.id>/<image.id>.<ext>"
+            or "/media/images/common/<image.id>.<ext>" if user is not set.
+            image.id is predicted as it is unknown at this stage.
+        '''        
+        user_folder = str(self.user.pk) if self.user else 'common'        
+        
+        root, ext = os.path.splitext(filename)
+        return os.path.join('media', 'images', user_folder, self.get_file_name(filename) + ext)    
+    
+    
+    def save(self, *args, **kwargs):
+        if self.is_main:
+            related_images = self.__class__.objects.filter(content_type=self.content_type, 
+                                                          object_id=self.object_id)
+            related_images.update(is_main=False)
+        if not self.pk:
+            self.order = self._get_next_pk()
+        super(AbstractAttachedImage, self).save(*args, **kwargs)         
+        image_saved.send(sender = self.content_type.model_class(), instance = self)
+        
+        
+    def delete(self, *args, **kwargs):
+        image_deleted.send(sender = self.content_type.model_class(), instance = self)
+        super(AbstractAttachedImage, self).delete(*args, **kwargs)            
+        
+        
+    def __unicode__(self):
+        try:
+            if self.user:
+                return u"AttachedImage #%d for [%s] by [%s]" % (self.pk, self.content_object, self.user)
+            else:
+                return u"AttachedImage #%d for [%s]" % (self.pk, self.content_object,)
+        except:
+            try:
+                return u"AttachedImage #%d" % (self.pk)
+            except TypeError:
+                return u"new AttachedImage"
+                        
+    class Meta:
+        abstract=True
+        
+
+
+class AttachedImage(AbstractAttachedImage):
+    '''
+        Image model that can be attached to any other Django model using 
+        generic relations.
+    '''    
+    class Meta:
+        ordering = ['-order']
+
+
+class AttachedImageInline(GenericTabularInline):
+    model = AttachedImage
+
---
+++ packages/django-generic-images/django-generic-images-0.28.tar.gz/django-generic-images-0.28/generic_utils/app_utils.py
@@ -0,0 +1,104 @@
+#coding: utf-8
+from django.conf.urls.defaults import *
+from django.http import Http404
+from django.core.urlresolvers import reverse            
+        
+
+class PluggableSite(object):
+    ''' Base class for reusable apps. 
+        The approach is similar to django AdminSite.
+        For usage case please check photo_albums app.
+    '''
+    def __init__(self, instance_name, queryset, app_name,
+                 object_regex = r'\d+', lookup_field = 'pk',
+                 extra_context=None, template_object_name = 'object',
+                 has_edit_permission = lambda request, obj: True,
+                 context_processors=None):
+        self.object_regex = object_regex
+        self.lookup_field = lookup_field
+        self.instance_name = instance_name
+        self.queryset = queryset
+        self.extra_context = extra_context or {}
+        self.app_name = app_name
+        self.has_edit_permission = has_edit_permission
+        self.template_object_name = template_object_name
+        self.context_processors = context_processors
+        
+    def reverse(self, url, args=None, kwargs=None):
+        ''' Reverse an url taking self.app_name in account '''
+        return reverse("%s:%s" % (self.instance_name, url,), 
+                        args=args,
+                        kwargs=kwargs, 
+                        current_app = self.app_name)
+                
+        
+    def check_permissions(self, request, object):
+        if not self.has_edit_permission(request, object):
+            raise Http404('Not allowed')
+        
+        
+    def get_object(self, *args, **kwargs):
+        '''
+        If one positioned argument is given it is used as
+        a value for lookup with key=self.lookup_field ('pk'
+        by default). If keyword arguments are given they are
+        passed as queryset's get lookup parameters.
+        '''
+        if args and not kwargs:
+            kwargs = {self.lookup_field: args[0]} 
+            args = []           
+        return self.queryset.get(*args, **kwargs)
+        
+        
+    def get_object_or_404(self, *args, **kwargs):
+        '''
+        If one positioned argument is given it is used as
+        a value for lookup with key=self.lookup_field ('pk'
+        by default). If keyword arguments are given they are
+        passed as queryset's get lookup parameters. If no object
+        is found Http404 exception is raised. 
+        '''
+        try:
+            return self.get_object(*args, **kwargs)                
+        except self.queryset.model.DoesNotExist:
+            raise Http404('No %s matches the given query.' % self.queryset.model._meta.object_name)
+    
+
+    def get_common_context(self, object):
+        context = {self.template_object_name: object, 'current_app': self.app_name}
+        if (self.extra_context):
+            context.update(self.extra_context)
+        return context
+    
+    
+    def get_object_and_context(self, *args, **kwargs):
+        obj = self.get_object_or_404(*args, **kwargs)
+        return obj, self.get_common_context(obj)
+    
+                    
+    def make_regex(self, url):
+        return r"^(?P<object_id>%s)/%s%s$" % (self.object_regex, self.app_name, url)
+    
+    
+    def patterns(self):
+        ''' This method should return url patterns 
+            (like urlpatterns variable in urls.py).
+            It is helpful to construct regex with make_regex method.
+            Example::
+            
+            return patterns('photo_albums.views',                                
+                                url(
+                                    self.make_regex('/'),
+                                    'show_album',
+                                    {'album_site': self},
+                                    name = 'show_album',
+                                ),
+                           )
+        '''
+        
+        raise NotImplementedError
+
+
+    @property
+    def urls(self):
+        return self.patterns(), self.app_name, self.instance_name
