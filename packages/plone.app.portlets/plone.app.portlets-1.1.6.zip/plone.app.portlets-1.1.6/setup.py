363df3d673073455345e2ca6498520269b9ff9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:53 +0000

    Add package plone.app.portlets/1.1.6/plone.app.portlets-1.1.6.zip

---
+++ packages/plone.app.portlets/plone.app.portlets-1.1.6.zip/plone.app.portlets-1.1.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '1.1.6'
+
+setup(name='plone.app.portlets',
+      version=version,
+      description="Plone integration for the basic plone.portlets package",
+      long_description="""\
+plone.app.portlets provides a Plone-specific user interface for 
+plone.portlets, as well as a standard set of portlets that ship with Plone.
+""",
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        ],
+      keywords='portlets viewlets plone',
+      author='Martin Aspeli',
+      author_email='optilude@gmx.net',
+      url='http://pypi.python.org/pypi/plone.app.portlets',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.portlets',
+      ],
+      )
