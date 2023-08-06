0f70b2d4e1ebb50d1b7c894834149edcc33835b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:31 +0000

    Add package ConversionKit/0.3.0/ConversionKit-0.3.0.tar.gz

---
+++ packages/ConversionKit/ConversionKit-0.3.0.tar.gz/ConversionKit-0.3.0/setup.py
@@ -0,0 +1,52 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import sys, os
+
+version = '0.3.0'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    "ConversionKit\n"
+    "+++++++++++++\n"
+    "\n.. contents ::\n"
+    "\n"+read('doc/index.txt')
+    + '\n'
+    + read('CHANGELOG.txt')
+    + '\n'
+    'License\n'
+    '=======\n'
+    + read('LICENSE.txt')
+    + '\n'
+    'Download\n'
+    '========\n'
+)
+
+
+setup(
+    name='ConversionKit',
+    version=version,
+    description="A general purpose conversion library",
+    long_description=long_description,
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='GNU AGPLv3',
+    packages=find_packages(exclude=['ez_setup', 'example', 'test']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+    ],
+    entry_points="""
+    """,
+)
+
