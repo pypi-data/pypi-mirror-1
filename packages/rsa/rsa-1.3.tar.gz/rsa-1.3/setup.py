1a1607aff33a004645c0d16e9f762385bd3c8bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:59 +0000

    Add package rsa/1.3/rsa-1.3.tar.gz

---
+++ packages/rsa/rsa-1.3.tar.gz/rsa-1.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='rsa',
+	version='1.3',
+    description='Pure-Python RSA implementation', 
+    long_description='Pure-Python RSA implementation',
+    author='Sybren Stuvel',
+    author_email='sybren@stuvel.eu', 
+    maintainer='Sybren Stuvel',
+    maintainer_email='sybren@stuvel.eu',
+	url='http://www.stuvel.eu/rsa',
+	packages=['rsa'],
+    license='GPL + EUPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: European Union Public Licence (EUPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Graphics :: Capture :: Digital Camera',
+        'Topic :: Artistic Software',
+        'Natural Language :: English',
+    ],
+)
