2db67b2e4f523196ee36928da14951b87406f2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package plone.portlet.collection/1.1.2/plone.portlet.collection-1.1.2.tar.gz

---
+++ packages/plone.portlet.collection/plone.portlet.collection-1.1.2.tar.gz/plone.portlet.collection-1.1.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.2'
+
+setup(name='plone.portlet.collection',
+      version=version,
+      description="A portlet that fetches results from a collection",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Geir Baekholt - Jarn',
+      author_email='baekholt@jarn.com',
+      url='http://svn.plone.org/svn/plone/plone.portlet.collection',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', "plone.portlet"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.memoize',
+          'plone.portlets',
+          'plone.app.portlets',
+          'plone.app.vocabularies',
+          'plone.app.form',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
