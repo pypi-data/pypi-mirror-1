d865ccc1f1aa780954c14d383f2cec3f5446528a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:19 +0000

    Add package chameleon.zpt/1.0b16/chameleon.zpt-1.0b16.tar.gz

---
+++ packages/chameleon.zpt/chameleon.zpt-1.0b16.tar.gz/chameleon.zpt-1.0b16/docs/api.rst
@@ -0,0 +1,19 @@
+.. _pagetemplate_api_module:
+
+:mod:`chameleon.zpt`
+--------------------------
+
+``PageTemplate*`` constructors create templates from XML files.
+
+.. automodule:: chameleon.zpt.template
+
+  .. autoclass:: chameleon.zpt.template.PageTemplate
+
+  .. autoclass:: chameleon.zpt.template.PageTemplateFile
+
+A template loader class is provided (for use with Pylons and other
+platforms).
+  
+.. automodule:: chameleon.zpt.loader
+
+  .. autoclass:: chameleon.zpt.loader.TemplateLoader
---
+++ packages/chameleon.zpt/chameleon.zpt-1.0b16.tar.gz/chameleon.zpt-1.0b16/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '1.0b16'
+
+install_requires = [
+    'setuptools',
+    'zope.interface',
+    'zope.component',
+    'zope.i18n >= 3.5',
+    'chameleon.core >=1.0b31',
+    ]
+
+setup(name='chameleon.zpt',
+      version=version,
+      description="Zope Page Template engine based on Chameleon",
+      long_description=open("README.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Text Processing :: Markup :: XML",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Malthe Borch and the Zope Community',
+      author_email='zope-dev@zope.org',
+      url='',
+      license='BSD',
+      namespace_packages=['chameleon'],
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require = {'test': install_requires},
+      )
