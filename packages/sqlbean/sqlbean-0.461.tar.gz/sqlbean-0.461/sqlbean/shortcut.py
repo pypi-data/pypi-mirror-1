fb1d5db0159ecd6ed05ca39e22a890c255a6f5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:02 +0000

    Add package sqlbean/0.461/sqlbean-0.461.tar.gz

---
+++ packages/sqlbean/sqlbean-0.461.tar.gz/sqlbean-0.461/sqlbean/shortcut.py
@@ -0,0 +1,5 @@
+from sqlbean.db.query import Query
+from sqlbean.db.mc import mc, MMcCache, McCache
+from sqlbean.db.relations import ForeignKey, OneToMany
+from sqlbean.model import Model
+from sqlbean.mcmodel import McModel
