433db1f1d20e066bbd4dcdbf900c2c86df0e5b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:53 +0000

    Add package Products.FCKeditor/2.6.4.1/Products.FCKeditor-2.6.4.1.zip

---
+++ packages/Products.FCKeditor/Products.FCKeditor-2.6.4.1.zip/Products.FCKeditor-2.6.4.1/Products/FCKeditor/skins/fckeditorplone/getFck_basehref.py
@@ -0,0 +1,34 @@
+## Script (Python) "getFck_basehref.py"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=pa_meta_types=[]
+##title= fck custom config for Plone
+##
+request=context.REQUEST
+
+
+# fix basepath for static portlet edition
+
+if '++' in request.URLPATH0 :
+    basehref = request.URLPATH0.split('++')[0].rstrip('/')
+    
+
+elif '/portal_factory' in request.URLPATH0 :
+    basehref = request.URL4
+elif context.meta_type in pa_meta_types or context.isPrincipiaFolderish:
+    if 'replaceField/replaceField' in request.URLPATH0 :
+       basehref = request.URL2
+    else:   
+       basehref = request.URL1
+# kss templates url are finishing with 'replaceField/replaceField'       
+elif 'replaceField/replaceField' in request.URLPATH0 :
+    basehref = request.URL3
+else : 
+    basehref = request.URL2
+
+return basehref
+
+
