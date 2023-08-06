acbfb1e2755ee85e0f286203e17142d8049754a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:34 +0000

    Add package Morelia/0.0.4/Morelia-0.0.4.tar.gz

---
+++ packages/Morelia/Morelia-0.0.4.tar.gz/Morelia-0.0.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup # , find_packages
+import morelia
+
+setup(name='Morelia',
+      version=morelia.__version__, # '0.0.2',
+      description='put the squeeze on your features',
+      author='Phlip',
+      author_email='phlip2005@gmail.com',
+      url='http://c2.com/cgi/wiki?MoreliaViridis',
+
+      py_modules=['morelia'],
+    keywords = "django testing bdd",
+    install_requires=[],
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ]
+
+     )
