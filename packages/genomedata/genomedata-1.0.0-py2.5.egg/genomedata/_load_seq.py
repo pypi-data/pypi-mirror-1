3d1e7afab979dddf42a8093fbe4c03f7ea58f85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:22 +0000

    Add package genomedata/1.0.0/genomedata-1.0.0-py2.5.egg

---
+++ packages/genomedata/genomedata-1.0.0-py2.5.egg/genomedata/_load_seq.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+from __future__ import division, with_statement
+
+"""
+load_seq: DESCRIPTION
+"""
+
+__version__ = "$Revision: 2640 $"
+
+# Copyright 2008-2009 Michael M. Hoffman <mmh1@washington.edu>
+
+from errno import EEXIST
+from os import extsep
+from re import compile, VERBOSE
+import sys
+
+from numpy import frombuffer
+from path import path
+from tables import openFile
+
+from . import SEQ_ATOM, SEQ_DTYPE, EXT
+from ._util import EXT_GZ, FILTERS_GZIP, gzip_open, LightIterator
+
+MIN_GAP_LEN = 100000
+assert not MIN_GAP_LEN % 2 # must be even for division
+
+# sre_constants.MAXREPEAT is 65535, so I have to break repeats into
+# two segments
+REGEX_SEGMENT_LEN = MIN_GAP_LEN // 2 # max == MAXREPEAT
+
+DNA_LETTERS_UNAMBIG = "ACGTacgt"
+
+SUPERCONTIG_NAME_FMT = "supercontig_%s"
+
+def create_supercontig(h5file, index, seq, start, end):
+    name = SUPERCONTIG_NAME_FMT % index
+    supercontig = h5file.createGroup("/", name)
+
+    seq_array = frombuffer(seq, SEQ_DTYPE)
+    h5file.createCArray(supercontig, "seq", SEQ_ATOM, seq_array.shape)
+
+    supercontig.seq[...] = seq_array
+
+    attrs = supercontig._v_attrs
+    attrs.start = start
+    attrs.end = end
+
+# XXXopt: the all-regex approach is much slower than the hybrid
+# approach (3 min to load chr21, 6 min to load chr1), but it is easier
+# to understand the code and get it correct
+#
+# the previous code (r22) might have worked fine. Consider backing down to
+# it at some point.
+#
+# XXXopt: a numpy implementation might be better
+re_gap_segment = compile(r"""
+(?:([^%s]{%d}[^%s]{%d,})                                  # group(1): ambig
+   |                                                      #  OR
+   ((?:(?:[%s]+|^)(?:[^%s]{1,%d}[^%s]{,%d}(?![^%s]))*)+)) # group(2): unambig
+""" % (DNA_LETTERS_UNAMBIG, REGEX_SEGMENT_LEN,
+       DNA_LETTERS_UNAMBIG, REGEX_SEGMENT_LEN,
+       DNA_LETTERS_UNAMBIG, DNA_LETTERS_UNAMBIG, REGEX_SEGMENT_LEN,
+       DNA_LETTERS_UNAMBIG, REGEX_SEGMENT_LEN-1,
+       DNA_LETTERS_UNAMBIG), VERBOSE)
+
+def read_seq(h5file, seq):
+    supercontig_index = 0
+
+    for m_segment in re_gap_segment.finditer(seq):
+        seq_unambig = m_segment.group(2)
+        if seq_unambig:
+            span = m_segment.span()
+            create_supercontig(h5file, supercontig_index, seq_unambig, *span)
+            supercontig_index += 1
+        else:
+            assert m_segment.group(1)
+
+def load_seq(outdirname, filenames, verbose=False):
+    outdirpath = path(outdirname)
+    try:
+        outdirpath.makedirs()
+    except OSError, err:
+        if err.errno != EEXIST:
+            raise
+
+    for filename in filenames:
+        if verbose:
+            print >>sys.stderr, filename
+
+        if filename.endswith(EXT_GZ):
+            infile = gzip_open(filename)
+        else:
+            infile = file(filename)
+
+        # needed to process gzip_open stuff
+        with infile as infile:
+            for defline, seq in LightIterator(infile):
+                h5filename = outdirpath / extsep.join([defline, EXT])
+                h5file = openFile(h5filename, "w", defline,
+                                  filters=FILTERS_GZIP)
+                with h5file as h5file:
+                    h5file.root._v_attrs.dirty = True
+                    read_seq(h5file, seq)
+
+def parse_options(args):
+    from optparse import OptionParser
+
+    usage = "%prog [OPTION]... GENOMEDATADIR SEQFILE..."
+    version = "%%prog %s" % __version__
+    parser = OptionParser(usage=usage, version=version)
+
+    parser.add_option("-v", "--verbose", dest="verbose",
+                      default=False, action="store_true",
+                      help="Print status updates and diagnostic messages")
+
+    options, args = parser.parse_args(args)
+
+    if not len(args) >= 2:
+        parser.error("Inappropriate number of arguments")
+
+    return options, args
+
+def main(args=sys.argv[1:]):
+    options, args = parse_options(args)
+    genomedatadir = args[0]
+    seqfiles = args[1:]
+    kwargs = {"verbose": options.verbose}
+    return load_seq(genomedatadir, seqfiles, **kwargs)
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ packages/genomedata/genomedata-1.0.0-py2.5.egg/genomedata/load_genomedata.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+from __future__ import division, with_statement
+
+"""
+load_genomedata: DESCRIPTION
+"""
+
+__version__ = "$Revision: 2715 $"
+
+# Copyright 2009 Orion Buske <orion.buske@gmail.com>
+
+import os
+import sys
+
+from shutil import rmtree
+from subprocess import call
+from tempfile import mkdtemp
+
+from ._load_seq import load_seq
+from ._open_data import open_data
+from ._load_data import DEFAULT_CHUNK_SIZE, load_data
+from ._close_data import close_data
+
+def die(msg="Unexpected error!"):
+    print >>sys.stderr, msg
+    sys.exit(1)
+
+def load_genomedata(genomedatadir, tracks=None, seqfiles=None,
+                    chunk_size=DEFAULT_CHUNK_SIZE, verbose=False):
+    """Loads genomedata collection with given data
+
+    genomedatadir: name of output directory for genomedata
+    tracks: a list of tracks to add, with a (track_name, track_filename) tuple
+      for each track to add
+    seqfiles: list of filenames containing sequence data to add
+    """
+
+    try:
+        # Generate hdf5 data in temporary directory and copy out when done
+        tempdatadir = mkdtemp(prefix="genomedata.")
+        if verbose:
+            print ">> Using temporary directory: %s" % tempdatadir
+
+        # Load sequences if any are specified
+        if seqfiles is not None and len(seqfiles) > 0:
+            for seqfile in seqfiles:
+                if not os.path.isfile(seqfile):
+                    die("Could not find sequence file: %s" % seqfile)
+
+            if verbose:
+                print ">> Loading sequence files:"
+
+            load_seq(tempdatadir, seqfiles, verbose=verbose)
+
+        # Load tracks if any are specified
+        if tracks is not None and len(tracks) > 0:
+            # Open hdf5 with track names
+            try:
+                track_names = []
+                for track_name, track_filename in tracks:
+                    if os.path.isfile(track_filename):
+                        if track_name not in track_names:  # No duplicates
+                            track_names.append(track_name)
+                    else:
+                        die("Could not find track file: %s" % track_filename)
+            except ValueError:
+                die("Error saving data from tracks: %s" % tracks)
+
+            if verbose:
+                print ">> Opening genomedata with %d tracks" % len(track_names)
+
+            open_data(tempdatadir, track_names, verbose=verbose)
+
+            # Load track data
+            for track_name, track_filename in tracks:
+                load_data(tempdatadir, track_name, track_filename,
+                          chunk_size=chunk_size, verbose=verbose)
+
+        # Close hdf5 files
+        try:
+            close_data(tempdatadir, verbose=verbose)  # Close genomedata
+        except:
+            die("Error saving metadata!")
+
+        # Make output directory
+        if not os.path.isdir(genomedatadir):
+            if verbose:
+                print ">> Creating directory: %s" % genomedatadir
+
+            os.makedirs(genomedatadir)
+
+        # Close and repack hdf5 files to output directory
+        tempfiles = os.listdir(tempdatadir)
+        for tempfilename in tempfiles:
+            # Repack file to output dir
+            if verbose:
+                print ">> Repacking: %s into genomedata" % tempfilename
+
+            tempfilepath = os.path.join(tempdatadir, tempfilename)
+            outfilepath = os.path.join(genomedatadir, tempfilename)
+            cmd_args = ["h5repack", "-f", "GZIP=1", tempfilepath, outfilepath]
+            retcode = call(cmd_args)
+            if retcode != 0:
+                die("HDF5 repacking failed!")
+    except:
+        print >>sys.stderr, "Error creating genomedata!"
+        raise
+    finally:
+        try:
+            # Remove temp directory and all contents
+            if verbose:
+                print ">> Cleaning up...",
+
+            sys.stdout.flush()
+            rmtree(tempdatadir)
+            if verbose:
+                print "done"
+        except Exception, e:
+            print >>sys.stderr, "\nCleanup failed: %s" % str(e)
+
+    if verbose:
+        print "\n===== Genomedata successfully created in: %s =====\n" % \
+            genomedatadir
+
+    return genomedatadir
+
+def parse_options(args):
+    from optparse import OptionParser
+
+    usage = ("%prog [OPTIONS] GENOMEDATADIR"
+             "\ne.g. %prog -t high=signal.high.wig -t low=signal.low.bed.gz"
+             " -s chrX.fa -s chrY.fa.gz mygenomedata")
+    version = "%%prog %s" % __version__
+    description = ("--track and --sequence may be repeated to specify multiple"
+                   " trackname=trackfile pairings and sequence files,"
+                   " respectively")
+    parser = OptionParser(usage=usage, version=version,
+                          description=description)
+
+#     parser.add_option("-c", "--chunk-size", dest="chunk_size",
+#                       metavar="NROWS", type="int",
+#                       default=DEFAULT_CHUNK_SIZE,
+#                       help="Chunk hdf5 data into blocks of NROWS. A higher"
+#                       " value increases compression but slows random access."
+#                       " Must always be smaller than the max size for a"
+#                       " dataset. [default: %default]")
+    parser.add_option("-s", "--sequence", action="append",
+                      dest="seqfile", default=[],
+                      help="Add the sequence data in the specified file")
+    parser.add_option("-t", "--track", action="append",
+                      dest="track", default=[],
+                      help="Add data for the given track. TRACK"
+                      " should be specified in the form: NAME=FILE,"
+                      " such as: -t signal=signal.wig")
+
+    options, args = parser.parse_args(args)
+
+    if not len(args) == 1:
+        parser.error("Inappropriate number of arguments")
+
+    return options, args
+
+def main(args=sys.argv[1:]):
+    options, args = parse_options(args)
+    genomedatadir = args[0]
+    seqfiles = options.seqfile
+
+    # Parse tracks into list of tuples
+    try:
+        tracks = []
+        for track_expr in options.track:
+            track_name, track_filename = track_expr.split("=")
+            tracks.append((track_name, track_filename))  # Tuple
+    except ValueError:
+        die(("Error parsing track expression: %s\nMake sure to specify tracks"
+             "in NAME=FILE form, such as: -t high=signal.high") % track_expr)
+
+    kwargs = {"verbose": options.verbose}
+#              "chunk_size": options.chunk_size
+    load_genomedata(genomedatadir, tracks, seqfiles, **kwargs)
+
+if __name__ == "__main__":
+    sys.exit(main())
