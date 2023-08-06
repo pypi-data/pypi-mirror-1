40879fdab5a4f4eb657b8c2fd3d693745283db70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:17 +0000

    Add package tiddlyweb/0.9.70/tiddlyweb-0.9.70.tar.gz

---
+++ packages/tiddlyweb/tiddlyweb-0.9.70.tar.gz/tiddlyweb-0.9.70/setup.py
@@ -0,0 +1,29 @@
+"""
+Try to setup tiddlyweb.
+Seem to be missing some details on how to make
+this the best it can be.
+"""
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from tiddlyweb import __version__ as VERSION
+
+setup(name = 'tiddlyweb',
+        version = VERSION,
+        description = 'An optionally headless, extensible RESTful datastore for tiddlers: bits of stuff.',
+        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        author = 'Chris Dent',
+        author_email = 'cdent@peermore.com',
+        url = 'http://tiddlyweb.com/',
+        download_url = 'http://tiddlyweb.peermore.com/dist/',
+        packages = ['tiddlyweb', 'tiddlyweb.filters', 'tiddlyweb.model', 'tiddlyweb.wikitext', 'tiddlyweb.serializations', 'tiddlyweb.stores', 'tiddlyweb.web', 'tiddlyweb.web.challengers', 'tiddlyweb.web.extractors', 'tiddlyweb.web.handler', ],
+        scripts = ['twanager',],
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['selector', 'simplejson', 'html5lib', 'cherrypy'],
+        include_package_data = True,
+        )
