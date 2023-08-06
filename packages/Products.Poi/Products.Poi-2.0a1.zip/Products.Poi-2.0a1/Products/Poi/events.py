91cbab46d3fca46b2110eb2775475c463a3c5da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:10 +0000

    Add package Products.Poi/2.0a1/Products.Poi-2.0a1.zip

---
+++ packages/Products.Poi/Products.Poi-2.0a1.zip/Products.Poi-2.0a1/Products/Poi/events.py
@@ -0,0 +1,77 @@
+import logging
+
+from Products.CMFCore.utils import getToolByName
+from collective.watcherlist.interfaces import IWatcherList
+from Products.Poi.interfaces import IIssue
+
+logger = logging.getLogger('Poi')
+
+
+def post_issue(object, event):
+    """Finalise posting of an issue.
+
+    If an anonymous user is posting, Creator would normally be set to
+    the root zope manager, as this user will become the owner.
+    Instead we give a more sensible default.
+
+    And we do the 'post' transition.
+
+    And send the initial email.
+
+    """
+    portal_membership = getToolByName(object, 'portal_membership')
+    if portal_membership.isAnonymousUser():
+        object.setCreators(('(anonymous)',))
+    portal_workflow = getToolByName(object, 'portal_workflow')
+    portal_workflow.doActionFor(object, 'post')
+
+
+def mail_issue_change(object, event):
+    """Send an email on some transitions of an issue.
+
+    Specifically: new issue and resolved issue.
+    """
+    if event.transition and event.transition.id == 'post':
+        watchers = IWatcherList(object)
+        watchers.send('new-issue-mail')
+    elif event.new_state.id == 'resolved':
+        watchers = IWatcherList(object)
+        # Only mail the original poster, if available.
+        address = object.getContactEmail()
+        if address:
+            watchers.send('resolved-issue-mail',
+                          only_these_addresses=[address])
+
+
+def removedResponse(object, event):
+    issue = event.oldParent
+    if IIssue.providedBy(issue):
+        issue.reindexObject(idxs=['SearchableText'])
+        issue.notifyModified()
+
+
+def modifiedNewStyleResponse(object, event):
+    """A response is modified or created so update its parent.
+    """
+
+    if len(event.descriptions) > 0:
+        parent = event.descriptions[0]
+        if IIssue.providedBy(parent):
+            parent.reindexObject(idxs=['SearchableText'])
+            parent.notifyModified()
+
+
+def addedNewStyleResponse(object, event):
+    """A response has been added.
+    """
+    issue = event.newParent
+    if IIssue.providedBy(issue):
+        issue.reindexObject(idxs=['SearchableText'])
+        issue.notifyModified()
+        sendResponseNotificationMail(issue)
+
+
+def sendResponseNotificationMail(issue):
+    # As we take the last response by default, we can keep this simple.
+    watchers = IWatcherList(issue)
+    watchers.send('new-response-mail')
