4763705fb8827f209b75ab06ed301472bb05fe7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:52 +0000

    Add package archgenxml/2.2/archgenxml-2.2.tar.gz

---
+++ packages/archgenxml/archgenxml-2.2.tar.gz/archgenxml-2.2/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import re
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+version = open(os.path.join(os.path.dirname(__file__), 'src', 'archgenxml', 
+                            'version.txt')).read()
+shortdesc = 'UML to code generator for Plone'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+setup(name='archgenxml',
+      version=version,
+      license='GPL',
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Programming Language :: Python',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: OS Independent',
+            'Topic :: Software Development :: Code Generators',
+            'Framework :: Zope2',
+            'Framework :: Plone',
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords="zope plone UML generator",
+      author='Phil Auersperg, Jens Klein, et al',
+      author_email='dev@bluedynamics.com',
+      url='http://plone.org/products/archgenxml',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      entry_points={
+        'console_scripts':[
+            'archgenxml = archgenxml.ArchGenXML:main',
+            'agx_taggedvalues = archgenxml.TaggedValueSupport:main',
+            'agx_stereotypes = archgenxml.UMLProfile:main',
+            'agx_argouml_profile = archgenxml.ArgoUMLProfileGenerator:main',
+            ]
+        },
+      test_suite='archgenxml.tests.runalltests.suite',
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'xmiparser>=1.2',
+          'zope.interface',
+          'zope.component',
+          'zope.documenttemplate',
+      ],
+      extras_require = dict(
+          i18n=['i18ndude']
+      ),
+      
+      )
---
+++ packages/archgenxml/archgenxml-2.2.tar.gz/archgenxml-2.2/umltools/argouml/README.txt
@@ -0,0 +1,10 @@
+This file should be fed to the ArgoUML profile settings to make it aware of the Plone-related stereotypes, tagged values, etc.
+
+Put the path to this file in this location in ArgoUML:
+
+Edit -> Settings -> Preferences -> Profile File
+
+argouml_profle.xmi is the old profile used with archgenxml <= 1.5.2.
+
+Please use archgenxml_profile.xmi to have the latest stereotypes and tagged-values.
+You should use this one if you use ArgoUML >= 0.26 if you don't want to have problems updating the profile in the future.
