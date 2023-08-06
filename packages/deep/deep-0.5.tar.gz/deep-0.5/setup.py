ccfa887d835c5aec0cf21e281ff6c6deb43abd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:04 +0000

    Add package deep/0.5/deep-0.5.tar.gz

---
+++ packages/deep/deep-0.5.tar.gz/deep-0.5/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='deep',
+      version='0.5',
+      packages = ['deep'],
+      author="Fergal Daly",
+      author_email="fergal@esatclear.ie",
+      description="Easy, flexible deep comparison and testing of structured data",
+      url="http://code.google.com/p/python-deep/",
+      download_url="http://code.google.com/p/python-deep/downloads/list",
+      )
