6f1bec7000f2f7d13f21494f8e3d121ff3383bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:32 +0000

    Add package dyce/0.2/dyce-0.2.tar.gz

---
+++ packages/dyce/dyce-0.2.tar.gz/dyce-0.2/src/dyce/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""dyce -- General purpose python dice toolkit and custom expression parser.
+
+$Author: david.eyk $\n
+$Rev: 7 $\n
+$Date: 2008-12-09 10:19:20 -0600 (Tue, 09 Dec 2008) $
+"""
+
+__author__ = "$Author: david.eyk $"[9:-2]
+__version__ = "$Rev: 7 $"[6:-2]
+__date__ = "$Date: 2008-12-09 10:19:20 -0600 (Tue, 09 Dec 2008) $"[7:-2]
+
+from dice import *
+from dcalc import *
+
+roller = Dice()
+
