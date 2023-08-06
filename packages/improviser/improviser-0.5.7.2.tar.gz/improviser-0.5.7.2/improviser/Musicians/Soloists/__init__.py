c43bd1355a2fa521e6d82407e87e423f7370b08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:38 +0000

    Add package improviser/0.5.7.2/improviser-0.5.7.2.tar.gz

---
+++ packages/improviser/improviser-0.5.7.2.tar.gz/improviser-0.5.7.2/improviser/Musicians/Soloists/__init__.py
@@ -0,0 +1,5 @@
+from SoloInstrument import SoloInstrument
+from SimpleSoloInstrument import SimpleSoloInstrument
+from ChromaticSoloist import ChromaticSoloist
+
+__all__ = ['SoloInstrument', 'SimpleSoloInstrument', 'ChromaticSoloist']
