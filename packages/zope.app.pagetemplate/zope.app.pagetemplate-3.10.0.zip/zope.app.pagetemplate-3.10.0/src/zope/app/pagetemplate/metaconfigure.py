0f971291bcab9df8b1423cd79867446b359080b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:05 +0000

    Add package zope.app.pagetemplate/3.10.0/zope.app.pagetemplate-3.10.0.zip

---
+++ packages/zope.app.pagetemplate/zope.app.pagetemplate-3.10.0.zip/zope.app.pagetemplate-3.10.0/src/zope/app/pagetemplate/metaconfigure.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""ZCML configuration directives for configuring the default zope:
+namespace in TALES.
+
+$Id: metaconfigure.py 106892 2009-12-22 18:40:31Z hannosch $
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.configuration.fields import GlobalObject
+from zope.browserpage.metaconfigure import registerType
+from zope.interface import Interface
+from zope.schema import TextLine
+
+# BBB
+from zope.browserpage.metaconfigure import clear
+
+
+class IExpressionTypeDirective(Interface):
+    """Register a new TALES expression type"""
+
+    name = TextLine(
+        title=u"Name",
+        description=u"""Name of the expression. This will also be used
+        as the prefix in actual TALES expressions.""",
+        required=True
+        )
+
+    handler = GlobalObject(
+        title=u"Handler",
+        description=u"""Handler is class that implements
+        zope.tales.interfaces.ITALESExpression.""",
+        required=True
+        )
+
+def expressiontype(_context, name, handler):
+    _context.action(
+        discriminator = ("tales:expressiontype", name),
+        callable = registerType,
+        args = (name, handler)
+        )
---
+++ packages/zope.app.pagetemplate/zope.app.pagetemplate-3.10.0.zip/zope.app.pagetemplate-3.10.0/src/zope/app/pagetemplate/namedtemplate.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (c) 2005-2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id: namedtemplate.py 106935 2009-12-22 21:41:38Z hannosch $
+"""
+
+# BBB
+from zope.browerpage.namedtemplate import implementation
+from zope.browerpage.namedtemplate import INamedTemplate
+from zope.browerpage.namedtemplate import NamedTemplate
+from zope.browerpage.namedtemplate import NamedTemplateImplementation
+from zope.browerpage.namedtemplate import NamedTemplatePathAdapter
