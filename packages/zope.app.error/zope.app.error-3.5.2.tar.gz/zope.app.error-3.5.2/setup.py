639ed09f3c1b62d8819aa72ba575e020ce90431d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:56 +0000

    Add package zope.app.error/3.5.2/zope.app.error-3.5.2.tar.gz

---
+++ packages/zope.app.error/zope.app.error-3.5.2.tar.gz/zope.app.error-3.5.2/setup.py
@@ -0,0 +1,61 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.error package
+
+$Id: setup.py 94931 2009-01-22 11:02:10Z nadako $
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.app.error',
+    version = '3.5.2',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description = "Error reporting utility management UI for Zope3",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license='ZPL 2.1',
+    keywords = "zope3 error reporting utility views UI",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://pypi.python.org/pypi/zope.app.error',
+	packages=find_packages('src'),
+	package_dir = {'': 'src'},
+    extras_require=dict(
+        test=['zope.app.testing']),
+    namespace_packages=['zope', 'zope.app'],
+    install_requires=['setuptools',
+                      'zope.error',
+                      'zope.app.publisher',
+                      ],
+    include_package_data = True,
+
+    zip_safe = False,
+    )
