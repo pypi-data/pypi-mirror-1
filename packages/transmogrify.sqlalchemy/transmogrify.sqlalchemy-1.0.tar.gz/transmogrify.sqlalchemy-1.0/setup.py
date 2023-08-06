2ada04bd18ae82fa53b040b78a74a748b5b7be2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package transmogrify.sqlalchemy/1.0/transmogrify.sqlalchemy-1.0.tar.gz

---
+++ packages/transmogrify.sqlalchemy/transmogrify.sqlalchemy-1.0.tar.gz/transmogrify.sqlalchemy-1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='transmogrify.sqlalchemy',
+      version=version,
+      description="Feed data from SQLAlchemy into a transmogrifier pipeline",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='transmogrify sql import',
+      author='Wichert Akkerman - Jarn',
+      author_email='support@jarn.com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['transmogrify'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          "collective.transmogrifier",
+          "SQLAlchemy >=0.4,<0.5dev",
+#         "zope.interface",
+      ],
+      test_suite='nose.collector',
+      entry_points="""
+      """,
+      )
