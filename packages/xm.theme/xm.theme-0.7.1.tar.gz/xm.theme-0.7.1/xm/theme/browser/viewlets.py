ff0d07bdb4c99290a5be7f6417a1c4a2f1aa83c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:47 +0000

    Add package xm.theme/0.7.1/xm.theme-0.7.1.tar.gz

---
+++ packages/xm.theme/xm.theme-0.7.1.tar.gz/xm.theme-0.7.1/xm/theme/browser/viewlets.py
@@ -0,0 +1,62 @@
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import ViewletBase
+from zope.component import getMultiAdapter
+
+
+class XMProjectHeaderViewlet(ViewletBase):
+    render = ViewPageTemplateFile('templates/project_header.pt')
+
+    def update(self):
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.site_url = self.portal_state.portal_url()
+        self.project_title = self._get_project_title()
+        self.project_url = self._get_project_url()
+
+    def _get_project(self):
+        try:
+            project = self.context.getProject()
+        except AttributeError:
+            project = None
+        return project
+
+    def _get_project_title(self):
+        project = self._get_project()
+        if project is not None:
+            return project.title
+        else:
+            return u'Extreme Management'
+
+    def _get_project_url(self):
+        project = self._get_project()
+        if project is not None:
+            return project.absolute_url()
+        else:
+            return self.site_url
+
+
+class XMSearchBoxViewlet(ViewletBase):
+    render = ViewPageTemplateFile('templates/searchbox.pt')
+
+    def update(self):
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.site_url = self.portal_state.portal_url()
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        props = getToolByName(self.context, 'portal_properties')
+        livesearch = props.site_properties.getProperty('enable_livesearch',
+                                                       False)
+        if livesearch:
+            self.search_input_id = "searchGadget"
+        else:
+            self.search_input_id = ""
+
+        folder = context_state.folder()
+        self.folder_path = '/'.join(folder.getPhysicalPath())
+
+
+class GoogleSearchViewlet(ViewletBase):
+
+        render = ViewPageTemplateFile('templates/googlesearch.pt')
