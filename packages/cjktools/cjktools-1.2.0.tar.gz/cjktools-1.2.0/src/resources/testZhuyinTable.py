597bcb98454a3b89a7c73155de2748320ffa7123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:41 +0000

    Add package cjktools/1.2.0/cjktools-1.2.0.tar.gz

---
+++ packages/cjktools/cjktools-1.2.0.tar.gz/cjktools-1.2.0/src/resources/testZhuyinTable.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------#
+# testZhuyinTable.py
+# vim: ts=4 sw=4 sts=4 et tw=78:
+# Fri Dec 22 18:02:31 2006
+#
+#----------------------------------------------------------------------------# 
+
+import re
+import unittest
+
+import zhuyinTable
+
+#----------------------------------------------------------------------------#
+
+def suite():
+    testSuite = unittest.TestSuite((
+            unittest.makeSuite(ZhuyinTableTestCase)
+        ))
+    return testSuite
+
+#----------------------------------------------------------------------------#
+
+class ZhuyinTableTestCase(unittest.TestCase):
+    """
+    This class tests the ZhuyinTable class. 
+    """
+    def setUp(self):
+        pass
+
+    def testZhuyinToPinyin(self):
+        dictObj = zhuyinTable.getZhuyinToPinyinTable()
+        self.assertEqual(dictObj[u'ㄔㄚ'], u'cha')
+        return
+    
+    def testPinyinToZhuyin(self):
+        dictObj = zhuyinTable.getPinyinToZhuyinTable()
+        self.assertEqual(dictObj[u'cha'], u'ㄔㄚ')
+        return
+
+    def testPinyinParsing(self):
+        basePattern = zhuyinTable.pinyinRegexPattern()
+        pattern = re.compile(r'^(%s)+$' % basePattern, re.UNICODE)
+        assert not pattern.match(u'gdaymatehowsitgoing')
+        assert pattern.match(u'woshangbeijingdaxue')
+        assert pattern.match(u'woquguoxianjiapo')
+        return
+
+    def tearDown(self):
+        pass
+
+#----------------------------------------------------------------------------#
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=1).run(suite())
+
+#----------------------------------------------------------------------------#
+
+# vim: ts=4 sw=4 sts=4 et tw=78:
+
---
+++ packages/cjktools/cjktools-1.2.0.tar.gz/cjktools-1.2.0/src/resources/zhuyinTable.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------#
+# zhuyinTable.py
+# vim: ts=4 sw=4 sts=4 et tw=78:
+# Fri Dec 22 16:05:44 2006
+#
+#----------------------------------------------------------------------------#
+
+"""
+An interface to the zhuyin <-> pinyin table.
+"""
+
+#----------------------------------------------------------------------------#
+
+import codecs
+import pkg_resources
+
+from cjktools.table import parseLines 
+
+#----------------------------------------------------------------------------#
+
+def _open_conversion_file():
+    return codecs.getreader('utf8')(pkg_resources.resource_stream(
+            'cjktools_data', 'tables/zhuyin_pinyin_conv_table'))
+
+def getZhuyinToPinyinTable():
+    """Returns a dictionary mapping zhuyin to pinyin."""
+    table = {}
+    for zhuyin, pinyin in parseLines(_open_conversion_file()):
+        table[zhuyin] = pinyin
+
+    return table
+
+#----------------------------------------------------------------------------#
+
+def getPinyinToZhuyinTable():
+    """Returns a dictionary mapping zhuyin to pinyin."""    
+    table = {}
+    for zhuyin, pinyin in parseLines(_open_conversion_file()):
+        table[pinyin] = zhuyin
+
+    return table
+
+#----------------------------------------------------------------------------#
+
+def pinyinRegexPattern():
+    """
+    Returns a pinyin regex pattern, with optional tone number.
+    """
+    allPinyin = ['r']
+
+    for zhuyin, pinyin in parseLines(_open_conversion_file()):
+        allPinyin.append(pinyin)
+
+    # Sort from longest to shortest, so as to make maximum matches whenever
+    # possible.
+    allPinyin.sort(lambda x, y: cmp(len(y), len(x)))
+
+    # Build a generic pattern for a single pinyin with an optional tone.
+    pattern = '(%s)([0-5]?)' % '|'.join(allPinyin)
+
+    return pattern
+
+#----------------------------------------------------------------------------#
+
+def zhuyinRegexPattern():
+    """
+    Returns a zhuyin regex pattern.
+    """
+    allPinyin = []
+
+    for zhuyin, pinyin in parseLines(_open_conversion_file()):
+        allPinyin.append(pinyin)
+
+    pattern = '(%s)[0-4]?' % '|'.join(allPinyin)
+
+    return pattern
+
+#----------------------------------------------------------------------------#
