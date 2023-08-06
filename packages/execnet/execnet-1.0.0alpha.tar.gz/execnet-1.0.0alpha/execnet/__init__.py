8a40eeeed7de243ef7aaf6a546f8ffec216a534b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:57 +0000

    Add package execnet/1.0.0alpha/execnet-1.0.0alpha.tar.gz

---
+++ packages/execnet/execnet-1.0.0alpha.tar.gz/execnet-1.0.0alpha/execnet/__init__.py
@@ -0,0 +1,15 @@
+"""
+execnet: Elastic Python Deployment.
+package for connecting to local and remote Python Interpreters.
+
+(c) 2009, Holger Krekel and others
+"""
+
+__version__ = "1.0.0alpha"
+__author__ = "holger krekel <holger@merlinux.eu> and others"
+
+from execnet.gateway import PopenGateway, SocketGateway, SshGateway
+from execnet.gateway import HostNotFound
+from execnet.xspec import makegateway, XSpec
+from execnet.multi import MultiGateway,MultiChannel
+from execnet.rsync import RSync
