b750c65dec09df955dfebb39939c17ad7420811a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:06 +0000

    Add package wsapi4plone.client/0.1a2/wsapi4plone.client-0.1a2.tar.gz

---
+++ packages/wsapi4plone.client/wsapi4plone.client-0.1a2.tar.gz/wsapi4plone.client-0.1a2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+version = '0.1a2'
+
+setup(name='wsapi4plone.client',
+      version=version,
+      author='WebLion Group, Penn State University',
+      author_email='support@weblion.psu.edu',
+      description="""An XML-RPC client specially created for interfacing with a Plone site using the Web Services API for Plone (wsapi4plone).""",
+      long_description=open("README.txt").read() + "\n" + open("CHANGES.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=["Framework :: Plone",
+                   "Framework :: Zope2",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 2.4",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Intended Audience :: Developers",
+                   "Development Status :: 3 - Alpha",
+                   ],
+      keywords='wsapi, api, xmlrpc, weblion',
+      url='https://weblion.psu.edu/trac/weblion/wiki/WebServicesApiPlone',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['wsapi4plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'httplib2',
+                        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,)
