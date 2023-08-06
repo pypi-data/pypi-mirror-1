a106f3b3f02d35435104c638619eeea191ab90b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:00 +0000

    Add package collective.singing/0.2b3/collective.singing-0.2b3.tar.gz

---
+++ packages/collective.singing/collective.singing-0.2b3.tar.gz/collective.singing-0.2b3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '0.2b3'
+
+setup(name='collective.singing',
+      version=version,
+      description="A Zope library for sending notifications and newsletters",
+      long_description="""\
+""",
+
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zope plone notification newsletter',
+      author='Daniel Nouri, Thomas Clement Mogensen and contributors',
+      author_email='singing-dancing@googlegroups.com',
+      url='http://pypi.python.org/pypi/collective.singing',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+
+      # If the dependency to z3c.form gives you trouble within a Zope
+      # 2 environment, try the `fakezope2eggs` recipe
+      install_requires=[
+          'setuptools',
+          'zope.app.catalog',
+          'z3c.form',
+          'zc.queue',
+      ],
+      dependency_links=[
+      ],
+      
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
