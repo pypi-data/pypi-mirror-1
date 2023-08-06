8f8904081e7ff3a4ccccb29a9630595dc285c2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:36 +0000

    Add package chameleon.zpt/1.0b11/chameleon.zpt-1.0b11.tar.gz

---
+++ packages/chameleon.zpt/chameleon.zpt-1.0b11.tar.gz/chameleon.zpt-1.0b11/src/chameleon/zpt/template.py
@@ -0,0 +1,27 @@
+from chameleon.core import template
+
+import language
+
+class PageTemplate(template.Template):
+    __doc__ = template.Template.__doc__ # for Sphinx autodoc
+
+    default_parser = language.Parser()
+    version = 1
+    
+    def __init__(self, body, parser=None, format=None, doctype=None):
+        if parser is None:
+            parser = self.default_parser
+        super(PageTemplate, self).__init__(body, parser, format, doctype)
+
+class PageTemplateFile(template.TemplateFile):
+    __doc__ = template.TemplateFile.__doc__ # for Sphinx autodoc
+
+    default_parser = language.Parser()
+    version = 1
+    
+    def __init__(self, filename, parser=None, format=None,
+                 doctype=None, **kwargs):
+        if parser is None:
+            parser = self.default_parser
+        super(PageTemplateFile, self).__init__(filename, parser, format,
+                                               doctype, **kwargs)
