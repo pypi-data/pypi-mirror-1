0898c278569b51f63283be1a6d36aab161fb2108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:28 +0000

    Add package zope.app.publication/3.5.3/zope.app.publication-3.5.3.tar.gz

---
+++ packages/zope.app.publication/zope.app.publication-3.5.3.tar.gz/zope.app.publication-3.5.3/setup.py
@@ -0,0 +1,70 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.publication package
+
+$Id: setup.py 98035 2009-03-13 15:39:25Z nadako $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.app.publication',
+    version = '3.5.3',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='Zope publication',
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license='ZPL 2.1',
+    keywords = "zope3 publication",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://pypi.python.org/pypi/zope.app.publication',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['zope', 'zope.app'],
+    extras_require = dict(
+        test=['zope.app.testing',
+              'zope.app.securitypolicy',
+              'zope.app.zcmlfiles',
+              'zope.app.dav',
+              'zope.app.zptpage',
+              ]),
+    install_requires=['zope.interface',
+                      'ZODB3',
+                      'zope.component',
+                      'zope.error',
+                      'zope.i18n',
+                      'zope.app.http',
+                      'zope.app.applicationcontrol',
+                      'zope.app.exception',
+                      'zope.app.security',
+                      'setuptools',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    )
