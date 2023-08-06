8b15e05df041da8a0929114246f2c4222f0970e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:09 +0000

    Add package improviser/0.7.5.1/improviser-0.7.5.1.tar.gz

---
+++ packages/improviser/improviser-0.7.5.1.tar.gz/improviser-0.7.5.1/improviser/Musicians/Drummers/__init__.py
@@ -0,0 +1,8 @@
+from RockDrum import RockDrum
+from JazzDrum import JazzDrum
+from BlastBeat import BlastBeat
+from Metronome import Metronome
+from Snare import Snare
+from DanceBeat import DanceBeat
+
+__all__ = ['RockDrum', 'JazzDrum', 'BlastBeat', 'Metronome', 'Snare', 'DanceBeat']
