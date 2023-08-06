7cf26b8268188509da5ee4a3f70ac2cc95814dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:02 +0000

    Add package cutplace/0.4.1/cutplace-0.4.1-py2.5.egg

---
+++ packages/cutplace/cutplace-0.4.1-py2.5.egg/cutplace/test_data.py
@@ -0,0 +1,58 @@
+"""
+Tests for data formats.
+"""
+import codecs
+import data
+import logging
+import unittest
+
+class DataFormatTest(unittest.TestCase):
+    """
+    Tests for data formats.
+    """
+    _TEST_ENCODING = "iso-8859-1"
+
+    def testCreateDataFormat(self):
+        for formatName in [data.FORMAT_CSV, data.FORMAT_DELIMITED, data.FORMAT_FIXED, data.FORMAT_ODS]:
+            format = data.createDataFormat(formatName)
+            self.assertTrue(format)
+        self.assertRaises(data.DataFormatSyntaxError, data.createDataFormat, "no-such-format")
+            
+    def testCsvDataFormat(self):
+        format = data.CsvDataFormat()
+        self.assertTrue(format.name)
+        self.assertEqual(format.get(data.KEY_ENCODING), codecs.lookup("ascii"))
+
+        format.set(data.KEY_ENCODING, DataFormatTest._TEST_ENCODING)
+        self.assertEqual(format.get(data.KEY_ENCODING), codecs.lookup(DataFormatTest._TEST_ENCODING))
+
+        self.assertEqual(format.get(data.KEY_LINE_DELIMITER), data.ANY)
+        self.assertEqual(format.get(data.KEY_ITEM_DELIMITER), data.ANY)
+        self.assertEqual(format.get(data.KEY_QUOTE_CHARACTER), "\"")
+        self.assertEqual(format.get(data.KEY_ESCAPE_CHARACTER), "\"")
+        format.validateAllRequiredPropertiesHaveBeenSet()
+        
+    def testDelimitedDataFormat(self):
+        format = data.DelimitedDataFormat()
+        self.assertTrue(format.name)
+        self.assertRaises(data.DataFormatSyntaxError, format.validateAllRequiredPropertiesHaveBeenSet)
+
+        formatWithCr = data.DelimitedDataFormat()
+        formatWithCr.set(data.KEY_LINE_DELIMITER, data.CR)
+        self.assertEqual(formatWithCr.get(data.KEY_LINE_DELIMITER), "\r")
+        
+    def testOdsDataFormat(self):
+        format = data.OdsDataFormat()
+        self.assertTrue(format.name)
+        format.validateAllRequiredPropertiesHaveBeenSet()
+        format.set(data.KEY_ALLOWED_CHARACTERS, "32:")
+        self.assertRaises(data.DataFormatSyntaxError, format.set, data.KEY_ENCODING, DataFormatTest._TEST_ENCODING)
+        
+    def testBrokenEncoding(self):
+        format = data.CsvDataFormat()
+        self.assertRaises(data.DataFormatSyntaxError, format.set, data.KEY_ENCODING, "me-no-encoding")
+        
+if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger("cutplace.test_data").setLevel(logging.INFO)
+    unittest.main()
