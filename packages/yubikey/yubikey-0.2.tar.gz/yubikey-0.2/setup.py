d8ac061b6872d4226a30ce17a51c9e892b14b71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:06 +0000

    Add package yubikey/0.2/yubikey-0.2.tar.gz

---
+++ packages/yubikey/yubikey-0.2.tar.gz/yubikey-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "yubikey",
+    version = "0.2",
+    py_modules = ['yubikey'],
+
+    author = "kaleissin",
+    author_email = "kaleissin at gmail com",
+    description = "Validate a Yubikey against Yubico's web-service",
+    license = "MIT",
+    keywords = "yubikey auth web-services",
+)
+
