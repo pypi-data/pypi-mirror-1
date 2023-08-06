878855e8dea66811aeb237044e7498537877082b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:07 +0000

    Add package zope.app.security/3.7.1/zope.app.security-3.7.1.tar.gz

---
+++ packages/zope.app.security/zope.app.security-3.7.1.tar.gz/zope.app.security-3.7.1/src/zope/app/security/basicauthadapter.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Backward compatibility for the IHTTPCredentials -> ILoginPassword adapter
+
+$Id: basicauthadapter.py 98044 2009-03-13 19:39:44Z nadako $
+"""
+
+# BBB
+from zope.publisher.http import BasicAuthAdapter
