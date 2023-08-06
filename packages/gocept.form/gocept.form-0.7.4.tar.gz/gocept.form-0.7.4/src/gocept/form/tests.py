e3b0718314727a53aca3853a88678fa292002d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:10 +0000

    Add package gocept.form/0.7.4/gocept.form-0.7.4.tar.gz

---
+++ packages/gocept.form/gocept.form-0.7.4.tar.gz/gocept.form-0.7.4/src/gocept/form/tests.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2007-2008 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id: tests.py 5829 2008-05-23 09:54:52Z zagy $
+
+import os
+import unittest
+
+from zope.testing import doctest
+
+import zope.app.testing.functional
+
+
+Layer = zope.app.testing.functional.ZCMLLayer(
+    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
+    __name__, 'gocept.form.layer', allow_teardown=True)
+
+
+def FunctionalDocFileSuite(*paths, **kw):
+    try:
+        layer = kw['layer']
+    except KeyError:
+        layer = Layer
+    else:
+        del kw['layer']
+    kw['package'] = doctest._normalize_module(kw.get('package'))
+    test = zope.app.testing.functional.FunctionalDocFileSuite(
+        *paths, **kw)
+    test.layer = layer
+    return test
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(FunctionalDocFileSuite(
+        'base.txt',
+        'confirm-action.txt',
+        'destructive-action.txt',
+        'grouped.txt',
+        'jsvalidation.txt',
+        'multiple-constraints.txt'
+    ))
+    return suite
