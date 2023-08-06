d7ceb202b3b79aa47cd7a098e7915e1397cc53c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:04 +0000

    Add package plone.app.users/1.0b1/plone.app.users-1.0b1.zip

---
+++ packages/plone.app.users/plone.app.users-1.0b1.zip/plone.app.users-1.0b1/plone/app/users/tests/base.py
@@ -0,0 +1,26 @@
+"""Base class for flexible user registration test cases.
+
+This is in a separate module because it's potentially useful to other
+packages which register accountpanels. They should be able to import it
+without the PloneTestCase.setupPloneSite() side effects.
+"""
+
+from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
+from Products.Five.testbrowser import Browser
+
+
+class TestCase(FunctionalTestCase):
+    """base test case which adds amin user"""
+
+    def afterSetUp(self):
+        super(TestCase, self).afterSetUp()
+        self.browser = Browser()
+        self.portal.acl_users._doAddUser('admin', 'secret', ['Manager'], [])
+
+    def setMailHost(self):
+        self.portal.MailHost.smtp_host = 'localhost'
+        setattr(self.portal, 'email_from_address', 'admin@foo.com')
+        
+    def unsetMailHost(self):
+        self.portal.MailHost.smtp_host = ''
+        setattr(self.portal, 'email_from_address', '')
