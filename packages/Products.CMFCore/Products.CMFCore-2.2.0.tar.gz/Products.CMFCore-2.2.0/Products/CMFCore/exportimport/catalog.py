f1a36df7bb93b88aa6f698003c315bcbbfb36b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:50 +0000

    Add package Products.CMFCore/2.2.0/Products.CMFCore-2.2.0.tar.gz

---
+++ packages/Products.CMFCore/Products.CMFCore-2.2.0.tar.gz/Products.CMFCore-2.2.0/Products/CMFCore/exportimport/catalog.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Catalog tool setup handlers.
+
+$Id: catalog.py 107359 2009-12-30 15:33:55Z jens $
+"""
+
+from Products.GenericSetup.utils import exportObjects
+from Products.GenericSetup.utils import importObjects
+
+from Products.CMFCore.utils import getToolByName
+
+
+def importCatalogTool(context):
+    """Import catalog tool.
+    """
+    site = context.getSite()
+    tool = getToolByName(site, 'portal_catalog', None)
+    if tool is None:
+        logger = context.getLogger('catalog')
+        logger.debug('Nothing to import.')
+        return
+
+    importObjects(tool, '', context)
+
+def exportCatalogTool(context):
+    """Export catalog tool.
+    """
+    site = context.getSite()
+    tool = getToolByName(site, 'portal_catalog', None)
+    if tool is None:
+        logger = context.getLogger('catalog')
+        logger.debug('Nothing to export.')
+        return
+
+    exportObjects(tool, '', context)
