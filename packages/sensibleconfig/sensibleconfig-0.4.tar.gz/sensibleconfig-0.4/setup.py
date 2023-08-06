cc5e9c5387527bbb6c80a8bbb54631758f7dc5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package sensibleconfig/0.4/sensibleconfig-0.4.tar.gz

---
+++ packages/sensibleconfig/sensibleconfig-0.4.tar.gz/sensibleconfig-0.4/setup.py
@@ -0,0 +1,17 @@
+
+
+from distutils.core import setup
+
+from sensibleconfig import __doc__ as scdoc
+
+setup(
+    name='sensibleconfig',
+    version='0.4',
+    long_description=scdoc,
+    author='Ali Afshar',
+    author_email='aafshar@gmail.com',
+    url='http://pidsoff.appspot.com/sensibleconfig',
+
+    py_modules=['sensibleconfig']
+)
+
