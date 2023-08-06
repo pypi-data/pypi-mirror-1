8ba095706fc37a9ed1327dc7aa131df8138d8037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:51 +0000

    Add package hurry.resource/0.4.1/hurry.resource-0.4.1.tar.gz

---
+++ packages/hurry.resource/hurry.resource-0.4.1.tar.gz/hurry.resource-0.4.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import sys, os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('src', 'hurry', 'resource', 'README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
+setup(
+    name='hurry.resource',
+    version = '0.4.1',
+    description="Flexible resources for web applications.",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Martijn Faassen',
+    author_email='faassen@startifact.com',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['hurry'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        ],
+    entry_points={},
+    )
