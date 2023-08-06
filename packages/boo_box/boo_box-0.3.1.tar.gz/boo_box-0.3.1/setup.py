3cdc3544d9961f29c508c2df566945fa800e5be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:54 +0000

    Add package boo_box/0.3.1/boo_box-0.3.1.tar.gz

---
+++ packages/boo_box/boo_box-0.3.1.tar.gz/boo_box-0.3.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+
+version = '0.3.1'
+
+setup(name='boo_box',
+      version=version,
+      description="Boo-Box",
+      long_description = open("README.txt").read(),
+      classifiers=['Intended Audience :: Developers'
+      'Development Status :: 4 - Beta'
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Programming Language :: Python'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Walter Cruz',
+      author_email='walter@waltercruz.com',
+      url='http://www.assembla.com/spaces/py_boo_box',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
