02f9838942cba6315e86c3678105b7f6a1779377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:46 +0000

    Add package dolmen.content/0.2.1/dolmen.content-0.2.1.tar.gz

---
+++ packages/dolmen.content/dolmen.content-0.2.1.tar.gz/dolmen.content-0.2.1/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+from os.path import join
+
+name = 'dolmen.content'
+version = '0.2.1'
+readme = open(join('src', 'dolmen', 'content', 'README.txt')).read()
+history = open(join('docs', 'HISTORY.txt')).read()
+
+install_requires = [
+    'setuptools',
+    'grok',
+    'martian',
+    'dolmen.field',
+    'zope.schema',
+    'zope.component',
+    'zope.interface',
+    'zope.dublincore',
+    'zope.i18nmessageid',
+    'zope.app.publisher',
+    ]
+
+tests_require = install_requires + [
+    'zope.testing',
+    'zope.app.testing',
+    'zope.app.zcmlfiles',
+    ]
+
+setup(name = name,
+      version = version,
+      description = 'Dolmen content-type framework',
+      long_description = readme + '\n\n' + history,
+      keywords = 'Grok Zope3 CMS Dolmen Content',
+      author = 'Souheil Chelfouh',
+      author_email = 'souheil@chelfouh.com',
+      url = 'http://gitweb.dolmen-project.org/',
+      download_url = '',
+      license = 'GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages = ['dolmen'],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = False,
+      tests_require = tests_require,
+      install_requires = install_requires,
+      extras_require = {'test': tests_require},
+      test_suite="dolmen.content",
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Zope3',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+)
