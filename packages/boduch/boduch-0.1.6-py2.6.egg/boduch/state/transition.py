ff8bb327ca223a9a03d0d41e76d414b0748e0051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:57 +0000

    Add package boduch/0.1.6/boduch-0.1.6-py2.6.egg

---
+++ packages/boduch/boduch-0.1.6-py2.6.egg/boduch/state/transition.py
@@ -0,0 +1,33 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from zope.interface import implements
+from boduch.interface import IStateTransition
+from boduch.type import Type
+from boduch.event import publish, EventStateTransitionTransition
+
+class StateTransition(Type):
+    implements(IStateTransition)
+    def __init__(self, machine=None, target=None, predicate=None):
+        Type.__init__(self)
+        self.machine=machine
+        self.target=target
+        self.predicate=predicate
+        
+    def transition(self):
+        publish(EventStateTransitionTransition, kw={"transition":self})
+    
+__all__=['StateTransition']
