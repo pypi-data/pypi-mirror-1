84c629a83f855bcd30965278ef0970a952e0359f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:04 +0000

    Add package ks.lib/1.0.4/ks.lib-1.0.4.tar.gz

---
+++ packages/ks.lib/ks.lib-1.0.4.tar.gz/ks.lib-1.0.4/src/ks/lib/schema/validation.py
@@ -0,0 +1,22 @@
+### -*- coding: utf-8 -*- #############################################
+# Разработано компанией Ключевые Решения (http://keysolutions.ru/)
+# Все права защищены, 2006-2007
+#
+# Developed by Key Solutions (http://keysolutions.ru/)
+# All right reserved, 2006-2007
+#######################################################################
+"""validation utils for the Zope 3 based ks.lib.schema package
+
+$Id: validation.py 23861 2007-11-25 00:13:00Z xen $
+"""
+__author__  = "Anatoly Bubenkov"
+__license__ = "ZPL"
+__version__ = "$Revision: 23861 $"
+__date__ = "$Date: 2007-11-25 02:13:00 +0200 (Sun, 25 Nov 2007) $"
+
+from zope.schema.interfaces import RequiredMissing
+
+def iterableRequired(value):
+    if not value:
+        raise RequiredMissing
+    return True
