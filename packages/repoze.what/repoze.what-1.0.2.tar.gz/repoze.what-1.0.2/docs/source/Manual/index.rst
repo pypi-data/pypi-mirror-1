8cc09290a7445b252e0860af2c0db7fb4c883399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:26 +0000

    Add package repoze.what/1.0.2/repoze.what-1.0.2.tar.gz

---
+++ packages/repoze.what/repoze.what-1.0.2.tar.gz/repoze.what-1.0.2/docs/source/Manual/index.rst
@@ -0,0 +1,17 @@
+*****************************
+The :mod:`repoze.what` Manual
+*****************************
+
+:Author: Gustavo Narea.
+:Version: |version|
+
+Below are the contents for the :mod:`repoze.what` manual:
+
+.. toctree::
+    :maxdepth: 2
+
+    GettingStarted
+    Predicates
+    ManagingSources
+    Plugins/index
+    InnerWorkings
---
+++ packages/repoze.what/repoze.what-1.0.2.tar.gz/repoze.what-1.0.2/repoze/what/predicates.py
@@ -0,0 +1,467 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2007, Agendaless Consulting and Contributors.
+# Copyright (c) 2008, Florent Aide <florent.aide@gmail.com>.
+# Copyright (c) 2008-2009, Gustavo Narea <me@gustavonarea.net>.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+"""
+Built-in predicate checkers.
+
+This is module provides the predicate checkers that were present in the 
+original "identity" framework of TurboGears 1, plus others.
+
+"""
+
+__all__ = ['Predicate', 'CompoundPredicate', 'All', 'Any', 
+           'has_all_permissions', 'has_any_permission', 'has_permission', 
+           'in_all_groups', 'in_any_group', 'in_group', 'is_user', 
+           'not_anonymous', 'PredicateError']
+
+
+#{ Predicates
+
+
+class Predicate(object):
+    """
+    Generic predicate checker.
+    
+    This is the base predicate class. It won't do anything useful for you, 
+    unless you subclass it.
+    
+    """
+    
+    def __init__(self, msg=None):
+        """
+        Create a predicate and use ``msg`` as the error message if it fails.
+        
+        :param msg: The error message, if you want to override the default one
+            defined by the predicate.
+        :type msg: str
+        
+        You may use the ``msg`` keyword argument with any predicate.
+        
+        """
+        if msg:
+            self.message = msg
+    
+    def evaluate(self, environ, credentials):
+        """
+        Raise an exception if the predicate is not met.
+        
+        :param environ: The WSGI environment.
+        :type environ: dict
+        :param credentials: The :mod:`repoze.what` ``credentials`` dictionary
+            as a short-cut.
+        :type credentials: dict
+        :raise NotImplementedError: When the predicate doesn't define this
+            method.
+        :raises PredicateError: If the predicate is not met (use :meth:`unmet`
+            to raise it).
+        
+        This is the method that must be overridden by any predicate.
+        
+        For example, if your predicate is "The current month is the specified
+        one", you may define the following predicate checker::
+        
+            from datetime import date
+            from repoze.what.predicates import Predicate
+            
+            class is_month(Predicate):
+                message = 'The current month must be %(right_month)s'
+                
+                def __init__(self, right_month, **kwargs):
+                    self.right_month = right_month
+                    super(is_month, self).__init__(**kwargs)
+                
+                def evaluate(self, environ, credentials):
+                    today = date.today()
+                    if today.month != self.right_month:
+                        # Raise an exception because the predicate is not met.
+                        self.unmet()
+        
+        .. attention::
+            Do not evaluate predicates by yourself using this method. See
+            :func:`repoze.what.authorize.check_authorization`.
+
+        .. warning::
+        
+            To make your predicates thread-safe, keep in mind that they may
+            be instantiated at module-level and then shared among many threads,
+            so avoid predicates from being modified after their evaluation. 
+            This is, the ``evaluate()`` method should not add, modify or 
+            delete any attribute of the predicate.
+        
+        """
+        self.eval_with_environ(environ)
+    
+    def _eval_with_environ(self, environ):
+        """
+        Check whether the predicate is met.
+        
+        :param environ: The WSGI environment.
+        :type environ: dict
+        :return: Whether the predicate is met or not.
+        :rtype: bool
+        :raise NotImplementedError: This must be defined by the predicate
+            itself.
+        
+        .. deprecated:: 1.0.2
+            Only :meth:`evaluate` will be used as of :mod:`repoze.what` v2.
+        
+        """
+        raise NotImplementedError
+    
+    def eval_with_environ(self, environ):
+        """
+        Make sure this predicate is met.
+        
+        :param environ: The WSGI environment.
+        :raises PredicateError: If the predicate is not met.
+        
+        .. versionchanged:: 1.0.1
+            In :mod:`repoze.what`<1.0.1, this method returned a ``bool`` and
+            set the ``error`` instance attribute of the predicate to the
+            predicate message.
+        
+        .. deprecated:: 1.0.2
+            Define :meth:`evaluate` instead.
+        
+        """
+        from warnings import warn
+        msg = 'Predicate._eval_with_environ(environ) is deprecated ' \
+              'for forward compatibility with repoze.what v2; use ' \
+              'Predicate.evaluate(environ, credentials) instead'
+        warn(msg, DeprecationWarning, stacklevel=2)
+        if not self._eval_with_environ(environ):
+            self.unmet()
+    
+    def unmet(self, **placeholders):
+        """
+        Raise an exception because this predicate is not met.
+        
+        :raises PredicateError: If the predicate is not met.
+        
+        ``placeholders`` represent the placeholders for the predicate message.
+        The predicate's attributes will also be taken into account while
+        creating the message with its placeholders.
+        
+        For example, if you have a predicate that checks that the current
+        month is the specified one, where the predicate message is defined with
+        two placeholders as in::
+        
+            The current month must be %(right_month)s and it is %(this_month)s
+        
+        and the predicate has an attribute called ``right_month`` which
+        represents the expected month, then you can use this method as in::
+        
+            self.unmet(this_month=this_month)
+        
+        Then :meth:`unmet` will build the message using the ``this_month``
+        keyword argument and the ``right_month`` attribute as the placeholders
+        for ``this_month`` and ``right_month``, respectively. So, if
+        ``this_month`` equals ``3`` and ``right_month`` equals ``5``,
+        the message for the exception to be raised will be::
+        
+            The current month must be 5 and it is 3
+        
+        """
+        # Include the predicate attributes in the placeholders:
+        all_placeholders = self.__dict__.copy()
+        all_placeholders.update(placeholders)
+        msg = self.message % all_placeholders
+        raise PredicateError(msg)
+
+
+class CompoundPredicate(Predicate):
+    """A predicate composed of other predicates."""
+    
+    def __init__(self, *predicates, **kwargs):
+        super(CompoundPredicate, self).__init__(**kwargs)
+        self.predicates = predicates
+
+
+class Not(Predicate):
+    """
+    Negate the specified predicate.
+    
+    :param predicate: The predicate to be negated.
+    
+    Example::
+    
+        # The user *must* be anonymous:
+        p = Not(not_anonymous())
+    
+    """
+    message = u"The condition must not be met"
+
+    def __init__(self, predicate, **kwargs):
+        super(Not, self).__init__(**kwargs)
+        self.predicate = predicate
+    
+    def evaluate(self, environ, credentials):
+        try:
+            self.predicate.evaluate(environ, credentials)
+        except PredicateError, error:
+            return
+        self.unmet()
+
+
+class All(CompoundPredicate):
+    """
+    Check that all of the specified predicates are met.
+    
+    :param predicates: All of the predicates that must be met.
+    
+    Example::
+    
+        # Grant access if the current month is July and the user belongs to
+        # the human resources group.
+        p = All(is_month(7), in_group('hr'))
+    
+    """
+    
+    def evaluate(self, environ, credentials):
+        """
+        Evaluate all the predicates it contains.
+        
+        :param environ: The WSGI environment.
+        :param credentials: The :mod:`repoze.what` ``credentials``.
+        :raises PredicateError: If one of the predicates is not met.
+        
+        """
+        for p in self.predicates:
+            p.evaluate(environ, credentials)
+
+
+class Any(CompoundPredicate):
+    """
+    Check that at least one of the specified predicates is met.
+    
+    :param predicates: Any of the predicates that must be met.
+    
+    Example::
+    
+        # Grant access if the currest user is Richard Stallman or Linus
+        # Torvalds.
+        p = Any(is_user('rms'), is_user('linus'))
+    
+    """
+    message = u"At least one of the following predicates must be met: " \
+               "%(failed_predicates)s"
+    
+    def evaluate(self, environ, credentials):
+        """
+        Evaluate all the predicates it contains.
+        
+        :param environ: The WSGI environment.
+        :param credentials: The :mod:`repoze.what` ``credentials``.
+        :raises PredicateError: If none of the predicates is met.
+        
+        """
+        errors = []
+        for p in self.predicates:
+            try:
+                p.evaluate(environ, credentials)
+                return
+            except PredicateError, exc:
+                errors.append(unicode(exc))
+        failed_predicates = ', '.join(errors)
+        self.unmet(failed_predicates=failed_predicates)
+
+
+class is_user(Predicate):
+    """
+    Check that the authenticated user's username is the specified one.
+    
+    :param user_name: The required user name.
+    :type user_name: str
+    
+    Example::
+    
+        p = is_user('linus')
+    
+    """
+    
+    message = u'The current user must be "%(user_name)s"'
+
+    def __init__(self, user_name, **kwargs):
+        super(is_user, self).__init__(**kwargs)
+        self.user_name = user_name
+
+    def evaluate(self, environ, credentials):
+        if credentials and \
+           self.user_name == credentials.get('repoze.what.userid'):
+            return
+        self.unmet()
+
+
+class in_group(Predicate):
+    """
+    Check that the user belongs to the specified group.
+    
+    :param group_name: The name of the group to which the user must belong.
+    :type group_name: str
+    
+    Example::
+    
+        p = in_group('customers')
+    
+    """
+    
+    message = u'The current user must belong to the group "%(group_name)s"'
+
+    def __init__(self, group_name, **kwargs):
+        super(in_group, self).__init__(**kwargs)
+        self.group_name = group_name
+
+    def evaluate(self, environ, credentials):
+        if credentials and self.group_name in credentials.get('groups'):
+            return
+        self.unmet()
+
+
+class in_all_groups(All):
+    """
+    Check that the user belongs to all of the specified groups.
+    
+    :param groups: The name of all the groups the user must belong to.
+    
+    Example::
+    
+        p = in_all_groups('developers', 'designers')
+    
+    """
+    
+    
+    def __init__(self, *groups, **kwargs):
+        group_predicates = [in_group(g) for g in groups]
+        super(in_all_groups,self).__init__(*group_predicates, **kwargs)
+
+
+class in_any_group(Any):
+    """
+    Check that the user belongs to at least one of the specified groups.
+    
+    :param groups: The name of any of the groups the user may belong to.
+    
+    Example::
+    
+        p = in_any_group('directors', 'hr')
+    
+    """
+    
+    message = u"The member must belong to at least one of the following " \
+               "groups: %(group_list)s"
+
+    def __init__(self, *groups, **kwargs):
+        self.group_list = ", ".join(groups)
+        group_predicates = [in_group(g) for g in groups]
+        super(in_any_group,self).__init__(*group_predicates, **kwargs)
+
+
+class not_anonymous(Predicate):
+    """
+    Check that the current user has been authenticated.
+    
+    Example::
+    
+        # The user must have been authenticated!
+        p = not_anonymous()
+    
+    """
+    
+    message = u"The current user must have been authenticated"
+
+    def evaluate(self, environ, credentials):
+        if not credentials:
+            self.unmet()
+
+
+class has_permission(Predicate):
+    """
+    Check that the current user has the specified permission.
+    
+    :param permission_name: The name of the permission that must be granted to 
+        the user.
+    
+    Example::
+    
+        p = has_permission('hire')
+    
+    """
+    message = u'The user must have the "%(permission_name)s" permission'
+
+    def __init__(self, permission_name, **kwargs):
+        super(has_permission, self).__init__(**kwargs)
+        self.permission_name = permission_name
+
+    def evaluate(self, environ, credentials):
+        if credentials and \
+           self.permission_name in credentials.get('permissions'):
+            return
+        self.unmet()
+
+
+class has_all_permissions(All):
+    """
+    Check that the current user has been granted all of the specified 
+    permissions.
+    
+    :param permissions: The names of all the permissions that must be
+        granted to the user.
+    
+    Example::
+    
+        p = has_all_permissions('view-users', 'edit-users')
+    
+    """
+    
+    def __init__(self, *permissions, **kwargs):
+        permission_predicates = [has_permission(p) for p in permissions]
+        super(has_all_permissions, self).__init__(*permission_predicates,
+                                                  **kwargs)
+
+
+class has_any_permission(Any):
+    """
+    Check that the user has at least one of the specified permissions.
+    
+    :param permissions: The names of any of the permissions that have to be
+        granted to the user.
+    
+    Example::
+    
+        p = has_any_permission('manage-users', 'edit-users')
+    
+    """
+    
+    message = u"The user must have at least one of the following " \
+               "permissions: %(permission_list)s"
+
+    def __init__(self, *permissions, **kwargs):
+        self.permission_list = ", ".join(permissions)
+        permission_predicates = [has_permission(p) for p in permissions]
+        super(has_any_permission,self).__init__(*permission_predicates,
+                                                **kwargs)
+
+
+#{ Exceptions
+
+
+class PredicateError(Exception):
+    """Exception raised by a :class:`Predicate` it's not met."""
+    pass
+
+
+#}
