7c2e42a743f73f733a947477364109bb3708f8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:53 +0000

    Add package getpaid.payflowpro/1.0b1/getpaid.payflowpro-1.0b1.tar.gz

---
+++ packages/getpaid.payflowpro/getpaid.payflowpro-1.0b1.tar.gz/getpaid.payflowpro-1.0b1/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='getpaid.payflowpro',
+    version='1.0b1',
+    license = 'ZPL2.1',
+    author='Rob LaRubbio',
+    author_email='rob@onenw.org',
+    description='Get paid paypal payment processor functionality',
+    long_description = (
+        read('README.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'getpaid', 'payflowpro', 'README.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Software Development :: Libraries",
+        ],
+    url='http://code.google.com/p/getpaid',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['getpaid'],
+    include_package_data=True,
+    install_requires = ['setuptools',
+                        'python-payflowpro',
+                       ],
+    zip_safe = False,
+    )
