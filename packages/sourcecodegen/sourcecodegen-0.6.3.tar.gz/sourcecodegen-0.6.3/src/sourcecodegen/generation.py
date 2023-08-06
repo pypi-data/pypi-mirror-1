2c4958a70b46142b16fa87c934aaacd76fb39420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:17 +0000

    Add package sourcecodegen/0.6.3/sourcecodegen-0.6.3.tar.gz

---
+++ packages/sourcecodegen/sourcecodegen-0.6.3.tar.gz/sourcecodegen-0.6.3/src/sourcecodegen/generation.py
@@ -0,0 +1,13 @@
+from visitor import ASTVisitor
+
+class ModuleSourceCodeGenerator(object):
+    """Generates Python source code from an AST tree (as parsed by the
+    ``compiler.parse`` method)."""
+
+    def __init__(self, tree):
+        self.tree = tree
+
+    def getSourceCode(self):
+        visitor = ASTVisitor(self.tree)
+        return visitor()
+
