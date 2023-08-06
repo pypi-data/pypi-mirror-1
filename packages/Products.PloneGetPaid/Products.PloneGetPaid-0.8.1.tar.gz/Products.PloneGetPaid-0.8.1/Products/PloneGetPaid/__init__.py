4138cdaddb2e3d49349d97a9ffda5c082f40ed67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:48 +0000

    Add package Products.PloneGetPaid/0.8.1/Products.PloneGetPaid-0.8.1.tar.gz

---
+++ packages/Products.PloneGetPaid/Products.PloneGetPaid-0.8.1.tar.gz/Products.PloneGetPaid-0.8.1/Products/PloneGetPaid/__init__.py
@@ -0,0 +1,23 @@
+"""
+$Id: __init__.py 1690 2008-07-30 04:50:25Z matt.halstead $
+"""
+
+from config import PLONE3
+# CMFonFive has been integrated in CMFCore since version 2.0, so in
+# Plone 3.0 we do not depend on it anymore.
+if PLONE3:
+    _GETPAID_DEPENDENCIES_ = [ ]
+else:
+    _GETPAID_DEPENDENCIES_ = [ 'CMFonFive' ]
+
+import os, sys
+from Globals import package_home
+
+import _patch
+
+pkg_home = package_home( globals() )
+lib_path = os.path.join( pkg_home, 'lib' )
+if os.path.exists( lib_path ):
+    sys.path.append( lib_path )
+import catalog
+import permissions
