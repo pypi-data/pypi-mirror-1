db083bae9c51f3a1dde3390c3ca9d11f6e5f1dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:05 +0000

    Add package boduch/0.0.5/boduch-0.0.5.tar.gz

---
+++ packages/boduch/boduch-0.0.5.tar.gz/boduch-0.0.5/src/boduch/constant/priority.py
@@ -0,0 +1,25 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This module defines constants related to handle priority."""
+
+PRIORITY_CRITICAL=40
+PRIORITY_MAJOR=   30
+PRIORITY_MINOR=   20
+PRIORITY_TRIVIAL= 10
+
+__all__=['PRIORITY_CRITICAL', 'PRIORITY_MAJOR', 'PRIORITY_MINOR',\
+         'PRIORITY_TRIVIAL']
---
+++ packages/boduch/boduch-0.0.5.tar.gz/boduch-0.0.5/src/boduch/handle/set.py
@@ -0,0 +1,41 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This module defines event handles for Set events."""
+
+from boduch.handle import Handle
+from boduch.event import lock, unlock
+from boduch import PRIORITY_CRITICAL
+
+class HandleSetPush(Handle):
+    """Handle that actually places the data onto the set in response to the
+    EventSetPush event."""
+    def __init__(self, *args, **kw):
+        Handle.__init__(self, *args, **kw)
+        self.priority=PRIORITY_CRITICAL
+        
+    def run(self):
+        """Run the handle.  First we get the set instance and the new object
+        from the event.  Next we acquire the event lock and perform the
+        push."""
+        event_obj=self.data['event']
+        set_obj=event_obj.data['set']
+        data=event_obj.data['obj']
+        lock()
+        set_obj.data.append(data)
+        unlock()
+        
+__all__=['HandleSetPush']
