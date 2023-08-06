41b2caae325aa9b694df862217900167fbcce532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:11 +0000

    Add package xm.portlets/0.7/xm.portlets-0.7.tar.gz

---
+++ packages/xm.portlets/xm.portlets-0.7.tar.gz/xm.portlets-0.7/xm/portlets/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+ProjectPortletMessageFactory = MessageFactory('xm.portlets')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
