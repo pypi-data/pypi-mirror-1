872481c2d2e6f139598261a7481c589152666bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:38 +0000

    Add package transmissionrpc/0.4/transmissionrpc-0.4.tar.gz

---
+++ packages/transmissionrpc/transmissionrpc-0.4.tar.gz/transmissionrpc-0.4/transmissionrpc/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2008-2010 Erik Svensson <erik.public@gmail.com>
+# Licensed under the MIT license.
+
+from constants import *
+from transmission import TransmissionError, Torrent, Session, Client
+
+__author__    = u'Erik Svensson <erik.public@gmail.com>'
+__version__   = u'0.4'
+__copyright__ = u'Copyright (c) 2008-2010 Erik Svensson'
+__license__   = u'MIT'
