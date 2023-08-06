88e2e8c88679f4f965fdb8e3ace773e33de12640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package pbp.scripts/0.2.1/pbp.scripts-0.2.1.tar.gz

---
+++ packages/pbp.scripts/pbp.scripts-0.2.1.tar.gz/pbp.scripts-0.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2.1'
+long_description = open("README.txt").read()
+classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+
+setup(name='pbp.scripts',
+      version=version,
+      description="Expert Python Programming - Contains scripts presented throughout the book",
+      long_description=long_description,
+      classifiers=classifiers,
+      keywords='',
+      author='Tarek Ziade',
+      author_email='tarek@ziade.org',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pbp'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'guppy'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
