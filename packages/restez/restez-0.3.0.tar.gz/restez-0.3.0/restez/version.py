8e39df17398a9b23d8222163b675a435f7b0d6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:52 +0000

    Add package restez/0.3.0/restez-0.3.0.tar.gz

---
+++ packages/restez/restez-0.3.0.tar.gz/restez-0.3.0/restez/version.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+'''version
+'''
+
+__author__ = 'Chris Miles'
+__copyright__ = '(c) Chris Miles 2009. All rights reserved.'
+__id__ = '$Id: version.py 24 2009-09-08 02:16:37Z miles.chris $'
+__url__ = '$URL: https://restez.googlecode.com/svn/trunk/restez/version.py $'
+
+version = '0.3.0'
