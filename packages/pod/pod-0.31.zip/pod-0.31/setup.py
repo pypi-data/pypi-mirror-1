dc1c4f0a1c7d2401424c7a7e9da9532012169247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:02 +0000

    Add package pod/0.31/pod-0.31.zip

---
+++ packages/pod/pod-0.31.zip/pod-0.31/setup.py
@@ -0,0 +1,17 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "pod",
+    version = "0.31",
+    author = "Andrew Carter, et al",
+    author_email = "andrewjcarter@gmail.com",
+    description = "A Python Object Database Implemented Using cPickle and SQLite - An easy way to store and fetch Python objects",
+    license = "BSD",
+    keywords = "python object database pickle persistent persistence object relational mapper ODBMS ORM",
+    url = "http://code.google.com/p/pickled-object-database/",   # project home page, if any
+    packages     = find_packages(),
+    long_description = "The 0.30 release changes underlying architecture to increase insert speed, adds new features.  Not backward compatible.  Visit http://code.google.com/p/pickled-object-database/wiki/what_is_new"
+)
