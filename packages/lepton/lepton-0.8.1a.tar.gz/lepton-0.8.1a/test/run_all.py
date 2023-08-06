f17aa92ea849b423db0f04550d283c586bfb6557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:56 +0000

    Add package lepton/0.8.1a/lepton-0.8.1a.tar.gz

---
+++ packages/lepton/lepton-0.8.1a.tar.gz/lepton-0.8.1a/test/run_all.py
@@ -0,0 +1,26 @@
+#############################################################################
+#
+# Copyright (c) 2008 by Casey Duncan and contributors
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the MIT License
+# A copy of the license should accompany this distribution.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#
+#############################################################################
+"""Run all particle unit tests"""
+
+__version__ = '$Id: run_all.py 34 2008-08-06 23:41:16Z casey.duncan $'
+
+import unittest
+
+from controller_test import *
+from emitter_test import *
+from group_test import *
+from system_test import *
+from domain_test import *
+
+if __name__ == '__main__':
+	unittest.main()
