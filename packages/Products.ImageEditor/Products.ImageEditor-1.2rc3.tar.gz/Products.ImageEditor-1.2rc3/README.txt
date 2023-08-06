7e7e830c1454f679b9d3c9915ef50fc8fc468cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:34 +0000

    Add package Products.ImageEditor/1.2rc3/Products.ImageEditor-1.2rc3.tar.gz

---
+++ packages/Products.ImageEditor/Products.ImageEditor-1.2rc3.tar.gz/Products.ImageEditor-1.2rc3/README.txt
@@ -0,0 +1,38 @@
+Introduction
+============
+Once installed this product adds a "Image Editor" tab for the image content type and News Item content type.
+
+Features
+========
+
+* rotate
+* flip
+* blur
+* compress
+* contrast
+* brightness
+* sharpen
+* add drop shadows
+* crop
+* resize
+* sepia
+* save as another content type.
+
+Roadmap / Proposal
+==================
+
+* Override the Archetype's ImageWidget to add link to the image editor (only in widget edit mode ?)
+* Categorize actions (effects, transformations, colors)
+* Migration from 1.1 to 1.2
+
+Dependencies
+============
+
+* collective.js.jquery
+* collective.js.jqueryui
+
+Contributors
+============
+* Jean-Michel FRANCOIS
+* Davi Lima
+* Sylvain Boureliou
---
+++ packages/Products.ImageEditor/Products.ImageEditor-1.2rc3.tar.gz/Products.ImageEditor-1.2rc3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+dependencies = [
+    'setuptools'
+]
+
+#now get plone dependencies
+dep_file = open(os.path.join('Products', 'ImageEditor', 'dependencies.txt'))
+dependencies.extend([ d.strip('\n').strip() for d in dep_file.readlines() ])
+
+setup(name='Products.ImageEditor',
+      version='1.2rc3',
+      description="A product that adds an editor tab to ATImage and News Item so you can rotate, flip, blur, compress, contrast, brightness, sharpen, add drop shadows, crop, resize an image.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='atimage plone image rotate flip blur compress contrast brightness sharpen drop shadows crop resize',
+      author='Nathan Van Gheem',
+      author_email='vangheem@gmail.com',
+      url='http://www.plone.org/products/products-imageeditor',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=dependencies,
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
