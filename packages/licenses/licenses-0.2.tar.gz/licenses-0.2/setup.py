47fa8f2bbaed6cfc53c03fbdec05d8a6eb1ee609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:49 +0000

    Add package licenses/0.2/licenses-0.2.tar.gz

---
+++ packages/licenses/licenses-0.2.tar.gz/licenses-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+__version__ = 0.2
+
+setup(
+    name='licenses',
+    version=__version__,
+
+    # general metadata
+    description='List of open (http://opensource.org/, http://opendefinition.org/) licenses',
+    license='PD',
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    url='http://www.knowledgeforge.net/okfn/licenses/',
+    py_modules=['licenses'],
+)
