26bfdb3da84ab3213fe41f95b08cc3a4408f48d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:55 +0000

    Add package SPARQLWrapper/1.3.0/SPARQLWrapper-1.3.0.tar.gz

---
+++ packages/SPARQLWrapper/SPARQLWrapper-1.3.0.tar.gz/SPARQLWrapper-1.3.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+      name='SPARQLWrapper',
+      version='1.3.0',
+      description='SPARQL Endpoint interface to Python',
+      long_description = 'This is a wrapper around a SPARQL service. It helps in creating the query URI and, possibly, convert the result into a more manageable format.',
+      license = 'W3C SOFTWARE NOTICE AND LICENSE', #Should be removed by PEP  314
+      author="Ivan Herman, Sergio Fernandez, Carlos Tejo Alonso",
+      author_email="ivan at ivan-herman net, sergio.fernandez at fundacionctic org, carlos.tejo at fundacionctic org",
+      url = 'http://sparql-wrapper.sourceforge.net/',
+      download_url = 'http://sourceforge.net/projects/sparql-wrapper/files',
+      platforms = ['any'], #Should be removed by PEP  314
+      packages=['SPARQLWrapper'],
+      requires=['simplejson'], # Used by distutils to create metadata PKG-INFO
+      install_requires=['simplejson == 2.0.9',], #Used by setuptools to install the dependencies
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: W3C License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.5',
+      ],
+      keywords = 'python SPARQL',
+      requires_python = '>=2.5', # Future in PEP 345
+      scripts = ['ez_setup.py']
+)
+
