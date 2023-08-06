039735cf584627d8d43a1295b5951a1e97e94a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:01 +0000

    Add package Products.SmartPrintNG/1.1.1/Products.SmartPrintNG-1.1.1-py2.4.egg

---
+++ packages/Products.SmartPrintNG/Products.SmartPrintNG-1.1.1-py2.4.egg/Products/SmartPrintNG/skins/smartprintng/smartprint.js
@@ -0,0 +1,154 @@
+/*
+ * SmartPrintNG - high-quality export of Plone content to
+ * PDF, RTF, ODT, WML and DOCX
+ *
+ * (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+ *
+ * reviewed by SauZheR to use JQuery framework
+ */
+
+var in_progres = false;
+
+
+// remove stuff from the copied content region
+
+function cleanupContent(node) {
+
+    var arr = node.getElementsByTagName('div');
+    for (var i=0; i<arr.length; i++) {
+        var classname= arr[i].className;
+        if (classname.indexOf('documentActions') != -1 ||
+            classname.indexOf('documentByLine') != -1) {
+            arr[i].parentNode.removeChild(arr[i]);
+        }
+    }
+
+    var arr = node.getElementsByTagName('p');
+    for (var i=0; i<arr.length; i++) {
+        if (arr[i].id == 'link-presentation') {
+            arr[i].parentNode.removeChild(arr[i]);
+        }
+    }
+
+    var arr = node.getElementsByTagName('div');
+    for (var i=0; i<arr.length; i++) {
+        if (arr[i].id == 'smartprint') {
+            arr[i].parentNode.removeChild(arr[i]);
+        }
+    }
+}
+
+// return either the 'content' element or the 'region-content' element
+
+function getContentElement() {
+
+    var el = $('#content');
+    if (! el[0])
+        el = $('#region-content');
+
+
+    if (! el[0])
+        alert('No DOM node with id "region-content" or "content" found');
+
+    return el;
+}
+
+
+function openControl() {
+
+    if (! $('#smartprint')[0]) {
+
+        var divContent  = getContentElement().clone(true);
+        if (! divContent) {
+            alert('Error: the element with id=content could not be found!');
+            return;
+        }
+
+        var newdiv = document.createElement('div');
+        newdiv.id = 'smartprint';
+        getContentElement().append(newdiv);
+    } else {
+
+        if (in_progres) {
+            alert('Conversion in progres...please wait');
+            return;
+        }
+
+        closeControl();
+    }
+}
+
+function closeControl() {
+    $('#smartprint').remove();
+}
+
+
+// open the SP control and show the selection HTML snippet
+
+function showResponse(request) {
+    if ($('#smartprint'))
+        $('#smartprint')[0].innerHTML = request.responseText;
+}
+
+function smartPrintSelection(url, template) {
+
+    openControl();
+
+    var url = url + '/' + template;
+    var r = $.ajax( {url: url,
+                    type: 'GET',
+                    complete: showResponse
+                            });
+
+}
+
+
+function startConversion(url) {
+
+    var divContent  = getContentElement().clone(true);
+    if (! divContent) {
+        alert('Error: the element with id=content could not be found!');
+        return;
+    }
+    divContent =divContent[0]
+    // Not sure if we should cleanup the HTML on the server side.
+    cleanupContent(divContent);
+
+    // put HTML into the form in order to generate a query string from it
+    $('#smartprint-html')[0].value = divContent.innerHTML
+
+    var postBody= $('#smartprint-selection-form').serialize();
+
+    in_progres = true;
+
+    // Load "in-progres" page
+    var r = $.ajax( {url: url + '/sp_progres',
+                              type: 'GET',
+                              asynchronous: false,
+                              complete: showResponse
+                            });
+
+    var r2 = $.ajax({url:url + '/smartPrintConvert',
+                               type: 'POST',
+                               asynchronous: true,
+                               data: postBody,
+                               requestHeaders: ['content-type', 'application/x-www-form-urlencoded'],
+                               complete: function(request) {
+                                    in_progres = false;
+                                    closeControl();
+                                    if (request.status == 200) {
+                                        var filename = request.responseText;
+                                        document.location = url + '/smartPrintDeliver?filename=' + filename;
+                                    } else {
+                                        var status = request.status;
+                                        alert('Conversion failed (Error code: ' + status + ')');
+                                    }
+                               },
+                               onFailure: function(request){
+                                   in_progres = false;
+                                   var status = request.status;
+                                   alert('Conversion failed (Error code: ' + status + ')');
+                                   closeControl();
+                               }
+                            });
+}
---
+++ packages/Products.SmartPrintNG/Products.SmartPrintNG-1.1.1-py2.4.egg/Products/SmartPrintNG/standard_extractor.py
@@ -0,0 +1,27 @@
+##########################################################################
+# SmartPrintNG - high-quality export of Plone content to
+# PDF, RTF, ODT, WML and DOCX
+#
+# (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+"""
+Multipurpose HTML extractor for Plone content
+"""
+
+from zope.interface import implements
+from zope.component import adapts
+
+from Products.SmartPrintNG.interfaces import IHTMLExtractor 
+from Products.ATContentTypes.interface.document import IATDocument
+
+from BeautifulSoup import BeautifulSoup
+
+
+def standard_html_extractor(obj):
+    """ Extract HTML from an object by rendering it. """
+
+    html = obj.view()
+    soup = BeautifulSoup(html)
+    body = str(soup.findAll('div', {'class' : 'documentContent'})[0])
+    return body
