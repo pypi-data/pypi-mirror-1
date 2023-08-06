f10a62c4d67093d5f78b5c47f45b4ebeb6a98333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:23 +0000

    Add package liqpay/1.0.1/liqpay-1.0.1.tar.gz

---
+++ packages/liqpay/liqpay-1.0.1.tar.gz/liqpay-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "liqpay",
+    version = "1.0.1",
+    packages = find_packages(),
+    author = "Sergey Kirillov",
+    author_email = "serg@rainboo.com",
+    description = "Python interface for Liqpay e-comerce platform",
+    url = "http://bitbucket.org/rushman/liqpay/",
+)
