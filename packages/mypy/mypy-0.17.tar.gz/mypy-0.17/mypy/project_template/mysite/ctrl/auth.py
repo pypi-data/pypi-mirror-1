c6ac26b88f112edd98a58ad057db5402c6be7d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:20 +0000

    Add package mypy/0.17/mypy-0.17.tar.gz

---
+++ packages/mypy/mypy-0.17.tar.gz/mypy-0.17/mypy/project_template/mysite/ctrl/auth.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+from mypy.route_render import route_render_func
+
+@route_render_func
+def apply():
+    pass
+
+@route_render_func
+def reg():
+    pass
---
+++ packages/mypy/mypy-0.17.tar.gz/mypy-0.17/mypy/project_template/mysite/util/security.py
@@ -0,0 +1,4 @@
+#coding:utf-8
+
+def challenge_key():
+    return "xxxxxx"
