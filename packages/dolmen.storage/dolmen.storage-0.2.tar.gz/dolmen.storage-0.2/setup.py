ac0dc007fed84836b4cbeea098395efb1d7b9c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:56 +0000

    Add package dolmen.storage/0.2/dolmen.storage-0.2.tar.gz

---
+++ packages/dolmen.storage/dolmen.storage-0.2.tar.gz/dolmen.storage-0.2/setup.py
@@ -0,0 +1,54 @@
+from os.path import join
+from setuptools import setup, find_packages
+
+name = 'dolmen.storage'
+version = '0.2'
+readme = open(join('src', 'dolmen', 'storage', 'README.txt')).read()
+history = open(join('docs', 'HISTORY.txt')).read()
+
+install_requires = [
+    'setuptools',
+    'zope.schema',
+    'zope.component',
+    'zope.interface',
+    'zope.annotation',
+    'zope.container',
+    'grokcore.component',
+    ]
+
+tests_require = [
+    'zope.component',
+    'zope.testing',
+    'zope.app.testing',
+    'zope.app.zcmlfiles',
+    ]
+
+setup(name = name,
+      version = version,
+      description = 'Dolmen Zope3 Grok Storage Annotation',
+      long_description = readme + '\n\n' + history,
+      keywords = 'Grok Zope3 CMS Dolmen',
+      author = 'Souheil Chelfouh',
+      author_email = 'trollfot@gmail.com',
+      url = 'http://gitweb.dolmen-project.org',
+      download_url = '',
+      license = 'GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages = ['dolmen'],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = False,
+      tests_require = tests_require,
+      install_requires = install_requires,
+      extras_require = {'test': tests_require},
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Zope3',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+)
---
+++ packages/dolmen.storage/dolmen.storage-0.2.tar.gz/dolmen.storage-0.2/src/dolmen/storage/tests/test_all.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+import unittest
+import pkg_resources
+
+from zope.testing import doctest
+from zope.app.testing import functional
+
+ftesting_zcml = os.path.join(os.path.dirname(__file__), 'ftesting.zcml')
+FunctionalLayer = functional.ZCMLLayer(
+    ftesting_zcml, __name__, 'FunctionalLayer', allow_teardown=True)
+
+
+def suiteFromPackage(name):
+    files = pkg_resources.resource_listdir(__name__, name)
+    suite = unittest.TestSuite()
+    for filename in files:
+        if not filename.endswith('.py'):
+            continue
+        if filename.endswith('_fixture.py'):
+            continue
+        if filename == '__init__.py':
+            continue
+
+        dottedname = 'dolmen.storage.tests.%s.%s' % (name, filename[:-3])
+        test = doctest.DocTestSuite(
+            dottedname,
+            optionflags=doctest.ELLIPSIS+doctest.NORMALIZE_WHITESPACE)
+        suite.addTest(test)
+    return suite
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+
+    readme = functional.FunctionalDocFileSuite(
+        '../README.txt',
+        optionflags=(doctest.ELLIPSIS + doctest.NORMALIZE_WHITESPACE),
+        )
+    readme.layer = FunctionalLayer
+    suite.addTest(readme)
+    
+    for name in ['container', 'annotations']:
+        suite.addTest(suiteFromPackage(name))
+    
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
