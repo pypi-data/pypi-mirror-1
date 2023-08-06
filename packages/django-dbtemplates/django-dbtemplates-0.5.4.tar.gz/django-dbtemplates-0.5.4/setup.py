3a1a69160be860d3fb7d2430c745a44a695abaa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:38 +0000

    Add package django-dbtemplates/0.5.4/django-dbtemplates-0.5.4.tar.gz

---
+++ packages/django-dbtemplates/django-dbtemplates-0.5.4.tar.gz/django-dbtemplates-0.5.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='django-dbtemplates',
+    version=__import__('dbtemplates').__version__,
+    description='Template loader for database stored templates with extensible cache backend',
+    long_description=open('docs/overview.txt').read(),
+    author='Jannis Leidel',
+    author_email='jannis@leidel.info',
+    url='http://github.com/jezdez/django-dbtemplates/wikis/',
+    download_url='http://github.com/jezdez/django-dbtemplates/zipball/0.5.4',
+    packages=[
+        'dbtemplates',
+        'dbtemplates.management',
+        'dbtemplates.management.commands'
+    ],
+    package_dir={'dbtemplates': 'dbtemplates'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
