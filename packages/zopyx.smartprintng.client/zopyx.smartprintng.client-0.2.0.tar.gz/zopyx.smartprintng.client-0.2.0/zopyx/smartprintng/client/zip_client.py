99785e0fcd39797a93137ee5f0faacd95e24607d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:51 +0000

    Add package zopyx.smartprintng.client/0.2.0/zopyx.smartprintng.client-0.2.0.tar.gz

---
+++ packages/zopyx.smartprintng.client/zopyx.smartprintng.client-0.2.0.tar.gz/zopyx.smartprintng.client-0.2.0/zopyx/smartprintng/client/zip_client.py
@@ -0,0 +1,61 @@
+##########################################################################
+# zopyx.smartprintng.client - client library for the SmartPrintNG server
+# (C) 2009, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+import os
+import sys
+import base64
+import xmlrpclib
+import zipfile
+import tempfile
+import zipfile
+
+
+class Proxy(object):
+
+    def __init__(self, host='localhost', port=6543):
+        self.host = host
+        self.port = port
+
+    def availableConverters(self):
+        server = xmlrpclib.ServerProxy('http://%s:%d/availableConverters' % (self.host, self.port))
+        return server()
+
+    def convertZIP(self, dirname, converter_name='pdf-prince'):
+        """ XMLRPC client to SmartPrintNG server """
+        cwd = os.getcwd()
+        os.chdir(dirname)
+        server = xmlrpclib.ServerProxy('http://%s:%d/convertZIP' % (self.host, self.port))
+        zip_filename = tempfile.mktemp()
+        ZF = zipfile.ZipFile(zip_filename, 'w')
+        for fname in os.listdir('.'):
+            if not os.path.isfile(fname):
+                continue
+            fullname = os.path.join(dirname, fname)
+            ZF.write(fname)
+        ZF.close()
+
+        # send the ZIP archive base64 encoded
+        zip_data = server.convertZIP(base64.encodestring(file(zip_filename, 'rb').read()),
+                                     converter_name)
+
+        # and receive the result PDF as base64 encoded ZIP archive
+        zip_temp = tempfile.mktemp()
+        file(zip_temp, 'wb').write(base64.decodestring(zip_data))
+        ZF = zipfile.ZipFile(zip_temp, 'r')
+        names = ZF.namelist()
+        output_filename = os.path.abspath(names[0])
+        file(output_filename, 'wb').write(ZF.read(names[0]))
+        ZF.close()
+        os.unlink(zip_filename)
+        os.unlink(zip_temp)
+        os.chdir(cwd)
+        return output_filename
+
+if __name__ == '__main__':
+    # usage: convertZIP <dirname>
+    proxy = Proxy()
+    print proxy.availableConverters()
+    print proxy.convertZIP(sys.argv[1])
+
