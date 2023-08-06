be7ed98fc8ae94167bf92e0745ca8043137dd259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:42 +0000

    Add package atrealtheme.algol/1.0.0beta1/atrealtheme.algol-1.0.0beta1-py2.4.egg

---
+++ packages/atrealtheme.algol/atrealtheme.algol-1.0.0beta1-py2.4.egg/atrealtheme/algol/browser/interfaces.py
@@ -0,0 +1,9 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+
+class IAlgolTheme(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer.
+    """
+    """Marker interface that defines a ZSubject.
+       It will be used for the viewlets that we want to add to the
+       "My Theme" skin only.
+    """
