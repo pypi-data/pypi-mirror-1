aff3e311dbf37834a9afbbf1865d2215e43af948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:18 +0000

    Add package ZODB3/3.9.0a1/ZODB3-3.9.0a1-py2.5-win32.egg

---
+++ packages/ZODB3/ZODB3-3.9.0a1-py2.5-win32.egg/ZEO/__init__.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+"""ZEO -- Zope Enterprise Objects.
+
+See the file README.txt in this directory for an overview.
+
+ZEO is now part of ZODB; ZODB's home on the web is
+
+    http://www.zope.org/Wikis/ZODB
+
+"""
