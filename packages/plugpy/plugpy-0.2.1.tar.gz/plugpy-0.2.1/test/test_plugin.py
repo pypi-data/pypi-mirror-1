ee754938f738f8cf18d564beed992326a7acb333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:16 +0000

    Add package plugpy/0.2.1/plugpy-0.2.1.tar.gz

---
+++ packages/plugpy/plugpy-0.2.1.tar.gz/plugpy-0.2.1/test/test_plugin.py
@@ -0,0 +1,37 @@
+from plugpy import load_plugins, reload_plugins, dispatch
+
+config = dict()
+plugin_path='./test/plugin'
+debug=True
+
+
+def test_load():
+    res = load_plugins(config, plugin_path=plugin_path, debug=debug)
+    assert res != None
+    assert len(res) == 2
+
+def test_dispath():
+    res = reload_plugins(config, plugin_path=plugin_path, debug=debug)
+    res = dispatch('test', 1,2)
+    assert res != None
+    assert len(res) == 1
+    assert res[0][0] == 3
+
+def test_reload_dispath():
+    res = reload_plugins(config, plugin_path=plugin_path, debug=debug)
+    res = dispatch('test', 1,2)
+    assert res != None
+    assert len(res) == 1
+    assert res[0][0] == 3
+
+def test_dispath_noargs():
+    res = reload_plugins(config, plugin_path=plugin_path, debug=debug)
+    res = dispatch('test2')
+    assert res != None
+    assert len(res) == 1
+    assert res[0][0] == "test"
+
+#def test_import_modules():
+#    import_modules("../werkzeug-main/werkzeug/")
+#    assert True
+
