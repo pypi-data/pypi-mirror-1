8b9e6115cb6280a884a118f75c45512576bce5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:01 +0000

    Add package genshi_view/0.2/genshi_view-0.2.tar.gz

---
+++ packages/genshi_view/genshi_view-0.2.tar.gz/genshi_view-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = '0.2'
+
+setup(name='genshi_view',
+      version=version,
+      description="paste template for a view using webob + genshi",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Jeff Hammel',
+      author_email='k0scist@gmail.com',
+      url='http://k0s.org/hg/genshi_view',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[ 'PasteScript' ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [paste.paster_create_template]
+      genshi_view = genshi_view:GenshiViewTemplate
+      """,
+      )
+      
