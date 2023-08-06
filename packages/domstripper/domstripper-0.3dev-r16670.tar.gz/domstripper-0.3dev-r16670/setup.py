ded42686dda5b215862adabc6c8ae0bbfda98747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:44 +0000

    Add package domstripper/0.3dev-r16670/domstripper-0.3dev-r16670.tar.gz

---
+++ packages/domstripper/domstripper-0.3dev-r16670.tar.gz/domstripper-0.3dev-r16670/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.3'
+
+setup(name='domstripper',
+      version=version,
+      description="lxml.html based DOM manipulator",
+      long_description="""\
+Feed it a block of HTML and a list of CSS selectors and it will remove everything from the DOM tree except those elements you want to keep.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='html xml dom',
+      author='Peter Bengtsson',
+      author_email='peter@fry-it.com',
+      url='http://www.peterbe.com/plog/domstripper',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      test_suite='test_domstripper',
+      zip_safe=True,
+      install_requires=[
+          'lxml'
+          
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
