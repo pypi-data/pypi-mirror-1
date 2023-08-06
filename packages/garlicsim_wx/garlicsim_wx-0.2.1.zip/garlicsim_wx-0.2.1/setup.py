fe9d9e96ac570176db76c4a3b46b5b4576ebd2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:10 +0000

    Add package garlicsim_wx/0.2.1/garlicsim_wx-0.2.1.zip

---
+++ packages/garlicsim_wx/garlicsim_wx-0.2.1.zip/garlicsim_wx-0.2.1/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# Copyright 2009 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+Setuptools setup file for garlicsim_wx.
+'''
+
+import os
+import setuptools
+import distutils # Just for deleting the "build" directory.
+
+try:
+    distutils.dir_util.remove_tree('build', verbose=True)
+except Exception:
+    pass
+
+my_long_description = \
+'''\
+garlicsim_wx, a wxPython GUI for garlicsim.
+
+The final goal of this project is to become a fully-fledged application for
+working with simulations, friendly enough that it may be used by
+non-programmers.
+'''
+
+my_classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Topic :: Scientific/Engineering',
+]
+
+
+
+setuptools.setup(
+    name='garlicsim_wx',
+    version='0.2.1',
+    requires=['garlicsim (== 0.2.1)'],
+    description='Gui for garlicsim, a Pythonic framework for working with simulations',
+    author='Ram Rachum',
+    author_email='cool-rr@cool-rr.com',
+    url='http://garlicsim.org',
+    packages=setuptools.find_packages(),
+    license="Proprietary",
+    long_description = my_long_description,
+    classifiers = my_classifiers,
+    include_package_data = True,
+)
