25bba13c37ac2c252174abab4d06c8b25d2bdfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:19 +0000

    Add package django_satprep/0.1.1/django_satprep-0.1.1.tar.gz

---
+++ packages/django_satprep/django_satprep-0.1.1.tar.gz/django_satprep-0.1.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django_satprep',
+      version='0.1.1',
+      description='Basic testing conveniences for Django',
+      author='Michael Richardson',
+      author_email='michael@mtrichardson.com',
+      url='http://bitbucket.org/mtrichardson/django_satprep/',
+      packages=['django_satprep'],
+     )
+
