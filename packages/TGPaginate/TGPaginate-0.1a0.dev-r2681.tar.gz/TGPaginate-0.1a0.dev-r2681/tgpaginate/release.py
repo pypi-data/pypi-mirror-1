51442e446a9afe1d059c7627cd6e16c78fdb8b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:23 +0000

    Add package TGPaginate/0.1a0.dev-r2681/TGPaginate-0.1a0.dev-r2681.tar.gz

---
+++ packages/TGPaginate/TGPaginate-0.1a0.dev-r2681.tar.gz/TGPaginate-0.1a0.dev-r2681/tgpaginate/release.py
@@ -0,0 +1,22 @@
+# Release information about TGPaginate
+
+version = "0.1a0"
+
+description = "Pagination utilities for TurboGears"
+
+long_description = """Still a work in progress.
+This is a branch from the TG paginator that allows multiple paginations and
+support for Ajax.
+
+Right now it's focused on AjaxPaginatedGrid, it's a new widget that interacts 
+with the pagination decorator to provide fast access to the data and 
+interaction with other scripts. Check the widget browser for a demo.
+Uses Prototype.
+
+Tested and works with Opera, FireFox and IE6 (please report how it behaves with
+other browsers).
+"""
+
+author = "Claudio Martinez"
+email = "claudio.s.martinez@gmail.com"
+license = "MIT"
