38e38acaf78b1b312670044eb45bc9f7f000d9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:26 +0000

    Add package pgmigrate/1.1.2/pgmigrate-1.1.2.tar.gz

---
+++ packages/pgmigrate/pgmigrate-1.1.2.tar.gz/pgmigrate-1.1.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='pgmigrate',
+      version='1.1.2',
+      description='Database schema migration tool for Postgres',
+      author='Sergey Kirillov',
+      author_email='sergey.kirillov@gmail.com',
+      url='http://code.google.com/p/pgmigrate/',
+      packages=find_packages(),
+      scripts=['pgmigrate.py'],
+      install_requires=['lxml', 'psycopg2'],
+      license="Apache License 2.0",
+      keywords="database migration tool postgres",
+      zip_safe=True,
+
+    entry_points = {
+        'console_scripts': [
+            'pgmigrate = pgmigrate:main',
+        ],
+    }	
+)
+
