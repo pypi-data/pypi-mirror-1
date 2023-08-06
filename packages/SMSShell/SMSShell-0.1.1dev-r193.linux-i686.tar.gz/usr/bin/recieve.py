d980f8f3d579c75120cca90a75af8dbd394010e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:46 +0000

    Add package SMSShell/0.1.1dev-r193/SMSShell-0.1.1dev-r193.linux-i686.tar.gz

---
+++ packages/SMSShell/SMSShell-0.1.1dev-r193.linux-i686.tar.gz/usr/bin/recieve.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python -O
+# -*- coding: utf-8 -*-
+#receive.py
+
+"""receive.py - script that processes messages received thru kannel
+
+AUTHOR: Emanuel Gardaya Calso
+
+"""
+
+from smsshell.scripts import recieve
+
+recieve.main()
+
