be1f3caa07e84a87f7c126d45ad4247afffa1ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:59 +0000

    Add package GitCommit/0.9.3/GitCommit-0.9.3.tar.gz

---
+++ packages/GitCommit/GitCommit-0.9.3.tar.gz/GitCommit-0.9.3/gitcommitlib.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os, sys
+VERSION = (0,9,3)
+
+def get_version():
+    strversion = [str(x) for x in VERSION]
+    return '.'.join(strversion)
+
+class NotFoundError(RuntimeError):
+    pass
+
+class TestsFailedError(RuntimeError):
+    pass
+
+def find_managepy(): 
+    curpath = os.getcwd()
+    while True:
+        curdirlist = os.listdir(curpath)
+        if 'manage.py' in curdirlist:
+            return curpath + '/manage.py'
+        if '.git' in curdirlist:
+            raise NotFoundError
+        curpath, tail = os.path.split(curpath)
+        if tail == '':
+            raise NotFoundError
+        
+def run_tests():
+    try:
+        managepy = find_managepy()
+    except NotFoundError:
+        return
+    result = os.system(managepy + " test")
+    if result != 0:
+        raise TestsFailedError
+
+def check_for_todos():
+    result = os.system("git grep -ni '#[ \t]*todo'")
+    if result == 0:
+        print "There are TODO statements. Continue with commit? [y/N] ",
+        c = sys.stdin.read(1)
+        if c.lower() != 'y':
+            print "aborting . . . "
+            exit()
+
+    
+        
---
+++ packages/GitCommit/GitCommit-0.9.3.tar.gz/GitCommit-0.9.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import gitcommitlib
+
+setup(name='GitCommit',
+      version=gitcommitlib.get_version(),
+      author_email='gattster@gmail.com',
+      author='Philip Gatt',
+      py_modules=['gitcommitlib'],
+      scripts=['scripts/gitcommit'],
+      description='Does a few checks before running "git commit -av". Checks include running django unit tests and checking for TODO statements',
+      )
