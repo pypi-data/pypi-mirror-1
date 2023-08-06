e608c4b56dbeddcf60a9422f0d0d6ff5bda64ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:48 +0000

    Add package Plone/4.0b1/Plone-4.0b1.zip

---
+++ packages/Plone/Plone-4.0b1.zip/Plone-4.0b1/Products/CMFPlone/tests/testDateTimeIntegration.py
@@ -0,0 +1,74 @@
+# tests for issues related to changes in `DateTime` 2.12
+# please see tickets:
+#
+#   http://dev.plone.org/plone/ticket/10140
+#   http://dev.plone.org/plone/ticket/10141
+#   http://dev.plone.org/plone/ticket/10171
+#
+# for more information about this.  please also note that these tests
+# may produce false positives when run in the GMT time zone!
+
+from Products.CMFPlone.tests.PloneTestCase import PloneTestCase
+from Products.CMFPlone.tests.PloneTestCase import FunctionalTestCase
+from DateTime import DateTime
+
+
+class DateTimeTests(PloneTestCase):
+
+    def testModificationDate(self):
+        obj = self.folder
+        before = DateTime()
+        obj.processForm(values=dict(Description='foo!'))
+        after = DateTime()
+        modified = obj.ModificationDate()   # the string representation...
+        modified = DateTime(modified)       # is usually parsed again in Plone
+        self.failUnless(int(before) <= int(modified) <= int(after),
+            (before, modified, after))
+
+    def testCreationDate(self):
+        before = DateTime()
+        obj = self.folder[self.folder.invokeFactory('Document', 'foo')]
+        after = DateTime()
+        creation = obj.CreationDate()       # the string representation...
+        creation = DateTime(creation)       # is usually parsed again in Plone
+        self.failUnless(int(before) <= int(creation) <= int(after),
+            (before, creation, after))
+
+    def testEffectiveDate(self):
+        obj = self.folder
+        date = DateTime() + 365             # expire one year from today
+        date = DateTime(date.ISO8601())     # but strip off milliseconds
+        obj.setEffectiveDate(date)
+        obj.processForm(values=dict(Description='foo!'))
+        effective = obj.EffectiveDate()     # the string representation...
+        effective = DateTime(effective)     # is usually parsed again in Plone
+        self.failUnless(date.equalTo(effective), (date, effective))
+
+    def testExpirationDate(self):
+        obj = self.folder
+        date = DateTime() + 365             # expire one year from today
+        date = DateTime(date.ISO8601())     # but strip off milliseconds
+        obj.setExpirationDate(date)
+        obj.processForm(values=dict(Description='foo!'))
+        expired = obj.ExpirationDate()      # the string representation...
+        expired = DateTime(expired)         # is usually parsed again in Plone
+        self.failUnless(date.equalTo(expired), (date, expired))
+
+
+class DateTimeTests(FunctionalTestCase):
+
+    def testPublicationDateKeepsTimeZone(self):
+        # see http://dev.plone.org/plone/ticket/10141
+        self.setRoles(('Manager',))
+        obj = self.portal['front-page']
+        obj.setEffectiveDate('2020-02-20 16:00')
+        browser = self.getBrowser()
+        browser.open(obj.absolute_url())
+        browser.getLink('Edit').click()
+        browser.getControl('Save').click()
+        self.failUnless(obj.EffectiveDate().startswith('2020-02-20T16:00:00'))
+
+
+def test_suite():
+    from unittest import defaultTestLoader
+    return defaultTestLoader.loadTestsFromName(__name__)
