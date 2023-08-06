b2b75a09b12124cb9bbab362ba1ebac69eab04a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:45 +0000

    Add package repoze.tm/1.0a5/repoze.tm-1.0a5.tar.gz

---
+++ packages/repoze.tm/repoze.tm-1.0a5.tar.gz/repoze.tm-1.0a5/setup.py
@@ -0,0 +1,37 @@
+__version__ = '1.0a5'
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+setup(name='repoze.tm',
+      version=__version__,
+      description='Zope-like transaction manager via WSGI middleware',
+      long_description=README,
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        ],
+      keywords='web application server wsgi zope repoze',
+      author="Agendaless Consulting",
+      author_email="repoze-dev@lists.repoze.org",
+      url="http://www.repoze.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['repoze'],
+      zip_safe=False,
+      install_requires=['ZODB3'], # only transaction, really
+      test_suite = "repoze.tm.tests",
+      entry_points="""
+      [paste.filter_app_factory]
+      tm = repoze.tm:make_tm
+      """,
+      )
+
