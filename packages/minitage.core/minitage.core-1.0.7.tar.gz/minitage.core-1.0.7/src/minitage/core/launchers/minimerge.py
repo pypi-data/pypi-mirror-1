64e406f435cb4bd4453e9773ecfb286f01a178eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:29 +0000

    Add package minitage.core/1.0.7/minitage.core-1.0.7.tar.gz

---
+++ packages/minitage.core/minitage.core-1.0.7.tar.gz/minitage.core-1.0.7/src/minitage/core/launchers/minimerge.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+__docformat__ = 'restructuredtext en'
+
+import sys
+import os
+import re
+import shutil
+
+from minitage.core.cli import do_read_options
+from minitage.core import core
+from minitage.core.common import first_run
+
+def launch():
+    try:
+        prefix = os.path.abspath(sys.exec_prefix)
+        config = os.path.join(prefix, 'etc', 'minimerge.cfg')
+        if not os.path.isfile(config):
+            first_run()
+
+        options = do_read_options()
+        minimerge = core.Minimerge(options)
+        minimerge.main()
+    except Exception, e:
+        #raise
+        sys.stderr.write('Minimerge executation failed:\n')
+        sys.stderr.write('\t%s\n' % e)
+
+# vim:set ft=python sts=4 ts=4 tw=80 et:
