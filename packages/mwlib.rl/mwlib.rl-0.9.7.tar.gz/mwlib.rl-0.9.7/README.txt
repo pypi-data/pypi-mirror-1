bb98fd0e0800793b3ec110dde41598c737aa2b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:28 +0000

    Add package mwlib.rl/0.9.7/mwlib.rl-0.9.7.tar.gz

---
+++ packages/mwlib.rl/mwlib.rl-0.9.7.tar.gz/mwlib.rl-0.9.7/README.txt
@@ -0,0 +1,128 @@
+.. -*- mode: rst; coding: utf-8 -*-
+
+======================================================================
+mwlib.rl - reportlab pdf writer 
+======================================================================
+
+
+Overview
+======================================================================
+mwlib.rl provides a library for writing pdf documents from mediawiki_ 
+articles which were parsed by the mwlib library. 
+
+
+Installation
+======================================================================
+You need to have setuptools/easy_install installed. Installation
+should be as easy as typing::
+  
+  $ easy_install mwlib.rl
+
+If you don't have setuptools installed, download the source package, 
+unpack it and run::
+
+  $ python setup.py install
+
+(this will also install setuptools)
+
+*texvc*
+ You have to make sure that *texvc* is in your system PATH. *texvc* 
+ is supplied by the mediawiki installation. It should be located in
+ the following directory: mediawiki_install_path/math
+
+ texvc also requires:
+ * LaTeX
+ * dvipng
+ * AMS* packages for LaTeX (maybe included in LaTex distribution)
+
+You will also need:
+
+*mwlib*
+  mwlib parses mediawiki articles
+
+*pygments*
+  for source code formatting
+  http://pygments.org/ (debian packet:  python-pygments)
+
+*fribidi*
+  package for handling bidirectional (right-to-left / left-to-right) text. gnu freebidi and the python bindings are needed
+  http://fribidi.freedesktop.org/wiki/  (debian packages: libfribidi0 and libfribidi-dev)
+  http://pyfribidi.sourceforge.net/index.html (debian packages: python-pyfribidi)
+  
+
+
+PDF Customization
+======================================================================
+
+Customizing the resulting PDFs is possible by adding a custom configuration file. 
+The file needs to named customconfig.py and should reside next to the pdfstyles.py file. 
+Basically you can override anything in the pdfstyles.py file with your custom configuration. 
+Any changes need to be done with care in order not to break things!
+Check the pdfstyles.py file for more information.
+
+    
+Contact/Further Information
+======================================================================
+For further information please visit our trac instance running at
+http://code.pediapress.com
+The current development version can also be found there.
+
+ChangeLog
+======================================================================
+
+2009-02-18 release 0.9.7
+------------------------
+
+* add translations
+* improve styling
+
+2009-02-03 release 0.9.4
+------------------------
+
+* improve rendering of galleries
+* improve page breaks
+* use new image scaling method from mwlib
+
+
+2009-02-03 release 0.9.3
+------------------------
+
+* use correct alignment and background color for table cells
+* text alignment is now justified by default
+* workaround for greyscale images with alphachannel (#429)
+
+License
+======================================================================
+Copyright (c) 2007, 2008 PediaPress GmbH
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above
+  copyright notice, this list of conditions and the following
+  disclaimer in the documentation and/or other materials provided
+  with the distribution. 
+
+* Neither the name of PediaPress GmbH nor the names of its
+  contributors may be used to endorse or promote products derived
+  from this software without specific prior written permission. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+.. _mediawiki: http://www.mediawiki.org
---
+++ packages/mwlib.rl/mwlib.rl-0.9.7.tar.gz/mwlib.rl-0.9.7/mwlib/rl/rltables.py
@@ -0,0 +1,325 @@
+#! /usr/bin/env python
+#! -*- coding:utf-8 -*-
+
+# Copyright (c) 2007, PediaPress GmbH
+# See README.txt for additional licensing information.
+
+from __future__ import division
+
+import re
+
+from mwlib.utils import all
+from mwlib import log
+from mwlib.advtree import Text, ItemList, Table, Row, Cell
+from mwlib import styleutils
+
+from reportlab.lib import colors
+from customflowables import Figure
+#import debughelper
+from pdfstyles import printHeight, printWidth
+
+
+log = log.Log('rlwriter')
+
+
+def checkSpans(data, t):
+    """
+    use colspan and rowspan attributes to build rectangular table data array
+    """
+    styles = []
+    rowspans = []
+    maxCols = 0
+
+    for (i,row) in enumerate(data):
+        added_cells = 0
+        for (j,cell) in enumerate(row):
+            colspan = cell.get('colspan', 1)
+            rowspan  = cell.get('rowspan', 1)
+            if rowspan > (len(data) - i):  # check for broken rowspans
+                rowspan = len(data) - i       
+            if colspan > 1:
+                styles.append( ('SPAN',(j,i), (j+colspan-1,i)) ) 
+                for cols in range(colspan-1):
+                    data[i].insert(j + 1,{'content':'','inserted':'colspan'})
+            if rowspan > 1:
+                styles.append( ('SPAN',(j,i),(j + colspan-1,i+rowspan-1)) )
+                for row_offset in range(rowspan-1):
+                    for c in range(colspan):
+                        data[i+row_offset+1].insert(j,{'content':'', 'inserted':'rowspan'})
+                        t.children[i+row_offset+1].children.insert(j, Cell())
+        maxCols = max(maxCols, len(data[i]))
+
+    d = []
+    for row in data: # extract content from cells
+        newRow = [cell['content'] for cell in row] 
+        while len(newRow) < maxCols: # make sure table is rectangular
+            newRow.append('')
+        d.append(newRow)
+    return (d, styles)
+
+
+def style(attributes):
+    """
+    extract the style info and return a reportlab style list
+    try to guess if a border and/or frame
+    """
+    borderBoxes = [u'prettytable', u'metadata', u'wikitable', u'infobox', u'toccolours', u'navbox', u'float-right', 'taxobox']
+    styleList = []
+    hasBorder = False
+    hasGrid = False
+    style = attributes.get('style', {})
+
+    if attributes.get('border', 0) > 0 or style.get('border', 0) > 0:
+        hasGrid = True
+    bgColor = attributes.get('background-color') or style.get('background-color')
+    if bgColor and bgColor!= 'transparent':
+        hasBorder = True
+    classes = set([ c.strip() for c in attributes.get('class','').split()])
+    if set(borderBoxes).intersection(classes):
+        hasGrid = True
+    bs = attributes.get('border-spacing',None)
+    if bs:
+        bs_val = re.match('(?P<bs>\d)',bs)
+        if bs_val and int(bs_val.groups('bs')[0]) > 0:
+            hasGrid =True
+                
+    styleList.append( ('VALIGN',(0,0),(-1,-1),'TOP') )
+    if hasGrid:
+        styleList.extend([ ('INNERGRID',(0,0),(-1,-1),0.25,colors.black),
+                           ('BOX',(0,0),(-1,-1),0.25,colors.black),
+                           ])
+    elif hasBorder:
+        styleList.extend([ ('BOX',(0,0),(-1,-1),0.25,colors.black),
+                           ])        
+    return styleList
+
+def scaleImages(data):
+    for row in data:
+        for cell in row:
+            for (i,e) in enumerate(cell):
+                if isinstance(e,Figure): # scale image to half size
+                    cell[i] = Figure(imgFile = e.imgPath, captionTxt=e.captionTxt, captionStyle=e.cs, imgWidth=e.imgWidth/2.0,imgHeight=e.imgHeight/2.0, margin=e.margin, padding=e.padding,align=e.align)
+
+            
+def getColWidths(data, table=None, recursionDepth=0, nestingLevel=1):
+    """
+    the widths for the individual columns are calculated. if the horizontal size exceeds the pagewidth
+    the fontsize is reduced 
+    """
+
+    if nestingLevel > 1:
+        scaleImages(data)
+
+    if not data:
+        return None
+       
+    availWidth = printWidth - 12 # twice the total cell padding
+    minwidths  = [ 0 for x in range(len(data[0]))]
+    summedwidths = [ 0 for x in range(len(data[0]))]
+    maxbreaks = [ 0 for x in range(len(data[0]))]
+    for (i, row) in enumerate(data):        
+        for (j,cell) in enumerate(row):
+            cellwidth = 0
+            try:
+                colspan = getattr(table.children[i].children[j], 'colspan', 1)
+            except IndexError: # caused by empty row b/c of rowspanning
+                colspan = 1
+            for e in cell:
+                minw, minh = e.wrap(0,printHeight)
+                maxw, maxh = e.wrap(availWidth, printHeight)
+                minw += 6  # FIXME +6 is the cell padding we are using
+                cellwidth += minw
+                if maxh > 0:
+                    rows = minh / maxh - 0.5 # approx. #linebreaks - smooted out - 
+                else:
+                    rows = 0
+                if colspan > 1:                    
+                    for offset in range(colspan):
+                        minwidths[j+offset] = max(minw/colspan, minwidths[j+offset])
+                        maxbreaks[j+offset] = max(rows/colspan, maxbreaks[j+offset])
+                else:
+                    minwidths[j] = max(minw, minwidths[j])
+                    maxbreaks[j] = max(rows,maxbreaks[j])
+            summedwidths[j] = max(cellwidth, summedwidths[j])
+
+    parent_cells = table.getParentNodesByClass(Cell)
+    parent_tables = table.getParentNodesByClass(Table)
+    # nested tables in colspanned cell are expanded to full page width
+    if nestingLevel == 2 and parent_cells and parent_tables and parent_cells[0].colspan == parent_tables[0].numcols:
+        availWidth -= 8
+    elif nestingLevel > 1:
+        return minwidths
+
+    remainingSpace = availWidth - sum(summedwidths)
+    if remainingSpace < 0 : 
+        remainingSpace = availWidth - sum(minwidths)
+        if remainingSpace < 0:
+            if recursionDepth == 0:
+                scaleImages(data)
+                return getColWidths(data, table=table, recursionDepth=1, nestingLevel=nestingLevel)
+            else:
+                return None
+        else:
+            _widths = minwidths
+    else:
+        _widths = summedwidths
+        
+    totalbreaks = sum(maxbreaks)
+    if totalbreaks == 0:
+        return minwidths
+    else:
+        widths = [ _widths[col] + remainingSpace*(breaks/totalbreaks) for (col,breaks) in enumerate(maxbreaks) ]
+        return widths
+    
+
+def splitCellContent(data):
+    # FIXME: this is a hotfix for tables which contain extremly large cells which cant be handeled by reportlab
+    import math
+    n_data = []
+    splitCellCount = 14 # some arbitrary constant...: if more than 14 items are present in a cell, the cell is split into two cells in two rows
+    for row in data:
+        maxCellItems = 0
+        for cell in row:
+            maxCellItems = max(maxCellItems,len(cell))
+        if maxCellItems > splitCellCount:
+            for splitRun in range(int(math.ceil(maxCellItems / splitCellCount))):
+                n_row = []
+                for cell in row:
+                    if len(cell) > splitRun*splitCellCount:
+                        n_row.append(cell[splitRun*splitCellCount:(splitRun+1)*splitCellCount])
+                    else:
+                        n_row.append('')                   
+                n_data.append(n_row)                    
+        else:
+            n_data.append(row)
+    return n_data
+
+
+
+def getContentType(t):
+    nodeInfo = []
+    for row in t.children:
+        rowNodeInfo = []
+        for cell in row:
+            cellNodeTypes = []
+            cellTextLen = 0
+            for item in cell.children:
+                if not item.isblocknode: # any inline node is treated as a regular TextNode for simplicity
+                    cellNodeTypes.append(Text)
+                else:
+                    cellNodeTypes.append(item.__class__)            
+                cellTextLen += len(item.getAllDisplayText())
+            if cell.children:
+                rowNodeInfo.append( (cellNodeTypes, cellTextLen) )
+        if rowNodeInfo:
+            nodeInfo.append(rowNodeInfo)
+    return nodeInfo
+
+def reformatTable(t, maxCols):
+    nodeInfo = getContentType(t)
+    numCols = maxCols
+    numRows = len(t.rows)
+
+    onlyTables = len(t.children) > 0 #if table is empty onlyTables and onlyLists are False
+    onlyLists = len(t.children) > 0
+    if not nodeInfo:
+        onlyTables = False
+        onlyLists = False
+    for row in nodeInfo:
+        for cell in row:
+            cellNodeTypes, cellTextLen = cell
+            if not all(nodetype==Table for nodetype in cellNodeTypes):
+                onlyTables = False
+            if not all(nodetype==ItemList for nodetype in cellNodeTypes):
+                onlyLists = False
+            
+    if onlyTables and numCols > 1:
+        log.info('got table only table - removing container')
+        t = removeContainerTable(t)
+    if onlyLists and numCols > 2 :
+        log.info('got list only table - reducing columns to 2')
+        t = reduceCols(t, colnum=2)
+    if onlyLists:
+        log.info('got list only table - splitting list items')
+        t = splitListItems(t)
+        pass
+    return t
+
+def splitListItems(t):
+    nt = t.copy()
+    for r in t.children:
+        nr = Row()
+        cols = []
+        maxItems = 0
+        for cell in r:           
+            items = []
+            for c in cell.children:
+                if c.__class__ == ItemList:
+                    items.extend(c.children)                   
+            cols.append(items)
+            maxItems = max(maxItems,len(items))
+        for i in range(maxItems):            
+            for (j,col) in enumerate(cols):
+                try:
+                    item = cols[j][i]
+                    il = ItemList()
+                    il.appendChild(item)
+                    nc = Cell()                    
+                    nc.appendChild(il)
+                    nr.appendChild(nc)
+                except IndexError:                    
+                    nr.appendChild(Cell())
+            nt.appendChild(nr)
+            nr = Row()        
+    return nt
+    
+def reduceCols(t, colnum=2):
+    nt = t.copy()
+    nt.children = []
+    for r in t.children:
+        nr = Row()
+        for c in r:
+            nc = c.copy()
+            if len(nr.children) == colnum:
+                nt.appendChild(nr)
+                nr=Row()
+            nr.appendChild(nc)
+        if len(nr.children)>0:
+            while len(nr.children) < colnum:
+                nr.appendChild(Cell())
+            nt.appendChild(nr)
+    return nt
+
+def removeContainerTable(containertable):
+    newtables = []
+    for row in containertable:
+        for cell in row:
+            for item in cell:
+                if item.__class__ == Table:
+                    newtables.append(item)
+                else:
+                    log.info("unmatched node:", item.__class__)
+    return newtables
+
+
+def tableBgStyle(table):
+    bg_style = []    
+    for (i, row) in enumerate(table.children):
+        if not row.__class__ == Row:
+            continue
+        rgb = styleutils.rgbBgColorFromNode(row)
+        if rgb:
+            bg_style.append(('BACKGROUND', (0,i), (-1,i), colors.Color(rgb[0], rgb[1], rgb[2])))
+        colspan_sum = 0
+        for (j, cell) in enumerate(row.children):
+            if not cell.__class__ == Cell:
+                continue
+            rgb = styleutils.rgbBgColorFromNode(cell)
+            colspan = cell.colspan
+            start_col = colspan_sum
+            end_col = colspan_sum + colspan -1
+            colspan_sum += colspan
+            rowspan = cell.rowspan
+            if rgb:
+                bg_style.append(('BACKGROUND', (start_col,i), (end_col,i+rowspan-1), colors.Color(rgb[0], rgb[1], rgb[2])))
+    return bg_style
