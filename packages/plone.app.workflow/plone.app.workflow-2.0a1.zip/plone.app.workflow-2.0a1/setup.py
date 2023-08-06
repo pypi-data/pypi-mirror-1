35400f38d680dec6068e445abcc9975e011e4939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:36 +0000

    Add package plone.app.workflow/2.0a1/plone.app.workflow-2.0a1.zip

---
+++ packages/plone.app.workflow/plone.app.workflow-2.0a1.zip/plone.app.workflow-2.0a1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+version = '2.0a1'
+
+setup(name='plone.app.workflow',
+      version=version,
+      description="workflow and security settings for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Martin Aspeli',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.workflow',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'Products.CMFCalendar',
+            'Products.PloneTestCase',
+        ]
+      ),
+      install_requires=[
+        'setuptools',
+        'kss.core',
+        'plone.memoize',
+        'plone.app.kss',
+        'transaction',
+        'zope.component',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.schema',
+        'Plone',
+        'Products.CMFCore',
+        'Products.DCWorkflow',
+        'Products.statusmessages',
+        'Acquisition',
+        'DateTime',
+        'Zope2',
+      ],
+      )
