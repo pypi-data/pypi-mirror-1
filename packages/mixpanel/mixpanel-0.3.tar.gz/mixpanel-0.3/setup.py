99cd2c6d16be9dee900c3b2a0ed58b6c411c5dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:44 +0000

    Add package mixpanel/0.3/mixpanel-0.3.tar.gz

---
+++ packages/mixpanel/mixpanel-0.3.tar.gz/mixpanel-0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+
+setup (
+    name = 'mixpanel',
+    version = '0.3',
+    description = 'Small Python library to make calls to mixpanel\'s API. Requires beanstalkd server.',
+    long_description = """
+    This library includes a client and a server. The client is used to queue
+    requests which will be made to mixpanel's API. The server daemon pulls the
+    queued items out and sends the requests. Both require a running beanstalkd
+    server. You can get beanstalkd from http://xph.us/software/beanstalkd/.
+    """,
+    author = 'Tom Davis',
+    author_email = 'binjured@gmail.com',
+    url = 'http://github.com/tdavis/mixpanel/tree/master',
+    download_url = 'http://cloud.github.com/downloads/tdavis/mixpanel/mixpanel-0.3.tar.gz',
+    classifiers = [ 'Development Status :: 4 - Beta',
+                    'License :: OSI Approved :: BSD License',
+                    'Programming Language :: Python',
+                    'Intended Audience :: Developers',
+                    'Topic :: Software Development :: Libraries :: Python Modules',
+                  ],
+    platforms = ['Any',],
+    keywords = ['mixpanel',],
+    packages = ['mixpanel',],
+    package_dir = {'mixpanel': '.'},
+    scripts = ['scripts/mixpaneld',],
+    # beanstalkc: beanstalkd client library
+    # daemon: script used to daemonize mixpaneld script
+    install_requires=['beanstalkc', 'daemon>=1.0.1']
+)
+
