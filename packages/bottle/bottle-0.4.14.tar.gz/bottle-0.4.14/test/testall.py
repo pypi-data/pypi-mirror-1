8b45fb35a906eeb224343cc4b45d3e36e2750f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:27 +0000

    Add package bottle/0.4.14/bottle-0.4.14.tar.gz

---
+++ packages/bottle/bottle-0.4.14.tar.gz/bottle-0.4.14/test/testall.py
@@ -0,0 +1,19 @@
+import unittest
+import sys, os.path
+TESTDIR = os.path.dirname(os.path.abspath(__file__))
+DISTDIR = os.path.dirname(TESTDIR)
+sys.path.insert(0, TESTDIR)
+sys.path.insert(0, DISTDIR)
+
+from test_templates import suite as suite1
+from test_routes import suite as suite2
+from test_environ import suite as suite3
+
+suite = unittest.TestSuite()
+suite.addTest(suite1)
+suite.addTest(suite2)
+suite.addTest(suite3)
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
