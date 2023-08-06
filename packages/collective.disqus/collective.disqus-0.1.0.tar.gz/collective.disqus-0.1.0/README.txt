bc324f21aac99a2f63dda3a33b4bfaf48e006397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:07 +0000

    Add package collective.disqus/0.1.0/collective.disqus-0.1.0.tar.gz

---
+++ packages/collective.disqus/collective.disqus-0.1.0.tar.gz/collective.disqus-0.1.0/README.txt
@@ -0,0 +1,40 @@
+Introduction
+============
+
+Integrates `DISQUS`_ comment system with `Plone`_.
+
+
+Installation
+============
+
+Add to your buildout::
+
+  eggs =
+    collective.disqus
+  ...
+  zcml =
+    collective.disqus
+    
+Use *Add products* in *Site Setup* or *QuickInstaller*.
+
+
+Configuration
+=============
+
+Go to *Site Setup* -> *DISQUS comment system* control panel form and configure *Website short name*.
+
+
+Contributors
+============
+
+Project initiated by `Wojciech Lichota`_.
+
+Contributors:
+
+* Wojciech Lichota [sargo]
+
+
+.. _DISQUS: http://disqus.com
+.. _Plone: http://plone.org
+.. _Wojciech Lichota: http://lichota.pl
+
---
+++ packages/collective.disqus/collective.disqus-0.1.0.tar.gz/collective.disqus-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name = 'collective.disqus',
+      version = version,
+      description = "Integrates DISQUS comment system with Plone.",
+      long_description = open("README.txt").read() + "\n" +
+                         open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers = [
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords = 'plone disqus discusion blog comment',
+      author = 'Wojciech Lichota',
+      author_email = 'wojciech@lichota.pl',
+      url = 'http://lichota.pl/blog/topics/disqus',
+      license = 'Zope Public License, Version 2.1 (ZPL)',
+      packages = find_packages('src'),
+      include_package_data = True,
+      package_dir = {'':'src'},
+      namespace_packages = ['collective'],
+      zip_safe = False,
+      install_requires = [
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points = """
+      # -*- Entry points: -*-
+      """,
+      )
