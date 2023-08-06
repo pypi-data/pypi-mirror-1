d57972b7d8594ae828113af52925801c00511e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:02 +0000

    Add package collective.anonymousbrowser/0.3dev-r73429/collective.anonymousbrowser-0.3dev-r73429.zip

---
+++ packages/collective.anonymousbrowser/collective.anonymousbrowser-0.3dev-r73429.zip/collective.anonymousbrowser-0.3dev-r73429/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='collective.anonymousbrowser',
+    version="0.3",
+    description='A zope.testbrowser extension with useragent faking and proxy abilities',
+    license='GPL',
+    url="https://svn.plone.org/svn/collective/collective.anonymousbrowser/trunk",
+    long_description='\n'.join(
+        open(os.path.join(*path)).read() for path in [
+            ("README.txt",),
+            ("collective", "anonymousbrowser", "tests", "browser.txt",),
+            ("docs", "HISTORY.txt")]),
+    author='Mathieu Pasquet',
+    author_email='kiorky@cryptelium.net',
+    #url='',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    extras_require={'test': ['IPython', 'zope.testing', 'mocker']},
+    install_requires=[
+        'lxml',
+        'mechanize',
+        'setuptools',
+        'zope.testbrowser',
+    ],
+)
