205141394d5c010ec1b945b851301eccc243516f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:15 +0000

    Add package softwarefabrica.django.forms/0.9dev-BZR-r37-panta-elasticworld.org-20091110103112-cilkf24f2o503tkm/softwarefabrica.django.forms-0.9dev_BZR_r37_panta_elasticworld.org_20091110103112_cilkf24f2o503tkm-py2.5.egg

---
+++ packages/softwarefabrica.django.forms/softwarefabrica.django.forms-0.9dev_BZR_r37_panta_elasticworld.org_20091110103112_cilkf24f2o503tkm-py2.5.egg/softwarefabrica/django/forms/bzrcachedrev.py
@@ -0,0 +1,4 @@
+info    = {'revinfo': (37, 'panta@elasticworld.org-20091110103112-cilkf24f2o503tkm'), 'basepath': '/arc/bzr/softwarefabrica-forms.bzr/trunk/', 'revno': 37, 'revname': 'panta@elasticworld.org-20091110103112-cilkf24f2o503tkm', 'inpath': u'softwarefabrica/django/forms/version.py'}
+revno   = 37
+revname = 'panta@elasticworld.org-20091110103112-cilkf24f2o503tkm'
+revinfo = (37, 'panta@elasticworld.org-20091110103112-cilkf24f2o503tkm')
