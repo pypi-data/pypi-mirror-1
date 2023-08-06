5e919e09ebeef931c19d7c3e43808e291855e16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:38 +0000

    Add package Morelia/0.0.6/Morelia-0.0.6.tar.gz

---
+++ packages/Morelia/Morelia-0.0.6.tar.gz/Morelia-0.0.6/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import morelia
+
+setup(name         = 'Morelia',
+      version      = morelia.__version__,
+      description  = 'for "Behavior Driven Development" -- ' +
+                     'a client-facing scripting language to ' +
+                     'put the squeeze on all your features',
+      author       = 'Phlip',
+      author_email = 'phlip2005@gmail.com',
+      url          = 'http://c2.com/cgi/wiki?MoreliaViridis',
+      py_modules   = ['morelia'],
+      keywords     = "test bdd behavior",
+      classifiers  = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+      ]
+    )
