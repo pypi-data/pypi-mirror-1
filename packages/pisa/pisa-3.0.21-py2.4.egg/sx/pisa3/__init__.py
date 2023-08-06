9e8b99dd6fae0f2884608fbd51ffd5f7673ce916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:14 +0000

    Add package pisa/3.0.21/pisa-3.0.21-py2.4.egg

---
+++ packages/pisa/pisa-3.0.21-py2.4.egg/sx/pisa3/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: ISO-8859-1 -*-
+#############################################
+## (C)opyright by Dirk Holtwick, 2002-2007 ##
+## All rights reserved                     ##
+#############################################
+
+__reversion__ = "$Revision: 225 $"
+__author__    = "$Author: holtwick $"
+__date__      = "$Date: 2008-06-02 13:28:53 +0200 (Mo, 02 Jun 2008) $"
+
+REQUIRED_INFO = """
+****************************************************
+IMPORT ERROR!
+%s
+****************************************************
+
+The following Python packages are required for PISA:
+- ReportlabToolkit>=2.1 <http://www.reportlab.org/>
+- HTML5lib <http://code.google.com/p/html5lib/>
+
+Optional packages:
+- pyPDF <http://pybrary.net/pyPdf/>
+- PIL <http://www.pythonware.com/products/pil/>
+
+""".lstrip()
+
+try:
+    from pisa import *
+except ImportError, e:
+    import sys
+    sys.stderr.write(REQUIRED_INFO % e)
+    raise
+
+__version__   = VERSION
