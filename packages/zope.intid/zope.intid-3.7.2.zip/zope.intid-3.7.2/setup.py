40c4b961016ae45ea3ce167f2d4dee118bb317d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:59 +0000

    Add package zope.intid/3.7.2/zope.intid-3.7.2.zip

---
+++ packages/zope.intid/zope.intid-3.7.2.zip/zope.intid-3.7.2/setup.py
@@ -0,0 +1,71 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.intid package
+
+$Id: setup.py 107162 2009-12-27 13:28:36Z hannosch $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name = 'zope.intid',
+      version = '3.7.2',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Integer Id Utility',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 integer id utility",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.intid',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(test=['zope.testing',
+                                  'zope.site',
+                                  'zope.traversing',
+                                  'zope.container',]),
+      install_requires = ['setuptools',
+                          'ZODB3',
+                          'zope.lifecycleevent>=3.5.2',
+                          'zope.component',
+                          'zope.event',
+                          'zope.interface',
+                          'zope.keyreference',
+                          'zope.location>=3.5.4',
+                          'zope.security',
+                          ],
+      include_package_data = True,
+      zip_safe = False,
+      )
