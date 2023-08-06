a00db222a581cd86ff1204bdd078cee55e41a6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:03 +0000

    Add package bug.gd/0.1.9/bug.gd-0.1.9.zip

---
+++ packages/bug.gd/bug.gd-0.1.9.zip/bug.gd-0.1.9/buggdXMLRPC.py
@@ -0,0 +1,41 @@
+#
+# File    : buggdXMLRPC.py
+# Author  : ted shroyer
+# Company : Miracle Labs
+# Project : bug.gd
+# Date    : March 5, 2008
+#
+# Description:  Provide a simple interface to bug.gd that wraps the bug.gd xml-
+#               rpc api. 
+#
+
+import xmlrpclib
+
+# reserved API key for this Python API. 
+apikey = 99990
+
+class buggdXMLRPC:
+    "The buggd xml-rpc wrapper class.  The available buggd xml-rpc commands are encampsulated here."
+
+    def __init__(self):
+        self.url = 'http://soon.bug.gd/api'
+        self.server = xmlrpclib.Server(self.url)
+
+    def getUserID(self, email, reserved = apikey):
+        return self.server.getUserID(reserved, email)
+
+    def getErrorInfo(self, error_id, reserved = apikey):
+        return self.server.getErrorInfo(reserved, error_id)
+
+    def getErrorComments(self, error_id, reserved = apikey):
+        return self.server.getErrorComments(reserved, error_id)
+
+    def doSearch(self, search_id, reserved = apikey):
+        return self.server.doSearch(reserved, search_id)
+
+    def submitError(self, email, error_text, reserved = apikey):
+        return self.server.submitError(reserved, email, error_text)
+
+    def changeURL(self, url):
+        self.url = url
+        self.server = smlrpclib.Server(self.url)
---
+++ packages/bug.gd/bug.gd-0.1.9.zip/bug.gd-0.1.9/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='bug.gd',
+      version='0.1.9',
+      description='Get help for Python errors in your interactive console',
+      author='ted from bug.gd',
+      author_email='bugs@bug.gd',
+      py_modules=['buggd', 'buggdXMLRPC', 'error_help', 'buggdUtil'],
+      url='http://bug.gd/download/',
+      scripts=['error_help_config.py'],
+     )
+
+# Tell the user he/she need to run config
+print """\n***** bug.gd error network\n\nHello! You're installing the bug.gd error network package. This tool is
+designed to find workarounds to common errors.\n"""
+
+print """\n\n******** Setup is ALMOST complete!
+******** Setup is ALMOST complete!
+
+To complete installation you need to run the script error_help_config.py from your python scripts directory."""
+
