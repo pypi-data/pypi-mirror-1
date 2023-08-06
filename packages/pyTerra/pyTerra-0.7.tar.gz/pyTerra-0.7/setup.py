da71568040e0c2dbe0ac396bd7ed47f8cdb997c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:45 +0000

    Add package pyTerra/0.7/pyTerra-0.7.tar.gz

---
+++ packages/pyTerra/pyTerra-0.7.tar.gz/pyTerra-0.7/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup, Extension
+    
+
+name="pyTerra"
+version="0.7"
+description="Terraserver Module for Python"
+author="Howard Butler"
+author_email="hobu.inc@gmail.com"
+url="http://bitbucket.org/hobu/pyterra/"
+
+
+classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        
+]
+
+
+setup(name=name,
+      version=version,
+      description=description,
+      author=author,
+      author_email=author_email,
+      url=url,
+      classifiers = classifiers,
+      packages = ['pyTerra.SOAPpy', 'pyTerra.SOAPpy.wstools', 'pyTerra.TerraImage','pyTerra.pyTerra']
+      )
