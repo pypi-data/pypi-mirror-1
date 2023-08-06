5f74831cf02f74a9f67340e887cbd910ca00edec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:29 +0000

    Add package tiddlyweb/0.9.34/tiddlyweb-0.9.34.tar.gz

---
+++ packages/tiddlyweb/tiddlyweb-0.9.34.tar.gz/tiddlyweb-0.9.34/setup.py
@@ -0,0 +1,30 @@
+"""
+Try to setup tiddlyweb.
+Seem to be missing some details on how to make
+this the best it can be.
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from tiddlyweb import __version__ as VERSION
+
+setup(name = 'tiddlyweb',
+        version = VERSION,
+        description = 'An optionally headless, extensible RESTful datastore for TiddlyWiki',
+        author = 'Chris Dent',
+        author_email = 'cdent@peermore.com',
+        url = 'http://tiddlyweb.peermore.com/',
+        download_url = 'http://tiddlyweb.peermore.com/dist/',
+        packages = ['tiddlyweb', 'tiddlyweb.filters', 'tiddlyweb.model', 'tiddlyweb.serializations', 'tiddlyweb.stores', 'tiddlyweb.web', 'tiddlyweb.web.challengers', 'tiddlyweb.web.extractors', 'tiddlyweb.web.handler', 'cherrypy', 'cherrypy.wsgiserver'],
+        scripts = ['twanager',],
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['selector', 'simplejson', 'BeautifulSoup', 'wikklytext', 'html5lib'],
+        include_package_data = True,
+        )
+
+
+
+
