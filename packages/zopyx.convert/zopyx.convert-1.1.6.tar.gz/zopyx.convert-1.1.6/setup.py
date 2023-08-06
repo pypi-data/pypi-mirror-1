6f2df9d58cf007266135aeaff2fc4da9a2c8ff49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:01 +0000

    Add package zopyx.convert/1.1.6/zopyx.convert-1.1.6.tar.gz

---
+++ packages/zopyx.convert/zopyx.convert-1.1.6.tar.gz/zopyx.convert-1.1.6/setup.py
@@ -0,0 +1,45 @@
+##########################################################################
+# zopyx.convert - XSL-FO related functionalities
+#
+# (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+import os
+from setuptools import setup, find_packages
+
+
+CLASSIFIERS = [
+    'Programming Language :: Python',
+]
+
+version_file = os.path.join('src', 'zopyx', 'convert', 'version.txt')
+version = open(version_file).read().strip()
+
+readme_file = os.path.join('src', 'zopyx', 'convert', 'README.txt')
+desc = unicode(open(readme_file).read().strip(), 'utf-8')
+
+changes_file = os.path.join('src', 'zopyx', 'convert', 'CHANGES.txt')
+changes = open(changes_file).read().strip()
+
+long_description = desc + '\n\nChanges:\n========\n\n' + changes
+
+
+setup(name='zopyx.convert',
+      version=version,
+      license='ZPL (see LICENSE.txt)',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      maintainer='Andreas Jung',
+      maintainer_email='info@zopyx.com',
+      classifiers=CLASSIFIERS,
+      url='http://cheeseshop.python.org/pypi/zopyx.convert',
+      description='A Python interface to XSL-FO libraries (Conversion HTML to PDF, RTF, DOCX, WML and ODT)',
+      long_description=long_description,
+      packages=['zopyx', 'zopyx.convert'],
+      package_dir = {'': 'src'},
+      include_package_data = True,
+      zip_safe=False,
+      install_requires=['setuptools', 'elementtree', 'BeautifulSoup'],
+      namespace_packages=['zopyx'],
+      entry_points={'console_scripts': ['xslfo-convert = zopyx.convert.cli:main',]},
+      )                       
---
+++ packages/zopyx.convert/zopyx.convert-1.1.6.tar.gz/zopyx.convert-1.1.6/src/zopyx/convert/logger.py
@@ -0,0 +1,14 @@
+##########################################################################
+# zopyx.convert - XSL-FO related functionalities
+#
+# (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+
+import logging
+import sys
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+LOG = logging.getLogger('root')
