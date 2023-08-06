c59d414e410697c35291667f691f4fec4bb1aeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:41 +0000

    Add package compset/0.1/compset-0.1.tar.gz

---
+++ packages/compset/compset-0.1.tar.gz/compset-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='compset',
+      description='A Python set class that allows complements.',
+      version='0.1',
+      packages=['compset'],
+      author='Terry Jones',
+      author_email='terry@jon.es',
+      url='http://blogs.fluidinfo.com/terry',
+      )
