b5020156e899d7a50ce22f031c5b3b9840cf1445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:30 +0000

    Add package ice.adverlet/0.2.3/ice.adverlet-0.2.3.tar.gz

---
+++ packages/ice.adverlet/ice.adverlet-0.2.3.tar.gz/ice.adverlet-0.2.3/src/ice/adverlet/browser/interfaces.py
@@ -0,0 +1,15 @@
+### -*- coding: utf-8 -*- ####################################################
+#
+# Author: Ilshad Habibullin, 2008 <astoon.net at gmail.com>
+#
+##############################################################################
+
+__license__ = "GPL v.3"
+
+from zope.interface import Interface
+
+class IManageUITemplate(Interface):
+    """ Customizable template """
+
+class IUndoTemplate(Interface):
+    """ Customizable template """
