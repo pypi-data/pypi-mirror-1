2c83e22da28a7117c30a15b1e690cbf008622c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:56 +0000

    Add package zope.documenttemplate/3.4.1/zope.documenttemplate-3.4.1.tar.gz

---
+++ packages/zope.documenttemplate/zope.documenttemplate-3.4.1.tar.gz/zope.documenttemplate-3.4.1/setup.py
@@ -0,0 +1,62 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.documenttemplate package
+
+$Id: setup.py 91995 2008-10-10 16:35:52Z sidnei $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.documenttemplate',
+      version = '3.4.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Document Templating Markup Language (DTML)',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 template dtml",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.documenttemplate',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.security',
+                'zope.testing',
+                ],
+          untrusted=['zope.security[untrustedpython]'
+                ],
+          ),
+      install_requires = [
+          'setuptools',
+          'zope.structuredtext'],
+      include_package_data = True,
+      zip_safe = False,
+      )
