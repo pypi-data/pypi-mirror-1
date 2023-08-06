60d676d871f31f136143495f28430db085041c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:01 +0000

    Add package Products.listen/0.6.0/Products.listen-0.6.0.tar.gz

---
+++ packages/Products.listen/Products.listen-0.6.0.tar.gz/Products.listen-0.6.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.6.0'
+
+f = open('README.txt')
+readme = "".join(f.readlines())
+f.close()
+
+setup(name='Products.listen',
+      version=version,
+      description="listen is a mailing list product for Plone",
+      long_description=readme,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Robert Marianski',
+      author_email='rmarianski at openplans.org',
+      url='http://oss.openplans.org/listen',
+      license='GPL',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
