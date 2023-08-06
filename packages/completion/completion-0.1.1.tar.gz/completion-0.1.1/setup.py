0f26340ecd578a1166a19b42ac48fc25eb428b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:58 +0000

    Add package completion/0.1.1/completion-0.1.1.tar.gz

---
+++ packages/completion/completion-0.1.1.tar.gz/completion-0.1.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from distutils.core import setup
+setup(name='completion',
+            version='0.1.1',
+            description='Python Autocompletion Framework - Still Alpha',
+            author='Chris Dew',
+            author_email='cmsdew+completion@gmail.com',
+            url='http://finalcog.com/node/14',
+            py_modules=['completion'],
+            )
+
