be5f8c38c6b4e7c6fc30a758b6274754a9280e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:36 +0000

    Add package tokyocabinet/0.5/tokyocabinet-0.5.tar.gz

---
+++ packages/tokyocabinet/tokyocabinet-0.5.tar.gz/tokyocabinet-0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, Extension
+
+
+setup(
+    name = "tokyocabinet",
+    version = "0.5",
+    packages = ['tokyocabinet'],
+    ext_modules = [
+        Extension(
+            "tokyocabinet.btree", ['tokyocabinet/btree.c'],
+            libraries=["tokyocabinet"]
+        ),
+        Extension(
+            "tokyocabinet.hash", ['tokyocabinet/hash.c'],
+            libraries=["tokyocabinet"]
+        ),
+        Extension(
+            "tokyocabinet.table", ['tokyocabinet/table.c'],
+            libraries=["tokyocabinet"]
+        )
+    ],
+    description = """tokyocabinet aims to be a complete python wrapper for the 
+        Tokyo Cabinet database library by Mikio Hirabayashi (http://1978th.net/).
+        
+        So far the btree, hash and table APIs have been fully wrapped.""",
+    author = "Elisha Cook",
+    author_email = "ecook@justastudio.com",
+    url = "http://code.google.com/p/python-tokyocabinet/"
+)
