4858869aebc694e0f9713cf3f1acac1051734ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:17 +0000

    Add package collective.easyslider/0.3rc1/collective.easyslider-0.3rc1.tar.gz

---
+++ packages/collective.easyslider/collective.easyslider-0.3rc1.tar.gz/collective.easyslider-0.3rc1/collective/easyslider/Extensions/Install.py
@@ -0,0 +1,58 @@
+from Products.CMFCore.utils import getToolByName
+from collective.easyslider.interfaces import ISliderPage
+from zope.interface import noLongerProvides
+try:
+    #For Zope 2.10.4
+    from zope.annotation.interfaces import IAnnotations
+except ImportError:
+    #For Zope 2.9
+    from zope.app.annotation.interfaces import IAnnotations
+
+def install(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-collective.easyslider:default')
+
+def remove_annotations(items_to_check):
+    for item in items_to_check:
+        item = item.getObject()
+        noLongerProvides(item, ISliderPage)
+        item.reindexObject(idxs=['object_provides'])
+    
+        annotations = IAnnotations(item)
+        metadata = annotations.get('collective.easyslider', None)
+        if metadata is not None:
+            del annotations['collective.easyslider']
+
+def remove_layout(portal, items):
+
+    for item in items:
+        utils = portal.plone_utils
+        layout = utils.browserDefault(item.getObject())
+            
+        if layout[1][0] == "sliderview":
+            layout[0].setLayout(layout[0].getDefaultLayout())
+
+def uninstall(portal, reinstall=False):
+    
+    if not reinstall:
+    
+        #remove interfaces
+        catalog = portal.portal_catalog
+        remove_annotations(catalog.searchResults(object_provides=ISliderPage.__identifier__))
+        items = catalog.searchResults(portal_type=('Folder', 'Topic', 'Large Plone Folder'))
+        remove_annotations(items)
+        remove_layout(portal, items)
+        
+    portal_actions = getToolByName(portal, 'portal_actions')
+    object_buttons = portal_actions.object_buttons
+    object_tabs = portal_actions.object
+
+    actions_to_remove = ('enable_slider', 'disable_slider', 'slider_settings')
+    for action in actions_to_remove:
+        if action in object_buttons.objectIds():
+            object_buttons.manage_delObjects([action])
+        if action in object_tabs.objectIds():
+            object_tabs.manage_delObjects([action])
+    
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-collective.easyslider:uninstall')
---
+++ packages/collective.easyslider/collective.easyslider-0.3rc1.tar.gz/collective.easyslider-0.3rc1/collective/easyslider/browser/slider.py
@@ -0,0 +1,154 @@
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
+from Products.Five.browser import BrowserView
+from zope.formlib import form
+from plone.app.form import base as ploneformbase
+from collective.easyslider.interfaces import *
+from plone.app.form.widgets.wysiwygwidget import WYSIWYGWidget
+from collective.easyslider import easyslider_message_factory as _
+from zope.interface import implements
+from zope.component import adapts
+from collective.easyslider.widgets import SlidesWidget
+from collective.easyslider.settings import PageSliderSettings, ViewSliderSettings
+from collective.easyslider.utils import slider_settings_css
+
+class SliderPageSettingsForm(ploneformbase.EditForm):
+    """
+    The page that holds all the slider settings
+    """
+    form_fields = form.FormFields(IPageSliderSettings)
+    #our revised SlidesWidget that only displays slides really
+    form_fields['slides'].custom_widget = SlidesWidget 
+
+    label = _(u'heading_slider_settings_form', default=u"Slider Settings")
+    description = _(u'description_slider_settings_form', default=u"Configure the parameters for this slider.")
+    form_name = _(u'title_slider_settings_form', default=u"Slider settings")
+    
+    
+class AddSlideAdapter(SchemaAdapterBase):
+    """
+    Since AddSlideForm is for an ISlideContext,
+    we create this adapter so it is called to
+    populate the slides
+    """
+    adapts(ISlideContext)
+    implements(ISlide)
+    
+    def __init__(self, context):
+        super(AddSlideAdapter, self).__init__(context)
+        
+        self.settings = PageSliderSettings(context.context)
+        
+        if context.index > (len(self.settings.slides) -1) or context.index < 0:
+            #a valid index was not specified, just create a new one...
+            context.index = -1
+    
+    def get_slide(self):
+        if self.context.index == -1: # creating new
+            return ""
+        else:
+            return self.settings.slides[self.context.index]
+        
+    def set_slide(self, value):
+        slides = self.settings.slides
+        
+        if self.context.index == -1:
+            slides.append(value)
+            self.context.index = len(slides) - 1
+        else:
+            slides[self.context.index] = value
+            
+        self.settings.slides = slides
+        
+    slide = property(get_slide, set_slide)
+    
+    
+class AddSlideForm(ploneformbase.EditForm):
+    """
+    The add/edit form for a slide
+    """
+    form_fields = form.FormFields(ISlide)
+    form_fields['slide'].custom_widget = WYSIWYGWidget
+    
+    template = ViewPageTemplateFile('updateslide.pt')
+    
+    label = _(u'heading_add_slide_form', default=u"")
+    description = _(u'description_add_slide_form', default=u"")
+    form_name = _(u'title_add_slide_form', default=u"Add/Update Slide")
+    
+    def __init__(self, context, request):
+        super(AddSlideForm, self).__init__(context, request)
+            
+        
+class SlidesView(BrowserView):
+    """
+    View of all the slides
+    This uses the same page template as the slides widget--just a different __init__ method
+    to setup the call_context and css members
+    """
+    template = ViewPageTemplateFile('slides.pt')
+    
+    def __init__(self, context, request):
+        super(SlidesView, self).__init__(context, request)
+        
+        self.settings = PageSliderSettings(context.context)
+        self.call_context = self.context.context
+        self.css = slider_settings_css(self.settings) # since this uses the same .pt file
+        
+    def __call__(self):
+        return self.template()
+        
+        
+class SlideView(BrowserView):
+    """
+    For doing operations on a slide
+    """
+    
+    def __init__(self, context, request):
+        super(SlideView, self).__init__(context, request)
+        self.settings = PageSliderSettings(context.context)
+    
+    def move_up(self, ajax=None):
+        
+        if self.context.index > 0:
+            slides = self.settings.slides
+            index = self.context.index
+        
+            tmp = slides[index-1]
+            slides[index-1] = slides[index]
+            slides[index] = tmp
+        
+            self.settings.slides = slides
+        
+        if ajax is None:
+            self.request.response.redirect(self.context.context.absolute_url() + "/@@slider-settings")
+        else:
+            return 'done'
+        
+    def move_down(self, ajax=None):
+        
+        if self.context.index < len(self.settings.slides):
+            slides = self.settings.slides
+            index = self.context.index
+        
+            tmp = slides[index+1]
+            slides[index+1] = slides[index]
+            slides[index] = tmp
+        
+            self.settings.slides = slides
+        
+        if ajax is None:
+            self.request.response.redirect(self.context.context.absolute_url() + "/@@slider-settings")
+        else:
+            return 'done'
+            
+    def remove(self, ajax=None):
+        if self.context.index < len(self.settings.slides) and self.context.index >= 0:
+            slides = self.settings.slides
+            del slides[self.context.index]
+            self.settings.slides = slides
+
+        if ajax is None:
+            self.request.response.redirect(self.context.context.absolute_url() + "/@@slider-settings")
+        else:
+            return 'done'
