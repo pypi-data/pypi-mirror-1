57bdb98e54e59dcd3cf1f3022200a9ccb035f4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:48 +0000

    Add package TowelStuff/0.1.1/TowelStuff-0.1.1.tar.gz

---
+++ packages/TowelStuff/TowelStuff-0.1.1.tar.gz/TowelStuff-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='TowelStuff',
+    version='0.1.1',
+    author='John M. Gabriele',
+    author_email='jmg3000@gmail.com',
+    packages=['towel_stuff', 'towel_stuff.test'],
+    scripts=['bin/stowe-towels.py', 'bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.txt').read(),
+)
