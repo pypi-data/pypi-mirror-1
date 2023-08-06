98c7cccf542677512b1db484e2ef0bfb7c55526b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:24 +0000

    Add package django-staticmediamgr/0.3/django-staticmediamgr-0.3.tar.gz

---
+++ packages/django-staticmediamgr/django-staticmediamgr-0.3.tar.gz/django-staticmediamgr-0.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+try:
+    f = open('README.rst', 'r')
+    long_description = f.read()
+    f.close()
+except IOError:
+    long_description = ''
+
+setup(name='django-staticmediamgr',
+      version='0.3',
+      description='A Django app to copy static media files over to a remote place, optionally minifying css and js.',
+      long_description=long_description,
+      author='Corey Oordt, The Washington Times',
+      author_email='coordt@washingtontimes.com',
+      url='http://opensource.washingtontimes.com/projects/django-staticmediamgr/',
+      packages=['staticmediamgr'],
+      classifiers=['Development Status :: 4 - Beta',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            ],
+)
