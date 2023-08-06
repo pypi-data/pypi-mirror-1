f982db346f8e5bcf6bcec8abd02be954568ae6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:01 +0000

    Add package cssutils/0.9.6a4/cssutils-0.9.6a4-py2.4.egg

---
+++ packages/cssutils/cssutils-0.9.6a4-py2.4.egg/tests/test_scripts_csscombine.py
@@ -0,0 +1,36 @@
+"""Testcases for cssutils.scripts.csscombine"""
+__version__ = '$Id: test_scripts_csscombine.py 1644 2009-01-14 11:39:05Z cthedot $'
+
+from cssutils.script import csscombine
+import basetest
+import cssutils
+import os
+import urllib
+
+class CSSCombine(basetest.BaseTestCase):
+
+    C = '@namespace s2"uri";s2|sheet-1{top:1px}s2|sheet-2{top:2px}proxy{top:3px}' 
+
+    def test_combine(self):
+        "scripts.csscombine"        
+        # path, SHOULD be keyword argument!
+        csspath = os.path.join(os.path.dirname(__file__), '..', '..', 
+                               'sheets', 'csscombine-proxy.css')
+        combined = csscombine(csspath)
+        self.assertEqual(combined, self.C)
+        combined = csscombine(path=csspath, targetencoding='ascii')
+        self.assertEqual(combined, '@charset "ascii";' + self.C)
+
+        # url
+        cssurl = u'file:' + urllib.pathname2url(os.path.abspath(csspath))
+        combined = csscombine(url=cssurl)
+        self.assertEqual(combined, self.C)
+        combined = csscombine(url=cssurl, targetencoding='ascii')
+        self.assertEqual(combined, '@charset "ascii";' + self.C)
+
+        cssutils.log.raiseExceptions = True 
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
