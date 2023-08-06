930845f7f17057ab8aec24f5f3c1a11884c6af37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package generator_tools/0.3.3/generator_tools-0.3.3-py2.5.zip

---
+++ packages/generator_tools/generator_tools-0.3.3-py2.5.zip/generator_tools-0.3.3-py2.5/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+import sys
+
+py_version_t = sys.version_info[:2]
+py_version_s = ".".join([str(x) for x in py_version_t])
+
+if __name__ == '__main__':
+    version_ext = ""
+    if 'sdist' in sys.argv:
+        version_ext = '-py'+py_version_s
+
+    setup(
+        name = 'generator_tools',
+        version = '0.3.3'+version_ext,
+        description = 'generator_tools enable copying and pickling generators',
+        author = 'Kay Schluehr',
+        author_email = 'kay@fiber-space.de',
+        url = 'http://www.fiber-space.de/',
+        download_url = 'http://www.fiber-space.de/downloads/download.html',
+        license = "BSD",
+        packages = ['generator_tools',
+                    'generator_tools.tests',
+                    ],
+        package_data={'': ['LICENSE.txt'],
+                      'generator_tools': ['doc/*.html',
+                                          'doc/*.png']},
+    )
+
