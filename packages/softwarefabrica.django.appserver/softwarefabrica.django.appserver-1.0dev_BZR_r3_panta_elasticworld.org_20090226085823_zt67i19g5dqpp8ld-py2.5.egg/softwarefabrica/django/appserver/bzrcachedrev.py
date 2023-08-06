2d86bae2b553603ef028d31d09930c4dcfcef367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:27 +0000

    Add package softwarefabrica.django.appserver/1.0dev-BZR-r3-panta-elasticworld.org-20090226085823-zt67i19g5dqpp8ld/softwarefabrica.django.appserver-1.0dev_BZR_r3_panta_elasticworld.org_20090226085823_zt67i19g5dqpp8ld-py2.5.egg

---
+++ packages/softwarefabrica.django.appserver/softwarefabrica.django.appserver-1.0dev_BZR_r3_panta_elasticworld.org_20090226085823_zt67i19g5dqpp8ld-py2.5.egg/softwarefabrica/django/appserver/bzrcachedrev.py
@@ -0,0 +1,4 @@
+info    = {'revinfo': (3, 'panta@elasticworld.org-20090226085823-zt67i19g5dqpp8ld'), 'basepath': '/arc/bzr/softwarefabrica-appserver.bzr/trunk/', 'revno': 3, 'revname': 'panta@elasticworld.org-20090226085823-zt67i19g5dqpp8ld', 'inpath': u'softwarefabrica/django/appserver/version.py'}
+revno   = 3
+revname = 'panta@elasticworld.org-20090226085823-zt67i19g5dqpp8ld'
+revinfo = (3, 'panta@elasticworld.org-20090226085823-zt67i19g5dqpp8ld')
