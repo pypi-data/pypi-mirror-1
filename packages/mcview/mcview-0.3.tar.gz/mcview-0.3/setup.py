fd24c1b39df0d6dbca486819f228956a8314afad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:50 +0000

    Add package mcview/0.3/mcview-0.3.tar.gz

---
+++ packages/mcview/mcview-0.3.tar.gz/mcview-0.3/setup.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+"""mcview: a 3D / graph event viewer for high-energy physics event simulations"""
+
+## Get setuptools
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+
+longdesc = """mcview uses the Python wrapper for HepMC to load and view HepMC
+events as 3D final-state representations in (log-)momentum space, and to dump
+the graph structure to PDF and graphviz formats.
+"""
+
+## Setup definition
+setup(name = 'mcview',
+      version = 0.3,
+      #include_package_data = True,
+      install_requires = ['pyhepmc', 'pydot'], # 'visual'/'vpython'
+      scripts = ['mcview'],
+      author = ['Andy Buckley'],
+      author_email = 'andy@insectnation.org',
+      #url = 'http://projects.hepforge.org/professor/',
+      description = 'A 3D / graph event viewer for high-energy physics event simulations',
+      long_description = longdesc,
+      keywords = 'generator montecarlo simulation data hep physics particle',
+      license = 'GPL',
+      classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Physics']
+      )
