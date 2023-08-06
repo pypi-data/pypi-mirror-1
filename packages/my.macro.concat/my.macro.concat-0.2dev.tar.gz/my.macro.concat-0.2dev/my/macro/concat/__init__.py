d0ae506f8a47c783395c29e4ef0bdd26c503bf3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:38 +0000

    Add package my.macro.concat/0.2dev/my.macro.concat-0.2dev.tar.gz

---
+++ packages/my.macro.concat/my.macro.concat-0.2dev.tar.gz/my.macro.concat-0.2dev/my/macro/concat/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Recipe concat"""
+
+class Recipe(object):
+    """zc.buildout recipe"""
+
+    def __init__(self, buildout, name, options):
+        self.buildout, self.name, self.options = buildout, name, options
+        self.target=options.get("target", None)
+        self.option=options.get("option", None)
+        self.parts = options.pop('parts', name).strip().split()
+        print self.parts
+        #self.parts=options.get("parts", None)
+
+        if not (self.target and self.option and self.parts):
+            self.logger.error("""You need to specify a target part, an option
+                    and parts from which concat the option on the target part""")
+            raise zc.buildout.UserError("options missing")
+
+    def install(self):
+        """Installer"""
+        # XXX Implement recipe functionality here
+        
+        # Return files that were created by the recipe. The buildout
+        # will remove all returned files upon reinstall.
+        result = []
+        for part in self.parts:
+            result.append(self.buildout[part][self.option])
+        self.buildout[self.target][self.option] = '\n'.join(result)
+        return self.buildout #tuple()
+
+    update = install
+
+    #def update(self):
+    #    """Updater"""
+    #    pass
