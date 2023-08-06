a4484e0b87fc8c014745080aa30025c0b83385ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:17 +0000

    Add package z3c.pt/1.0b17/z3c.pt-1.0b17.zip

---
+++ packages/z3c.pt/z3c.pt-1.0b17.zip/z3c.pt-1.0b17/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import sys
+version = '1.0b17'
+
+install_requires = [
+    'setuptools',
+    'zope.interface',
+    'zope.component',
+    'zope.i18n >= 3.5',
+    'zope.traversing',
+    'zope.contentprovider',
+    'chameleon.zpt >= 1.0b17',
+    ]
+
+if sys.version_info[:3] < (2,5,0):
+    install_requires.append('elementtree')
+
+setup(name='z3c.pt',
+      version=version,
+      description="Python template compiler which supports the Genshi and "
+                  "ZPT template languages including macro extensions and "
+                  "internationalization.",
+      long_description=open("README.txt").read() + open("CHANGES.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Text Processing :: Markup :: XML",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Malthe Borch and the Zope Community',
+      author_email='zope-dev@zope.org',
+      url='',
+      license='ZPL',
+      namespace_packages=['z3c'],
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      )
