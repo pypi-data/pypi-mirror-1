8ac49d5cbcc4d0a049b6737e1b25dac6a90e9c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:17 +0000

    Add package ll-xist/3.6.4/ll_xist-3.6.4-py2.5-macosx-10.5-i386.egg

---
+++ packages/ll-xist/ll_xist-3.6.4-py2.5-macosx-10.5-i386.egg/EGG-INFO/scripts/db2ul4.py
@@ -0,0 +1,9 @@
+#!/System/Library/Frameworks/Python.framework/Versions/2.5/Resources/Python.app/Contents/MacOS/Python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.scripts import db2ul4
+	sys.exit(db2ul4.main())
---
+++ packages/ll-xist/ll_xist-3.6.4-py2.5-macosx-10.5-i386.egg/EGG-INFO/scripts/dtd2xsc.py
@@ -0,0 +1,10 @@
+#!/System/Library/Frameworks/Python.framework/Versions/2.5/Resources/Python.app/Contents/MacOS/Python
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.xist.scripts import dtd2xsc
+	sys.exit(dtd2xsc.main())
