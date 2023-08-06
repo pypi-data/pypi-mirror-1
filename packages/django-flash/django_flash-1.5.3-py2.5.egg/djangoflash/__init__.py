5cb6e4cc50ee8ecf9bae3097dd5b69fbd384561c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:35 +0000

    Add package django-flash/1.5.3/django_flash-1.5.3-py2.5.egg

---
+++ packages/django-flash/django_flash-1.5.3-py2.5.egg/djangoflash/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Django-flash
+    ~~~~~~~~~~~~
+
+    Rails-like *flash* messages support for Django.
+
+    :copyright: 2008-2009, Destaquenet Technology Solutions.
+    :license: BSD.
+"""
+
+__version__ = '1.5.3'
+
