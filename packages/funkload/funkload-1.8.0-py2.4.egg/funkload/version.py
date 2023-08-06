f09c140cbcea6d9ff26d8bdffedeecbd9e75ea8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:28 +0000

    Add package funkload/1.8.0/funkload-1.8.0-py2.4.egg

---
+++ packages/funkload/funkload-1.8.0-py2.4.egg/funkload/version.py
@@ -0,0 +1,23 @@
+# (C) Copyright 2005 Nuxeo SAS <http://nuxeo.com>
+# Author: bdelbosc@nuxeo.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+#
+"""The FunkLoad version.
+
+$Id: version.py 52907 2008-07-23 21:05:25Z bdelbosc $
+"""
+__version__ = '1.8.0'
