3efa95fb2a45a21aecec9c03cb790e203893fa08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:29 +0000

    Add package se.portlet.gallery/1.2.1bugfix-r8/se.portlet.gallery-1.2.1bugfix_r8-py2.4.egg

---
+++ packages/se.portlet.gallery/se.portlet.gallery-1.2.1bugfix_r8-py2.4.egg/se/portlet/gallery/__init__.py
@@ -0,0 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+GalleryPortletMessageFactory = MessageFactory('se.portlet.gallery')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
