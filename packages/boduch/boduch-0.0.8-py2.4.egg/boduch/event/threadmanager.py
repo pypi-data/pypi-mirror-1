de21eebaf04c4ca3140f081d452953198e84f061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:22 +0000

    Add package boduch/0.0.8/boduch-0.0.8-py2.4.egg

---
+++ packages/boduch/boduch-0.0.8-py2.4.egg/boduch/event/threadmanager.py
@@ -0,0 +1,36 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from zope.interface import implements
+from boduch.interface import IThreadManager
+from boduch.event import EventThread
+
+class ThreadManager:
+    implements(IThreadManager)
+    threaded=False
+    
+    @classmethod
+    def get_threaded(cls):
+        """Return true if the thread manager is running in threaded mode."""
+        return cls.threaded
+    
+    @classmethod
+    def start_event_thread(cls, handles):
+        """Start a new thread of control to run the specified list of event
+        handlers in."""
+        EventThread(handles).run()    
+    
+__all__=['ThreadManager']
