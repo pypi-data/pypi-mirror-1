f693fd8d65312e2e72cce6c66155f7a5855e8bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:21 +0000

    Add package improviser/0.8.2.3/improviser-0.8.2.3.tar.gz

---
+++ packages/improviser/improviser-0.8.2.3.tar.gz/improviser-0.8.2.3/improviser/qtImproviser.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from PyQt4 import QtCore, QtGui
+from qtGUI.MainWindow import ImproviserMainWindow
+from qtGUI.PreferencesDialog import PreferencesDialog
+from sys import argv
+
+def show_main(params):
+	i = ImproviserMainWindow()
+	i.loggedin = params["login"]
+	i.default_folder = params["default_folder"]
+	i.checkupdates = params["checkupdates"]
+	i.credentials = {"username": params["username"], 
+			 "password": params["password"]}
+
+	if 'soundfont' not in params:
+		i.no_fluidsynth = True
+		i.soundfont = ''
+		i.driver = ''
+	else:
+		i.no_fluidsynth = False
+		i.soundfont = params["soundfont"]
+		i.driver = params["driver"]
+	i.connect_widgets()
+	i.show()
+
+app = QtGui.QApplication(argv)
+p = PreferencesDialog(show_main)
+p.show()
+app.exec_()
+
+
