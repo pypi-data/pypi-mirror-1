3b8e768ff8c8b52a615ebf7b8cce0a2b08f23709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:21 +0000

    Add package pyoai/2.3/pyoai-2.3.tar.gz

---
+++ packages/pyoai/pyoai-2.3.tar.gz/pyoai-2.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyoai',
+    version='2.3',
+    author='Infrae',
+    author_email='jasper@infrae.com',
+    url='http://www.infrae.com/download/oaipmh',
+    description="""\
+The oaipmh module is a Python implementation of an "Open Archives
+Initiative Protocol for Metadata Harvesting" (version 2) client and server.
+The protocol is described here:
+
+http://www.openarchives.org/OAI/openarchivesprotocol.html
+""",
+    long_description="""\
+The oaipmh module is a Python implementation of an "Open Archives
+Initiative Protocol for Metadata Harvesting" (version 2) client and server.
+The protocol is described here:
+
+http://www.openarchives.org/OAI/openarchivesprotocol.html
+
+With it, you can easily harvest existing OAI-PMH compliant data providers,
+as well as construct your own OAI-PMH data provider.
+""",
+    
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    zip_safe=False,
+    license='BSD',
+    keywords='OAI-PMH xml archive',
+    install_requires=['lxml'],
+)
