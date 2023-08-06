a9a4bd0c118c153d5247d2e67b3c319a33918cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:56 +0000

    Add package Products.plonehrm/2.1.3/Products.plonehrm-2.1.3.tar.gz

---
+++ packages/Products.plonehrm/Products.plonehrm-2.1.3.tar.gz/Products.plonehrm-2.1.3/Products/plonehrm/browser/substitution.py
@@ -0,0 +1,239 @@
+from zope.interface import implements
+from zope.formlib import form
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Acquisition import aq_inner, aq_parent
+from DateTime import DateTime
+from Products.Five import BrowserView
+from Products.CMFPlone.utils import getSiteEncoding
+from plone.app.portlets.portlets import base
+
+from Products.plonehrm.interfaces import IEmployee
+from Products.plonehrm.interfaces import IWorkLocation
+from Products.plonehrm.browser.interfaces import ISubstitutionPortlet
+from Products.plonehrm import PloneHrmMessageFactory as _
+
+from plonehrm.contracts.interfaces import IContract
+
+
+class BaseSubstitutionView(BrowserView):
+    """Substitute some variables or print the available variables.
+
+    Only useful to call on Document-like content types.  It has two uses:
+
+    - When called on a Document within one of the template tools of
+      plonehrm modules, like portal_contracts and
+      portal_jobperformance, it can list parameters that are available
+      for substitution.
+
+    - When called on a child of an Employee (e.g. Contract or
+      JobPerformanceInterview), you can substitute variables mentioned
+      in those documents.
+
+    Inherit from this in your employee module.
+    """
+
+    # override this mapping to localize the substitution terms (yes, one
+    # language per site only)
+    substitution_translations = {
+        '[company_address]': '[company_address]',
+        '[company_postal_code]': '[company_postal_code]',
+        '[company_city]': '[company_city]',
+        '[company_official_name]': '[company_official_name]',
+        '[contract_duration]': '[contract_duration]',
+        '[contract_function]': '[contract_function]',
+        '[contract_hours_per_week]': '[contract_hours_per_week]',
+        '[contract_part_full_time]': '[contract_part_full_time]',
+        '[contract_startdate]': '[contract_startdate]',
+        '[contract_trial_period]': '[contract_trial_period]',
+        '[contract_wage]': '[contract_wage]',
+        '[employee_address]': '[employee_address]',
+        '[employee_city]': '[employee_city]',
+        '[employee_date_of_birth]': '[employee_date_of_birth]',
+        '[employee_gender]': '[employee_gender]',
+        '[employee_initials]': '[employee_initials]',
+        '[employee_last_name]': '[employee_last_name]',
+        '[employee_official_name]': '[employee_official_name]',
+        '[employee_place_of_birth]': '[employee_place_of_birth]',
+        '[employee_postal_code]': '[employee_postal_code]',
+        '[today]': '[today]',
+        '[worklocation_address]': '[worklocation_address]',
+        '[worklocation_postal_code]': '[worklocation_postal_code]',
+        '[worklocation_city]': '[worklocation_city]',
+        '[worklocation_trade_name]': '[worklocation_trade_name]',
+        '[worklocation_vacation_days]': '[worklocation_vacation_days]',
+    }
+
+    def __init__(self, context, request):
+        self.context = aq_inner(context)
+
+        # We're called on a child of an employee. So we need to
+        # grab our parent (=employee) and our grandparent
+        # (=worklocation).
+        self.employee = aq_parent(aq_inner(self.context))
+        if not IEmployee.providedBy(self.employee):
+            self.employee = None
+        self.worklocation = aq_parent(aq_inner(self.employee))
+        if not IWorkLocation.providedBy(self.worklocation):
+            self.worklocation = None
+
+        # Call an update method.  Now you might only need to override
+        # that method when you want to customize this view.
+        self.params = self.calcParams()
+
+        # keys is no longer used by calcParams (because it builds a new
+        # sorted list), but it does get used for displaying the available
+        # terms in the views
+        self.keys = self.substitution_translations.values()
+        self.keys.sort()
+
+    def calcParams(self):
+        """Calculate the values of the parameters for substitution.
+        """
+        r = {}
+        ploneview = self.context.restrictedTraverse('@@plone')
+
+        # We ignore [naam_formule] but make a note of it anyway.
+        r['[employee_official_name]'] = self.employee and \
+            self.employee.officialName()
+        if self.employee:
+            gender = self.employee.getGender()
+            if gender == 'man':
+                gender = 'De Heer'
+            elif gender == 'vrouw':
+                gender = 'Mevrouw'
+        r['[employee_gender]'] = self.employee and gender
+        r['[employee_initials]'] = self.employee and \
+            self.employee.getInitials()
+        r['[employee_last_name]'] = self.employee and (
+            self.employee.getMiddleName() + ' ' +
+            self.employee.getLastName()).strip()
+        r['[employee_address]'] = self.employee and self.employee.getAddress()
+        r['[employee_postal_code]'] = self.employee and \
+            self.employee.getPostalCode()
+        r['[employee_city]'] = self.employee and self.employee.getCity()
+        r['[employee_place_of_birth]'] = self.employee and \
+            self.employee.getPlaceOfBirth()
+        r['[employee_date_of_birth]'] = self.employee and \
+            ploneview.toLocalizedTime(self.employee.getBirthDate())
+
+        # Totally skip the contract parameters when you are in the
+        # FunctioningTool
+        if not 'portal_jobperformance' in self.context.absolute_url():
+            contract = self.context
+            if not IContract.providedBy(contract):
+                contract = None
+            r['[contract_startdate]'] = contract and ploneview.toLocalizedTime(
+                contract.getStartdate())
+            r['[contract_part_full_time]'] = contract and \
+                contract.getEmploymentType()
+            r['[contract_hours_per_week]'] = contract and contract.getHours()
+            r['[contract_function]'] = contract and contract.getFunction()
+            r['[contract_wage]'] = contract and contract.getWage()
+            r['[contract_trial_period]'] = contract and \
+                contract.getTrialPeriod()
+            r['[contract_duration]'] = contract and contract.getDuration()
+
+        # Note: Do something with the CAO?
+
+        # Work Location:
+        r['[worklocation_trade_name]'] = self.worklocation and \
+            self.worklocation.Title()
+        r['[worklocation_address]'] = self.worklocation and \
+            '<br />'.join(self.worklocation.getAddress())
+        r['[worklocation_postal_code]'] = self.worklocation and \
+            self.worklocation.getPostalCode()
+        r['[worklocation_city]'] = self.worklocation and \
+            self.worklocation.getCity()
+        r['[worklocation_vacation_days]'] = self.worklocation and \
+            self.worklocation.getVacationDays()
+
+        # Company:
+        r['[company_official_name]'] = self.worklocation and \
+            self.worklocation.getOfficialName()
+        r['[company_address]'] = self.worklocation and \
+            '<br />'.join(self.worklocation.getCompanyAddress())
+        r['[company_postal_code]'] = self.worklocation and \
+            self.worklocation.getCompanyPostalCode()
+        r['[company_city]'] = self.worklocation and \
+            self.worklocation.getCompanyCity()
+
+        r['[today]'] = ploneview.toLocalizedTime(DateTime())
+
+        return r
+
+    def substitute(self, text):
+        keytuples = self.substitution_translations.items()
+        # We will reverse the keys as otherwise $NAME would also match
+        # $NAMELIST or so.
+        # Actually, we switched to [name] so that would never match
+        # [namelist], but let's keep this reversal for those who prefer
+        # their paramaters prefixed with a dollar sign.
+        keytuples.sort(key=lambda a: -len(a[1]))
+
+        encoding = getSiteEncoding(self.context)
+        for key, translated_key in keytuples:
+            value = self.params[key]
+            if isinstance(value, unicode):
+                value = value.encode(encoding)
+            if value is None:
+                value = '&hellip;'
+            if not isinstance(value, str):
+                # e.g. an integer
+                value = str(value)
+            text = text.replace(translated_key, value)
+        return text
+
+
+class Assignment(base.Assignment):
+    implements(ISubstitutionPortlet)
+
+    @property
+    def title(self):
+        return _(u"Available parameters")
+
+
+class Renderer(base.Renderer):
+
+    _template = ViewPageTemplateFile('parameters.pt')
+
+    @property
+    def available(self):
+        """Should the portlet be shown?
+
+        Portlet is available when there are keys.
+
+        They are only handy for showing when viewing (editing really)
+        a Document within portal_jobperformance or portal_contracts.
+        """
+        url = self.context.absolute_url()
+        tool_ids = ('portal_jobperformance', 'portal_contracts')
+        for tool_id in tool_ids:
+            if tool_id in url:
+                if url.split('/')[-1] != tool_id:
+                    return len(self.keys) > 0
+                return False
+        return False
+
+    def render(self):
+        return self._template()
+
+    @property
+    def keys(self):
+        context = aq_inner(self.context)
+        view = context.restrictedTraverse('@@substituter')
+        return view.keys
+
+
+class AddForm(base.AddForm):
+    form_fields = form.Fields(ISubstitutionPortlet)
+    label = _(u"Add Substitution parameters Portlet")
+    description = _(u"This portlet displays substitution parameters.")
+
+    def create(self, data):
+        return Assignment()
+
+
+class EditForm(base.EditForm):
+    form_fields = form.Fields(ISubstitutionPortlet)
+    label = _(u"Edit Substitution parameters Portlet")
+    description = _(u"This portlet displays substitution parameters.")
---
+++ packages/Products.plonehrm/Products.plonehrm-2.1.3.tar.gz/Products.plonehrm-2.1.3/Products/plonehrm/content/employee.py
@@ -0,0 +1,291 @@
+__author__ = """Reinout van Rees <reinout@zestsoftware.nl>"""
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import BaseFolder
+from Products.Archetypes.atapi import BaseFolderSchema
+from Products.Archetypes.atapi import CalendarWidget
+from Products.Archetypes.atapi import ComputedField
+from Products.Archetypes.atapi import DateTimeField
+from Products.Archetypes.atapi import registerType
+from Products.Archetypes.atapi import Schema
+from Products.Archetypes.atapi import SelectionWidget
+from Products.Archetypes.atapi import StringField
+from Products.Archetypes.atapi import StringWidget
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces.structure import INonStructuralFolder
+from Products.CMFPlone.utils import safe_unicode
+from Products.plonehrm import PloneHrmMessageFactory as _
+from Products.plonehrm import config
+from Products.plonehrm.interfaces import IEmployee
+from zope.interface import implements
+
+
+schema = Schema((
+    StringField(
+        name='employeeNumber',
+        widget=StringWidget(
+            label=_(u'label_employeeNumber', default=u'Employee number'),
+        ),
+    ),
+    StringField(
+        name='firstName',
+        widget=StringWidget(
+            label=_(u'plonehrm_label_firstName', default=u'Firstname'),
+        ),
+    ),
+    StringField(
+        name='middleName',
+        widget=StringWidget(
+            label=_(u'plonehrm_label_middleName', default=u'Middle name'),
+        ),
+    ),
+    StringField(
+        name='lastName',
+        widget=StringWidget(
+            label=_(u'plonehrm_label_lastName', default=u'Last name'),
+        ),
+        required=1
+    ),
+    StringField(
+        name='initials',
+        widget=StringWidget(
+            label=_(u'plonehrm_label_initials', default=u'Initials'),
+        )
+    ),
+    StringField(
+        name='address',
+        widget=StringWidget(
+            label=_(u'label_address', default='Address'),
+        ),
+    ),
+    StringField(
+        name='postalCode',
+        widget=StringWidget(
+            label=_(u'label_postalCode', default=u'Postalcode'),
+        ),
+    ),
+    StringField(
+        name='city',
+        widget=StringWidget(
+            label=_(u'label_city', default=u'City'),
+        ),
+    ),
+    StringField(
+        name='state',
+        widget=StringWidget(
+            label=_(u'label_state', default=u'State/Province'),
+        ),
+    ),
+    StringField(
+        name='country',
+        widget=StringWidget(
+            label=_(u'label_country', default=u'Country'),
+        ),
+    ),
+    StringField(
+        name='telephone',
+        widget=StringWidget(
+            label=_(u'label_telephone', default=u'Phone'),
+        ),
+    ),
+    StringField(
+        name='mobilePhone',
+        widget=StringWidget(
+            label=_(u'label_mobilePhone', default=u'Mobilephone'),
+        ),
+    ),
+    StringField(
+        name='email',
+        widget=StringWidget(
+            label=_(u'label_email', default=u'Email'),
+        ),
+    ),
+    DateTimeField(
+        name='birthDate',
+        required=1,
+        validators=("isDateBeforeToday", "checkIfTooYoung"),
+        widget=CalendarWidget(
+            starting_year=1940,
+            show_hm=0,
+            label=_(u'label_birthDate', default=u'Birth date'),
+        ),
+    ),
+    StringField(
+        name='placeOfBirth',
+        widget=StringWidget(
+            label=_(u'label_placeOfBirth', default=u'Place of birth'),
+        ),
+    ),
+    StringField(
+        name='gender',
+        widget=SelectionWidget(
+            format="select",
+            label=_(u'label_gender', default=u'Gender'),
+        ),
+        vocabulary='_genderVocabulary'
+    ),
+    StringField(
+        name='civilStatus',
+        widget=SelectionWidget(
+            format="select",
+            label=_(u'label_civilStatus', default=u'Civil status'),
+        ),
+        vocabulary='_civilStatusVocabulary'
+    ),
+    StringField(
+        name='idType',
+        widget=SelectionWidget(
+            format="select",
+            label=_(u'label_idType', default=u'Type of ID'),
+        ),
+        vocabulary='_idTypeVocabulary'
+    ),
+    StringField(
+        name='idNumber',
+        widget=StringWidget(
+            label=_(u'label_idNumber', default=u'ID number'),
+        ),
+    ),
+    DateTimeField(
+        name='idEndDate',
+        widget=CalendarWidget(
+            show_hm=0,
+            starting_year=2007,
+            label=_(u'label_idEndDate', default=u'Expiration date'),
+        ),
+    ),
+    StringField(
+        name='nationality',
+        widget=StringWidget(
+            label=_(u'label_nationality', default=u'Nationality'),
+        ),
+    ),
+    StringField(
+        name='socialSecurityNumber',
+        widget=StringWidget(
+            label=_(u'label_socialSecurityNumber',
+                    default=u'Social security number'),
+        ),
+    ),
+    StringField(
+        name='bankNumber',
+        widget=StringWidget(
+            label=_(u'label_bankNumber', default=u'Bank number'),
+        ),
+    ),
+   DateTimeField(
+        name='workStartDate',
+        validators=("isDateBeforeToday"),
+        widget=CalendarWidget(
+            starting_year=1940,
+            show_hm=0,
+            label=_(u'label_work_start_date', default=u'Work start date'),
+        ),
+    ),
+    ComputedField(
+        searchable=True,
+        name='title',
+        widget=ComputedField._properties['widget'](
+            label=_(u'plonehrm_label_title', default=u'Title'),
+        ),
+        accessor="Title"
+    ),
+),
+)
+
+Employee_schema = BaseFolderSchema.copy() + schema.copy()
+Employee_schema.moveField('description', after='initials')
+Employee_schema['title'].widget.visible = 0
+Employee_schema['state'].widget.condition = 'object/showState'
+Employee_schema['country'].widget.condition = 'object/showCountry'
+
+
+class Employee(BaseFolder):
+    """
+    """
+    security = ClassSecurityInfo()
+    __implements__ = (BaseFolder.__implements__, )
+    implements(IEmployee, INonStructuralFolder)
+
+    _at_rename_after_creation = True
+    schema = Employee_schema
+
+    security.declarePublic('Title')
+    def Title(self):
+        """Return title, composed of the first/middle/last names.
+        """
+        parts = [self.getFirstName(),
+                 self.getMiddleName(),
+                 self.getLastName()]
+        # Filter out the empty bits:
+        parts = [safe_unicode(part) for part in parts if part]
+        return u' '.join(parts)
+
+    security.declarePublic('getInitials')
+    def getInitials(self):
+        """Get the initials and possibly add dots
+        """
+        initials = self.initials
+        if initials and initials.find('.') == -1:
+            # Add dots here
+            initials = '.'.join(initials) + '.'
+        return initials
+
+    security.declarePublic('officialName')
+    def officialName(self):
+        """
+        """
+        parts = [self.getInitials(),
+                 self.getMiddleName(),
+                 self.getLastName()]
+        # Filter out the empty bits:
+        parts = [safe_unicode(part) for part in parts if part]
+        return u' '.join(parts)
+
+    security.declarePublic('showState')
+    def showState(self):
+        """Should the state widget be shown?
+        """
+        return self._extractVocabulary(name='show_state',
+                                       default=True)
+
+    security.declarePublic('showCountry')
+    def showCountry(self):
+        """Should the country widget be shown?
+        """
+        return self._extractVocabulary(name='show_country',
+                                       default=True)
+
+    security.declarePublic('_genderVocabulary')
+    def _genderVocabulary(self):
+        """Return vocabulary for gender.
+        """
+        return self._extractVocabulary(name='gender_vocabulary',
+                                       default=[])
+
+    security.declarePublic('_civilStatusVocabulary')
+    def _civilStatusVocabulary(self):
+        """Return vocabulary for civil status.
+        """
+        return self._extractVocabulary(name='civil_status_vocabulary',
+                                       default=[])
+
+    security.declarePublic('_idTypeVocabulary')
+    def _idTypeVocabulary(self):
+        """Return vocabulary for ID type.
+        """
+        return self._extractVocabulary(name='id_type_vocabulary',
+                                       default=[])
+
+    def _extractVocabulary(self, name='', default=True):
+        """Return vocabulary by name.
+        """
+        pp = getToolByName(self, 'portal_properties')
+        pdp = getattr(pp, 'personaldata_properties', None)
+        if not pdp:
+            return default
+        return pdp.getProperty(name, default)
+
+
+registerType(Employee, config.PROJECTNAME)
