a360bcb0748173788d99adae5e5c70d7d482f792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:15 +0000

    Add package collective.recipe.modwsgi/1.1/collective.recipe.modwsgi-1.1.tar.gz

---
+++ packages/collective.recipe.modwsgi/collective.recipe.modwsgi-1.1.tar.gz/collective.recipe.modwsgi-1.1/collective/recipe/modwsgi/__init__.py
@@ -0,0 +1,66 @@
+import logging
+import os
+import zc.buildout
+from zc.recipe.egg.egg import Eggs
+
+WRAPPER_TEMPLATE = """\
+import sys
+syspaths = [
+    %(syspath)s,
+    ]
+
+for path in reversed(syspaths):
+    if path not in sys.path:
+        sys.path[0:0]=[path]
+
+
+from paste.deploy import loadapp
+application = loadapp("config:%(config)s")
+"""
+
+class Recipe:
+    def __init__(self, buildout, name, options):
+        self.buildout=buildout
+        self.name=name
+        self.options=options
+        self.logger=logging.getLogger(self.name)
+
+        
+        if "config-file" not in options:
+            self.logger.error("You need to specify either a paste configuration file")
+            raise zc.buildout.UserError("No paste configuration given")
+
+
+
+    def install(self):
+        egg=Eggs(self.buildout, self.options["recipe"], self.options)
+        reqs,ws=egg.working_set()
+        path=[pkg.location for pkg in ws]
+        extra_paths = self.options.get('extra-paths', '')
+        extra_paths = extra_paths.split()
+        path.extend(extra_paths)
+
+        output=WRAPPER_TEMPLATE % dict(
+            config=self.options["config-file"],
+            syspath=",\n    ".join((repr(p) for p in path))
+            )
+
+        location=os.path.join(self.buildout["buildout"]["parts-directory"],
+                              self.name)
+        if not os.path.exists(location):
+            os.mkdir(location)
+            self.options.created(location)
+
+        target=os.path.join(location, "wsgi")
+        f=open(target, "wt")
+        f.write(output)
+        f.close()
+        os.chmod(target, 0755)
+        self.options.created(target)
+
+        return self.options.created()
+
+
+    def update(self):
+        self.install()
+
---
+++ packages/collective.recipe.modwsgi/collective.recipe.modwsgi-1.1.tar.gz/collective.recipe.modwsgi-1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='collective.recipe.modwsgi',
+      version=version,
+      description="WSGI from buildout",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Buildout",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Wichert Akkerman',
+      author_email='wichert@wiggy.net',
+      url='',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.recipe'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          'zc.recipe.egg',
+      ],
+      entry_points="""
+      [zc.buildout]
+      default = collective.recipe.modwsgi:Recipe
+      """,
+      )
