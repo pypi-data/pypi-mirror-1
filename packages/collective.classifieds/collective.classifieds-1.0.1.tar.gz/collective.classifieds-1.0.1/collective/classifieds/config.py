a48dc65187ee9b4bab001bdc060d22965b7466ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:04 +0000

    Add package collective.classifieds/1.0.1/collective.classifieds-1.0.1.tar.gz

---
+++ packages/collective.classifieds/collective.classifieds-1.0.1.tar.gz/collective.classifieds-1.0.1/collective/classifieds/config.py
@@ -0,0 +1,29 @@
+__author__ = """Four Digits <Ralph Jacobs>"""
+__docformat__ = 'plaintext'
+
+from Products.CMFCore.permissions import setDefaultRoles
+
+PROJECTNAME = "collective.classifieds"
+
+# Permissions
+DEFAULT_ADD_CONTENT_PERMISSION = "Add portal content"
+setDefaultRoles(DEFAULT_ADD_CONTENT_PERMISSION, ('Manager', 'Owner'))
+ADD_CONTENT_PERMISSIONS = {
+    'ClassifiedsCategory': 'Classifieds: Add ClassifiedsCategory',
+    'Classifieds': 'Classifieds: Add Classifieds',
+    'Classified': 'Classifieds: Add Classified',
+}
+
+setDefaultRoles('Classifieds: Add ClassifiedsCategory', ('Manager','Owner'))
+setDefaultRoles('Classifieds: Add Classifieds', ('Manager','Owner'))
+setDefaultRoles('Classifieds: Add Classified', ('Manager','Owner'))
+
+product_globals = globals()
+
+# Dependencies of Products to be installed by quick-installer
+# override in custom configuration
+DEPENDENCIES = []
+
+# Dependend products - not quick-installed - used in testcase
+# override in custom configuration
+PRODUCT_DEPENDENCIES = []
