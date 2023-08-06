502925a2a0a2eddd8dbd481ac52a9be8347b4f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:41 +0000

    Add package DNSFlash/0.3.0/DNSFlash-0.3.0.tar.gz

---
+++ packages/DNSFlash/DNSFlash-0.3.0.tar.gz/DNSFlash-0.3.0/dnsflash.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+
+###########################################################################
+#   Copyright (C) 2008 by SukkoPera                                       #
+#   sukkosoft@sukkology.net                                               #
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 2 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+#   This program is distributed in the hope that it will be useful,       #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#   GNU General Public License for more details.                          #
+#                                                                         #
+#   You should have received a copy of the GNU General Public License     #
+#   along with this program; if not, write to the                         #
+#   Free Software Foundation, Inc.,                                       #
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+###########################################################################
+
+
+import os
+import sys
+# See if we can use syslog
+try:
+	import syslog
+	haveSyslog = True
+except NameError:
+	haveSyslog = False
+from optparse import OptionParser
+from xml.dom.minidom import parse, parseString, getDOMImplementation
+
+from dnsomatic_api import DnsOMatic
+
+PROGRAM_VERSION = "0.3.0"
+DEFAULT_CONFIG_FILE = "/etc/dnsflash.conf"
+
+def getString (nodelist):
+	"""Helper function to parse the XML configuration file."""
+	if len (nodelist) >= 1 and nodelist[0].hasChildNodes ():
+		ret = nodelist[0].childNodes[0].data.strip ()
+	else:
+		ret = None
+	return ret
+
+def getStringList (nodelist):
+	"""Helper function to parse the XML configuration file."""
+	ret = []
+	for node in nodelist:
+		ret.append (node.childNodes[0].data.strip ())
+	return ret
+
+def readConfig (configFile):
+	"""Read username, password and other data from configuration file."""
+	try:
+		dom = parse (configFile)
+		for element in dom.getElementsByTagName ("config"):
+			service = element.getAttribute ("service")
+			if service == "dnsomatic":
+				username = getString (element.getElementsByTagName ("username"))
+				password = getString (element.getElementsByTagName ("password"))
+				ip = getString (element.getElementsByTagName ("ip_address"))
+				hostnames = getStringList (element.getElementsByTagName ("hostname"))
+				break
+	except IOError:
+		# File not found
+		print "Cannot open configuration file %s" % configFile
+		username = None
+		password = None
+		ip = None
+		hostnames = None
+	return username, password, ip, hostnames
+
+
+class Logger:
+	def __init__ (self, quiet, ident = None):
+		if quiet:
+			self.msg = self.quietprint
+		else:
+			self.msg = self.realprint
+
+		if haveSyslog:
+			syslog.openlog (ident, 0, syslog.LOG_USER)
+
+	def __del__ (self):
+		if haveSyslog:
+			syslog.closelog ()
+
+	def quietprint (self, msg):
+		if haveSyslog:
+			syslog.syslog (msg)
+		pass
+
+	def realprint (self, msg):
+		print msg
+
+	def err (self, msg):
+		if haveSyslog:
+			syslog.syslog (syslog.LOG_ERR, "ERROR: %s" % msg)
+		print "ERROR: %s" % msg
+
+def actionGetIP (logger):
+	d = DnsOMatic ()
+	ip = d.getCurrentLocalIP ()
+	if ip is not None:
+		logger.msg ("Your IP is: %s" % ip)
+		ret = 0
+	else:
+		logger.err ("Cannot retrieve IP")
+		ret = 1
+	return ret
+
+def actionUpdateIP (logger, cmd_username = None, cmd_password = None, cmd_ip = None, cmd_hostnames = None, config = None, force = False):
+	if config is not None:
+		#print "Using configuration file: %s" % config
+		username, password, ip, hostnames = readConfig (config)
+		ret = 0
+	else:
+		if cmd_username is None or cmd_username == "":
+			logger.err ("No username provided")
+			ret = 101
+		elif cmd_password is None or cmd_password == "":
+			logger.err ("No password provided")
+			ret = 102
+		else:
+			username = cmd_username
+			password = cmd_password
+			ip = cmd_ip
+			hostnames = cmd_hostnames
+			ret = 0
+
+	if ret == 0:
+		d = DnsOMatic (username, password)
+		res = d.update (ip, hostnames, force)
+		if hostnames is None or len (hostnames) == 0:
+			(status, ip) = res[0]
+			if status == "good":
+				logger.msg ("All IPs successfully updated to %s" % ip)
+			else:
+				logger.err ("Cannot update any IP: %s" % status)
+				ret = 1
+		else:
+			ret = 0		# Assume all went well
+			assert (len (hostnames) == len (res))
+			for hostname, (status, ip) in zip (hostnames, res):
+				if status == "good":
+					logger.msg ("IP for %s successfully updated to %s" % (hostname, ip))
+				elif status == "alreadyok":
+					logger.msg ("No need to update IP for %s, already pointing to %s" % (hostname, ip))
+				else:
+					logger.err ("Cannot update IP for %s: %s" % (hostname, status))
+					ret += 1
+	return ret
+
+def main ():
+	cmdline_parser = OptionParser (usage = "Usage: %prog -g [options]", description = "Update IP on the DNS-O-Matic service", version = "%s" % PROGRAM_VERSION)
+	cmdline_parser.add_option ("-g", "--getip", action = "store_true", dest = "no_update", help = "Do not update anything, only retrieve the local IP", default = False)
+	cmdline_parser.add_option ("-u", "--username", action = "store", type = "string", dest = "username", help = "Specify username for DNS-O-Matic login (has precedence over configuration file)", default = None)
+	cmdline_parser.add_option ("-p", "--password", action = "store", type = "string", dest = "password", help = "Specify password for DNS-O-Matic login", default = None)
+	cmdline_parser.add_option ("-i", "--ip", action = "store", type = "string", dest = "ip", help = "Specify IP for update", default = None)
+	cmdline_parser.add_option ("-c", "--configfile", action = "store", type = "string", dest = "configfile", help = "Read username and password from the specified file (Default: %s)" % DEFAULT_CONFIG_FILE, default = None)
+	cmdline_parser.add_option ("-n", "--hostname", action = "append", type = "string", dest = "hostnames", help = "Hostname to update (Might be used multiple times)", metavar = "HOSTNAME", default = None)
+	cmdline_parser.add_option ("-f", "--force", action = "store_true", dest = "force", help = "Force IP update, even if the hostname already points to the requeste IP address (Warning: might result in ban!)", default = False)
+	cmdline_parser.add_option ("-q", "--quiet", action = "store_true", dest = "quiet", help = "Only write to stdout in case of errors and log the rest to syslog (useful for crontab)", default = False)
+	(options, args) = cmdline_parser.parse_args ()
+
+	# Init logger
+	logger = Logger (options.quiet, "DNSFlash")
+
+	# Proceed, according to the action
+	if not options.no_update:
+		if options.configfile is not None:
+			# User has specified a configfile, use it
+			ret = actionUpdateIP (logger, config = options.configfile, force = options.force)
+		elif options.username is not None and options.password is not None:
+			# No configfile specified, but we have a username and password, so use these
+			ret = actionUpdateIP (logger, options.username, options.password, options.ip, options.hostnames, force = options.force)
+		elif os.path.isfile (DEFAULT_CONFIG_FILE):
+			# No configfile, nor username/password specified, but there is a system-wide configfile, so use this
+			ret = actionUpdateIP (logger, config = DEFAULT_CONFIG_FILE, force = options.force)
+		else:
+			# No configfile at all, nor username/password specified, so just get the local IP
+			ret = actionGetIP (logger)
+	else:
+			# User only wants the current IP
+			ret = actionGetIP (logger)
+	return ret
+
+# Go!
+if __name__ == "__main__":
+	sys.exit (main ())
---
+++ packages/DNSFlash/DNSFlash-0.3.0.tar.gz/DNSFlash-0.3.0/dnsomatic_api.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+
+###########################################################################
+#   Copyright (C) 2008 by SukkoPera                                       #
+#   sukkosoft@sukkology.net                                               #
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 2 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+#   This program is distributed in the hope that it will be useful,       #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#   GNU General Public License for more details.                          #
+#                                                                         #
+#   You should have received a copy of the GNU General Public License     #
+#   along with this program; if not, write to the                         #
+#   Free Software Foundation, Inc.,                                       #
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+###########################################################################
+
+import urllib
+import urllib2
+import socket
+
+# Maybe this could be turned to a base, service-independent class to be
+# inheritedor to implement other similar services.
+class DnsOMatic:
+	"""Uses the DNS-o-matic API to update the IP address.
+	Documentation about the DNS-O-Matic API is at https://www.dnsomatic.com/wiki/api.
+	Be aware that nscd might cache DNS resolutions, rendering this program useless!
+	"""
+	version = "0.3.0"
+	_updateUrl = "https://updates.dnsomatic.com"
+	_defaultUrl = "%s/nic/update" % _updateUrl
+	_defaultRealm = "DNSOMATIC"
+	_getIpUrl = "http://myip.dnsomatic.com"
+	_updateAllIPsKludge = "all.dnsomatic.com"
+	_defaultUserAgent = "SukkoSoft - dnsomatic_api.py - %s" % version
+
+	def __init__ (self, username = None, password = None, baseUrl = _defaultUrl, realm = _defaultRealm, userAgent = _defaultUserAgent):
+		self._username = username
+		self._password = password
+		self._baseUrl = baseUrl
+		self._realm = realm
+		self._userAgent = userAgent
+		self._setupHttp ()
+		
+	def _setupHttp (self):
+		"""Sets up all things needed to use Basic HTTP Authentication."""
+		if (1 and self._username is not None and self._username != '' and self._password is not None and self._password != ''):
+			# Create an OpenerDirector with support for Basic HTTP Authentication
+			passwdMan = urllib2.HTTPPasswordMgrWithDefaultRealm ()
+			auth_handler = urllib2.HTTPBasicAuthHandler (passwdMan)
+			auth_handler.add_password (None, self._updateUrl, self._username, self._password)
+			self._httpOpener = urllib2.build_opener (auth_handler)
+		else:
+			# Use the default handler
+			self._httpOpener = urllib2.build_opener ()
+
+	def getCurrentLocalIP (self):
+		"""Retrieve the current local IP through the DNS-O-Matic service."""
+		f = self._httpOpener.open (self._getIpUrl)
+		ip = "".join (f.readlines ()).strip ()
+		return ip
+	
+	# This could be a static method, but it is not supported in Python 2.3 (which comes with Slackware 10.0)
+	def getCurrentRemoteIP (self, hostname):
+		"""Retrieve the IP the hostname we want to update currently points to."""
+		try:
+			# This has been converted to use getaddrinfo() to avoid problems with DNS
+			# caching performed by nscd with gethostbyname() and other deprecated
+			# functions. I'm just not sure it will always work with that [0][4][0] :D.
+			#ip = socket.gethostbyname (hostname)
+			sa = socket.getaddrinfo (hostname, None)
+			ip = sa[0][4][0]
+		except socket.gaierror:
+			ip = None
+		return ip
+
+	def shouldUpdate (self, hostname, currentIP = None):
+		if hostname is None:
+			# No way to determine, so let's assume yes
+			ret = True
+		else:
+			if currentIP is None:
+				currentIP = self.getCurrentLocalIP ()
+			desiredIP = self.getCurrentRemoteIP (hostname)
+			if desiredIP is not None and currentIP == desiredIP:
+				ret = False
+			else:
+				ret = True
+		return ret
+
+	def updateSingle (self, hostname = None, ip = None, force = False):
+		"""Update a single address at the remote site."""
+		if ip is None:
+			# No IP to use for the update was specified. Get our
+			# current IP automatically and use it.
+			ip = self.getCurrentLocalIP ()
+		if force or self.shouldUpdate (hostname, ip):
+			# Either the IP to use for the update is different from
+			# the one the hostname currently points to, or the user
+			# wants to force the update, so let's go ahead.
+			params = {"myip": ip}
+			if hostname is not None:
+				# Only update the requested service
+				params["hostname"] = hostname
+			headers = {"User-Agent": self._userAgent}
+			enc_params = urllib.urlencode (params)
+			req = urllib2.Request (self._baseUrl, enc_params, headers)
+			try:
+				f = self._httpOpener.open (req)
+				result = "".join (f.readlines ()).strip ()
+				#print result
+				if result == "nohost":
+					# No such host
+					status = result
+				else:
+					status, ip = result.split ()
+			except urllib2.HTTPError:
+				status = "badauth"
+				ip = "0.0.0.0"
+			res = (status, ip)
+		else:
+			# Our local IP is already the same as the remote one
+			res = ("alreadyok", ip)
+		return res
+
+
+	def update (self, ip = None, hostnames = None, force = False):
+		"""Update several addresses to the same IP at the remote site."""
+		if hostnames is None or len (hostnames) == 0:
+			# Special case where we must update all the addresses
+			res = self.updateSingle (ip = ip, force = force)
+			ret = [res]
+		else:
+			# Update the requested addresses one by one
+			ret = []
+			for hostname in hostnames:
+				res = self.updateSingle (hostname, ip, force)
+				ret.append (res)
+		return ret
