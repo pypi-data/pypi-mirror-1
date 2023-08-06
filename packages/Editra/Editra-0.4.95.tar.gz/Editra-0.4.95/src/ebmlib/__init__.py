cccff250a393f84e21889469b8cbc97078df8e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:05 +0000

    Add package Editra/0.4.95/Editra-0.4.95.tar.gz

---
+++ packages/Editra/Editra-0.4.95.tar.gz/Editra-0.4.95/src/ebmlib/__init__.py
@@ -0,0 +1,33 @@
+###############################################################################
+# Name: __init__.py                                                           #
+# Purpose: Editra Buisness Model Library                                      #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2009 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
+
+"""
+Editra Buisness Model Library:
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__cvsid__ = "$Id: __init__.py 60840 2009-05-31 16:00:50Z CJP $"
+__revision__ = "$Revision: 60840 $"
+
+#-----------------------------------------------------------------------------#
+
+# Text Utils
+from searcheng import *
+from fchecker import *
+from fileutil import *
+from fileimpl import *
+
+from backupmgr import *
+
+# Storage Classes
+from histcache import *
+from clipboard import *
+
+# Misc
+from miscutil import *
