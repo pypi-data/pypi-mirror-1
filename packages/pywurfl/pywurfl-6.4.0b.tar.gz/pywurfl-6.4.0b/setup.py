d501d6eeaeced7103af7f41196e9a789846f42f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:53 +0000

    Add package pywurfl/6.4.0b/pywurfl-6.4.0b.tar.gz

---
+++ packages/pywurfl/pywurfl-6.4.0b.tar.gz/pywurfl-6.4.0b/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+from pywurfl import __version__, __url__, __license__
+from pywurfl import __doc__ as doc
+
+setup (name="pywurfl",
+       version=__version__,
+       author=["Armand Lynch", "Pau Aliagas"],
+       author_email=["lyncha@users.sourceforge.net", "pau@newtral.org"],
+       contact="Armand Lynch",
+       contact_email="lyncha@users.sourceforge.net",
+       license=__license__,
+       url=__url__,
+       packages=['pywurfl'],
+       scripts=['bin/wurfl2python.py'],
+       description=doc,
+       long_description=doc,
+       platforms="All",
+       classifiers=['Development Status :: 4 - Beta',
+                    'Environment :: Console',
+                    'Environment :: Web Environment',
+                    'Intended Audience :: Developers',
+                    'Intended Audience :: Telecommunications Industry',
+                    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+                    'Natural Language :: English',
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python',
+                    'Topic :: Database :: Front-Ends',
+                    'Topic :: Internet :: WAP',
+                    'Topic :: Software Development :: Libraries :: Python Modules',
+                    'Topic :: Utilities'
+                    ])
