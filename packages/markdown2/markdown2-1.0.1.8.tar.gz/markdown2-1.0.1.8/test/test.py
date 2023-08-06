41b2ed968a80dd433390d73932b1d230f9292065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:31 +0000

    Add package markdown2/1.0.1.8/markdown2-1.0.1.8.tar.gz

---
+++ packages/markdown2/markdown2-1.0.1.8.tar.gz/markdown2-1.0.1.8/test/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Copyright (c) 2007-2008 ActiveState Software Inc.
+# License: MIT (http://www.opensource.org/licenses/mit-license.php)
+
+"""The markdown2 test suite entry point."""
+
+import os
+from os.path import exists, join, abspath, dirname, normpath
+import sys
+import logging
+
+import testlib
+
+
+testdir_from_ns = {
+    None: os.curdir,
+}
+
+def setup():
+    top_dir = dirname(dirname(abspath(__file__)))
+    lib_dir = join(top_dir, "lib")
+    sys.path.insert(0, lib_dir)
+
+    pygments_dir = join(top_dir, "externals", "pygments")
+    if exists(pygments_dir):
+        sys.path.insert(0, pygments_dir)
+
+if __name__ == "__main__":
+    default_tags = ["-knownfailure"]
+    try:
+        import pygments
+    except ImportError:
+        # Skip pygments tests.
+        default_tags.append("-pygments")
+    retval = testlib.harness(testdir_from_ns=testdir_from_ns,
+                             setup_func=setup,
+                             default_tags=default_tags)
+    sys.exit(retval)
+
