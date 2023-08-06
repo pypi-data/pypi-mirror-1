0240655fb4a00ad361c8c3c80f53e67bbe07d054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:01 +0000

    Add package motmot.wxvideo/0.5.4/motmot.wxvideo-0.5.4.tar.gz

---
+++ packages/motmot.wxvideo/motmot.wxvideo-0.5.4.tar.gz/motmot.wxvideo-0.5.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+kws = {}
+if not int(os.getenv( 'DISABLE_INSTALL_REQUIRES','0' )):
+    kws['install_requires'] = [
+        'numpy>=1.0.4',
+        'PIL>=1.1.6',
+        'motmot.imops',
+        ]
+
+setup(name='motmot.wxvideo',
+      description='wx viewer of image sequences',
+      long_description = \
+"""Allows for display and resizing/rotation of images.
+
+This is a subpackage of the motmot family of digital image utilities.
+""",
+      packages = find_packages(),
+      namespace_packages = ['motmot'],
+      url='http://code.astraw.com/projects/motmot',
+      version='0.5.4',
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
+      license='BSD',
+      **kws)
