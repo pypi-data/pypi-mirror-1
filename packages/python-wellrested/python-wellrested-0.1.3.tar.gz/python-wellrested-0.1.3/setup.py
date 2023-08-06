0b80dce1be22223969773c24fa411248e3d6e8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:36 +0000

    Add package python-wellrested/0.1.3/python-wellrested-0.1.3.tar.gz

---
+++ packages/python-wellrested/python-wellrested-0.1.3.tar.gz/python-wellrested-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+version = '0.1.3'
+
+setup(
+    name='python-wellrested',
+    version=version,
+    description='A REST client providing a convenience wrapper over httplib2',
+    author='Nowell Strite',
+    author_email='nowell@strite.org',
+    url='http://github.com/nowells/python-wellrested/',
+    packages=['wellrested', 'wellrested.connections'],
+    install_requires=['httplib2'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    )
