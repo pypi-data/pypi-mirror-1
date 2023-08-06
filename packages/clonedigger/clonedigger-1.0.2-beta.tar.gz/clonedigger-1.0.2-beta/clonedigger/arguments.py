7661b2b840ac4ec848aab1bd62a8b8a1fbe85954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:42 +0000

    Add package clonedigger/1.0.2-beta/clonedigger-1.0.2-beta.tar.gz

---
+++ packages/clonedigger/clonedigger-1.0.2-beta.tar.gz/clonedigger-1.0.2-beta/clonedigger/arguments.py
@@ -0,0 +1,9 @@
+clustering_threshold = 10
+hashing_depth = 1
+clusterize_using_dcup = False
+report_unifiers = False
+print_time = True
+force = False  
+use_diff = True 
+clusterize_using_hash = False
+links_for_eclipse = False
