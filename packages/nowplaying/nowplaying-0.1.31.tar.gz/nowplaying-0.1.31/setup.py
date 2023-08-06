4f8d281f231b884eeebbceaf21e8ba947ae69bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:06 +0000

    Add package nowplaying/0.1.31/nowplaying-0.1.31.tar.gz

---
+++ packages/nowplaying/nowplaying-0.1.31.tar.gz/nowplaying-0.1.31/setup.py
@@ -0,0 +1,28 @@
+
+from distutils.core import setup
+
+import os
+def get_build():
+	path = "./.build"
+	if os.path.exists(path):
+		fp = open(path, "r")
+		build = eval(fp.read())
+		fp.close()
+	else:
+		build = 1
+	
+	fp = open(path, "w")
+	fp.write(str(build+1))
+	fp.close()
+	
+	return unicode(build)
+	
+setup(
+	name="nowplaying",
+	version = "0.1." + get_build(),
+	description = "Simple now-playing lookup for D-Bus enabled players",
+	license = "gpl3",
+	author = "Amr Hassan",
+	author_email = "amr.hassan@gmail.com",
+	packages = ["nowplaying", "nowplaying.players"],
+	)
