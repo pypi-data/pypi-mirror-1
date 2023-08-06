bed57d9798c02cf717c9c811582fb335358c3a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:58 +0000

    Add package smisk/1.0.1dev1/smisk-1.0.1dev1-py2.5-linux-i686.egg

---
+++ packages/smisk/smisk-1.0.1dev1-py2.5-linux-i686.egg/smisk/__init__.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+"Minimal FastCGI-based web application framework"
+
+from smisk.release import version   as __version__, author  as __author__, \
+                          email     as __email__,   license as __license__, \
+                          copyright as __copyright__
+
+from smisk.core import *
