6016f6971a90a5202312fdc51e07cb1b13ff78ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:20 +0000

    Add package Products.PyConBrasil/2.3.2/Products.PyConBrasil-2.3.2.tar.gz

---
+++ packages/Products.PyConBrasil/Products.PyConBrasil-2.3.2.tar.gz/Products.PyConBrasil-2.3.2/Products/PyConBrasil/Extensions/certificate.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Change these values to match your needs
+cert_file = '/home/apyb/certificado2009.png'
+font_file = '/home/apyb/verdana.ttf'
+
+def generate(name, role):
+
+    img = Image.open(cert_file)
+    draw = ImageDraw.Draw(img)
+
+    font = ImageFont.truetype(font_file, 128)
+
+    pos = 1750, 1620
+    draw.text(pos, name, font=font)
+
+    pos = 3500 ,2230
+    draw.text(pos, role, font=font)
+
+    return img.tostring()
+
+if __name__ == '__main__':
+    print generate('Dorneles Tremea', 'participante')
