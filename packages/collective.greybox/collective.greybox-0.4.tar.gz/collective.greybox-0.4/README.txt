c924db4f80c3add00b7399578c6529b6d0afd443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:40 +0000

    Add package collective.greybox/0.4/collective.greybox-0.4.tar.gz

---
+++ packages/collective.greybox/collective.greybox-0.4.tar.gz/collective.greybox-0.4/README.txt
@@ -0,0 +1,19 @@
+Simple python package for Plone, whiches makes the gre(a)ybox available for images
+
+INSTALL
+-------
+
+* watch docs/INSTALL.txt
+
+TESTED WITH
+-----------
+
+Plone 3.x
+Zope 2.10.5
+
+
+CONTACT
+-------
+
+info (a) 4teamwork.ch
+m.leimguber (a) 4teamwork.ch
---
+++ packages/collective.greybox/collective.greybox-0.4.tar.gz/collective.greybox-0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+
+version = '0.4'
+
+setup(name='collective.greybox',
+      version=version,
+      description="simple folder view using greybox",
+           long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='mathias leimgruber',
+      author_email='info@4teamwork.ch',
+      url='http://www.4teamwork.ch',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
