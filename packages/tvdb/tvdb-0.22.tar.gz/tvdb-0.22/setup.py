bfe29f922bdce25d1764b49da5782d5e47ce83c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:49 +0000

    Add package tvdb/0.22/tvdb-0.22.tar.gz

---
+++ packages/tvdb/tvdb-0.22.tar.gz/tvdb-0.22/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='tvdb',
+      version='0.22',
+      py_modules=['tvdb'],
+      description="Module for accessing the Web API at TheTVDB.com",
+      author="Andre P. LeBlanc",
+      author_email="andrepleblanc@gmail.com",
+      )
