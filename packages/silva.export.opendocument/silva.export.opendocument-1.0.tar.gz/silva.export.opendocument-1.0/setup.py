daec140afe5cc5a06f338d784842164d8d0f1efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:39 +0000

    Add package silva.export.opendocument/1.0/silva.export.opendocument-1.0.tar.gz

---
+++ packages/silva.export.opendocument/silva.export.opendocument-1.0.tar.gz/silva.export.opendocument-1.0/setup.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: setup.py 31013 2008-09-24 15:40:50Z sylvain $
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='silva.export.opendocument',
+      version=version,
+      description="Export Silva Document and Silva Publication to Open Document",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        ],
+      keywords='silva opendocument export',
+      author='Sylvain Viollon',
+      author_email='info@infrae.com',
+      url='https://svn.infrae.com/silva.export.opendocument/trunk',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva', 'silva.export'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'lxml',
+      ],
+      )
---
+++ packages/silva.export.opendocument/silva.export.opendocument-1.0.tar.gz/silva.export.opendocument-1.0/silva/export/opendocument/__init__.py
@@ -0,0 +1,3 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: __init__.py 28348 2008-04-15 10:02:21Z sylvain $
