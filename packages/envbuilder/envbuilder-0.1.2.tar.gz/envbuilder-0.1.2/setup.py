682a0f9ec717d3e17a958e6529545768b1345bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:57 +0000

    Add package envbuilder/0.1.2/envbuilder-0.1.2.tar.gz

---
+++ packages/envbuilder/envbuilder-0.1.2.tar.gz/envbuilder-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+readme = open('README.rst', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(
+    name='envbuilder',
+    version='0.1.2',
+    packages=find_packages(),
+    setup_requires=['nose'],
+    install_requires=['ConfigObj', 'argparse'],
+    zip_safe=False,
+    include_package_data=True,
+    entry_points = {
+        'console_scripts' : [
+            'envbuilder = envbuilder.run:main'
+            ]
+        },
+    description = "A package for automatic generation of virtualenvs",
+    long_description = README_TEXT
+    )
