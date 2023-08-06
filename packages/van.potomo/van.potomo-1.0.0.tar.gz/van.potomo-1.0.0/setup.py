8f1abef8b4c8e185179ede9f4d67831f5d2ead07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:11 +0000

    Add package van.potomo/1.0.0/van.potomo-1.0.0.tar.gz

---
+++ packages/van.potomo/van.potomo-1.0.0.tar.gz/van.potomo-1.0.0/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+long_description = (
+    '.. contents::\n\n'
+    + open(os.path.join('van', 'potomo', 'README.txt')).read()
+    + '\n\n'
+    + open(os.path.join('CHANGES.txt')).read()
+    )
+
+
+setup(name="van.potomo",
+      version='1.0.0',
+      license='ZPL 2.1',
+      url='http://pypi.python.org/pypi/van.potomo',
+      author_email='zope-dev@zope.org',
+      packages=find_packages(),
+      author="Vanguardistas LLC",
+      description="PO to MO build time compiler",
+      long_description=long_description,
+      namespace_packages=["van"],
+      install_requires = [
+          'setuptools',
+          'python-gettext'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Framework :: Setuptools Plugin',
+          'Programming Language :: Python',
+          'Development Status :: 4 - Beta',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development :: Build Tools',
+          ],
+
+      entry_points = """
+      [distutils.commands]
+      van_build_i18n = van.potomo:van_build_i18n
+      """,
+      include_package_data = True,
+      zip_safe = True,
+      )
