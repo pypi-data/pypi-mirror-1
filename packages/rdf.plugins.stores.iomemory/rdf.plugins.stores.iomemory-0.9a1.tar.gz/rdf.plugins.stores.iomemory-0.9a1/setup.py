5db6d3b31eae69ed803f2c7f8669e1b1be60d324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package rdf.plugins.stores.iomemory/0.9a1/rdf.plugins.stores.iomemory-0.9a1.tar.gz

---
+++ packages/rdf.plugins.stores.iomemory/rdf.plugins.stores.iomemory-0.9a1.tar.gz/rdf.plugins.stores.iomemory-0.9a1/setup.py
@@ -0,0 +1,37 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+import iomemory as package
+
+
+setup(
+    name = "rdf.plugins.stores.iomemory",
+    version = package.__version__,
+    description = package.__doc__.split("\n")[0],
+    long_description = package.__doc__,
+    author = "Daniel 'eikeon' Krech",
+    author_email = "eikeon@eikeon.com",
+    maintainer = "Daniel 'eikeon' Krech",
+    maintainer_email = "eikeon@eikeon.com",
+    url = "http://rdflib.net/",
+    license = "http://rdflib.net/latest/LICENSE",
+    platforms = ["any"],
+    classifiers = ["Programming Language :: Python",
+                   "License :: OSI Approved :: BSD License",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Operating System :: OS Independent",
+                   "Natural Language :: English",
+                   ],
+
+
+    py_modules = ["iomemory"],
+
+    install_requires = ['rdf>=0.9a4'],
+
+    tests_require = ["nose==0.10.1"],
+
+    test_suite = 'nose.collector',
+
+    )
