765b7114e1d6896e7fddb25bc1ce4d7411068c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:09 +0000

    Add package tiddlywebplugins.static/0.9/tiddlywebplugins.static-0.9.tar.gz

---
+++ packages/tiddlywebplugins.static/tiddlywebplugins.static-0.9.tar.gz/tiddlywebplugins.static-0.9/setup.py
@@ -0,0 +1,20 @@
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.9'
+
+setup(
+        namespace_packages = ['tiddlywebplugins'],
+        name = 'tiddlywebplugins.static',
+        version = VERSION,
+        description = 'A TiddlyWeb plugin that handles delivery of static files over HTTP.',
+        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        author = 'Chris Dent',
+        url = 'http://pypi.python.org/pypi/tiddlywebplugins.static',
+        packages = find_packages(exclude=['test']),
+        author_email = 'cdent@peermore.com',
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['setuptools', 'tiddlyweb'],
+        zip_safe = False,
+        )
