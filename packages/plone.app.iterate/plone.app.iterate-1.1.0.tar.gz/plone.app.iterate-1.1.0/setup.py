4c308895e23c6466ab677a18a2d9e0e133254aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:11 +0000

    Add package plone.app.iterate/1.1.0/plone.app.iterate-1.1.0.tar.gz

---
+++ packages/plone.app.iterate/plone.app.iterate-1.1.0.tar.gz/plone.app.iterate-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.0'
+
+setup(name='plone.app.iterate',
+      version=version,
+      description="check-out/check-in staging for Plone",
+      long_description=\
+          open(os.path.join("plone", "app", "iterate", "README.txt")).read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Kapil Thangavelu',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.app.iterate',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          "plone.locking",
+      ],
+      )
