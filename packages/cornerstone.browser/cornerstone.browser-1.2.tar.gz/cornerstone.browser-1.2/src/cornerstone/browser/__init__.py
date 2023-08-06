577e6332eab389ed41079c1fb515898ba3a9f386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:58 +0000

    Add package cornerstone.browser/1.2/cornerstone.browser-1.2.tar.gz

---
+++ packages/cornerstone.browser/cornerstone.browser-1.2.tar.gz/cornerstone.browser-1.2/src/cornerstone/browser/__init__.py
@@ -0,0 +1,18 @@
+from base import RequestMixin
+from base import RequestTool
+from base import XBrowserView
+from base import AjaxBrowserView
+
+from menu import MenuBase
+from menu import MenuItemBase
+
+from renderer import RendererBase
+
+from tmpl import SelectionVocabBase
+from tmpl import HTMLRendererMixin
+
+from Products.CMFCore import DirectoryView
+DirectoryView.registerDirectory('cornerstone_browser', globals())
+
+def initialize(context):
+    pass
