df218b49ed778171f8e3b45129512a467e31cb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:55 +0000

    Add package zope.pluggableauth/1.0.1/zope.pluggableauth-1.0.1.tar.gz

---
+++ packages/zope.pluggableauth/zope.pluggableauth-1.0.1.tar.gz/zope.pluggableauth-1.0.1/src/zope/pluggableauth/__init__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Pluggable Authentication Utility
+
+$Id: __init__.py 108413 2010-01-23 23:32:38Z trollfot $
+"""
+
+from zope.pluggableauth import interfaces
+from zope.pluggableauth.authentication import PluggableAuthentication
