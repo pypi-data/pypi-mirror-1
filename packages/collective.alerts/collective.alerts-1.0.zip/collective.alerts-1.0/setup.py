4da3fa2d2d4b861af6c4f4b5597bc10a6012747d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:15 +0000

    Add package collective.alerts/1.0/collective.alerts-1.0.zip

---
+++ packages/collective.alerts/collective.alerts-1.0.zip/collective.alerts-1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.alerts',
+      version=version,
+      description="Implements Cory LaViska's JQuery Alerts feature for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone jquery alerts',
+      author='JC Brand, Cory LaViska',
+      author_email='jc@opkode.com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
