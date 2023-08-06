5e3450067a6eb8ee093ffbdb85288690a1a88db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:31 +0000

    Add package chameleon.core/1.0b24/chameleon.core-1.0b24.tar.gz

---
+++ packages/chameleon.core/chameleon.core-1.0b24.tar.gz/chameleon.core-1.0b24/src/chameleon/core/generation.py
@@ -0,0 +1,258 @@
+from zope.i18n import interpolate
+from zope.i18n import translate
+from zope.i18nmessageid import Message
+
+import utils
+import types
+
+function_template = """\
+def bind():
+%(imports)s
+\tdef %(name)s(%(arguments)s):
+%(source)s
+%(return)s
+\treturn %(name)s
+"""
+
+def indent_block(text, level=2):
+    return "\n".join(("\t" * level + s for s in text.split('\n')))
+                     
+def function_wrap(name, args, imports, source, return_expr=""):
+    format_values = {
+        'name': name,
+        'imports': indent_block('\n'.join(imports), 1),
+        'arguments': ', '.join(args),
+        'source': indent_block(source),
+        'return': indent_block("return %s" % return_expr)}
+    
+    return function_template % format_values
+
+class Marker(object):
+    def __nonzero__(self):
+        return False
+
+marker = Marker()
+
+def fast_translate(msgid, domain=None, mapping=None, context=None,
+                   target_language=None, default=None):
+    """This translation function does not attempt to negotiate a
+    language if ``None`` is passed."""
+
+    if msgid is None:
+        return
+    
+    if target_language is not None:
+        return translate(
+            msgid, domain=domain, mapping=mapping, context=context,
+            target_language=target_language, default=default)
+    
+    if isinstance(msgid, Message):
+        default = msgid.default
+        mapping = msgid.mapping
+
+    if default is None:
+        default = unicode(msgid)
+
+    if not isinstance(default, basestring):
+        return default
+
+    return interpolate(default, mapping)
+
+def initialize_tal():
+    return ({}, utils.repeatdict())
+
+def initialize_default():
+    return utils.default()
+
+def initialize_stream():
+    out = BufferIO()
+    return (out, out.write)
+
+initialize_scope = utils.econtext
+    
+class BufferIO(list):
+    write = list.append
+
+    def getvalue(self):
+        return ''.join(self)
+        
+class CodeIO(BufferIO):
+    """Stream buffer suitable for writing Python-code. This class is
+    used internally by the compiler to manage variable scopes, source
+    annotations and temporary variables."""
+
+    t_prefix = '_tmp'
+    v_prefix = '_tmpv'
+
+    annotation = None
+    
+    def __init__(self, symbols=None, encoding=None,
+                 indentation=0, indentation_string="\t"):
+        BufferIO.__init__(self)
+        self.symbols = symbols or object
+        self.symbol_mapping = {}
+        self.encoding = encoding
+        self.indentation = indentation
+        self.indentation_string = indentation_string
+        self.queue = []
+        self.scope = [set()]
+        self.annotations = {}
+        self._variables = {}
+        self.t_counter = 0
+        self.v_counter = 0
+        self.l_counter = 0
+
+    def new_var(self):
+        self.v_counter += 1
+        return "%s%d" % (self.v_prefix, self.v_counter)
+        
+    def save(self):
+        self.t_counter += 1
+        return "%s%d" % (self.t_prefix, self.t_counter)
+
+    def restore(self):
+        var = "%s%d" % (self.t_prefix, self.t_counter)
+        self.t_counter -= 1
+        return var
+
+    def indent(self, amount=1):
+        if amount > 0:
+            self.flush()
+            self.indentation += amount
+
+    def outdent(self, amount=1):
+        if amount > 0:
+            self.flush()
+            self.indentation -= amount
+
+    def annotate(self, annotation):
+        if annotation.label is not None:
+            annotation = annotation.label
+
+        # make sure the annotation is a base string type
+        if isinstance(annotation, unicode):
+            annotation = unicode(annotation)
+        else:
+            annotation = str(annotation)
+
+        # encode unicode string if required
+        if isinstance(annotation, unicode) and self.encoding:
+            annotation = annotation.encode(self.encoding)
+            
+        self.annotation = self.annotations[self.l_counter] = annotation
+
+    def out(self, value):
+        if isinstance(value, types.expression):
+            assert isinstance(value, types.value)
+        elif isinstance(value, unicode) and self.encoding:
+            value = value.encode(self.encoding)
+        self.queue.append(value)
+            
+    def flush(self):
+        if self.queue:
+            # optimize queue to merge adjacent strings
+            optimized = []
+
+            current = None
+            while self.queue:
+                next = self.queue.pop(0)
+
+                if type(current) in (str, unicode) and \
+                   type(next) in (str, unicode):
+                    current = current + next
+                    continue
+                elif current is not None:
+                    optimized.append(current)
+
+                current = next
+            else:
+                optimized.append(current)            
+
+            assert len(self.queue) == 0
+            
+            self.write(
+                "%s(%s)" %
+                (self.symbols.write, " + ".join(
+                isinstance(expression, types.value) and expression or \
+                repr(expression) for expression in optimized)))
+
+    def write(self, string):
+        if isinstance(string, unicode) and self.encoding:
+            string = string.encode(self.encoding)
+
+        self.l_counter += len(string.split('\n'))-1
+        self.flush()
+        
+        indent = self.indentation_string * self.indentation
+
+        # if a source code annotation is set, write it as a
+        # triple-quoted string prior to the source line
+        if self.annotation:
+            if isinstance(self.annotation, unicode) and self.encoding:
+                self.annotation = self.annotation.encode(self.encoding)
+            BufferIO.write(
+                self, "%s%s\n" % (indent, repr(self.annotation)))
+            self.annotation = None
+            
+        BufferIO.write(self, indent + string + '\n')
+
+    def getvalue(self):
+        self.flush()
+        return BufferIO.getvalue(self)
+
+    def escape(self, variable):
+        self.write("if '&' in %s:" % variable)
+        self.indent()
+        self.write("%s = %s.replace('&', '&amp;')" % (variable, variable))
+        self.outdent()
+        self.write("if '<' in %s:" % variable)
+        self.indent()
+        self.write("%s = %s.replace('<', '&lt;')" % (variable, variable))
+        self.outdent()
+        self.write("if '>' in %s:" % variable)
+        self.indent()
+        self.write("%s = %s.replace('>', '&gt;')" % (variable, variable))
+        self.outdent()
+        self.write("if '\"' in %s:" % variable)
+        self.indent()
+        self.write("%s = %s.replace('\"', '&quot;')" % (variable, variable))
+        self.outdent()
+
+    def convert_to_string(self, variable):
+        self.write("if isinstance(%s, unicode):" % variable)
+        self.indent()
+        self.write("%s = %s.encode('%s')" % (variable, variable, self.encoding))
+        self.outdent()
+
+    def coerce_to_string(self, variable):
+        self.write("if isinstance(%s, unicode):" % variable)
+        self.indent()
+        self.write("%s = %s.encode('%s')" % (variable, variable, self.encoding))
+        self.outdent()
+        self.write("elif not isinstance(%s, str):" % variable)
+        self.indent()
+        self.write("%s = str(%s)" % (variable, variable))
+        self.outdent()
+
+    def coerce_to_unicode(self, variable):
+        """Coerces variable to unicode (actually ``str``, because it's
+        much faster)."""
+        
+        self.write("if not isinstance(%s, basestring):" % variable)
+        self.indent()
+        self.write("%s = str(%s)" % (variable, variable))
+        self.outdent()
+        
+    def begin(self, clauses):
+        if isinstance(clauses, (list, tuple)):
+            for clause in clauses:
+                self.begin(clause)
+        else:
+            clauses.begin(self)
+                
+    def end(self, clauses):
+        if isinstance(clauses, (list, tuple)):
+            for clause in reversed(clauses):
+                self.end(clause)
+        else:
+            clauses.end(self)
---
+++ packages/chameleon.core/chameleon.core-1.0b24.tar.gz/chameleon.core-1.0b24/src/chameleon/core/utils.py
@@ -0,0 +1,466 @@
+from zope import interface
+
+import sys
+import interfaces
+import htmlentitydefs
+import re
+
+types = sys.modules['types']
+
+from UserDict import UserDict
+
+# check if we're able to coerce unicode to str
+try:
+    str(u'La Pe\xf1a')
+    unicode_required_flag = False
+except UnicodeEncodeError:
+    unicode_required_flag = True
+
+def coerces_gracefully(encoding):
+    if encoding != sys.getdefaultencoding() and unicode_required_flag:
+        return False
+    return True
+
+entities = "".join((
+    '<!ENTITY %s "&#%s;">' % (name, text) for (name, text) in \
+    htmlentitydefs.name2codepoint.items()))
+
+re_annotation = re.compile(r'^\s*u?[\'"](.*)[\'"]$')
+
+s_counter = 0
+marker = object()
+
+def handler(key=None):
+    def decorate(f):
+        def g(node):
+            if key is None:
+                return f(node, None)
+            return f(node, node.get(key))
+        g.__ns__ = key
+        return g
+    return decorate
+
+def attribute(tokens, factory=None, default=None, encoding=None):
+    def get(self):
+        for token in isinstance(tokens, tuple) and tokens or (tokens,):
+            value = self.attrib.get(token)
+            if value is not None:
+                if encoding or self.stream.encoding:
+                    value = value.encode(encoding or self.stream.encoding)
+                if factory is None:
+                    return value
+                f = factory(self.translator)
+                return f(value)
+
+        if default is not None:
+            return default
+    
+    return property(get)
+
+def escape(string, quote=None, encoding=None):
+    if not isinstance(string, unicode) and encoding:
+        string = string.decode(encoding)
+    else:
+        encoding = None
+
+    table = htmlentitydefs.codepoint2name
+    def get(char):
+        key = ord(char)
+        if key in table:
+            return '&%s;' % table[key]
+        return char
+    
+    string = "".join(map(get, string))
+
+    if quote is not None:
+        string = string.replace(quote, '\\'+quote)
+
+    if encoding:
+        string = string.encode(encoding)
+        
+    return string
+
+re_amp = re.compile(r'&(?![A-Za-z]+;)')
+
+def htmlescape(text):
+    while text:
+        m = re_amp.search(text)
+        if m is None:
+            break
+        text = text[:m.start()] + text[m.start():].replace('&', '&amp;', 1)
+        
+    return text.replace('<', '&lt;').replace('>', '&gt;')
+
+re_normalize = re.compile(r'(^[0-9]|\b[^A-Za-z])')
+def normalize_slot_name(name):
+    return re_normalize.sub('_', name)
+    
+def serialize(element, encoding=None, omit=False):
+    return "".join(serialize_element(element, encoding, omit=omit))
+
+def serialize_element(element, encoding, omit):
+    try:
+        name = element.tag.split('}')[-1]
+    except AttributeError:
+        yield htmlescape(element.text); return
+
+    if omit is False:
+        # tag opening
+        yield "<%s" % name
+
+        # attributes
+        for key, value in element.attrib.items():
+            yield ' %s="%s"' % (key, escape('"', encoding=encoding))
+
+        # elements with no text which have no children are self-closing.
+        if element.text is None and len(element) == 0:
+            yield ' />'; return
+
+        yield '>'
+            
+    if element.text is not None:
+        yield htmlescape(element.text)
+
+    for child in element:
+        for string in serialize_element(child, encoding, False):
+            yield string
+
+    if omit is False:
+        yield '</%s>' % name
+
+class default(object):
+    __slots__ = 'value',
+
+    def __init__(self):
+        self.value = None
+        
+    def __setitem__(self, key, value):
+        self.value = value
+
+    def __html__(self):
+        return self.value
+        
+class econtext(dict):
+    """Dynamic scope dictionary which is compatible with the
+    `econtext` of ZPT."""
+    
+    set_local = setLocal = dict.__setitem__
+    set_global = setGlobal = dict.__setitem__
+
+    @property
+    def vars(self):
+        return self
+    
+class scope(list):
+    def __init__(self, *args):
+        global s_counter
+        self.hash = s_counter
+        s_counter += 1
+
+    def __hash__(self):
+        return self.hash
+
+class emptydict(dict):
+    def __setitem__(self, key, value):
+        raise TypeError("Read-only dictionary does not support assignment.")
+    
+class repeatitem(object):
+    interface.implements(interfaces.ITALESIterator)
+    
+    def __init__(self, iterator, length):
+        self.length = length
+        self.iterator = iterator
+        
+    @property
+    def index(self):
+        try:
+            length = len(self.iterator)
+        except TypeError:
+            length = self.iterator.__length_hint__()
+        except:
+            raise TypeError("Unable to determine length.")
+
+        try:
+            return self.length - length - 1
+        except TypeError:
+            return None
+            
+    @property
+    def start(self):
+        return self.index == 0
+
+    @property
+    def end(self):
+        return self.index == self.length - 1
+
+    def number(self):
+        return self.index + 1
+
+    def odd(self):
+        return bool(self.index % 2)
+
+    def even(self):
+        return not self.odd()
+
+class repeatdict(dict):
+    def insert(self, key, iterable):
+        """We realize the iterable and extract a new iterable from it;
+        this allows us to keep track of the iteration and make
+        available the repeat dictionary."""
+
+        if not isinstance(iterable, (tuple, list)):
+            try:
+                if iterable is None:
+                    iterable = ()
+                else:
+                    iterable = tuple(iterable)
+            except TypeError:
+                # The message below to the TypeError is the Python
+                # 2.5-style exception message. Python 2.4.X also
+                # raises a TypeError, but with a different message.
+                # ("TypeError: iteration over non-sequence").  The
+                # Python 2.5 error message is more helpful.  We
+                # construct the 2.5-style message explicitly here so
+                # that both Python 2.4.X and Python 2.5+ will raise
+                # the same error.  This makes writing the tests eaiser
+                # and makes the output easier to understand.
+                raise TypeError("%r object is not iterable" %
+                                type(iterable).__name__)
+            
+        iterator = iter(iterable)
+        length = len(iterable)
+        self[key] = (iterator, length)
+        
+        return iterator
+        
+    def __getitem__(self, key):
+        value, length = dict.__getitem__(self, key)
+
+        if not isinstance(value, repeatitem):
+            value = repeatitem(value, length)
+            self[key] = (value, length)
+            
+        return value
+
+    def get(self, key, default):
+        if key not in self:
+            return default
+        return self[key]
+        
+class odict(UserDict):
+    def __init__(self, dict = None):
+        self._keys = []
+        UserDict.__init__(self, dict)
+
+    def __delitem__(self, key):
+        UserDict.__delitem__(self, key)
+        self._keys.remove(key)
+
+    def __setitem__(self, key, item):
+        """Case insensitive set item."""
+        
+        keys = tuple(key.lower() for key in self._keys)
+        _key = key.lower()
+        if _key in keys:
+            for k in self._keys:
+                if k.lower() == _key:
+                    self._keys.remove(k)
+                    key = k
+                    break
+                
+        UserDict.__setitem__(self, key, item)
+        self._keys.append(key)
+
+    def clear(self):
+        UserDict.clear(self)
+        self._keys = []
+
+    def copy(self):
+        dict = UserDict.copy(self)
+        dict._keys = self._keys[:]
+        return dict
+
+    def items(self):
+        return zip(self._keys, self.values())
+
+    def keys(self):
+        return self._keys
+
+    def popitem(self):
+        try:
+            key = self._keys[-1]
+        except IndexError:
+            raise KeyError('dictionary is empty')
+
+        val = self[key]
+        del self[key]
+
+        return (key, val)
+
+    def setdefault(self, key, failobj = None):
+        UserDict.setdefault(self, key, failobj)
+        if key not in self._keys: self._keys.append(key)
+
+    def update(self, dict):
+        UserDict.update(self, dict)
+        for key in dict.keys():
+            if key not in self._keys: self._keys.append(key)
+
+    def values(self):
+        return map(self.get, self._keys)
+    
+def get_attributes_from_namespace(element, namespace):
+    if namespace is None:
+        attrs = dict([
+            (name, value) for (name, value) in element.attrib.items() \
+            if '{' not in name])
+    else:
+        attrs = dict([
+            (name, value) for (name, value) in element.attrib.items() \
+            if name.startswith('{%s}' % namespace)])
+
+    return attrs
+
+def get_namespace(element):
+    if '}' in element.tag:
+        return element.tag.split('}')[0][1:]
+    return element.nsmap.get(None)
+
+def format_kwargs(kwargs):
+    items = []
+    for name, value in kwargs.items():
+        if isinstance(value, (int, float, basestring)):
+            items.append((name, value))
+        elif isinstance(value, dict):
+            items.append((name, '{...} (%d)' % len(value)))
+        else:
+            items.append((name,
+                "<%s %s at %s>" % (
+                    type(value).__name__,
+                    getattr(value, '__name__', "-"),
+                    hex(id(value)))))
+    
+    return ["%s: %s" % item for item in items]
+
+def raise_exc(exc):
+    raise exc
+
+def raise_template_exception(source, description, kwargs, exc_info):
+    """Re-raise exception raised while calling ``template``, given by
+    the ``exc_info`` tuple (see ``sys.exc_info``)."""
+
+    kwargs = kwargs.copy()
+    
+    # merge dynamic scope
+    kwargs.update(kwargs.pop('econtext', {}))
+
+    # omit keyword arguments that begin with an underscore; these are
+    # used internally be the template engine and should not be exposed
+    map(kwargs.__delitem__, [k for k in kwargs if k.startswith('_')])
+    
+    # format keyword arguments; consecutive arguments are indented for
+    # readability
+    try:
+        formatted_arguments = format_kwargs(kwargs)
+    except:
+        # the ``pprint.pformat`` method calls the representation
+        # method of the arguments; this may fail and since we're
+        # already in an exception handler, there's no point in
+        # pursuing this further
+        formatted_arguments = ()
+    
+    for index, string in enumerate(formatted_arguments[1:]):
+        formatted_arguments[index+1] = " "*15 + string
+
+    # extract line number from traceback object
+    cls, exc, tb = exc_info
+    try:
+        lineno = tb.tb_next.tb_lineno-1
+    except AttributeError:
+        lineno = 1
+        
+    # locate source code annotation (these are available from
+    # the template source as comments)
+    if source:
+        source = source.split('\n')
+        for i in reversed(range(lineno)):
+            m = re_annotation.match(source[i])
+            if m is not None:
+                annotation = m.group(1)
+                break
+        else:
+            annotation = ""
+
+        error_msg = "Caught exception rendering template."
+    else:
+        annotation = ""
+        error_msg = "Caught exception processing template: %s.\n" % description
+
+    __dict__ = exc.__dict__
+    __name__ = cls.__name__
+    
+    error_string = str(exc)
+
+    if issubclass(cls, Exception):
+        class RuntimeError(cls):
+            def __init__(self, *args, **kwargs):
+                pass
+            
+            def __str__(self):
+                return "%s\n%s\n%s: %s" % (
+                    error_msg, str(self.__debug_info__),
+                    __name__, error_string)
+
+        if isinstance(cls, types.TypeType):
+            exc = RuntimeError.__new__(RuntimeError)
+            exc.__dict__.update(__dict__)
+        else:
+            cls = RuntimeError
+
+        # add/extend debug info
+        try:
+            debug_info = RuntimeError.__debug_info__
+        except AttributeError:
+            debug_info = RuntimeError.__debug_info__ = DebugInfo()
+        debug_info.append(annotation, description, formatted_arguments)
+
+    raise cls, exc, tb
+
+class DebugInfo(object):
+    def __init__(self):
+        self.info = []
+        
+    def append(self, *args):
+        self.info.append(args)
+
+    def __str__(self):
+        error_format = ' - Expression: "%s"\n' \
+                       ' - Instance:   %s\n' \
+                       ' - Arguments:  %s\n'
+
+        annotation, description, formatted_arguments = self.info[-1]
+        return error_format % (
+            annotation, description, "\n".join(formatted_arguments))
+
+# importing these here to avoid circular reference
+
+import config
+
+def xhtml_attr(name):
+    return "{%s}%s" % (config.XHTML_NS, name)
+
+def tal_attr(name):
+    return "{%s}%s" % (config.TAL_NS, name)
+
+def meta_attr(name):
+    return "{%s}%s" % (config.META_NS, name)
+
+def metal_attr(name):
+    return "{%s}%s" % (config.METAL_NS, name)
+
+def i18n_attr(name):
+    return "{%s}%s" % (config.I18N_NS, name)
+
+def py_attr(name):
+    return "{%s}%s" % (config.PY_NS, name)
+
