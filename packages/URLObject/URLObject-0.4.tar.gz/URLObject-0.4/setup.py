c4d72097e5cd717bd87a45f93ef26d7782f6fb6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:17 +0000

    Add package URLObject/0.4/URLObject-0.4.tar.gz

---
+++ packages/URLObject/URLObject-0.4.tar.gz/URLObject-0.4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='URLObject',
+    version='0.4',
+    description='A utility class for manipulating URLs.',
+    author='Zachary Voase',
+    author_email='zacharyvoase@me.com',
+    url='http://bitbucket.org/zacharyvoase/urlobject/',
+    py_modules=['urlobject'],
+)
