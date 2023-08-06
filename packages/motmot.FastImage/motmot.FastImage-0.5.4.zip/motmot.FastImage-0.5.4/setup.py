68b9bb7eb8c9d1cfd422fdf2a0a15c1c267ad631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:00 +0000

    Add package motmot.FastImage/0.5.4/motmot.FastImage-0.5.4.zip

---
+++ packages/motmot.FastImage/motmot.FastImage-0.5.4.zip/motmot.FastImage-0.5.4/setup.py
@@ -0,0 +1,25 @@
+import os, sys
+from setuptools import setup, find_packages, Extension
+
+kws = {}
+if not int(os.getenv( 'DISABLE_INSTALL_REQUIRES','0' )):
+    kws['install_requires'] = ['numpy',
+                               ]
+
+setup(name="motmot.FastImage",
+      author="Andrew Straw",
+      author_email="strawman@astraw.com",
+      description="Pythonic API for the framewave SIMD library",
+      url='http://code.astraw.com/projects/motmot',
+      license="BSD",
+      version='0.5.4',
+      namespace_packages=['motmot'],
+      packages = find_packages(),
+      ext_modules=[Extension(name="motmot.FastImage.FastImage",
+                             sources=['src/FastImage.pyx',
+                                      'src/fic.c','src/fic_sobel.c'],
+                             libraries=['fwBase','fwImage'],
+                             ),
+                   ],
+      **kws)
+
