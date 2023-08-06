0a7104b68946cb8ebdb787945ef108e7ffab6f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package ore.yui/0.4.0/ore.yui-0.4.0-py2.5.egg

---
+++ packages/ore.yui/ore.yui-0.4.0-py2.5.egg/ore/yui/js.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (c) 2008 Kapil Thangavelu
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from zope.viewlet.viewlet import JavaScriptViewlet
+
+YahooBase = JavaScriptViewlet('yui/yahoo/yahoo.js')
+YahooEvent = JavaScriptViewlet('yui/event/event.js')
+YahooConnection = JavaScriptViewlet('yui/connection/connection.js')
+YahooDom = JavaScriptViewlet('yui/dom/dom.js')
+YahooLogger = JavaScriptViewlet('yui/logger/logger-min.js')
+YahooElement = JavaScriptViewlet('yui/element/element-beta.js')
+YahooDataSource = JavaScriptViewlet('yui/datasource/datasource-beta.js')
+YahooDataTable = JavaScriptViewlet('yui/datatable/datatable-beta.js')
+YahooTree = JavaScriptViewlet('yui/treeview/treeview.js')
