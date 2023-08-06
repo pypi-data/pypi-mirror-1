1cab3720c6f877e94302ef32de2f1bf59d060c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:48 +0000

    Add package PyUtilib/2.0.1/PyUtilib-2.0.1-py2.6.egg

---
+++ packages/PyUtilib/PyUtilib-2.0.1-py2.6.egg/pyutilib/plugin/loader/__init__.py
@@ -0,0 +1,17 @@
+#  _________________________________________________________________________
+#
+#  PyUtilib: A Python utility library.
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  For more information, see the PyUtilib README.txt file.
+#  _________________________________________________________________________
+
+"""
+The pyutilib.plugin.loader package includes plugins for loading
+plugins and services.
+"""
+
+from plugin_importLoader import *
+from plugin_eggLoader import *
---
+++ packages/PyUtilib/PyUtilib-2.0.1-py2.6.egg/pyutilib/tpl/__init__.py
@@ -0,0 +1,1 @@
+# TPL top-level package
