1793ec62343c4917f79496c60ce2ae784c74c8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:54 +0000

    Add package zc.buildout/1.1.2/zc.buildout-1.1.2.tar.gz

---
+++ packages/zc.buildout/zc.buildout-1.1.2.tar.gz/zc.buildout-1.1.2/dev.py
@@ -0,0 +1,66 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Bootstrap the buildout project itself.
+
+This is different from a normal boostrapping process because the
+buildout egg itself is installed as a develop egg.
+
+$Id: dev.py 90479 2008-08-27 22:44:48Z georgyberdyshev $
+"""
+
+import os, sys, urllib2
+
+is_jython = sys.platform.startswith('java')
+
+if is_jython:
+    import subprocess
+
+for d in 'eggs', 'develop-eggs', 'bin':
+    if not os.path.exists(d):
+        os.mkdir(d)
+
+try:
+    import pkg_resources
+except ImportError:
+    ez = {}
+    exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
+                         ).read() in ez
+    ez['use_setuptools'](to_dir='eggs', download_delay=0)
+
+    import pkg_resources
+
+if is_jython:
+    subprocess.Popen([sys.executable] + ['setup.py', '-q', 'develop', '-m', '-x',
+                        '-d', 'develop-eggs'], 
+                        env = {'PYTHONPATH': os.path.dirname(pkg_resources.__file__)}).wait()
+else:
+    os.spawnle(os.P_WAIT, sys.executable, sys.executable, 'setup.py',
+               '-q', 'develop', '-m', '-x', '-d', 'develop-eggs',
+               {'PYTHONPATH': os.path.dirname(pkg_resources.__file__)},
+               )
+
+pkg_resources.working_set.add_entry('src')
+
+import zc.buildout.easy_install
+zc.buildout.easy_install.scripts(
+    ['zc.buildout'], pkg_resources.working_set , sys.executable, 'bin')
+
+bin_buildout = os.path.join('bin', 'buildout')
+
+if is_jython:
+    # Jython needs the script to be called twice via sys.executable
+    assert subprocess.Popen([sys.executable] + [bin_buildout]).wait() == 0
+    sys.exit(subprocess.Popen([sys.executable] + [bin_buildout]).wait())
+else:
+    sys.exit(os.spawnl(os.P_WAIT, bin_buildout, bin_buildout))
---
+++ packages/zc.buildout/zc.buildout-1.1.2.tar.gz/zc.buildout-1.1.2/setup.py
@@ -0,0 +1,82 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+version = "1.1.2"
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'zc', 'buildout', 'buildout.txt')
+        + '\n' +
+        read('src', 'zc', 'buildout', 'unzip.txt')
+        + '\n' +
+        read('src', 'zc', 'buildout', 'repeatable.txt')
+        + '\n' +
+        read('src', 'zc', 'buildout', 'downloadcache.txt')
+        + '\n' +
+        read('src', 'zc', 'buildout', 'setup.txt')
+        + '\n' +
+        read('src', 'zc', 'buildout', 'update.txt')
+        + '\n' +
+        read('src', 'zc', 'buildout', 'debugging.txt')
+        + '\n' +
+        read('src', 'zc', 'buildout', 'testing.txt')
+        + '\n' +
+        read('src', 'zc', 'buildout', 'easy_install.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        )
+
+open('doc.txt', 'w').write(long_description)
+
+name = "zc.buildout"
+setup(
+    name = name,
+    version = version,
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "System for managing development buildouts",
+    long_description=long_description,
+    license = "ZPL 2.1",
+    keywords = "development build",
+    url='http://www.python.org/pypi/zc.buildout',
+
+    data_files = [('.', ['README.txt'])],
+    packages = ['zc', 'zc.buildout'],
+    package_dir = {'': 'src'},
+    namespace_packages = ['zc'],
+    install_requires = 'setuptools',
+    include_package_data = True,
+    entry_points = {'console_scripts':
+                    ['buildout = %s.buildout:main' % name]}, 
+    zip_safe=False,
+    classifiers = [
+       'Intended Audience :: Developers',
+       'License :: OSI Approved :: Zope Public License',
+       'Topic :: Software Development :: Build Tools',
+       'Topic :: Software Development :: Libraries :: Python Modules',
+       ],
+    )
