03aaa30849a7e114f08f703f4a8dca6021f8fd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:48 +0000

    Add package collective.classifieds/0.6/collective.classifieds-0.6-py2.4.egg

---
+++ packages/collective.classifieds/collective.classifieds-0.6-py2.4.egg/collective/classifieds/content/interfaces.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+
+
+class IClassifiedsCategory(Interface):
+    """Marker interface for .ClassifiedsCategory.ClassifiedsCategory
+    """
+
+class IClassifieds(Interface):
+    """Marker interface for .Classifieds.Classifieds
+    """
+
+class IClassified(Interface):
+    """Marker interface for .Classified.Classified
+    """
+
