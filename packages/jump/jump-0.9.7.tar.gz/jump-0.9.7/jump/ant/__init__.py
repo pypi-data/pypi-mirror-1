4cd0ffb79567f1352f098c4d8d90a4d9a46f0bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:45 +0000

    Add package jump/0.9.7/jump-0.9.7.tar.gz

---
+++ packages/jump/jump-0.9.7.tar.gz/jump-0.9.7/jump/ant/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Olli Wang (olliwang@ollix.com) on 2009-11-21.
+Copyright (c) 2009 Ollix. All rights reserved.
+
+Jump is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version.
+
+Jump is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with Jump. If not, see <http://www.gnu.org/licenses/>.
+"""
