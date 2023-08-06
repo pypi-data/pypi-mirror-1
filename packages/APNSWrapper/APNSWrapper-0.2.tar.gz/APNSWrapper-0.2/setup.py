cf27e7e974183aa628d097bbf47a8dea7baec2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:25 +0000

    Add package APNSWrapper/0.2/APNSWrapper-0.2.tar.gz

---
+++ packages/APNSWrapper/APNSWrapper-0.2.tar.gz/APNSWrapper-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name = "APNSWrapper",
+    version = "0.2",
+    packages = find_packages('.'),
+    classifiers = ["Intended Audience :: Customer Service", "Topic :: Internet" ],
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['ssl', 'docutils>=0.3'],
+
+    package_data = {
+        '': ['*.dat'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Max Klymyshyn, Sonettic",
+    author_email = "klymyshyn@gmail.com",
+    description = "This is wrapper for Apple Push Notification Service.",
+    license = "ALv2",
+    keywords = "apns push notification service wrapper apple",
+    url = "http://code.google.com/p/apns-python-wrapper/",
+	long_description = "The Wrapper support for alerts, badges, sounds and custom arguments. Feedback Service wrapper support for iterations through feedback tuples."
+)
