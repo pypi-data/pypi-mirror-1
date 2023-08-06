23078f1384aa173c91387c937cfb96993d9f7c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:55 +0000

    Add package pkginfo/0.4.1/pkginfo-0.4.1.tar.gz

---
+++ packages/pkginfo/pkginfo-0.4.1.tar.gz/pkginfo-0.4.1/pkginfo/tests/__init__.py
@@ -0,0 +1,25 @@
+# requirements
+
+def additional_tests():
+    import doctest
+    return doctest.DocFileSuite('../../docs/index.rst',
+                                module_relative=True,
+                               )
+
+def _checkSample(testcase, installed):
+    testcase.assertEqual(installed.name, 'pkginfo')
+    testcase.assertEqual(installed.version, '0.4.1')
+    testcase.assertEqual(installed.keywords,
+                        'distribution sdist installed metadata' )
+    testcase.assertEqual(list(installed.supported_platforms), [])
+
+def _checkClassifiers(testcase, installed):
+    testcase.assertEqual(list(installed.classifiers),
+                         [
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: Python Software Foundation License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python :: 2',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: System :: Software Distribution',
+    ])
