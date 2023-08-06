a0b912d135f3fb17afab5e539b14e498715fb8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package pypaster/0.5.3/pypaster-0.5.3.tar.gz

---
+++ packages/pypaster/pypaster-0.5.3.tar.gz/pypaster-0.5.3/setup.py
@@ -0,0 +1,27 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+    name = "pypaster",
+    version = "0.5.3",
+    packages = find_packages(),
+    package_data = {'' : ['./ez_setup.py']},
+    requires = ['simplejson (>=0.9.1)'],
+    install_requires = ['simplejson>=0.9.1'],
+    provides = ['pypaster'],
+    entry_points = {
+        'console_scripts': [
+            'pypaster = pypaster.pypaster:main',
+        ]
+    },
+
+    # metadata for upload to PyPI
+    author = "Matt Kemp",
+    author_email = "matt@mattikus.com",
+    description = "Uploads code snippets to http://pypaste.com",
+    license = "MIT",
+    keywords = "pastebin, pypaste",
+    url = "https://hg.mattikus.com/pypaster/",   # project home page, if any
+)
+
