a082beee104ec2132b89812972b9d014cec8ad95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:39 +0000

    Add package files/1.0/files-1.0.tar.gz

---
+++ packages/files/files-1.0.tar.gz/files-1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+data = {
+    "name": "files",
+    "version": "1.0",
+    "description": "Python file and path manipulation",
+    "license": "GNU GPL 3",
+    "author": "Pavel Panchekha",
+    "author_email": "pavpanchekha@gmail.com",
+    "url": "http://panchekha.no-ip.com:8080/pavpan/files.py/",
+    "py_modules": ["files"],
+}
+
+setup(**data)
