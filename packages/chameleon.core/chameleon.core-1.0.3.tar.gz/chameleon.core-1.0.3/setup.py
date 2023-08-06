64643cf5bf4146f99ee0f6786aaae7df923a0f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:44 +0000

    Add package chameleon.core/1.0.3/chameleon.core-1.0.3.tar.gz

---
+++ packages/chameleon.core/chameleon.core-1.0.3.tar.gz/chameleon.core-1.0.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys
+version = '1.0.3'
+
+install_requires = [
+    'setuptools',
+    'zope.interface',
+    'zope.i18n >= 3.5',
+    'sourcecodegen>=0.6.11',
+    ]
+
+if sys.version_info[:3] < (2,5,0):
+    install_requires.append('elementtree')
+
+setup(name='chameleon.core',
+      version=version,
+      description="Attribute language template compiler",
+      long_description=open("README.txt").read() + open("CHANGES.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Text Processing :: Markup :: XML",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Malthe Borch and the Zope Community',
+      author_email='zope-dev@zope.org',
+      url='',
+      license='BSD',
+      namespace_packages=['chameleon'],
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      )
