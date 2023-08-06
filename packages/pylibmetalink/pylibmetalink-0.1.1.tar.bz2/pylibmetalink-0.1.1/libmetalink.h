cd08b12980896db4acf9cbe9eb9d05834926b03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:37 +0000

    Add package pylibmetalink/0.1.1/pylibmetalink-0.1.1.tar.bz2

---
+++ packages/pylibmetalink/pylibmetalink-0.1.1.tar.bz2/pylibmetalink-0.1.1/libmetalink.h
@@ -0,0 +1,19 @@
+#ifndef LIBMETALINK_H
+#define LIBMETALINK_H 1
+
+#include <Python.h>
+#include <structmember.h>
+#include <metalink/metalink_parser.h>
+
+typedef struct
+{
+	PyObject_HEAD
+	PyObject *files;
+	metalink_t *metalink;
+} cMetalinkObject;
+
+PyObject *libmetalinkException;
+
+static uint8_t cMetalink_createPyObject(cMetalinkObject *self, __attribute__((unused)) PyObject *args);
+
+#endif /* LIBMETALINK_H */
