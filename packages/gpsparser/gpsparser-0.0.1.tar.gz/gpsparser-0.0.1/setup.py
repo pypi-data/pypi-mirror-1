ad924ba3c1ea7f2c131ee8bb308a9a5dcdb48d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:28 +0000

    Add package gpsparser/0.0.1/gpsparser-0.0.1.tar.gz

---
+++ packages/gpsparser/gpsparser-0.0.1.tar.gz/gpsparser-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+'''
+setup.py script for gpsparser.py
+'''
+
+from setuptools import setup, find_packages
+
+setup(name = 'gpsparser',
+      version = '0.0.1',      
+      author = "Val Schmidt",
+      author_email = "vschmidt@ccom.unh.edu",
+      description = "gpsparser is a package for the parsing of GPS NMEA strings.",
+      license = "GPL",
+      zip_safe = True
+)
