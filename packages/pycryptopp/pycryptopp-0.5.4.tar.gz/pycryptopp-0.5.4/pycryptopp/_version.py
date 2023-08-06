bd8d915fc231fc90d1dd30b30c5500f6575c9cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:39 +0000

    Add package pycryptopp/0.5.4/pycryptopp-0.5.4.tar.gz

---
+++ packages/pycryptopp/pycryptopp-0.5.4.tar.gz/pycryptopp-0.5.4/pycryptopp/_version.py
@@ -0,0 +1,16 @@
+
+try:
+    from pyutil.version_class import Version as pyutil_Version
+    Version = pyutil_Version
+except ImportError:
+    from distutils.version import LooseVersion as distutils_Version
+    Version = distutils_Version
+
+# This is the version of this tree, as created by setup.py darscver (from the pyutil
+# library) from the Darcs patch information: the main version number is taken
+# from the most recent release tag. If some patches have been added since the
+# last release, this will have a -NN "build number" suffix. Please see
+# pyutil.version_class for a description of what the different fields mean.
+
+verstr = "0.5.4"
+__version__ = Version(verstr)
