e75c9e5ed7ceed91827120559903f42847d9cf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:11 +0000

    Add package zope.event/3.4.1/zope.event-3.4.1.tar.gz

---
+++ packages/zope.event/zope.event-3.4.1.tar.gz/zope.event-3.4.1/setup.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.event package
+
+$Id: setup.py 97459 2009-03-03 20:42:35Z nadako $
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='zope.event',
+    version='3.4.1',
+    url='http://pypi.python.org/pypi/zope.event',
+    license='ZPL 2.1',
+    description='Very basic event publishing system',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src', 'zope', 'event', 'README.txt')
+        + '\n' +
+        'Download\n'
+        '********\n'
+        ),
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope',],
+      include_package_data=True,
+      install_requires=['setuptools'],
+      zip_safe=False,
+      )
