2f8314787b0cb2ac7ee94d94971a1524875f2f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:48 +0000

    Add package z3c.contents/0.5.0/z3c.contents-0.5.0.zip

---
+++ packages/z3c.contents/z3c.contents-0.5.0.zip/z3c.contents-0.5.0/src/z3c/contents/tests.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id:$
+"""
+__docformat__ = "reStructuredText"
+
+import unittest
+from zope.testing import doctest
+from zope.app.testing import functional
+
+from z3c.contents import testing
+
+functional.defineLayer('TestLayer', 'ftesting.zcml')
+
+
+def test_suite():
+    docTest = functional.FunctionalDocFileSuite('BROWSER.txt',
+        setUp=testing.doctestSetUp, tearDown=testing.doctestTearDown,
+        optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
+    docTest.layer = TestLayer
+    return unittest.TestSuite((
+        doctest.DocFileSuite('README.txt',
+            setUp=testing.setUp, tearDown=testing.tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            ),
+        docTest,
+        ))
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
