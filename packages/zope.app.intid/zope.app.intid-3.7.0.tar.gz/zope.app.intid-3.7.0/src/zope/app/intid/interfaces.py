5dc73f43d3bfaf124d203ac1c03b7c3812595d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:17 +0000

    Add package zope.app.intid/3.7.0/zope.app.intid-3.7.0.tar.gz

---
+++ packages/zope.app.intid/zope.app.intid-3.7.0.tar.gz/zope.app.intid-3.7.0/src/zope/app/intid/interfaces.py
@@ -0,0 +1,18 @@
+"""
+Interfaces for the unique id utility.
+
+$Id: interfaces.py 95798 2009-01-31 21:15:12Z nadako $
+"""
+
+# BBB
+from zope.intid.interfaces import (
+    IIntIdsQuery,
+    IIntIdsSet,
+    IIntIdsManage,
+    IIntIds,
+    IIntIdEvent,
+    IIntIdRemovedEvent,
+    IntIdRemovedEvent,
+    IIntIdAddedEvent,
+    IntIdAddedEvent
+)
