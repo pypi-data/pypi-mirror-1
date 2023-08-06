64e0a5906b29c8a656945947b23f744d9bca15e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:38 +0000

    Add package improviser/0.5.7.5/improviser-0.5.7.5.tar.gz

---
+++ packages/improviser/improviser-0.5.7.5.tar.gz/improviser-0.5.7.5/improviser/Musicians/Drummers/__init__.py
@@ -0,0 +1,6 @@
+from RockDrum import RockDrum
+from JazzDrum import JazzDrum
+from BlastBeat import BlastBeat
+from Metronome import Metronome
+
+__all__ = ['RockDrum', 'JazzDrum', 'BlastBeat', 'Metronome']
---
+++ packages/improviser/improviser-0.5.7.5.tar.gz/improviser-0.5.7.5/improviser/Progressions/Contemporary.py
@@ -0,0 +1,24 @@
+reincarnatie = [(0, ["I", "bIIIM7"]),
+		(8, ["Vm", "bVIIM"]),
+		(12, ["I", "bIIIM7"]),
+		(16, ["Vm", "bVIIM"]),
+		(20, ["bIIIM", "bIIIM", "bVIIM", "bVIIM", "IV", "IV"]),
+		(21, ["Im", "Vm"]),
+		(22, ["bIIIM", "bIIIM", "bVIIM", "bVIIM", "IV", "IV"]),
+		(23, ["Im", "Vm"]),
+		(24, 'R')]
+
+fietsen_intro = [
+	   (0, ["I5", "bIII5"]),
+	   (3, ["IV5", "bIII5"]),
+	   (4, ["I5", "bIII5"]),
+	   (7, ["IV5", "bIII5"]),
+	   (8, ["I5", "bIII5"]),
+	   (11, ["IV5", "bIII5"]),
+	   (12, ["I5", "bIII5"]),
+	   (15, ["IV5", "V7#9"]),
+	   (16, 'R')]
+
+fietsen_couplet = [(0, ["I", "IV", "#Vdom7", "V7"]),
+		   (1, 'R')]
+
