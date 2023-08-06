100af210774f5d41eea65a2645b974ed83a59d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:16 +0000

    Add package zope.app.security/3.7.2/zope.app.security-3.7.2.tar.gz

---
+++ packages/zope.app.security/zope.app.security-3.7.2.tar.gz/zope.app.security-3.7.2/src/zope/app/security/metaconfigure.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Backward-compatibility imports for the global principal registry directives
+
+$Id: metaconfigure.py 97947 2009-03-12 02:31:26Z nadako $
+"""
+from zope.principalregistry.metaconfigure import (
+    principal,
+    unauthenticatedPrincipal,
+    unauthenticatedGroup,
+    authenticatedGroup,
+    everybodyGroup,
+    )
---
+++ packages/zope.app.security/zope.app.security-3.7.2.tar.gz/zope.app.security-3.7.2/src/zope/app/security/principalregistry.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Backward-compatibility imports for the global principal registry
+
+$Id: principalregistry.py 97947 2009-03-12 02:31:26Z nadako $
+"""
+
+# BBB: these were moved to zope.principalregistry
+from zope.principalregistry.principalregistry import (
+    DuplicateLogin,
+    DuplicateId,
+    PrincipalRegistry,
+    principalRegistry,
+    PrincipalBase,
+    Group,
+    Principal,
+    UnauthenticatedPrincipal,
+    fallback_unauthenticated_principal,
+    UnauthenticatedGroup,
+    AuthenticatedGroup,
+    EverybodyGroup,
+    )
