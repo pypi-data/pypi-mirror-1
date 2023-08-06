466481f7ba86ac22a3b8575a6c4911134d3c1bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:18 +0000

    Add package optcomplete/1.2-devel/optcomplete-1.2-devel.tar.gz

---
+++ packages/optcomplete/optcomplete-1.2-devel.tar.gz/optcomplete-1.2-devel/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+# Install script for tengis.
+#
+
+__version__ = "$Revision$"
+__author__ = "Martin Blais <blais@furius.ca>"
+
+from distutils.core import setup
+
+def read_version():
+    try:
+        return open('VERSION', 'r').readline().strip()
+    except IOError, e:
+        raise SystemExit(
+            "Error: you must run setup from the root directory (%s)" % str(e))
+
+
+# Include all files without having to create MANIFEST.in
+def add_all_files(fun):
+    import os, os.path
+    from os.path import abspath, dirname, join
+    def f(self):
+        for root, dirs, files in os.walk('.'):
+            if '.hg' in dirs: dirs.remove('.hg')
+            self.filelist.extend(join(root[2:], fn) for fn in files
+                                 if not fn.endswith('.pyc'))
+        return fun(self)
+    return f
+from distutils.command.sdist import sdist
+sdist.add_defaults = add_all_files(sdist.add_defaults)
+
+
+setup(name="optcomplete",
+      version=read_version(),
+      description=\
+      "Automatic shell completion support for scripts that use optparse.",
+      long_description="""
+This module provide automatic bash completion support for programs that use the
+optparse module.  The premise is that the optparse options parser specifies
+enough information (and more) for us to be able to generate completion strings
+esily.  Another advantage of this over traditional completion schemes where the
+completion strings are hard-coded in a separate bash source file, is that the
+same code that parses the options is used to generate the completions, so the
+completions is always up-to-date with the program itself.
+
+In addition, we allow you specify a list of regular expressions or code that
+define what kinds of files should be proposed as completions to this file if
+needed.  If you want to implement more complex behaviour, you can instead
+specify a function, which will be called with the current directory as an
+argument.
+
+You need to activate bash completion using the shell script function that comes
+with optcomplete (see http://furius.ca/optcomplete for more details).
+""",
+      license="BSD",
+      author="Martin Blais",
+      author_email="blais@furius.ca",
+      url="http://furius.ca/optcomplete",
+      package_dir = {'': 'lib/python'},
+      py_modules = ['optcomplete'],
+     )
