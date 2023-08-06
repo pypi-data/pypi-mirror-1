c22881ee5d733fbee077952387e9d04c24f67493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:55 +0000

    Add package z3c.objpath/1.0/z3c.objpath-1.0.tar.gz

---
+++ packages/z3c.objpath/z3c.objpath-1.0.tar.gz/z3c.objpath-1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('src', 'z3c', 'objpath', 'README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
+setup(
+    name='z3c.objpath',
+    version='1.0',
+    description="Generate and resolve paths to to objects.",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Martijn Faassen',
+    author_email='faassen@startifact.com',
+    license='ZPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        ],
+    entry_points={},
+    )
