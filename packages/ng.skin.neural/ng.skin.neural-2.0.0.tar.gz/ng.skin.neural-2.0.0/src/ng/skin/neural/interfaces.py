81822eb8026ae2a5969f669d5de4c06b038f54cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:42 +0000

    Add package ng.skin.neural/2.0.0/ng.skin.neural-2.0.0.tar.gz

---
+++ packages/ng.skin.neural/ng.skin.neural-2.0.0.tar.gz/ng.skin.neural-2.0.0/src/ng/skin/neural/interfaces.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""The definitions of new skin interface
+
+$Id: interfaces.py 50456 2008-02-01 05:47:30Z cray $
+"""
+__author__  = "Andrey Orlov, 2008"
+__license__ = "GPL"
+__version__ = "$Revision: 50456 $"
+
+from ng.skin.base.interfaces import NGBaseSkin
+
+class NeuralSkin(NGBaseSkin):
+    """Skin for Neural Ru"""
