34b52ad77853c10b138ef7c63eb3eab7f93b34a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:44 +0000

    Add package setuptools_dummy/0.1.0.4/setuptools_dummy-0.1.0.4.tar.gz

---
+++ packages/setuptools_dummy/setuptools_dummy-0.1.0.4.tar.gz/setuptools_dummy-0.1.0.4/setup.py
@@ -0,0 +1,50 @@
+
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    try:
+        from ez_setup import use_setuptools
+    except ImportError:
+        print "can't find ez_setup"
+        print "try: wget http://peak.telecommunity.com/dist/ez_setup.py"
+        sys.exit(1)
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+VERSION = (0, 1, 0, 4)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+
+setup(
+    name = 'setuptools_dummy',
+    version = __versionstr__,
+    description = 'Setuptools Dummy Filefinder',
+    long_description = '\n'.join((
+        'Setuptools Dummy Filefinder',
+        '',
+        'plugin for enabling all files integration',
+        'even if they are not under version control system',
+    )),
+    author = 'centrum holdings s.r.o',
+    author_email='devel@centrumholdings.com',
+    license = 'BSD',
+    url = 'http://github.com/ella/setuptools-dummy/tree/master',
+
+    py_modules = ["setuptools_dummy"],
+    include_package_data = False,
+
+    classifiers = [
+        "Topic :: Software Development :: Version Control",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+    ],
+    entry_points = {
+        'setuptools.file_finders': ['dummy = setuptools_dummy:dummylsfiles'],
+    },
+)
