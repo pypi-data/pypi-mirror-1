ebbba986cab38bd47962eddd132cf22d47a44426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:18 +0000

    Add package alterootheme.intensesimplicity/1.0a/alterootheme.intensesimplicity-1.0a-py2.4.egg

---
+++ packages/alterootheme.intensesimplicity/alterootheme.intensesimplicity-1.0a-py2.4.egg/alterootheme/intensesimplicity/browser/viewlets.py
@@ -0,0 +1,9 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.app.layout.viewlets.common import ViewletBase
+
+from plone.app.layout.viewlets import common
+
+class GlobalSectionsViewlet(common.GlobalSectionsViewlet):
+    render = ViewPageTemplateFile("templates/sections.pt")
+
