d3aa6b52e2e5da9f33cda9a93becfa2f5db28ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:59 +0000

    Add package adsense.portlet/1.0dev/adsense.portlet-1.0dev-py2.4.egg

---
+++ packages/adsense.portlet/adsense.portlet-1.0dev-py2.4.egg/wwp/adsense/portlet/__init__.py
@@ -0,0 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+wwp_adsenseMessageFactory = MessageFactory('adsense.portlet')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
