edcdf69514df2e52d0532320662a2fcdd4385c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:59 +0000

    Add package pisa/3.0.32/pisa-3.0.32.zip

---
+++ packages/pisa/pisa-3.0.32.zip/pisa-3.0.32/demo/wsgi/pisawsgidemo.py
@@ -0,0 +1,46 @@
+﻿#!/bin/python2.5
+# -*- coding: UTF-8 -*-
+#############################################
+## (C)opyright by Dirk Holtwick, 2008      ##
+## All rights reserved                     ##
+#############################################
+
+__version__ = "$Revision: 103 $"
+__author__  = "$Author: holtwick $"
+__date__    = "$Date: 2007-10-31 17:08:54 +0100 (Mi, 31 Okt 2007) $"
+__svnid__   = "$Id: pisa.py 103 2007-10-31 16:08:54Z holtwick $"
+
+from wsgiref.simple_server import make_server
+import logging
+
+import sx.pisa3.pisa_wsgi as pisa_wsgi
+
+def SimpleApp(environ, start_response):
+    
+    # That's the magic!
+    #
+    # Set the environment variable "pisa.topdf" to the filename
+    # you would like to have for the resulting PDF 
+    environ["pisa.topdf"] = "index.pdf"
+    
+    # Simple Hello World example
+    start_response(
+        '200 OK', [
+        ('content-type', "text/html"),
+        ])
+    return ["Hello <strong>World</strong>"]
+
+if __name__ == '__main__':
+
+    HOST = ''
+    PORT = 8080
+    logging.basicConfig(level=logging.DEBUG)
+    
+    app = SimpleApp
+    
+    # Add PISA WSGI Middleware  
+    app = pisa_wsgi.PisaMiddleware(app)
+    
+    httpd = make_server(HOST, PORT, app)
+    print "Serving HTTP on port %d..." % PORT
+    httpd.serve_forever()
---
+++ packages/pisa/pisa-3.0.32.zip/pisa-3.0.32/test/witherror.py
@@ -0,0 +1,25 @@
+# -*- coding: ISO-8859-1 -*-
+#############################################
+## (C)opyright by Dirk Holtwick            ##
+## All rights reserved                     ##
+#############################################
+
+__version__ = "$Revision: 194 $"
+__author__  = "$Author: holtwick $"
+__date__    = "$Date: 2008-04-18 18:59:53 +0200 (Fr, 18 Apr 2008) $"
+
+import ho.pisa as pisa
+
+def helloWorld():
+    filename = __file__ + ".pdf"
+    pdf = pisa.CreatePDF(
+        u"Hello <strong>World</strong> <img src='data:image/jpg;base64,?´*'>",
+        file(filename, "wb"),
+        show_error_as_pdf=True,
+        )
+    if not pdf.err:
+        pisa.startViewer(filename)
+
+if __name__=="__main__":
+    pisa.showLogging()
+    helloWorld()
