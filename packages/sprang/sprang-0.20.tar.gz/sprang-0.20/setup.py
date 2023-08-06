76be2628f3b6a0bb8dc4ff60dca20d83dd5140a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:23 +0000

    Add package sprang/0.20/sprang-0.20.tar.gz

---
+++ packages/sprang/sprang-0.20.tar.gz/sprang-0.20/setup.py
@@ -0,0 +1,15 @@
+#import ez_setup, os
+#ez_setup.use_setuptools()
+import os
+from setuptools import setup, find_packages
+setup(
+    name = "sprang",
+    version = "0.20",
+    author = "JingleManSweep",
+    author_email = "jinglemansweep@gmail.com",
+    description = "Helper shell script allowing posting and retrieving of text snippets via 'sprunge.us' pastebin service.",
+    url = "http://github.com/jingleman/sprang",
+    packages = find_packages(),
+    scripts = [os.path.join("bin", "sprang"),],
+    include_package_data = True
+)
