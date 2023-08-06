fc21033d697d5ce0a21f53c222be1a37b776eb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:02 +0000

    Add package dectools/0.1.1/dectools-0.1.1.tar.gz

---
+++ packages/dectools/dectools-0.1.1.tar.gz/dectools-0.1.1/dectools/test/test_all.py
@@ -0,0 +1,15 @@
+# Test stuff, because WingZ testing is just broken.
+
+import test_call_before
+import test_call_if2
+import test_call_if
+import test_call_instead
+import test_call_once
+import test_make_call_if
+import test_make_call_instead
+import test_log
+import test_make_call_once
+import test_dict_as_called
+import test_pre_post
+import test_invariants
+
