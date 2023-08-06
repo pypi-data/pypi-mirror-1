108007416b2f0b5eeafdc91786d0108cf6395f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:42 +0000

    Add package pyspacewar/0.9.4/pyspacewar-0.9.4.tar.gz

---
+++ packages/pyspacewar/pyspacewar-0.9.4.tar.gz/pyspacewar-0.9.4/src/pyspacewar/version.py
@@ -0,0 +1,16 @@
+"""
+PySpaceWar version number tracker.
+
+$Id: version.py 252 2007-01-05 02:21:13Z mg $
+"""
+
+svn_revision = "$Revision: 252 $"[11:-2]
+version = "0.9.4"
+
+if version.endswith('svn'):
+    version += svn_revision
+    # This is slightly misleading: svn_revision contains the last changed
+    # revision number for version.py, not the revision number of the whole
+    # repository.  Using os.popen('svnversion').read() would be better, but
+    # only if the end-user has subversion installed.
+
