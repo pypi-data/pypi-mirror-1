eae675c0aee6f5ba7b8827d964e8e7b0022f98cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:52 +0000

    Add package notifications/0.1/notifications-0.1.tar.gz

---
+++ packages/notifications/notifications-0.1.tar.gz/notifications-0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="notifications",
+      version="0.1",
+      description="Client for the iPhone/iPod Touch `Notifications' app",
+      author="Thomas Jost",
+      author_email="thomas.jost@gmail.com",
+      license="ISC",
+      py_modules=['notifications'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Communications',
+          'Topic :: Internet :: WWW/HTTP',
+      ]
+)
