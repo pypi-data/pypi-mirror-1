cb8c703c453c3fc8ee9073ced40b1c10f8708d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:45 +0000

    Add package improviser/0.7.1.0/improviser-0.7.1.0.tar.gz

---
+++ packages/improviser/improviser-0.7.1.0.tar.gz/improviser-0.7.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+	name="improviser",
+	version="0.7.1.0",
+	description="Automatic music generation software",
+	long_description="Experiments in musical content generation.",
+	author="Bart Spaans",
+	author_email="onderstekop@gmail.com",
+	url="http://www.onderstekop.nl/",
+	packages = ['improviser', 'improviser.Blocks', 'improviser.Musicians',
+			'improviser.Musicians.Bassists', 'improviser.Musicians.Pianists', 'improviser.Musicians.Drummers',
+			'improviser.Musicians.Accompaniment', 'improviser.Musicians.Guitarists', 'improviser.Musicians.Soloists',
+			'improviser.Progressions', 'improviser.Visualizations','improviser.Movements', 'improviser.qtGUI'],
+	classifiers = [],
+)
