ecd33cb73e128d2ff7ca31965b0f4e2120f7bf10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:56 +0000

    Add package zope.rdb/3.4.1/zope.rdb-3.4.1.tar.gz

---
+++ packages/zope.rdb/zope.rdb-3.4.1.tar.gz/zope.rdb-3.4.1/setup.py
@@ -0,0 +1,70 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.rdb package
+
+$Id: setup.py 91998 2008-10-10 16:53:36Z sidnei $
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
+setup(
+    name='zope.rdb',
+    version='3.4.1',
+    url='http://pypi.python.org/pypi/zope.rdb',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+    license='ZPL 2.1',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        'Framework :: Zope3',
+        ],
+    description='Zope RDBMS transaction integration',
+    long_description=long_description,
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope',],
+    include_package_data=True,
+    install_requires=['setuptools',
+                      'ZODB3',
+                      'zope.interface',
+                      'zope.i18nmessageid',
+                      'zope.security',
+                      'zope.configuration',
+                      'zope.schema',
+                      'zope.thread',
+                      'zope.app.container',
+                      ],
+    zip_safe=False,
+    )
