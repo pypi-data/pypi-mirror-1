cf518848703fbf4c4814585aa5bb6039470b5010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:55 +0000

    Add package python-daemon/1.4.6/python-daemon-1.4.6.tar.gz

---
+++ packages/python-daemon/python-daemon-1.4.6.tar.gz/python-daemon-1.4.6/setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2008–2009 Ben Finney <ben+python@benfinney.id.au>
+# Copyright © 2008 Robert Niederreiter, Jens Klein
+#
+# This is free software: you may copy, modify, and/or distribute this work
+# under the terms of the Python Software Foundation License, version 2 or
+# later as published by the Python Software Foundation.
+# No warranty expressed or implied. See the file LICENSE.PSF-2 for details.
+
+""" Distribution setup for python-daemon library.
+    """
+
+import textwrap
+from setuptools import setup, find_packages
+
+main_module_name = 'daemon'
+main_module = __import__(main_module_name, fromlist=['version'])
+version = main_module.version
+
+short_description, long_description = (
+    textwrap.dedent(d).strip()
+    for d in main_module.__doc__.split(u'\n\n', 1)
+    )
+
+
+setup(
+    name = "python-daemon",
+    version = version.version,
+    packages = find_packages(),
+
+    # setuptools metadata
+    zip_safe = False,
+    test_suite = "tests.suite",
+    tests_require=[
+        "MiniMock >=1.2.2",
+        ],
+    install_requires = [
+        "setuptools",
+        "lockfile >=0.7",
+        ],
+
+    # PyPI metadata
+    author = version.author_name,
+    author_email = version.author_email,
+    description = short_description,
+    license = version.license,
+    keywords = u"daemon fork unix",
+    url = main_module._url,
+    long_description = long_description,
+    classifiers = [
+        # Refer to http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    )
