560de3164443ddcec8721784c4deed981cdc35cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:55 +0000

    Add package zc.catalogqueue/0.3.1/zc.catalogqueue-0.3.1.tar.gz

---
+++ packages/zc.catalogqueue/zc.catalogqueue-0.3.1.tar.gz/zc.catalogqueue-0.3.1/setup.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+name = 'zc.catalogqueue'
+version = '0.3.1'
+
+import os
+from setuptools import setup, find_packages
+
+entry_points = """
+"""
+
+def read(rname):
+    return open(os.path.join(os.path.dirname(__file__), *rname.split('/')
+                             )).read()
+
+long_description = (
+        read('src/zc/catalogqueue/README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('src/zc/catalogqueue/queue.txt')
+        + '\n' +
+        'Download\n'
+        '********\n'
+        )
+
+setup(
+    name = name,
+    version = version,
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    description = '',
+    long_description=long_description,
+    license = 'ZPL 2.1',
+    
+    packages = find_packages('src'),
+    namespace_packages = ['zc'],
+    package_dir = {'': 'src'},
+    install_requires = [
+        'pytz',
+        'setuptools',
+        'ZODB3',
+        'zope.interface',
+        ],
+    zip_safe = False,
+    entry_points=entry_points,
+    include_package_data = True,
+    )
