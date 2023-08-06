013c091e63e0861986e145a0458a597d2d56616f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:39 +0000

    Add package nl/0.8/nl-0.8-py2.5.egg

---
+++ packages/nl/nl-0.8-py2.5.egg/nl/test/functional_test.py
@@ -0,0 +1,156 @@
+import nl
+
+
+class cms_test(object):
+    def setup(self):
+        from nl.examples import cms2
+        self.cms = cms2
+
+    def teardown(self):
+        pass
+
+#    def thing_test(self):
+#        pooh = self.cms.Person('pooh')
+#        assert repr(pooh) == 'pooh is a Person'
+#        assert str(pooh) == 'pooh'
+
+#    def second_test(self):
+#        # john is a person
+#        john = self.cms.Person('john')
+#        # pete is a person
+#        pete = self.cms.Person('pete')
+#        # jane is a person
+#        jane = self.cms.Person('jane')
+#        # c1 is a content
+#        c1 = self.cms.Content('c1')
+#        # c2 is a content
+#        c2 = self.cms.Content('c2')
+#        # john has role manager
+#        p1 = nl.Prop(john, self.cms.Has(what=self.cms.manager))
+#        # jane has create_perm
+#        p2 = nl.Prop(jane, self.cms.Has(what=self.cms.create_perm))
+#        # jane wants to create c1
+#        p3 = nl.Prop(jane, self.cms.Wants(to=self.cms.Create(what=c1)))
+#        # pete wants to create c2
+#        p4 = nl.Prop(pete, self.cms.Wants(to=self.cms.Create(what=c2)))
+#        # input everything into the db
+#        nl.kb.tell(john, pete, jane, c1, c2, p1, p2, p3, p4)
+#        # to the question is jane owner of c1?, the answer is no
+#        assert nl.kb.ask(nl.Prop(jane, self.cms.IsOwner(of=c1))) == 'no'
+#        # extend the db
+#        nl.kb.extend()
+#        # to the question is jane owner of c1?, the answer is yes
+#        assert nl.kb.ask(nl.Prop(jane, self.cms.IsOwner(of=c1), nl.Duration(start=nl.now))) == 'jane isowner of c1 at %s.0' % nl.time._now
+#        # to the question has c1 private state?, the answer is yes
+#        assert nl.kb.ask(nl.Prop(c1, self.cms.Has(what=self.cms.private))) == 'c1 has what private at %s.0' % nl.time._now
+#        # to the question is pete owner of c2?, the answer is no
+#        assert nl.kb.ask(nl.Prop(pete, self.cms.IsOwner(of=c2))) == 'no'
+#        # jane wants to publish c1
+#        nl.kb.tell(nl.Prop(jane, self.cms.Wants(to=self.cms.Publish(what=c1))))
+#        # pete wants to publish c2
+#        nl.kb.tell(nl.Prop(pete, self.cms.Wants(to=self.cms.Publish(what=c2))))
+#        # extend the db
+#        nl.kb.extend()
+#        # to the question is c1 public?, the answer is no
+#        assert nl.kb.ask(nl.Prop(c1, self.cms.Has(what=self.cms.public))) == 'no'
+#        # to the question is c2 public?, the answer is no
+#        assert nl.kb.ask(nl.Prop(c2, self.cms.Has(what=self.cms.public))) == 'no'
+#        # to the question can jane view c1?, the answer is yes
+#        assert nl.kb.ask(nl.Prop(jane, self.cms.Can(what=self.cms.View(what=c1)))) == 'jane can what view what c1 at %s.0' % nl.time._now
+#        # to the question can pete view c1?, the answer is no
+#        assert nl.kb.ask(nl.Prop(pete, self.cms.Can(what=self.cms.View(what=c1)))) == 'no'
+#        # john wants to publish c1
+#        nl.kb.tell(nl.Prop(john, self.cms.Wants(to=self.cms.Publish(what=c1))))
+#        # extend the db
+#        nl.kb.extend()
+#        # to the question is c1 private?, the answer is no
+#        assert nl.kb.ask(nl.Prop(c1, self.cms.Has(what=self.cms.private))) == 'no'
+#        # to the question is c1 public?, the answer is yes
+#        assert nl.kb.ask(nl.Prop(c1, self.cms.Has(what=self.cms.public))) == 'c1 has what public at -1.0'
+#        # to the question can pete view c1?, the answer is yes
+#        assert nl.kb.ask(nl.Prop(pete, self.cms.Can(what=self.cms.View(what=c1)))) == 'pete can what view what c1 at -1.0'
+#
+#
+#
+#        assert not nl.kb.app.root()['props'].has_key('c1 has what private at -1.0')
+#        assert nl.kb.app.root()['props'].has_key('c1 has what public at -1.0')
+
+    def third_test(self):
+        #from nl.examples import cms2
+        #self.cms = cms2
+        # john is a person
+        john = self.cms.Person('john')
+        # pete is a person
+        pete = self.cms.Person('pete')
+        # jane is a person
+        jane = self.cms.Person('jane')
+        # c1 is a content
+        c1 = self.cms.Content('c1')
+        # c2 is a content
+        c2 = self.cms.Content('c2')
+        # john has role manager
+        p1 = nl.Prop(john, self.cms.Has(what=self.cms.manager), self.cms.Duration(start=self.cms.Instant('now')))
+        # jane has create_perm
+        p2 = nl.Prop(jane, self.cms.Has(what=self.cms.create_perm), self.cms.Duration(start=self.cms.Instant('now')))
+        # jane wants to create c1
+        p3 = nl.Prop(jane, self.cms.Wants(to=self.cms.Create(what=c1)), self.cms.Instant('now'))
+        # pete wants to create c2
+        p4 = nl.Prop(pete, self.cms.Wants(to=self.cms.Create(what=c2)), self.cms.Instant('now'))
+        # input everything into the db
+        nl.kb.tell(john, pete, jane, c1, c2, p1, p2, p3, p4)
+        # to the question is jane owner of c1?, the answer is no
+        assert nl.kb.ask(nl.Prop(jane, self.cms.IsOwner(of=c1), self.cms.Duration(start=self.cms.Instant('now')))) == 'no'
+        # extend the db
+        nl.kb.extend()
+        # to the question is jane owner of c1?, the answer is yes
+        assert nl.kb.ask(nl.Prop(jane, self.cms.IsOwner(of=c1), self.cms.Duration(start=self.cms.Instant('now')))) == 'yes'
+        # to the question has c1 private state?, the answer is yes
+        assert nl.kb.ask(nl.Prop(c1, self.cms.Has(what=self.cms.private), self.cms.Duration(start=self.cms.Instant('now')))) == 'yes'
+        # to the question is pete owner of c2?, the answer is no
+        assert nl.kb.ask(nl.Prop(pete, self.cms.IsOwner(of=c2), self.cms.Duration(start=self.cms.Instant('now')))) == 'no'
+        # jane wants to publish c1
+        nl.kb.tell(nl.Prop(jane, self.cms.Wants(to=self.cms.Publish(what=c1)), self.cms.Instant('now')))
+        # pete wants to publish c2
+        nl.kb.tell(nl.Prop(pete, self.cms.Wants(to=self.cms.Publish(what=c2)), self.cms.Instant('now')))
+        # extend the db
+        nl.kb.extend()
+        # to the question is c1 public?, the answer is no
+        assert nl.kb.ask(nl.Prop(c1, self.cms.Has(what=self.cms.public), self.cms.Duration(start=self.cms.Instant('now')))) == 'no'
+        # to the question is c2 public?, the answer is no
+        assert nl.kb.ask(nl.Prop(c2, self.cms.Has(what=self.cms.public), self.cms.Duration(start=self.cms.Instant('now')))) == 'no'
+        # to the question can jane view c1?, the answer is yes
+        assert nl.kb.ask(nl.Prop(jane, self.cms.Can(what=self.cms.View(what=c1)), self.cms.Duration(start=self.cms.Instant('now')))) == 'yes'
+        # to the question can pete view c1?, the answer is no
+        assert nl.kb.ask(nl.Prop(pete, self.cms.Can(what=self.cms.View(what=c1)), self.cms.Duration(start=self.cms.Instant('now')))) == 'no'
+        # john wants to publish c1
+        nl.kb.tell(nl.Prop(john, self.cms.Wants(to=self.cms.Publish(what=c1)), self.cms.Instant('now')))
+        # extend the db
+        nl.kb.extend()
+        # to the question is c1 private?, the answer is no
+        assert nl.kb.ask(nl.Prop(c1, self.cms.Has(what=self.cms.private), nl.Duration(start=nl.Instant('now')))) == 'no'
+        # to the question is c1 public?, the answer is yes
+        assert nl.kb.ask(nl.Prop(c1, self.cms.Has(what=self.cms.public), nl.Duration(start=nl.Instant('now')))) == 'yes'
+        # to the question can pete view c1?, the answer is yes
+        assert nl.kb.ask(nl.Prop(pete, self.cms.Can(what=self.cms.View(what=c1)), nl.Duration(start=nl.Instant('now')))) == 'yes'
+
+        # what can pete view? -> c1
+        assert nl.kb.ask(self.cms.Content('X1'), nl.Prop(pete, self.cms.Can(what=self.cms.View(what='X1')), nl.Duration(start=nl.Instant('now')))) == [{'X1': 'c1'}]
+
+        # who can view what?
+        assert nl.kb.ask(self.cms.Content('X1'), self.cms.Person('X2'), nl.Prop(self.cms.Person('X2'), self.cms.Can(what=self.cms.View(what='X1')), nl.Duration(start=nl.Instant('now')))) == [{'X2': 'admin', 'X1': 'c1'}, {'X2': 'john', 'X1': 'c1'}, {'X2': 'pete', 'X1': 'c1'}, {'X2': 'jane', 'X1': 'c1'}]
+
+        # c2 is private
+        nl.kb.tell(nl.Prop(c2, self.cms.Has(what=self.cms.private), nl.Duration(start=nl.Instant('now'))))
+        nl.kb.extend()
+
+        # who can view what?
+        assert nl.kb.ask(self.cms.Content('X1'), self.cms.Person('X2'), nl.Prop(self.cms.Person('X2'), self.cms.Can(what=self.cms.View(what='X1')), nl.Duration(start=nl.Instant('now')))) == [{'X2': 'admin', 'X1': 'c1'}, {'X2': 'john', 'X1': 'c1'}, {'X2': 'pete', 'X1': 'c1'}, {'X2': 'jane', 'X1': 'c1'}, {'X2': 'admin', 'X1': 'c2'}, {'X2': 'john', 'X1': 'c2'}]
+
+        # what permissions has admin?
+        assert nl.kb.ask(self.cms.Permission('X2'), nl.Prop(self.cms.manager, self.cms.Has(what=self.cms.Permission('X2')), nl.Duration(start=nl.Instant('X3')))) == [{'X2': 'basic_perm'}, {'X2': 'manage_perm'}, {'X2': 'create_perm'}]
+
+        # can admin view c2?
+        assert nl.kb.ask(nl.Prop(self.cms.admin, self.cms.Can(what=self.cms.View(what=c2)), nl.Duration(start=nl.Instant('now')))) == 'yes'
+
+
+
---
+++ packages/nl/nl-0.8-py2.5.egg/nl/utils.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2008 by Enrique Pérez Arnaud <enriquepablo@gmail.com>
+#
+# This file is part of ln.
+#
+# ln is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ln is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ln.  If not, see <http://www.gnu.org/licenses/>.
+# utils module
+
+import re
+from persistent import Persistent
+
+import clips
+from nl.log import logger
+
+clips.DebugConfig.ExternalTraceback = True
+
+# vars are always XNUM
+varpat = re.compile(r'^[A-Z]\w*\d+$')
+
+class_constraint = '?%(val)s&:(or (eq (class ?%(val)s) %(cls)s) (subclassp (class ?%(val)s) %(cls)s))'
+_name_def = '(defclass Name (is-a USER))'
+_reduce_class = '''
+(deffunction reduce-class (?instance ?class)
+    (if (eq (length$
+                (find-all-instances ((?a ?class))(eq (instance-name ?a) ?instance)))
+             0)
+    then (make-instance ?instance of ?class)
+    else (return TRUE)))'''
+
+clips.Build(_name_def)
+clips.Build(_reduce_class)
+#clips.Build(_del_daemon)
+logger.info(_name_def)
+logger.info(_reduce_class)
+#logger.info(_del_daemon)
+
+
+subclasses = {}
+def register(clsname, cls):
+    subclasses[clsname] = cls
+
+
+class Name(Persistent):
+    """
+    """
+    _v_clips_class = clips.FindClass('Name')
+
+    @classmethod
+    def from_clips(cls, instance):
+        if not isinstance(instance, clips._clips_wrap.Instance):
+            instance = clips.FindInstance(instance)
+        clsname = str(instance.Class.Name)
+        cls = subclasses[clsname]
+        if clsname == 'Name':
+            return cls(str(instance))
+        else:
+            return cls.from_clips(instance)
+
+    def get_var_constraint(self, vrs, ancestor, mod_path, ci):
+        constraint = ''
+        if self.value in vrs:
+            if vrs[self.value]:
+                v_ci = clips_instance(*(vrs[self.value]))
+                constraint = '&:(eq %s %s)' % (v_ci, ci)
+            else:
+                constraint = '&:(eq %s ?%s)' % (ci, self.value)
+        else:
+            constraint = '&:(or (eq (class %(ci)s) %(cls)s) (subclassp (class %(ci)s) %(cls)s))' % {'ci': ci, 'cls': self.__class__.__name__}
+            vrs[self.value] = (ancestor, mod_path)
+        return constraint
+
+    def get_var_slot_constraint(self, vrs, val):
+        if self.value in vrs:
+            if vrs[self.value]:
+                return '?%(val)s&:(eq ?%(val)s %(var)s)' % {'val': val,
+                                       'var': clips_instance(*(vrs[self.value]))}
+            else:
+                return '?%s' % self.value
+        else:
+            vrs[self.value]= ()
+            return class_constraint % {'val': self.value,
+                                           'cls': self.__class__.__name__}
+
+    def put_var(self, vrs):
+        if self.value in vrs and vrs[self.value]:
+            return clips_instance(*(vrs[self.value]))
+        if self.value not in vrs:
+            vrs[self.value] = ()
+        return '?%s' % self.value
+
+register('Name', Name)
+
+
+def clips_instance(ancestor, mod_path):
+    send_str = '(send ?%s get-%s)'
+    for mod in mod_path:
+        ancestor = send_str % (ancestor, mod)
+        send_str = '(send %s get-%s)'
+    return ancestor
