8f2fe96dfa10e4cf406f8b187fd237d70150f04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:09 +0000

    Add package zope.sendmail/3.5.0/zope.sendmail-3.5.0.tar.gz

---
+++ packages/zope.sendmail/zope.sendmail-3.5.0.tar.gz/zope.sendmail-3.5.0/setup.py
@@ -0,0 +1,50 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.sendmail package
+
+$Id: setup.py 88033 2008-07-05 09:40:31Z andreasjung $
+"""
+from setuptools import setup, find_packages
+
+
+setup(name='zope.sendmail',
+      version = '3.5.0',
+      url='http://cheeseshop.python.org/pypi/zope.sendmail',
+      license='ZPL 2.1',
+      description='Zope sendmail',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description='\n\n'.join([
+          open('README.txt').read(),
+          open('CHANGES.txt').read(),
+          ]),
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['setuptools',
+                        'ZODB3',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.schema',
+                        'zope.security',
+                        'zope.app.component',
+                       ],
+      include_package_data = True,
+      zip_safe = False,
+      )
