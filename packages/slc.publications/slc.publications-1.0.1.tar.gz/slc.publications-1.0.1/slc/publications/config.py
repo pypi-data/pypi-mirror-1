295dbf67a46242d3e1a058184b39b9aead993788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:44 +0000

    Add package slc.publications/1.0.1/slc.publications-1.0.1.tar.gz

---
+++ packages/slc.publications/slc.publications-1.0.1.tar.gz/slc.publications-1.0.1/slc/publications/config.py
@@ -0,0 +1,11 @@
+
+__author__ = """SYSLAB.COM <info@syslab.com>"""
+__docformat__ = 'plaintext'
+
+PROJECTNAME = "slc.publications"
+product_globals = globals()
+
+# Set your default author here
+AUTHOR = u"European Agency for Safety and Health at Work" 
+
+DEFAULT_ADD_CONTENT_PERMISSION = "Add portal content"
