0b9581b533ad2ac5a2e66ec2338c10b6069a31ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:59 +0000

    Add package taras.recipe.distutils/0.1.3final/taras.recipe.distutils-0.1.3final.tar.gz

---
+++ packages/taras.recipe.distutils/taras.recipe.distutils-0.1.3final.tar.gz/taras.recipe.distutils-0.1.3final/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.3'
+
+setup(name='taras.recipe.distutils',
+    version=version,
+    description="Recipe for zc.buildout that downloads one or multiple distutils-archives and installs them without requiring for zc.distutils to be installed.",
+    long_description="""\
+""",
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Plugins',
+          'Framework :: Buildout',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Installation/Setup',
+          'License :: OSI Approved :: BSD License',
+          ],
+    keywords='distutils buildout',
+    author='Taras Mankovski',
+    author_email='tarasm@gmail.com',
+    url='http://github.com/taras/taras.recipe.distutils',
+    license='BSD License',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['taras', 'taras.recipe'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points={'zc.buildout':
+      ['default = taras.recipe.distutils:Recipe']
+      },
+)
