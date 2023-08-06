9fda72a1369e5607c465dc7f598711214ff2b29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:34 +0000

    Add package mailman/3.0.0a3/mailman-3.0.0a3-py2.6.egg

---
+++ packages/mailman/mailman-3.0.0a3-py2.6.egg/mailman/interfaces/database.py
@@ -0,0 +1,97 @@
+# Copyright (C) 2007-2009 by the Free Software Foundation, Inc.
+#
+# This file is part of GNU Mailman.
+#
+# GNU Mailman is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# GNU Mailman is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# GNU Mailman.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Interfaces for database interaction.
+
+By providing an object with this interface and declaring it in a package
+setup.py file as an entry point in the 'mailman.database' group with the name
+'initializer', you can distribute entirely different database layers for
+Mailman's back end.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+__metaclass__ = type
+__all__ = [
+    'DatabaseError',
+    'IDatabase',
+    'SchemaVersionMismatchError',
+    ]
+
+from zope.interface import Interface, Attribute
+
+from mailman.interfaces.errors import MailmanError
+from mailman.version import DATABASE_SCHEMA_VERSION
+
+
+
+class DatabaseError(MailmanError):
+    """A problem with the database occurred."""
+
+
+class SchemaVersionMismatchError(DatabaseError):
+    """The database schema version number did not match what was expected."""
+
+    def __init__(self, got):
+        super(SchemaVersionMismatchError, self).__init__()
+        self._got = got
+
+    def __str__(self):
+        return ('Incompatible database schema version '
+                '(got: {0}, expected: {1})'.format(
+                    self._got, DATABASE_SCHEMA_VERSION))
+
+
+
+class IDatabase(Interface):
+    """Database layer interface."""
+
+    def initialize(debug=None):
+        """Initialize the database layer, using whatever means necessary.
+
+        :param debug: When None (the default), the configuration file
+            determines whether the database layer should have increased
+            debugging or not.  When True or False, this overrides the
+            configuration file setting.
+        """
+
+    def _reset():
+        """Reset the database to its pristine state.
+
+        This is only used by the test framework.
+        """
+
+    def begin():
+        """Begin the current transaction."""
+
+    def commit():
+        """Commit the current transaction."""
+
+    def abort():
+        """Abort the current transaction."""
+
+    list_manager = Attribute(
+        """The IListManager instance provided by the database layer.""")
+
+    user_manager = Attribute(
+        """The IUserManager instance provided by the database layer.""")
+
+    message_store = Attribute(
+        """The IMessageStore instance provided by the database layer.""")
+
+    pendings = Attribute(
+        """The IPending instance provided by the database layer.""")
---
+++ packages/mailman/mailman-3.0.0a3-py2.6.egg/mailman/queue/outgoing.py
@@ -0,0 +1,130 @@
+# Copyright (C) 2000-2009 by the Free Software Foundation, Inc.
+#
+# This file is part of GNU Mailman.
+#
+# GNU Mailman is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# GNU Mailman is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# GNU Mailman.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Outgoing queue runner."""
+
+import os
+import sys
+import socket
+import logging
+
+from datetime import datetime
+from lazr.config import as_timedelta
+
+from mailman.config import config
+from mailman.core import errors
+from mailman.queue import Runner
+from mailman.queue.bounce import BounceMixin
+from mailman.utilities.modules import find_name
+
+
+# This controls how often _do_periodic() will try to deal with deferred
+# permanent failures.  It is a count of calls to _do_periodic()
+DEAL_WITH_PERMFAILURES_EVERY = 10
+
+log = logging.getLogger('mailman.error')
+
+
+
+class OutgoingRunner(Runner, BounceMixin):
+    """The outgoing queue runner."""
+
+    def __init__(self, slice=None, numslices=1):
+        Runner.__init__(self, slice, numslices)
+        BounceMixin.__init__(self)
+        # We look this function up only at startup time.
+        self._func = find_name(config.mta.outgoing)
+        # This prevents smtp server connection problems from filling up the
+        # error log.  It gets reset if the message was successfully sent, and
+        # set if there was a socket.error.
+        self._logged = False
+        self._retryq = config.switchboards['retry']
+
+    def _dispose(self, mlist, msg, msgdata):
+        # See if we should retry delivery of this message again.
+        deliver_after = msgdata.get('deliver_after', datetime.fromtimestamp(0))
+        if datetime.now() < deliver_after:
+            return True
+        # Make sure we have the most up-to-date state
+        try:
+            pid = os.getpid()
+            self._func(mlist, msg, msgdata)
+            # Failsafe -- a child may have leaked through.
+            if pid <> os.getpid():
+                log.error('child process leaked thru: %s', pid)
+                os._exit(1)
+            self._logged = False
+        except socket.error:
+            # There was a problem connecting to the SMTP server.  Log this
+            # once, but crank up our sleep time so we don't fill the error
+            # log.
+            port = int(config.mta.port)
+            if port == 0:
+                port = 'smtp'
+            # Log this just once.
+            if not self._logged:
+                log.error('Cannot connect to SMTP server %s on port %s',
+                          config.mta.host, port)
+                self._logged = True
+            return True
+        except errors.SomeRecipientsFailed, e:
+            # Handle local rejects of probe messages differently.
+            if msgdata.get('probe_token') and e.permfailures:
+                self._probe_bounce(mlist, msgdata['probe_token'])
+            else:
+                # Delivery failed at SMTP time for some or all of the
+                # recipients.  Permanent failures are registered as bounces,
+                # but temporary failures are retried for later.
+                #
+                # BAW: msg is going to be the original message that failed
+                # delivery, not a bounce message.  This may be confusing if
+                # this is what's sent to the user in the probe message.  Maybe
+                # we should craft a bounce-like message containing information
+                # about the permanent SMTP failure?
+                if e.permfailures:
+                    self._queue_bounces(mlist.fqdn_listname, e.permfailures,
+                                        msg)
+                # Move temporary failures to the qfiles/retry queue which will
+                # occasionally move them back here for another shot at
+                # delivery.
+                if e.tempfailures:
+                    now = datetime.now()
+                    recips = e.tempfailures
+                    last_recip_count = msgdata.get('last_recip_count', 0)
+                    deliver_until = msgdata.get('deliver_until', now)
+                    if len(recips) == last_recip_count:
+                        # We didn't make any progress, so don't attempt
+                        # delivery any longer.  BAW: is this the best
+                        # disposition?
+                        if now > deliver_until:
+                            return False
+                    else:
+                        # Keep trying to delivery this message for a while
+                        deliver_until = now + as_timedelta(
+                            config.mta.delivery_retry_period)
+                    msgdata['last_recip_count'] = len(recips)
+                    msgdata['deliver_until'] = deliver_until
+                    msgdata['recips'] = recips
+                    self._retryq.enqueue(msg, msgdata)
+        # We've successfully completed handling of this message
+        return False
+
+    _do_periodic = BounceMixin._do_periodic
+
+    def _clean_up(self):
+        BounceMixin._clean_up(self)
+        Runner._clean_up(self)
