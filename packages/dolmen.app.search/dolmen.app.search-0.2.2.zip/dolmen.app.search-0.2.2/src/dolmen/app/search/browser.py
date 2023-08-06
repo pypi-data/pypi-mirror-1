37fe14d8f52c049b1a768bfc9fa527fc382f9793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:25 +0000

    Add package dolmen.app.search/0.2.2/dolmen.app.search-0.2.2.zip

---
+++ packages/dolmen.app.search/dolmen.app.search-0.2.2.zip/dolmen.app.search-0.2.2/src/dolmen/app/search/browser.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import grok
+from zope.interface import Interface
+from zope.component import getUtility
+from dolmen.app.layout import Page, master
+from dolmen.app.search import ICatalogSearcher
+
+
+class Search(grok.Viewlet):
+    grok.context(Interface)
+    grok.name('search.viewlet')
+    grok.viewletmanager(master.Top)
+    grok.order(10)
+    grok.require("dolmen.content.View")
+
+    def update(self):
+        self.term = self.request.form.get('search_term', u'')
+
+
+class Results(Page):
+    grok.context(Interface)
+    grok.name('search.result')
+    grok.require("dolmen.content.View")
+
+    def update(self):
+        searcher = getUtility(ICatalogSearcher, 'searcher.sitecatalog')
+        term = self.request.form.get('search_term', u'')
+        if term:
+            self.results = searcher.search(term + '*')
+        else:
+            self.results = []
+        self.label = u"Found %i results for %s" % (len(self.results), term)
