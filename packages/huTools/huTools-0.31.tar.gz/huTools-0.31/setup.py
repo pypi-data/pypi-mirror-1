51b7764ef9dcb47a2495d9decc17bd4a57e452aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:44 +0000

    Add package huTools/0.31/huTools-0.31.tar.gz

---
+++ packages/huTools/huTools-0.31.tar.gz/huTools-0.31/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='huTools',
+      maintainer='Maximillian Dornseif',
+      maintainer_email='md@hudora.de',
+      url='https://cybernetics.hudora.biz/projects/wiki/huTools',
+      version='0.31',
+      description='Various tiny tools and toys to make Python coding less work more fun.',
+      long_description='''huTools is a collection of many totally non earth shattering modules:
+      Various tiny tools and toys to make Python coding less work more fun:
+      
+      * calendar              - date based calculations and format conversions for HTTP and ATOM.
+      * checksummming         - calculate various checksums including EAN/GTIN/NVE/SSCC and DPD GeoPost
+      * luids                 - locally unique user-ids with strong guarantees of beeing unique.
+      * printing              - access printers (very primitive)
+      * ReReadingConfigParser - ConfigParser which detects changes in config files
+      * unicode               - real-world unicode handling (very primitive)
+      * world                 - country codes (very primitive)''',
+      license='BSD',
+      classifiers=['Intended Audience :: Developers',
+                   'Programming Language :: Python'],
+      
+      packages = find_packages(),
+      zip_safe = False,      
+)
