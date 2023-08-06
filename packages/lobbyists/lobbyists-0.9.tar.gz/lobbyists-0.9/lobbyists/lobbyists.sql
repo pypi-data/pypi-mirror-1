40dfc6d76d334752d8da638107afdaedaae37761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:06 +0000

    Add package lobbyists/0.9/lobbyists-0.9.tar.gz

---
+++ packages/lobbyists/lobbyists-0.9.tar.gz/lobbyists-0.9/lobbyists/lobbyists.sql
@@ -0,0 +1,228 @@
+DROP TABLE IF EXISTS filing;
+DROP TABLE IF EXISTS org;
+DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS government_entity;
+DROP TABLE IF EXISTS url;
+DROP TABLE IF EXISTS registrant;
+DROP TABLE IF EXISTS country;
+DROP TABLE IF EXISTS filing_registrant;
+DROP TABLE IF EXISTS state;
+DROP TABLE IF EXISTS client_status;
+DROP TABLE IF EXISTS state_or_local_gov;
+DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS lobbyist_status;
+DROP TABLE IF EXISTS lobbyist_indicator;
+DROP TABLE IF EXISTS lobbyist;
+DROP TABLE IF EXISTS filing_lobbyists;
+DROP TABLE IF EXISTS govt_entity;
+DROP TABLE IF EXISTS filing_govt_entities;
+DROP TABLE IF EXISTS issue_code;
+DROP TABLE IF EXISTS issue;
+DROP TABLE IF EXISTS filing_issues;
+DROP TABLE IF EXISTS affiliated_org;
+DROP TABLE IF EXISTS affiliated_org_urls;
+DROP TABLE IF EXISTS filing_affiliated_orgs;
+
+DROP INDEX IF EXISTS lobbyist_index;
+DROP INDEX IF EXISTS client_index;
+DROP INDEX IF EXISTS registrant_index;
+DROP INDEX IF EXISTS affiliated_org_index;
+
+CREATE TABLE filing(
+  id VARCHAR(36) PRIMARY KEY,
+  type VARCHAR(64),
+  year INTEGER,
+  period VARCHAR(64),
+  filing_date VARCHAR(20),      -- ISO 8601 extended date+time format
+  amount INTEGER,
+  registrant REFERENCES registrant,  -- optional
+  client REFERENCES client -- optional
+);
+
+CREATE TABLE org(
+  name VARCHAR(256) PRIMARY KEY ON CONFLICT IGNORE
+);
+
+CREATE TABLE person(
+  name VARCHAR(256) PRIMARY KEY ON CONFLICT IGNORE
+);
+
+-- Unfortunately, this isn't always a valid URL. It may contain all
+-- sorts of random text.
+CREATE TABLE url(
+  url VARCHAR(256) PRIMARY KEY ON CONFLICT IGNORE
+);
+
+CREATE TABLE country(
+  name VARCHAR(64) PRIMARY KEY ON CONFLICT IGNORE
+);
+
+CREATE TABLE state(
+  name VARCHAR(64) PRIMARY KEY ON CONFLICT IGNORE
+);
+
+CREATE TABLE registrant(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  address VARCHAR(256),
+  description VARCHAR(256),
+  country REFERENCES country,
+  senate_id INTEGER,
+  name REFERENCES org,
+  ppb_country REFERENCES country
+);
+
+CREATE TABLE client_status(
+  status VARCHAR(32) PRIMARY KEY
+);
+
+CREATE TABLE state_or_local_gov(
+  val VARCHAR(7) PRIMARY KEY
+);
+  
+CREATE TABLE client(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  country REFERENCES country,
+  senate_id INTEGER,
+  name REFERENCES org,
+  ppb_country REFERENCES country,
+  state REFERENCES state,
+  ppb_state REFERENCES state,
+  status REFERENCES client_status,
+  description VARCHAR(256),
+  state_or_local_gov REFERENCES state_or_local_gov,
+  contact_name VARCHAR(256) REFERENCES person
+);
+
+CREATE TABLE lobbyist_status(
+  status VARCHAR(32) PRIMARY KEY
+);
+
+CREATE TABLE lobbyist_indicator(
+  status VARCHAR(16) PRIMARY KEY
+);
+
+CREATE TABLE lobbyist(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name REFERENCES person,
+  status REFERENCES lobbyist_status,
+  indicator REFERENCES lobbyist_indicator,
+  official_position VARCHAR(256)
+);
+
+-- Lobbyists elements in the XML documents are badly broken. Among
+-- other problems, filings sometimes list the exact same Lobbyist
+-- element (with identical attribute values) more than once. When
+-- these are encountered, just ignore any occurrences after the
+-- first. (In the future, it might be nice to flag these records so
+-- they can be reported to the Senate.)
+CREATE TABLE filing_lobbyists(
+  filing REFERENCES filing,
+  lobbyist REFERENCES lobbyist,
+  PRIMARY KEY(filing, lobbyist) ON CONFLICT IGNORE
+);
+
+CREATE TABLE govt_entity(
+  name VARCHAR(64) PRIMARY KEY ON CONFLICT IGNORE
+);
+
+-- Filings sometimes list the exact same GovernmentEntity more than
+-- once. Just ignore any occurrences after the first. (In the future,
+-- it might be nice to flag these records so they can be reported to
+-- the Senate.)
+CREATE TABLE filing_govt_entities(
+  filing REFERENCES filing,
+  govt_entity REFERENCES govt_entity,
+  PRIMARY KEY(filing, govt_entity) ON CONFLICT IGNORE
+);
+
+CREATE TABLE issue_code(
+  code VARCHAR(80) PRIMARY KEY ON CONFLICT IGNORE
+);
+
+CREATE TABLE issue(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  code REFERENCES code,
+  specific_issue VARCHAR
+);
+
+CREATE TABLE filing_issues(
+  filing REFERENCES filing,
+  issue REFERENCES issue,
+  PRIMARY KEY(filing, issue)
+);
+
+CREATE TABLE affiliated_org(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name REFERENCES org,
+  country REFERENCES country,
+  ppb_country REFERENCES country
+);
+
+CREATE TABLE affiliated_org_urls(
+  org REFERENCES affiliated_org,
+  url REFERENCES url,
+  PRIMARY KEY(org, url) ON CONFLICT IGNORE
+);
+
+CREATE TABLE filing_affiliated_orgs(
+  filing REFERENCES filing,
+  org REFERENCES affiliated_org,
+  PRIMARY KEY(filing, org) ON CONFLICT IGNORE
+);
+
+-- Create indexes for tables that get looked up during import. They
+-- make a HUGE difference in import performance.
+CREATE UNIQUE INDEX lobbyist_index ON lobbyist(
+  name,
+  status,
+  indicator,
+  official_position
+);
+
+CREATE UNIQUE INDEX client_index ON client(
+  country,
+  senate_id,
+  name,
+  ppb_country,
+  state,
+  ppb_state,
+  status,
+  description,
+  state_or_local_gov,
+  contact_name
+);
+
+CREATE UNIQUE INDEX registrant_index ON registrant(
+  address,
+  description,
+  country,
+  senate_id,
+  name,
+  ppb_country
+);
+
+CREATE UNIQUE INDEX affiliated_org_index ON affiliated_org(
+  name,
+  country,
+  ppb_country
+);
+
+-- 3 possible state/local govt values.
+INSERT INTO state_or_local_gov VALUES('unspecified');
+INSERT INTO state_or_local_gov VALUES('n');
+INSERT INTO state_or_local_gov VALUES('y');
+
+-- 3 possible client statuses.
+INSERT INTO client_status VALUES('active');
+INSERT INTO client_status VALUES('terminated');
+INSERT INTO client_status VALUES('administratively terminated');
+
+-- 4 possible lobbyist statuses.
+INSERT INTO lobbyist_status VALUES('active');
+INSERT INTO lobbyist_status VALUES('terminated');
+INSERT INTO lobbyist_status VALUES('undetermined');
+
+-- 3 possible lobbyist indicators
+INSERT INTO lobbyist_indicator VALUES('not covered');
+INSERT INTO lobbyist_indicator VALUES('covered');
+INSERT INTO lobbyist_indicator VALUES('undetermined');
---
+++ packages/lobbyists/lobbyists-0.9.tar.gz/lobbyists-0.9/lobbyists/util.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+#
+# util.py - Utility functions for the lobbyists package.
+# Copyright (C) 2008 by Drew Hess <dhess@bothan.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+"""Utility functions for the lobbyists package."""
+
+from . import lobbyists
+import sqlite3
+import os.path
+
+
+def load_db(docs, dbname, clobber=False, commit_per_doc=False):
+    """Load filing records from lobbyist documents into an sqlite3 database.
+
+    Parses and imports filing records from one or more Senate
+    LD-1/LD-2 XML documents into an sqlite3 database. Records are
+    parsed and imported one at a time.
+
+    docs - A sequence of URLs or filenames identifying the LD-1/LD-2
+    XML documents load load.
+
+    dbname - The filename of the sqlite3 database to load. If the
+    database doesn't exist, load_db creates it.
+
+    clobber - If True, and the database already exists, the database's
+    contents will be clobbered prior to loading.
+
+    commit_per_doc - If True, load_db will commit the changes to the
+    database after loading each individual document. If False (the
+    default), load_db only commits when the last document is
+    loaded. Per-document committing ensures that successfully loaded
+    documents are committed to the database in case of parsing or
+    importing errors in subsequent documents, but is slower.
+
+    This function has the side-effect of creating and/or modifying the
+    database.
+
+    Returns the database's sqlite3.Connection object.
+
+    """
+    create_db = clobber or not os.path.exists(dbname)
+    con = sqlite3.connect(dbname)
+    if create_db:
+        lobbyists.create_db(con)
+    for doc in docs:
+        lobbyists.import_filings(con.cursor(), lobbyists.parse_filings(doc))
+        if commit_per_doc:
+            con.commit()
+    if not commit_per_doc:
+        con.commit()
+    return con
+
+
+def load_main(argv=None):
+    """Run the lobbyists-load script directly from Python.
+
+    Note that argv[0] is the program name.
+
+    """
+    import optparse
+    import sys
+
+    if argv is None:
+        argv = sys.argv
+    usage = """%prog [OPTIONS] db doc.xml ...
+
+Parse one or more Senate LD-1/LD-2 XML documents and load them into an
+sqlite3 database.
+
+Each document may be identified either by a URL or a file, so long as
+it's a valid Senate LD-1/LD-2 XML document.
+
+If db doesn't exist, %prog will create it prior to loading the first
+document."""
+    parser = optparse.OptionParser(usage=usage,
+                                   version=lobbyists.VERSION)
+    parser.add_option('-C', '--clobber-database', action='store_true',
+                      dest='clobber',
+                      help='clobber the existing database contents prior to ' \
+                          'loading first document')
+    parser.add_option('-c', '--commit-per-document', action='store_true',
+                      dest='commit',
+                      help='commit changes to the database after importing ' \
+                          'each document (default is to commit only after ' \
+                          'all documents are imported)')
+    (options, args) = parser.parse_args(argv[1:])
+    if len(args) < 2:
+        parser.error('specify exactly one sqlite3 database and at least one ' \
+                         'XML document')
+    con = load_db(args[1:], args[0], options.clobber, options.commit)
+    con.close()
+    return 0
