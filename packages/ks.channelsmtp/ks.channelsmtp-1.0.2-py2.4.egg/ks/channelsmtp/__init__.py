bd58af42fbbb9d6499fb2a756aea7f4d6285b869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:29 +0000

    Add package ks.channelsmtp/1.0.2/ks.channelsmtp-1.0.2-py2.4.egg

---
+++ packages/ks.channelsmtp/ks.channelsmtp-1.0.2-py2.4.egg/ks/channelsmtp/__init__.py
@@ -0,0 +1,14 @@
+### -*- coding: utf-8 -*- #############################################
+# Разработано компанией Ключевые Решения (http://keysolutions.ru/)
+# Все права защищены, 2006-2007
+#
+# Developed by Key Solutions (http://keysolutions.ru/)
+# All right reserved, 2006-2007
+#######################################################################
+# $Id: __init__.py 12583 2007-11-05 17:53:26Z uoxyum $
+#######################################################################
+
+# Make it a Python package
+
+import zope.i18nmessageid
+_ = zope.i18nmessageid.MessageFactory('ks')
