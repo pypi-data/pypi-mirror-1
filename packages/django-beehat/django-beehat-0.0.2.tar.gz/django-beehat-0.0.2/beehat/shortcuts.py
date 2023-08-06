4fe44706db8462d968d0c07e08d83b5980f18aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:12 +0000

    Add package django-beehat/0.0.2/django-beehat-0.0.2.tar.gz

---
+++ packages/django-beehat/django-beehat-0.0.2.tar.gz/django-beehat-0.0.2/beehat/shortcuts.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 John Paulett (john -at- paulett.org)
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from django.shortcuts import render_to_response
+
+def render_response(request, *args, **kwargs):
+    """Adds the RequestContext tp django.shortcuts.render_to_response."""
+    kwargs['context_instance'] = RequestContext(request)
+    return render_to_response(*args, **kwargs)
