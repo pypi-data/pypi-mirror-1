508437b22705cc615156e39c37c25198d87a9078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:35 +0000

    Add package zopyx.smartprintng.server/0.5.0/zopyx.smartprintng.server-0.5.0.tar.gz

---
+++ packages/zopyx.smartprintng.server/zopyx.smartprintng.server-0.5.0.tar.gz/zopyx.smartprintng.server-0.5.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.5.0'
+
+setup(name='zopyx.smartprintng.server',
+      version=version,
+      description="ZOPYX SmartPrintNG Server",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='SmartPrintNG Conversion repoze.bfg',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zopyx', 'zopyx.smartprintng'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'repoze.bfg',
+          'uuid',
+          'zopyx.convert2',
+          'zope.sendmail',
+          'transaction',
+          # -*- Extra requirements: -*-
+      ],
+      test_suite='nose.collector',
+      tests_require=('nose',),
+      entry_points="""\
+      [paste.app_factory]
+      app = zopyx.smartprintng.server.run:app
+      """
+      )
