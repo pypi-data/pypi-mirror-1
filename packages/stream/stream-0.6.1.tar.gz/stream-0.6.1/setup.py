e42394734338e0a3379f738047029e1bb2be7f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:17 +0000

    Add package stream/0.6.1/stream-0.6.1.tar.gz

---
+++ packages/stream/stream-0.6.1.tar.gz/stream-0.6.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os, sys
+from distutils.core import setup
+
+sys.path.insert(0, os.path.dirname(__file__))
+try:
+    import stream
+finally:
+    del sys.path[0]
+
+classifiers = """
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Utilities
+"""
+
+setup(
+	name = 'stream',
+	version = stream.__version__,
+	description = stream.__doc__.split('\n')[0],
+  long_description = ''.join(open('stream.py').readlines()[2:106]),
+	author = 'Anh Hai Trinh',
+	author_email = 'moc.liamg@hnirt.iah.hna:otliam'[::-1],
+  keywords='lazy iterable iterator generator stream data flow functional processing',
+	url = 'http://github.com/aht/stream.py',
+	platforms=['any'],
+	classifiers=filter(None, classifiers.split("\n")),
+	py_modules = ['stream']
+)
