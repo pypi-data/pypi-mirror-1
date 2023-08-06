cf1ce0cbe796d0d9e54190e877346ee0a202729d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:02 +0000

    Add package lpqueue/0.1.2/lpqueue-0.1.2.tar.gz

---
+++ packages/lpqueue/lpqueue-0.1.2.tar.gz/lpqueue-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="lpqueue",
+      version='0.1.2',
+      description='Lightweight Python Network Message Queuing',
+      author='Nick Devito',
+      author_email='nick@nick125.com',
+      url='http://nick125.com/projects/lpqueue/',
+      packages=['lpqueue'],
+      scripts=['lpserver'],
+)
