e399d9d85b71963a4c08577f8e7bfd858b3083d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:50 +0000

    Add package collective.collage.easyslider/1.0.2/collective.collage.easyslider-1.0.2.tar.gz

---
+++ packages/collective.collage.easyslider/collective.collage.easyslider-1.0.2.tar.gz/collective.collage.easyslider-1.0.2/README.txt
@@ -0,0 +1,4 @@
+Introduction
+============
+
+Integrates Css Globe Easy Slider (http://cssglobe.com/) into Collage.
---
+++ packages/collective.collage.easyslider/collective.collage.easyslider-1.0.2.tar.gz/collective.collage.easyslider-1.0.2/collective/collage/easyslider/browser/viewlets.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 11/12/2009
+
+@author: jpg
+'''
+from Products.Collage.browser.viewlet import AliasViewlet
+
+
+class ManageViewlet(AliasViewlet):
+    """ Manage viewlet for easyslider parameters
+    """
+    
