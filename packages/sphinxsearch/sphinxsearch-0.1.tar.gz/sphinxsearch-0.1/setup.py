749e1c323e153515fdf8c40900ed94003820b1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package sphinxsearch/0.1/sphinxsearch-0.1.tar.gz

---
+++ packages/sphinxsearch/sphinxsearch-0.1.tar.gz/sphinxsearch-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='sphinxsearch',
+    version='0.1',
+    description='A module for querying the sphinx search daemon',
+    author='Andrew Aksyonoff',
+    author_email='shodan at shodan ru',
+    url='http://www.sphinxsearch.com/',
+    packages=['sphinxsearch'],
+)
