e4661c17cb1da23656cb79133f80926352159f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:34 +0000

    Add package libpg/1.1/libpg-1.1.zip

---
+++ packages/libpg/libpg-1.1.zip/libpg-1.1/libpgSetup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+setup (name = "libpg",
+        version = "1.1",
+        maintainer = "Rich Healey",
+        maintainer_email = "healey.rich@gmail.com",
+        py_modules=['libpg', 'debug_globals', 'debug_decs']
+                        
+                )
