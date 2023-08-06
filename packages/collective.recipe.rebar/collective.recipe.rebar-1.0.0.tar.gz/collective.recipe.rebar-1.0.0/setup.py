d302bfbd4c2082598c67448a7bc0a67d845c3247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:57 +0000

    Add package collective.recipe.rebar/1.0.0/collective.recipe.rebar-1.0.0.tar.gz

---
+++ packages/collective.recipe.rebar/collective.recipe.rebar-1.0.0.tar.gz/collective.recipe.rebar-1.0.0/setup.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of collective.recipe.rebar
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0.0'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('collective', 'recipe', 'rebar', 'README.txt')
+    + '\n' +
+    'Contributors\n' 
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' + 
+    read('CHANGES.txt')
+    + '\n' +
+   'Download\n'
+    '********\n'
+    )
+entry_point = 'collective.recipe.rebar:Recipe'
+entry_points = {"zc.buildout": ["default = %s" % entry_point]}
+
+tests_require=['zope.testing', 'zc.buildout']
+
+setup(name='collective.recipe.rebar',
+      version=version,
+      description="Recipe for rebar based applications",
+      long_description=long_description,
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Zope Public License',
+        ],
+      keywords='rebar recipe buildout erlang',
+      author='Ali Sabil',
+      author_email='ali.sabil@gmail.com',
+      url='',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.recipe'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=['setuptools',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'collective.recipe.rebar.tests.test_docs.test_suite',
+      entry_points=entry_points,
+      )
