a035346c97592d8b7926c7f64ce1cf22ae8cc92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:56 +0000

    Add package zope.testbrowser/3.5.1/zope.testbrowser-3.5.1.tar.gz

---
+++ packages/zope.testbrowser/zope.testbrowser-3.5.1.tar.gz/zope.testbrowser-3.5.1/setup.py
@@ -0,0 +1,70 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.testbrowser package
+
+$Id: setup.py 92016 2008-10-10 17:49:38Z sidnei $
+"""
+import os
+from setuptools import setup, find_packages
+
+long_description = (
+    '.. contents::\n\n'
+    + open('README.txt').read()
+    + '\n\n'
+    + open(os.path.join('src', 'zope', 'testbrowser', 'README.txt')).read()
+    + '\n\n'
+    + open('CHANGES.txt').read()
+    )
+
+setup(
+    name = 'zope.testbrowser',
+    version = '3.5.1',
+    url = 'http://pypi.python.org/pypi/zope.testbrowser',
+    license = 'ZPL 2.1',
+    description = 'Programmable browser for functional black-box tests',
+    author = 'Zope Corporation and Contributors',
+    author_email = 'zope3-dev@zope.org',
+    long_description = long_description,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Internet :: WWW/HTTP',
+        ],
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['zope',],
+    tests_require = ['zope.testing'],
+    install_requires = [
+        'ClientForm >= 0.2.8',
+        'mechanize',
+        'setuptools',
+        'zope.interface',
+        'zope.schema',
+        ],
+    extras_require = dict(
+        test = [
+            'zope.app.component',
+            'zope.app.folder',
+            'zope.app.securitypolicy',
+            'zope.app.testing',
+            'zope.app.zcmlfiles',
+            ],
+        ),
+    include_package_data = True,
+    zip_safe = False,
+    )
