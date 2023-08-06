749734296005ac45d95f66cb83a935e6374affc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package nowandnext/0.2.5.dev-r425/nowandnext-0.2.5.dev_r425-py2.4.egg

---
+++ packages/nowandnext/nowandnext-0.2.5.dev_r425-py2.4.egg/nowandnext/digiguide/dgxml.py
@@ -0,0 +1,77 @@
+import elementtree.ElementTree as ET
+from nowandnext.calendar.convert import timedelta_to_minutes
+from nowandnext.digiguide.categories import CATEGORIES
+
+class dgxmlfeed( object ):
+    def __init__(self, channels=None ):
+        assert type(channels) == list
+        
+        self.oldestStart = None
+        self.newestEnd = None
+        self.itemCount = 0
+        self._xml = ET.Element("channels")
+        
+        if channels == None:
+            channels = []
+            
+        for channel in channels:
+            self._xml.append( channel )
+            
+    @classmethod
+    def program(cls, scheduleitem=None ):
+        xml = ET.Element("programme")
+        
+        xml.attrib["name"] = scheduleitem.getTitle()
+        xml.attrib["startdate"] = scheduleitem.getStartTime().strftime("%Y/%m/%d")
+        xml.attrib["starttime"] = scheduleitem.getStartTime().strftime("%H%M")
+        
+        category = scheduleitem.getCategory()
+        assert category in CATEGORIES, "Category %s is unknown in the list of categories" % category
+        xml.attrib["category"] = scheduleitem.getCategory()
+        
+        td_duration = scheduleitem.getEndTime() - scheduleitem.getStartTime()
+        mins_duration = timedelta_to_minutes( td_duration )
+        
+        xml.attrib["duration"] = str( mins_duration )
+        
+        synopsis = ET.Element("synopsis")
+        synopsis.text = scheduleitem.getDescription()
+        
+        xml.append( synopsis )
+        
+        return xml
+        
+            
+    @classmethod
+    def channel( cls, name, programs=None ):
+        if programs==None:
+            programs = []
+        
+        assert type(programs) == list
+        
+        xml = ET.Element("channel")
+        xml.attrib["name"] = name
+        
+        x_programs = ET.Element("programmes")
+        xml.append( x_programs )
+        
+        for program in programs:
+            x_programs.append( program )
+            
+        return xml
+
+    def dump( self, thefile ):
+        """
+        Write out the digiguide XML to a file.
+        """
+        
+        tree = ET.ElementTree( self._xml )
+        tree.write( thefile, encoding="utf8" )
+
+    def dumps( self ):
+        return
+    
+if __name__ == "__main__":
+    import sys
+    foo = dgxmlfeed( [ dgxmlfeed.channel("ResonanceFM"), ] )
+    foo.dump( sys.stdout )
