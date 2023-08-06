5c2643b73de6d807ab4bce833b3971e073b85785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:12 +0000

    Add package collective.groupdelegation/0.2/collective.groupdelegation-0.2.zip

---
+++ packages/collective.groupdelegation/collective.groupdelegation-0.2.zip/collective.groupdelegation-0.2/collective/groupdelegation/__init__.py
@@ -0,0 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+PGMPMessageFactory = MessageFactory('collective.groupdelegation')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
