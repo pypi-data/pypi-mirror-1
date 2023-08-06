0bf89a6539011dcbb74ef136ffb3c6db7a451097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:41 +0000

    Add package zope.index/3.5.0/zope.index-3.5.0.tar.gz

---
+++ packages/zope.index/zope.index-3.5.0.tar.gz/zope.index-3.5.0/setup.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.index package
+
+$Id$
+"""
+from setuptools import setup, find_packages
+
+long_description = (open('README.txt').read() +
+                    '\n\n' +
+                    open('CHANGES.txt').read())
+
+setup(name='zope.index',
+      version='3.5.0',
+      url='http://pypi.python.org/pypi/zope.index',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description="Indices for using with catalog like text, field, etc.",
+      long_description=long_description,
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope',],
+      extras_require={'test': ['zope.testing']},
+      install_requires=['setuptools',
+                        'ZODB3>=3.8.0b1',
+                        'zope.interface'],
+      include_package_data = True,
+      zip_safe=False,
+      )
