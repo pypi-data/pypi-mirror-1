efa08e0832f3d88cbea95e0918819cf777982f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:30 +0000

    Add package megrok.pagetemplate/0.2/megrok.pagetemplate-0.2.tar.gz

---
+++ packages/megrok.pagetemplate/megrok.pagetemplate-0.2.tar.gz/megrok.pagetemplate-0.2/src/megrok/pagetemplate/__init__.py
@@ -0,0 +1,4 @@
+from megrok.pagetemplate.components import PageTemplate
+from grokcore.viewlet import view
+from grokcore.view import template, templatedir, layer
+from grokcore.component import name
