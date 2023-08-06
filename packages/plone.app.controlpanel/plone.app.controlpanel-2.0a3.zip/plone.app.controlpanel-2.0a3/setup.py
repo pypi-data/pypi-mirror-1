9c1f93540a92ce6386644bc82bceed155106a011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:55 +0000

    Add package plone.app.controlpanel/2.0a3/plone.app.controlpanel-2.0a3.zip

---
+++ packages/plone.app.controlpanel/plone.app.controlpanel-2.0a3.zip/plone.app.controlpanel-2.0a3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '2.0a3'
+
+setup(name='plone.app.controlpanel',
+      version=version,
+      description="Formlib-based controlpanels for Plone.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone controlpanel formlib',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.controlpanel',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.app.form',
+        'plone.app.workflow',
+        'plone.fieldsets',
+        'plone.memoize',
+        'plone.protect',
+        'plone.locking',
+        'zope.ramcache',
+      ],
+      )
+
