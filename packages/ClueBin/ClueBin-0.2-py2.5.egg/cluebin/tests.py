ce6e15fbe5beba222e4434857d6058787f2aa704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:43 +0000

    Add package ClueBin/0.2/ClueBin-0.2-py2.5.egg

---
+++ packages/ClueBin/ClueBin-0.2-py2.5.egg/cluebin/tests.py
@@ -0,0 +1,20 @@
+import unittest
+import doctest
+
+
+def test_suite():
+    flags = doctest.ELLIPSIS
+    suite = unittest.TestSuite()
+    suite.addTest(doctest.DocTestSuite('cluebin.pastebin',
+                                       optionflags=flags))
+
+    return suite
+
+
+def main():
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite())
+
+
+if __name__ == '__main__':
+    main()
