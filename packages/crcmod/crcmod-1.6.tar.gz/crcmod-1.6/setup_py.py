59b26668c5554286fc5e1f6f2545e6dfa2c19c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:44 +0000

    Add package crcmod/1.6/crcmod-1.6.tar.gz

---
+++ packages/crcmod/crcmod-1.6.tar.gz/crcmod-1.6/setup_py.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+name='crcmod',
+version='1.6',
+description='CRC Generator',
+author='Ray Buvel',
+author_email='rlbuvel@gmail.com',
+url='http://crcmod.sourceforge.net/',
+packages=['crcmod'],
+
+)
