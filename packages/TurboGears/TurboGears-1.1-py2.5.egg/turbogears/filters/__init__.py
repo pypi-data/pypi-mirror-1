2955fb9f5047384152bfd16f904b9eeb86beb8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:32 +0000

    Add package TurboGears/1.1/TurboGears-1.1-py2.5.egg

---
+++ packages/TurboGears/TurboGears-1.1-py2.5.egg/turbogears/filters/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+"""Package collecting various request filters."""
+
+from base import NestedVariablesFilter, VirtualPathFilter
+from safemultipart import SafeMultipartFilter
