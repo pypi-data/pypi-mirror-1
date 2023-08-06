544a342ea06b3af736c3e0185ed0f45d93312380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:35 +0000

    Add package Products.PloneHelpCenter/1.5/Products.PloneHelpCenter-1.5-py2.4.egg

---
+++ packages/Products.PloneHelpCenter/Products.PloneHelpCenter-1.5-py2.4.egg/Products/PloneHelpCenter/interfaces.py
@@ -0,0 +1,14 @@
+from zope.interface import Interface
+
+class IHelpCenterContent(Interface):
+    """ The HelpCenter
+    """
+
+class IHelpCenterFolder(Interface):
+    """ Marker interface for PHCFolder class that is
+        the ancestory of the PHC containers.
+    """
+
+class IHelpCenterHowTo(Interface):
+    """ Marker interface for HelpCenterHowTo class.
+    """
