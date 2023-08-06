56278ee581865a02e4312425be93051b8b339e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:58 +0000

    Add package zope.app.pagetemplate/3.8.0/zope.app.pagetemplate-3.8.0.zip

---
+++ packages/zope.app.pagetemplate/zope.app.pagetemplate-3.8.0.zip/zope.app.pagetemplate-3.8.0/src/zope/app/pagetemplate/__init__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Zope-specific Page Templates
+
+$Id: __init__.py 106656 2009-12-16 20:38:11Z hannosch $
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
