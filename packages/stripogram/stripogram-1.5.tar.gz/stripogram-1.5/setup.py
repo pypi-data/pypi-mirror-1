8d737ee45b059e97f75f7dea5e1095b902deb966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:19 +0000

    Add package stripogram/1.5/stripogram-1.5.tar.gz

---
+++ packages/stripogram/stripogram-1.5.tar.gz/stripogram-1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stripogram",
+    version="1.5",
+    description="Strip-o-Gram HTML Conversion Library",
+    author="Chris Withers",
+    author_email="chris@simplistix.co.uk",
+    url="https://sourceforge.net/projects/squishdot",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    )
