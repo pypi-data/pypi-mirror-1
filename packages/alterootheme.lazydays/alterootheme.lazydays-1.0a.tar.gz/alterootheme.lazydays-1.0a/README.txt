767f8415e63b06413e9ac57600e09d8a58820e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:29 +0000

    Add package alterootheme.lazydays/1.0a/alterootheme.lazydays-1.0a.tar.gz

---
+++ packages/alterootheme.lazydays/alterootheme.lazydays-1.0a.tar.gz/alterootheme.lazydays-1.0a/README.txt
@@ -0,0 +1,7 @@
+alterootheme.lazydays Package Readme
+=========================
+
+Overview
+--------
+
+A Theme for Plone 3.0 based on OpenWebDesign.org Lazy Days theme
---
+++ packages/alterootheme.lazydays/alterootheme.lazydays-1.0a.tar.gz/alterootheme.lazydays-1.0a/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.0a'
+
+setup(name='alterootheme.lazydays',
+      version=version,
+      description="A Theme for Plone 3.0 based on OpenWebDesign.org Lazy Days theme",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='David Bain',
+      author_email='david.bain@alteroo.com',
+      url='http://themes.alteroo.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['alterootheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
