139f20d525051440c95aabdd180d5fcea3be4c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:11 +0000

    Add package betahaus.emaillogin/0.5/betahaus.emaillogin-0.5.tar.gz

---
+++ packages/betahaus.emaillogin/betahaus.emaillogin-0.5.tar.gz/betahaus.emaillogin-0.5/betahaus/emaillogin/skins/mail_password.py
@@ -0,0 +1,33 @@
+## Script (Python) "mail_password"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##title=Mail a user's password
+##parameters=
+
+from betahaus.emaillogin import email2username
+from Products.CMFPlone import PloneMessageFactory as pmf
+REQUEST=context.REQUEST
+
+
+email = REQUEST.get('email', None)
+if email:
+    found_username = email2username(context, email)
+    if found_username:
+        REQUEST.set('userid', found_username)
+    else:
+        context.plone_utils.addPortalMessage(pmf("Invalid email address."))
+        response = context.mail_password_form()
+        
+
+userid = REQUEST.get('userid', '')
+if userid != '':
+    try:
+        response = context.portal_registration.mailPassword(REQUEST['userid'], REQUEST)
+    except ValueError, e:
+        context.plone_utils.addPortalMessage(pmf(str(e)))
+        response = context.mail_password_form()
+        
+return response
