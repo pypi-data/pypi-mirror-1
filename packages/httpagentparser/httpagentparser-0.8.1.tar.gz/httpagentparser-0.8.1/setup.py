8ec0bd4e5ea4a93b99e9bcc7ec22638803bbc796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:15 +0000

    Add package httpagentparser/0.8.1/httpagentparser-0.8.1.tar.gz

---
+++ packages/httpagentparser/httpagentparser-0.8.1.tar.gz/httpagentparser-0.8.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='httpagentparser',
+    version='0.8.1',
+    description='Extracts OS Browser etc information from http user agent string',
+    packages=find_packages(),
+    author='Shekhar Tiwatne',
+    author_email='pythonic@gmail.com',
+    url="http://pythonik.blogspot.com",
+    license="http://www.zope.org/Resources/ZPL",
+    )
+
