390f4084f7c9e8e18f874b945876bb323837c29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:50 +0000

    Add package collective.dancefloor/0.3.1/collective.dancefloor-0.3.1.tar.gz

---
+++ packages/collective.dancefloor/collective.dancefloor-0.3.1.tar.gz/collective.dancefloor-0.3.1/collective/dancefloor/tools.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# File: tools.py
+#
+# Copyright (c) InQuant GmbH
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+__author__ = """Stefan Eletzhofer <stefan.eletzhofer@inquant.de>"""
+__docformat__ = 'plaintext'
+
+from collective.dancing.channel import ChannelContainer
+from collective.dancing.collector import CollectorContainer
+from collective.dancefloor.channels import LocalNewsletterLookup
+
+
+def add_tools(container):
+
+    if "channels" not in container.keys():
+        container["channels"] = ChannelContainer("channels")
+        channels = container.get("channels")
+        del channels['default-channel']
+
+    if "collectors" not in container.keys():
+        container["collectors"] = CollectorContainer("collectors")
+
+    if "newsletter_lookup" not in container.keys():
+        container["newsletter_lookup"] = LocalNewsletterLookup("collectors")
+
+# vim: set ft=python ts=4 sw=4 expandtab :
