a690ce88664171b7ab6bc7a6bcfe54343c35d905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package rdf.plugins.serializers.n3/0.9a1/rdf.plugins.serializers.n3-0.9a1.tar.gz

---
+++ packages/rdf.plugins.serializers.n3/rdf.plugins.serializers.n3-0.9a1.tar.gz/rdf.plugins.serializers.n3-0.9a1/n3/__init__.py
@@ -0,0 +1,11 @@
+#
+"""\
+A n3 serializer plugin for `rdf`.
+
+This plugin is intended to be installed into rdf.plugins.serializers.
+
+TODO: example usage.
+
+"""
+
+__version__ = "0.9a1"
---
+++ packages/rdf.plugins.serializers.n3/rdf.plugins.serializers.n3-0.9a1.tar.gz/rdf.plugins.serializers.n3-0.9a1/setup.py
@@ -0,0 +1,36 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+import n3 as package
+
+
+setup(
+    name = "rdf.plugins.serializers.n3",
+    version = package.__version__,
+    description = package.__doc__.split("\n")[0],
+    long_description = package.__doc__,
+    author = "Daniel 'eikeon' Krech",
+    author_email = "eikeon@eikeon.com",
+    maintainer = "Daniel 'eikeon' Krech",
+    maintainer_email = "eikeon@eikeon.com",
+    url = "http://rdflib.net/",
+    license = "http://rdflib.net/latest/LICENSE",
+    platforms = ["any"],
+    classifiers = ["Programming Language :: Python",
+                   "License :: OSI Approved :: BSD License",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Operating System :: OS Independent",
+                   "Natural Language :: English",
+                   ],
+
+    packages = ["n3"],
+
+    install_requires = ['rdf>=0.9a4'],
+
+    tests_require = ["nose==0.10.1"],
+
+    test_suite = 'nose.collector',
+
+    )
