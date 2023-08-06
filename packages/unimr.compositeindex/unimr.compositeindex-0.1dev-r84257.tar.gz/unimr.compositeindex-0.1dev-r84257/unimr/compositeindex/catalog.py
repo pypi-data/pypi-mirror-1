a786563bca1189aa194315e56f28f8e2a351e630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:49 +0000

    Add package unimr.compositeindex/0.1dev-r84257/unimr.compositeindex-0.1dev-r84257.tar.gz

---
+++ packages/unimr.compositeindex/unimr.compositeindex-0.1dev-r84257.tar.gz/unimr.compositeindex-0.1dev-r84257/unimr/compositeindex/catalog.py
@@ -0,0 +1,24 @@
+import logging
+
+from Acquisition import aq_parent
+
+from Products.ZCatalog.Catalog import Catalog
+
+from CompositeIndex import compositeSearchArgumentsMap
+
+from wrapper import wrap_method, call
+
+from config import PROJECTNAME
+
+logger = logging.getLogger(PROJECTNAME)
+
+
+def search(self, request, sort_index=None, reverse=0, limit=None, merge=1):
+
+    compositeSearchArgumentsMap(self,request)
+    
+    return call(self,'search',request, sort_index=sort_index, reverse=reverse, limit=limit, merge=merge)
+
+
+def patch():
+    wrap_method(Catalog, 'search', search)
