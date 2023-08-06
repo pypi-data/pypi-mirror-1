d3f264ec1407fda8abe9dc059c2d1bc14a9c2bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:52 +0000

    Add package boduch/0.1.9/boduch-0.1.9-py2.5.egg

---
+++ packages/boduch/boduch-0.1.9-py2.5.egg/boduch/subscription/data/set.py
@@ -0,0 +1,28 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from boduch.event import subscribe, EventSetPush, EventSetSet, EventSetSort,\
+                         EventSetPop, EventSetGet
+from boduch.handle import HandleSetPush, HandleSetSet, HandleSetSort,\
+                          HandleSetPop, HandleSetGet
+
+SubSetPush=subscribe(EventSetPush, HandleSetPush)
+SubSetSet=subscribe(EventSetSet, HandleSetSet)
+SubSetSort=subscribe(EventSetSort, HandleSetSort)
+SubSetPop=subscribe(EventSetPop, HandleSetPop)
+SubSetGet=subscribe(EventSetGet, HandleSetGet)
+
+__all__=['SubSetPush', 'SubSetSet', 'SubSetSort', 'SubSetPop', 'SubSetGet']
