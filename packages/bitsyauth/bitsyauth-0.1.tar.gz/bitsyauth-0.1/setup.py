443a2aef56679b8bd1a855fe2069c51d61f91c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:55 +0000

    Add package bitsyauth/0.1/bitsyauth-0.1.tar.gz

---
+++ packages/bitsyauth/bitsyauth-0.1.tar.gz/bitsyauth-0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='bitsyauth',
+      version=version,
+      description="form + digest auth middleware",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='auth',
+      author='Jeff Hammel',
+      author_email='k0scist@gmail.com',
+      url='http://k0s.org/hg/bitsyauth',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['Paste',
+                        'PasteScript',
+                        'markup',
+                        'skimpygimpy',
+                        ],
+      dependency_links=['https://svn.openplans.org/svn/standalone/markup#egg=markup',
+                        'http://svn.pythonpaste.org/Paste/trunk#egg=Paste',
+                        ],
+      entry_points="""
+      # -*- Entry points: -*-
+              
+      """,
+      )
