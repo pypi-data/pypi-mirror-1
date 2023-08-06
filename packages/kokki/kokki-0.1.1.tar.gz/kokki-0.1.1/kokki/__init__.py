6cb26f6eec22379075150e07fcbe766dd34ba3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:53 +0000

    Add package kokki/0.1.1/kokki-0.1.1.tar.gz

---
+++ packages/kokki/kokki-0.1.1.tar.gz/kokki-0.1.1/kokki/__init__.py
@@ -0,0 +1,14 @@
+
+__version__ = "0.1.1"
+
+from kokki import cookbook
+from kokki.base import *
+from kokki.cookbook import *
+from kokki.environment import *
+from kokki.packaging import *
+from kokki.providers import *
+from kokki.recipe import *
+from kokki.runner import *
+from kokki.service import *
+from kokki.source import *
+from kokki.system import *
