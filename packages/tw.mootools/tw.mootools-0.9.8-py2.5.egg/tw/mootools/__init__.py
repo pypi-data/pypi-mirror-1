738d6b4876a0dfd8bb8de9d63c3d6457cacd7d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:39 +0000

    Add package tw.mootools/0.9.8/tw.mootools-0.9.8-py2.5.egg

---
+++ packages/tw.mootools/tw.mootools-0.9.8-py2.5.egg/tw/mootools/__init__.py
@@ -0,0 +1,6 @@
+from tw.mootools.base import (moo_core_js, moo_core_js_compressed,
+                              moo_more_js, moo_more_js_compressed,
+                              moo_js, moo_core_js_compressed,
+                              MooTools, MooToolsCompressed, MootoolsLink,
+                              MootoolsCoreLink, MootoolsMoreLink)
+from tw.mootools.dates import CalendarDatePicker
