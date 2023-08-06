3c4d7a0fe3f29e830395fe04e4f3291e5807cf76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:08 +0000

    Add package domestos/0.1/domestos-0.1.tar.gz

---
+++ packages/domestos/domestos-0.1.tar.gz/domestos-0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name="domestos",
+	version="0.1",
+	description="DomestOS",
+	author="Louis King",
+	author_email="jinglemansweep@gmail.com",
+	url="http://www.louisking.co.uk",
+	packages=["domestos"],
+	package_dir={"domestos": "domestos"},
+	package_data={"domestos": []},
+	long_description="""DomestOS Home Automation Platform""",
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"Environment :: Console",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Natural Language :: English",
+		"Operating System :: POSIX :: Linux",
+		"Programming Language :: Python :: 2.5",
+		"Topic :: System :: Networking :: Monitoring",		
+	]
+)
