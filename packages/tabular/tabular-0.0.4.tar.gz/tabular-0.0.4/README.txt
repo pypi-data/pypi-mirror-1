02d0887377329737dad2d1a576eeef3e929f21d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:11 +0000

    Add package tabular/0.0.4/tabular-0.0.4.tar.gz

---
+++ packages/tabular/tabular-0.0.4.tar.gz/tabular-0.0.4/README.txt
@@ -0,0 +1,15 @@
+Installation
+=============
+
+python setup.py install
+
+Tests
+=============
+
+Run nosetests from the package directory by typing either:
+
+1)  python setup.py test
+
+- or -
+
+2) nosetests
---
+++ packages/tabular/tabular-0.0.4.tar.gz/tabular-0.0.4/tabular/__init__.py
@@ -0,0 +1,15 @@
+import io, fast, spreadsheet, utils, web
+
+from tabarray import *
+from io import *
+from fast import *
+from spreadsheet import *
+from utils import *
+from web import *
+
+__all__ = []
+__all__.extend(io.__all__)
+__all__.extend(fast.__all__)
+__all__.extend(spreadsheet.__all__)
+__all__.extend(utils.__all__)
+__all__.extend(web.__all__)
