2d1cfaafa8f847000dd45d69c8dc4908ffcf104f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:34 +0000

    Add package stompy/0.2.2/stompy-0.2.2.tar.gz

---
+++ packages/stompy/stompy-0.2.2.tar.gz/stompy-0.2.2/stompy/distmeta.py
@@ -0,0 +1,20 @@
+"""Implementation of the STOMP protocol in Python.
+"""
+VERSION = (0, 2, 2)
+__version__ = ".".join(map(str, VERSION))
+__author__ = "Benjamin W. Smith"
+__contact__ = "benjaminwarfield@just-another.net"
+__homepage__ = "http://bitbucket.org/benjaminws/python-stomp/"
+__docformat__ = "restructuredtext"
+__long_description__ = open("docs/introduction.rst").read()
+
+
+def is_stable_release():
+    return bool(not VERSION[1] % 2)
+
+
+def version_with_meta():
+    meta = "unstable"
+    if is_stable_release():
+        meta = "stable"
+    return "%s (%s)" % (__version__, meta)
