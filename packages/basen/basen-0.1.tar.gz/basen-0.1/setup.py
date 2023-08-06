0b24729d021994c9ab3b9a67c6dc093e5bc181a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:00 +0000

    Add package basen/0.1/basen-0.1.tar.gz

---
+++ packages/basen/basen-0.1.tar.gz/basen-0.1/setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+from distutils.core import setup
+#from setuptools import setup
+
+setup(name='basen',
+      version='0.1',
+      py_modules=['basen'],
+      author='MIZUNO J.Y.',
+      url='http://p.mjy.name/pro:python-basen'
+      )
+
