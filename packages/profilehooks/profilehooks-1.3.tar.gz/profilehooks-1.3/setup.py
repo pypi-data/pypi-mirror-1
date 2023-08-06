cb915106b23ade8df1e1112446f3cdafadbc7a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:34 +0000

    Add package profilehooks/1.3/profilehooks-1.3.tar.gz

---
+++ packages/profilehooks/profilehooks-1.3.tar.gz/profilehooks-1.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='profilehooks',
+    version='1.3',
+    author='Marius Gedminas',
+    author_email='marius@gedmin.as',
+    url='http://mg.pov.lt/profilehooks/',
+    description='Decorators for profiling individual functions',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT',
+
+    py_modules=['profilehooks'],
+    test_suite='test_profilehooks',
+    zip_safe=False,
+)
