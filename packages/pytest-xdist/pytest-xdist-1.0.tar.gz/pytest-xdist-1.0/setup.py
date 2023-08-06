3ded0ecea520357ecbd13d1c3d23a047245e3ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:08 +0000

    Add package pytest-xdist/1.0/pytest-xdist-1.0.tar.gz

---
+++ packages/pytest-xdist/pytest-xdist-1.0.tar.gz/pytest-xdist-1.0/setup.py
@@ -0,0 +1,37 @@
+"""
+py.test 'xdist' plugin for distributed testing and loop-on-failing modes. 
+
+See http://pytest.org/plugin/xdist.html for documentation and, after
+installation of the ``pytest-xdist`` PyPI package, ``py.test -h`` 
+for the new options. 
+"""
+
+from setuptools import setup
+
+setup(
+    name="pytest-xdist",
+    version="1.0",
+    description='py.test figleaf coverage plugin',
+    long_description=__doc__,
+    license='GPLv2 or later',
+    author='holger krekel and contributors',
+    author_email='py-dev@codespeak.net,holger@merlinux.eu', 
+    url='http://bitbucket.org/hpk42/pytest-xdist',
+    platforms=['linux', 'osx', 'win32'],
+    packages = ['xdist'],
+    entry_points = {'pytest11': ['xdist = xdist.plugin'],},
+    zip_safe=False,
+    install_requires = ['execnet>=1.0.3', 'py>=1.2.0'],
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: POSIX',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: MacOS :: MacOS X',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Software Development :: Quality Assurance',
+    'Topic :: Utilities',
+    'Programming Language :: Python',
+    ],
+)
