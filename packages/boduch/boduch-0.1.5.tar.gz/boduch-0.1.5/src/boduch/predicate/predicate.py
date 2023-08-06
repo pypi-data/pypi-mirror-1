9f781f3e33b236b8e6b84a95fdce41e52e47d6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:55 +0000

    Add package boduch/0.1.5/boduch-0.1.5.tar.gz

---
+++ packages/boduch/boduch-0.1.5.tar.gz/boduch-0.1.5/src/boduch/predicate/predicate.py
@@ -0,0 +1,43 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from zope.interface import implements
+from boduch.interface import IPredicate
+from boduch.type import Type
+
+class Predicate(Type):
+    implements(IPredicate)
+    def __init__(self, lop, rop):
+        Type.__init__(self)
+        self.lop=lop
+        self.rop=rop
+        self.result=False
+        
+    def invoke_operators(self):
+        if callable(self.lop):
+            lop=self.lop()
+        else:
+            lop=self.lop
+        if callable(self.rop):
+            rop=self.rop()
+        else:
+            rop=self.rop
+        return lop, rop
+        
+    def __nonzero__(self):
+        raise NotImplementedError("Predicate.__nonzero__")
+    
+__all__=['Predicate']
