065ff3489bbdd1868e615ae91856fabbf0311714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:31 +0000

    Add package Products.ExtendedPathIndex/2.7/Products.ExtendedPathIndex-2.7.zip

---
+++ packages/Products.ExtendedPathIndex/Products.ExtendedPathIndex-2.7.zip/Products.ExtendedPathIndex-2.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '2.7'
+
+setup(name='Products.ExtendedPathIndex',
+      version=version,
+      description="Zope catalog index for paths",
+      long_description=open("README.txt").read() + "\n" + \
+              open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        ],
+      keywords='Zope catalog index',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            # 'transaction',
+        ]
+      ),
+      install_requires=[
+          'setuptools',
+          # 'ZODB3',
+          # 'Zope2',
+      ],
+      )
