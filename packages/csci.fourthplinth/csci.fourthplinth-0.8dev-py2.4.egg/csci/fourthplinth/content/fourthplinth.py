16cdc6caec58e8a94532eb7bea81158f30432ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:14 +0000

    Add package csci.fourthplinth/0.1dev/csci.fourthplinth-0.8dev-py2.4.egg

---
+++ packages/csci.fourthplinth/csci.fourthplinth-0.8dev-py2.4.egg/csci/fourthplinth/content/fourthplinth.py
@@ -0,0 +1,107 @@
+"""Definition of the fourthplinth content type
+"""
+
+from zope.interface import implements, directlyProvides
+
+from Products.Archetypes import atapi
+from Products.ATContentTypes.content import base
+from Products.ATContentTypes.content import schemata
+
+from csci.fourthplinth import fourthplinthMessageFactory as _
+from csci.fourthplinth.interfaces import Ifourthplinth
+from csci.fourthplinth.config import PROJECTNAME
+
+fourthplinthSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
+
+    # -*- Your Archetypes field definitions here ... -*-
+
+    atapi.StringField(
+        'tweetlayout',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Tweet layout"),
+            description=_(u"$plinther$ $time$ $url$"),
+        ),
+        required=True,
+    ),
+
+
+
+    atapi.StringField(
+        'tusername',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Username"),
+            description=_(u"Twitter login username"),
+        ),
+    ),
+    
+    
+    atapi.StringField(
+        'tpassword',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Twitter Password"),
+            description=_(u"Password"),
+        ),
+    ),
+
+   
+    
+    atapi.BooleanField(
+        'debugmode',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.BooleanWidget(
+            label=_(u"Debug Mode"),
+            description=_(u"enter debug mode"),
+        ),
+    ),
+
+
+    atapi.LinesField(
+        'tschedule',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.LinesWidget(
+            label=_(u"Posts to schedule"),
+            description=_(u"Enter: Date, Time, Tweet use new line for each entry"),
+            rows=20,
+            columns=30,
+        ),
+    ),
+
+
+
+))
+
+# Set storage on fields copied from ATContentTypeSchema, making sure
+# they work well with the python bridge properties.
+
+fourthplinthSchema['title'].storage = atapi.AnnotationStorage()
+fourthplinthSchema['description'].storage = atapi.AnnotationStorage()
+
+schemata.finalizeATCTSchema(fourthplinthSchema, moveDiscussion=False)
+
+class fourthplinth(base.ATCTContent):
+    """Fourth Plinth"""
+    implements(Ifourthplinth)
+
+    meta_type = "fourthplinth"
+    schema = fourthplinthSchema
+
+    title = atapi.ATFieldProperty('title')
+    description = atapi.ATFieldProperty('description')
+    
+    # -*- Your ATSchema to Python Property Bridges Here ... -*-
+    tweetlayout = atapi.ATFieldProperty('tweetlayout')
+
+
+    debugmode = atapi.ATFieldProperty('debugmode')
+
+    tschedule = atapi.ATFieldProperty('tschedule')
+
+    tpassword = atapi.ATFieldProperty('tpassword')
+
+    tusername = atapi.ATFieldProperty('tusername')
+
+
+atapi.registerType(fourthplinth, PROJECTNAME)
---
+++ packages/csci.fourthplinth/csci.fourthplinth-0.8dev-py2.4.egg/csci/fourthplinth/interfaces/fourthplinth.py
@@ -0,0 +1,44 @@
+from zope import schema
+from zope.interface import Interface
+
+from zope.app.container.constraints import contains
+from zope.app.container.constraints import containers
+
+from csci.fourthplinth import fourthplinthMessageFactory as _
+
+class Ifourthplinth(Interface):
+    """Fourth Plinth"""
+    
+    # -*- schema definition goes here -*-
+    tweetlayout = schema.TextLine(
+        title=_(u"Tweet layout"), 
+        required=True,
+        description=_(u"$plinther$ $time$ $url$"),
+    )
+
+
+
+    debugmode = schema.Bool(
+        title=_(u"Debug Mode"), 
+        required=False,
+        description=_(u"enter debug mode"),
+    )
+
+    tschedule = schema.Text(
+        title=_(u"Generated schedule for today"), 
+        required=False,
+        description=_(u"Field description"),
+    )
+
+    tpassword = schema.TextLine(
+        title=_(u"Twitter Password"), 
+        required=True,
+        description=_(u"Password"),
+    )
+
+    tusername = schema.TextLine(
+        title=_(u"New Field"), 
+        required=False,
+        description=_(u"Field description"),
+    )
+
