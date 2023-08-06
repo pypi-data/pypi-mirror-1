fa5b67258ef6d622d07e57c255f2cd946fc3f182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:17 +0000

    Add package pyjld.os/0.1/pyjld.os-0.1-py2.5.egg

---
+++ packages/pyjld.os/pyjld.os-0.1-py2.5.egg/pyjld/os/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+""" 
+pyjld.os package
+
+@author: Jean-Lou Dupont
+"""
+__author__  = "Jean-Lou Dupont"
+__email     = "python (at) jldupont.com"
+__version__ = "0.1"
+__fileid    = "$Id: __init__.py 5 2009-03-30 16:40:08Z jeanlou.dupont $"
+
+
+__classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: Public Domain',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    ]
+
+__dependencies = []
+
+from tools import *
