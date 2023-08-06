1217b16f14c401441480e7523e3e1364518d7d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:42 +0000

    Add package django-ctrl/2.1/django-ctrl-2.1.tar.gz

---
+++ packages/django-ctrl/django-ctrl-2.1.tar.gz/django-ctrl-2.1/src/djctrl/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '2.1'
+__author__ = 'Zachary Voase <zacharyvoase@me.com>'
+
+
+from djctrl import common, core
+from djctrl.common import *
+from djctrl.core import *
+
+
+__all__ = vars().keys()
---
+++ packages/django-ctrl/django-ctrl-2.1.tar.gz/django-ctrl-2.1/src/djctrl/common/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .ajax import AjaxController
+from .auth import AuthController
+from .cache import CacheController
+from .form import FormController
+from .mime import MIMEController
+from .resource import ResourceController
+
+__all__ = vars().keys()
