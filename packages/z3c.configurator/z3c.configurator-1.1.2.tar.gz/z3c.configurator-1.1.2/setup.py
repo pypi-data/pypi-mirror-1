736da77546b41ff0fa6c24a112919b0e4a386b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:32 +0000

    Add package z3c.configurator/1.1.2/z3c.configurator-1.1.2.tar.gz

---
+++ packages/z3c.configurator/z3c.configurator-1.1.2.tar.gz/z3c.configurator-1.1.2/setup.py
@@ -0,0 +1,81 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: setup.py 94517 2009-01-04 21:21:17Z icemac $
+"""
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name = 'z3c.configurator',
+    version='1.1.2',
+    author = "Zope Community",
+    author_email = "zope-dev@zope.org",
+    description = "Dynamic configuration",
+    long_description=(
+        read('README.txt')
+        + '\n\n.. contents::\n\n' +
+        read('src', 'z3c', 'configurator', 'README.txt')
+        + '\n\n' +
+        read('src', 'z3c', 'configurator', 'browser', 'README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license = 'ZPL 2.1',
+    keywords = 'zope3 z3c configurator configuration',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.configurator',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c'],
+    extras_require = dict(
+        test = [
+            'zope.app.testing',
+            ],
+        ),
+    install_requires = [
+          'setuptools',
+          'zope.annotation',
+          'zope.app.pagetemplate',
+          'zope.app.securitypolicy',
+          'zope.app.testing',
+          'zope.app.zcmlfiles',
+          'zope.cachedescriptors',
+          'zope.component',
+          'zope.dublincore',
+          'zope.formlib',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
+          'zope.testbrowser',
+          'zope.testing',
+          ],
+    dependency_links = ['http://download.zope.org/distribution'],
+    zip_safe=False,
+    )
+
---
+++ packages/z3c.configurator/z3c.configurator-1.1.2.tar.gz/z3c.configurator-1.1.2/src/z3c/configurator/vocabulary.py
@@ -0,0 +1,29 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Vocabularies
+
+$Id: vocabulary.py 72645 2007-02-16 09:45:11Z dobe $
+"""
+__docformat__ = "reStructuredText"
+from zope import component
+from zope.schema import vocabulary
+import interfaces
+
+def pluginNamesVocabulary(context):
+    """a vocabulary that returns all names of registered configuration
+    plugins"""
+    terms = []
+    plugins = dict(component.getAdapters(
+        (context,), interfaces.IConfigurationPlugin))
+    return vocabulary.SimpleVocabulary.fromValues(sorted(plugins.keys()))
