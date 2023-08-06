f737da65db7c22dd3d192ea96e571f16a485da7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:45 +0000

    Add package wtop/0.6.2-test2/wtop-0.6.2-test2.tar.gz

---
+++ packages/wtop/wtop-0.6.2-test2.tar.gz/wtop-0.6.2-test2/setup.py
@@ -0,0 +1,26 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+import os, os.path
+
+
+cfg_file_path = '/etc'                                        # unix, osx, etc
+if (os.name != 'posix') and (os.environ.has_key('HOMEPATH')): # windows
+    cfg_file_path = os.environ.get('HOMEPATH')
+
+setup(
+    name='wtop',
+    version='0.6.2-test2',
+    packages = find_packages(),
+
+    data_files=[(cfg_file_path, ['wtop.cfg'])],
+    scripts=['wtop', 'logrep'],
+    py_modules=['logrep'],
+
+    url="http://code.google.com/p/wtop/",
+    author="Carlos Bueno",
+    author_email="carlos@bueno.org",
+
+    license='BSD',
+    description='running statistics for webservers, plus powerful log-grepping tools'
+)
