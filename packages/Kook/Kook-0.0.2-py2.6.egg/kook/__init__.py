93377c0f04220a3667e1dcacc5d798d65f82bffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:10 +0000

    Add package Kook/0.0.2/Kook-0.0.2-py2.6.egg

---
+++ packages/Kook/Kook-0.0.2-py2.6.egg/kook/__init__.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+###
+### $Release: 0.0.2 $
+### copyright(c) 2008-2009 kuwata-lab.com all rights reserved.
+### MIT License
+###
+
+__RELEASE__ = "$Release: 0.0.2 $".split(' ')[1]
+__all__ = (
+    'KookError', 'KookRecipeError', 'KookCommandError',
+    #'product', 'ingreds', 'byprods', 'coprods', 'priority', 'spices', 'if_exists',
+    #'Recipe', 'TaskRecipe', 'FileRecipe',
+    #'Cookbook', 'Kitchen', # 'Cookable', 'Material', 'Cooking', 'create_context',
+)
+
+import sys, os, re, types
+from kook.utils import *
+
+
+class KookError(Exception):  # StandardError is not available in Python 3.0
+    pass
+
+
+class KookRecipeError(KookError):
+    pass
+
+
+class KookCommandError(KookError):
+    pass
+
+
+_quiet      = False
+_forced     = False
+_debug_level = 0
+_cmd_prompt = '$ '
+_msg_prompt = '### '
+_dbg_prompt = '*** debug: '
+_stdout = sys.stdout
+_stderr = sys.stderr
+
+
+def _debug(msg, level=1, depth=0):
+    if _debug_level >= level:
+        write = _stderr.write
+        write(_dbg_prompt)
+        if depth: write('+' * depth + ' ')
+        write(msg)
+        if msg[-1] != "\n": write("\n")
+
+
+def _report_msg(msg, level=None):
+    if not _quiet:
+        write = _stderr.write
+        write(_msg_prompt)
+        if level: write('*' * level + ' ')
+        write(msg)
+        if msg[-1] != "\n": write("\n")
+
+
+def _report_cmd(cmd):
+    if not _quiet:
+        write = _stderr.write
+        write(_cmd_prompt)
+        write(cmd)
+        if cmd[-1] != "\n": write("\n")
