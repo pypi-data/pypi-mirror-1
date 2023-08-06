bbe286fc938bd0c402f325e6061e5e53c4f04bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:03 +0000

    Add package nl/0.91/nl-0.91.tar.gz

---
+++ packages/nl/nl-0.91.tar.gz/nl-0.91/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = "nl",
+    version = "0.91",
+    url = 'http://bitbucket.org/enriquepablo/nl/wiki/Home',
+    license = 'GPL',
+    description = "A python library that provides a production system with an API modelled on the natural language",
+    author = 'Enrique Perez Arnaud',
+    author_email = 'enriquepablo@gmail.com',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    classifiers = [
+                   "Topic :: Scientific/Engineering :: Artificial Intelligence",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules"
+                  ],
+    zip_safe = False,
+    dependency_links = [
+        'http://sourceforge.net/project/showfiles.php?group_id=114052',
+        'http://sourceforge.net/projects/pyclips/files/',
+        'http://bitbucket.org/enriquepablo/nl/downloads/',
+        'http://bitbucket.org/enriquepablo/nl/downloads/pyclips-1.0.7.348_clips_6.24-py2.5-linux-i686.egg',
+    ],
+    install_requires = ['setuptools', 'pyclips'],
+    entry_points = {
+        'console_scripts':
+            [ 'plot_ph22 = nl.examples.physics22:plotPh22', ],
+    }
+)
