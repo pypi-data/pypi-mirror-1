3bc2ac7b97b84c0d6411a57b6fcfd2531f39f989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:13 +0000

    Add package tiddlywebwiki/0.12.1/tiddlywebwiki-0.12.1.tar.gz

---
+++ packages/tiddlywebwiki/tiddlywebwiki-0.12.1.tar.gz/tiddlywebwiki-0.12.1/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from tiddlywebwiki import __version__ as VERSION
+
+
+setup(name = 'tiddlywebwiki',
+        version = VERSION,
+        description = 'A TiddlyWeb plugin to provide a multi-user TiddlyWiki environment.',
+        author = 'Chris Dent',
+        author_email = 'cdent@peermore.com',
+        packages = ['tiddlywebwiki'],
+        py_modules = ['differ'],
+        scripts = ['twinstance'],
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['tiddlyweb', 'tiddlywebplugins.wikklytextrender', 'tiddlywebplugins.status', 'BeautifulSoup', 'wikklytext'],
+        include_package_data = True,
+        )
---
+++ packages/tiddlywebwiki/tiddlywebwiki-0.12.1.tar.gz/tiddlywebwiki-0.12.1/tiddlywebwiki/__init__.py
@@ -0,0 +1,20 @@
+"""
+A TiddlyWeb plugin providing a multi-user TiddlyWiki environment.
+"""
+
+__version__ = '0.12.1'
+
+
+def init(config):
+    # These imports must be in init otherwise getting
+    # the version number causes cyclic imports to happen.
+    import tiddlywebwiki.importer
+    import tiddlywebwiki.instancer
+    import tiddlywebwiki.manage
+    from tiddlyweb.config import merge_config
+    from tiddlywebwiki.config import config as twwconfig
+    merge_config(config, twwconfig)
+    tiddlywebwiki.importer.init(config)
+    tiddlywebwiki.instancer.init(config)
+    tiddlywebwiki.manage.init(config)
+    # XXX and add selector for POST a wiki
