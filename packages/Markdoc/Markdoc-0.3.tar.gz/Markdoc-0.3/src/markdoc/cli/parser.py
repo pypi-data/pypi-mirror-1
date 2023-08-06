5e6dcb32642da878aafd529bdd0f3bc297026d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:13 +0000

    Add package Markdoc/0.3/Markdoc-0.3.tar.gz

---
+++ packages/Markdoc/Markdoc-0.3.tar.gz/Markdoc-0.3/src/markdoc/cli/parser.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import argparse
+
+import markdoc
+from markdoc.config import Config
+
+
+parser = argparse.ArgumentParser(**{
+    'prog': 'markdoc',
+    'description': 'A lightweight Markdown-based wiki build tool.',
+    'version': markdoc.__version__,
+})
+
+config = parser.add_argument('--config', '-c', default=os.getcwd(),
+    help="Use the specified Markdoc config (a YAML file or a directory "
+         "containing markdoc.yaml)")
+
+quiet = parser.add_argument('--quiet', '-q', action='store_true',
+    help="Suppress non-error output")
+
+subparsers = parser.add_subparsers(dest='command', title='commands', metavar='COMMAND')
