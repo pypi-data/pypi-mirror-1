f1bb9baa7ce7a5db254adef82a1b24ceeeac25df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:26 +0000

    Add package z3c.configurator/1.2.0/z3c.configurator-1.2.0.tar.gz

---
+++ packages/z3c.configurator/z3c.configurator-1.2.0.tar.gz/z3c.configurator-1.2.0/src/z3c/configurator/i18n.py
@@ -0,0 +1,9 @@
+"""The i18n definitions.
+
+$Id: i18n.py 106794 2009-12-20 03:54:58Z fafhrd $
+"""
+__docformat__ = "reStructuredText"
+
+import zope.i18nmessageid
+
+_ = zope.i18nmessageid.MessageFactory('z3c.configurator')
