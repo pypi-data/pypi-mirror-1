f34f938b8f8bf776a1c1e3562217bc4533a22cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:48 +0000

    Add package pyspacewar/0.9.6/pyspacewar-0.9.6.tar.gz

---
+++ packages/pyspacewar/pyspacewar-0.9.6.tar.gz/pyspacewar-0.9.6/src/pyspacewar/version.py
@@ -0,0 +1,25 @@
+"""
+PySpaceWar version number tracker.
+
+$Id: version.py 289 2009-09-30 18:10:17Z mg $
+"""
+
+version = "0.9.6"
+
+
+def get_svn_revision(unknown=''):
+    """Return the latest revision number of the files in the package."""
+    import os, subprocess, pyspacewar
+    package_root = os.path.dirname(__file__)
+    try:
+        p = subprocess.Popen(['svnversion', package_root],
+                             stdout=subprocess.PIPE)
+    except OSError:
+        return unknown
+    else:
+        return p.communicate()[0].strip()
+
+
+if version.endswith('svn'):
+    version += get_svn_revision()
+
