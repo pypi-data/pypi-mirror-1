82d79eb7e93382a04a86919868f737c4ef7df408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:41 +0000

    Add package icemac.ab.importer/1.0/icemac.ab.importer-1.0.tar.gz

---
+++ packages/icemac.ab.importer/icemac.ab.importer-1.0.tar.gz/icemac.ab.importer-1.0/src/icemac/ab/importer/browser/testing.py
@@ -0,0 +1,10 @@
+# -*- coding: latin-1 -*-
+# Copyright (c) 2008-2009 Michael Howitz
+# See also LICENSE.txt
+# $Id: testing.py 683 2009-11-21 14:44:10Z icemac $
+
+import zope.app.testing.functional
+
+
+zope.app.testing.functional.defineLayer(
+    'ImporterLayer', 'ftesting.zcml', allow_teardown=True)
