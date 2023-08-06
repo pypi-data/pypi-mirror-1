e666cf769d6c2effedaa39622ff9f1b3ac729346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:39 +0000

    Add package collective.templateengines/0.3.1/collective.templateengines-0.3.1.tar.gz

---
+++ packages/collective.templateengines/collective.templateengines-0.3.1.tar.gz/collective.templateengines-0.3.1/collective/templateengines/context/plone.py
@@ -0,0 +1,80 @@
+"""
+
+    Plone-specific security and context mappings.
+
+"""
+
+__author__ = "Mikko Ohtamaa <mikko@redinnovation.com>"
+__copyright__ = "2008 Red Innovation Oy"
+__license__ = "3-Clause BSD"
+
+from zope import interface
+from zope.app.component.hooks import getSite
+from zope.component import getMultiAdapter, getSiteManager
+
+from collective.templateengines.interfaces import *
+
+from AccessControl import ClassSecurityInfo
+from AccessControl import getSecurityManager
+
+from Products.CMFCore.utils import getToolByName
+
+class ArchetypesSecureContext:
+    """ Wrap Archetypes object inside a secure context.
+    
+    Use the priviledges of currently logged in user.
+    
+    TODO: Field access rights are not respected
+    """
+    
+    interface.implements(ITemplateContext)
+
+    def __init__(self, context, expose_schema=True):
+        """
+        
+        @param expose_schema: Map AT schema accessors directly to template variables for
+            engines which cannot traverse Zope content (Cheetah).
+        """
+        
+        security=getSecurityManager()
+        
+        #portal_state = getMultiAdapter((context, context.REQUEST), name=u'plone_portal_state')
+        
+        portal_state = context.restrictedTraverse("@@plone_portal_state")
+        
+        site = getSite()
+
+        self.namespace = {
+            "portal" : site,
+            "context" : context,
+            "portal_url" : getToolByName(context, 'portal_url'),
+            "object_url" : context.absolute_url(),
+            "user" : security.getUser(),
+            "request" : context.REQUEST,
+            "uid" : context.UID(), # Archetypes unique identifier number
+            "portal_state" : portal_state
+        }        
+        
+        if expose_schema:
+            schema = context.Schema()
+            for f in schema.fields():
+                name = f.getName()
+                try:
+                    value = f.get(context)
+                    self.namespace[name] = value
+                except TypeError:
+                    # membrane field "listed" gives TypeError, don't know why
+                    pass
+                
+    def addMapping(self, name, var):
+        self.namespace[name] = var
+        
+    def getMappings(self):
+        return self.namespace
+    
+    def getTraversingContext(self):
+        """ Get zope traversing context - don't confuse context with context! """
+        return self.namespace["context"]
+        
+    
+    
---
+++ packages/collective.templateengines/collective.templateengines-0.3.1.tar.gz/collective.templateengines-0.3.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.3.1'
+
+setup(name='collective.templateengines',
+      version=version,
+      description="Generic interface for Python template engines",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        
+        ],
+      keywords='template templates engine python interface zope cheetah django backend utils generic',
+      author='Mikko Ohtamaa',
+      author_email='mikko.ohtamaa@twinapex.com',
+      url='http://pypi.python.org/pypi?%3Aaction=pkg_edit&name=collective.templateengines',
+      license='License :: OSI Approved :: BSD License',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.interface',          
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
