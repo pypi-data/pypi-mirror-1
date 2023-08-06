ad36f28b8c1873f9f520c28dd7a81b6feacf22dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:04 +0000

    Add package collective.classifieds/1.0.1/collective.classifieds-1.0.1-py2.4.egg

---
+++ packages/collective.classifieds/collective.classifieds-1.0.1-py2.4.egg/collective/classifieds/content/__init__.py
@@ -0,0 +1,13 @@
+__author__ = """Four Digits <Ralph Jacobs>"""
+__docformat__ = 'plaintext'
+
+# Subpackages
+from Products.validation import validation
+
+from Validators import FloatValidator
+validation.register(FloatValidator('isFloat'))
+
+# Classes
+import ClassifiedsCategory
+import Classifieds
+import Classified
