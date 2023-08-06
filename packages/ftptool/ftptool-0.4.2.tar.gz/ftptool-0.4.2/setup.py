4e3097706e0443874fa5a966061194e2c1d3b4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:44 +0000

    Add package ftptool/0.4.2/ftptool-0.4.2.tar.gz

---
+++ packages/ftptool/ftptool-0.4.2.tar.gz/ftptool-0.4.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import os
+
+readme_fname = os.path.join(os.path.dirname(__file__), "README.rst")
+readme_text = open(readme_fname).read()
+
+setup(name="ftptool", version="0.4.2",
+    url="http://blogg.se",
+    description="Higher-level interface to ftplib",
+    author="Blogg Esse AB",
+    author_email="opensource@blogg.se",
+    long_description=readme_text,
+    py_modules=["ftptool"])
