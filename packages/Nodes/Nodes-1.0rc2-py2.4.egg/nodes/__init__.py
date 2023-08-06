1a29c50bbc2ecddc1e9025d1ddbd36678af10e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:37 +0000

    Add package Nodes/1.0rc2/Nodes-1.0rc2-py2.4.egg

---
+++ packages/Nodes/Nodes-1.0rc2-py2.4.egg/nodes/__init__.py
@@ -0,0 +1,11 @@
+'''Node root.'''
+__version__='$Revision: 24$'
+from core import*
+from globs import*
+from construct import*
+from basic import*
+from logic import*
+try:
+	import psycoptima
+except:
+	pass
