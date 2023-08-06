acd88a8477d0635a93b7f92cf507d8b1b4a92b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:00 +0000

    Add package collective.anonymousview/1.0/collective.anonymousview-1.0.tar.gz

---
+++ packages/collective.anonymousview/collective.anonymousview-1.0.tar.gz/collective.anonymousview-1.0/collective/anonymousview/tests/test_anonymousview.py
@@ -0,0 +1,14 @@
+from collective.anonymousview.tests.base import TestCase
+from zope.testing import doctestunit
+from Testing import ZopeTestCase as ztc
+import doctest
+import unittest
+
+def test_suite():
+    """This sets up a test suite that actually runs the tests in collective.anonymousview_integration.txt"""
+    return unittest.TestSuite([
+        ztc.FunctionalDocFileSuite(
+            'tests/anonymousview_integration.txt', package='collective.anonymousview',
+            test_class=TestCase),
+        ])
+
