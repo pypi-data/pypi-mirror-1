88641e3147abe18f88851abdf1269f610ab111cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:23 +0000

    Add package django-addons/0.3/django-addons-0.3.tar.gz

---
+++ packages/django-addons/django-addons-0.3.tar.gz/django-addons-0.3/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import os
+
+setup(name='django-addons',
+      version="0.3",
+      description='Addon framework for Django',
+      author='Lauri Võsandi',
+      author_email='lauri@indifex.com',
+      url='http://www.bitbucket.org/indifex/django-addons/wiki/',
+      download_url='http://bitbucket.org/indifex/django-addons/get/tip.gz',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Utilities'],
+)
