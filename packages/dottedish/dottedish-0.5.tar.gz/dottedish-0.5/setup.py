25cffa4de40e61eec91b4fe95da8851b358d822e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:40 +0000

    Add package dottedish/0.5/dottedish-0.5.tar.gz

---
+++ packages/dottedish/dottedish-0.5.tar.gz/dottedish-0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.5'
+
+setup(name='dottedish',
+      version=version,
+      description="A dotted key accessor that handles numeric keys for list entries",
+      long_description="""\
+""",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2",
+      ], 
+      keywords='dictionary dict dotted deep',
+      author='Tim Parkin, Matt Goodall',
+      author_email='developers@ish.io',
+      url='http://ish.io/projects/show/dottedish',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
