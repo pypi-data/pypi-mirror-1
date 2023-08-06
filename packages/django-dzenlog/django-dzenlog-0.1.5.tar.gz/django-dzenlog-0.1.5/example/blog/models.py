922d9f92bbab49ac09b5a235cd35455d6771904d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:32 +0000

    Add package django-dzenlog/0.1.5/django-dzenlog-0.1.5.tar.gz

---
+++ packages/django-dzenlog/django-dzenlog-0.1.5.tar.gz/django-dzenlog-0.1.5/example/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django_dzenlog.models import GeneralPost
+
+class TextPost(GeneralPost):
+    body = models.TextField(_('Post\'s body'))
+
+    def _get_template(self):
+        return 'blog/text_post.html'
+
+
+class LinkPost(GeneralPost):
+    url = models.URLField(_('URL'), default='http://example.com', verify_exists=False)
+    description = models.TextField(_('URL\'s description'), blank=True)
+
+    def _get_template(self):
+        return 'blog/link_post.html'
+
