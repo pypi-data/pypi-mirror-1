2d9307238a7f949711a4ecc7e9cb9d16f36870e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:50 +0000

    Add package an_example_pypi_project/0.0.1/an_example_pypi_project-0.0.1-py2.6.egg

---
+++ packages/an_example_pypi_project/an_example_pypi_project-0.0.1-py2.6.egg/an_example_pypi_project/useful.py
@@ -0,0 +1,16 @@
+"""A very useful module indeed. 
+
+
+
+"""
+
+def public_fn_with_docstring():
+    """
+    """
+
+def public_fn_without_docstring():
+    return True 
+
+def _private_fn_with_docstring():
+    """I have a docstring, but won't be imported if you just use :members:"""   
+    
