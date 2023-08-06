8e03aae596a23a0a2945642a49fe68cdab8d2920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:13 +0000

    Add package collective.plonebookmarklets/2.1.1/collective.plonebookmarklets-2.1.1.tar.gz

---
+++ packages/collective.plonebookmarklets/collective.plonebookmarklets-2.1.1.tar.gz/collective.plonebookmarklets-2.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.1.1'
+
+setup(name='collective.plonebookmarklets',
+      version=version,
+      description="This product installs an 'Add Bookmarklet' document_action to your Plone site",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='tagging bookmarklets social bookmarking',
+      author='enPraxis',
+      author_email='info@enpraxis.net',
+      url='http://plone.org/products/plonebookmarklets',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
