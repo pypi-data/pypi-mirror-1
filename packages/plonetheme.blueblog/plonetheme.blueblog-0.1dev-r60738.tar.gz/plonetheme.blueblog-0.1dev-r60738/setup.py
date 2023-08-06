1d912276951e81c9088f7c59731af077d0c532d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:02 +0000

    Add package plonetheme.blueblog/0.1dev-r60738/plonetheme.blueblog-0.1dev-r60738.tar.gz

---
+++ packages/plonetheme.blueblog/plonetheme.blueblog-0.1dev-r60738.tar.gz/plonetheme.blueblog-0.1dev-r60738/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='plonetheme.blueblog',
+      version=version,
+      description="An installable theme for Plone 3.0",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Jan Filip Tristan Hasecke',
+      author_email='blenderkid@web.de',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
