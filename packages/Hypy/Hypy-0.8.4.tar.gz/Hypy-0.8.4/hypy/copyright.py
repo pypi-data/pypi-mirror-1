89c93cbcfc61b1c677589c63e47e54c0c0107081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:29 +0000

    Add package Hypy/0.8.4/Hypy-0.8.4.tar.gz

---
+++ packages/Hypy/Hypy-0.8.4.tar.gz/Hypy-0.8.4/hypy/copyright.py
@@ -0,0 +1,11 @@
+"""
+Hypy is copyright (c) 2009 Cory Dodt.
+
+GENERATED FILE -- DO NOT EDIT
+"""
+from datetime import date
+
+author = "Cory Dodt"
+releaseDate = "2009-09-19"
+releaseRevision = "f6d37217eed8"
+__version__ = "0.8.4"
