88bbba9c237e6c5719ba1592b27e97a5c23b4cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:08 +0000

    Add package doapfiend-gentoo/0.1.0/doapfiend-gentoo-0.1.0.tar.gz

---
+++ packages/doapfiend-gentoo/doapfiend-gentoo-0.1.0.tar.gz/doapfiend-gentoo-0.1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name="doapfiend-gentoo",
+    license="BSD-2",
+    version="0.1.0",
+    description="Doapfiend plugin. Search for DOAP using a Gentoo package name.",
+    maintainer="Rob Cakebread",
+    author="Rob Cakebread",
+    author_email="rob@doapspace.org",
+    url="http://trac.doapspace.org/doapfiend/wiki/DfGentoo",
+    keywords="doapfiend.plugin doap rdf doapfiend gentoo semantic web",
+    classifiers=["Development Status :: 2 - Pre-Alpha",
+                 "Intended Audience :: Developers",
+                 "Programming Language :: Python",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 ],
+    py_modules = ['doapfiend_gentoo'],
+    entry_points = {
+        'doapfiend.plugins': [
+        'gentooplugin = doapfiend_gentoo:GentooPlugin'
+        ]
+    },
+)
+
