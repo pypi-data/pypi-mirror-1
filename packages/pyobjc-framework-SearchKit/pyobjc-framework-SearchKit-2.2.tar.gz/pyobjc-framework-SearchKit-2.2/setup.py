ad8db81057053832424efae8257a311bacdd3da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:17 +0000

    Add package pyobjc-framework-SearchKit/2.2/pyobjc-framework-SearchKit-2.2.tar.gz

---
+++ packages/pyobjc-framework-SearchKit/pyobjc-framework-SearchKit-2.2.tar.gz/pyobjc-framework-SearchKit-2.2/setup.py
@@ -0,0 +1,22 @@
+''' 
+Wrappers for the "SearchKit" framework on MacOSX. SearchKit is a content
+indexing and search solution.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+'''
+from pyobjc_setup import setup
+
+setup(
+    min_os_level='10.5',
+    name='pyobjc-framework-SearchKit',
+    version='2.2',
+    description = "Wrappers for the framework SearchKit on Mac OS X",
+    packages = [ "SearchKit" ],
+    install_requires = [ 
+        'pyobjc-core>=2.2',
+        'pyobjc-framework-Cocoa>=2.2',
+    ],
+)
