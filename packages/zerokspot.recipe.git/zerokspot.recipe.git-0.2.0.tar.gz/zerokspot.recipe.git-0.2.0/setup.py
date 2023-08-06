bf611e8c71e784751ab85ca2a3a9c9e6c56451df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:55 +0000

    Add package zerokspot.recipe.git/0.2.0/zerokspot.recipe.git-0.2.0.tar.gz

---
+++ packages/zerokspot.recipe.git/zerokspot.recipe.git-0.2.0.tar.gz/zerokspot.recipe.git-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='zerokspot.recipe.git',
+        author='Horst Gutmann',
+        author_email='zerok@zerokspot.com',
+        description='Simple zc.buildout recipe for sources in a git repository',
+        version='0.2.0',
+        url='http://github.com/zerok/zerokspot.gitrecipe/',
+        install_requires=['setuptools', 'zc.buildout'],
+        namespace_packages=['zerokspot'],
+        packages=find_packages(exclude=['ez_setup']),
+        entry_points={'zc.buildout': ['default = zerokspot.recipe.git:Recipe']},
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Plugins',
+            'Framework :: Buildout',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: System :: Installation/Setup',
+            'License :: OSI Approved :: BSD License',
+            ]
+        )
