8093916b935a6e0329890cd21aacf88dbf58a138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:50 +0000

    Add package humboldt.cmfbibliographyat/0.1.3.1/humboldt.cmfbibliographyat-0.1.3.1.tar.gz

---
+++ packages/humboldt.cmfbibliographyat/humboldt.cmfbibliographyat-0.1.3.1.tar.gz/humboldt.cmfbibliographyat-0.1.3.1/humboldt/cmfbibliographyat/adapters/adapters.py
@@ -0,0 +1,76 @@
+##########################################################################
+# humboldt.cmfbibliographyat - published under the GNU Public License V 2
+# Written by Andreas Jung, ZOPYX Ltd. & Co. KG, D-72070 Tuebingen, Germany
+##########################################################################
+
+# Zope 3 imports
+from zope.interface import implements
+from zope.component import getMultiAdapter
+
+# Zope 2 imports
+from DateTime.DateTime import DateTime
+
+# CMF imports
+from Products.CMFCore.utils import getToolByName
+
+# fatsyndication imports
+from Products.fatsyndication.adapters import BaseFeed
+from Products.fatsyndication.adapters import BaseFeedSource
+from Products.fatsyndication.adapters import BaseFeedEntry
+from Products.basesyndication.interfaces import IFeedSource
+from Products.basesyndication.interfaces import IFeedEntry
+from Products.fatsyndication.adapters.feedsource import BaseFeedSource
+
+from Products.CMFBibliographyAT.interface import IBibliographicItem
+
+class BibFolderSource(BaseFeedSource):
+    """ Adopting Bibfolder to IFeedSource """
+
+    implements(IFeedSource)
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFeedEntries(self, max_only=True):
+        """ See IFeedSource """
+
+        brains = self.context.getFolderContents()
+        objs = [brain.getObject() for brain in brains]
+        objs = [o for o in objs if IBibliographicItem.providedBy(o)]
+        for o in objs:
+            o2 = IFeedEntry(o, None)
+            if o2:
+                yield o2
+
+
+class BibFolderFeed(BaseFeed):
+    """Adapter for Bibfolder to IFeed """
+
+    def getModifiedDate(self):
+        """ See IFeed """
+        return DateTime()
+
+
+class BibReferenceEntry(BaseFeedEntry):
+    """ Adopt IBibliographicItem to IFeedEntry """
+
+    implements(IFeedEntry)
+
+    def __init__(self, context):
+        self.context = context
+
+    def getDescription(self):
+        return ''
+
+    def getBody(self):
+
+        """ Look up a view from request/detail_view providing additional information
+            for the adapted bibliographic item. 'simple_entry_view' and 'full_entry_view'
+            are page-templates provided out-of-the-box.
+        """
+
+        view_name = self.context.REQUEST.get('detail_view', 'simple_entry_viedw')
+        view = self.context.restrictedTraverse(view_name, None)
+        if view:
+            return view()
+        return ''
