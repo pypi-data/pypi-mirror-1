37a7b0c1c1c8786beab5a000019031f931fd7539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:04 +0000

    Add package Products.plonehrm/2.0rc2/Products.plonehrm-2.0rc2.tar.gz

---
+++ packages/Products.plonehrm/Products.plonehrm-2.0rc2.tar.gz/Products.plonehrm-2.0rc2/Products/plonehrm/config.py
@@ -0,0 +1,25 @@
+PROJECTNAME = "plonehrm"
+product_globals = globals()
+
+QI_DEPS = (
+    'CMFPlacefulWorkflow',
+    'plonehrm.jobperformance',
+    'plonehrm.checklist',
+    'plonehrm.contracts',
+    'plonehrm.notes',
+    )
+
+# Make a dict of dicts to list the properties.
+PLONEHRM_PROPERTIES = {}
+PLONEHRM_PROPERTIES['birthday_notification_period'] = \
+    dict(default=7, type='int')
+PLONEHRM_PROPERTIES['contract_expiry_notification_period'] = \
+    dict(default=6*7, type='int')
+PLONEHRM_PROPERTIES['trial_ending_notification_period'] = \
+    dict(default=2*7, type='int')
+PLONEHRM_PROPERTIES['birthday_notification'] = \
+    dict(default=True, type='boolean')
+PLONEHRM_PROPERTIES['contract_expiry_notification'] = \
+    dict(default=True, type='boolean')
+PLONEHRM_PROPERTIES['trial_ending_notification'] = \
+    dict(default=True, type='boolean')
