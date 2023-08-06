0b34fb7b5c00b82cf2531716b0a63e2c9d39a29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:42 +0000

    Add package archetypes.schemaextender/1.3/archetypes.schemaextender-1.3.zip

---
+++ packages/archetypes.schemaextender/archetypes.schemaextender-1.3.zip/archetypes.schemaextender-1.3/archetypes/schemaextender/field.py
@@ -0,0 +1,83 @@
+from zope.interface import implements
+from Products.Archetypes import atapi
+
+from archetypes.schemaextender.interfaces import IExtensionField
+from archetypes.schemaextender.interfaces import ITranslatableExtensionField
+
+HAS_LP = True
+try:
+    from Products.LinguaPlone.interfaces import ITranslatable
+    from Products.LinguaPlone.utils import generatedMutatorWrapper
+except ImportError:
+    HAS_LP = False
+
+
+class BaseExtensionField(object):
+    """Mix-in class to make Archetypes fields not depend on generated
+    accessors and mutators, and use AnnotationStorage by default.
+
+    See README.txt for more information.
+    """
+
+    implements(IExtensionField)
+
+    storage = atapi.AnnotationStorage()
+
+    def getAccessor(self, instance):
+        def accessor(**kw):
+            return self.get(instance, **kw)
+        return accessor
+
+    def getEditAccessor(self, instance):
+        def edit_accessor(**kw):
+            return self.getRaw(instance, **kw)
+        return edit_accessor
+
+    def getMutator(self, instance):
+        def mutator(value, **kw):
+            self.set(instance, value, **kw)
+        return mutator
+
+    def getIndexAccessor(self, instance):
+        if getattr(self, 'index_method', None) == '_at_edit_accessor':
+            return self.getEditAccessor(instance)
+        else:
+            return self.getAccessor(instance)
+
+
+class TranslatableExtensionField(BaseExtensionField):
+    """Extension field for a translatable content item.
+
+    Needs to copy the language-independant values across translations.
+    """
+    implements(ITranslatableExtensionField)
+
+    def getMutator(self, instance):
+        name = self.getName()
+        generatedMutator = generatedMutatorWrapper(name)
+        def mutator(value, **kw):
+            if (not ITranslatable.providedBy(instance) or
+                not self.languageIndependent):
+                return self.getTranslationMutator(instance)(value, **kw)
+            # Use the generatedMutator from LinguaPlone
+            return generatedMutator(instance, value, **kw)
+        return mutator
+
+    def getTranslationMutator(self, instance):
+        """Return a mutator for translatable values"""
+        def mutator(value, **kw):
+            return self.set(instance, value, **kw)
+        return mutator
+
+    def isLanguageIndependent(self, instance):
+        """Get the language independed flag for i18n content."""
+        return self.languageIndependent
+
+
+# We support LinguaPlone implicitly in the same way Archetypes does.
+if HAS_LP:
+    ExtensionField = TranslatableExtensionField
+else:
+    ExtensionField = BaseExtensionField
+
+__all__ = (ExtensionField, )
