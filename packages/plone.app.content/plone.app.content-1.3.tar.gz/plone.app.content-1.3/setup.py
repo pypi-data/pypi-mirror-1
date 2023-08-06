0a2ecfb2eb7c8a8d5f5ebd2993eae062fbcd181d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:13 +0000

    Add package plone.app.content/1.3/plone.app.content-1.3.tar.gz

---
+++ packages/plone.app.content/plone.app.content-1.3.tar.gz/plone.app.content-1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3'
+
+setup(name='plone.app.content',
+      version=version,
+      description="Content Views for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Plone",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Programming Language :: Python",
+      ],
+      keywords='plone content views viewlet',
+      author='Jeroen Vloothuis',
+      author_email='jeroen.vloothuis@xs4all.nl',
+      url='http://svn.plone.org/svn/plone/plone.app.content',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.i18n',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
