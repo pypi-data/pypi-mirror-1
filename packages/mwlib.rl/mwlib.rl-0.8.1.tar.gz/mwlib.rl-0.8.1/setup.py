06e9335efb9f941eac023f4b64ca3f0c33fe7f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:09 +0000

    Add package mwlib.rl/0.8.1/mwlib.rl-0.8.1.tar.gz

---
+++ packages/mwlib.rl/mwlib.rl-0.8.1.tar.gz/mwlib.rl-0.8.1/setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2007, PediaPress GmbH
+# See README.txt for additional licensing information.
+
+import os
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+import distutils.util
+
+version=None
+execfile(distutils.util.convert_path('mwlib/rl/__init__.py')) 
+# adds 'version' to local namespace
+
+install_requires=["mwlib>=0.8.2, <0.9.0", "pygments>=0.10", "mwlib.ext>=0.8.2"]
+
+def read_long_description():
+    fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.txt")
+    return open(fn).read()
+
+setup(
+    name="mwlib.rl",
+    version=str(version),
+    entry_points = {
+        'mwlib.writers': ['rl = mwlib.rl.rlwriter:writer'],
+    },
+    install_requires=install_requires,
+
+    packages=["mwlib", "mwlib.rl", "mwlib.fonts"],
+    namespace_packages=['mwlib'],
+    zip_safe = False,
+    include_package_data = True,
+    url = "http://code.pediapress.com/",
+    description="generate pdfs from mediawiki markup",
+    long_description = read_long_description(),
+    license="BSD License",
+    maintainer="pediapress.com",
+    maintainer_email="info@pediapress.com",
+)
