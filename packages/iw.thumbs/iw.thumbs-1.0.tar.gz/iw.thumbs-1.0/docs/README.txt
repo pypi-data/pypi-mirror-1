f13ba620269c8982f6da908172f8ca513ec1bd5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:11 +0000

    Add package iw.thumbs/1.0/iw.thumbs-1.0.tar.gz

---
+++ packages/iw.thumbs/iw.thumbs-1.0.tar.gz/iw.thumbs-1.0/docs/README.txt
@@ -0,0 +1,7 @@
+Paste factory
+-------------
+
+The package provide a `Paste` factory.
+
+Here is a sample configuration::
+
---
+++ packages/iw.thumbs/iw.thumbs-1.0.tar.gz/iw.thumbs-1.0/iw/thumbs/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+__docformat__ = 'restructuredtext'
+
+def sample_app(environ, start_response):
+    return ['Hello']
