89f747418e3bad7c420d6bb7a821173ac40386f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:04 +0000

    Add package z3c.layer.ready2go/0.6.0/z3c.layer.ready2go-0.6.0.zip

---
+++ packages/z3c.layer.ready2go/z3c.layer.ready2go-0.6.0.zip/z3c.layer.ready2go-0.6.0/src/z3c/layer/ready2go/tests.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: tests.py 82519 2007-12-29 00:55:45Z rogerineichen $
+"""
+import re
+import unittest
+import z3c.ptcompat
+from zope.testing import renormalizing
+from zope.app.testing import functional
+
+import z3c.layer.ready2go
+
+layer = functional.defineLayer('TestLayer', 'ftesting.zcml')
+
+
+class IReady2GoTestSkin(z3c.layer.ready2go.IReady2GoBrowserLayer):
+    """The ready2go layer test skin."""
+
+
+def getRootFolder():
+    return functional.FunctionalTestSetup().getRootFolder()
+
+
+def test_suite():
+    z3c.ptcompat.config.disable()
+    suite = unittest.TestSuite()
+
+    s = functional.FunctionalDocFileSuite(
+        'README.txt',
+        globs={'getRootFolder': getRootFolder},
+        checker = renormalizing.RENormalizing([
+            (re.compile(r'httperror_seek_wrapper:', re.M), 'HTTPError:'),
+            ])
+        )
+    s.layer = TestLayer
+    suite.addTest(s)
+
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
