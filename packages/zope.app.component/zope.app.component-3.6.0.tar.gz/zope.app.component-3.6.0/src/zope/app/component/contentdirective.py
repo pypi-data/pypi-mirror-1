4c7ea19b4e0b6f3f078a82e7efc607317fc517af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:13 +0000

    Add package zope.app.component/3.6.0/zope.app.component-3.6.0.tar.gz

---
+++ packages/zope.app.component/zope.app.component-3.6.0.tar.gz/zope.app.component-3.6.0/src/zope/app/component/contentdirective.py
@@ -0,0 +1,21 @@
+#############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Backwards compatibility: moved this module to
+`zope.security.metaconfigure`.
+
+$Id: contentdirective.py 95328 2009-01-28 15:27:46Z brandon_rhodes $
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.security.metaconfigure import ClassDirective
