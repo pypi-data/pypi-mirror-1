3afa02742f0248a11da5e83b9f215fdaa9e507ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:44 +0000

    Add package pod/0.41/pod-0.41-py2.6.egg

---
+++ packages/pod/pod-0.41-py2.6.egg/pod/__init__.py
@@ -0,0 +1,12 @@
+from core   import Meta, Object, NoSave
+from db     import Db
+from query  import Query, RawQuery
+
+import types
+import fn
+
+from collection import List, Dict, Set
+
+
+
+        
