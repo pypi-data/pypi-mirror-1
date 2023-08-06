d5abc3d6f4e3b36a234a7e206f612866277996b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:52 +0000

    Add package genzshcomp/0.0.4/genzshcomp-0.0.4.tar.gz

---
+++ packages/genzshcomp/genzshcomp-0.0.4.tar.gz/genzshcomp-0.0.4/genzshcomp.py
@@ -0,0 +1,92 @@
+"""automatic generated to zsh completion function file"""
+
+__version__ = '0.0.4'
+__author__ = 'Hideo Hattroi <hhatto.jp@gmail.com>'
+__license__ = 'NewBSDLicense'
+
+
+def get_parser_type(parser_obj):
+    """return to 'argparse' or 'optparse'"""
+    return parser_obj.__module__
+
+
+def _escape_squarebracket(strings):
+    """escape to only squarebracket.
+
+    >>> print _escape_squarebracket("hoge")
+    hoge
+    >>> print _escape_squarebracket("[hoge")
+    \[hoge
+    >>> print _escape_squarebracket("hoge]")
+    hoge\]
+    >>> print _escape_squarebracket("[hoge]")
+    \[hoge\]
+    """
+    ret = []
+    for string in strings:
+        if string == '[' or string == ']':
+            string = '\\' + string
+        ret.append(string)
+    return "".join(ret)
+
+
+class ZshCompletionGenerator(object):
+
+    """Generator of Zsh Completion Function"""
+
+    def __init__(self, commandname=None, parser=None):
+        self.commandname = commandname
+        self.parser = parser
+        self.parser_type = get_parser_type(parser)
+
+    def _get_dircomp(self, opt):
+        """judged to directories and files completion, and
+        return to ':->dirfile' or ''
+        """
+        directory_comp = ":->dirfile"
+        ## version
+        if self.parser_type == 'optparse':
+            if '--version' == opt:
+                directory_comp = ""
+        else:   # argparse
+            if '-v' == opt or '--version' == opt:
+                directory_comp = ""
+        ## help
+        if '-h' == opt or '--help' == opt:
+            directory_comp = ""
+        return directory_comp
+
+    def get(self):
+        """return to string of zsh completion function."""
+        if self.parser_type == 'optparse':
+            actions = self.parser.option_list
+        elif self.parser_type == 'argparse':
+            actions = self.parser._actions
+        ret = []
+        ret.append("#compdef %s\n" % self.commandname)
+        ret.append("typeset -A opt_args")
+        ret.append("local context state line\n")
+        ret.append("_arguments -s -S \\")
+        for action in actions:
+            if action.metavar:
+                metavar = ":%s:" % action.metavar
+            else:
+                metavar = ""
+            if self.parser_type == 'optparse':
+                opts = [i for i in action._long_opts]
+                opts += [i for i in action._short_opts]
+            elif self.parser_type == 'argparse':
+                opts = action.option_strings
+            for opt in opts:
+                directory_comp = self._get_dircomp(opt)
+                tmp = "  \"%s[%s]%s:%s\" \\" % (opt,
+                      _escape_squarebracket(action.help),
+                      metavar, directory_comp)
+                ret.append(tmp)
+        ret.append("  \"::dirfile:_files\" \\")
+        state = "case $state in\n"\
+                "(dirfile)\n"\
+                "  _files -/ && return 0\n  ;;\nesac\n"
+        ret.append("  && return 0")
+        ret.append(state)
+        return "\n".join(ret)
---
+++ packages/genzshcomp/genzshcomp-0.0.4.tar.gz/genzshcomp-0.0.4/test/test_genzshcomp.py
@@ -0,0 +1,102 @@
+from unittest import TestCase, main
+from argparse import ArgumentParser
+from optparse import OptionParser
+import genzshcomp
+
+
+class TestParserType(TestCase):
+
+    def test_argparse(self):
+        target = ArgumentParser()
+        ret = genzshcomp.get_parser_type(target)
+        self.assertEqual(ret, "argparse")
+
+    def test_optparse(self):
+        target = OptionParser()
+        ret = genzshcomp.get_parser_type(target)
+        self.assertEqual(ret, "optparse")
+
+
+class TestEscape(TestCase):
+
+    def test_non_squarebracket(self):
+        self.assertEqual("hoge", genzshcomp._escape_squarebracket("hoge"))
+
+    def test_squarebracket_left(self):
+        self.assertEqual("\\[hoge", genzshcomp._escape_squarebracket("[hoge"))
+
+    def test_squarebracket_right(self):
+        self.assertEqual("hoge\\]", genzshcomp._escape_squarebracket("hoge]"))
+
+    def test_squarebracket_leftright(self):
+        self.assertEqual("\\[hoge\\]",
+                         genzshcomp._escape_squarebracket("[hoge]"))
+
+    def test_squarebracket_rightdouble(self):
+        self.assertEqual("hoge\\]\\]",
+                         genzshcomp._escape_squarebracket("hoge]]"))
+
+    def test_squarebracket_leftdouble(self):
+        self.assertEqual("\\[\\[hoge",
+                         genzshcomp._escape_squarebracket("[[hoge"))
+
+    def test_squarebracket_leftrightdouble(self):
+        self.assertEqual("\\[\\[hoge\\]\\]",
+                         genzshcomp._escape_squarebracket("[[hoge]]"))
+
+
+class TestReturnDirCompString(TestCase):
+
+    def test_argparse_help_short(self):
+        parser = ArgumentParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual('', generator._get_dircomp('-h'))
+
+    def test_argparse_help_long(self):
+        parser = ArgumentParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual('', generator._get_dircomp('--help'))
+
+    def test_argparse_version_short(self):
+        parser = ArgumentParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual('', generator._get_dircomp('-v'))
+
+    def test_argparse_version_long(self):
+        parser = ArgumentParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual('', generator._get_dircomp('--version'))
+
+    def test_argparse_dirfiles(self):
+        parser = ArgumentParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual(':->dirfile', generator._get_dircomp('-c'))
+
+    def test_optparse_help_short(self):
+        parser = OptionParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual('', generator._get_dircomp('-h'))
+
+    def test_optparse_help_long(self):
+        parser = OptionParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual('', generator._get_dircomp('--help'))
+
+    def test_optparse_version_short(self):
+        parser = OptionParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual(':->dirfile', generator._get_dircomp('-v'))
+
+    def test_optparse_version_long(self):
+        parser = OptionParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertNotEqual(':->dirfile', generator._get_dircomp('--version'))
+
+    def test_optparse_dirfiles(self):
+        parser = OptionParser()
+        generator = genzshcomp.ZshCompletionGenerator('dummy', parser)
+        self.assertEqual(':->dirfile', generator._get_dircomp('-c'))
+
+
+if __name__ == '__main__':
+    main()
