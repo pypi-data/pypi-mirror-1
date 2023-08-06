4f375ad852aafcaefec17f58158392215ea64704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:18 +0000

    Add package django-testmaker/0.2a2/django-testmaker-0.2a2.tar.gz

---
+++ packages/django-testmaker/django-testmaker-0.2a2.tar.gz/django-testmaker-0.2a2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-testmaker",
+    version = "0.2a2",
+    packages = find_packages(),
+    author = "Eric Holscher",
+    author_email = "eric@ericholscher.com",
+    description = "A package to help automate creation of testing in Django",
+    url = "http://code.google.com/p/django-testmaker/",
+)
