1f68f0b278aab02aad5a01a6efda8601ea095845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:52 +0000

    Add package Links/1.0/Links-1.0.tar.gz

---
+++ packages/Links/Links-1.0.tar.gz/Links-1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Links',
+      version='1.0',
+      description='Link-utilities missing from urllib|urllib2',
+      author='kaleissin',
+      author_email='kaleissin at gmail com',
+      license='MIT',
+      py_modules=['check'],
+      platform=['all'],
+      url = '',
+     )
+
