d800fd6afdfb0f3660600c98483630b1d4441070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:56 +0000

    Add package collective.formcriteria/0.8/collective.formcriteria-0.8.tar.gz

---
+++ packages/collective.formcriteria/collective.formcriteria-0.8.tar.gz/collective.formcriteria-0.8/collective/formcriteria/criteria/common.py
@@ -0,0 +1,75 @@
+from zope import interface
+
+from Products.Archetypes import atapi
+from Products.ATContentTypes import criteria
+
+from collective.formcriteria import interfaces
+from collective.formcriteria import form
+
+missing = object()
+
+def makeVocabularyForFields(*fields):
+    return atapi.DisplayList(
+        (field.getName(), field.widget.label)
+        for field in fields)
+
+def replaceCriterionRegistration(old, new):
+    indices = criteria._criterionRegistry.indicesByCriterion(
+        old.meta_type)
+    criteria.unregisterCriterion(old)
+    criteria.registerCriterion(new, indices)
+
+class FormCriterion(object):
+    """A criterion that generates a search form field."""
+    interface.implements(interfaces.IFormCriterion)
+
+    schema = atapi.Schema((
+        atapi.LinesField(
+            'formFields',
+            widget=atapi.MultiSelectionWidget(
+                label=u'Form Fields',
+                description=
+                u'Select any fields for this criterion that should'
+                u'appear on a search form',
+                format='checkbox')),))
+
+    makeFormKey = form.makeFormKey
+
+    def getFormFieldValue(self, field_name, raw=False, REQUEST=None,
+                          **kw):
+        """
+        Get the field value from the request.
+
+        Fall back to the criterion value.
+        """
+        if REQUEST is None:
+            REQUEST = self.REQUEST
+
+        field = self.getField(field_name)
+        full_name = self.makeFormKey(self.getId(), field_name)
+
+        # Remove the criterion ids from and relevant request keys
+        form = dict(
+            (key.replace(full_name, field_name, 1), REQUEST[key])
+            for key in REQUEST.keys() if key.startswith(full_name))
+
+        if not form:
+            if raw:
+                return field.getRaw(self, **kw)
+            return field.get(self, **kw)
+
+        result = field.widget.process_form(
+            self, field, form,
+            empty_marker=missing, emptyReturnsMarker=True)
+
+        if result is missing:
+            return field.get(self, **kw)
+
+        value, mutator_kw = result
+        return value
+
+    def Value(self, **kw):
+        return self.getFormFieldValue('value', **kw)
+
+    def getRawValue(self, **kw):
+        return self.getFormFieldValue('value', raw=True, **kw)
---
+++ packages/collective.formcriteria/collective.formcriteria-0.8.tar.gz/collective.formcriteria-0.8/collective/formcriteria/topic.py
@@ -0,0 +1,61 @@
+from zope import interface
+
+from Products.Archetypes import atapi
+from Products.ATContentTypes import interfaces as atct_ifaces
+from Products.ATContentTypes import config 
+from Products.ATContentTypes.content import topic
+from Products.ATContentTypes import criteria
+from Products.CMFPlone import CatalogTool
+
+from collective.formcriteria import interfaces
+
+fake_sort_indices = {'unsorted': 'Relevance',
+                     'sort_on': 'User Selected'}
+
+class Topic(topic.ATTopic):
+    """A collection supporting form criteria"""
+    interface.implements(interfaces.IFormTopic)
+
+    schema = topic.ATTopic.schema.copy() + atapi.Schema((
+        atapi.StringField(
+            'formLayout',
+            default='atct_topic_view',
+            vocabulary='getPossibleFormLayouts',
+            widget=atapi.SelectionWidget(
+                label=u'Form Results Layout',
+                description=
+                u'Select the display layout use for results.  '
+                u'Used only with the "Search Form" layout')),
+        ))
+
+    def getPossibleFormLayouts(self):
+        """Return all valid form results display laouts"""
+        return atapi.DisplayList(
+            layout for layout in self.getAvailableLayouts()
+            if layout[0] != 'criteria_form')
+
+    def criteriaByIndexId(self, indexId):
+        catalog_tool = getattr(self, CatalogTool.CatalogTool.id)
+
+        if indexId in fake_sort_indices:
+            meta_type = indexId
+        else:
+            meta_type = catalog_tool.Indexes[indexId].meta_type
+
+        results = criteria._criterionRegistry.criteriaByIndex(
+            meta_type)
+        return results
+
+    def listSortCriteria(self):
+        """Return a list of our sort criteria objects.
+        """
+        return [val for val in self.listCriteria() if
+                atct_ifaces.IATTopicSortCriterion.isImplementedBy(val)]
+
+    def getFriendlyName(self, index):
+        """Get the friendly name for an index from the tool"""
+        if index in fake_sort_indices:
+            return fake_sort_indices[index]
+        return getattr(self, config.TOOLNAME).getFriendlyName(index)
+    
+atapi.registerType(Topic, 'collective.formcriteria')
