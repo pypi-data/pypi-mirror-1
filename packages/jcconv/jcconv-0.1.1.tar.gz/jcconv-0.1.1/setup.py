0466755e6de617ee0b14a695aeeb08e09fe4ae30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:15 +0000

    Add package jcconv/0.1.1/jcconv-0.1.1.tar.gz

---
+++ packages/jcconv/jcconv-0.1.1.tar.gz/jcconv-0.1.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import sys
+import unittest
+
+import jcconv
+from jcconv import __version__, __license__, __author__
+from jcconv import jcconv_test
+
+import sys
+reload(sys).setdefaultencoding("UTF-8")
+
+if __name__ == '__main__':
+  # run module test
+  loader = unittest.TestLoader()
+  result = unittest.TestResult()
+  suite  = loader.loadTestsFromModule(jcconv_test)
+  suite.run(result)
+  if not result.wasSuccessful():
+    print "unit tests have failed!"
+    print "aborted to make a source distribution"
+    sys.exit(1)
+
+  # build distribution package
+  setup(
+    name             = 'jcconv',
+    version          = __version__,
+    py_modules       = ['jcconv/jcconv', 'jcconv/jcconv_test'],
+    description      = 'jcconv "JapaneseCharacterCONVerter", interconvert hiragana, katakana, halfwidth kana',
+    long_description = jcconv.__doc__,
+    author           = __author__,
+    author_email     = 'taichino@gmail.com',
+    url              = 'http://github.com/taichino/jcconv',
+    keywords         = 'japanese converter, hiragana, katakana, half-width kana',
+    license          = __license__,
+    classifiers      = ["Development Status :: 3 - Alpha",
+                        "Intended Audience :: Developers",
+                        "License :: OSI Approved :: MIT License",
+                        "Operating System :: POSIX",
+                        "Programming Language :: Python",
+                        "Topic :: Software Development :: Libraries :: Python Modules"]
+    )
