735bb677c35eab819a293f073a124d1b171cdda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:55 +0000

    Add package zope.app.principalannotation/3.7.0/zope.app.principalannotation-3.7.0.tar.gz

---
+++ packages/zope.app.principalannotation/zope.app.principalannotation-3.7.0.tar.gz/zope.app.principalannotation-3.7.0/src/zope/app/principalannotation/__init__.py
@@ -0,0 +1,23 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Implementation of `IPrincipalAnnotationUtility`.
+
+$Id: __init__.py 97695 2009-03-09 14:50:04Z nadako $
+"""
+
+# BBB: the functionality was moved to zope.principalannotation
+from zope.principalannotation.utility import (
+    PrincipalAnnotationUtility,
+    Annotations,
+    annotations) 
