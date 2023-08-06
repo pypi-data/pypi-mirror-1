585542c9820175ad0be79e00e3d7c367830e6ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:35 +0000

    Add package qam/0.1.3/qam-0.1.3.tar.gz

---
+++ packages/qam/qam-0.1.3.tar.gz/qam-0.1.3/src/qam/__init__.py
@@ -0,0 +1,8 @@
+"""QAM is a simple RPC Framework which uses the AMQP Protocoll as Transport Protocoll."""
+VERSION = (0, 1, 3)
+__version__ = ".".join(map(str, VERSION))
+__author__ = "QAM Team"
+__contact__ = "qamteam@gmail.com"
+__homepage__ = "http://packages.python.org/qam"
+__docformat__ = "restructuredtext"
+
