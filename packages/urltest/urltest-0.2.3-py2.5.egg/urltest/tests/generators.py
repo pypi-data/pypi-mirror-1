3581c846fb28012abb8cc8f1344c1840e7600f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:26 +0000

    Add package urltest/0.2.3/urltest-0.2.3-py2.5.egg

---
+++ packages/urltest/urltest-0.2.3-py2.5.egg/urltest/tests/generators.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"These tests check the test generator in urltest"
+
+# standard library
+import unittest
+
+# module under test
+from urltest import test_generator
+
+
+class GeneratorTests(unittest.TestCase):
+
+    def test_method_generator_returns_callable(self):
+        item = {'url': "/", 'code': 200}
+        test_method = test_generator(item)
+        assert hasattr(test_method, '__call__')
+
+if __name__ == "__main__":
+    unittest.main()
