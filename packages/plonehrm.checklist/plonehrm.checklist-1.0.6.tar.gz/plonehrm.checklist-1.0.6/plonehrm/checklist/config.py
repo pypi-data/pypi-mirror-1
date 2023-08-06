a818611ec46664a30d4545af8bcd032a1079e83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:59 +0000

    Add package plonehrm.checklist/1.0.6/plonehrm.checklist-1.0.6.tar.gz

---
+++ packages/plonehrm.checklist/plonehrm.checklist-1.0.6.tar.gz/plonehrm.checklist-1.0.6/plonehrm/checklist/config.py
@@ -0,0 +1,8 @@
+"""Basic configuration settings.
+"""
+
+__author__ = """Jean-Paul Ladage <j.ladage@zestsoftware.nl>"""
+__docformat__ = 'plaintext'
+
+PROJECTNAME = "plonehrm.checklist"
+product_globals = globals()
