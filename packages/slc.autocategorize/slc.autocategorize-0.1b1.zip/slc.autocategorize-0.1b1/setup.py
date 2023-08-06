63a45b86ca66c23b41bad4048d14c55ba0c12b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:04 +0000

    Add package slc.autocategorize/0.1b1/slc.autocategorize-0.1b1.zip

---
+++ packages/slc.autocategorize/slc.autocategorize-0.1b1.zip/slc.autocategorize-0.1b1/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1b1'
+
+tests_require=[
+        'interlude',
+        ]
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='slc.autocategorize',
+    version=version,
+    description="Automatically inherit the parent folder's categories when you "
+    "create an object.",
+    long_description = (
+        read('README.txt')
+        + '\n' +
+        read("slc", "autocategorize", "README.txt")
+        + '\n' +
+        'Change history\n'
+        '**************\n'
+        + '\n' +
+        read("docs", "HISTORY.txt")
+        ),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='JC Brand, Syslab.com GmbH',
+    author_email='brand@syslab.com',
+    url='http://svn.plone.org/svn/plone/plone.example',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['slc'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=["PasteScript"],
+    paster_plugins = ["ZopeSkel"],
+    )
