d7d775f22e8d4fcb7c818922bcf6f515d4130c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:22 +0000

    Add package wikitools/1.0/wikitools-1.0.zip

---
+++ packages/wikitools/wikitools-1.0.zip/wikitools-1.0/setup.py
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wikitools',
+      version='1.0',
+      description='Python package for interacting with a MediaWiki wiki',
+	  long_description = """A Python package for interacting with a MediaWiki wiki using the MediaWiki API.
+Designed for MediaWiki version 1.15 and higher, should work on 1.13, older
+versions may have bugs.
+The edit-API must be enabled on the site to use editing features.
+This is atill a beta release and may have bugs, especially when dealing with
+non-ASCII text.""",
+      author='Alex Z. (User:Mr.Z-man @ en.wikipedia)',
+      author_email='mrzmanwiki@gmail.com',
+      url='http://code.google.com/p/python-wikitools/',
+	  license='GPL v3',
+      packages=['wikitools'],
+	  package_data={'wikitools': ['wikitools/COPYING', 'wikitools/README', 'wikitools/CHANGELOG']},
+     )
