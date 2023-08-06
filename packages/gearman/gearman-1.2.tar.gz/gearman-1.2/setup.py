93511f5816fea291a54616721ae3c7619a6c7d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:24 +0000

    Add package gearman/1.2/gearman-1.2.tar.gz

---
+++ packages/gearman/gearman-1.2.tar.gz/gearman-1.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'gearman',
+    version = '1.2',
+    description = 'Gearman Client',
+    author = 'Samuel Stauffer',
+    author_email = 'samuel@descolada.com',
+    url = 'http://danga.com/gearman/',
+    packages = ['gearman'],
+)
