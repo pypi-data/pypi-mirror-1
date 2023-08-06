1bff3641d02a9a3504d5e13f17343e9d8899da9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:09 +0000

    Add package feed2mb/0.7.8/feed2mb-0.7.8-py2.5.egg

---
+++ packages/feed2mb/feed2mb-0.7.8-py2.5.egg/feed2mb/shortener.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# feed2twitter
+# Copyright (C) 2009 EBC - Empresa Brasil de Comunicação
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import urllib
+from xml.dom.minidom import parseString
+
+class TinyURL(object):
+    def short(self,url):
+        try:
+            params = urllib.urlencode({'url':url})
+            fp = urllib.urlopen("http://tinyurl.com/api-create.php", params)
+            return fp.read()
+
+        except IOError, e:
+            raise "urllib error."
+
+class IsGD(object):
+    def short(self,url):
+        try:
+            params = urllib.urlencode({'longurl':url})
+            url = "http://is.gd/api.php?%s" % params
+            fp = urllib.urlopen(url)
+            return fp.read()
+
+        except IOError, e:
+            raise "urllib error."
+
+class MigreME(object):
+    def short(self,url):
+        try:
+            params = urllib.urlencode({'url':url})
+            url = "http://migre.me/api.xml?%s" % params
+            fp = urllib.urlopen(url)
+            dom = parseString(fp.read())
+            return dom.getElementsByTagName('migre')[0].childNodes[0].data
+
+        except IOError, e:
+            raise "urllib error."
+
+services = {
+    'tinyurl' : TinyURL,
+    'is.gd' : IsGD,
+    'migre.me' : MigreME
+}
