d13460f1da3218b1f5bf02f39893775368ca592b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:16 +0000

    Add package zc.zodbdgc/0.5.0/zc.zodbdgc-0.5.0.tar.gz

---
+++ packages/zc.zodbdgc/zc.zodbdgc-0.5.0.tar.gz/zc.zodbdgc-0.5.0/src/zc/zodbdgc/tests.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""XXX short summary goes here.
+
+$Id: tests.py 100884 2009-06-11 19:35:53Z jim $
+"""
+from zope.testing import doctest, setupstack, renormalizing
+import re
+import unittest
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite(
+            'README.test', 'oidset.test',
+            setUp=setupstack.setUpDirectory, tearDown = setupstack.tearDown,
+            checker=renormalizing.RENormalizing([
+                (re.compile('usage'), 'Usage'),
+                (re.compile('options'), 'Options'),
+                ]),
+            ),
+        ))
+
