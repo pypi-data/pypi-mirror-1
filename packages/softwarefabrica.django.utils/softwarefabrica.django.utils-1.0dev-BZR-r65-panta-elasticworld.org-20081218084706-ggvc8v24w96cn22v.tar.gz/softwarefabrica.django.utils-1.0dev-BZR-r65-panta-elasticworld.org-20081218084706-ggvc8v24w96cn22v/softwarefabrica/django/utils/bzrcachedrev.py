d537b8913782b1159f8c6e551435b6168eb5e5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:13 +0000

    Add package softwarefabrica.django.utils/1.0dev-BZR-r65-panta-elasticworld.org-20081218084706-ggvc8v24w96cn22v/softwarefabrica.django.utils-1.0dev-BZR-r65-panta-elasticworld.org-20081218084706-ggvc8v24w96cn22v.tar.gz

---
+++ packages/softwarefabrica.django.utils/softwarefabrica.django.utils-1.0dev-BZR-r65-panta-elasticworld.org-20081218084706-ggvc8v24w96cn22v.tar.gz/softwarefabrica.django.utils-1.0dev-BZR-r65-panta-elasticworld.org-20081218084706-ggvc8v24w96cn22v/softwarefabrica/django/utils/bzrcachedrev.py
@@ -0,0 +1,4 @@
+info    = {'revinfo': (65, 'panta@elasticworld.org-20081218084706-ggvc8v24w96cn22v'), 'basepath': '/arc/bzr/softwarefabrica-utils.bzr/trunk/', 'revno': 65, 'revname': 'panta@elasticworld.org-20081218084706-ggvc8v24w96cn22v', 'inpath': u'softwarefabrica/django/utils/version.py'}
+revno   = 65
+revname = 'panta@elasticworld.org-20081218084706-ggvc8v24w96cn22v'
+revinfo = (65, 'panta@elasticworld.org-20081218084706-ggvc8v24w96cn22v')
