de0a6495de6dc0547d720d1fd6f66900d50bd20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:02 +0000

    Add package clepy/0.3.1/clepy-0.3.1.tar.gz

---
+++ packages/clepy/clepy-0.3.1.tar.gz/clepy-0.3.1/clepy/tests/test_mattwilson.py
@@ -0,0 +1,12 @@
+# vim: set expandtab ts=4 sw=4 filetype=python:
+
+"""
+
+There's doctests inside the functions.  I might add more tests here if I feel
+like it.
+
+"""
+
+__id__ = "$Id: test_mattwilson.py 3 2008-05-12 16:48:43Z mw44118 $"
+
+
---
+++ packages/clepy/clepy-0.3.1.tar.gz/clepy-0.3.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+
+    name="clepy",
+    version="0.3.1",
+    packages=["clepy"],
+
+    install_requires=[
+        'decorator >= 3.0',
+        'multiprocessing',
+    ],
+
+    url="http://code.google.com/p/clepy",
+    license="MIT License",
+    description="utilities from the Cleveland Python user group", 
+    maintainer="W. Matthew Wilson",
+    maintainer_email="matt@tplus1.com",
+)
+
