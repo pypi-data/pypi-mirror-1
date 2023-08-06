cf4246668e83e9ef848b0d59d130453464d34acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:04 +0000

    Add package quantities/0.5.2/quantities-0.5.2.zip

---
+++ packages/quantities/quantities-0.5.2.zip/quantities-0.5.2/quantities/tests/test_50_persistence.py
@@ -0,0 +1,41 @@
+﻿# -*- coding: utf-8 -*-
+
+import pickle
+
+from nose.tools import *
+from numpy.testing import *
+from numpy.testing.utils import *
+
+import numpy as np
+import quantities as pq
+
+from . import assert_quantity_equal, assert_quantity_almost_equal
+
+
+def test_unitquantitiy_persistance():
+    x = pq.m
+    y = pickle.loads(pickle.dumps(x))
+    assert_array_equal(x, y)
+
+    x = pq.CompoundUnit("pc/cm**3")
+    y = pickle.loads(pickle.dumps(x))
+    assert_array_equal(x, y)
+
+def test_quantitiy_persistance():
+    x = 20*pq.m
+    y = pickle.loads(pickle.dumps(x))
+    assert_array_equal(x, y)
+
+def test_uncertainquantitiy_persistance():
+    x = pq.UncertainQuantity(20, 'm', 0.2)
+    y = pickle.loads(pickle.dumps(x))
+    assert_array_equal(x, y)
+
+def test_unitconstant_persistance():
+    x = pq.constants.m_e
+    y = pickle.loads(pickle.dumps(x))
+    assert_array_equal(x, y)
+
+
+if __name__ == "__main__":
+    run_module_suite()
