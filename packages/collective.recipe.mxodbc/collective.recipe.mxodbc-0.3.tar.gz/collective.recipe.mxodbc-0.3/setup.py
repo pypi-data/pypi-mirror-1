c5bfbdcedec5af89f418ec6adde08731b229a95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:42 +0000

    Add package collective.recipe.mxodbc/0.3/collective.recipe.mxodbc-0.3.tar.gz

---
+++ packages/collective.recipe.mxodbc/collective.recipe.mxodbc-0.3.tar.gz/collective.recipe.mxodbc-0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '0.3'
+
+setup(name='collective.recipe.mxodbc',
+      version=version,
+      description="A buildout recipe to install eGenix mx.ODBC and a licence",
+      long_description="""\
+""",
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "Framework :: Buildout",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='build mx.ODBC',
+      author='Jarn',
+      author_email='info@jarn.com',
+      url='http://svn.plone.org/svn/collective/buildout/collective.recipe.mxodbc',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.recipe'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [zc.buildout]
+      default = collective.recipe.mxodbc:Recipe
+      """,
+      )
