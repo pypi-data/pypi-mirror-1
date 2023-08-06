e0aa6044de54a236beaf97dccebc4b70439a2c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:38 +0000

    Add package collective.filepreviewbehavior/1.0a1/collective.filepreviewbehavior-1.0a1-py2.4.egg

---
+++ packages/collective.filepreviewbehavior/collective.filepreviewbehavior-1.0a1-py2.4.egg/collective/filepreviewbehavior/interfaces.py
@@ -0,0 +1,10 @@
+
+
+from Products import ARFilePreview
+
+
+class IPreviewable( ARFilePreview.interfaces.IPreviewAware ):
+    """ Behavior for enabling Products.ARFilePreview support for dexterity
+        content types
+    """
+
