b6392b447538c56e907e47b37f2bbb9df1821d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:26 +0000

    Add package JCC/2.0/JCC-2.0.tar.gz

---
+++ packages/JCC/JCC-2.0.tar.gz/JCC-2.0/jcc/config.py
@@ -0,0 +1,7 @@
+
+INCLUDES=['/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers']
+CFLAGS=['-fno-strict-aliasing']
+DEBUG_CFLAGS=['-O0', '-g', '-DDEBUG']
+LFLAGS=['-framework', 'JavaVM']
+SHARED=True
+
