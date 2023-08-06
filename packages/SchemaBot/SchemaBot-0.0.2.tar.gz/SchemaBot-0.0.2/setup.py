fd0c7f3a69deda39348c66f6eff2630745c06069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package SchemaBot/0.0.2/SchemaBot-0.0.2.tar.gz

---
+++ packages/SchemaBot/SchemaBot-0.0.2.tar.gz/SchemaBot-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.2'
+
+setup(name='SchemaBot',
+      version=version,
+      description="Database schema version control library for SQLAlchemy",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Chris Miles',
+      author_email='miles.chris@gmail.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          "SQLAlchemy >= 0.4",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
