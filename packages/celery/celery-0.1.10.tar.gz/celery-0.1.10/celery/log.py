2ef952d9d3d97c5e1e2b49b0e3c737e366e0f2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:02 +0000

    Add package celery/0.1.10/celery-0.1.10.tar.gz

---
+++ packages/celery/celery-0.1.10.tar.gz/celery-0.1.10/celery/log.py
@@ -0,0 +1,48 @@
+import multiprocessing
+import os
+import sys
+import time
+import logging
+from celery.conf import LOG_FORMAT, DAEMON_LOG_LEVEL
+
+
+def setup_logger(loglevel=DAEMON_LOG_LEVEL, logfile=None, format=LOG_FORMAT,
+        **kwargs):
+    """Setup the ``multiprocessing`` logger. If ``logfile`` is not specified,
+    ``stderr`` is used.
+    
+    Returns logger object.
+    """
+    logger = multiprocessing.get_logger()
+    if logfile:
+        if hasattr(logfile, "write"):
+            log_file_handler = logging.StreamHandler(logfile)
+        else:
+            log_file_handler = logging.FileHandler(logfile)
+        formatter = logging.Formatter(format)
+        log_file_handler.setFormatter(formatter)
+        logger.addHandler(log_file_handler)
+    else:
+        multiprocessing.log_to_stderr()
+    logger.setLevel(loglevel)
+    return logger
+
+
+def emergency_error(logfile, message):
+    """Emergency error logging, for when there's no standard file
+    descriptors open because the process has been daemonized or for
+    some other reason."""
+    logfh_needs_to_close = False
+    if not logfile:
+        logfile = sys.stderr
+    if hasattr(logfile, "write"):
+        logfh = logfile
+    else:
+        logfh = open(logfile, "a")
+        logfh_needs_to_close = True
+    logfh.write("[%(asctime)s: FATAL/%(pid)d]: %(message)s\n" % {
+                    "asctime": time.asctime(),
+                    "pid": os.getpid(),
+                    "message": message})
+    if logfh_needs_to_close:
+        logfh.close()
---
+++ packages/celery/celery-0.1.10.tar.gz/celery-0.1.10/celery/messaging.py
@@ -0,0 +1,45 @@
+from carrot.messaging import Publisher, Consumer
+from celery import conf
+import uuid
+
+
+class NoProcessConsumer(Consumer):
+    
+    def receive(self, message_data, message):
+        raise NotImplementedError(
+                "Don't use process_next() or wait() with the TaskConsumer!")
+
+
+class TaskPublisher(Publisher):
+    exchange = conf.AMQP_EXCHANGE
+    routing_key = conf.AMQP_ROUTING_KEY
+
+    def delay_task(self, task_name, *task_args, **task_kwargs):
+        return self._delay_task(task_name=task_name, args=task_args,
+                                kwargs=task_kwargs)
+
+    def delay_task_in_set(self, task_name, taskset_id, task_args,
+            task_kwargs):
+        return self._delay_task(task_name=task_name, part_of_set=taskset_id,
+                                args=task_args, kwargs=task_kwargs)
+
+    def _delay_task(self, task_name, part_of_set=None, args=None, kwargs=None):
+        args = args or []
+        kwargs = kwargs or {}
+        task_id = str(uuid.uuid4())
+        message_data = {
+            "id": task_id,
+            "task": task_name,
+            "args": args,
+            "kwargs": kwargs,
+        }
+        if part_of_set:
+            message_data["taskset"] = part_of_set
+        self.send(message_data)
+        return task_id
+
+
+class TaskConsumer(NoProcessConsumer):
+    queue = conf.AMQP_CONSUMER_QUEUE
+    exchange = conf.AMQP_EXCHANGE
+    routing_key = conf.AMQP_ROUTING_KEY
