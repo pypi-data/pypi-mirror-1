16705c8270b1738f22fd608f096623f9df0078b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:55 +0000

    Add package pysform/0.1/pysform-0.1-py2.5.egg

---
+++ packages/pysform/pysform-0.1-py2.5.egg/pysform/__init__.py
@@ -0,0 +1,14 @@
+from pysform.form import Form
+
+#all_by_module = {
+#    'pysform.forms':       ['Form'],
+#    'pysform.elements':    ['ButtonElement', 'CheckboxElement', 'FileElement',
+#            'HiddenElement', 'ImageElement', 'SubmitElement', 'ResetElement',
+#            'CancelElement', 'TextElement', 'ConfirmElement', 'DateElement', 
+#            'DateTimeElement', 'EmailElement', 'PasswordElement', 'TimeElement',
+#            'URLElement', 'SelectElement', 'TextAreaElement', 'PassThru', 
+#            'StaticElement', 'RenderGroupElement', 'HeaderElement',
+#            'RadioElement', 'CheckboxGroupElement'
+#    ]
+#        
+#}
