a72cfa6a2397c5794e49ffaf1eae16c6cf5464be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:27 +0000

    Add package nl/0.73/nl-0.73.tar.gz

---
+++ packages/nl/nl-0.73.tar.gz/nl-0.73/src/nl/log.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2008 by Enrique Pérez Arnaud <enriquepablo@gmail.com>
+#
+# This file is part of ln.
+#
+# ln is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ln is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ln.  If not, see <http://www.gnu.org/licenses/>.
+
+# this snippet comes from
+# http://www.mechanicalcat.net/richard/log/Python/Simple_usage_of_Python_s_logging_module
+
+from os import path
+import logging
+
+logger = logging.getLogger('nl')
+here = path.join(path.dirname(__file__))
+log_file = here + '/clips/log.clp'
+if not path.isfile(log_file):
+    f = open(log_file)
+    f.write('log file for nl\n\n')
+    f.close()
+hdlr = logging.FileHandler(log_file)
+formatter = logging.Formatter('%(message)s')
+# formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.FATAL)
