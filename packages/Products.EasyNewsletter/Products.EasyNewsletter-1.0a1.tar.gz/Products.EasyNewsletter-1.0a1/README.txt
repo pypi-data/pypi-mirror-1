b111e9de4ec5f187a8e59c3f6d5baa052c016f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:23 +0000

    Add package Products.EasyNewsletter/1.0a1/Products.EasyNewsletter-1.0a1.tar.gz

---
+++ packages/Products.EasyNewsletter/Products.EasyNewsletter-1.0a1.tar.gz/Products.EasyNewsletter-1.0a1/README.txt
@@ -0,0 +1,26 @@
+EasyNewsletter
+============== 
+
+A newsletter for Plone.
+
+Features
+========
+
+- Plonish (uses Plone's Collections to collect content)
+
+- Subscribing / Unsubscribing
+
+- Variable templates to generate a newsletter
+
+Documentation 
+=============
+
+For more documentation please visit: http://packages.python.org/EasyNewsletter/
+
+Changes
+=======
+
+1.0 beta 1 (2009-09-19)
+---------------------
+
+* First version for Plone 3
---
+++ packages/Products.EasyNewsletter/Products.EasyNewsletter-1.0a1.tar.gz/Products.EasyNewsletter-1.0a1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a1'
+
+setup(name='Products.EasyNewsletter',
+      version=version,
+      description="A newsletter for Plone",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 4 - Beta",
+        ],
+      keywords='',
+      author='Kai Diefenbach',
+      author_email='kai.diefenbach@iqpp.de',
+      url='http://www.iqpp.de',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'distribute',
+          # -*- Extra requirements: -*-
+          'Products.TemplateFields',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
