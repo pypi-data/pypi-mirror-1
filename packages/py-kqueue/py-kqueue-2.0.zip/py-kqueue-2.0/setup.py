441bb531ff64ff6381d194b30691bec0f0ee3d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:26 +0000

    Add package py-kqueue/2.0/py-kqueue-2.0.zip

---
+++ packages/py-kqueue/py-kqueue-2.0.zip/py-kqueue-2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.core import Extension
+
+module1 = Extension('kqueuemodule',
+                    sources = ['kqueuemodule.c'])
+
+setup (name = 'py-kqueue',
+       version = '2.0',
+       description = 'This is a kqueue package',
+       author='Jaromir Dolecek',author_email='jdolecek@netbsd.org',
+       ext_modules = [module1])
