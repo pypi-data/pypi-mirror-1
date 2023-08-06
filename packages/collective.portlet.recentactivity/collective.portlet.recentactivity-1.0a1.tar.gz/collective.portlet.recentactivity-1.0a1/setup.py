6d9b01f8fcb5c0c2912ae945ca68d70837e4ad95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:51 +0000

    Add package collective.portlet.recentactivity/1.0a1/collective.portlet.recentactivity-1.0a1.tar.gz

---
+++ packages/collective.portlet.recentactivity/collective.portlet.recentactivity-1.0a1.tar.gz/collective.portlet.recentactivity-1.0a1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a1'
+
+setup(name='collective.portlet.recentactivity',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Timo Stollenwerk',
+      author_email='timo@zmag.de',
+      url='http://svn.plone.org/svn/collective/collective.portlet.recentactivity',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+      )
