9f166e75f5762d00fa960187b5b5f1da820d5418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:32 +0000

    Add package jaraco.nxt/1.1/jaraco.nxt-1.1.zip

---
+++ packages/jaraco.nxt/jaraco.nxt-1.1.zip/jaraco.nxt-1.1/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: UTF-8 -*-
+
+""" Setup script for building jaraco.nxt distribution
+
+Copyright © 2008 Jason R. Coombs
+"""
+
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+__author__ = 'Jason R. Coombs <jaraco@jaraco.com>'
+__version__ = '$Rev: 835 $'[6:-2]
+__svnauthor__ = '$Author: jaraco $'[9:-2]
+__date__ = '$Date: 2009-02-19 10:05:45 -0500 (Thu, 19 Feb 2009) $'[7:-2]
+
+setup (name = 'jaraco.nxt',
+		version = '1.1',
+		description = 'Logo Mindstorms NXT Routines',
+		author = 'Jason R. Coombs',
+		author_email = 'jaraco@jaraco.com',
+		url = 'http://www.jaraco.com/projects/jaraco.nxt',
+		packages = find_packages('src'),
+		package_dir = {'':'src'},
+		zip_safe=True,
+		namespace_packages = ['jaraco',],
+		license = 'MIT',
+		classifiers = [
+			"Development Status :: 4 - Beta",
+			"Intended Audience :: Developers",
+			"Programming Language :: Python",
+		],
+		entry_points = dict(
+			console_scripts = [
+				'nxt-control = jaraco.nxt.controller:serve_forever',
+			],
+		),
+		install_requires=[
+			'pyserial>=2.2',
+			'enum>=0.4.3',
+			'jaraco.util>=2.0',
+			'pyglet',
+		],
+		extras_require = {
+		},
+		dependency_links = [
+		],
+		tests_require=[
+			'nose>=0.10',
+		],
+		test_suite = "nose.collector",
+	)
---
+++ packages/jaraco.nxt/jaraco.nxt-1.1.zip/jaraco.nxt-1.1/src/jaraco/nxt/__init__.py
@@ -0,0 +1,42 @@
+#!python
+
+# $Id: __init__.py 556 2008-06-30 03:49:15Z jaraco $
+
+"""
+jaraco.nxt package
+
+Modules suitable for interacting with the Lego Mindstorms NXT
+products.
+
+Requires a bluetooth connection (and utilizes serial protocol).
+"""
+
+__author__='Jason R. Coombs <jaraco@jaraco.com>'
+__svnauthor__='$Author: jaraco $'[9:-2]
+
+import serial
+import struct
+import time
+
+import messages
+
+def add_options(parser):
+	parser.add_option("-p", "--port")
+
+class Connection(serial.Serial):
+	"""
+	A low-level connection to an NXT brick
+	
+	Requires that the brick is already paired with this device using
+	Bluetooth.
+	
+	Example usage:
+	conn = Connection('COM3')
+	"""
+	def receive(self):
+		'Receive a message from the NXT'
+		return messages.Message.read(self)
+
+	def send(self, message):
+		"Send a message to the NXT"
+		self.write(str(message))
