7d59c78470b62cb741ed2f5e63b0722cdf9fc5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:38 +0000

    Add package pyquery/0.2/pyquery-0.2.tar.gz

---
+++ packages/pyquery/pyquery-0.2.tar.gz/pyquery-0.2/pyquery/pyquery.py
@@ -0,0 +1,487 @@
+#-*- coding:utf-8 -*-
+#
+# Copyright (C) 2008 - Olivier Lauzanne <olauzanne@gmail.com>
+#
+# Distributed under the BSD license, see LICENSE.txt
+from lxml.cssselect import css_to_xpath
+from lxml import etree
+from copy import deepcopy
+
+def selector_to_xpath(selector):
+    """JQuery selector to xpath.
+    TODO: patch cssselect to add :first, :last, ...
+    """
+    selector = selector.replace('[@', '[')
+    return css_to_xpath(selector)
+
+def fromstring(context):
+    """use html parser if we don't have clean xml
+    """
+    try:
+        return etree.fromstring(context)
+    except etree.XMLSyntaxError:
+        return etree.fromstring(context, etree.HTMLParser())
+
+no_default = object()
+
+class FlexibleElement(object):
+    """property to allow a flexible api"""
+    def __init__(self, pget, pset=no_default, pdel=no_default):
+        self.pget = pget
+        self.pset = pset
+        self.pdel = pdel
+    def __get__(self, instance, klass):
+        class _element(object):
+            """real element to support set/get/del attr and item and js call
+            style"""
+            def __call__(prop, *args, **kwargs):
+                return self.pget(instance, *args, **kwargs)
+            __getattr__ = __getitem__ = __setattr__ = __setitem__ = __call__
+            def __delitem__(prop, name):
+                if self.pdel is not no_default:
+                    return self.pdel(instance, name)
+                else:
+                    raise NotImplementedError()
+            __delattr__ = __delitem__
+            def __repr__(prop):
+                return '<flexible_element %s>' % self.pget.func_name
+        return _element()
+    def __set__(self, instance, value):
+        if self.pset is not no_default:
+            self.pset(instance, value)
+        else:
+            raise NotImplementedError()
+
+class PyQuery(list):
+    """See the README.txt"""
+    def __init__(self, *args, **kwargs):
+        html = None
+        elements = []
+
+        if 'parent' in kwargs:
+            self._parent = kwargs.pop('parent')
+        else:
+            self._parent = no_default
+
+        if kwargs:
+            # specific case to get the dom
+            if 'filename' in kwargs:
+                html = file(kwargs['filename']).read()
+            elif 'url' in kwargs:
+                from urllib2 import urlopen
+                html = urlopen(kwargs['url']).read()
+            else:
+                raise ValueError('Invalid keyword arguments %s' % kwargs)
+            elements = [fromstring(html)]
+        else:
+            # get nodes
+
+            # determine context and selector if any
+            selector = context = no_default
+            length = len(args)
+            if len(args) == 1:
+                context = args[0]
+            elif len(args) == 2:
+                selector, context = args
+            else:
+                raise ValueError("You can't do that." +\
+                        " Please, provide arguments")
+
+            # get context
+            if isinstance(context, basestring):
+                try:
+                    elements = [fromstring(context)]
+                except Exception, e:
+                    raise ValueError('%r, %s' % (e, context))
+            elif isinstance(context, self.__class__):
+                # copy
+                elements = context[:]
+            elif isinstance(context, list):
+                elements = context
+            elif isinstance(context, etree._Element):
+                elements = [context]
+
+            # select nodes
+            if elements and selector is not no_default:
+                xpath = selector_to_xpath(selector)
+                results = [tag.xpath(xpath) for tag in elements]
+                # Flatten the results
+                elements = []
+                for r in results:
+                    elements.extend(r)
+
+        list.__init__(self, elements)
+
+    def __call__(self, *args):
+        # just return a new instance
+        length = len(args)
+        if length == 0:
+            raise ValueError('You must provide at least a selector')
+        if len(args) == 1 and not args[0].startswith('<'):
+            args += (self,)
+        result = self.__class__(*args, **dict(parent=self))
+        return result
+
+    # keep original list api prefixed with _
+    _append = list.append
+    _extend = list.extend
+
+    # improve pythonic api
+    def __add__(self, other):
+        assert isinstance(other, self.__class__)
+        return self.__class__(self[:] + other[:])
+
+    def extend(self, other):
+        assert isinstance(other, self.__class__)
+        self._extend(other[:])
+
+    def __str__(self):
+        return ''.join([etree.tostring(e) for e in self])
+
+    def __repr__(self):
+        r = []
+        for el in self:
+            c = el.get('class')
+            c = c and '.' + '.'.join(c.split(' ')) or ''
+            id = el.get('id')
+            id = id and '#' + id or ''
+            r.append('<%s%s%s>' % (el.tag, id, c))
+        return '[' + (', '.join(r)) + ']'
+
+    ##############
+    # Traversing #
+    ##############
+
+    def each(self, func):
+        for e in self:
+            func(self.__class__([e]))
+        return self
+
+    @property
+    def length(self):
+        return len(self)
+
+    def size():
+        return len(self)
+
+    ##############
+    # Attributes #
+    ##############
+    def attr(self, *args, **kwargs):
+
+        mapping = {'class_': 'class', 'for_': 'for'}
+
+        attr = value = no_default
+        length = len(args)
+        if length == 1:
+            attr = args[0]
+            attr = mapping.get(attr, attr)
+        elif length == 2:
+            attr, value = args
+            attr = mapping.get(attr, attr)
+        elif kwargs:
+            attr = {}
+            for k, v in kwargs.items():
+                attr[mapping.get(k, k)] = v
+        else:
+            raise ValueError('Invalid arguments %s %s' % (args, kwargs))
+
+        if not self:
+            return None
+        elif isinstance(attr, dict):
+            for tag in self:
+                for key, value in attr.items():
+                    tag.set(key, value)
+        elif value is no_default:
+            return self[0].get(attr)
+        elif value is None or value == '':
+            return self.removeAttr(attr)
+        else:
+            for tag in self:
+                tag.set(attr, value)
+        return self
+
+    def removeAttr(self, name):
+        for tag in self:
+            del tag.attrib[name]
+        return self
+
+    attr = FlexibleElement(pget=attr, pdel=removeAttr)
+
+    #######
+    # CSS #
+    #######
+    def height(self, value=no_default):
+        return self.attr('height', value)
+
+    def width(self, value=no_default):
+        return self.attr('width', value)
+
+    def addClass(self, value):
+        for tag in self:
+            values = value.split(' ')
+            classes = set((tag.get('class') or '').split())
+            classes = classes.union(values)
+            classes.difference_update([''])
+            tag.set('class', ' '.join(classes))
+        return self
+
+    def removeClass(self, value):
+        for tag in self:
+            values = value.split(' ')
+            classes = set((tag.get('class') or '').split())
+            classes.difference_update(values)
+            classes.difference_update([''])
+            tag.set('class', ' '.join(classes))
+        return self
+
+    def toggleClass(self, value):
+        for tag in self:
+            values = set(value.split(' '))
+            classes = set((tag.get('class') or '').split())
+            values_to_add = values.difference(classes)
+            classes.difference_update(values)
+            classes = classes.union(values_to_add)
+            classes.difference_update([''])
+            tag.set('class', ' '.join(classes))
+        return self
+
+    def css(self, *args, **kwargs):
+
+        attr = value = no_default
+        length = len(args)
+        if length == 1:
+            attr = args[0]
+        elif length == 2:
+            attr, value = args
+        elif kwargs:
+            attr = kwargs
+        else:
+            raise ValueError('Invalid arguments %s %s' % (args, kwargs))
+
+        if isinstance(attr, dict):
+            for tag in self:
+                stripped_keys = [key.strip().replace('_', '-')
+                                 for key in attr.keys()]
+                current = [el.strip()
+                           for el in (tag.get('style') or '').split(';')
+                           if el.strip()
+                           and not el.split(':')[0].strip() in stripped_keys]
+                for key, value in attr.items():
+                    key = key.replace('_', '-')
+                    current.append('%s: %s' % (key, value))
+                tag.set('style', '; '.join(current))
+        elif isinstance(value, basestring):
+            attr = attr.replace('_', '-')
+            for tag in self:
+                current = [el.strip()
+                           for el in (tag.get('style') or '').split(';')
+                           if el.strip()
+                              and not el.split(':')[0].strip() == attr.strip()]
+                current.append('%s: %s' % (attr, value))
+                tag.set('style', '; '.join(current))
+        return self
+
+    css = FlexibleElement(pget=css, pset=css)
+
+    ###################
+    # CORE UI EFFECTS #
+    ###################
+    def hide(self):
+        return self.css('display', 'none')
+
+    def show(self):
+        return self.css('display', 'block')
+
+    ########
+    # HTML #
+    ########
+    def val(self, value=no_default):
+        return self.attr('value', value)
+
+    def html(self, value=no_default):
+        if value is no_default:
+            if not self:
+                return None
+            tag = self[0]
+            children = tag.getchildren()
+            if not children:
+                return tag.text
+            html = tag.text or ''
+            html += ''.join(map(etree.tostring, children))
+            return html
+        else:
+            if isinstance(value, self.__class__):
+                new_html = str(value)
+            elif isinstance(value, basestring):
+                new_html = value
+
+            for tag in self:
+                for child in tag.getchildren():
+                    tag.remove(child)
+                root = etree.fromstring('<root>' + new_html + '</root>')
+                children = root.getchildren()
+                if children:
+                    tag.extend(children)
+                tag.text = root.text
+                tag.tail = root.tail
+        return self
+
+    def text(self, value=no_default):
+        def get_text(tag):
+            text = []
+            if tag.text:
+                text.append(tag.text)
+            for child in tag.getchildren():
+                text.extend(get_text(child))
+            if tag.tail:
+                text.append(tag.tail)
+            return text
+
+        if value is no_default:
+            if not self:
+                return None
+            return ' '.join([''.join(get_text(tag)).strip() for tag in self])
+
+        for tag in self:
+            for child in tag.getchildren():
+                tag.remove(child)
+            tag.text = value
+        return self
+
+    ################
+    # Manipulating #
+    ################
+
+    def _get_root(self, value):
+        is_pyquery_results = isinstance(value, self.__class__)
+        is_string = isinstance(value, basestring)
+        assert is_string or is_pyquery_results, value
+        if is_string:
+            root = etree.fromstring('<root>' + value + '</root>')
+        elif is_pyquery_results:
+            root = value
+        if hasattr(root, 'text') and isinstance(root.text, basestring):
+            root_text = root.text
+        else:
+            root_text = ''
+        return root, root_text
+
+    def append(self, value):
+        root, root_text = self._get_root(value)
+        for i, tag in enumerate(self):
+            if len(tag) > 0: # if the tag has children
+                last_child = tag[-1]
+                if not last_child.tail:
+                    last_child.tail = ''
+                last_child.tail += root_text
+            else:
+                if not tag.text:
+                    tag.text = ''
+                tag.text += root_text
+            if i > 0:
+                root = deepcopy(list(root))
+            tag.extend(root)
+            root = tag[-len(root):]
+        return self
+
+    def appendTo(self, value):
+        value.append(self)
+        return self
+
+    def prepend(self, value):
+        root, root_text = self._get_root(value)
+        for i, tag in enumerate(self):
+            if not tag.text:
+                tag.text = ''
+            if len(root) > 0:
+                root[-1].tail = tag.text
+                tag.text = root_text
+            else:
+                tag.text = root_text + tag.text
+            if i > 0:
+                root = deepcopy(list(root))
+            tag[:0] = root
+            root = tag[:len(root)]
+        return self
+
+    def prependTo(self, value):
+        value.prepend(self)
+        return self
+
+    def after(self, value):
+        root, root_text = self._get_root(value)
+        for i, tag in enumerate(self):
+            if not tag.tail:
+                tag.tail = ''
+            tag.tail += root_text
+            if i > 0:
+                root = deepcopy(list(root))
+            parent = tag.getparent()
+            index = parent.index(tag) + 1
+            parent[index:index] = root
+            root = parent[index:len(root)]
+        return self
+
+    def insertAfter(self, value):
+        value.after(self)
+        return self
+
+    def before(self, value):
+        root, root_text = self._get_root(value)
+        for i, tag in enumerate(self):
+            previous = tag.getprevious()
+            if previous != None:
+                if not previous.tail:
+                    previous.tail = ''
+                previous.tail += root_text
+            else:
+                parent = tag.getparent()
+                if not parent.text:
+                    parent.text = ''
+                parent.text += root_text
+            if i > 0:
+                root = deepcopy(list(root))
+            parent = tag.getparent()
+            index = parent.index(tag)
+            parent[index:index] = root
+            root = parent[index:len(root)]
+        return self
+
+    def insertBefore(self, value):
+        value.before(self)
+        return self
+
+    def replaceWith(self, value):
+        self.before(value)
+        for tag in self:
+            parent = tag.getparent()
+            parent.remove(tag)
+        return self
+
+    def replaceAll(self, expr):
+        if self._parent is no_default:
+            raise ValueError(
+                    'replaceAll can only be used with an object with parent')
+        self._parent(expr).replaceWith(self)
+        return self
+
+    def clone(self):
+        self[:] = [deepcopy(tag) for tag in self]
+        return self
+
+    def empty(self):
+        for tag in self:
+            tag.text = None
+            tag[:] = []
+        return self
+
+    def remove(self, expr=no_default):
+        if expr is no_default:
+            for tag in self:
+                parent = tag.getparent()
+                parent.remove(tag)
+        else:
+            results = self.__class__(expr, self)
+            results.remove()
+        return self
---
+++ packages/pyquery/pyquery-0.2.tar.gz/pyquery-0.2/pyquery/test.py
@@ -0,0 +1,146 @@
+#-*- coding:utf-8 -*-
+#
+# Copyright (C) 2008 - Olivier Lauzanne <olauzanne@gmail.com>
+#
+# Distributed under the BSD license, see LICENSE.txt
+from webob import Request, Response, exc
+from lxml import etree
+import unittest
+import doctest
+import os
+
+import pyquery
+from pyquery import PyQuery as pq
+from ajax import PyQuery as pqa
+
+dirname = os.path.dirname(os.path.abspath(pyquery.__file__))
+path_to_html_file = os.path.join(dirname, 'test.html')
+
+def input_app(environ, start_response):
+    resp = Response()
+    req = Request(environ)
+    if req.path_info == '/':
+        resp.body = '<input name="youyou" type="text" value="" />'
+    elif req.path_info == '/submit':
+        resp.body = '<input type="submit" value="OK" />'
+    else:
+        resp.body = ''
+    return resp(environ, start_response)
+
+
+class DocTest(doctest.DocFileCase):
+
+    path = os.path.join(dirname, 'README.txt')
+
+    def __init__(self, *args, **kwargs):
+        parser = doctest.DocTestParser()
+        doc = open(self.path).read()
+        test = parser.get_doctest(doc, globals(), '', self.path, 0)
+        doctest.DocFileCase.__init__(self, test, optionflags=doctest.ELLIPSIS)
+
+    def setUp(self):
+        test = self._dt_test
+        test.globs.update(globals())
+
+class TestSelector(unittest.TestCase):
+    klass = pq
+    html = """
+           <html>
+            <body>
+              <div>node1</div>
+              <div id="node2">node2</div>
+              <div class="node3">node3</div>
+            </body>
+           </html>
+           """
+
+    html2 = """
+           <html>
+            <body>
+              <div>node1</div>
+            </body>
+           </html>
+           """
+
+    def test_selector_from_doc(self):
+        doc = etree.fromstring(self.html)
+        assert len(self.klass(doc)) == 1
+        assert len(self.klass('div', doc)) == 3
+        assert len(self.klass('div#node2', doc)) == 1
+
+    def test_selector_from_html(self):
+        assert len(self.klass(self.html)) == 1
+        assert len(self.klass('div', self.html)) == 3
+        assert len(self.klass('div#node2', self.html)) == 1
+
+    def test_selector_from_obj(self):
+        e = self.klass(self.html)
+        assert len(e('div')) == 3
+        assert len(e('div#node2')) == 1
+
+    def test_selector_from_html_from_obj(self):
+        e = self.klass(self.html)
+        assert len(e('div', self.html2)) == 1
+        assert len(e('div#node2', self.html2)) == 0
+
+    def test_class(self):
+        e = self.klass(self.html)
+        assert isinstance(e, self.klass)
+        n = e('div', self.html2)
+        assert isinstance(n, self.klass)
+        assert n._parent is e
+
+def application(environ, start_response):
+    req = Request(environ)
+    response = Response()
+    if req.method == 'GET':
+        response.body = '<pre>Yeah !</pre>'
+    else:
+        response.body = '<a href="/plop">Yeah !</a>'
+    return response(environ, start_response)
+
+def secure_application(environ, start_response):
+    if 'REMOTE_USER' not in environ:
+        return exc.HTTPUnauthorized('vomis')(environ, start_response)
+    return application(environ, start_response)
+
+class TestAjaxSelector(TestSelector):
+    klass = pqa
+
+    def test_proxy(self):
+        e = self.klass([])
+        val = e.get('http://pypi.python.org/pypi/pyquery/')
+        assert len(val('div#breadcrumb')) == 1, (val.response, val)
+
+    def test_get(self):
+        e = self.klass(app=application)
+        val = e.get('/')
+        assert len(val('pre')) == 1, val
+
+    def test_secure_get(self):
+        e = self.klass(app=secure_application)
+        val = e.get('/', environ=dict(REMOTE_USER='gawii'))
+        assert len(val('pre')) == 1, val
+        val = e.get('/', REMOTE_USER='gawii')
+        assert len(val('pre')) == 1, val
+
+    def test_secure_get_not_authorized(self):
+        e = self.klass(app=secure_application)
+        val = e.get('/')
+        assert len(val('pre')) == 0, val
+
+    def test_post(self):
+        e = self.klass(app=application)
+        val = e.post('/')
+        assert len(val('a')) == 1, val
+
+    def test_subquery(self):
+        e = self.klass(app=application)
+        n = e('div')
+        val = n.post('/')
+        assert len(val('a')) == 1, val
+
+if __name__ == '__main__':
+    fails, total = unittest.main()
+    if fails == 0:
+        print 'OK'
