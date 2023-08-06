3f9d9eb5dff768a717dbbd9c99460d1deec5de59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:26 +0000

    Add package zope.app.wsgi/3.5.2/zope.app.wsgi-3.5.2.tar.gz

---
+++ packages/zope.app.wsgi/zope.app.wsgi-3.5.2.tar.gz/zope.app.wsgi-3.5.2/setup.py
@@ -0,0 +1,62 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.wsgi package
+
+$Id: setup.py 98839 2009-04-03 14:01:53Z achapman $
+"""
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.wsgi',
+      version = '3.5.2',
+      url='http://pypi.python.org/pypi/zope.app.wsgi',
+      license='ZPL 2.1',
+      description='WSGI application for the zope.publisher',
+      long_description=\
+          open('README.txt').read() + \
+          '\n\n' + \
+          open('CHANGES.txt').read(),
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      classifiers=['Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Zope Public License',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+                   'Framework :: Zope3',
+                   ],
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=['zope.app.testing',
+                                  'zope.app.securitypolicy',
+                                  'zope.app.zcmlfiles',
+                                  'zope.testbrowser']),
+      install_requires=['setuptools',
+                        'ZConfig',
+                        'zope.app.appsetup',
+                        'zope.app.publication',
+                        'zope.event',
+                        'zope.interface',
+                        'zope.publisher',
+                        'zope.security',
+                        ],
+      entry_points={
+          'paste.app_factory': [
+              'main = zope.app.wsgi.paste:ZopeApplication'
+          ]
+      },
+      include_package_data = True,
+      zip_safe = False,
+      )
