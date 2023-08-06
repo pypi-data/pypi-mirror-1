d031fc6d741a478d23f8a93099005148489817a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:29 +0000

    Add package solrpy/0.8/solrpy-0.8.tar.gz

---
+++ packages/solrpy/solrpy-0.8.tar.gz/solrpy-0.8/setup.py
@@ -0,0 +1,17 @@
+# bootstrap easy_install
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'solrpy',
+    version = '0.8',
+    url = 'http://code.google.com/p/solrpy',
+    license = 'http://opensource.org/licenses/apache2.0.php',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires = [],
+    description = 'client for the solr search service',
+    tests_require = ["nose>=0.10.1"],
+    test_suite = 'nose.collector',
+)
