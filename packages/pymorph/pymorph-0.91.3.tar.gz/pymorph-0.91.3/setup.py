1c5db48303aabb7de731e84efcca0274d33ead85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:15 +0000

    Add package pymorph/0.91.3/pymorph-0.91.3.tar.gz

---
+++ packages/pymorph/pymorph-0.91.3.tar.gz/pymorph-0.91.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+long_description='''Image Morphology Toolbox
+
+The image morphology toolbox implements the basic binary and
+grayscale morphology operations, working with numpy arrays to
+hold image data.
+
+This is a pure Python package which is the companion package
+to the book "Hands-on Morphological Image Processing."
+'''
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Image Recognition',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+
+setup(name='pymorph',
+      version='0.91.3',
+      description='Image Morphology Toolbox',
+      long_description=long_description,
+      author='Luis Pedro Coelho',
+      author_email='lpc@mcu.edu',
+      url='http://luispedro.org/pymorph/',
+      license='BSD',
+      packages=find_packages(),
+      )
+
+
