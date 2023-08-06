37d590547bc0f309868df0afc91c4ef25dd9a1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:32 +0000

    Add package Editra/0.4.85/Editra-0.4.85-py2.6.egg

---
+++ packages/Editra/Editra-0.4.85-py2.6.egg/Editra/src/ebmlib/__init__.py
@@ -0,0 +1,22 @@
+###############################################################################
+# Name: __init__.py                                                           #
+# Purpose: Editra Buisness Model Library                                      #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2009 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
+
+"""
+Editra Buisness Model Library:
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__cvsid__ = "$Id: __init__.py 60249 2009-04-20 01:18:29Z CJP $"
+__revision__ = "$Revision: 60249 $"
+
+#-----------------------------------------------------------------------------#
+
+from searcheng import *
+from fchecker import *
+from fileutil import *
