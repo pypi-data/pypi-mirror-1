bd1030df0a0a048136babea260d2a9405d73fa47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:01 +0000

    Add package sflib/1.0dev-BZR-r35-panta-elasticworld.org-20081217105838-wg2o50uwu3sj5102/sflib-1.0dev-BZR-r35-panta-elasticworld.org-20081217105838-wg2o50uwu3sj5102.tar.gz

---
+++ packages/sflib/sflib-1.0dev-BZR-r35-panta-elasticworld.org-20081217105838-wg2o50uwu3sj5102.tar.gz/sflib-1.0dev-BZR-r35-panta-elasticworld.org-20081217105838-wg2o50uwu3sj5102/sflib/bzrcachedrev.py
@@ -0,0 +1,4 @@
+info    = {'revinfo': (35, 'panta@elasticworld.org-20081217105838-wg2o50uwu3sj5102'), 'basepath': '/arc/bzr/python-sflib.bzr/trunk/', 'revno': 35, 'revname': 'panta@elasticworld.org-20081217105838-wg2o50uwu3sj5102', 'inpath': u'sflib/version.py'}
+revno   = 35
+revname = 'panta@elasticworld.org-20081217105838-wg2o50uwu3sj5102'
+revinfo = (35, 'panta@elasticworld.org-20081217105838-wg2o50uwu3sj5102')
