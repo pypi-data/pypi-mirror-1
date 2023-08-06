7543c3f6b24c3a153da847ca507033455d892b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:47 +0000

    Add package collective.bloggingmaps/1.0a2/collective.bloggingmaps-1.0a2.zip

---
+++ packages/collective.bloggingmaps/collective.bloggingmaps-1.0a2.zip/collective.bloggingmaps-1.0a2/collective/bloggingmaps/extenders.py
@@ -0,0 +1,170 @@
+from zope.interface import implements
+from zope.component import adapts, getMultiAdapter
+
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from archetypes.schemaextender.field import ExtensionField
+from archetypes.markerfield import InterfaceMarkerField
+
+from Products.Archetypes.atapi import StringField
+from Products.Archetypes.atapi import (BooleanWidget, SelectionWidget)
+from Products.Archetypes.utils import DisplayList
+
+
+from Products.ATContentTypes.interface import IATFolder
+from Products.ATContentTypes.interface import IATBTreeFolder
+from Products.ATContentTypes.interface import IATEvent
+from Products.ATContentTypes.interface import IATTopic
+
+from Products.Maps.field import LocationField, LocationWidget
+from Products.Maps.interfaces import IMapEnabled
+
+from collective.blogging import BLOG_PERMISSION
+from collective.bloggingmaps import _
+
+
+class GeoLocationField(ExtensionField, LocationField):
+    """ An map location field """
+
+class MarkerIconField(ExtensionField, StringField):
+    """ A map marker icon field """
+
+    def getDefault(self, content_instance):
+        config = getMultiAdapter((content_instance, content_instance.REQUEST),
+                                    name="maps_configuration")
+        return config.default_location
+
+    @property
+    def enforceVocabulary(self):
+        return True
+
+    def Vocabulary(self, content_instance):
+        config = getMultiAdapter((content_instance, content_instance.REQUEST),
+                                    name="maps_configuration")
+        marker_icons = config.marker_icons
+        result = DisplayList()
+        for icon in marker_icons:
+            result.add(icon['name'], icon['name'])
+        return result
+
+
+class MapExtender(object):
+    """ Add a new map location field to all ATEvent based types. """
+    adapts(IATEvent)
+    implements(ISchemaExtender)
+
+    fields = [
+        GeoLocationField(
+            'geolocation',
+            schemata = "blog",
+            languageIndependent = True,
+            write_permission = BLOG_PERMISSION,
+            required=False,
+            validators=('isGeoLocation',),
+            widget=LocationWidget(
+                label='Location',
+                label_msgid='label_geolocation',
+                description_msgid='help_geolocation',
+                i18n_domain='maps',
+            ),
+        ),
+
+        MarkerIconField(
+            'markerIcon',
+            schemata = "blog",
+            languageIndependent = True,
+            write_permission = BLOG_PERMISSION,
+            widget=SelectionWidget(
+                format="select",
+                label='Marker icon',
+                label_msgid='label_markericon',
+                description_msgid='help_markericon',
+                i18n_domain='maps',
+            ),
+        ),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
+
+
+class FolderExtender(object):
+    """ Add a new marker field to all ATFolder based types. """
+    adapts(IATFolder)
+    implements(ISchemaExtender)
+
+    fields = [
+        InterfaceMarkerField("enable_maps",
+            schemata = "blog",
+            write_permission = BLOG_PERMISSION,
+            languageIndependent = True,
+            interfaces = (IMapEnabled,),
+            widget = BooleanWidget(
+                label = _(u"label_enable_maps",
+                    default=u"Maps enabled"),
+                description = _(u"help_enable_maps",
+                    default=u"Enable entry maps to be available in the blog view."),
+            ),
+        ),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
+
+
+class LargeFolderExtender(object):
+    """ Add a new marker field to all ATBTreeFolder based types. """
+    adapts(IATBTreeFolder)
+    implements(ISchemaExtender)
+
+    fields = [
+        InterfaceMarkerField("enable_maps",
+            schemata = "blog",
+            write_permission = BLOG_PERMISSION,
+            languageIndependent = True,
+            interfaces = (IMapEnabled,),
+            widget = BooleanWidget(
+                label = _(u"label_enable_maps",
+                    default=u"Maps enabled"),
+                description = _(u"help_enable_maps",
+                    default=u"Enable entry maps to be available in the blog view."),
+            ),
+        ),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
+
+class TopicExtender(object):
+    """ Add a new marker field to all ATTopic based types. """
+    adapts(IATTopic)
+    implements(ISchemaExtender)
+
+    fields = [
+        InterfaceMarkerField("enable_maps",
+            schemata = "blog",
+            write_permission = BLOG_PERMISSION,
+            languageIndependent = True,
+            interfaces = (IMapEnabled,),
+            widget = BooleanWidget(
+                label = _(u"label_enable_maps",
+                    default=u"Maps enabled"),
+                description = _(u"help_enable_maps",
+                    default=u"Enable entry maps to be available in the blog view."),
+            ),
+        ),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
