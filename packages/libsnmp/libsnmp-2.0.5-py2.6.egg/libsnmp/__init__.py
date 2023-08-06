c71a2990ed41953d61018316da4065489967dc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:03 +0000

    Add package libsnmp/2.0.5/libsnmp-2.0.5-py2.6.egg

---
+++ packages/libsnmp/libsnmp-2.0.5-py2.6.egg/libsnmp/__init__.py
@@ -0,0 +1,18 @@
+# $Id: __init__.py 43 2006-11-06 22:39:17Z daedalus $
+# $Revision: 43 $
+#
+#    libsnmp - a Python SNMP library
+#    Copyright (c) 2003 Justin Warren <daedalus@eigenmagic.com>
+
+__all__ = [
+    'asynrole',
+    'debug',
+    'util',
+    'rfc1155',
+    'rfc1157',
+    'rfc1902',
+    'rfc1905',
+    'v1',
+    'v2',
+    ]
+
