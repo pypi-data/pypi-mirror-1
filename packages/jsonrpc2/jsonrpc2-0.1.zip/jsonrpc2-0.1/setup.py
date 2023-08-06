7f4a299ce3820997f97f0d8f33a724630f6d07fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:37 +0000

    Add package jsonrpc2/0.1/jsonrpc2-0.1.zip

---
+++ packages/jsonrpc2/jsonrpc2-0.1.zip/jsonrpc2-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+version="0.1"
+
+setup(
+    name="jsonrpc2",
+    description="WSGI Framework for JSON RPC 2.0",
+    long_description="""\
+jsonrpc2 is WSGI Framework for JSON RPC 2.0.
+JSON RPC 2.0 Spec can be seen on http://groups.google.com/group/json-rpc/web/json-rpc-1-2-proposal.
+
+
+""",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+    author='Atsushi Odagiri',
+    author_email='aodagx@gmail.com',
+    keywords='wsgi request web http',
+    url='http://bitbucket.org/aodag/microapps/overview/',
+    version=version,
+    install_requires=[
+        "simplejson",
+        "WebOb",
+    ],
+)
+
