ce14c11ff4199c8ba3724147ada641c5759a5201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:42 +0000

    Add package wikipydia/0.1.3/wikipydia-0.1.3.tar.gz

---
+++ packages/wikipydia/wikipydia-0.1.3.tar.gz/wikipydia-0.1.3/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name='wikipydia',
+      version='0.1.3',
+      description='Python Client Library for the Wikipedia API',
+      author='James Dennis',
+      author_email='jd@j2labs.net',
+      url='http://github.com/j2labs/wikipydia',
+      packages=['wikipydia'])
