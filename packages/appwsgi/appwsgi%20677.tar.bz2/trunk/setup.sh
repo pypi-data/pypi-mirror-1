b21310caf30726a5a1d7af49d93def62f96832ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:19 +0000

    Add package appwsgi/677/appwsgi%20677.tar.bz2

---
+++ packages/appwsgi/appwsgi%20677.tar.bz2/trunk/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright(c) gert.cuykens@gmail.com
+
+echo "setup file permission"
+chown -R www-data:www-data .
+chmod -R 700 .
+
+echo "setup database"
+mysql -u root -p < database.sql
+
+exit 0
+
+#echo "remove all .svn"
+#find . -name .svn -print0 | xargs -0 rm -rf
+
+#FILES=(`ls sql/*.sql 2>/dev/null`)
+#for FILE in ${FILES[@]}; do
+#if [ "sql/database.sql" != $FILE ]; then
+#echo $FILE
+#mysql -u root www < $FILE
+#fi
+#done
+
