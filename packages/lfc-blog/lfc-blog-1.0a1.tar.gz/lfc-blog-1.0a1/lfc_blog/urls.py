ea256f4ef2a1b7fef6058ca83992e021d554b39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:33 +0000

    Add package lfc-blog/1.0a1/lfc-blog-1.0a1.tar.gz

---
+++ packages/lfc-blog/lfc-blog-1.0a1.tar.gz/lfc-blog-1.0a1/lfc_blog/urls.py
@@ -0,0 +1,7 @@
+# django imports
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns("lfc_blog.views",
+    url(r'(?P<slug>.+)/tag/(?P<tag>[^/]+)$', "lfc_tagged_object_list", name="lfc_blog_tag_list"),
+    url(r'^(?P<slug>.+)/archive/(?P<month>\d{1,2})/(?P<year>\d{4})$', "archive", name="lfc_archive"),
+)
