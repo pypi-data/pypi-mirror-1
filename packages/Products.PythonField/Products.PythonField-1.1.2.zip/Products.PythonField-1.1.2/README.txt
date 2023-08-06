84674ebc91a6802b208565f3164fc53f4677fd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:04 +0000

    Add package Products.PythonField/1.1.2/Products.PythonField-1.1.2.zip

---
+++ packages/Products.PythonField/Products.PythonField-1.1.2.zip/Products.PythonField-1.1.2/README.txt
@@ -0,0 +1,1 @@
+See ./Products/PythonField/readme.txt
---
+++ packages/Products.PythonField/Products.PythonField-1.1.2.zip/Products.PythonField-1.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join("Products", "PythonField", "version.txt")).read().strip()
+
+setup(name='Products.PythonField',
+      version=version,
+      description="",
+      long_description= \
+        open(os.path.join("Products", "PythonField", "README.txt")).read() + \
+        '\n' + \
+        open('INSTALL.txt').read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+      ],
+      keywords='Plone ScriptableFields PythonField',
+      author='Jens Klein',
+      author_email='jens@bluedynamics.com',
+      url='http://plone.org/products/scriptablefields',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
