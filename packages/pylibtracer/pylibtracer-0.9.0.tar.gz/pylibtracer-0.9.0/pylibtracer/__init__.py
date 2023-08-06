c55f6f18078520014c1fe81ee56fae6e3c4f120b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:40 +0000

    Add package pylibtracer/0.9.0/pylibtracer-0.9.0.tar.gz

---
+++ packages/pylibtracer/pylibtracer-0.9.0.tar.gz/pylibtracer-0.9.0/pylibtracer/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Olli Wang (olliwang@ollix.com) on 2009-11-19.
+Copyright (c) 2009 Ollix. All rights reserved.
+"""
+
+VERSION = "0.9.0"
---
+++ packages/pylibtracer/pylibtracer-0.9.0.tar.gz/pylibtracer-0.9.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+setup.py
+
+Created by Olli Wang (olliwang@ollix.com) on 2009-11-19.
+Copyright (c) 2009 Ollix. All rights reserved.
+"""
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import pylibtracer
+
+
+setup(name='pylibtracer',
+      version=pylibtracer.VERSION,
+      description='pylibtracer is a Python library used to find Python ' \
+                  'modules you are using in a project.',
+      license="MIT",
+      author='Olli Wang',
+      author_email='olliwang@ollix.com',
+      url='http://opensource.ollix.com/pylibtracer',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      extras_require = {},
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      classifiers=[
+          'Intended Audience :: Developers',
+          "License :: OSI Approved",
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Utilities',
+      ],
+)
