83b2f3058b7d507a32976da695a42710fa396002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:52 +0000

    Add package tha.sdistmaker/1.0/tha.sdistmaker-1.0.tar.gz

---
+++ packages/tha.sdistmaker/tha.sdistmaker-1.0.tar.gz/tha.sdistmaker-1.0/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os.path
+
+version = '1.0'
+
+long_description = '\n\n'.join([
+    open('README.txt').read(),
+    open(os.path.join('src', 'tha', 'sdistmaker', 'USAGE.txt')).read(),
+    open('TODO.txt').read(),
+    open('CHANGES.txt').read(),
+    ])
+
+tests_require = [
+    'z3c.testsetup>=0.3',
+    'zope.testing',
+    ]
+
+setup(name='tha.sdistmaker',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
+      keywords=[],
+      author='The Health Agency',
+      author_email='techniek@thehealthagency.com',
+      url='http://www.thehealthagency.com',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['tha'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'tha.tagfinder',
+                        ],
+      extras_require = {'test': tests_require},
+      tests_require=tests_require,
+      entry_points={
+          'console_scripts': [
+              'make_sdist = tha.sdistmaker.maker:main',
+              'sdists_from_tags = tha.sdistmaker.iterator:main',
+          ]},
+      )
