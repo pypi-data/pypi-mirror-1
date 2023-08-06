0d5086e5390f1334fc3ba0f8ecd408e95b548cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:28 +0000

    Add package osc.recipe.sysegg/0.1/osc.recipe.sysegg-0.1.tar.gz

---
+++ packages/osc.recipe.sysegg/osc.recipe.sysegg-0.1.tar.gz/osc.recipe.sysegg-0.1/osc/recipe/sysegg/__init__.py
@@ -0,0 +1,30 @@
+import os
+import pkg_resources
+
+class Recipe:
+    def __init__(self, buildout, name, options):
+        self.buildout = buildout
+        self.name = name
+        self.options = options
+        options.setdefault('eggs', '')
+
+    def install(self):
+        eggs = self.options['eggs']
+        eggs = eggs.split('\n')
+        
+        dev_egg_dir = self.buildout['buildout']['develop-eggs-directory']
+        
+        for egg in eggs:
+            try:
+                dist = pkg_resources.require(egg)[0]
+                egg_egg_link = os.path.join(dev_egg_dir,
+                                                '%s.egg-link' % dist.project_name)
+                f = open(egg_egg_link, 'w')
+                f.write(dist.location)
+                f.close()
+                print 'Using %s for %s' % (dist.location, egg)
+            except pkg_resources.DistributionNotFound:
+                print 'No system distribution for %s found.' % egg
+        return ()
+
+    update = install
