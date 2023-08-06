d4412ed06a30c9ddfdc2b3103f99d84c2cb74a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:59 +0000

    Add package pisa/3.0.15/pisa-3.0.15-py2.4.egg

---
+++ packages/pisa/pisa-3.0.15-py2.4.egg/sx/pisa3/pisa_version.py
@@ -0,0 +1,16 @@
+# -*- coding: ISO-8859-1 -*-
+#############################################
+## (C)opyright by Dirk Holtwick, 2002-2008 ##
+## All rights reserved                     ##
+#############################################
+
+__reversion__ = "$Revision: 171 $"
+__author__    = "$Author: holtwick $"
+__date__      = "$Date: 2008-03-13 18:35:28 +0100 (Do, 13 Mrz 2008) $"
+__version__   = VERSION = "VERSION{3.0.15}VERSION"[8:-8]
+__build__     = BUILD   = "BUILD{2008-03-13 18:29}BUILD"[6:-6]
+
+VERSION_STR = "pisa %s (Build %s)\n(c) Dirk Holtwick <dirk.holtwick@gmail.com>, Germany\nWebsite http:www.htmltopdf.org" % (
+    VERSION,
+    BUILD,
+    )
