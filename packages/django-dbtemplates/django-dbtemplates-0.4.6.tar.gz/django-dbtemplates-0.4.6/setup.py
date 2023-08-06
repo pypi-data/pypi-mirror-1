23bd592ef9d7d5bcc10ca199c8ab3cf28a3114eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package django-dbtemplates/0.4.6/django-dbtemplates-0.4.6.tar.gz

---
+++ packages/django-dbtemplates/django-dbtemplates-0.4.6.tar.gz/django-dbtemplates-0.4.6/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='django-dbtemplates',
+    version='0.4.6',
+    description='Template loader for database stored templates',
+    long_description=open('README.rst').read(),
+    author='Jannis Leidel',
+    author_email='jannis@leidel.info',
+    url='http://code.google.com/p/django-databasetemplateloader/',
+    scripts=['dbtemplates/sync_templates.py',],
+    packages=[
+        'dbtemplates',
+        'dbtemplates.management',
+        'dbtemplates.management.commands'
+    ],
+    package_dir={'dbtemplates': 'dbtemplates'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ]
+)
