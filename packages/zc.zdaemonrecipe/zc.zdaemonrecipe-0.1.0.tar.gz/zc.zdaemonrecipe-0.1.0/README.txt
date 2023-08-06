cad3b2ba8f58f44b175e0e6d5889e83108b53fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package zc.zdaemonrecipe/0.1.0/zc.zdaemonrecipe-0.1.0.tar.gz

---
+++ packages/zc.zdaemonrecipe/zc.zdaemonrecipe-0.1.0.tar.gz/zc.zdaemonrecipe-0.1.0/README.txt
@@ -0,0 +1,16 @@
+**************
+zdaemon recipe
+**************
+
+The zdaemon recipe provides support for generating zdaemon-based run
+scripts.
+
+.. contents::
+
+Releases
+********
+
+0.1 (2008-05-01)
+================
+
+Initial release.
---
+++ packages/zc.zdaemonrecipe/zc.zdaemonrecipe-0.1.0.tar.gz/zc.zdaemonrecipe-0.1.0/setup.py
@@ -0,0 +1,59 @@
+##############################################################################
+#
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+name = 'zc.zdaemonrecipe'
+version = '0.1.0'
+
+import os
+from setuptools import setup, find_packages
+
+entry_points = '''
+[zc.buildout]
+default=zc.zdaemonrecipe:Recipe
+'''
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('zc', 'zdaemonrecipe', 'README.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        )
+
+open('doc.txt', 'w').write(long_description)
+
+setup(
+    name = name,
+    version = version,
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    description = 'ZC Buildout recipe for zdaemon scripts',
+    long_description=long_description,
+    license = 'ZPL 2.1',
+    
+    entry_points=entry_points,
+    packages = find_packages('.'),
+    namespace_packages = ['zc'],
+    extras_require = dict(test=['zdaemon']),
+    install_requires = ['setuptools',
+                        'zc.buildout', 'zc.recipe.egg',
+                        'ZConfig'],
+    zip_safe = False,
+    )
