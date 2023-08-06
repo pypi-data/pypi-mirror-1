93846d5bfc54a1b3d5387b10e78ed25c30b80f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:20 +0000

    Add package zope.dublincore/3.4.3/zope.dublincore-3.4.3.tar.gz

---
+++ packages/zope.dublincore/zope.dublincore-3.4.3.tar.gz/zope.dublincore-3.4.3/setup.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.dublincore package
+
+$Id: setup.py 101325 2009-06-29 15:57:45Z aaron $
+"""
+from setuptools import setup, find_packages
+
+long_description = (open('README.txt').read() +
+                    '\n\n' +
+                    open('CHANGES.txt').read())
+
+setup(
+    name="zope.dublincore",
+    version = '3.4.3',
+    url='http://pypi.python.org/pypi/zope.dublincore',
+    license='ZPL 2.1',
+    description='Zope Dublin Core implementation',
+    long_description=long_description,
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['zope'],
+    include_package_data=True,
+    extras_require=dict(
+        test=[
+            'zope.app.component',
+            'zope.testing',
+            'zope.security',
+            'zope.app.testing']
+        ),
+    install_requires = ['setuptools',
+                        'zope.annotation',
+                        'zope.component',
+                        'zope.datetime',
+                        'zope.interface',
+                        ],
+    zip_safe = False
+    )
