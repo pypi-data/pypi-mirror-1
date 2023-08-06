e967e1efbb886a83a7a28e57bdb12016fab5329b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package json_schema/0.3/json_schema-0.3.tar.gz

---
+++ packages/json_schema/json_schema-0.3.tar.gz/json_schema-0.3/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="json_schema", version="0.3",
+    url="http://lericson.blogg.se/code/category/json_schema.html",
+    author="Ludvig Ericson", author_email="ludvig@blogg.se",
+    description="JSON schema definition and validation library",
+    packages=["json_schema"])
