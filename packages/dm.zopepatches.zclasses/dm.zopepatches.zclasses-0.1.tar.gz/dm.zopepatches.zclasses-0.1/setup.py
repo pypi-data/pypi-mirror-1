5aaa7313786e8e9108e856da5ff70c0fcb3f7132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:14 +0000

    Add package dm.zopepatches.zclasses/0.1/dm.zopepatches.zclasses-0.1.tar.gz

---
+++ packages/dm.zopepatches.zclasses/dm.zopepatches.zclasses-0.1.tar.gz/dm.zopepatches.zclasses-0.1/setup.py
@@ -0,0 +1,47 @@
+from os.path import abspath, dirname, join
+try:
+  # try to use setuptools
+  from setuptools import setup
+  setupArgs = dict(
+      include_package_data=True,
+      namespace_packages=['dm', 'dm.zopepatches'],
+      zip_safe=False,
+      entry_points = dict(
+        ),
+      )
+except ImportError:
+  # use distutils
+  from distutils import setup
+  setupArgs = dict(
+    )
+
+cd = abspath(dirname(__file__))
+pd = join(cd, 'dm', 'zopepatches', 'zclasses')
+
+def pread(filename, base=pd): return open(join(base, filename)).read().rstrip()
+
+setup(name='dm.zopepatches.zclasses',
+      version=pread('VERSION.txt').split('\n')[0],
+      description='Through the Web application development without deep programming skills (former ZClasses functionality in the Zope core).',
+      long_description=pread('README.txt'),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+#       'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Framework :: Zope2',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        ],
+      author='Dieter Maurer',
+      author_email='dieter@handshake.de',
+      url='http://pypi.python.org/dm.zopepatches.zclasses',
+      packages=['dm', 'dm.zopepatches', 'dm.zopepatches.zclasses'],
+      keywords='application development menu web',
+      license='BSD',
+      **setupArgs
+      )
+
+
+
