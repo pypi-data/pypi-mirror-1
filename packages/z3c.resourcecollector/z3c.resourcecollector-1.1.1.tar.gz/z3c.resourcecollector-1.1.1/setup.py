09adcabe86aa597e3d74e36174464da8529acb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package z3c.resourcecollector/1.1.1/z3c.resourcecollector-1.1.1.tar.gz

---
+++ packages/z3c.resourcecollector/z3c.resourcecollector-1.1.1.tar.gz/z3c.resourcecollector-1.1.1/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='z3c.resourcecollector',
+      version='1.1.1',
+      url='http://launchpad.net/z3c.resourcecollector',
+      license='ZPL',
+      description='collects multiple resources and combines them to one',
+      author='Zoltan Szabo',
+      author_email='zoltan.szabo@informmedia.ro',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['z3c'],
+      extras_require=dict(test=['zope.app.testing',
+                                'zope.testing',
+                                'z3c.testing',
+                                'zope.testbrowser'
+                                ]),
+      install_requires=['setuptools',
+                        'zope.configuration',
+                        'zope.interface',
+                        'zope.component',
+                        'zope.app.component',
+                        'zope.app.appsetup',
+                        'zope.app.wsgi',
+                        'zope.cachedescriptors',
+                        'zope.publisher',
+                        'zope.schema',
+                        'zope.security',
+                        'zope.viewlet',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
