3bd4d14407580f2233ea33d0fc9812abf2f82a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:07 +0000

    Add package clepy/0.3.7/clepy-0.3.7.tar.gz

---
+++ packages/clepy/clepy-0.3.7.tar.gz/clepy-0.3.7/tests/test_mattwilson.py
@@ -0,0 +1,28 @@
+# vim: set expandtab ts=4 sw=4 filetype=python:
+
+import tempfile, unittest
+
+from mock import Mock, patch
+
+import clepy.mattwilson as wmw
+
+class TestMattWilson(unittest.TestCase):
+
+    def test_draw_ascii_spinner(self):
+        wmw.draw_ascii_spinner(0.001)
+
+m, t = Mock(), Mock()
+
+@patch('subprocess.call', m)
+@patch('tempfile.NamedTemporaryFile', t)
+def test_edit_with_editor_1():
+
+    """
+    Verify we can load a blank editor.
+    """
+
+    wmw.edit_with_editor()
+    assert m.called
+    assert t.called
+
+__id__ = "$Id: test_mattwilson.py 20 2009-08-16 02:39:46Z mw44118 $"
