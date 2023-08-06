2710b55419422ab5e12639c42f01d223c6bed3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:43 +0000

    Add package ClueReleaseManager.paste/1.0dev/ClueReleaseManager.paste-1.0dev.tar.gz

---
+++ packages/ClueReleaseManager.paste/ClueReleaseManager.paste-1.0dev.tar.gz/ClueReleaseManager.paste-1.0dev/src/ClueReleaseManager/paste/paste/tests/test_crm.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+__docformat__ = 'restructuredtext'
+
+import os
+import sys
+import unittest, doctest
+from unittest import TestCase
+
+import paste.fixture
+from paste.deploy import loadapp
+
+here_dir = os.path.dirname(os.path.abspath(__file__))
+conf_dir = os.path.dirname(os.path.dirname(here_dir))
+
+test_file = os.path.join(here_dir, 'development.ini')
+
+current_dir = os.path.dirname(__file__)
+class TestController(TestCase):
+
+    def __init__(self, *args, **kwargs):
+
+        TestCase.__init__(self, *args, **kwargs)
+
+def doc_suite(test_dir, setUp=None, tearDown=None, globs=None):
+    """Returns a test suite, based on doctests found in /doctest."""
+    suite = []
+    if globs is None:
+        globs = globals()
+    globs['wsgiapp'] = loadapp('config:development.ini', relative_to = here_dir)
+    globs['app'] = paste.fixture.TestApp(globs['wsgiapp'])
+
+    flags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE |
+             doctest.REPORT_ONLY_FIRST_FAILURE)
+
+    package_dir = os.path.split(test_dir)[0]
+    if package_dir not in sys.path:
+        sys.path.append(package_dir)
+
+    doctest_dir = os.path.join(package_dir, 'doctests')
+
+    # filtering files on extension
+    docs = [os.path.join(doctest_dir, doc) for doc in
+            os.listdir(doctest_dir) if doc.endswith('.txt')]
+
+    for test in docs:
+        suite.append(doctest.DocFileSuite(test, optionflags=flags,
+                                          globs=globs, setUp=setUp,
+                                          tearDown=tearDown,
+                                          module_relative=False))
+
+    return unittest.TestSuite(suite)
+
+def test_suite():
+    """returns the test suite"""
+    return doc_suite(current_dir)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
+
