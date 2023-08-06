cb6b29b5dd0a0dfdc377148eb396ce1a344d52b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:06 +0000

    Add package django-jqueryautocomplete/0.3/django-jqueryautocomplete-0.3.tar.gz

---
+++ packages/django-jqueryautocomplete/django-jqueryautocomplete-0.3.tar.gz/django-jqueryautocomplete-0.3/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-jqueryautocomplete',
+    version='0.3',
+    description='Very basic and un-tested proof of concept jQuery auto-complete app based off of Corey Hart\'s jQuery plugin.',
+    author='Oscar Duignan',
+    author_email='oscarduignan@gmail.com',
+    url='http://github.com/oscarduignan/django-jqueryautocomplete/',
+    packages=['jqueryautocomplete'],
+    license='BSD'
+)
+
