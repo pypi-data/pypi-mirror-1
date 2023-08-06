32ffc4ef8e46e1213adfb5744561f74825e22d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:42 +0000

    Add package pyrant/0.2.0/pyrant-0.2.0.tar.gz

---
+++ packages/pyrant/pyrant-0.2.0.tar.gz/pyrant-0.2.0/pyrant/utils.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from protocol import DB_TABLE, TABLE_COLUMN_SEP
+
+
+def from_python(value):
+    """
+    Returns value prepared for storage. This is required for search because
+    some Python types cannot be converted to string and back without changes
+    in semantics, e.g. True-->"True"-->True and False-->"False"-->True.
+
+    Note that we don't convert the value to bytes here, it's done by
+    pyrant.protocol._pack.
+    """
+    if isinstance(value, bool):
+        return 1 if value else ''
+    return value
+
+def to_python(elem, dbtype, sep=None):
+    """
+    Returns pythonic representation of a database record.
+    """
+    if dbtype == DB_TABLE:
+        # Split element by \x00 which is the column separator
+        elems = elem.split(TABLE_COLUMN_SEP)
+        if elems[0]:
+            return dict((elems[i], elems[i+1]) for i in xrange(0, len(elems), 2))
+        else:
+            return
+
+    if sep and sep in elem:
+        return elem.split(sep)
+
+    return elem
+
+def csv_to_dict(lines):
+    return dict(line.split('\t', 1) for line in lines.splitlines() if line)
