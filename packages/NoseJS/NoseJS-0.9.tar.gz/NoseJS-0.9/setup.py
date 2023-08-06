e662613335e2463958e112868c4f813ca5e3cee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:05 +0000

    Add package NoseJS/0.9/NoseJS-0.9.tar.gz

---
+++ packages/NoseJS/NoseJS-0.9.tar.gz/NoseJS-0.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import re
+version = None
+for line in open("./nosejs/__init__.py"):
+    m = re.search("__version__\s+=\s+(.*)", line)
+    if m:
+        version = m.group(1).strip()[1:-1] # quotes
+        break
+assert version
+
+setup(
+    name='NoseJS',
+    version=version,
+    description="A Nose plugin to run JavaScript tests using Rhino in a Java subprocess.",
+    long_description=open("README.txt",'r').read(),
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=['nose'],
+    entry_points="""
+    [nose.plugins.0.10]
+    nosejs = nosejs:NoseJS
+    """
+) 
