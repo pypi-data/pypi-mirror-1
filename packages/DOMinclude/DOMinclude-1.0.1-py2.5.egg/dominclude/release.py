da47e7e14a2f6aab5bd3bee3612a0a19678e582a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:32 +0000

    Add package DOMinclude/1.0.1/DOMinclude-1.0.1-py2.5.egg

---
+++ packages/DOMinclude/DOMinclude-1.0.1-py2.5.egg/dominclude/release.py
@@ -0,0 +1,26 @@
+# Release information about DOMinclude
+"""This is a TurboGears_ widget project that wraps the DOMinclude_ JavaScript
+library for DOM-based popups.
+
+You can view the widgets in the TurboGears Toolbox_.
+
+For further information, visit the `DOMinclude widget homepage`_.
+
+.. _turbogears: http://www.turbogears.org
+.. _dominclude: http://onlinetools.org/tools/dominclude/
+.. _toolbox: http://docs.turbogears.org/1.0/Toolbox
+.. _dominclude widget homepage: http://docs.turbogears.org/tgDOMinclude
+"""
+
+version = "1.0.1"
+
+description = "TurboGears widget that wraps the DOMinclude library for DOM-based popups."
+long_description = __doc__
+author = "Christian Heilmann, Kevin Dangoor"
+email = "dangoor+dominclude@gmail.com"
+copyright = "Copyright 2006 - 2008"
+
+# if it's open source, you might want to specify these
+url = "http://docs.turbogears.org/tgDOMinclude"
+download_url = "http://pypi.python.org/pypi/DOMinclude/"
+license = "MIT"
