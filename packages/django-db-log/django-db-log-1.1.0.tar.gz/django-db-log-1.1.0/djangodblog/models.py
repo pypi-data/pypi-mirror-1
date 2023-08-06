72726d5dd3adfb5d0a42910d0b00b8e49e3e8627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:11 +0000

    Add package django-db-log/1.1.0/django-db-log-1.1.0.tar.gz

---
+++ packages/django-db-log/django-db-log-1.1.0.tar.gz/django-db-log-1.1.0/djangodblog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+try:
+    from idmapper.models import SharedMemoryModel as Model
+except ImportError:
+    Model = models.Model
+
+import datetime
+
+__all__ = ('Error', 'ErrorBatch')
+
+class ErrorBatch(Model):
+    class_name      = models.CharField(_('Type'), max_length=128)
+    message         = models.TextField()
+    traceback       = models.TextField()
+    is_resolved     = models.BooleanField(default=False)
+    times_seen      = models.PositiveIntegerField(default=1)
+    last_seen       = models.DateTimeField(default=datetime.datetime.now)
+    first_seen      = models.DateTimeField(default=datetime.datetime.now)
+    url             = models.URLField(verify_exists=False, null=True, blank=True)
+    server_name     = models.CharField(max_length=128, db_index=True)
+    checksum        = models.CharField(max_length=32, db_index=True)
+
+    class Meta:
+        unique_together = (('class_name', 'server_name', 'checksum'),)
+        verbose_name_plural = 'Error batches'
+
+class Error(Model):
+    class_name      = models.CharField(_('type'), max_length=128)
+    message         = models.TextField()
+    traceback       = models.TextField()
+    datetime        = models.DateTimeField(default=datetime.datetime.now)
+    url             = models.URLField(verify_exists=False, null=True, blank=True)
+    server_name     = models.CharField(max_length=128, db_index=True)
