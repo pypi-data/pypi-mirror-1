23f7f6fee62006d44c122d8d61c95c33697cfe8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:58 +0000

    Add package pyobjc-framework-AddressBook/2.2b1/pyobjc-framework-AddressBook-2.2b1.tar.gz

---
+++ packages/pyobjc-framework-AddressBook/pyobjc-framework-AddressBook-2.2b1.tar.gz/pyobjc-framework-AddressBook-2.2b1/setup.py
@@ -0,0 +1,43 @@
+''' 
+Wrappers for framework 'AddressBook'. 
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+'''
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, Extension
+
+try:
+    from PyObjCMetaData.commands import extra_cmdclass, extra_options
+except ImportError:
+    extra_cmdclass = {}
+    extra_options = lambda name: {}
+
+setup(
+    name='pyobjc-framework-AddressBook',
+    version="2.2b1",
+    description = "Wrappers for the framework AddressBook on Mac OS X",
+    long_description = __doc__,
+    author = 'Ronald Oussoren',
+    author_email = 'pyobjc-dev@lists.sourceforge.net',
+    url= 'http://pyobjc.sourceforge.net/',
+    platforms = [ "MacOS X" ],
+    packages = [ "AddressBook" ],
+    package_dir = { '': 'Lib' },
+    install_requires = [ 
+        'pyobjc-core>=2.2b1',
+        'pyobjc-framework-Cocoa>=2.2b1',
+    ],
+    dependency_links = [],
+    package_data = { 
+        '': ['*.bridgesupport'] 
+    },
+    cmdclass = extra_cmdclass,
+    options = extra_options('AddressBook'),
+    zip_safe = True,
+    test_suite='PyObjCTest',
+)
