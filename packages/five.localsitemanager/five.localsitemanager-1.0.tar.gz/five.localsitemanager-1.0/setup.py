42806377e1b268bba2db0528e339bc2afe506013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:41 +0000

    Add package five.localsitemanager/1.0/five.localsitemanager-1.0.tar.gz

---
+++ packages/five.localsitemanager/five.localsitemanager-1.0.tar.gz/five.localsitemanager-1.0/setup.py
@@ -0,0 +1,39 @@
+"""Setup for five.localsitemanager package
+
+$Id$
+"""
+from setuptools import setup, Extension
+
+version = '1.0'
+
+setup(name='five.localsitemanager',
+      version=version,
+      url='http://svn.zope.org/five.localsitemanager',
+      license='ZPL 2.1',
+      description='Local site manager implementation for Zope 2',
+      author='Rocky Burt and Contributors',
+      author_email='z3-five@codespeak.net',
+      long_description=open("README.txt").read() + "\n" + 
+                       open("INSTALL.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          'Environment :: Web Environment',
+          'Framework :: Zope2',
+          'License :: OSI Approved :: Zope Public License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP :: Site Management',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='zope zope2 zope3 five sitemanager',
+
+      packages=['five', 'five.localsitemanager'],
+      package_dir = {'': 'src'},
+      namespace_packages=['five',],
+      include_package_data = True,
+      install_requires=[
+        'setuptools',
+        'zope.component < 3.5dev',
+      ],
+      zip_safe = False,
+      )
