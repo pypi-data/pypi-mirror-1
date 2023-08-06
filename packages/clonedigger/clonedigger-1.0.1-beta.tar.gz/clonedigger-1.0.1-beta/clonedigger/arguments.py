c712af898c00bfe8d30924b77fcb6d0607265f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:58 +0000

    Add package clonedigger/1.0.1-beta/clonedigger-1.0.1-beta.tar.gz

---
+++ packages/clonedigger/clonedigger-1.0.1-beta.tar.gz/clonedigger-1.0.1-beta/clonedigger/arguments.py
@@ -0,0 +1,8 @@
+clustering_threshold = 10
+hashing_depth = 1
+clusterize_using_dcup = False
+report_unifiers = False
+print_time = True
+force = False  
+use_diff = True 
+clusterize_using_hash = False
---
+++ packages/clonedigger/clonedigger-1.0.1-beta.tar.gz/clonedigger-1.0.1-beta/clonedigger/test.py
@@ -0,0 +1,13 @@
+def f():
+    aaa = (1 + 3) + 4
+    ddd = 1 + 3
+    ddd = 2 + 4
+    vv = 2 + 4
+    ddd = 2 + 4
+def f():
+    aaa = (2 + 4) + 4
+    ddd = 1 + 3
+    ddd = 2 + 4
+    ddd = 2 + 4
+    ddd = 2 + 4
+
