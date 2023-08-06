651be59bd8f20f71169be1357f33712704198333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:17 +0000

    Add package plone.app.imaging/1.0b8/plone.app.imaging-1.0b8.zip

---
+++ packages/plone.app.imaging/plone.app.imaging-1.0b8.zip/plone.app.imaging-1.0b8/src/plone/app/imaging/interfaces.py
@@ -0,0 +1,31 @@
+from zope.interface import Interface
+from zope.schema import List, TextLine
+from zope.i18nmessageid import MessageFactory
+from Products.Archetypes.interfaces import IBaseObject as IATBaseObject
+
+_ = MessageFactory('plone.app.imaging')
+
+
+class IImagingSchema(Interface):
+    """ schema for configlet form """
+
+    allowed_sizes = List(title=_(u'Allowed image sizes'),
+        description = _(u'Specify all allowed maximum image dimensions, one per line. '
+                         'The required format is <name> <width>:<height>.'),
+        value_type = TextLine(), default = [], required = False)
+
+
+class IImageScaleHandler(Interface):
+    """ handler for retrieving scaled versions of an image """
+
+    def getScale(instance, scale):
+        """ return scaled and aq-wrapped version for given image data """
+
+    def createScale(instance, scale, width, height, data=None):
+        """ create & return a scaled version of the image as retrieved
+            from the field or optionally given data """
+
+
+class IBaseObject(IATBaseObject):
+    """ marker interface used to be able to avoid having to use
+        `overrides.zcml` to register our version of the traversal adapter """
