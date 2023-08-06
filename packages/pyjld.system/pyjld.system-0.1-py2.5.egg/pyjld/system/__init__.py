06968c33ee4aec74694eb4e269dafd8f6d993e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:24 +0000

    Add package pyjld.system/0.1/pyjld.system-0.1-py2.5.egg

---
+++ packages/pyjld.system/pyjld.system-0.1-py2.5.egg/pyjld/system/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+""" 
+pyjld.system: various system level utilities (e.g. daemon)
+
+This package contains various system level utilities.
+
+@author: Jean-Lou Dupont
+"""
+__author__  = "Jean-Lou Dupont"
+__email     = "python (at) jldupont.com"
+__version__ = "0.1"
+__fileid    = "$Id: __init__.py 21 2009-04-01 20:21:40Z jeanlou.dupont $"
+
+
+__classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: Public Domain',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    ]
+
+__dependencies = ['python_daemon',]
+
---
+++ packages/pyjld.system/pyjld.system-0.1-py2.5.egg/pyjld/system/daemon/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+""" 
+pyjld.system.daemon
+
+"""
+__author__  = "Jean-Lou Dupont"
+__email     = "python (at) jldupont.com"
+__fileid    = "$Id: __init__.py 29 2009-04-02 14:27:41Z jeanlou.dupont $"
+
+from base import *
