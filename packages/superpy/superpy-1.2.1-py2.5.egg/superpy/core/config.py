49c59c5246db6cc7d4dbeffd86b1c2a8840eb551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:52 +0000

    Add package superpy/1.2.1/superpy-1.2.1-py2.5.egg

---
+++ packages/superpy/superpy-1.2.1-py2.5.egg/superpy/core/config.py
@@ -0,0 +1,18 @@
+"""Module containing configuration information for superpy.
+
+This module should define the following configuration variables:
+
+  smtpServer: String name of smtp server to use when sending mail.
+  
+"""
+
+smtpServer = 'FIXME'
+
+# Location that superpy service will use for stdout
+serviceStdoutFile = 'c:/temp/superpy_service_log.txt'
+
+# Location that superpy service will use for stderr
+serviceStderrFile = 'c:/temp/superpy_service_err.txt'
+
+# Location that superpy service will use for its logging
+serviceLogFile = 'c:/temp/superpy_log.txt'
