44f5a736034fb3d011c1e8c777a41e04f3d330fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:48 +0000

    Add package tender_multipass/0.2/tender_multipass-0.2.tar.gz

---
+++ packages/tender_multipass/tender_multipass-0.2.tar.gz/tender_multipass-0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="tender_multipass",
+      version="0.2",
+      description="Utilities to work with Tender's multipass authentication",
+      author="Michael Richardson",
+      author_email="michael@mtrichardson.com",
+      url="http://bitbucket.org/mtrichardson/tender-multipass/",
+      py_modules=["tender_multipass"],
+      install_requires=["M2Crypto"],
+      license="MIT License",
+)
