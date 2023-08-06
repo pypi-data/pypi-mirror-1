ec1a6e998eef583574f818935ba9b420fd5f21e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:55 +0000

    Add package InsideTrac/0.2.6/InsideTrac-0.2.6.tar.gz

---
+++ packages/InsideTrac/InsideTrac-0.2.6.tar.gz/InsideTrac-0.2.6/setup.py
@@ -0,0 +1,30 @@
+# The setup script for the InsideTrac plugin
+
+from setuptools import setup, find_packages
+
+setup(name='InsideTrac',
+      version='0.2.6',
+      packages=find_packages(exclude=['*.tests*']),
+      install_requires=[
+        'feedparser',
+        'trac>=0.11.5',
+        'TracScheduler',
+        'IniAdmin'
+        ],
+      test_suite='nose.collector',
+      setup_requires=[
+        'Nose',
+        'coverage'
+        ],
+      entry_points = """
+        [trac.plugins]
+        insidetrac.api = insidetrac.api
+        insidetrac.model = insidetrac.model
+        """,
+      author = "Michael J. Pedersen",
+      author_email = "m.pedersen@icelus.org",
+      description = "Trac plugin and Greasemonkey scripts to help monitor external projects",
+      package_data = {'insidetrac': ['templates/*']},
+      license = "LGPL",
+      url = "http://www.insidetrac.org/"
+      )
