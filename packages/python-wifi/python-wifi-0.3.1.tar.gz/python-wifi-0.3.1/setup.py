2c744e8df8546d8cd3e36f97e17c82ce428530e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package python-wifi/0.3.1/python-wifi-0.3.1.tar.gz

---
+++ packages/python-wifi/python-wifi-0.3.1.tar.gz/python-wifi-0.3.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: latin1 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "python-wifi",
+    version = "0.3.1",
+    packages = ['pythonwifi'],
+
+    # metadata for upload to PyPI
+    author = "Róman Joost",
+    author_email = "roman@bromeco.de",
+    description = """\
+Python-Wifi is a Python library that provides access to information
+about a W-Lan card's capabilities, like the wireless extensions
+written in C.""",
+    long_description = open('README').read() +
+        '\n\n' +
+        open('NEWS').read(),
+    license = "LGPL",
+    keywords = "wifi wireless wlan iwconfig iwtools",
+    url = "https://developer.berlios.de/projects/pythonwifi/",
+    download_url = "https://developer.berlios.de/projects/pythonwifi/", 
+    classifiers = [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: System :: Networking',
+    ],
+)
