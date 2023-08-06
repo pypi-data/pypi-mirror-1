363e6d261fd3cc6013fd27166c17cc2789184840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:59 +0000

    Add package z3c.baseregistry/1.2.0/z3c.baseregistry-1.2.0.tar.gz

---
+++ packages/z3c.baseregistry/z3c.baseregistry-1.2.0.tar.gz/z3c.baseregistry-1.2.0/src/z3c/baseregistry/tests/testdoc.py
@@ -0,0 +1,40 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Base Components test setup
+
+$Id: testdoc.py 107148 2009-12-27 09:03:32Z fafhrd $
+"""
+__docformat__ = "reStructuredText"
+import doctest
+import unittest
+from zope.app.testing import placelesssetup, setup
+
+def setUp(test):
+    placelesssetup.setUp(test)
+    setup.setUpTestAsModule(test, name='README')
+
+def tearDown(test):
+    placelesssetup.tearDown(test)
+
+def test_suite():
+    return unittest.TestSuite((
+            doctest.DocFileSuite(
+                '../README.txt',
+                setUp=setUp, tearDown=tearDown,
+                optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+                ),
+            ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
