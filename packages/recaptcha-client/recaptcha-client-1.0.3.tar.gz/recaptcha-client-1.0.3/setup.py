e35008736a6d7faf8834f2dc229673a0c7bc105d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:18 +0000

    Add package recaptcha-client/1.0.3/recaptcha-client-1.0.3.tar.gz

---
+++ packages/recaptcha-client/recaptcha-client-1.0.3.tar.gz/recaptcha-client-1.0.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import ez_setup
+ez_setup.use_setuptools(version='0.6c3')
+
+from setuptools import setup, find_packages
+
+setup(name='recaptcha-client',
+      version='1.0.3',
+      url = "http://recaptcha.net",
+      author = "Ben Maurer",
+      author_email = "support@recaptcha.net",
+      description = "A plugin for reCAPTCHA and reCAPTCHA Mailhide",
+      long_description = """\
+Provides a CAPTCHA for Python using the reCAPTCHA service. Does not require
+any imaging libraries because the CAPTCHA is served directly from reCAPTCHA.
+Also allows you to securely obfuscate emails with Mailhide. This functionality
+requires pycrypto. This library requires two types of API keys. If you'd like
+to use the CAPTCHA, you'll need a key from http://recaptcha.net/api/getkey.
+For Mailhide, you'll need a key from http://mailhide.recaptcha.net/apikey.
+
+The trunk can be checked out from
+http://recaptcha.googlecode.com/svn/trunk/recaptcha-plugins/python,
+and the associated Google Code project lives at
+http://code.google.com/p/recaptcha.
+
+The package maintainer can be contacted at jabronson@gmail.com.
+""",
+
+      license = "MIT/X11",
+      classifiers = [
+        "Topic :: Security",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        ],
+
+
+      packages = find_packages(),
+
+      extras_require = {
+        'mailhide' : ['pycrypto'],
+        },
+      namespace_packages = ['recaptcha'],
+      )
