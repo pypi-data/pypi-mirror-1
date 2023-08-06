a49ec60211ebdf176b88eba1cba0ec8d04796c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:38 +0000

    Add package plib/0.4.4/plib-0.4.4.tar.gz

---
+++ packages/plib/plib-0.4.4.tar.gz/plib-0.4.4/plib/extensions/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+Sub-Package EXTENSIONS of Package PLIB -- Python/C Extensions
+Copyright (C) 2008 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+
+This sub-package contains extensions written using the
+Python/C API.
+"""
+
+from _extensions import *
