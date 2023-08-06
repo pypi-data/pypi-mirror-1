e34c0146c243529c0a16bdcb0d49b60730a82224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:20 +0000

    Add package ClueMapperSecure/0.6.2/ClueMapperSecure-0.6.2-py2.5.egg

---
+++ packages/ClueMapperSecure/ClueMapperSecure-0.6.2-py2.5.egg/clue/secure/tests.py
@@ -0,0 +1,19 @@
+import unittest
+import doctest
+
+
+def test_suite():
+    flags = doctest.ELLIPSIS
+
+    suite = unittest.TestSuite()
+    suite.addTest(doctest.DocTestSuite('clue.secure.auth',
+                                       optionflags=flags))
+    return suite
+
+
+def main():
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite())
+
+if __name__ == '__main__':
+    main()
