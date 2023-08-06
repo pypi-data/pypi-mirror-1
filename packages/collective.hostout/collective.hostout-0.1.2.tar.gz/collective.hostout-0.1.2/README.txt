9c1c57de754d6ed1b86dc21ec38d35917f073506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:36 +0000

    Add package collective.hostout/0.1.2/collective.hostout-0.1.2.tar.gz

---
+++ packages/collective.hostout/collective.hostout-0.1.2.tar.gz/collective.hostout-0.1.2/README.txt
@@ -0,0 +1,10 @@
+
+**************************************************************
+Hostout:  one click deployment for buildout based applications
+**************************************************************
+collective.hostout is a zc.buildout recipe used to create a script for deploying your buildout.
+You can use it to create a hosting environment for your local buildout and then repeatedly
+deploy your buildout and custom code to that host.
+
+.. contents::
+
---
+++ packages/collective.hostout/collective.hostout-0.1.2.tar.gz/collective.hostout-0.1.2/setup.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+name = "collective.hostout"
+setup(
+    name = name,
+    version = "0.1.2",
+    author = "Dylan Jay",
+    author_email = "software@pretaweb.com",
+    description = "ZC Buildout recipe for deploying your buildout to a server",
+    license = "GPL",
+    keywords = "buildout, deploy, deployment, server, plone, django",
+    url='http://www.python.org/pypi/'+name,
+    long_description=(
+        read('README.txt')
+ #       + '\n' +
+ #       read('CHANGES.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('collective', 'recipe', 'hostout', 'README.txt')
+        + '\n'
+        ),
+
+    packages = find_packages(),
+    include_package_data = True,
+#    data_files = [('.', ['*.txt'])],
+#    package_data = {'':('*.txt')},
+    namespace_packages = ['collective', 'collective.recipe'],
+    install_requires = ['zc.buildout', 'setuptools', 'Fabric<0.1.0','functools'],
+    entry_points = {'zc.buildout':
+                    ['default = collective.hostout:Recipe']},
+    zip_safe = True,
+    )
