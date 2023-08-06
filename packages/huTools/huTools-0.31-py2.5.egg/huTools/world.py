7d3280ebd4be5d7c639fdaf1a724972447ad967c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:44 +0000

    Add package huTools/0.31/huTools-0.31-py2.5.egg

---
+++ packages/huTools/huTools-0.31-py2.5.egg/huTools/world.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+world.py - countries of the world (to be procise: countries we do buiseness with)
+
+Created by Maximillian Dornseif on 2007-05-01.
+Copyright (c) 2007 HUDORA GmbH. BSD Licensed.
+"""
+
+__revision__ = "$Revision: 3248 $"
+
+COUNTRY_CHOICES = [('DE', 'Deutschland')] \
+                   + sorted([
+                   ('AT', 'Östereich'),
+                   ('CH', 'Schweiz'),
+                   ('BE', 'Belgien'),
+                   ('FR', 'Frankreich'),
+                   ('ES', 'Spanien'),
+                   ('NL', 'Niederlande'),
+                   ('IT', 'Italien'),
+                   ('GB', 'Grossbritannien'),
+                   ('LI', 'Lichtenstein'),
+                   ('LU', 'Luxenburg'),
+                   ('PT', 'Portugal'),
+                   ('CZ', 'Tschechien'),
+                   ('SE', 'Schweden'),
+                   ('GR', 'Griechenland'),
+                   ('DK', 'Daenemark'),
+                   ('US', 'USA'),
+                   ('AR', 'Argentinien'),
+                   ('HU', 'Ungarn'),
+                   ('LV', 'Lettland'),
+                   ('PL', 'Polen'),
+                   ('LT', 'Littauen'),
+                   ('SK', 'Slowakei'),
+                   ('HR', 'Kroatioen'),
+                   ('IE', 'Irland'),
+                   ('SI', 'Slowenien'),
+                   ('EE', 'Estland'),
+                  ])
