919bd18f0238473a82ebb2c804666787ceb256f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:25 +0000

    Add package z3c.testsetup/0.2.3/z3c.testsetup-0.2.3.tar.gz

---
+++ packages/z3c.testsetup/z3c.testsetup-0.2.3.tar.gz/z3c.testsetup-0.2.3/src/z3c/testsetup/doctesting.py
@@ -0,0 +1,97 @@
+##############################################################################
+#
+# Copyright (c) 2007-2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Test setup helpers for doctests.
+"""
+import unittest
+import os.path
+from zope.testing import doctest, cleanup
+from z3c.testsetup.base import BasicTestSetup
+from z3c.testsetup.util import get_package
+
+class DocTestSetup(BasicTestSetup):
+    """A test setup for doctests."""
+
+    globs = {}
+
+    optionflags = (doctest.ELLIPSIS+
+                   doctest.NORMALIZE_WHITESPACE+
+                   doctest.REPORT_NDIFF)
+
+    encoding = 'utf-8'
+
+    def __init__(self, package, setup=None, teardown=None, globs=None,
+                 optionflags=None, encoding=None, **kw):
+        BasicTestSetup.__init__(self, package, **kw)
+        self.setUp = setup or self.setUp
+        self.tearDown = teardown or self.tearDown
+        self.encoding = encoding or self.encoding
+        if globs is not None:
+            self.globs = globs
+        if optionflags is not None:
+            self.optionflags = optionflags
+        
+
+class UnitDocTestSetup(DocTestSetup):
+    """A unit test setup for packages.
+
+    A collection of methods to search for appropriate doctest files in
+    a given package. ``UnitTestSetup`` is also able to 'register' the
+    tests found and to deliver them as a ready-to-use
+    ``unittest.TestSuite`` instance.
+
+    While the functionality to search for testfiles is mostly
+    inherited from the base class, the focus here is to setup the
+    tests correctly.
+
+    See file `unittestsetup.py` in the tests/testsetup directory to
+    learn more about ``UnitTestSetup``.
+    """
+
+    optionflags = (doctest.ELLIPSIS+
+                   doctest.NORMALIZE_WHITESPACE+
+                   doctest.REPORT_NDIFF)
+
+    regexp_list = [
+        '^\s*:(T|t)est-(L|l)ayer:\s*(unit)\s*',
+        ]
+
+    globs = dict()
+
+    def setUp(self, test):
+        pass
+
+    def tearDown(self, test):
+        cleanup.cleanUp()
+
+    def getTestSuite(self):
+        docfiles = self.getDocTestFiles(package=self.package)
+        suite = unittest.TestSuite()
+        for name in docfiles:
+            if os.path.isabs(name):
+                # We get absolute pathnames, but we need relative ones...
+                common_prefix = os.path.commonprefix([self.package.__file__,
+                                                      name])
+                name = name[len(common_prefix):]
+            suite.addTest(
+                doctest.DocFileSuite(
+                name,
+                package=self.package,
+                setUp=self.setUp,
+                tearDown=self.tearDown,
+                globs=self.globs,
+                optionflags=self.optionflags,
+                **self.additional_options
+                ))
+        return suite
+
---
+++ packages/z3c.testsetup/z3c.testsetup-0.2.3.tar.gz/z3c.testsetup-0.2.3/src/z3c/testsetup/tests/test_testsetup.py
@@ -0,0 +1,153 @@
+import os
+import sys
+import gc
+import re
+import unittest
+from zope.testing import doctest, cleanup, renormalizing
+import zope.component.eventtesting
+from z3c.testsetup.util import get_package
+
+TESTFILES = ['basicsetup.txt',
+             os.path.join('functional', 'functionaldoctestsetup.txt'),
+             'pythontestsetup.txt', 'unitdoctestsetup.txt', 'util.txt',
+             'unittestsetup.txt']
+
+def pnorm(path):
+    """Normalization of paths to use forward slashes. This is needed
+    to make sure the tests work on windows.
+    """
+    return path.replace(os.sep, '/')
+
+def get_testcases_from_suite(suite):
+    result=[]
+    for elem in list(suite):
+        if isinstance(elem, unittest.TestCase):
+            result.append(elem)
+        if isinstance(elem, unittest.TestSuite):
+            result.extend(
+                get_testcases_from_suite(elem))
+    return result
+
+
+def get_filenames_from_suite(suite):
+    testcases = get_testcases_from_suite(suite)
+    result = []
+    for testcase in testcases:
+        filename = str(testcase)
+        if ' ' in filename:
+            filename = str(get_package(testcase.__module__).__file__)
+            filename = os.path.splitext(filename)[0] + '.py'
+        result.append(filename)
+    result.sort()
+    return result
+
+def get_basenames_from_suite(suite):
+    basenames = [os.path.basename(x) for x in get_filenames_from_suite(suite)]
+    basenames.sort()
+    return basenames
+    
+
+def setUpZope(test):
+    zope.component.eventtesting.setUp(test)
+
+def cleanUpZope(test):
+    cleanup.cleanUp()
+
+checker = renormalizing.RENormalizing([
+    # str(Exception) has changed from Python 2.4 to 2.5 (due to
+    # Exception now being a new-style class).  This changes the way
+    # exceptions appear in traceback printouts.
+    (re.compile(r"ConfigurationExecutionError: <class '([\w.]+)'>:"),
+                r'ConfigurationExecutionError: \1:'),
+    ])
+
+def testrunner_suite():
+    def setUp(test):
+        test.globs['saved-sys-info'] = (
+            sys.path[:],
+            sys.argv[:],
+            sys.modules.copy(),
+            gc.get_threshold(),
+            )
+        test.globs['this_directory'] = os.path.split(__file__)[0]
+        test.globs['testrunner_script'] = __file__
+        test.globs['get_basenames_from_suite'] = get_basenames_from_suite
+
+    def tearDown(test):
+        sys.path[:], sys.argv[:] = test.globs['saved-sys-info'][:2]
+        gc.set_threshold(*test.globs['saved-sys-info'][3])
+        sys.modules.clear()
+        sys.modules.update(test.globs['saved-sys-info'][2])
+    suites = [
+        doctest.DocFileSuite(
+        'testrunner.txt', 'README.txt', 'testgetter.txt',
+        package='z3c.testsetup',
+        setUp=setUp, tearDown=tearDown,
+        optionflags=doctest.ELLIPSIS+doctest.NORMALIZE_WHITESPACE,
+        checker=checker),
+        ]
+
+    suite = unittest.TestSuite(suites)
+    return suite
+
+def zopeapptestingless_suite():
+    def setUp(test):
+        test.globs['saved-sys-info'] = (
+            sys.path[:],
+            sys.argv[:],
+            sys.modules.copy(),
+            gc.get_threshold(),
+            )
+        mlist = [x for x in sys.modules.keys()
+                 if 'zope.app' in x or 'z3c.testsetup' in x]
+        for m in mlist:
+            del sys.modules[m]
+        plist = [x for x in sys.path if 'zope.app' in x]
+        for p in plist:
+            del sys.path[sys.path.index(p)]
+
+        test.globs['this_directory'] = os.path.split(__file__)[0]
+        test.globs['testrunner_script'] = __file__
+        test.globs['get_basenames_from_suite'] = get_basenames_from_suite
+
+    def tearDown(test):
+        sys.path[:], sys.argv[:] = test.globs['saved-sys-info'][:2]
+        gc.set_threshold(*test.globs['saved-sys-info'][3])
+        sys.modules.clear()
+        sys.modules.update(test.globs['saved-sys-info'][2])
+    suites = [
+        doctest.DocFileSuite(
+        'nozopeapptesting.txt',
+        package='z3c.testsetup',
+        setUp=setUp, tearDown=tearDown,
+        optionflags=doctest.ELLIPSIS+doctest.NORMALIZE_WHITESPACE,
+        checker=checker),
+        ]
+
+    suite = unittest.TestSuite(suites)
+    return suite
+
+
+def suiteFromFile(filename):
+    suite = unittest.TestSuite()
+    test = doctest.DocFileSuite(filename,
+                                package = 'z3c.testsetup',
+                                setUp=setUpZope,
+                                tearDown=cleanUpZope,
+                                globs={'pnorm':pnorm,
+                                       'get_basenames_from_suite':
+                                       get_basenames_from_suite},
+                                checker=checker,
+                                optionflags=doctest.ELLIPSIS+
+                                doctest.NORMALIZE_WHITESPACE)
+
+    suite.addTest(test)
+    return suite
+
+def test_suite():
+    suite = unittest.TestSuite()
+    for name in TESTFILES:
+        suite.addTest(suiteFromFile(name))
+    suite.addTest(testrunner_suite())
+    suite.addTest(zopeapptestingless_suite())
+    return suite
