ef9c26189d121ae593e9a04d9fcc423ef07ea0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:45 +0000

    Add package SPARQLWrapper/1.2.0/sparqlwrapper-1.2.0.tar.gz

---
+++ packages/SPARQLWrapper/sparqlwrapper-1.2.0.tar.gz/sparql-wrapper-python-1.2.0/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(name="SPARQLWrapper",
+      description="SPARQL Interface",
+      version="1.2.0",
+      author="Ivan Herman, Sergio Fernández, Carlos Tejo Alonso",
+      author_email="ivan@ivan-herman.net",
+	  maintainer="Ivan Herman, Sergio Fernández, Carlos Tejo Alonso",
+	  maintainer_email="sparql-wrapper-devel@lists.sourceforge.net",
+      packages=['SPARQLWrapper'])
+
