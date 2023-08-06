8487fd89b243d6313007e7a77bb4804b0c5cad3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:13 +0000

    Add package hatenadiary/0.1/hatenadiary-0.1.tar.gz

---
+++ packages/hatenadiary/hatenadiary-0.1.tar.gz/hatenadiary-0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1'
+
+#import ez_setup
+#ez_setup.use_setuptools()
+
+from setuptools import setup
+
+setup(
+    name = "hatenadiary",
+    version = __version__,
+    author='Yoshiori SHOJI',
+    author_email='yoshiori@google.com',
+    description='A python wrapper around the Hatena Diary AtomPub',
+)
