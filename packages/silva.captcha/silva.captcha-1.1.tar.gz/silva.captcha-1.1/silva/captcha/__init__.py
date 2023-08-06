3d9cf358000e15a7a6ba5d2046525264ec420276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:33 +0000

    Add package silva.captcha/1.1/silva.captcha-1.1.tar.gz

---
+++ packages/silva.captcha/silva.captcha-1.1.tar.gz/silva.captcha-1.1/silva/captcha/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: __init__.py 31899 2008-11-12 10:35:19Z sylvain $
+
+from zope.component import getMultiAdapter
+from AccessControl import ModuleSecurityInfo, allow_module
+from Products.Silva.ExtensionRegistry import extensionRegistry
+
+import install
+
+def initialize(context):
+    extensionRegistry.register(
+        'silva.captcha', 'Silva Captcha', context, [],
+        install, depends_on='Silva')
+
+allow_module('silva.captcha')
+module_security = ModuleSecurityInfo('silva.captcha')
+module_security.declareProtected('View', 'validate')
+def validate(context, request, input):
+    """Validate a potential captcha entry for a Python Script.
+    """
+    captcha = getMultiAdapter((context, request), name='captcha')
+    return captcha.verify(input)
---
+++ packages/silva.captcha/silva.captcha-1.1.tar.gz/silva.captcha-1.1/silva/captcha/install.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: install.py 31899 2008-11-12 10:35:19Z sylvain $
+
+from Products.Five.site.localsite import enableLocalSiteHook, FiveSite
+
+from zope.app.component.interfaces import ISite
+from zope.app.component.hooks import setSite
+from zope.component import queryUtility
+
+from plone.keyring.interfaces import IKeyManager
+
+from utility import SilvaKeyManager
+
+
+def install(context):
+    """Install Captcha Support.
+    """
+    if not ISite.providedBy(context):
+        enableLocalSiteHook(context)
+        setSite(context)
+    sm = context.getSiteManager()
+    sm.registerUtility(IKeyManager, SilvaKeyManager())
+
+def uninstall(context):
+    """Uninstall Captcha Support.
+    """
+    sm = context.getSiteManager()
+    utility = sm.queryUtility(IKeyManager)
+    parent = utility.aq_parent
+    parent.manage_delObjects(['IKeyManager'])
+
+def is_installed(context):
+    return not (queryUtility(IKeyManager) is None)
+
+
+
