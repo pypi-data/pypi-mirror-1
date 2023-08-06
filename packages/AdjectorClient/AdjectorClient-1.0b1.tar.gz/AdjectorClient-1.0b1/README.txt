8a62536d6c1296653894a32dd96d34db6824fc18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:03 +0000

    Add package AdjectorClient/1.0b1/AdjectorClient-1.0b1.tar.gz

---
+++ packages/AdjectorClient/AdjectorClient-1.0b1.tar.gz/AdjectorClient-1.0b1/README.txt
@@ -0,0 +1,31 @@
+Adjector 1.0b
+*************
+
+Hi there.  Thanks for using Adjector, a lightweight, flexible, open-source
+ad server written in Python.
+
+Adjector is licensed under the GPL, version 2 or 3, at your option.
+For more information, see LICENSE.txt.
+
+This Distribution
+-----------------
+This is the client-only Adjector distribution.
+This distribution does not have as many dependencies, but can only access
+a database created by the full version of Adjector.  This version
+is intended for installation on systems that need ads served, but which
+are using another system to configure the ads.
+  
+A Trac plugin is also available.  The full Adjector version and the Trac
+plugin can be downloaded at
+http://projects.icapsid.net/adjector/wiki/Download
+
+Documentation
+-------------
+All of our documentation is online at 
+http://projects.icapsid.net/adjector
+
+You may wish to get started with 'Installing the Adjector Client' at 
+http://projects.icapsid.net/adjector/wiki/ClientInstall
+
+For questions, comments, help, or any other information, visit us online
+or email hello@icapsid.net.
---
+++ packages/AdjectorClient/AdjectorClient-1.0b1.tar.gz/AdjectorClient-1.0b1/setup.py
@@ -0,0 +1,71 @@
+#-------------------------------------------------------------------------
+# Copyright 2009-2010 David Isaacson, Stou Sandalski, Information Capsid
+#
+# This file is part of the program Adjector.
+#
+# Adjector is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) version 3 of the License.
+#
+# Adjector is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Adjector. If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------
+
+try:
+    from setuptools import setup, find_packages
+except ImportError: #Automagically downloads and installs setuptools if you don't have it
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import sys
+from pkg_resources import require, DistributionNotFound
+
+try:
+    require('Adjector')
+    print
+    print 'You already have the full Adjector package installed.'
+    print 'You are attempting to install a stripped-down version of Adjector;'
+    print 'NO EXTRA FUNCTIONALITY will be provided that you do not already have.'
+    print 'However if you are sure you want to do this, you need to remove Adjector'
+    print 'from your site-packages directory first. '
+    print
+    sys.exit()
+
+except DistributionNotFound:
+    pass
+
+setup(
+    name='AdjectorClient',
+        # It would be better to call this something else, but then we might see conflicts (don't want both installed)
+    version='1.0b1',
+    description='Client-only package for Adjector, a lightweight, fast, flexible ad server.',
+    long_description='''This is the client-only package for Adjector.  This package should be used if you would like to render the ads directly from your application, instead of through the server.\n\n Adjector is a lightweight, fast, flexible, open-source ad server.  It serves plain text, HTML, and Javascript ads to your web application in several different ways, even from a separate machine.  It tracks views and clicks.  It is designed to save you time and effort and stay out of your way.  Written in Pylons.''',
+    author='David Isaacson',
+    author_email='david@icapsid.net',
+    url='http://projects.icapsid.net/adjector',
+    license='GPL v2 or v3 (at your option)',
+    install_requires=[
+        'SQLAlchemy>=0.5',
+        'Elixir>=0.6.1',
+        'pytz',
+        'pysqlite>=2.0' # Only req'd for sqlite of course : )
+    ],
+    
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['adjector'],
+    
+    #include_package_data=True,
+    #test_suite='nose.collector',
+    #package_data={'adjector': ['i18n/*/LC_MESSAGES/*.mo']},
+    #message_extractors={'adjector': [
+    #        ('**.py', 'python', None),
+    #        ('public/**', 'ignore', None)]},
+    #zip_safe=False,
+)
