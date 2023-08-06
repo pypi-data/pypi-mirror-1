8051c0decfaa94a32c0387830efa9bcce4b43515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package PyHesiod/0.2.0/PyHesiod-0.2.0.tar.gz

---
+++ packages/PyHesiod/PyHesiod-0.2.0.tar.gz/PyHesiod-0.2.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from setuptools import setup
+from distutils.extension import Extension
+from Pyrex.Distutils import build_ext
+
+setup(
+    name="PyHesiod",
+    version="0.2.0",
+    description="PyHesiod - Python bindings for the Heisod naming library",
+    author="Evan Broder",
+    author_email="broder@mit.edu",
+    license="MIT",
+    py_modules=['hesiod'],
+    ext_modules=[
+        Extension("_hesiod",
+                  ["_hesiod.pyx"],
+                  libraries=["hesiod"])
+        ],
+    cmdclass= {"build_ext": build_ext}
+)
