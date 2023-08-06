a5f53dcd7354a2e6cd516a20d53bfe253f823ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:44 +0000

    Add package owyl/0.2.1/owyl-0.2.1.tar.gz

---
+++ packages/owyl/owyl-0.2.1.tar.gz/owyl-0.2.1/setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""setup -- setuptools setup file for Owyl.
+
+$Author: david.eyk $\n
+$Rev: 20 $\n
+$Date: 2009-01-05 22:43:30 -0600 (Mon, 05 Jan 2009) $
+"""
+
+__author__ = "$Author: david.eyk $"[9:-2]
+__revision__ = "$Rev: 20 $"
+__date__ = "$Date: 2009-01-05 22:43:30 -0600 (Mon, 05 Jan 2009) $"[7:-2]
+
+__version__ = "0.2.1"
+__release__ = '.r'.join((__version__, __revision__))
+
+__description__ = "The goal of Owyl: provide a fast and flexible Behavior Tree library implemented in python."
+__long_description__ = """You have Pyglet. You've got Rabbyt. But who do your sprites go to for advice? Owyl, of course.
+
+The goal of Owyl: provide a fast and flexible Behavior Tree library implemented in python. For more information on Behavior Trees, see the articles at http://aigamedev.com/hierarchical-logic
+
+"""
+__classifiers__ = ["Development Status :: 3 - Alpha",
+                   "Environment :: Console",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Games/Entertainment",
+                   "Topic :: Scientific/Engineering :: Artificial Intelligence",
+                   "Topic :: Software Development :: Libraries",]
+
+import sys
+
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES=['egenix-mx-base',]
+ZIP_SAFE = True
+
+setup(
+    name = "owyl",
+    version = __version__,
+    author = "David Eyk",
+    author_email = "eykd@eykd.net",
+    url = "http://code.google.com/p/owyl/",
+    description = __description__,
+    long_description = __long_description__,
+    download_url = "http://code.google.com/p/owyl/downloads/list",
+    classifiers = __classifiers__,
+
+    package_dir = {'': 'src',},
+    packages = find_packages('src'),
+
+    include_package_data = True,
+    exclude_package_data = {'src':['*.c', '*.h',  '*.pyx', '*.pxd', '*.g']},
+    #data_files=['src/data',],
+
+    install_requires=INSTALL_REQUIRES,
+    zip_safe = ZIP_SAFE,
+
+    test_suite = "nose.collector",
+    )
+
---
+++ packages/owyl/owyl-0.2.1.tar.gz/owyl-0.2.1/tests/testowyl.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""testowyl -- some tests for owyl.
+
+
+
+Copyright 2008 David Eyk. All rights reserved.
+
+$Author: david.eyk $\n
+$Rev: 6 $\n
+$Date: 2009-01-05 19:18:13 -0600 (Mon, 05 Jan 2009) $
+"""
+
+__author__ = "$Author: david.eyk $"[9:-2]
+__revision__ = "$Rev: 6 $"[6:-2]
+__date__ = "$Date: 2009-01-05 19:18:13 -0600 (Mon, 05 Jan 2009) $"[7:-2]
+
+import unittest
+
+import owyl
+
+class OwylTest(unittest.TestCase):
+    def testSucceed(self):
+        """Can we succeed?
+        """
+        s = owyl.succeed()
+        t = s()
+        self.assertEqual(t.next(), True)
+        self.assertRaises(StopIteration, t.next)
+
+        t = s()
+        self.assertEqual(t.next(), True)
+        self.assertRaises(StopIteration, t.next)
+
+    def testFail(self):
+        """Can we fail?
+        """
+        s = owyl.fail()
+        t = s()
+        self.assertEqual(t.next(), False)
+        self.assertRaises(StopIteration, t.next)
+
+        t = s()
+        self.assertEqual(t.next(), False)
+        self.assertRaises(StopIteration, t.next)
+
+
+    def testVisitSequenceSuccess(self):
+        """Can we visit a successful sequence?
+        """
+        tree = owyl.sequence(owyl.succeed(),
+                             owyl.succeed(),
+                             owyl.succeed())
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [True, True, True, True])
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [True, True, True, True])
+
+    def testVisitSequenceFailure(self):
+        """Can we visit a failing sequence?
+        """
+        tree = owyl.sequence(owyl.succeed(),
+                             owyl.succeed(),
+                             owyl.fail(),
+                             owyl.succeed())
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [True, True, False, False])
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [True, True, False, False])
+        
+    def testVisitSelectorSuccess(self):
+        """Can we visit a successful selector?
+        """
+        tree = owyl.selector(owyl.fail(),
+                             owyl.fail(),
+                             owyl.succeed(),
+                             owyl.fail())
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [False, False, True, True])
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [False, False, True, True])
+
+    def testVisitSelectorFailure(self):
+        """Can we visit a failing selector?
+        """
+        tree = owyl.selector(owyl.fail(),
+                             owyl.fail(),
+                             owyl.fail())
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [False, False, False, False])
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [False, False, False, False])
+
+    def testParallel_AllSucceed_Success(self):
+        """Can we visit a suceeding parallel (all succeed)?
+        """
+        tree = owyl.parallel(owyl.sequence(owyl.succeed(),
+                                           owyl.succeed()),
+                             owyl.sequence(owyl.succeed(),
+                                           owyl.succeed()),
+                             policy=owyl.PARALLEL_SUCCESS.REQUIRE_ALL)
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [True,])
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [True,])
+
+    def testParallel_OneSucceeds_Success(self):
+        """Can we visit a suceeding parallel (one succeeds)?
+        """
+        tree = owyl.parallel(owyl.sequence(owyl.succeed(),
+                                           owyl.succeed()),
+                             owyl.sequence(owyl.succeed(),
+                                           owyl.fail()),
+                             policy=owyl.PARALLEL_SUCCESS.REQUIRE_ONE)
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [True,])
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [True,])
+
+    def testParallel_AllSucceed_Failure(self):
+        """Can we visit a failing parallel (all succeed)?
+        """
+        tree = owyl.parallel(owyl.sequence(owyl.succeed(),
+                                           owyl.fail()),
+                             owyl.sequence(owyl.succeed(),
+                                           owyl.succeed()),
+                             policy=owyl.PARALLEL_SUCCESS.REQUIRE_ALL)
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [False,])
+
+    def testParallel_OneSucceeds_Failure(self):
+        """Can we visit a failing parallel (one succeeds)?
+        """
+        tree = owyl.parallel(owyl.sequence(owyl.fail(),
+                                           owyl.fail()),
+                             owyl.sequence(owyl.fail(),
+                                           owyl.fail()),
+                             policy=owyl.PARALLEL_SUCCESS.REQUIRE_ONE)
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [False,])
+
+        v = owyl.visit(tree)
+
+        results =  [x for x in v if x is not None]
+        self.assertEqual(results, [False,])
+
+
+
+if __name__ == "__main__":
+    import testoob
+    testoob.main()
