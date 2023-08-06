368926a79140579c9c75e2e0346673b9e78c0885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package visionegg/1.1/visionegg-1.1-py2.4-macosx-10.3-fat.egg

---
+++ packages/visionegg/visionegg-1.1-py2.4-macosx-10.3-fat.egg/VisionEgg/PyroApps/GridGUI.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+# The Vision Egg: GridGUI
+#
+# Copyright (C) 2001-2003 Andrew Straw.
+# Author: Andrew Straw <astraw@users.sourceforge.net>
+# URL: <http://www.visionegg.org/>
+#
+# Distributed under the terms of the GNU Lesser General Public License
+# (LGPL). See LICENSE.TXT that came with this file.
+#
+# $Id: GridGUI.py 1224 2003-09-18 13:12:50Z astraw $
+
+import VisionEgg, string
+__version__ = VisionEgg.release_name
+__cvs__ = string.split('$Revision: 1224 $')[1]
+__date__ = string.join(string.split('$Date: 2003-09-18 07:12:50 -0600 (Thu, 18 Sep 2003) $')[1:3], ' ')
+__author__ = 'Andrew Straw <astraw@users.sourceforge.net>'
+
+import sys, os
+import Tkinter
+import VisionEgg.PyroApps.EPhysGUIUtils as client_utils
+
+def get_control_list():
+    return [("grid_server",GridControlFrame,GridControlFrame.title)]
+
+class GridMetaParameters:
+    def __init__(self):
+        pass
+        
+class GridControlFrame(client_utils.StimulusControlFrame):
+    title = "Grid for 3D calibration"
+    def __init__(self, master=None, suppress_go_buttons=0,**kw):
+        client_utils.StimulusControlFrame.__init__(self,
+                                                   master,
+                                                   suppress_go_buttons,
+                                                   GridControlFrame.title,
+                                                   GridMetaParameters,
+                                                   **kw)
+        Tkinter.Label( self.param_frame,
+                       text="No variables to control" ).grid()
+
+    def get_shortname(self):
+        """Used as basename for saving parameter files"""
+        return "grid"
+
+    def update_tk_vars(self):
+        pass
+    
+    def send_values(self,dummy_arg=None):
+        pass
+        if self.connected:
+            self.meta_controller.set_parameters( self.meta_params )
+
+    def get_duration_sec(self):
+        return 0.0
+
+if __name__=='__main__':
+    frame = GridControlFrame()
+    frame.pack(expand=1,fill=Tkinter.BOTH)
+    frame.winfo_toplevel().title("%s"%(os.path.basename(os.path.splitext(sys.argv[0])[0]),))
+    frame.mainloop()
