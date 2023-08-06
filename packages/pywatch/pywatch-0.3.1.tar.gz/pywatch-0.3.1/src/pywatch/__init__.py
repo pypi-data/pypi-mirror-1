5c25993b48c815172b1759d822a6b122ea82593e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:28 +0000

    Add package pywatch/0.3.1/pywatch-0.3.1.tar.gz

---
+++ packages/pywatch/pywatch-0.3.1.tar.gz/pywatch-0.3.1/src/pywatch/__init__.py
@@ -0,0 +1,31 @@
+import sys
+
+from optparse import OptionParser
+
+from pywatch.watcher import Watcher
+
+def main(args=None):
+    """
+    Used by the pywatch script to handle command-line
+    args.
+    """
+
+    if not args:
+        args = sys.argv[1:]
+
+    usage = 'usage: %prog [options] "command" file1 file2 ...'
+    parser = OptionParser(usage=usage)
+    parser.add_option("-v", "--verbose",
+                      action="store_true",
+                      dest="verbose",
+                      default=False,
+                      help="Output timestamp when commands are run.")
+    options, args = parser.parse_args(args)
+
+    if len(args) < 2:
+        print parser.error("You must provide a shell command and at least one file.")
+
+    cmds = [args[0], ]
+    files = args[1:]
+    w = Watcher(cmds=cmds, files=files, verbose=options.verbose)
+    w.run_monitor()
