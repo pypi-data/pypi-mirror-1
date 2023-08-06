c046c007e9ea06d8c0f648dc5604f7e450ca95a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:02 +0000

    Add package simples3/0.5/simples3-0.5.tar.gz

---
+++ packages/simples3/simples3-0.5.tar.gz/simples3-0.5/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+import simples3
+intro = open("README", "U").read()
+usage = "\nUsage\n-----\n\n" + simples3.__doc__
+changes = open("changes.rst", "U").read()
+long_description = intro + usage + "\n" + changes
+
+setup(name="simples3", version=simples3.__version__,
+      url="http://lericson.blogg.se/code/",
+      author="Ludvig Ericson", author_email="ludvig@lericson.se",
+      description="Simple, quick Amazon AWS S3 interface",
+      long_description=long_description,
+      py_modules=["simples3"])
