8072bb49587ec6936e01b04142c7bef3cdebb011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:38 +0000

    Add package iw.thumbs/0.5/iw.thumbs-0.5-py2.4.egg

---
+++ packages/iw.thumbs/iw.thumbs-0.5-py2.4.egg/iw/thumbs/url.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import re
+import os
+
+_default_parser = re.compile('^/+thumbs/+(?P<width>[0-9]+)x(?P<height>[0-9]+)/{0,1}(?P<path>/.+)')
+
+def default_parser(url, regexp=_default_parser):
+    m = regexp.match(url)
+    if m:
+        w = m.group('width')
+        h = m.group('height')
+        path = m.group('path')
+        path = path.replace('/', os.sep)
+        return path, (int(w), int(h))
+    return None
+
