2400b89bc3b034ed117ce9cf0ac6929d02ad6498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:18 +0000

    Add package alchemist.ui/0.4.0/alchemist.ui-0.4.0.tar.gz

---
+++ packages/alchemist.ui/alchemist.ui-0.4.0.tar.gz/alchemist.ui-0.4.0/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="alchemist.ui",
+    version="0.4.0",
+    install_requires=['setuptools',
+                      'ore.alchemist',
+                      'zope.formlib',
+                      'zc.table',
+                      'zope.viewlet',
+                      'simplejson'],
+    packages=find_packages(exclude=["*.tests"]),
+    namespace_packages=['alchemist'],
+    package_data = {
+      '': ['*.txt', '*.zcml', '*.pt'],
+    },
+    zip_safe=False,
+    author='Kapil Thangavelu',
+    author_email='kapil.foss@gmail.com',
+    description="user interface components for use with zope3 relational database applications, crud, listing and relation views",
+    long_description = read('changes.txt'),
+    license='LGPL',
+    classifiers=['Programming Language :: Python',
+                 'Framework :: Zope3',
+                 ],        
+    keywords="zope zope3",
+    )
