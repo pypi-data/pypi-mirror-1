151377eb90f8942a70e9f4704c565af333c2afe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:28 +0000

    Add package setuptools_trial/0.2/setuptools_trial-0.2.tar.gz

---
+++ packages/setuptools_trial/setuptools_trial-0.2.tar.gz/setuptools_trial-0.2/setuptools_trial/setuptools_trial.py
@@ -0,0 +1,80 @@
+from setuptools.command import test
+
+
+class TrialTest(test.test):
+    """
+    Twisted Trial setuptools command
+    """
+
+    user_options = test.test.user_options + [
+        ('coverage','c', "Report coverage data"),
+        ('reactor=','r', "Which reactor to use"),
+        ('reporter=', None, "Customize Trial's output with a Reporter plugin"),
+    ]
+
+    boolean_options = ['coverage']
+
+    def initialize_options(self):
+        test.test.initialize_options(self)
+        self.coverage = None
+        self.reactor = None
+        self.reporter = None
+
+    def finalize_options(self):
+        if self.test_suite is None:
+            if self.test_module is None:
+                self.test_suite = self.distribution.test_suite
+            else:
+                self.test_suite = self.test_module
+        elif self.test_module:
+            raise DistutilsOptionError(
+                "You may specify a module or a suite, but not both"
+            )
+
+        self.test_args = self.test_suite
+
+    def run_tests(self):
+        # We do the import from Twisted inside the function instead of the top
+        # of the file because since Twisted is a setup_requires, we can't
+        # assume that Twisted will be installed on the user's system prior
+        # to using Tahoe, so if we don't do the import here, then importing
+        # from this plugin will fail.
+        from twisted.scripts import trial
+
+        config = trial.Options()
+
+        # Handle parsing the trial options passed through the setuptools
+        # trial command.
+        cmd_options = []
+        if self.reactor is not None:
+            cmd_options.extend(['--reactor', self.reactor])
+        if self.reporter is not None:
+            cmd_options.extend(['--reporter', self.reporter])
+        config.parseOptions(cmd_options)
+
+
+        args = self.test_args
+        if type(args) == str:
+            args = [args,]
+
+        config['tests'] = args
+
+        if self.coverage:
+            config.opt_coverage()
+
+        trial._initialDebugSetup(config)
+        trialRunner = trial._makeRunner(config)
+        suite = trial._getSuite(config)
+
+        # run the tests
+        if config['until-failure']:
+            test_result = trialRunner.runUntilFailure(suite)
+        else:
+            test_result = trialRunner.run(suite)
+
+        # write coverage data
+        if config.tracer:
+            sys.settrace(None)
+            results = config.tracer.results()
+            results.write_results(show_missing=1, summary=False,
+                                  coverdir=config.coverdir)
