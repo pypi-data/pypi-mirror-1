fdd31b727fed13916b63727452970d8a10a721d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:23 +0000

    Add package tiddlywebplugins.instancer/0.2.5/tiddlywebplugins.instancer-0.2.5.tar.gz

---
+++ packages/tiddlywebplugins.instancer/tiddlywebplugins.instancer-0.2.5.tar.gz/tiddlywebplugins.instancer-0.2.5/setup.py
@@ -0,0 +1,26 @@
+import os
+
+try: # development environment
+	import mangler
+except ImportError:
+	pass
+
+from setuptools import setup, find_packages
+
+
+VERSION = "0.2.5"
+
+
+setup(
+	name = "tiddlywebplugins.instancer",
+	version = VERSION,
+	url = "http://pypi.python.org/pypi/tiddlywebplugins.instancer",
+	description = "TiddlyWeb plugin to simplify instance management for verticals",
+	long_description = file(os.path.join(os.path.dirname(__file__), "README")).read(),
+	platforms = "Posix; MacOS X; Windows",
+	author = "FND",
+	author_email = "FNDo@gmx.net",
+	namespace_packages = ["tiddlywebplugins"],
+	packages = find_packages(exclude=["test"]),
+	install_requires = ["setuptools", "tiddlyweb", "tiddlywebplugins.utils"] # XXX: include optional tiddlywebwiki?
+)
