04e0cc47fb3a03ab13bba90eed89ff5753e1c9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:38 +0000

    Add package mimir/0.5.0/mimir-0.5.0.tar.gz

---
+++ packages/mimir/mimir-0.5.0.tar.gz/mimir-0.5.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2005-2008 Ralph Meijer
+# See LICENSE for details.
+
+from setuptools import setup
+
+setup(name='mimir',
+      version='0.5.0',
+      description=u'Mimir daemons',
+      author='Ralph Meijer',
+      author_email='ralphm@ik.nu',
+      url='http://mimir.ik.nu/',
+      license='MIT',
+      packages=[
+          'mimir',
+          'mimir.aggregator',
+          'mimir.aggregator.test',
+          'mimir.monitor',
+          'twisted.plugins',
+      ],
+      package_data={'twisted.plugins': ['twisted/plugins/mimir_aggregator.py',
+                                        'twisted/plugins/mimir_monitor.py']},
+      data_files=[('share/mimir', ['db/monitor.sql'])],
+      install_requires = ['wokkel >= 0.3.1',
+                          'simplejson',
+                          'feedparser'],
+)
