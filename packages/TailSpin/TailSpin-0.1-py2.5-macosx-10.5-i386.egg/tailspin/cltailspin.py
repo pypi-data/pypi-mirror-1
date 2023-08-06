48d0cb25b935287dddac6cc9203b0b020bde5d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:06 +0000

    Add package TailSpin/0.1/TailSpin-0.1-py2.5-macosx-10.5-i386.egg

---
+++ packages/TailSpin/TailSpin-0.1-py2.5-macosx-10.5-i386.egg/tailspin/cltailspin.py
@@ -0,0 +1,10 @@
+from .cltailspin_h import helper
+
+def cltailspin(func):
+    func.recur = False
+    def help(*args,**kw):
+        return helper(func,args,kw)
+    return help
+
+def force(thing):
+    thing.__force__()
