9f7feccb31f4065511e9692d933f364e31acb491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:19 +0000

    Add package collective.indexing/1.0a2/collective.indexing-1.0a2-py2.4.egg

---
+++ packages/collective.indexing/collective.indexing-1.0a2-py2.4.egg/collective/indexing/indexer.py
@@ -0,0 +1,63 @@
+from persistent import Persistent
+from zope.interface import implements
+from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
+from Products.Archetypes.CatalogMultiplex import CatalogMultiplex
+from collective.indexing.interfaces import IIndexQueueProcessor
+
+
+# container to hold references to the original indexing methods
+# these are populated by `collective.indexing.monkey`
+catalogMultiplexMethods = {}
+catalogAwareMethods = {}
+
+
+def getDispatcher(obj, name):
+    """ return named indexing method according on the used mixin class """
+    if isinstance(obj, CatalogMultiplex):
+        op = catalogMultiplexMethods.get(name, None)
+    elif isinstance(obj, CMFCatalogAware):
+        op = catalogAwareMethods.get(name, None)
+    else:
+        op = None
+    return op
+
+
+def index(obj, attributes=None):
+    op = getDispatcher(obj, 'index')
+    if op is not None:
+        op(obj)
+
+def reindex(obj, attributes=None):
+    op = getDispatcher(obj, 'reindex')
+    if op is not None:
+        op(obj, attributes or [])
+
+def unindex(obj):
+    op = getDispatcher(obj, 'unindex')
+    if op is not None:
+        op(obj)
+
+
+class IPortalCatalogQueueProcessor(IIndexQueueProcessor):
+    """ an index queue processor for the standard portal catalog via
+        the `CatalogMultiplex` and `CMFCatalogAware` mixin classes """
+
+
+class PortalCatalogQueueProcessor(Persistent):
+    implements(IPortalCatalogQueueProcessor)
+
+    def index(self, obj, attributes=None):
+        index(obj, attributes)
+
+    def reindex(self, obj, attributes=None):
+        reindex(obj, attributes)
+
+    def unindex(self, obj):
+        unindex(obj)
+
+    def begin(self):
+        pass
+
+    def commit(self):
+        pass
+
---
+++ packages/collective.indexing/collective.indexing-1.0a2-py2.4.egg/collective/indexing/monkey.py
@@ -0,0 +1,80 @@
+# this modules takes care of monkey-patching the `CatalogMultiplex` (from
+# `Archetypes/CatalogMultiplex.py`) and `CMFCatalogAware` (from
+# `CMFCore/CMFCatalogAware.py`) mixin classes, so that indexing operations
+# will be added to the queue or, if disabled, directly dispatched to the
+# default indexer (using the original methods)
+
+from collective.indexing.utils import isActive, getIndexer
+from collective.indexing.indexer import catalogMultiplexMethods
+from collective.indexing.indexer import catalogAwareMethods
+from collective.indexing.indexer import index, reindex, unindex
+from collective.indexing.subscribers import filterTemporaryItems
+
+
+def indexObject(self):
+    if not isActive():
+        return index(self)
+    obj = filterTemporaryItems(self)
+    indexer = getIndexer()
+    if obj is not None and indexer is not None:
+        indexer.index(obj)
+
+
+def unindexObject(self):
+    if not isActive():
+        return unindex(self)
+    obj = filterTemporaryItems(self)
+    indexer = getIndexer()
+    if obj is not None and indexer is not None:
+        indexer.unindex(obj)
+
+
+def reindexObject(self, idxs=None):
+    if not isActive():
+        return reindex(self, idxs)
+    obj = filterTemporaryItems(self)
+    indexer = getIndexer()
+    if obj is not None and indexer is not None:
+        indexer.reindex(obj, idxs)
+
+
+# set up dispatcher containers for the original methods and
+# hook up the new methods if that hasn't been done before...
+from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
+from Products.Archetypes.CatalogMultiplex import CatalogMultiplex
+for module, container in ((CMFCatalogAware, catalogAwareMethods),
+                          (CatalogMultiplex, catalogMultiplexMethods)):
+    if not container:
+        container.update({
+            'index': module.indexObject,
+            'reindex': module.reindexObject,
+            'unindex': module.unindexObject,
+        })
+        module.indexObject = indexObject
+        module.reindexObject = reindexObject
+        module.unindexObject = unindexObject
+
+
+# before plone 3.1 renaming an item triggers a call to `reindexOnReorder`,
+# which uses the catalog to update the `getObjPositionInParent` index for
+# all objects in the given folder;  with queued indexing any renamed object's
+# id will still be present in the catalog at that time, but `getObject` will
+# fail, of course;  however, since using the catalog for this sort of thing
+# was a bad idea in the first place, the method is patched here and has been
+# fixed in plone 3.1 as well...
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.permissions import ModifyPortalContent
+from Products.CMFPlone.PloneTool import PloneTool
+from Products.CMFPlone.utils import getFSVersionTuple, base_hasattr as has
+
+def reindexOnReorder(self, parent):
+    """ Catalog ordering support """
+    mtool = getToolByName(self, 'portal_membership')
+    if mtool.checkPermission(ModifyPortalContent, parent):
+        for obj in parent.objectValues():
+            if has(obj, 'reindexObject') and has(obj, 'getObjPositionInParent'):
+                obj.reindexObject(['getObjPositionInParent'])
+
+if getFSVersionTuple() < (3,1):
+    PloneTool.reindexOnReorder = reindexOnReorder
+
