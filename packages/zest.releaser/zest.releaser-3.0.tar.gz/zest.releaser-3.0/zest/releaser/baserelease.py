4173ede769d8e88f74b09e30eef5f75f4bf0c45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:35 +0000

    Add package zest.releaser/3.0/zest.releaser-3.0.tar.gz

---
+++ packages/zest.releaser/zest.releaser-3.0.tar.gz/zest.releaser-3.0/zest/releaser/baserelease.py
@@ -0,0 +1,28 @@
+"""Provide a base for the three releasers"""
+
+from zest.releaser import utils
+from zest.releaser import choose
+
+
+class Basereleaser(object):
+
+    def __init__(self):
+        self.vcs = choose.version_control()
+        self.data = {}
+
+    def _run_entry_points(self, when):
+        which_releaser = self.__class__.__name__.lower()
+        utils.run_entry_points(which_releaser, when, self.data)
+
+    def run(self):
+        self._run_entry_points('before')
+        self.prepare()
+        self._run_entry_points('middle')
+        self.execute()
+        self._run_entry_points('after')
+
+    def prepare(self):
+        raise NotImplementedError()
+
+    def execute(self):
+        raise NotImplementedError()
