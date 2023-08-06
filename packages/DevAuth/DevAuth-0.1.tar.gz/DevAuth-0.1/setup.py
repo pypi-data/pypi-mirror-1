3bb783f9056d84f839b8cbf0739d7583dc3cd42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:07 +0000

    Add package DevAuth/0.1/DevAuth-0.1.tar.gz

---
+++ packages/DevAuth/DevAuth-0.1.tar.gz/DevAuth-0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+index_fn = os.path.join(os.path.dirname(__file__), 'docs', 'index.txt')
+
+setup(name='DevAuth',
+      version=version,
+      description="Authentication for developer access to applications",
+      long_description=open(index_fn).read(),
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Paste",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+      ],
+      keywords='wsgi debug tool',
+      author='Ian Bicking',
+      author_email='ianb@openplans.org',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'WebOb',
+        'Paste',
+        'tempita',
+      ],
+      entry_points="""
+      [paste.filter_app_factory]
+      main = devauth:make_middleware
+      """,
+      )
