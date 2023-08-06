68a0b08d5bc1b5e90e321ff3fb260541101aa059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:53 +0000

    Add package Clip2Zeus/0.8c/Clip2Zeus-0.8c.tar.gz

---
+++ packages/Clip2Zeus/Clip2Zeus-0.8c.tar.gz/Clip2Zeus-0.8c/clip2zeus/clip2zeus.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Monitors the system clipboard for text that contains URLs for conversion
+using 2ze.us
+"""
+
+from common import *
+
+__author__ = 'Josh VanderLinden'
+__version__ = '0.8c'
+
+def main():
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option('-p', '--port', dest='port', default=DEFAULT_PORT, help='The port for the daemon to listen on')
+    options, args = parser.parse_args()
+
+    params = dict(
+        port=options.port,
+    )
+
+    clip2zeus = Clip2ZeusApp.for_platform()
+    clip2zeus(**params).start()
+
+if __name__ == '__main__':
+    main()
+
