1268f3dd51b54de506b55fff3745394c2fe12879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:21 +0000

    Add package veliberator/0.1.1/veliberator-0.1.1.tar.gz

---
+++ packages/veliberator/veliberator-0.1.1.tar.gz/veliberator-0.1.1/setup.py
@@ -0,0 +1,36 @@
+"""Installing veliberator"""
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.1'
+
+setup(
+    name='veliberator',
+    version=version,
+    zip_safe=True,
+
+    scripts=['./bin/find_place.py',
+             './bin/synchronize.py'],
+    packages=find_packages(exclude=['tests',]),
+    install_requires = ['SQLAlchemy>=0.5',
+                        'Elixir>=0.6.1',],
+    include_package_data=True,
+    test_suite = 'tests.global_test_suite',
+
+    author='Fantomas42',
+    author_email='fantomas42@gmail.com',
+    url='http://fantomas.willbreak.it',
+ 
+    license='GPL',
+    platforms = 'any',
+    description="Python API for Velib.",
+    long_description=open(os.path.join("docs", "README.txt")).read(),
+    keywords='velib, api, service',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    )
