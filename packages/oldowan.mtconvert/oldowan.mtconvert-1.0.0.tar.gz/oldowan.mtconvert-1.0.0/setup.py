84439df10dec245a9261609c8f9d9e6b7d3febe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:47 +0000

    Add package oldowan.mtconvert/1.0.0/oldowan.mtconvert-1.0.0.tar.gz

---
+++ packages/oldowan.mtconvert/oldowan.mtconvert-1.0.0.tar.gz/oldowan.mtconvert-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys, os
+
+desc_lines = open('README', 'r').readlines()
+
+setup(name='oldowan.mtconvert',
+      version='1.0.0',
+      description=desc_lines[0],
+      long_description=''.join(desc_lines[2:]),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Scientific/Engineering :: Bio-Informatics"
+      ],
+      keywords='bioinformatics',
+      author='Ryan Raaum',
+      author_email='code@raaum.org',
+      url='http://www.raaum.org',
+      license='MIT',
+      platforms = ['Any'],
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=False,
+      install_requires=[
+          "oldowan.polymorphism >= 1.0.0",
+          "oldowan.mtdna >= 1.0.0"
+      ],
+      namespace_packages = ['oldowan'],
+      zip_safe=False,
+      test_suite = 'nose.collector',
+      )
