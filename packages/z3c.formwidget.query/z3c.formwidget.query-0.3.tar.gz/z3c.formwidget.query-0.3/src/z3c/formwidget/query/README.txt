a9a56bacfe2a82c973271d5017f359bfac39a88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package z3c.formwidget.query/0.3/z3c.formwidget.query-0.3.tar.gz

---
+++ packages/z3c.formwidget.query/z3c.formwidget.query-0.3.tar.gz/z3c.formwidget.query-0.3/src/z3c/formwidget/query/README.txt
@@ -0,0 +1,230 @@
+z3c.formwidget.query
+====================
+
+This package provides a widget to query a source.
+
+  >>> from z3c.form import testing
+  >>> testing.setupFormDefaults()
+
+We must register a form page template to render forms.
+
+  >>> from z3c.form import form, testing
+  >>> factory = form.FormTemplateFactory(
+  ...     testing.getPath('../tests/simple_subedit.pt'))
+
+Let's register our new template-based adapter factory:
+
+  >>> component.provideAdapter(factory)
+
+Sources
+-------
+
+Let's start by defining a source.
+
+  >>> from z3c.formwidget.query.interfaces import IQuerySource
+  >>> from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
+
+To make things simple, we'll just use unicode strings as values.
+
+  >>> class ItalianCities(object):
+  ...     interface.implements(IQuerySource)
+  ...
+  ...     vocabulary = SimpleVocabulary((
+  ...         SimpleTerm(u'Bologna', 'bologna', u'Bologna'),
+  ...         SimpleTerm(u'Palermo', 'palermo', u'Palermo'),
+  ...         SimpleTerm(u'Sorrento', 'sorrento', u'Sorrento')))
+  ...
+  ...     def __init__(self, context):
+  ...         self.context = context
+  ...
+  ...     __contains__ = vocabulary.__contains__
+  ...     __iter__ = vocabulary.__iter__
+  ...     getTerm = vocabulary.getTerm
+  ...     getTermByToken = vocabulary.getTermByToken
+  ...
+  ...     def search(self, query_string):
+  ...         return [v for v in self if query_string.lower() in v.value.lower()]
+
+  >>> from zope.schema.interfaces import IContextSourceBinder
+
+  >>> class ItalianCitiesSourceBinder(object):
+  ...     interface.implements(IContextSourceBinder)
+  ...
+  ...     def __call__(self, context):
+  ...         return ItalianCities(context)
+  
+Fields setup
+------------
+
+  >>> import zope.component
+  >>> import zope.schema
+  >>> import zope.app.form.browser
+  >>> from zope.publisher.interfaces.browser import IBrowserRequest
+
+First we have to create a field and a request:
+
+  >>> city = zope.schema.Choice(
+  ...     __name__='city',
+  ...     title=u'City',
+  ...     description=u'Select a city.',
+  ...     source=ItalianCitiesSourceBinder())
+
+Widgets
+-------
+
+There are two widgets available; one that corresponds to a single
+selection or a multi-selection of items from the source.
+
+To enable multiple selections, wrap the ``zope.schema.Choice`` in a field
+derived from ``zope.schema.Collection``.
+
+Let's begin with a single selection.
+
+  >>> class Location(object):
+  ...     city = None
+  
+  >>> location = Location()
+  >>> field = city.bind(location)
+
+  >>> from z3c.formwidget.query.widget import QuerySourceFieldRadioWidget
+
+  >>> def setupWidget(field, context, request):
+  ...     widget = QuerySourceFieldRadioWidget(field, request)
+  ...     widget.name = field.__name__
+  ...     widget.context = context
+  ...     return widget
+
+  >>> from z3c.form.testing import TestRequest
+  >>> request = TestRequest()
+
+An empty query is not carried out.
+  
+  >>> widget = setupWidget(field, location, request)
+  >>> 'type="radio"' in widget()
+  False
+
+Let's choose a city:
+
+  >>> location.city = u"Palermo"
+
+  >>> widget = setupWidget(field, location, request)
+
+We now expect a radio button to be present.
+  
+  >>> 'type="radio"' in widget()
+  True
+  
+We can put a query string in the request, and have our source queried.
+
+  >>> request = TestRequest(form={
+  ...     'city.widgets.query': u'bologna'})
+  
+  >>> widget = setupWidget(field, location, request)
+
+Verify results:
+  
+  >>> 'Bologna' in widget()
+  True
+
+  >>> 'Sorrento' in widget()
+  False
+
+Selecting 'Bologna' from the list should check the corresponding box.
+
+  >>> request = TestRequest(form={
+  ...     'city.widgets.query': u'bologna',
+  ...     'city': ('bologna',)})
+
+  >>> widget = setupWidget(field, location, request)
+
+  >>> 'checked="checked"' in widget()
+  True
+
+Now we want to try out selection of multiple items.
+
+  >>> cities = zope.schema.Set(
+  ...     __name__='cities',
+  ...     title=u'Cities',
+  ...     description=u'Select one or more cities.',
+  ...     value_type=zope.schema.Choice(
+  ...         source=ItalianCitiesSourceBinder()
+  ...     ))
+
+  >>> class Route(object):
+  ...     cities = ()
+  
+  >>> route = Route()
+  >>> field = cities.bind(route)
+
+  >>> from z3c.formwidget.query.widget import QuerySourceFieldCheckboxWidget
+
+  >>> def setupWidget(field, context, request):
+  ...     widget = QuerySourceFieldCheckboxWidget(field, request)
+  ...     widget.name = field.__name__
+  ...     widget.context = context
+  ...     return widget
+
+  >>> request = TestRequest()
+  
+An empty query is not carried out.
+  
+  >>> widget = setupWidget(field, route, request)
+  >>> 'type="checkbox"' in widget()
+  False
+
+Let's set a city on the route:
+
+  >>> route.cities = (u"Palermo",)
+
+  >>> widget = setupWidget(field, route, request)
+  >>> 'type="checkbox"' in widget()
+  True
+  
+Let's make a query for "bologna".
+
+  >>> request = TestRequest(form={
+  ...     'cities.widgets.query': u'bologna'})
+  
+  >>> widget = setupWidget(field, route, request)
+
+Verify results:
+
+  >>> 'Bologna' in widget()
+  True
+
+  >>> 'Sorrento' in widget()
+  False
+
+We'll select 'Bologna' from the list.
+
+  >>> request = TestRequest(form={
+  ...     'cities.widgets.query': u'bologna',
+  ...     'cities': ('bologna',)})
+
+  >>> widget = setupWidget(field, route, request)
+
+Verify that Bologna has been selected.
+
+  >>> 'checked="checked"' in widget()
+  True
+
+Let's try and simulate removing the item we've set on the
+context. We'll submit an empty tuple.
+
+  >>> request = TestRequest(form={
+  ...     'cities': ()})
+
+  >>> widget = setupWidget(field, route, request)
+
+We expect an unchecked box.
+
+  >>> 'type="checkbox"' in widget()
+  True
+  
+  >>> 'checked="checked"' in widget()
+  False
+
+Todo
+----
+
+- Functional testing with zope.testbrowser
---
+++ packages/z3c.formwidget.query/z3c.formwidget.query-0.3.tar.gz/z3c.formwidget.query-0.3/src/z3c/formwidget/query/widget.py
@@ -0,0 +1,192 @@
+import zope.component
+import zope.interface
+import zope.schema
+import zope.schema.interfaces
+
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.interfaces import ISource, IContextSourceBinder
+
+import z3c.form.interfaces
+import z3c.form.button
+import z3c.form.form
+import z3c.form.field
+import z3c.form.widget
+import z3c.form.term
+import z3c.form.browser.radio
+import z3c.form.browser.checkbox
+
+from z3c.formwidget.query import MessageFactory as _
+
+class SourceTerms(z3c.form.term.Terms):
+    
+    def __init__(self, context, request, form, field, widget, source):
+        self.context = context
+        self.request = request
+        self.form = form
+        self.field = field
+        self.widget = widget
+        
+        self.terms = source
+
+class QueryTerms(z3c.form.term.Terms):
+    
+    def __init__(self, context, request, form, field, widget, terms):
+        self.context = context
+        self.request = request
+        self.form = form
+        self.field = field
+        self.widget = widget
+        
+        self.terms = SimpleVocabulary(terms)
+            
+class QuerySubForm(z3c.form.form.Form):
+    zope.interface.implements(z3c.form.interfaces.ISubForm)
+    css_class = 'querySelectSearch'
+
+    fields = z3c.form.field.Fields(
+        zope.schema.TextLine(
+        __name__='query',
+        required=False))
+
+    def __init__(self, context, request, prefix=None):
+        super(QuerySubForm, self).__init__(context, request)
+
+        if prefix is not None:
+            self.prefix = prefix    
+
+    @z3c.form.button.buttonAndHandler(_(u"Search"))
+    def search(self, action):
+        data, errors = self.widgets.extract()
+        if not errors:
+            z3c.form.form.applyChanges(self, self.context, data)
+
+class QueryContext(object):
+    query = None
+
+class QuerySourceRadioWidget(z3c.form.browser.radio.RadioWidget):
+    """Query source widget that allows single selection."""
+    
+    _queryform = None
+    _resultsform = None
+
+    @property
+    def source(self):
+        return self.field.source
+
+    def update(self):
+        
+        # Allow the source to provide terms until we have more specific ones
+        # from the query. Things do not go well if self.terms is None
+
+        source = self.source
+        
+        if IContextSourceBinder.providedBy(source):
+            source = source(self.context)
+
+        assert ISource.providedBy(source)
+        
+        self.terms = SourceTerms(self.context, self.request, self.form, self.field, self, source)
+
+        # Set up query form
+
+        subform = self.subform = QuerySubForm(QueryContext(), self.request, self.name)
+        subform.update()
+
+        data, errors = subform.extractData()
+        if errors:
+            return
+
+        # Perform the search
+
+        query = data['query']
+        if query is not None:
+            terms = set(source.search(query))
+        else:
+            terms = set()
+        
+        # If we have values in the request, add these to the terms. 
+        # Otherwise, take tha value from
+        # the current saved value.
+
+        request_values = self.extract(default=z3c.form.interfaces.NOVALUE)
+        if request_values is not z3c.form.interfaces.NOVALUE:
+            if not isinstance(request_values, (tuple, set, list)):
+                request_values = (request_values,)
+
+            tokens = set([term.value for term in terms])
+            for token in request_values:
+                if token and token not in tokens:
+                    term = source.getTermByToken(token)
+                    terms.add(term)
+        else:
+            selection = zope.component.getMultiAdapter(
+                (self.context, self.field), z3c.form.interfaces.IDataManager).get()
+
+            if not isinstance(selection, (tuple, set, list)):
+                selection = [selection]
+            
+            values = set([term.token for term in terms])
+            for value in selection:
+                if value and value not in values:
+                    terms.add(source.getTerm(value))
+        
+        # set terms
+        self.terms = QueryTerms(self.context, self.request, self.form, self.field, self, terms)
+
+        # update widget - will set self.value
+        self.updateQueryWidget()
+
+    def extract(self, default=z3c.form.interfaces.NOVALUE):
+        value = self.extractQueryWidget(default)
+        if value is z3c.form.interfaces.NOVALUE:
+            return value
+        elif len(value) == 0:
+            return default
+        else:
+            return value
+        
+    def render(self):
+        subform = self.subform
+        return "\n".join((subform.render(), self.renderQueryWidget()))
+
+    def __call__(self):
+        self.update()
+        return self.render()
+
+    # For subclasses to override
+
+    def updateQueryWidget(self):
+        z3c.form.browser.radio.RadioWidget.update(self)
+
+    def renderQueryWidget(self):
+        return z3c.form.browser.radio.RadioWidget.render(self)
+        
+    def extractQueryWidget(self, default=z3c.form.interfaces.NOVALUE):
+        return z3c.form.browser.radio.RadioWidget.extract(self, default)
+
+class QuerySourceCheckboxWidget(
+    QuerySourceRadioWidget, z3c.form.browser.checkbox.CheckBoxWidget):
+    """Query source widget that allows multiple selections."""
+    
+    zope.interface.implementsOnly(z3c.form.interfaces.ICheckBoxWidget)
+
+    @property
+    def source(self):
+        return self.field.value_type.source
+
+    def updateQueryWidget(self):
+        z3c.form.browser.checkbox.CheckBoxWidget.update(self)
+
+    def renderQueryWidget(self):
+        return z3c.form.browser.checkbox.CheckBoxWidget.render(self)
+    
+    def extractQueryWidget(self, default=z3c.form.interfaces.NOVALUE):
+        return z3c.form.browser.checkbox.CheckBoxWidget.extract(self, default)
+
+@zope.interface.implementer(z3c.form.interfaces.IFieldWidget)
+def QuerySourceFieldRadioWidget(field, request):
+    return z3c.form.widget.FieldWidget(field, QuerySourceRadioWidget(request))
+
+@zope.interface.implementer(z3c.form.interfaces.IFieldWidget)
+def QuerySourceFieldCheckboxWidget(field, request):
+    return z3c.form.widget.FieldWidget(field, QuerySourceCheckboxWidget(request))
