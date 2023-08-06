e8220a0a1498a1b7e75c78c66cf74be1a1f93bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:29 +0000

    Add package SchemaBot/0.1.0a2/SchemaBot-0.1.0a2.tar.gz

---
+++ packages/SchemaBot/SchemaBot-0.1.0a2.tar.gz/SchemaBot-0.1.0a2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+from schemabot.version import author, author_email, copyright, version
+
+setup(
+    name='SchemaBot',
+    version=version,
+    description="Python package to automatically manage database schema version control when using SQLAlchemy. Databases can be easily upgraded or downgraded to any version of the schema.",
+    long_description="""\
+    """,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author=author,
+    author_email=author_email,
+    url='http://bitbucket.org/chrismiles/schemabot/',
+    download_url='http://pypi.python.org/pypi/SchemaBot',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+        "SQLAlchemy >= 0.4",
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
