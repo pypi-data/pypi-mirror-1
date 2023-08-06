990d9506a325156830df0140dd7712363c29a2eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:00 +0000

    Add package testresources/0.2.2/testresources-0.2.2.tar.gz

---
+++ packages/testresources/testresources-0.2.2.tar.gz/testresources-0.2.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="testresources",
+      version="0.2.2",
+      description="Testresources, a pyunit extension for managing expensive "
+                  "test resources",
+      maintainer="Test Resources developers",
+      maintainer_email="https://launchpad.net/~testresources-developers",
+      url="https://launchpad.net/testresources",
+      packages=['testresources', 'testresources.tests'],
+      package_dir = {'':'lib'}
+      )
