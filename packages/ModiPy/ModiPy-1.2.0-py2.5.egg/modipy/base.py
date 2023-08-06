a743df02a702267e51525b2871ee802ca546744f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:24 +0000

    Add package ModiPy/1.2.0/ModiPy-1.2.0-py2.5.egg

---
+++ packages/ModiPy/ModiPy-1.2.0-py2.5.egg/modipy/base.py
@@ -0,0 +1,32 @@
+# $Id$
+
+"""
+Some base level classes and definitions.
+"""
+
+class XMLConfigurable:
+    """
+    Something that is XML configurable, i.e.: pretty much
+    all the classes in the system, has some common functions
+    and attributes that we define here.
+    """
+    # These lists are used to perform some checking of
+    # the configuration that is passed in to make sure
+    # that it has all the right parts defined, and no
+    # extras.
+
+    # A list if tags that MUST be found in the element
+    # definition in the configuration stack
+    required_tags = []
+
+    # Other tags that are merely optional
+    optional_tags = []
+
+    def get_required_tags(self):
+        return self.required_tags
+
+    def get_optional_tags(self):
+        return self.optional_tags
+
+    def get_supported_tags(self):
+        return self.required_tags + self.optional_tags
---
+++ packages/ModiPy/ModiPy-1.2.0-py2.5.egg/modipy/interfaces.py
@@ -0,0 +1,36 @@
+#$Id$
+
+"""
+ModiPy interfaces
+"""
+from zope.interface import Interface, implements
+
+import logging
+import debug
+log = logging.getLogger('modipy')
+
+
+class IProvisioner(Interface):
+    """
+    Defines the Provisioner interface
+    """
+
+    def __init__(self, name='', namespace={}):
+        """
+        """
+
+    def perform_change(self, ignored, change):
+        """
+        Applies a change to the devices the change is defined to affect.
+        """
+
+    def apply_change(self, device, change):
+        """
+        Apply a change to a specific device.
+        """
+
+    def backout_change(self, device, change):
+        """
+        Back out a change that was applied to a specific device.
+        """
+
