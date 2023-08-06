62d2b0966668c4bf9435108198adf3226af841d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:31 +0000

    Add package plone.app.gloworm/0.1a7/plone.app.gloworm-0.1a7.tar.gz

---
+++ packages/plone.app.gloworm/plone.app.gloworm-0.1a7.tar.gz/plone.app.gloworm-0.1a7/setup.py
@@ -0,0 +1,42 @@
+# To build locally: python setup.py egg_info -RDb "" bdist_egg 
+# To release: python setup.py egg_info -RD sdist bdist_egg register upload
+# To create a named release: python setup.py egg_info -RDb "a1" sdist bdist_egg register upload
+# To release a dev build: python setup.py egg_info -rD sdist bdist_egg register upload
+# See http://peak.telecommunity.com/DevCenter/setuptools#release-tagging-options for more information.
+
+from setuptools import setup, find_packages
+
+version = '0.1a7'
+
+setup(name='plone.app.gloworm',
+      version=version,
+      description="A Firebug-like inspector tool for Plone.",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='WebLion Group, Penn State University',
+      author_email='support@weblion.psu.edu',
+      url='https://weblion.psu.edu/svn/weblion/weblion/plone.app.gloworm/',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      namespace_packages=['plone.app', 'plone'],
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'plone.app.customerize>=1.1.2',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
