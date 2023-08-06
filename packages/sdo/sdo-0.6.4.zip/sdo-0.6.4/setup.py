ceb5bb3fab643f1471baad8bc3687280bfc00ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:44 +0000

    Add package sdo/0.6.4/sdo-0.6.4.zip

---
+++ packages/sdo/sdo-0.6.4.zip/sdo-0.6.4/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+description = """\
+example of usage::
+
+import cx_Oracle
+import sdo
+conn=cx_Oracle.connect("scott/tiger")
+cur=conn.cursor()
+cur.execute("select geometry from city")
+print sdo.Geometry(cur.fetchone()[0]).wkt()
+""" 
+
+setup(
+    name='sdo',
+    version="0.6.4",
+    description = "Converts oracle SDO_GEOMETRY objects to WKT and GeoJSON.",
+    long_description= description,
+    author='tib00r',
+    author_email='tibor.arpas@infinit.sk',
+    #url='',
+    install_requires=["python-nicefloat"],
+    classifiers="Development Status :: 4 - Beta",
+    py_modules=['sdo','testsdo'],
+    include_package_data=True
+)
