426517a8b8691864ca878d4f0cb96829d9a4df71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:51 +0000

    Add package boduch/0.0.4/boduch-0.0.4-py2.5.egg

---
+++ packages/boduch/boduch-0.0.4-py2.5.egg/boduch/data/hash.py
@@ -0,0 +1,53 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from zope.interface import implements
+from boduch.interface import IHash
+from boduch.type import Type
+from boduch.event import publish, lock, unlock, EventHashPush, EventHashPop,\
+                         EventHashGet
+
+class Hash(Type):
+    implements(IHash)
+    def __init__(self, data={}):
+        """Constructor.  Initialize the Hash data."""
+        self.data=data
+        
+    def push(self, pair):
+        """Push key-value pair on the dictionary."""
+        publish(EventSetPush, kw={'hash':self, 'pair':pair})
+        lock()
+        self.data[pair[0]]=pair[1]
+        unlock()
+        
+    def pop(self, key):
+        """Pop an object from the dictionary."""
+        publish(EventSetPop, kw={'hash':self, 'key':key})
+        lock()
+        obj=self.data[key]
+        del self.data[key]
+        unlock()
+        return obj
+        
+    def get(self, key):
+        """Retrieve an object from the dictionary."""
+        publish(EventSetGet, kw={'hash':self, 'key':key})
+        lock()        
+        obj=self.data[key]
+        unlock()
+        return obj
+    
+__all__=['Hash']
---
+++ packages/boduch/boduch-0.0.4-py2.5.egg/boduch/type/util.py
@@ -0,0 +1,34 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This module defines some simple utility type functionality."""
+
+def is_type(obj, type):
+    """Return true if the specified obj is the specified type."""
+    candidates=[]
+    if hasattr(obj, '__name__'):
+        candidates.append(obj.__name__)
+    elif hasattr(obj, '__class__'):
+        class_obj=obj.__class__
+        candidates.append(class_obj.__name__)
+        for base in class_obj.__bases__:
+            candidates.append(base.__name__)
+    if hasattr(obj, '__bases__'):
+        for base in obj.__bases__:
+            candidates.append(base.__name__)
+    return type in candidates
+
+__all__=['is_type']
