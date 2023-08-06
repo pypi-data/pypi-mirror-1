3a3782d885916a16d704fb40d98d91fc163402ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:00 +0000

    Add package fusepy/1.0.r33/fusepy-1.0.r33.tar.gz

---
+++ packages/fusepy/fusepy-1.0.r33.tar.gz/fusepy-1.0.r33/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='fusepy',
+      version='1.0.r33',
+      description="Simple binding for FUSE",
+      author="Giorgos Verigakis",
+      author_email="verigak@gmail.com",
+      url="http://code.google.com/p/fusepy/",
+      py_modules=['fuse'])
