883a1e1bd7b6531629686f2e4c69b0f11d233484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:55 +0000

    Add package nowplaying/0.2.48/nowplaying-0.2.48.tar.gz

---
+++ packages/nowplaying/nowplaying-0.2.48.tar.gz/nowplaying-0.2.48/nowplaying/players/audacious15.py
@@ -0,0 +1,90 @@
+#	
+#	Copyright 2009 Amr Hassan <amr.hassan@gmail.com>
+#	
+#	This program is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; either version 2 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#	
+#	You should have received a copy of the GNU General Public License
+#	along with this program; if not, write to the Free Software
+#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#	MA 02110-1301, USA.
+
+from __base__ import *
+
+class Player(PlayerBase):
+	
+	def __init__(self):
+		PlayerBase.__init__(self, "Audacious", "1.5", "org.mpris.audacious")
+		
+	def get_track(self):
+		try:
+			object = self._get_dbus_object("/Player")
+			metadata = object.GetMetadata()
+		except:
+			return
+		
+		if "artist" in metadata and "title" in metadata and "album" in metadata:
+			return {"artist": utf8(metadata["artist"]), "title": utf8(metadata["title"]), "album": utf8(metadata["album"]), "uri": utf8(metadata["uri"])}
+	
+	def is_playing(self):
+		try:
+			object = self._get_dbus_object("/org/atheme/audacious")
+			return str(object.Status()) == "playing"
+		except:
+			return
+	
+	def play_pause(self):
+		try:
+			object = self._get_dbus_object("/org/atheme/audacious")
+			object.PlayPause()
+		except:
+			pass
+		
+	def play(self):
+		try:
+			object = self._get_dbus_object("/org/atheme/audacious")
+			object.Play()
+		except:
+			pass
+
+	def pause(self):
+		try:
+			object = self._get_dbus_object("/org/atheme/audacious")
+			object.Pause()
+		except:
+			pass
+	
+	def next(self):
+		try:
+			object = self._get_dbus_object("/Player")
+			object.Next()
+		except:
+			pass
+	
+	def previous(self):		
+		try:
+			object = self._get_dbus_object("/Player")
+			object.Prev()
+		except:
+			return
+	
+	def get_volume(self):
+		try:
+			object = self._get_dbus_object("/org/atheme/audacious")
+			return float(object.Volume()[0])
+		except:
+			return
+	
+	def set_volume(self, value):
+		try:
+			object = self._get_dbus_object("/org/atheme/audacious")
+			object.SetVolume(value, value)
+		except:
+			return
---
+++ packages/nowplaying/nowplaying-0.2.48.tar.gz/nowplaying-0.2.48/nowplaying/players/exaile03.py
@@ -0,0 +1,108 @@
+#	
+#	Copyright 2009 Amr Hassan <amr.hassan@gmail.com>
+#	
+#	This program is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; either version 2 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#	
+#	You should have received a copy of the GNU General Public License
+#	along with this program; if not, write to the Free Software
+#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#	MA 02110-1301, USA.
+
+from __base__ import *
+
+class Player(PlayerBase):
+	
+	def __init__(self):
+		PlayerBase.__init__(self, "Exaile", "0.3", "org.exaile.Exaile")
+	
+	def get_track(self):
+		if not self.is_playing(): return
+		
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		return {"artist": utf8(obj.GetTrackAttr("artist")),
+				"title": utf8(obj.GetTrackAttr("title")),
+				"album": utf8(obj.GetTrackAttr("album")),
+				"uri": utf8(obj.GetTrackAttr("__loc")),
+				"bitrate": utf8(obj.GetTrackAttr("__bitrate")),
+				"length": utf8(obj.GetTrackAttr("__length")),
+				"number": utf8(obj.GetTrackAttr("tracknumber")),
+				"playcount": utf8(obj.GetTrackAttr("__playcount")),
+				}
+	
+	def is_playing(self):
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		return obj.Query().startswith("status: playing")
+	
+	def play_pause(self):
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		obj.PlayPause()
+
+	def play(self):
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		obj.Play()
+
+	def pause(self):
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		if self.is_playing():
+			self.PlayPause()
+		
+	def next(self):
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		obj.Next()
+	
+	def previous(self):
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		obj.Prev()
+	
+	def get_volume(self):
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		return float(obj.GetVolume())
+	
+	def set_volume(self, value):
+		try:
+			obj = self._get_dbus_object("/org/exaile/Exaile")
+		except:
+			return
+		
+		obj.ChangeVolume(int(value))
+	
