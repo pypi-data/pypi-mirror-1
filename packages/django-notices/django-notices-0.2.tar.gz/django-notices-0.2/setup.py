a5c44f0d8487fedbaa48da4e0a4b71e8a3ab53e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:31 +0000

    Add package django-notices/0.2/django-notices-0.2.tar.gz

---
+++ packages/django-notices/django-notices-0.2.tar.gz/django-notices-0.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import django_notices
+
+setup(
+    name='django-notices',
+    version=".".join(map(str, django_notices.__version__)),
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://github.com/dcramer/django-notices',
+    install_requires=['django'],
+    description = 'A message notification system for Django.',
+    packages=find_packages(),
+    include_package_data=True,
+)
