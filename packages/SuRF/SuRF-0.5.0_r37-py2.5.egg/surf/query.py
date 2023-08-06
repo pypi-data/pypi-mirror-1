77e5327c86870f66b52fec90e8bd4c1bc028e013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:41 +0000

    Add package SuRF/0.5.0-r37/SuRF-0.5.0_r37-py2.5.egg

---
+++ packages/SuRF/SuRF-0.5.0_r37-py2.5.egg/surf/query.py
@@ -0,0 +1,196 @@
+# Copyright (c) 2009, Digital Enterprise Research Institute (DERI),
+# NUI Galway
+# All rights reserved.
+
+# author: Cosmin Basca
+# email: cosmin.basca@gmail.com
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer
+#      in the documentation and/or other materials provided with
+#      the distribution.
+#    * Neither the name of DERI nor the
+#      names of its contributors may be used to endorse or promote  
+#      products derived from this software without specific prior
+#      written permission.
+
+# THIS SOFTWARE IS PROVIDED BY DERI ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DERI BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# -*- coding: utf-8 -*-
+__author__ = 'Cosmin Basca'
+
+from rdf.graph import Graph, ConjunctiveGraph
+from rdf.term import URIRef, Literal, BNode, RDF, RDFS
+from rdf.namespace import Namespace
+
+'''
+abstract representation of a query, based on rdflib concepts, and basic python
+types
+'''
+class InvalidTypeQueryException(Exception):
+    '''
+    The invalid Query Type exception, is raised when the query type is different
+    from:
+        - select
+        - ask
+        - describe
+        - construct
+    '''
+    def __init__(self,message):
+        self.message = message
+        
+    def __str__(self):
+        return self.message
+
+# SPARQL vriables are represented as strings that follow the sparql
+# variable definition: "?name" represents the SPARQL variable name
+class Query(object):
+    '''
+    The `Query` object is used by surf to construct queries in a programatic manner
+    The class supports the major SPARQL query types: *select*,*ask*,*describe*,*construct*
+    Although it follows the SPARQL format the query can be translated to other Query
+    formats such as **PROLOG**, for now though only *SPARQL* is supported.
+    
+    the query methods can be chained.
+    '''
+    __query_types__ = ['select','ask','describe','construct']
+    
+    @classmethod
+    def select(cls,*vars):
+        '''
+        Creates a **select** `query`
+        '''
+        q = cls('select')
+        q.select_clauses.extend(vars)
+        return q
+    
+    @classmethod
+    def ask(cls,*patterns):
+        '''
+        Creates a **ask** `query`
+        '''
+        askq = cls('ask')
+        for pattern in patterns:
+            if type(pattern) in [list, tuple]:
+                if len(pattern) == 4:
+                    s,p,o,c = pattern
+                elif len(pattern) == 3:
+                    s,p,o = pattern
+                    c = None
+                else:
+                    continue
+                
+                if not c:
+                    c = '' # default graph
+                if c not in askq.ask_clauses:
+                    askq.ask_clauses[c] = []
+                askq.ask_clauses[c].append((s,p,o))
+        return askq
+    
+    @classmethod
+    def describe(cls):
+        '''
+        Creates a **describe** `query`
+        '''
+        return cls('describe')
+    
+    @classmethod
+    def construct(cls,*vars):
+        '''
+        Creates a **construct** `query`
+        '''
+        q = cls('construct')
+        q.select_clauses.extend(vars)
+        return q
+    
+    def __init__(self,q_type='select'):
+        '''
+        the `query` initialization.
+        `q_type` = the query type
+        '''
+        if q_type.lower() not in Query.__query_types__:
+            raise InvalidTypeQueryException('Unsupported Query type [%s]'%q_type)
+        
+        self.query_type = q_type.lower()
+        
+        self.select_clauses = []
+        self.where_clauses = {'':[]} # '' = default graph
+        self.filter_clauses = {'':[]} # '' = default graph
+        self.offset_value = None
+        self.limit_value = None
+        self.order_by_clauses = []
+        self.distinct_clauses = []
+        self.ask_clauses = {'':[]}
+    
+    def where(self,s,p,o,c=None,optional=False,filter=''):
+        '''
+        a `where` clause, accepts the `subject`, `predicate`, `object` and `context`,
+        also `optional` specifies if the clause is optional and `filter` wether it
+        is accompanied by a `filter`, `filter` syntax must follow **SPARQL** syntax
+        '''
+        if not c:
+            c = '' # default graph
+        if c not in self.where_clauses:
+            self.where_clauses[c] = []
+        self.where_clauses[c].append((s,p,o,optional,filter))
+        return self
+    
+    def order_by(self,*vars):
+        '''
+        order by the specified variables (`vars`)
+        '''
+        self.order_by_clauses.extend(vars)
+        return self
+    
+    def offset(self,value):
+        '''
+        the `offset` of the results
+        '''
+        self.offset_value = value
+        return self
+    
+    def limit(self,value):
+        '''
+        the `limit` of the results
+        '''
+        self.limit_value = value
+        return self
+    
+    def distinct(self,*vars):
+        '''
+        specify which variables (`vars`) are `distinct`
+        '''
+        self.distinct_clauses.extend(vars)
+        return self
+    
+    def filter(self,filter_expression,c=None):
+        '''
+        takes valid **SPARQL** filter expressions passed in as strings, no checks are
+        performed for malformed expressions, the sparql processor against which the
+        query is exectued will signall the error.
+        '''
+        if not c:
+            c = '' # default graph
+        if c not in self.filter_clauses:
+            self.filter_clauses[c] = []
+        self.filter_clauses[c].append(filter_expression)
+        return self
+    
+    def __str__(self):
+        return ''
---
+++ packages/SuRF/SuRF-0.5.0_r37-py2.5.egg/surf/util.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2009, Digital Enterprise Research Institute (DERI),
+# NUI Galway
+# All rights reserved.
+
+# author: Cosmin Basca
+# email: cosmin.basca@gmail.com
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer
+#      in the documentation and/or other materials provided with
+#      the distribution.
+#    * Neither the name of DERI nor the
+#      names of its contributors may be used to endorse or promote  
+#      products derived from this software without specific prior
+#      written permission.
+
+# THIS SOFTWARE IS PROVIDED BY DERI ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DERI BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# -*- coding: utf-8 -*-
+__author__ = 'Cosmin Basca'
+
+from namespace import *
+import re
+import new
+
+pattern_direct = re.compile('^[a-z0-9]{1,}_[a-zA-Z0-9_]{1,}$', re.DOTALL)
+pattern_inverse = re.compile('^is_[a-z0-9]{1,}_[a-zA-Z0-9_]{1,}_of$', re.DOTALL)
+
+def namespace_split(uri):
+    sp = '#' if uri.rfind('#') != -1 else '/'
+    base, predicate = uri.rsplit(sp,1)
+    return get_namespace('%s%s'%(base,sp))[1], predicate
+
+def uri_split(uri):
+    sp = '#' if uri.rfind('#') != -1 else '/'
+    base, predicate = uri.rsplit(sp,1)
+    return get_namespace('%s%s'%(base,sp))[0], predicate
+
+def uri_to_classname(uri):
+    ns_key, predicate = uri_split(uri)
+    return '%s%s'%(ns_key.title().replace('-','_'),predicate)
+
+def attr2rdf(attrname):
+    def tordf(attrname):
+        prefix, predicate = attrname.split('_',1)
+        ns = get_namespace_url(prefix)
+        try:
+            return ns[predicate]
+        except:
+            return None
+    
+    if pattern_inverse.match(attrname):
+        return  tordf(attrname.replace('is_','').replace('_of','')), False
+    elif pattern_direct.match(attrname):
+        return  tordf(attrname), True
+    return None, None
+
+def rdf2attr(uri,direct):
+    ns, predicate = uri_split(uri)
+    attribute = '%s_%s'%(ns.lower(),predicate)
+    return attribute if direct else 'is_%s_of'%attribute
+
+
+def is_attr_direct(attrname):
+    return True if pattern_direct.match(attrname) else False
+    
+def uri_to_class(uri):
+    return new.classobj(str(uri_to_classname(uri)),(),{'uri':uri})
+
+
