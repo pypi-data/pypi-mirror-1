1b7965fbfbb1591bbc31f3099a9fb3cbc8da79f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:23 +0000

    Add package garbas.watermark/1.1b1/garbas.watermark-1.1b1.tar.gz

---
+++ packages/garbas.watermark/garbas.watermark-1.1b1.tar.gz/garbas.watermark-1.1b1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.1b1'
+
+setup(name='garbas.watermark',
+      version=version,
+      description="AT Image field with watermark",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone',
+      author='Rok Garbas',
+      author_email='rok.garbas@gmail.com',
+      url='http://github.com/garbas/garbas.watermark',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['garbas'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
