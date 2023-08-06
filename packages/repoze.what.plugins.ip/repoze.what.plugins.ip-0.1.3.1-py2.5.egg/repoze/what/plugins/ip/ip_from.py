1cc1dd6aeecd8e6d09d143009a6f582838f438ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:29 +0000

    Add package repoze.what.plugins.ip/0.1.3.1/repoze.what.plugins.ip-0.1.3.1-py2.5.egg

---
+++ packages/repoze.what.plugins.ip/repoze.what.plugins.ip-0.1.3.1-py2.5.egg/repoze/what/plugins/ip/ip_from.py
@@ -0,0 +1,31 @@
+from repoze.what.predicates import Predicate
+from ipaddr import IPv4 as IP #: Googles ipaddr.py
+from types import ListType
+
+class ip_from(Predicate):
+    """ Only allow access to specified IPs """
+    
+    message = "Invalid Access" #: default message
+    
+    def __init__(self, allowed=None, message=None, **kwargs):
+        """
+        @param allowed: the ip or list of ips allowed to access the wsgi server 
+        @param message: The message to return when check fails 
+        """
+        
+        allowed = [allowed] if type(allowed) is not ListType else allowed 
+        
+        self.message = message if message else self.message
+        self.allowed = [IP(x) for x in allowed]
+        super(ip_from, self).__init__(**kwargs)
+
+    def evaluate(self, environ, credentials):
+        """ 
+        check that the request ip in the allowed list 
+        @return: Boolean
+        """
+        
+        remote = IP(environ.get("REMOTE_ADDR", "0.0.0.0"))
+        
+        if True not in [(remote in x) for x in self.allowed]:  
+            self.unmet(self.message)
