aee8311ab641db5a0408ab2831a13e349833d867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:00 +0000

    Add package z3c.recipe.runscript/0.1.1/z3c.recipe.runscript-0.1.1.tar.gz

---
+++ packages/z3c.recipe.runscript/z3c.recipe.runscript-0.1.1.tar.gz/z3c.recipe.runscript-0.1.1/src/z3c/recipe/runscript/tests/test_doc.py
@@ -0,0 +1,30 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""z3c.recipe.runscript test setup."""
+import unittest
+import zc.buildout.testing
+from zope.testing import doctest
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install_develop('z3c.recipe.runscript', test)
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite(
+            '../README.txt',
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            ),
+        ))
