7ebaf3a89c5b7ce1c264f3fc3c8adddf593cbec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:27 +0000

    Add package pyjld.os/0.6/pyjld.os-0.6-py2.5.egg

---
+++ packages/pyjld.os/pyjld.os-0.6-py2.5.egg/pyjld/os/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+""" 
+pyjld.os package
+
+Changelog:
+==========
+
+*0.6* added ``recursive_chmod``
+"""
+__author__  = "Jean-Lou Dupont"
+__email     = "python (at) jldupont.com"
+__version__ = "0.6"
+__fileid    = "$Id: __init__.py 75 2009-04-25 02:31:31Z jeanlou.dupont $"
+
+
+__classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: Public Domain',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    ]
+
+__dependencies = []
+
+from tools import *
