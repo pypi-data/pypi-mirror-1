533b54e222443792855cacaee8d23553b39d898f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:36 +0000

    Add package prettytable/0.1/prettytable-0.1-py2.6.egg

---
+++ packages/prettytable/prettytable-0.1-py2.6.egg/prettytable.py
@@ -0,0 +1,163 @@
+# Copyright (c) 2009, Luke Maurits <luke@maurits.id.au>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * The name of the author may not be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import copy
+
+class PrettyTable:
+
+	def __init__(self):
+
+		self.fields = []
+		self.rows = []
+		self.widths = []
+		self.aligns = []
+		
+		self.hrules = False
+
+	def __getslice__(self, i, j):
+
+		"""Return a new PrettyTable whose data rows are a slice of this one's
+
+		Arguments:
+			
+		i - beginning slice index
+		j - ending slice index"""
+		
+		newtable = copy.deepcopy(self)
+		newtable.rows = self.rows[i:j]
+		return newtable
+
+	def set_field_names(self, fields):
+
+		self.fields = fields
+		self.widths = [len(field) for field in fields]
+		self.aligns = len(fields)*["c"]
+
+	def set_field_align(self, fieldname, align):
+
+		"""Set the alignment of a field by its fieldname
+
+		Arguments:
+			
+		fieldname - name of the field whose alignment is to be changed
+		align - desired alignment - "l" for left, "c" for centre and "r" for right"""
+
+		if fieldname not in self.fields:
+			raise Exception("No field %s exists!" % fieldname)
+		if align not in ["l","c","r"]:
+			raise Exception("Alignment %s is invalid, use l, c or r!" % align)
+		self.aligns[self.fields.index(fieldname)] = align
+
+	def add_row(self, row):
+
+		"""Add a row to the table
+
+		Arguments:
+			
+		row - row of data, should be a list with as many elements as the table
+		has fields"""
+		
+		if len(row) != len(self.fields):
+			raise Exception("Row has incorrect number of values")
+		self.rows.append(row)
+		for i in range(0,len(row)):
+			if len(str(row[i])) > self.widths[i]:
+				self.widths[i] = len(str(row[i]))
+
+	def printt(self, start=0, end=None, fields=None, hrules=None):
+
+		"""Print table in current state to stdout.
+		
+		Arguments:
+			
+		start - index of first data row to include in output
+		end - index of last data row to include in output PLUS ONE (list slice style)"""
+
+		if not self.fields:
+			return
+		self._print_header(fields)
+		if hrules:
+			hrule = hrules
+		else:
+			hrule = self.hrules
+		for row in self.rows[start:end]:
+			self._print_row(row, fields, hrule)
+		if not self.hrules:
+			self._print_hrule()
+
+	def _print_hrule(self, fields=None):
+		bits = ["+"]
+		for field, width in zip(self.fields, self.widths):
+			if fields and field not in fields:
+				continue
+			bits.append((width+2)*"-")
+			bits.append("+")
+		print "".join(bits)
+
+	def _print_header(self, fields=None):
+
+		self._print_hrule()
+		bits = ["|"]
+		for field, width in zip(self.fields, self.widths):
+			if fields and field not in fields:
+				continue
+			bits.append(field.center(width+2))
+			bits.append("|")
+		print "".join(bits)
+		self._print_hrule()
+
+	def _print_row(self, row, fields, hrule):
+		bits = ["|"]
+		for field, value, width, align in zip(self.fields, row, self.widths, self.aligns):
+			if fields and field not in fields:
+				continue
+			if align == "l":
+				bits.append(str(value).ljust(width).center(width+2))
+			elif align == "r":
+				bits.append(str(value).rjust(width).center(width+2))
+			else:
+				bits.append(str(value).center(width+2))
+			bits.append("|")
+		print "".join(bits)
+		if hrule:
+			self._print_hrule()
+
+def main():
+
+	x = PrettyTable()
+	x.set_field_names(["City name", "Area", "Population", "Annual Rainfall"])
+	x.set_field_align("City name", "l") # Left align city names
+	x.add_row(["Adelaide",1295, 1158259, 600.5])
+	x.add_row(["Brisbane",5905, 1857594, 1146.4])
+	x.add_row(["Darwin", 112, 120900, 1714.7])
+	x.add_row(["Hobart", 1357, 205556, 619.5])
+	x.add_row(["Sydney", 2058, 4336374, 1214.8])
+	x.add_row(["Melbourne", 1566, 3806092, 646.9])
+	x.add_row(["Perth", 5386, 1554769, 869.4])
+	x.printt()
+
+if __name__ == "__main__":
+	main()
