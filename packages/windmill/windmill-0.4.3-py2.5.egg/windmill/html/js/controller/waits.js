2dc0df5ab6a7cb2305596825e9b6078ae3b909e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:12 +0000

    Add package windmill/0.4.3/windmill-0.4.3-py2.5.egg

---
+++ packages/windmill/windmill-0.4.3-py2.5.egg/windmill/html/js/controller/waits.js
@@ -0,0 +1,234 @@
+/*
+Copyright 2006-2007, Open Source Applications Foundation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+//Wait a specified number of milliseconds
+windmill.controller.waits.sleep = function (paramObj, obj) { 
+  windmill.waiting = true;
+  //if a number of milliseconds werent provided in a int format
+  if ((paramObj.milliseconds == "") || (parseInt(paramObj.milliseconds, 10) == NaN)){
+    paramObj.milliseconds = 5000;
+  }
+  
+  done = function(){
+    windmill.waiting = false;
+    windmill.controller.continueLoop();
+    //we passed the id in the parms object of the action in the ide
+    var aid = paramObj.aid;
+    delete paramObj.aid;
+    //set the result in the ide
+    windmill.xhr.setWaitBgAndReport(aid,true,obj);
+  }    
+  setTimeout('done()', paramObj.milliseconds);
+  return true;
+};
+  
+windmill.controller.waits.forJSTrue = function (paramObj, obj, pageLoad) { 
+  _this = this;
+  
+  //we passed the id in the parms object of the action in the ide
+  var aid = paramObj.aid;
+  delete paramObj.aid;
+  var count = 0;
+  var p = paramObj || {};
+  var timeout = 20000;
+  var isJsTest = (p.orig == 'js');
+  var testCondition = p.test;
+  
+  // If we get the weird string "NaN" (yes, the actual 
+  // string, "NaN" :)) value from the IDE, or some other 
+  // unusable string , just use the default value of 20 seconds
+  if (p.timeout) {
+    if (parseInt(p.timeout, 10) != NaN){
+      timeout = p.timeout;
+    }
+  }
+
+  var lookup = function () {
+    if (count >= timeout) {
+      if (isJsTest) {
+        windmill.jsTest.runTestItemArray();
+        windmill.jsTest.waiting = false;
+        windmill.jsTest.handleErr('waits.forElement timed out after ' + timeout + ' seconds.');
+      }
+      else {
+        if (pageLoad){ windmill.loaded(); }
+        else{ windmill.controller.continueLoop(); }
+      }
+        windmill.xhr.setWaitBgAndReport(aid,false,obj);
+        return false;
+    }
+    count += 2500;
+    
+    // Get a result
+    var result;
+    if (typeof testCondition == 'string') {
+      result = eval(testCondition);
+    }
+    else if (typeof testCondition == 'function') {
+      result = testCondition();
+    }
+    else {
+      throw new Error('waits.forTrue test condition must be a string or function.');
+    }
+    result = !!result; // Make sure we've got a Boolean
+    
+    if (!result){ var x = setTimeout(lookup, 1500); }
+    else {
+        c = function () {
+          //If this method is being called by the js test framework
+          if (isJsTest) {
+            windmill.jsTest.waiting = false;
+            windmill.jsTest.runTestItemArray();
+          }
+          else{ 
+             if (pageLoad){ windmill.loaded(); }
+             else{ windmill.controller.continueLoop(); }
+          }
+        
+           //set the result in the ide
+            windmill.xhr.setWaitBgAndReport(aid,true,obj);
+        }
+      setTimeout(c, 1000);
+    }
+  }
+    
+  lookup();
+   
+  //waits are going to wait, so I return true
+  //Optimally it would return false if it times out, so when it does return false
+  //the calling code will jump back up and process the ui accordingly
+  return true;
+
+};
+
+//wait for an element to show up on the page
+//if it doesn't after a provided timeout, defaults to 20 seconds
+windmill.controller.waits.forElement = function (paramObj,obj) { 
+    var p = paramObj || {};
+    var f = function () {
+      return windmill.controller._lookupDispatch(p);
+    };
+    p.test = f;
+    return windmill.controller.waits.forJSTrue(p, obj);
+};
+  
+//wait for an element to show up on the page
+//if it doesn't after a provided timeout, defaults to 20 seconds
+windmill.controller.waits.forNotElement = function (paramObj,obj) { 
+    var p = paramObj || {};
+    var f = function () {
+      var node = windmill.controller._lookupDispatch(p);
+      return !node;
+    };
+    p.test = f;
+    return windmill.controller.waits.forJSTrue(p, obj);
+};
+
+
+
+//This is more of an internal function used by wait and click events
+//To know when to try and reattach the listeners
+//But if users wanted this manually they could use it
+windmill.controller.waits.forPageLoad = function (paramObj,obj) {
+  var p = paramObj || {};
+  var sl = function(){
+    var f = function () {
+      try {
+        var v = opener.document.domain;
+      }
+      catch(err){
+        document.domain = windmill.docDomain;
+      }
+      try {
+        var d = windmill.testWindow.document.body;
+      }catch(err){ d = null;}
+    
+      if (d != null){
+        return true;
+      }
+      return false;
+    };
+    p.test = f;
+    return windmill.controller.waits.forJSTrue(p, obj, true);
+  }
+  setTimeout(sl, 3000);
+}
+  
+//Turn the loop back on when the page in the testingApp window is loaded
+//this is an internal wait used only for the first load of the page
+//a more generic one will be added if there is a need
+windmill.controller.waits._forNotTitleAttach = function (param_object) { 
+  _this = this;
+
+  var timeout = 80000;
+  var count = 0;
+  var p = param_object;
+    
+  if (p.timeout){
+    timeout = p.timeout;
+  }
+  this.lookup = function(){
+    if (count >= timeout){
+      windmill.controller.continueLoop();
+      return false;
+    }
+    try {
+      if (windmill.testWindow.document.title == p.title){
+	      var n = false;
+      }
+      else { var n = true };
+    }
+    catch(err){
+      n = false;
+    }
+    count += 2500;
+      
+    this.check(n);
+  }
+    
+  this.check = function(n){   
+
+    if (!n){
+      var x = setTimeout(function () { _this.lookup(); }, 1000);
+    }
+    else{
+      
+      try {  
+        if (typeof(windmill.testWindow.onload.listenReg) == 'undefined'){
+          windmill.loaded();
+        }
+      }
+      catch(err){ this.lookup() }
+        fleegix.event.suppressHandlerErrors(windmill.testWindow, 'onload');
+        fleegix.event.unlisten(windmill.testWindow, 'onload', windmill, 'loaded');
+        fleegix.event.listen(windmill.testWindow, 'onload', windmill, 'loaded');
+        _this.lookup();
+
+      return true;
+    }
+  }
+
+  this.lookup();
+  
+  //if windmill.timeout goes by and the tests haven't been started
+  //We go ahead and start them, longer waits can happen by changing windmill.timeout
+  ct = function(){ 
+	 	windmill.controller.continueLoop(); 
+	}       
+ 	windmill.loadTimeoutId = setTimeout('ct()', windmill.timeout); 
+  
+  return true;
+}
---
+++ packages/windmill/windmill-0.4.3-py2.5.egg/windmill/html/js/wm/registry.js
@@ -0,0 +1,60 @@
+/*
+Copyright 2006-2007, Open Source Applications Foundation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+windmill.registry = new function () {
+  this.methods  = {};
+  this.locator = [];
+  this.option  = [];
+};
+
+//Setup all the current methods supported
+windmill.registry.locator.push('id','link','xpath','jsid', 'name');
+windmill.registry.option.push('text','url','option','validator','destination','stopOnFailure', 'milliseconds', 'timeout','js', 'status','domain');
+
+//Setup method registry
+windmill.registry.methods['open']                = {'locator': false, 'option': 'url' };
+windmill.registry.methods['waits.sleep']         = {'locator': false, 'option': 'milliseconds' };
+windmill.registry.methods['waits.forElement']    = {'locator': true, 'option': 'timeout' };
+windmill.registry.methods['waits.forNotElement']    = {'locator': true, 'option': 'timeout' };
+windmill.registry.methods['waits.forPageLoad']    = {'locator': false, 'option': 'timeout' };
+windmill.registry.methods['click']               = {'locator': true, 'option': false };
+windmill.registry.methods['doubleClick']         = {'locator': true, 'option': false};
+windmill.registry.methods['mousedown']           = {'locator': true, 'option': false};
+windmill.registry.methods['mouseup']             = {'locator': true, 'option': false};
+windmill.registry.methods['mouseout']            = {'locator': true,'option': false};
+windmill.registry.methods['mouseover']           = {'locator': true,'option': false};
+windmill.registry.methods['type']                = {'locator': true, 'option': 'text'};
+windmill.registry.methods['radio']               = {'locator': true, 'option': false };
+windmill.registry.methods['check']               = {'locator': true, 'option': false };
+windmill.registry.methods['select']              = {'locator': true, 'option': 'option'};
+windmill.registry.methods['goBack']             = {'locator': false, 'option': false };
+windmill.registry.methods['goForward']          = {'locator': false, 'option': false };
+windmill.registry.methods['refresh']            = {'locator': false, 'option': false };
+windmill.registry.methods['setTestWindow']       = {'locator': false, 'option': 'path'};
+windmill.registry.methods['setOptions']         = {'locator': false, 'option':'stopOnFailure'};
+windmill.registry.methods['reWriteAlert']       = {'locator': false, 'option': false };
+windmill.registry.methods['storeURL']           = {'locator': true, 'option': false };
+windmill.registry.methods['storeVarFromJS']     = {'locator': false, 'option': 'options' };
+windmill.registry.methods['setDomain']           = {'locator': false, 'option': 'domain' };
+windmill.registry.methods['complex']            = {'locator': false, 'option': false };
+windmill.registry.methods['asserts.assertJS']    = {'locator': false, 'option': 'js' };
+windmill.registry.methods['asserts.assertProperty'] = {'locator': true, 'option': 'validator' };
+windmill.registry.methods['asserts.assertText']     = {'locator': true, 'option': 'validator' };
+windmill.registry.methods['asserts.assertValue']    = {'locator': true, 'option': 'validator' };
+windmill.registry.methods['asserts.assertChecked']  = {'locator': true, 'option': false };
+windmill.registry.methods['asserts.assertSelected'] = {'locator': true, 'option': 'validator' } ;
+windmill.registry.methods['asserts.assertNode']     = {'locator': true, 'option': false };
+windmill.registry.methods['asserts.assertImageLoaded']  = {'locator': true, 'option': false };
