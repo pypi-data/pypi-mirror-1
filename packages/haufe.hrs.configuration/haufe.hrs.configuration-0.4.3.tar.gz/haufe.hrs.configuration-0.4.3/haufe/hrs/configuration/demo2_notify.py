c0338ca774fcd795237efb2d12dd4d8f040d9162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package haufe.hrs.configuration/0.4.3/haufe.hrs.configuration-0.4.3.tar.gz

---
+++ packages/haufe.hrs.configuration/haufe.hrs.configuration-0.4.3.tar.gz/haufe.hrs.configuration-0.4.3/haufe/hrs/configuration/demo2_notify.py
@@ -0,0 +1,31 @@
+#################################################################
+# haufe.hrs.configuration - a pseudo-hierarchical configuration
+# management infrastructure
+# (C) 2008, Haufe Mediengruppe, Freiburg
+#################################################################
+
+
+if __name__ == '__main__':
+    from haufe.hrs.configuration import ConfigurationService
+
+    service = ConfigurationService(watch=True)
+    service.registerModel('example/model')
+    service.loadConfiguration('example/sample_config/all-in-one.ini')
+
+    # keep artificially the main thread alive forever
+    while True:
+        print service.getConfiguration()
+        try:
+            import time
+            time.sleep(5)
+        except KeyboardInterrupt:
+            # ...until c^c signal
+            print 'stop monitoring...'
+            # stop monitoring
+            if service.notifier:
+                service.notifier.stop()
+            print 'done'
+            break
+        except Exception, err:
+            # otherwise keep on looping
+            print err
