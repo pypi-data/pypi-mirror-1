65bf55d9d3967c448ff7e6eebca820b7a2a608d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:10 +0000

    Add package kss.plugin.timer/0.1.1/kss.plugin.timer-0.1.1.tar.gz

---
+++ packages/kss.plugin.timer/kss.plugin.timer-0.1.1.tar.gz/kss.plugin.timer-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kss.plugin.timer',
+    version="0.1.1",
+    #description="",
+    author="Guido Wesdorp and Zest software",
+    author_email="info@zestsoftware.nl",
+    url="http://pypi.python.org/pypi/kss.plugin.timer",
+    install_requires=["kss.base", "setuptools"],
+    packages=find_packages(),
+    namespace_packages=['kss', 'kss.plugin'],
+    include_package_data=True,
+    test_suite = 'kss.plugin.timer.tests.test_suite',
+    entry_points={
+        'kss.plugin': [
+            'kss.plugin.timer=kss.plugin.timer.config:Kss.plugin.timer'
+            ],
+        },
+)
