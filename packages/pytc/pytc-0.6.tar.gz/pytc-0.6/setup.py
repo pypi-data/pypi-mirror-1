7f9904bdc2b58a6e5f2cb048bfbd209df9a84b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:04 +0000

    Add package pytc/0.6/pytc-0.6.tar.gz

---
+++ packages/pytc/pytc-0.6.tar.gz/pytc-0.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+from distutils.core import setup, Extension
+
+if sys.version_info < (2, 3):
+  raise Error, "Python 2.3 or later is required"
+
+include_dirs = []
+library_dirs = ['/usr/local/lib']
+
+if sys.platform == 'darwin':
+  # darwin ports
+  include_dirs.append('/opt/local/include')
+  library_dirs.append('/opt/local/lib')
+  # fink
+  include_dirs.append('/sw/include')
+  library_dirs.append('/sw/lib')
+
+ext = Extension('pytc',
+                libraries = ['tokyocabinet', 'z', 'pthread', 'm', 'c'],
+                sources = ['pytc.c'],
+                include_dirs = include_dirs,
+                library_dirs = library_dirs,
+               )
+
+setup(name = 'pytc',
+      version = '0.6',
+      description = 'Tokyo Cabinet Python bindings',
+      long_description = '''
+        Tokyo Cabinet Python bindings
+      ''',
+      license='BSD',
+      author = 'Tasuku SUENAGA',
+      author_email = 'gunyarakun@sourceforge.jp',
+      ext_modules = [ext]
+     )
