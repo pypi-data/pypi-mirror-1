67cb5b54b57c0d004f2ac6c706032407d77cd5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:52 +0000

    Add package plone.app.z3cform/0.4.7/plone.app.z3cform-0.4.7.tar.gz

---
+++ packages/plone.app.z3cform/plone.app.z3cform-0.4.7.tar.gz/plone.app.z3cform-0.4.7/plone/app/z3cform/templates.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+plone.app.z3cform
+
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+
+$Id: templates.py 22262 2008-08-18 21:20:25Z optilude $
+"""
+
+import os.path
+
+import plone.z3cform.interfaces
+import plone.z3cform.templates
+
+import zope.app.pagetemplate.viewpagetemplatefile
+
+from Products.CMFDefault.interfaces import ICMFDefaultSkin
+
+import plone.app.z3cform
+path = lambda p: os.path.join(os.path.dirname(plone.app.z3cform.__file__), p)
+
+layout_factory = plone.z3cform.templates.ZopeTwoFormTemplateFactory(
+    path('layout.pt'),
+    form=plone.z3cform.interfaces.IFormWrapper,
+    request=ICMFDefaultSkin)
+
+class Macros(plone.z3cform.templates.Macros):
+    template = zope.app.pagetemplate.viewpagetemplatefile.ViewPageTemplateFile(
+        'macros.pt')
+
+    def __getitem__(self, key):
+        return self.template.macros[key]
---
+++ packages/plone.app.z3cform/plone.app.z3cform-0.4.7.tar.gz/plone.app.z3cform-0.4.7/plone/app/z3cform/tests/example.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+plone.app.z3cform
+
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+
+$Id: example.py 22031 2008-08-06 18:30:52Z csenger $
+"""
+from zope import interface, schema
+from z3c.form import form, field, button, group
+from plone.app.z3cform.layout import FormWrapper
+
+
+class MySchema(interface.Interface):
+    age = schema.Int(title=u"Age")
+
+
+class MyForm(form.Form):
+    fields = field.Fields(MySchema)
+    label = u"Please enter your age"
+    ignoreContext = True # don't use context to get widget data
+
+    @button.buttonAndHandler(u'Apply')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+
+
+class MyFormWrapper(FormWrapper):
+    form = MyForm
+
+
+class MyGroupSchema(interface.Interface):
+    name = schema.TextLine(title=u"name")
+
+
+class MyGroup(group.Group):
+    label = u'Secondary Group'
+    fields = field.Fields(MyGroupSchema)
+
+    
+class MyGroupForm(group.GroupForm, form.Form):
+    fields = field.Fields(MySchema)
+    label = u"Please enter your age and Name"
+    ignoreContext = True # don't use context to get widget data
+
+    groups = [MyGroup, ]
+
+    @button.buttonAndHandler(u'Apply')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+
+
+class MyGroupFormWrapper(FormWrapper):
+    form = MyGroupForm
+
