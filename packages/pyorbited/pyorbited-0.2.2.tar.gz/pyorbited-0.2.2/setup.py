1acaadeb16a6e201cd08a013c2652d2b853e0385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:10 +0000

    Add package pyorbited/0.2.2/pyorbited-0.2.2.tar.gz

---
+++ packages/pyorbited/pyorbited-0.2.2.tar.gz/pyorbited-0.2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='pyorbited',
+    version='0.2.2',
+    author='Michael Carter',
+    author_email='CarterMichael@gmail.com',
+    license='MIT License',
+    url="http://www.orbited.org",
+    download_url="http://www.orbited.org/download.html",
+    description='A python client for Orbited (Orbit Event Daemon), a COMET server. Includes three implementations: pyevent, twisted, basic sockets.',
+    packages=[
+        'pyorbited',
+    ],
+    install_requires=[
+        "demjson >= 1.1"
+    ],
+    zip_safe = False,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
