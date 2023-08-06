904053b387191af24e5ac5cd8cf07b57eb2ddd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:32 +0000

    Add package zope.principalregistry/3.7.0/zope.principalregistry-3.7.0.tar.gz

---
+++ packages/zope.principalregistry/zope.principalregistry-3.7.0.tar.gz/zope.principalregistry-3.7.0/src/zope/principalregistry/tests/test_doc.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+# Copyright (c) 2001-2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests for the principal registry ZCML directives
+
+$Id: test_doc.py 98046 2009-03-13 20:12:47Z nadako $
+"""
+import unittest
+from zope.testing import doctest
+from zope.component.testing import setUp as setUpComponent 
+from zope.component.testing import tearDown as tearDownComponent
+from zope.configuration import xmlconfig 
+from zope.password.testing import setUpPasswordManagers
+
+def setUp(test=None):
+    setUpComponent()
+    setUpPasswordManagers()
+
+def tearDown(test=None):
+    tearDownComponent()
+
+def zcml(s):
+    import zope.principalregistry
+    context = xmlconfig.file('meta.zcml', zope.principalregistry)
+    xmlconfig.string(s, context)
+
+def reset():
+    tearDown()
+    setUp()
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('../README.txt',
+            setUp=setUp, globs={'zcml': zcml, 'reset': reset}),
+        ))
