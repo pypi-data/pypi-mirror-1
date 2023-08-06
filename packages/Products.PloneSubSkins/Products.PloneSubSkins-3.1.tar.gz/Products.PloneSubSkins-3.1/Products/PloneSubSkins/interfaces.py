60643b10d00435f218d4a25304e08d4013344be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:34 +0000

    Add package Products.PloneSubSkins/3.1/Products.PloneSubSkins-3.1.tar.gz

---
+++ packages/Products.PloneSubSkins/Products.PloneSubSkins-3.1.tar.gz/Products.PloneSubSkins-3.1/Products/PloneSubSkins/interfaces.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+
+##code-section HEAD
+##/code-section HEAD
+
+class ISubSkinsTool(Interface):
+    """Marker interface for .SubSkinsTool.SubSkinsTool
+    """
+
+##code-section FOOT
+##/code-section FOOT
