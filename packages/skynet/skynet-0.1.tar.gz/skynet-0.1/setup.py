affebcdb4b4f0b52f87bbf89ad1416c95183aa80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:42 +0000

    Add package skynet/0.1/skynet-0.1.tar.gz

---
+++ packages/skynet/skynet-0.1.tar.gz/skynet-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='skynet',
+      version='0.1',
+      py_modules=['skynet'],
+
+      description="A tool for creating sentient systems.",
+
+      author='Michael Gundlach',
+      author_email='gundlach@gmail.com',
+      url='http://xkcd.com/521',
+      keywords = "skynet xkcd",
+
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          ]
+     )
