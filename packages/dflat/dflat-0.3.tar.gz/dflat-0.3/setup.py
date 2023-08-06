f3ca8d20717eb6a3e5b7ab98374f9d6c551e0024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:27 +0000

    Add package dflat/0.3/dflat-0.3.tar.gz

---
+++ packages/dflat/dflat-0.3.tar.gz/dflat-0.3/setup.py
@@ -0,0 +1,18 @@
+# bootstrap easy_install
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dflat',
+    version = '0.3',
+    description = "a command line tool for working with dflat digital preservation file systems",
+    author = "Ed Summers",
+    author_email = "ehs@pobox.com",
+    url = "http://github.com/edsu/dflat",
+    py_modules = ['dflat', 'ez_setup'],
+    test_suite = 'test',
+    scripts = ['bin/dflat'],
+    install_requires = ['namaste'],
+)
