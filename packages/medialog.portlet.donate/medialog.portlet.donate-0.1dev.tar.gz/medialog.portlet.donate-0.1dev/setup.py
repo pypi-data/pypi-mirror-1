74fd90ef74d33f85e1e6f824d5c9ef7da70bb521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:36 +0000

    Add package medialog.portlet.donate/0.1dev/medialog.portlet.donate-0.1dev.tar.gz

---
+++ packages/medialog.portlet.donate/medialog.portlet.donate-0.1dev.tar.gz/medialog.portlet.donate-0.1dev/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='medialog.portlet.donate',
+      version=version,
+      description="A simple donate button",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Espen Moe-Nilssen',
+      author_email='espen@medialog.no',
+      url='http://medialog.no',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['medialog', 'medialog.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
