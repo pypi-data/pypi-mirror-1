9171c64dcf8e606cddbfc5d1fb386a76edba0a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:12 +0000

    Add package tw.yui/0.1a0/tw.yui-0.1a0-py2.5.egg

---
+++ packages/tw.yui/tw.yui-0.1a0-py2.5.egg/tw/yui/widgets.py
@@ -0,0 +1,13 @@
+from tw.api import Widget, JSLink, CSSLink
+
+from yuitest import *
+
+# declare your static resources here
+
+## JS dependencies can be listed at 'javascript' so they'll get included
+## before
+# my_js = JSLink(modname=__name__,
+#                filename='static/yui.js', javascript=[])
+
+# my_css = CSSLink(modname=__name__, filename='static/yui.css')
+
