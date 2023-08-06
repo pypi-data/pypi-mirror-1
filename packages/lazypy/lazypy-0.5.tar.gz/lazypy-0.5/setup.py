741b5887afad8700afed637ebe54f891c1a01ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:28 +0000

    Add package lazypy/0.5/lazypy-0.5.tar.gz

---
+++ packages/lazypy/lazypy-0.5.tar.gz/lazypy-0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from lazypy import __version__
+
+setup(
+    version = __version__,
+    description = "Lazy Evaluation for Python",
+    author = "Georg Bauer",
+    author_email = "gb@rfc1437.de",
+    url = "http://bitbucket.org/rfc1437/lazypy/",
+    name='lazypy', 
+    long_description=file("README").read(),
+    license='MIT/X',
+    platforms=['BSD','Linux','MacOS X', 'win32'],
+    packages=['lazypy', 'LazyEvaluation'],
+    scripts=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    test_suite="lazypy.tests",
+)
