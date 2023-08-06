b0b0425b7b19724ed245842f84eceb67fb988200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:18 +0000

    Add package experimental.catalogqueryplan/2.0/experimental.catalogqueryplan-2.0.zip

---
+++ packages/experimental.catalogqueryplan/experimental.catalogqueryplan-2.0.zip/experimental.catalogqueryplan-2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = '2.0'
+
+setup(name='experimental.catalogqueryplan',
+      version=version,
+      description="Static query optimized with one plan",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone catalog search',
+      author='Jarn AS',
+      author_email='info@jarn.com',
+      url='http://www.jarn.com/',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['experimental'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+)
