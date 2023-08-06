b7599674ad0e90294b2386e7bb15f085369e6acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package cmemcache_hash/0.1/cmemcache_hash-0.1.tar.gz

---
+++ packages/cmemcache_hash/cmemcache_hash-0.1.tar.gz/cmemcache_hash-0.1/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="cmemcache_hash", version="0.1",
+    url="http://lericson.se/",
+    author="Ludvig Ericson", author_email="ludvig@blogg.se",
+    description="cmemcache-style CRC32 hashing for python-memcached.",
+    py_modules=["cmemcache_hash"])
