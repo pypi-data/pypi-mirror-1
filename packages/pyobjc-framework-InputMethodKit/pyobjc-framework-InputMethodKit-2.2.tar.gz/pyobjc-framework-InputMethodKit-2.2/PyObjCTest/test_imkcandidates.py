a68b176cb1d46b5829f53ce2cf4939e53fc49da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:14 +0000

    Add package pyobjc-framework-InputMethodKit/2.2/pyobjc-framework-InputMethodKit-2.2.tar.gz

---
+++ packages/pyobjc-framework-InputMethodKit/pyobjc-framework-InputMethodKit-2.2.tar.gz/pyobjc-framework-InputMethodKit-2.2/PyObjCTest/test_imkcandidates.py
@@ -0,0 +1,29 @@
+
+from PyObjCTools.TestSupport import *
+from InputMethodKit import *
+
+class TestIMKCandidates (TestCase):
+    def testConstants(self):
+        self.failUnlessEqual(kIMKSingleColumnScrollingCandidatePanel, 1)
+        self.failUnlessEqual(kIMKScrollingGridCandidatePanel, 2)
+        self.failUnlessEqual(kIMKSingleRowSteppingCandidatePanel, 3)
+
+        self.failUnlessEqual(kIMKLocateCandidatesAboveHint, 1)
+        self.failUnlessEqual(kIMKLocateCandidatesBelowHint, 2)
+        self.failUnlessEqual(kIMKLocateCandidatesLeftHint, 3)
+        self.failUnlessEqual(kIMKLocateCandidatesRightHint, 4)
+
+        self.failUnlessIsInstance(IMKCandidatesOpacityAttributeName, unicode)
+
+    @min_os_level('10.6')
+    def testConstants10_6(self):
+        self.failUnlessIsInstance(IMKCandidatesSendServerKeyEventFirst, unicode)
+
+    def testMethods(self):
+        self.failUnlessResultIsBOOL(IMKCandidates.isVisible)
+        self.failUnlessResultIsBOOL(IMKCandidates.dismissesAutomatically)
+        self.failUnlessArgIsBOOL(IMKCandidates.setDismissesAutomatically_, 0)
+
+
+if __name__ == "__main__":
+    main()
