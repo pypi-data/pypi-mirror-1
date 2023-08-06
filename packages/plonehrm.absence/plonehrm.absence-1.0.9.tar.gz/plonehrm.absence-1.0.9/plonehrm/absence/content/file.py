27680e19ce9210295a4f655f99591dab0b3c98d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:41 +0000

    Add package plonehrm.absence/1.0.9/plonehrm.absence-1.0.9.tar.gz

---
+++ packages/plonehrm.absence/plonehrm.absence-1.0.9.tar.gz/plonehrm.absence-1.0.9/plonehrm/absence/content/file.py
@@ -0,0 +1,39 @@
+from DateTime import DateTime
+from Products.Archetypes import atapi
+from Products.ATContentTypes.content.file import ATFile
+from Products.ATContentTypes.content.file import ATFileSchema
+from Products.ATContentTypes.content import schemata
+
+from plonehrm.absence.config import PROJECTNAME
+from plonehrm.absence import AbsenceMessageFactory as _
+
+AbsenceFileSchema = ATFileSchema.copy() + atapi.Schema((
+        atapi.DateTimeField(
+            name='fileDate',
+            required=True,
+            default_method=DateTime,
+            widget=atapi.CalendarWidget(
+                label=_(u'label_file_date', default=u'File date'),
+                show_hm = False,
+                )
+            ),
+
+        ))
+
+AbsenceFileSchema['title'].storage = atapi.AnnotationStorage()
+AbsenceFileSchema['description'].storage = atapi.AnnotationStorage()
+schemata.finalizeATCTSchema(AbsenceFileSchema)
+
+
+class AbsenceFile(ATFile):
+
+    schema = AbsenceFileSchema
+    meta_type = "AbsenceFile"
+
+    title = atapi.ATFieldProperty('title')
+    description = atapi.ATFieldProperty('description')
+    file_date = atapi.ATDateTimeFieldProperty('fileDate')
+    main_date = atapi.ATDateTimeFieldProperty('fileDate')
+
+
+atapi.registerType(AbsenceFile, PROJECTNAME)
