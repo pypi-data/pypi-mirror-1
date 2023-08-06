857a5f0dbbb8987a8d9b5db9e3123ffa8d12437c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:08 +0000

    Add package mocktest/0.3.1/mocktest-0.3.1.tar.gz

---
+++ packages/mocktest/mocktest-0.3.1.tar.gz/mocktest-0.3.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import *
+
+setup(
+	name='mocktest',
+	version='0.3.1',
+	description='a mocking and expectation test library for python, inspired by rspec',
+	author='Tim Cuthbertson',
+	author_email='tim3d.junk+mocktest@gmail.com',
+	url='http://github.com/gfxmonk/mocktest/tree',
+	packages=find_packages(exclude=["test"]),
+	
+	long_description="""\
+	mocktest is a mocking and expectation test library for python.
+	It is similar to rspec's should_receive and associated matchers, and
+	offers a compatible unittest TestCase base class for automatically
+	verifying expectations.
+	""",
+	classifiers=[
+		"License :: OSI Approved :: BSD License",
+		"Programming Language :: Python",
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Software Development :: Testing",
+	],
+	keywords='test mock expect expectation stub rspec unittest',
+	license='BSD',
+	install_requires=[
+		'setuptools',
+	],
+)
