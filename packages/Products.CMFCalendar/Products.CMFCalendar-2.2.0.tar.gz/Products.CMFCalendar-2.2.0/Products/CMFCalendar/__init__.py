5b14ed52def60431a3beaa0a6ba46322c1903652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:50 +0000

    Add package Products.CMFCalendar/2.2.0/Products.CMFCalendar-2.2.0.tar.gz

---
+++ packages/Products.CMFCalendar/Products.CMFCalendar-2.2.0.tar.gz/Products.CMFCalendar-2.2.0/Products/CMFCalendar/__init__.py
@@ -0,0 +1,42 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" CMF Calendar product.
+
+$Id: __init__.py 94393 2008-12-27 22:02:51Z tseaver $
+"""
+
+def initialize(context):
+
+    from Products.CMFCore.utils import ContentInit
+    from Products.CMFCore.utils import ToolInit
+
+    import Event
+    import CalendarTool
+    from permissions import AddPortalContent
+
+
+    # Make sure security is initialized
+    import utils
+
+    ToolInit( 'CMF Calendar Tool'
+            , tools=(CalendarTool.CalendarTool,)
+            , icon='tool.gif'
+            ).initialize( context )
+
+    # BBB: register oldstyle constructors
+    ContentInit( 'CMF Calendar Content'
+               , content_types=()
+               , permission=AddPortalContent
+               , extra_constructors=(Event.addEvent,)
+               , visibility=None
+               ).initialize( context )
---
+++ packages/Products.CMFCalendar/Products.CMFCalendar-2.2.0.tar.gz/Products.CMFCalendar-2.2.0/Products/CMFCalendar/browser/tests.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" Unit tests for browser module.
+
+$Id: tests.py 104367 2009-09-18 22:43:32Z tseaver $
+"""
+
+import unittest
+from Testing import ZopeTestCase
+from Products.Five.schema import Zope2VocabularyRegistry
+
+from Products.CMFCalendar.testing import FunctionalLayer
+
+def _setupVocabulary(ztc):
+    from zope.schema.vocabulary import setVocabularyRegistry
+    setVocabularyRegistry(Zope2VocabularyRegistry())
+
+def _clearVocabulary(ztc):
+    from zope.schema.vocabulary import _clear
+    _clear()
+    
+
+def test_suite():
+    suite = unittest.TestSuite()
+    s = ZopeTestCase.FunctionalDocFileSuite('event.txt',
+                                            setUp=_setupVocabulary,
+                                            tearDown=_clearVocabulary,
+                                           )
+    s.layer = FunctionalLayer
+    suite.addTest(s)
+    return suite
+
+if __name__ == '__main__':
+    from Products.CMFCore.testing import run
+    run(test_suite())
