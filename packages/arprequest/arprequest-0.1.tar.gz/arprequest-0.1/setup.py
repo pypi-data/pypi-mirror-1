c48a7692f769e41bfe6461abe5f3bbb93b9f02f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:57 +0000

    Add package arprequest/0.1/arprequest-0.1.tar.gz

---
+++ packages/arprequest/arprequest-0.1.tar.gz/arprequest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='arprequest',
+	version='0.1',
+	description='A class which send an ARP Request to know if a host is online.',
+	keywords='arp network',
+	author='Antoine Millet',
+	author_email='antoine@inaps.org',
+	license='WTFPL',
+	packages=['arprequest'],
+)
