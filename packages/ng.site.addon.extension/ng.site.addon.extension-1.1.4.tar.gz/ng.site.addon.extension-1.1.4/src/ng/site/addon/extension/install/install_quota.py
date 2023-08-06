0fed111bd7e7d90f2d9e1072e6be9e57e39dabca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:11 +0000

    Add package ng.site.addon.extension/1.1.4/ng.site.addon.extension-1.1.4.tar.gz

---
+++ packages/ng.site.addon.extension/ng.site.addon.extension-1.1.4.tar.gz/ng.site.addon.extension-1.1.4/src/ng/site/addon/extension/install/install_quota.py
@@ -0,0 +1,27 @@
+### -*- coding: utf-8 -*- #############################################
+#######################################################################
+"""installObjectQueue script for the Zope 3 based
+ng.site.content.install package
+
+$Id: install_quota.py 52146 2008-12-23 13:58:08Z cray $
+"""
+__author__  = "Andrey Orlov, 2008"
+__license__ = "GPL"
+__version__ = "$Revision: 52146 $"
+
+from zope.app.file.interfaces import IImage
+from zope.app.file.image import Image
+from ng.app.quota.interfaces import IQuota
+from ng.app.quota.quota import Quota
+
+def installQuota(context, **kw):
+    """ Создаёт в сайт-менеджере Quota и регистрирует её под
+        необходимым интерфейсом
+    """
+    sm = context.getSiteManager()
+    
+    quota = sm[u'quota'] = Quota()
+    
+    sm.registerUtility(quota, provided=IQuota)
+
+    return "Success"
