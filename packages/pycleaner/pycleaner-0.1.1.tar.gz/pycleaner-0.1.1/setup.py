dc8bcefed5a73af3f81fcd6e29bd3859ebd94899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:56 +0000

    Add package pycleaner/0.1.1/pycleaner-0.1.1.tar.gz

---
+++ packages/pycleaner/pycleaner-0.1.1.tar.gz/pycleaner-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+
+setup(
+    name='pycleaner',
+    version='0.1.1',
+    author='Michael Carter',
+    author_email='CarterMichael@gmail.com',
+    license='MIT License',
+    description='Remove ~ and .pyc recursively',
+    long_description='',
+    packages= find_packages(),
+    zip_safe = False,
+    install_requires = [
+        # "event >= 0.3"
+    ],
+    
+    entry_points = '''    
+        [console_scripts]
+        pycleaner = pycleaner.clean:main
+    ''',
+    
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],        
+)
