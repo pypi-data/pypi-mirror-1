e297bfbd72e3c713bb62d55864d252fdb056c200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:07 +0000

    Add package ZopeSkel/2.2/ZopeSkel-2.2.zip

---
+++ packages/ZopeSkel/ZopeSkel-2.2.zip/ZopeSkel-2.2/zopeskel/archetype.py
@@ -0,0 +1,18 @@
+import copy
+
+from zopeskel.plone import Plone
+from zopeskel.base import var
+
+class Archetype(Plone):
+    _template_dir = 'templates/archetype'
+    summary = 'A Plone project that uses Archetypes'
+    required_templates = ['plone']
+    use_cheetah = True
+
+    vars = copy.deepcopy(Plone.vars)
+    vars.insert(0, var('title',
+                       'The title of the project',
+                       default='Plone Example'))
+
+
+
