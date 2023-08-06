4ea5236e7821dd2574a348005c7609a8faa37504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:26 +0000

    Add package collective.collage.easyslider/1.0.0/collective.collage.easyslider-1.0.0.tar.gz

---
+++ packages/collective.collage.easyslider/collective.collage.easyslider-1.0.0.tar.gz/collective.collage.easyslider-1.0.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0'
+
+setup(name='collective.collage.easyslider',
+      version=version,
+      description="Collage view for content slide.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Rcom',
+      author_email='info@rcom.com.ar',
+      url='http://jpg.rosario.com/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.collage'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'Plone',
+          'Products.Collage',
+          'collective.easyslider',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
