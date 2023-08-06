cd52d5d0aba665f74e032f90a78c3f51f33573ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:07 +0000

    Add package plone.app.discussion/1.0b3/plone.app.discussion-1.0b3.zip

---
+++ packages/plone.app.discussion/plone.app.discussion-1.0b3.zip/plone.app.discussion-1.0b3/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+version = '1.0b3'
+
+setup(name='plone.app.discussion',
+      version=version,
+      description="Enhanced discussion support for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Timo Stollenwerk',
+      author_email='timo@zmag.de',
+      url='http://pypi.python.org/pypi/plone.app.discussion',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.autopermission',
+          'collective.monkeypatcher',
+          'plone.app.layout',
+          'plone.app.registry',
+          'plone.app.z3cform',
+          'plone.indexer',
+          'plone.registry',
+          'plone.z3cform<=0.5.7',
+          'ZODB3',
+          'zope.interface',
+          'zope.component',
+          'zope.annotation',
+          'zope.event',
+          'zope.container',
+          'zope.lifecycleevent',
+          'zope.site',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
