68d18cbea805323b842d454a8c3bd0ace06b8fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:51 +0000

    Add package rum/0.3dev-20090708/rum-0.3dev_20090708-py2.6.egg

---
+++ packages/rum/rum-0.3dev_20090708-py2.6.egg/rum/exceptions.py
@@ -0,0 +1,18 @@
+from routes.util import RoutesException
+from formencode.api import Invalid
+from rum.genericfunctions import AmbiguousMethods, NoApplicableMethods
+
+class RumException(StandardError): pass
+class ConfigError(RumException): pass
+
+class NoTemplateDefined(LookupError, RumException): pass
+class NoRequest(RumException): pass
+class NoComponentRegistered(AttributeError, RumException): pass
+class NoComponentFound(LookupError, RumException): pass
+
+class RepositoryException(RumException): pass
+class BadId(RepositoryException): pass
+class ObjectNotFound(LookupError, RepositoryException): pass
+class InvalidData(Invalid, RumException): pass
+
+class SecurityDenial(RumException): pass
