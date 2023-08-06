86077853a5c3a02248c75614c992159f4c303047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:06 +0000

    Add package tw.mootools/0.1a2/tw.mootools-0.1a2-py2.5.egg

---
+++ packages/tw.mootools/tw.mootools-0.1a2-py2.5.egg/tw/mootools/release.py
@@ -0,0 +1,8 @@
+__PROJECT__ = "tw.mootools"
+__DESCRIPTION__ = "A Toscawidgtets packaging of Moo Tools"
+__VERSION__ = "0.1a2"
+__AUTHOR__ = "Christopher Perkins"
+__EMAIL__ = "chris@percious.com"
+__COPYRIGHT__ = "Copyright 2006-2008 Christopher Perkins and contributors"
+__LICENSE__ = "MIT"
+__URL__ = 'http://twtools.googlecode.com'
