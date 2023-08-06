550d0b8679745ad043cf14e40b480be4f499c308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:39 +0000

    Add package tw.dojo/0.9.8.1/tw.dojo-0.9.8.1.tar.gz

---
+++ packages/tw.dojo/tw.dojo-0.9.8.1.tar.gz/tw.dojo-0.9.8.1/tw/dojo/release.py
@@ -0,0 +1,11 @@
+# Metadata for the widget EGG. Should leave it for the (unimplemented) widget
+# tracker to track it
+
+__PROJECT__ = 'tw.dojo'
+__DESCRIPTION__ = 'Dojo 1.3.2 widget for ToscaWidgets'
+__URL__ = 'http://toscawidgets.org/'
+__VERSION__ = '0.9.8.1'
+__AUTHOR__ = 'Michele Bertoldi and Christopher Perkins'
+__EMAIL__ = "michele@vico10.com"
+__COPYRIGHT__ = "Copyright 2008 Michele Bertoldi"
+__LICENSE__ = 'MIT'
