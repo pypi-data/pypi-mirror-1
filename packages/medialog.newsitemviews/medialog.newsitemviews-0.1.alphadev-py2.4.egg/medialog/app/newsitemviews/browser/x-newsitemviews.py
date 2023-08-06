b73c974483af54eef7d0350515ba68dc13f27924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:44 +0000

    Add package medialog.newsitemviews/0.1.alphadev/medialog.newsitemviews-0.1.alphadev-py2.4.egg

---
+++ packages/medialog.newsitemviews/medialog.newsitemviews-0.1.alphadev-py2.4.egg/medialog/app/newsitemviews/browser/x-newsitemviews.py
@@ -0,0 +1,12 @@
+# zope imports
+from zope.interface import Interface
+from zope.interface import implements
+from zope.interface import alsoProvides, directlyProvides
+import string
+
+# Five imports
+from Products.Five.browser import BrowserView
+
+
+class newsitemviews(BrowserView):
+     pass
---
+++ packages/medialog.newsitemviews/medialog.newsitemviews-0.1.alphadev-py2.4.egg/medialog/app/newsitemviews/config.py
@@ -0,0 +1,24 @@
+from Products.CMFCore.permissions import AddPortalContent
+from Products.CMFCore.utils import getToolByName
+
+from Products.Archetypes.public import DisplayList
+
+
+GLOBALS                 = globals()
+
+PROJECTNAME             = "newsitemviews"
+SKINS_DIR               = 'skins'
+STYLESHEETS             = (
+                                                 
+                          )
+                          
+JAVASCRIPTS             = (
+                                              
+#                           {'id':           'pgv_sg_slightbox.js',                           
+#                           'expression':   "python: here.meta_type in ['ATFolder','ATTopic']"},                       
+                        )
+                        
+EXTERNAL_METHODS        = None
+
+
+NEWSITEMVIEWS = ('newsitem_viewII',)
