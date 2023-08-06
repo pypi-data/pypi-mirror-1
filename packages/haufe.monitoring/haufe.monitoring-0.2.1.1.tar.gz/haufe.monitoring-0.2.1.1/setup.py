baf6e46749b321236786b5eb61d489c31f773d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:48 +0000

    Add package haufe.monitoring/0.2.1.1/haufe.monitoring-0.2.1.1.tar.gz

---
+++ packages/haufe.monitoring/haufe.monitoring-0.2.1.1.tar.gz/haufe.monitoring-0.2.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2.1.1'
+
+setup(name='haufe.monitoring',
+      version=version,
+      description="Zope 2 ZEO Client Watch",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Zope2",
+        "Topic :: System :: Monitoring",
+        ],
+      keywords='',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['haufe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'ipcalc',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
