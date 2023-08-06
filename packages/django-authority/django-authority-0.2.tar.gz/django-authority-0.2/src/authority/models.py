80ab1e6f790e5b375dbff7dd7982cab9797d5e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:25 +0000

    Add package django-authority/0.2/django-authority-0.2.tar.gz

---
+++ packages/django-authority/django-authority-0.2.tar.gz/django-authority-0.2/src/authority/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.contrib.auth.models import User, Group
+from django.utils.translation import ugettext_lazy as _
+
+from authority.managers import PermissionManager
+
+class Permission(models.Model):
+    """
+    A granular permission model, per-object permission in other words.
+    This kind of permission is associated with a user/group and an object
+    of any content type.
+    """
+    codename = models.CharField(_('codename'), max_length=100)
+    content_type = models.ForeignKey(ContentType, related_name="row_permissions")
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+    user = models.ForeignKey(User, null=True, blank=True, related_name='granted_permissions')
+    group = models.ForeignKey(Group, null=True, blank=True)
+    creator = models.ForeignKey(User, null=True, blank=True, related_name='created_permissions')
+
+    objects = PermissionManager()
+
+    def __unicode__(self):
+        return self.codename
+
+    class Meta:
+        verbose_name = _('permission')
+        verbose_name_plural = _('permissions')
+        permissions = (
+            ('change_foreign_permissions', 'Can change foreign permissions'),
+            ('delete_foreign_permissions', 'Can delete foreign permissions'),
+        )
---
+++ packages/django-authority/django-authority-0.2.tar.gz/django-authority-0.2/src/authority/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.db.models.loading import get_model
+from django.utils.translation import ugettext
+from django.template.context import RequestContext
+from django.template import loader
+from django.contrib.auth.decorators import login_required
+
+from authority.models import Permission
+from authority.forms import UserPermissionForm
+from authority.templatetags.permissions import add_url_for_obj
+
+@require_POST
+@login_required
+def add_permission(request, app_label, module_name, pk, extra_context={},
+                   template_name='authority/permission_form.html'):
+    next = request.POST.get('next', '/')
+    codename = request.POST.get('codename', None)
+    if codename is None:
+        return HttpResponseForbidden(next)
+    model = get_model(app_label, module_name)
+    if model is None:
+        return permission_denied(request)
+    obj = get_object_or_404(model, pk=pk)
+    form = UserPermissionForm(data=request.POST, obj=obj,
+                              perm=codename, initial={'codename': codename})
+    if form.is_valid():
+        form.save(request)
+        request.user.message_set.create(
+            message=ugettext('You added a permission.'))
+        return HttpResponseRedirect(next)
+    else:
+        context = {
+            'form': form,
+            'form_url': add_url_for_obj(obj),
+            'next': next,
+            'perm': codename,
+        }
+        context.update(extra_context)
+        return render_to_response(template_name, context,
+                                  context_instance=RequestContext(request))
+
+@login_required
+def delete_permission(request, permission_pk):
+    permission = get_object_or_404(Permission, pk=permission_pk)
+    if request.user.has_perm('delete_foreign_permissions') \
+       or request.user == permission.creator:
+        permission.delete()
+        request.user.message_set.create(
+            message=ugettext('You removed the permission.'))
+    next = request.REQUEST.get('next') or '/'
+    return HttpResponseRedirect(next)
+
+def permission_denied(request, template_name=None, extra_context={}):
+    """
+    Default 403 handler.
+
+    Templates: `403.html`
+    Context:
+        request_path
+            The path of the requested URL (e.g., '/app/pages/bad_page/')
+    """
+    if template_name is None:
+        template_name = ('403.html', 'authority/403.html')
+    context = {
+        'request_path': request.path,
+    }
+    context.update(extra_context)
+    return HttpResponseForbidden(loader.render_to_string(template_name, context,
+                                 context_instance=RequestContext(request)))
