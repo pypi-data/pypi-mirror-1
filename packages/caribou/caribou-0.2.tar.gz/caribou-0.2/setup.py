5acec6a1d7a73d4ea487f75d9052905357b31959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:40 +0000

    Add package caribou/0.2/caribou-0.2.tar.gz

---
+++ packages/caribou/caribou-0.2.tar.gz/caribou-0.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+from setuptools import setup
+
+NAME = 'caribou'
+VERSION = '0.2'
+DESC = "python migrations for sqlite databases"
+LONG_DESC = DESC
+AUTHOR = 'clutchski'
+EMAIL = 'clutchski@gmail.com'
+URL = 'http://github.com/clutchski/caribou'
+LICENSE = 'Public Domain'
+
+setup( name = NAME
+     , version = VERSION
+     , description = DESC
+     , long_description = LONG_DESC
+     , author = AUTHOR
+     , author_email = EMAIL
+     , url = URL
+     , license = LICENSE
+     , platforms = 'any'
+     , py_modules=['caribou']
+     , scripts=['bin/caribou']
+     , install_requires=["argparse>=1.0.0"]
+     , classifiers=\
+         [ 'Development Status :: 4 - Beta'
+         , 'Intended Audience :: Developers'
+         , 'License :: Public Domain'
+         , 'Topic :: Database'
+         , 'Topic :: Software Development :: Version Control'
+         , 'Topic :: Software Development :: Build Tools'
+         , 'Programming Language :: Python :: 2.6'
+         , 'Programming Language :: Python :: 2.5'
+         ]
+     )
