612ba82ea14870ef4ca1a379e7a7951f5ae088ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:08 +0000

    Add package pymongo-bongo/0.1.3/pymongo-bongo-0.1.3.tar.gz

---
+++ packages/pymongo-bongo/pymongo-bongo-0.1.3.tar.gz/pymongo-bongo-0.1.3/mongobongo/document.py
@@ -0,0 +1,295 @@
+# This code is licensed under the New BSD License
+# 2009, Alexander Artemenko <svetlyak.40wt@gmail.com>
+# For other contacts, visit http://aartemenko.com
+
+"""
+This is a helper to work with MongoDB's documents.
+
+Author: Alexander Artemenko <svetlyak.40wt@gmail.com>
+"""
+
+import types
+from pymongo.dbref import DBRef
+from mongobongo.attributed import AttributedDict
+
+_DEFAULT_OPTIONS = dict(
+    ordering = None,
+)
+
+
+class Options(object):
+    """ Document's options.
+    """
+    def __init__(self, meta):
+        self.meta = meta
+        for attr_name, value in _DEFAULT_OPTIONS.iteritems():
+            setattr(self, attr_name, value)
+
+    def contribute_to_class(self, cls, name):
+        cls._meta = self
+
+        if self.meta:
+            meta_attrs = self.meta.__dict__.copy()
+            for name in self.meta.__dict__:
+                if name.startswith('_'):
+                    del meta_attrs[name]
+
+            for attr_name, value in _DEFAULT_OPTIONS.iteritems():
+                setattr(self, attr_name, meta_attrs.pop(attr_name, value))
+
+        del self.meta
+
+
+
+class CollectionManager(object):
+    """This class redefines some methods from pymongo.Collection
+       to wrap results with user's class."""
+
+    __db = None
+
+    def __init__(self, name, cursor_class, document_class):
+        self._collection_name = name
+        self._cursor_class = cursor_class
+        self._document_class = document_class
+
+    @property
+    def _collection(self):
+        return self.__db[self._collection_name]
+
+    @property
+    def collection_name(self):
+        return self._collection_name
+
+    def _get_db(self): return CollectionManager.__db
+    def _set_db(self, db): CollectionManager.__db = db
+    db = property(_get_db, _set_db)
+
+    def remove(self, query = {}):
+        """Removed objects from collection.
+           WARNING, by default, all objects are removed!
+        """
+        self._collection.remove(query)
+
+    def all(self):
+        return self._cursor_class(self._collection.find())
+
+    def find(self, query = {}):
+        return self._cursor_class(self._collection.find(query))
+
+    def find_one(self, query = {}):
+        data = list(self.find(query).limit(1))
+
+        if data:
+            return self._document_class(__kwargs = data[0])
+        return None
+
+    def dereference(self, dbref):
+        doc_cls = get_doc_class_for_collection(dbref.collection)
+        value = self.__db.dereference(dbref)
+        return doc_cls(__kwargs = value)
+
+    def save(self, obj):
+        def transform_docs_to_dbrefs(data):
+            def transform_value(value):
+                if isinstance(value, Document):
+                    if value._id is None:
+                        value.save()
+                    return DBRef(value.objects.collection_name, transform_value(value._id))
+                elif isinstance(value, types.DictType):
+                    return transform_dict(value)
+                elif isinstance(value, types.ListType):
+                    return [transform_value(v) for v in value]
+                return value
+
+            def transform_dict(object):
+                for (key, value) in object.iteritems():
+                    object[key] = transform_value(value)
+                return object
+
+            return transform_dict(data)
+
+        return self._collection.save(transform_docs_to_dbrefs(obj))
+
+
+    def __getattr__(self, name):
+        return getattr(self._collection, name)
+
+
+
+class DocumentBase(type):
+    """Metaclass to create classes which inherit the Document class."""
+
+    def __new__(cls, name, bases, attrs):
+        super_new = super(DocumentBase, cls).__new__
+        parents = [b for b in bases if isinstance(b, DocumentBase)]
+        if not parents:
+            # If this isn't a subclass of Model, don't do anything special.
+            return super_new(cls, name, bases, attrs)
+
+        module = attrs.pop('__module__')
+        new_class = super_new(cls, name, bases, {'__module__': module})
+        attr_meta = attrs.pop('Meta', None)
+
+        if not attr_meta:
+            meta = getattr(new_class, 'Meta', None)
+        else:
+            meta = attr_meta
+
+        new_class.add_to_class('_meta', Options(meta))
+
+        collection = attrs.pop('collection')
+
+        class CursorProxy(object):
+            _doctype = new_class
+
+            def __init__(self, real_cursor):
+                self.__cursor = real_cursor
+
+                if self._doctype._meta.ordering:
+                    self.sort(self._doctype._meta.ordering)
+
+            def next(self):
+                """Wraps result into the custom class"""
+                return self._doctype(__kwargs = self.__cursor.next())
+
+            def sort(self, *args, **kwargs):
+                self.__cursor.sort(*args, **kwargs)
+                return self
+
+            def __getattr__(self, name):
+                return getattr(self.__cursor, name)
+
+            def __getitem__(self, index):
+                """Wraps result into the custom class
+                   if it is one item"""
+                result = self.__cursor.__getitem__(index)
+
+                if isinstance(index, slice):
+                    return self
+                else:
+                    return self._doctype(__kwargs = result)
+
+            def __len__(self):
+                return self.__cursor.count(with_limit_and_skip=True)
+
+            def __iter__(self):
+                return self
+
+        # Set all neccessary methods
+        for key, value in attrs.iteritems():
+            new_class.add_to_class(key, value)
+
+        setattr(new_class, 'objects', CollectionManager(
+            collection,
+            CursorProxy,
+            new_class,
+        ))
+
+        register_doc_class(new_class)
+        return new_class
+
+    def add_to_class(cls, name, value):
+        if hasattr(value, 'contribute_to_class'):
+            value.contribute_to_class(cls, name)
+        else:
+            setattr(cls, name, value)
+
+
+
+class Document(object):
+    """
+    Helper to comfort work with document collections.
+    The most cool feature of this helper is automatic
+    mapping all data to user specified classes.
+    For example, you can define class with custom methods:
+
+    >>> from pymongo.document import Document
+    >>> class Article(Document):
+    ...     collection = 'articles'
+    ...     def get_url(self):
+    ...         return 'http://example.com/blog/%s/' % self.slug
+    >>>
+    >>> # after that, you can use this class to
+    >>> # instantiate objects, save them and fetch from database
+    >>>
+    >>> # first, you need to setup database connection
+    >>> from pymongo.connection import Connection
+    >>> Article.objects.db = Connection('localhost').test_database
+    >>>
+    >>> article = Article(slug = 'first-article',
+    ...                   title = 'First article',
+    ...                   text = 'Blah minor',
+    ...                   tags = ['test', 'short'])
+    >>> article.save()
+    >>>
+    >>> # Now try to search this object by slug
+    >>>
+    >>> obj = article.objects.find({'slug': 'first-article'})
+    >>> assert(obj is not None)
+    >>> assert(obj.slug == 'first-article')
+    >>> assert(obj.get_url() == 'http://example.com/blog/first-article/')
+    """
+
+    __metaclass__ = DocumentBase
+
+    def __init__(self, **kwargs):
+        try:
+            self.__dict__['_data'] = kwargs['__kwargs']
+        except KeyError:
+            self.__dict__['_data'] = kwargs
+
+
+    def __getattr__(self, name):
+        value = self._data.get(name, None)
+
+        if isinstance(value, dict):
+            return AttributedDict(value)
+
+        if isinstance(value, DBRef):
+            value = self.objects.dereference(value)
+            self._data[name] = value
+
+        return value
+
+
+    def __setattr__(self, name, value):
+        self._data[name] = value
+
+
+    def __getitem__(self, name):
+        return self._data[name]
+
+
+    def save(self):
+        self.objects.save(self._data)
+        return self
+
+    def remove(self):
+        self.objects.remove(self._data)
+        return self
+
+    def update(self, data):
+        self._data.update(data)
+
+
+
+class Cache(object):
+    __shared_state = dict(
+        doc_classes = {},
+    )
+
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+
+    def register_doc_class(self, cls):
+        self.doc_classes[cls.objects.collection_name] = cls
+
+    def get_doc_class_for_collection(self, collection_name):
+        return self.doc_classes.get(collection_name, None)
+
+
+
+cache = Cache()
+register_doc_class = cache.register_doc_class
+get_doc_class_for_collection = cache.get_doc_class_for_collection
+
---
+++ packages/pymongo-bongo/pymongo-bongo-0.1.3.tar.gz/pymongo-bongo-0.1.3/test/test_document.py
@@ -0,0 +1,217 @@
+import os
+import unittest
+
+from pdb import set_trace
+from pymongo import DESCENDING, ASCENDING
+from pymongo.connection import Connection
+from pymongo.database import Database
+
+from mongobongo.document import Document, get_doc_class_for_collection
+from mongobongo.attributed import AttributedDict
+
+
+
+def get_connection():
+    host = os.environ.get("DB_IP", "localhost")
+    port = int(os.environ.get("DB_PORT", 27017))
+    return Connection(host, port)
+
+
+class TestDoc(Document):
+    collection = 'test_docs'
+
+    def get_url(self):
+        return 'http://svetlyak.ru'
+
+
+class OrderedDoc(Document):
+    collection = 'test_docs'
+    class Meta:
+        ordering = [('user', ASCENDING)]
+
+
+class Documents(unittest.TestCase):
+    def setUp(self):
+        db = Database(get_connection(), "pymongo_test")
+
+        TestDoc.objects.db = db
+        TestDoc.objects.remove()
+
+    def test_save_and_find(self):
+        TestDoc(author = 'art', tags = ['test', 'python']).save()
+        TestDoc(author = 'vasily', tags = ['test', 'django']).save()
+
+        TestDoc.objects.ensure_index([('author', ASCENDING)])
+        docs = TestDoc.objects.all()
+        self.assertEqual(2, len(docs))
+
+        docs = list(docs)
+        self.assertEqual('art', docs[0].author)
+        self.assertEqual('vasily', docs[1].author)
+
+        self.assertEqual('art', TestDoc.objects.find({'tags': 'python'})[0].author)
+
+    def test_save_returns_object_with_id(self):
+        doc = TestDoc(author = 'art')
+
+        self.assertEqual(None, doc._id)
+        self.assertEqual(doc, doc.save())
+        self.assert_(doc._id)
+
+    def test_custom_method(self):
+        doc = TestDoc(author = 'art')
+        self.assertEqual('http://svetlyak.ru', doc.get_url())
+
+    def test_find_one(self):
+        TestDoc(author = 'art', tags = ['test', 'python']).save()
+        TestDoc(author = 'vasily', tags = ['test', 'django']).save()
+
+        doc = TestDoc.objects.find_one({'author': 'art'})
+        self.assertEqual('art', doc.author)
+
+    def test_update(self):
+        doc = TestDoc(author = 'art')
+        doc.update({'tag': 'test'})
+        self.assertEqual('test', doc.tag)
+
+    def test_find_one_can_return_none(self):
+        doc = TestDoc.objects.find_one({'author': 'unknown'})
+        self.assertEqual(None, doc)
+
+    def test_doc_can_be_subscriptable(self):
+        doc = TestDoc(__kwargs = {'the-title': 'Just a Poem'})
+        self.assertEqual('Just a Poem', doc['the-title'])
+
+    def test_access_to_inner_attributes(self):
+        doc = TestDoc(author = dict(
+            name = 'Alexander',
+            nick = 'svetlyak40wt',
+            address = dict(
+                country = 'Russia',
+                city = 'Moscow')
+        ))
+        self.assertEqual('Alexander', doc.author.name)
+        self.assertEqual('Russia', doc.author.address.country)
+        self.assertEqual(dict(country = 'Russia', city = 'Moscow'), doc.author.address)
+
+    def test_change_attributes(self):
+        doc = TestDoc()
+        doc.author = 'Alexander'
+        doc.article = {}
+        doc.article.title = 'Just a Poem'
+        doc.article.tags = ['one', 'two']
+        doc.save()
+
+        doc = TestDoc.objects.find_one()
+        self.assertEqual('Alexander', doc.author)
+        self.assertEqual(dict(title = 'Just a Poem', tags = ['one', 'two']), doc.article)
+
+    def test_dict_proxy(self):
+        d = dict(title = 'Just an Example', tags = ['test', 'python'])
+        dp = AttributedDict(d)
+        dp.author = dict(name = 'Alexander')
+        dp.author.lastname = 'Artemenko'
+
+        self.assertEqual('Alexander', d['author']['name'])
+        self.assertEqual('Artemenko', d['author']['lastname'])
+
+        del dp.author.lastname
+        self.assertEqual(dict(name='Alexander'), dp.author)
+
+    def test_sort_documents(self):
+        TestDoc(user = 'vasily').save()
+        TestDoc(user = 'alex').save()
+        TestDoc(user = 'zuger').save()
+        TestDoc(user = 'olga').save()
+
+        TestDoc.objects.ensure_index([('user', ASCENDING)])
+
+        docs = list(TestDoc.objects.all().sort([('user', ASCENDING)])[:2])
+
+        self.assertEqual(2, len(docs))
+        self.assertEqual('alex', docs[0].user)
+        self.assertEqual('olga', docs[1].user)
+
+    def test_default_ordering_for_iterator(self):
+        names = ['vasily', 'alex', 'zuger', 'olga']
+
+        for name in names:
+            OrderedDoc(user = name).save()
+
+        for name, doc in zip(sorted(names), OrderedDoc.objects.all()):
+            self.assertEqual(name, doc.user)
+
+    def test_default_ordering_for_single_items(self):
+        names = ['vasily', 'alex', 'zuger', 'olga']
+
+        for name in names:
+            OrderedDoc(user = name).save()
+
+        names = sorted(names)
+        objects = OrderedDoc.objects.all()
+
+        for i in range(len(names)):
+            self.assertEqual(names[i], objects[i].user)
+
+    def test_remove(self):
+        TestDoc(user = 'vasily').save()
+        alex = TestDoc(user = 'alex').save()
+
+        self.assertEqual(2, TestDoc.objects.count())
+
+        alex.remove()
+        self.assertEqual(1, TestDoc.objects.count())
+
+    def test_default_ordering_for_find_one(self):
+        names = ['vasily', 'alex', 'zuger', 'olga']
+
+        for name in names:
+            OrderedDoc(user = name).save()
+
+        self.assertEqual('alex', OrderedDoc.objects.find_one().user)
+
+
+
+
+class Article(Document):
+    collection = 'articles'
+
+
+class Author(Document):
+    collection = 'authors'
+
+
+class References(unittest.TestCase):
+    def setUp(self):
+        db = Database(get_connection(), "pymongo_test")
+
+        Article.objects.db = db
+        Article.objects.remove()
+        Author.objects.remove()
+
+    def testDocClassAutoregistration(self):
+        self.assertEqual(Article, get_doc_class_for_collection('articles'))
+        self.assertEqual(Author, get_doc_class_for_collection('authors'))
+
+
+    def testReferenceObject(self):
+        author = Author(name = 'Alexander')
+        author.save()
+
+        article =  Article(title = 'Life is miracle', author = author)
+        article.save()
+
+        article = Article.objects.find_one()
+
+        self.assertEqual('Alexander', article.author.name)
+        self.assertEqual(Author, type(article.author))
+
+
+    def testSaveReferenceObjectWithParent(self):
+        author = Author(name = 'Alexander')
+        article =  Article(title = 'Life is miracle', author = author)
+
+        self.assert_(author._id is None)
+        article.save()
+        self.assert_(author._id is not None)
+
