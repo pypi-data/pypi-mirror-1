1acf8df5428c10241687c374e42091969ba6402e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:48 +0000

    Add package collective.autorestart/0.1/collective.autorestart-0.1.tar.gz

---
+++ packages/collective.autorestart/collective.autorestart-0.1.tar.gz/collective.autorestart-0.1/collective/autorestart/__init__.py
@@ -0,0 +1,18 @@
+"""
+
+    Product bootstrapper.
+
+"""
+
+__license__ = "GPL"
+__author__  = "Mikko Ohtamaa <mikko.ohtamaa@twinapex.com>"
+__copyright__ = "2009 Twinapex Research"
+__docformat__ = "epytext"
+
+
+import bgthread
+
+bgthread.start_monitor()
+
+
+    
---
+++ packages/collective.autorestart/collective.autorestart-0.1.tar.gz/collective.autorestart-0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.autorestart',
+      version=version,
+      description="Automatically reloads changed code into Plone when you edit Python files",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Plone",
+        "Framework :: Zope2"
+        ],
+      keywords='plone reload development server hotstart',
+      author='Mikko Ohtamaa',
+      author_email='mikko.ohtamaa@twinapex.com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          #'ctypes', # pyinotify dependency, pypi entry broken
+          'pyinotify',
+          'plone.reload'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
