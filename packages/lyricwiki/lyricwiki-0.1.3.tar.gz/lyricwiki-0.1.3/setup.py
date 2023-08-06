49b93b055147525713313b1bcc3fb878f6f802dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:50 +0000

    Add package lyricwiki/0.1.3/lyricwiki-0.1.3.tar.gz

---
+++ packages/lyricwiki/lyricwiki-0.1.3.tar.gz/lyricwiki-0.1.3/setup.py
@@ -0,0 +1,29 @@
+
+from distutils.core import setup
+
+import os
+def get_build():
+	path = "./.build"
+	if os.path.exists(path):
+		fp = open(path, "r")
+		build = eval(fp.read())+1
+		fp.close()
+	else:
+		build = 1
+	
+	fp = open(path, "w")
+	fp.write(str(build))
+	fp.close()
+	
+	return unicode(build)
+	
+setup(
+	name="lyricwiki",
+	version = "0.1." + get_build(),
+	description = "Download lyrics from LyricWiki",
+	license = "gpl3",
+	author = "Amr Hassan",
+	author_email = "amr.hassan@gmail.com",
+	scripts = ["lyricwiki"],
+	py_modules = ["lyricwiki"]
+	)
