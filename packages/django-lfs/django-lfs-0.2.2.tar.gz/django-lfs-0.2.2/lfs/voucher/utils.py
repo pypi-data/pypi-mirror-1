9c5da4a371db2d8867c8b4e3a39dd0d9063d2684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:58 +0000

    Add package django-lfs/0.2.2/django-lfs-0.2.2.tar.gz

---
+++ packages/django-lfs/django-lfs-0.2.2.tar.gz/django-lfs-0.2.2/lfs/voucher/utils.py
@@ -0,0 +1,27 @@
+# python imports
+import random
+
+# lfs imports
+from lfs.voucher.models import VoucherOptions
+
+def create_voucher_number():
+    """
+    """
+    try:
+        options = VoucherOptions.objects.all()[0]
+    except IndexError:
+        letters = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+        length = 5
+        prefix = ""
+        suffix = ""
+    else:
+        letters = options.number_letters
+        length  = options.number_length
+        prefix  = options.number_prefix
+        suffix  = options.number_suffix
+
+    number = ""
+    for i in range(0, length):
+        number += random.choice(letters)
+        
+    return prefix + number + suffix
