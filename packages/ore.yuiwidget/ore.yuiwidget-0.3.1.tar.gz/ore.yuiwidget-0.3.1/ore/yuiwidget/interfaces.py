841171d2dc2b5367b1c37740f1b56f0c376d52b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:18 +0000

    Add package ore.yuiwidget/0.3.1/ore.yuiwidget-0.3.1.tar.gz

---
+++ packages/ore.yuiwidget/ore.yuiwidget-0.3.1.tar.gz/ore.yuiwidget-0.3.1/ore/yuiwidget/interfaces.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2008 Kapil Thangavelu <kapil.foss@gmail.com>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from zope.app.form.interfaces import ConversionError
+
+class IllegalHTML( ConversionError ):
+    """
+    illegal html, possible cross site scripting attempt
+    """
+
+    
+                 
