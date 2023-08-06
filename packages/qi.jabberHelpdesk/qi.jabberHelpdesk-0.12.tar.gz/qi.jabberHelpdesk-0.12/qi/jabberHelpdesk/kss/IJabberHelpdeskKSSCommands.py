a94a09b178410cb6180970e0f0d63f20f50f41fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:36 +0000

    Add package qi.jabberHelpdesk/0.12/qi.jabberHelpdesk-0.12.tar.gz

---
+++ packages/qi.jabberHelpdesk/qi.jabberHelpdesk-0.12.tar.gz/qi.jabberHelpdesk-0.12/qi/jabberHelpdesk/kss/IJabberHelpdeskKSSCommands.py
@@ -0,0 +1,12 @@
+from zope import interface
+
+class IJabberHelpdeskKSSCommands(interface.Interface):
+	"""Livechat KSS commands
+	"""
+
+	def resetScrollbar(selector):
+		"""
+		"""
+	def resetInput(selector):
+		"""
+		"""
