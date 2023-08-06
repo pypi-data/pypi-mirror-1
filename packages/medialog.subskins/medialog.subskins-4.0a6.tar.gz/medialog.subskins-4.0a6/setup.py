3c0c8afc976ab9232be87d7ec4441dc268b3e346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:03 +0000

    Add package medialog.subskins/4.0a6/medialog.subskins-4.0a6.tar.gz

---
+++ packages/medialog.subskins/medialog.subskins-4.0a6.tar.gz/medialog.subskins-4.0a6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '4.0a6'
+
+
+setup(name='medialog.subskins',
+      version=version,
+      description="An installable theme and theming tool for Plone 4 (and maybe 3)",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Espen Moe-Nilssen',
+      author_email='espen@medialog.no',       
+      url='http://svn.plone.org/svn/collective/medialog.subskins',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['medialog'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plonetheme.classic',
+          'Products.PloneSubSkins',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
