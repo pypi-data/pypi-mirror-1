6afcacce473841978ef1173fdcd2225a5416eaf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:56 +0000

    Add package pycryptopp/0.5.14/pycryptopp-0.5.14.tar.gz

---
+++ packages/pycryptopp/pycryptopp-0.5.14.tar.gz/pycryptopp-0.5.14/misc/show-tool-versions.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+import sys
+import subprocess
+
+print "python:", sys.version.replace("\n", " ")
+
+try:
+    out = subprocess.Popen(["buildbot", "--version"],
+                           stdout=subprocess.PIPE).communicate()[0]
+    print "buildbot:", out.replace("\n", " ")
+except EnvironmentError:
+    pass
+
+try:
+    out = subprocess.Popen(["darcs", "--version"],
+                           stdout=subprocess.PIPE).communicate()[0]
+    full = subprocess.Popen(["darcs", "--exact-version"],
+                            stdout=subprocess.PIPE).communicate()[0]
+    print
+    print "darcs:", out.replace("\n", " ")
+    print full.rstrip()
+except EnvironmentError:
+    pass
+
+try:
+    import platform
+    out = platform.platform()
+    print
+    print "platform:", out.replace("\n", " ")
+except EnvironmentError:
+    pass
+
+try:
+    import pkg_resources
+    out = str(pkg_resources.require("setuptools"))
+    print
+    print "setuptools:", out.replace("\n", " ")
+except (ImportError, EnvironmentError):
+    pass
