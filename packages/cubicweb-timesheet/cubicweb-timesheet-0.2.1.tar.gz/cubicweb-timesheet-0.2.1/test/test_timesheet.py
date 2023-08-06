e37353002ea6a82a6cfe30aa58dca8b3232c8bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:01 +0000

    Add package cubicweb-timesheet/0.2.1/cubicweb-timesheet-0.2.1.tar.gz

---
+++ packages/cubicweb-timesheet/cubicweb-timesheet-0.2.1.tar.gz/cubicweb-timesheet-0.2.1/test/test_timesheet.py
@@ -0,0 +1,25 @@
+"""template automatic tests"""
+
+from logilab.common.testlib import TestCase, unittest_main
+
+from cubicweb.devtools.testlib import AutomaticWebTest
+
+from logilab.common.testlib import unittest_main
+from cubicweb.devtools.testlib import AutomaticWebTest
+
+class AutomaticWebTest(AutomaticWebTest):
+
+    def to_test_etypes(self):
+        return set(('Activity', 'Resource', 'Resourcetype'))
+
+    def list_startup_views(self):
+        # XXX needs to specify req.form['project'| 'workcase'] to test
+        # [prj-]acstats views
+        return ('index',) # 'actstats', 'prj-actstats' 
+
+if __name__ == '__main__':
+    unittest_main()
+
+
+if __name__ == '__main__':
+    unittest_main()
