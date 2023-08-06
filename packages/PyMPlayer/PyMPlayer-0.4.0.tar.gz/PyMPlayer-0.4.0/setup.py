f051ef9fe0ff410ee1aa97752f0620eda3cc8b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:02 +0000

    Add package PyMPlayer/0.4.0/PyMPlayer-0.4.0.tar.gz

---
+++ packages/PyMPlayer/PyMPlayer-0.4.0.tar.gz/PyMPlayer-0.4.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# $Id: setup.py 361 2008-01-24 11:01:55Z darwin $
+
+from distutils.core import setup
+
+from pymplayer import __version__
+
+
+setup(
+    name='PyMPlayer',
+    version=__version__,
+    description='Thin, out-of-source wrapper and client/server for MPlayer',
+    author='Darwin M. Bautista',
+    author_email='djclue917@gmail.com',
+    url='http://unplug.eee.upd.edu.ph/pymplayer/',
+    license='GPL & LGPL',
+    py_modules=['pymplayer'],
+    platforms=['POSIX'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console :: Curses',
+        'Environment :: No Input/Output (Daemon)',
+        'Environment :: X11 Applications',
+        'Environment :: X11 Applications :: Gnome',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Internet',
+        'Topic :: Multimedia :: Video :: Display',
+        'Topic :: Software Development :: Libraries :: Python Modules']
+)
