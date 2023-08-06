def9e076c3e9f324da00d50af34bf02b1281be7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:03 +0000

    Add package scikits.samplerate/0.3.1/scikits.samplerate-0.3.1.tar.gz

---
+++ packages/scikits.samplerate/scikits.samplerate-0.3.1.tar.gz/scikits.samplerate-0.3.1/scikits/samplerate/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# Last Change: Thu Mar 26 01:00 AM 2009 J
+
+from info import __doc__
+
+from _samplerate import resample, available_convertors, src_version_str, \
+    convertor_description
+import version
+
+__all__ = filter(lambda s:not s.startswith('_'),dir())
+
+# from numpy.testing import NumpyTest
+# test = NumpyTest().test
+
---
+++ packages/scikits.samplerate/scikits.samplerate-0.3.1.tar.gz/scikits.samplerate-0.3.1/scikits/samplerate/version.py
@@ -0,0 +1,3 @@
+short_version = '0.3.1'
+dev =False
+version = '0.3.1'
