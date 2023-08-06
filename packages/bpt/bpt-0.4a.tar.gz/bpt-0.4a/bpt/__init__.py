d3a6bc6c441c0201b442081f957310ea734dc737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:54 +0000

    Add package bpt/0.4a/bpt-0.4a.tar.gz

---
+++ packages/bpt/bpt-0.4a.tar.gz/bpt-0.4a/bpt/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+'''\
+Common data for the BPT project
+'''
+#*****************************************************************************
+#       Copyright (C) 2009 Giuseppe Ottaviano <giuott@gmail.com>
+#
+#  Distributed under the terms of the GPL License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#*****************************************************************************
+
+__version__ = '0.4a'
+__author__ = 'Giuseppe Ottaviano <giuott@gmail.com>'
+
+import logging
+log = logging.getLogger('BPT') # pylint: disable-msg=C0103
+
+class UserError(Exception):
+    '''Error to be reported to the user'''
