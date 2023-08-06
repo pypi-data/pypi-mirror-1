5d22ef458e8d04e9c18775d5dc3d93340d8175dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:16 +0000

    Add package pisa/3.0.22/pisa-3.0.22.tar.gz

---
+++ packages/pisa/pisa-3.0.22.tar.gz/pisa-3.0.22/sx/pisa3/pisa_version.py
@@ -0,0 +1,16 @@
+# -*- coding: ISO-8859-1 -*-
+#############################################
+## (C)opyright by Dirk Holtwick, 2002-2008 ##
+## All rights reserved                     ##
+#############################################
+
+__reversion__ = "$Revision: 235 $"
+__author__    = "$Author: holtwick $"
+__date__      = "$Date: 2008-06-06 12:56:39 +0200 (Fr, 06 Jun 2008) $"
+__version__   = VERSION = "VERSION{3.0.22}VERSION"[8:-8]
+__build__     = BUILD   = "BUILD{2008-06-06}BUILD"[6:-6]
+
+VERSION_STR = "pisa %s (Build %s)\n(c) Dirk Holtwick <dirk.holtwick@gmail.com>, Germany\nWebsite http:www.htmltopdf.org" % (
+    VERSION,
+    BUILD,
+    )
