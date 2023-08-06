9b9f4c773b83f074297b13f8fecba8f90dd6f8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:50 +0000

    Add package dnuos/1.0b7/dnuos-1.0b7.tar.gz

---
+++ packages/dnuos/dnuos-1.0b7.tar.gz/dnuos-1.0b7/dnuos/cache.py
@@ -0,0 +1,90 @@
+"""A module for caching"""
+
+import os
+import shelve
+
+class PersistentDict(shelve.Shelf, object):
+    """A dict with persistence.
+
+    A wrapper around UpdateTrackingDict that adds the ability to
+    pickle written entries to and from a file. A predicate function
+    can be specified to control what entries to keep from a load to the
+    following save.
+    """
+    def __init__(self, *args, **kwargs):
+        """Construct a new PersistentDict instance.
+
+        This just stores the specified arguments. Call the load method
+        to initialize.
+
+        Arguments:
+            filename  - The persistence data file for loading and
+                        saving.
+        """
+
+        import anydbm
+        super(PersistentDict, self).__init__(
+            anydbm.open(kwargs['filename'], 'c'),
+            protocol=2)
+
+        self.written = []
+        self.version = kwargs.pop('version')
+        old_version = self.pop('__version__', None)
+        if old_version != self.version:
+            self.clear()
+
+    def save(self):
+        """Serialize data to file"""
+
+        for path in self.iterkeys():
+            if path not in self.written and not os.path.isdir(path):
+                del self[path]
+
+        self['__version__'] = self.version
+        self.close()
+
+
+class memoized(object):
+    """A decorator that caches a function's return value each time it's called.
+
+    If called later with the same argument, the cached value is
+    returned, and not re-evaluated.
+
+    The function must only take one argument. This argument is appended to
+    the list in written, so persisted and fresh data can be differentiated.
+
+    Example usage and behavior:
+
+    >>> def fake_dir(path):
+    ...     print 'fake_dir()'
+    ...     return '[dir data]'
+    ...
+    >>> cache = {'/old/dir': '[old dir data]'}
+    >>> written = []
+    >>> fake_dir = memoized(fake_dir, cache, written)
+    >>> fake_dir('/dev/null')
+    fake_dir()
+    '[dir data]'
+    >>> written
+    ['/dev/null']
+    >>> cache['/old/dir']
+    '[old dir data]'
+    >>> fake_dir('/dev/null')
+    '[dir data]'
+    """
+
+    def __init__(self, func, cache, written):
+
+        self.func = func
+        self.cache = cache
+        self.written = written
+
+    def __call__(self, path):
+
+        try:
+            value = self.cache[path]
+        except KeyError:
+            value = self.func(path)
+            self.cache[path] = value
+        self.written.append(path)
+        return value
