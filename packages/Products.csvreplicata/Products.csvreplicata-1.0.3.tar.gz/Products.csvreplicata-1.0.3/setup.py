6a9d1b7c7f50426f03906c8370cf24dbe9560c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:22 +0000

    Add package Products.csvreplicata/1.0.3/Products.csvreplicata-1.0.3.tar.gz

---
+++ packages/Products.csvreplicata/Products.csvreplicata-1.0.3.tar.gz/Products.csvreplicata-1.0.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.3'
+
+setup(name='Products.csvreplicata',
+      version=version,
+      description="CSV import/export for Archetypes",
+      long_description='\n'.join(
+        open(os.path.join(*path)).read() for path in [
+            ("README.txt",),
+            ("docs", "HISTORY.txt")]),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='archetypes, import/export, csv, replication, synchronisation',
+      author='Eric BREHAULT',
+      author_email='eric.brehault@makina-corpus.org',
+      url='http://plone.org/products/csvreplicata',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
