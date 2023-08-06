df97a51f0f2d520f99076a68785cdcda91b46c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:18 +0000

    Add package ZODB3/3.9.0a1/ZODB3-3.9.0a1.tar.gz

---
+++ packages/ZODB3/ZODB3-3.9.0a1.tar.gz/ZODB3-3.9.0a1/src/ZODB/tests/util.py
@@ -0,0 +1,63 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Conventience function for creating test databases
+
+$Id: util.py 92660 2008-10-28 21:33:18Z jim $
+"""
+
+from ZODB.MappingStorage import DB
+
+import os
+import tempfile
+import time
+import unittest
+import persistent
+import transaction
+import zope.testing.setupstack
+
+def setUp(test, name='test'):
+    transaction.abort()
+    d = tempfile.mkdtemp(prefix=name)
+    zope.testing.setupstack.register(test, zope.testing.setupstack.rmtree, d)
+    zope.testing.setupstack.register(
+        test, setattr, tempfile, 'tempdir', tempfile.tempdir)
+    tempfile.tempdir = d
+    zope.testing.setupstack.register(test, os.chdir, os.getcwd())
+    os.chdir(d)
+    zope.testing.setupstack.register(test, transaction.abort)
+
+tearDown = zope.testing.setupstack.tearDown
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.globs = {}
+        name = self.__class__.__name__
+        mname = getattr(self, '_TestCase__testMethodName', '')
+        if mname:
+            name += '-' + mname
+        setUp(self, name)
+
+    tearDown = tearDown
+
+def pack(db):
+    db.pack(time.time()+1)
+
+class P(persistent.Persistent):
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'P(%s)' % self.name
