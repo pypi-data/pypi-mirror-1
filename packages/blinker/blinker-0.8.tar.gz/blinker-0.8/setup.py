dd788101c47aa08497eca34664fd6fbc65f91129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:02 +0000

    Add package blinker/0.8/blinker-0.8.tar.gz

---
+++ packages/blinker/blinker-0.8.tar.gz/blinker-0.8/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+readme = open('README').read()
+import blinker
+version = blinker.__version__
+
+setup(name="blinker",
+      version=version,
+      packages=['blinker'],
+      author='Jason Kirtland',
+      author_email='jek@discorporate.us',
+      description='Fast, simple object-to-object and broadcast signaling',
+      keywords='signal emit events broadcast',
+      long_description=readme,
+      license='MIT License',
+      url='http://bitbucket.org/jek/blinker/',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.4',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          # 'Programming Language :: Python :: 3.1', # _saferef fails :(
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities',
+          ],
+)
