184313111663e0e8077fcd5e5b0a3bae9c61685a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:13 +0000

    Add package dolmen.blob/0.4/dolmen.blob-0.4.tar.gz

---
+++ packages/dolmen.blob/dolmen.blob-0.4.tar.gz/dolmen.blob-0.4/setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from os.path import join
+
+name = 'dolmen.blob'
+version = '0.4'
+readme = open(join('src', 'dolmen', 'blob', "README.txt")).read()
+history = open(join('docs', 'HISTORY.txt')).read()
+
+install_requires = [
+    'ZODB3 >= 3.9.0',
+    'dolmen.builtins >= 0.2',
+    'dolmen.file >= 0.3.2',
+    'grokcore.component',
+    'grokcore.view',
+    'setuptools',
+    'zope.copy == 3.5.0',
+    'zope.file',
+    'zope.mimetype',
+    'zope.location >= 3.7.0'
+    ]
+
+tests_require = install_requires + [
+    'zope.size',
+    'zope.testing',
+    'zope.filerepresentation',
+    'zope.app.testing',
+    'zope.app.zcmlfiles',
+    ]
+
+setup(name = name,
+      version = version,
+      description = 'Dolmen zodb blob handlers',
+      long_description = readme + '\n\n' + history,
+      keywords = 'Grok Zope3 CMS Dolmen',
+      author = 'Souheil Chelfouh',
+      author_email = 'trollfot@gmail.com',
+      url = '',
+      license = 'GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages = ['dolmen'],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = False,
+      tests_require = tests_require,
+      install_requires = install_requires,
+      extras_require = {'test': tests_require},
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Zope3',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+)
