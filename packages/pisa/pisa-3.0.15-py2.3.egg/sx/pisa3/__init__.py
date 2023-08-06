46973832ed4327c0080df592440b1967dac2bbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:59 +0000

    Add package pisa/3.0.15/pisa-3.0.15-py2.3.egg

---
+++ packages/pisa/pisa-3.0.15-py2.3.egg/sx/pisa3/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: ISO-8859-1 -*-
+#############################################
+## (C)opyright by Dirk Holtwick, 2002-2007 ##
+## All rights reserved                     ##
+#############################################
+
+__reversion__ = "$Revision: 160 $"
+__author__    = "$Author: holtwick $"
+__date__      = "$Date: 2008-03-12 16:40:03 +0100 (Mi, 12 Mrz 2008) $"
+
+from pisa import *
+
+__version__   = VERSION
