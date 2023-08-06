fe7bbfb1fe5deca25c6190e490e27befb3874525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:57 +0000

    Add package boduch/0.0.2/boduch-0.0.2-py2.4.egg

---
+++ packages/boduch/boduch-0.0.2-py2.4.egg/boduch/test/set.py
@@ -0,0 +1,58 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This module provides a test case for the Set class and a test
+suite which contains a the test case class."""
+
+import unittest
+
+from boduch.interface import ISet
+from boduch.event import Event, publish, subscribe, unsubscribe, lock, unlock
+from boduch.handle import Handle
+from boduch.data import Set
+
+class TestSet(unittest.TestCase):
+    def setUp(self):
+        self.set_obj=Set()
+        
+    def test_A_interface(self):
+        self.assertTrue(ISet.implementedBy(Set),\
+                        'ISet not implemented by Set.')
+        self.assertTrue(ISet.providedBy(self.set_obj),\
+                        'ISet not provided by Set instance.')
+                        
+    def test_B_push(self):
+        try:
+            self.set_obj.push('test')
+        except Exception, e:
+            self.fail(str(e))
+        
+    def test_C_get(self):
+        try:
+            self.set_obj.get(0)
+        except Exception, e:
+            self.fail(str(e))
+            
+    def test_D_pop(self):
+        try:
+            self.set_obj.pop(0)
+        except Exception, e:
+            self.fail(str(e))
+            
+SuiteSet=unittest.TestLoader().loadTestsFromTestCase(TestSet)
+
+__all__=['TestSet', 'SuiteSet']            
+                        
