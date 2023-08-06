ad9aab3129ce2571965dab4eedb9dc673de24bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:32 +0000

    Add package Factory/1.0/Factory-1.0.tar.gz

---
+++ packages/Factory/Factory-1.0.tar.gz/Factory-1.0/test_Factory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+if __name__ == "__main__":
+    import doctest
+    import functools
+    import itertools
+
+    extraglobs = {'functools': functools,
+                  'itertools': itertools}
+
+    doctest.testfile("README", extraglobs=extraglobs)
