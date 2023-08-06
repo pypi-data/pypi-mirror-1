6fec6646b4cc936dacd99d8f8bdf343b4d6040b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:47 +0000

    Add package TGCombine/1.0.4/TGCombine-1.0.4-py2.6.egg

---
+++ packages/TGCombine/TGCombine-1.0.4-py2.6.egg/tgcombine/release.py
@@ -0,0 +1,14 @@
+# Release information about TGCombine
+
+version = "1.0.4"
+
+description = "Turbogears Javascript/CSS Combine and Pack"
+long_description = "Allows you to maintain a nice formatted copy ofyou scripts and serve the packed versions"
+author = "Vince Spicer"
+email = "vinces1979@gmail.com"
+# copyright = "Vintage 2006 - a good year indeed"
+
+# if it's open source, you might want to specify these
+url = "http://tgcombine.googlecode.com"
+#download_url = "http://yourcool.site/download"
+license = "MIT"
