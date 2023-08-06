7b1f7e1034540b62818ff3ecd9ec2d4116deecab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:44 +0000

    Add package collective.project/0.9.3/collective.project-0.9.3.zip

---
+++ packages/collective.project/collective.project-0.9.3.zip/collective.project-0.9.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join("collective", "project", "version.txt")).read().strip()
+
+setup(name='collective.project',
+      version=version,
+      description="""Formerly Products.project, this package aims for "simple" project management with 4 (Dexterity) content types added to Plone: Client, Project, Iteration, and Task.""",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "INSTALL.txt")).read() +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='"project management"',
+      author='Alex Clark',
+      author_email='aclark@aclark.net',
+      url='https://svn.plone.org/svn/collective/collective.project/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
