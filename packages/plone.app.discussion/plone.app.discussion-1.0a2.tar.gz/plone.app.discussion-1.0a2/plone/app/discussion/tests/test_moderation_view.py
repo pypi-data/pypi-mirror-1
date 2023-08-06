b8ea197a8cf3add9f789600fd5b91cf34546f6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:29 +0000

    Add package plone.app.discussion/1.0a2/plone.app.discussion-1.0a2.tar.gz

---
+++ packages/plone.app.discussion/plone.app.discussion-1.0a2.tar.gz/plone.app.discussion-1.0a2/plone/app/discussion/tests/test_moderation_view.py
@@ -0,0 +1,102 @@
+import unittest
+from datetime import datetime, timedelta
+
+from plone.registry import Registry
+
+from zope.component import createObject
+
+from Acquisition import aq_base, aq_parent, aq_inner
+
+from OFS.Image import Image
+
+from plone.app.vocabularies.types import BAD_TYPES
+
+from Products.CMFCore.FSImage import FSImage
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.tests import dummy
+from Products.PloneTestCase.ptc import PloneTestCase
+
+from plone.app.discussion.browser.moderation import View
+from plone.app.discussion.interfaces import IConversation, IComment
+from plone.app.discussion.interfaces import IReplies, IDiscussionSettings
+from plone.app.discussion.tests.layer import DiscussionLayer
+
+
+class ModerationViewTest(PloneTestCase):
+
+    layer = DiscussionLayer
+
+    def afterSetUp(self):
+        self.loginAsPortalOwner()
+        typetool = self.portal.portal_types
+        typetool.constructContent('Document', self.portal, 'doc1')
+        self.portal_discussion = getToolByName(self.portal,
+                                               'portal_discussion',
+                                               None)
+        self.membership_tool = getToolByName(self.folder,
+                                             'portal_membership')
+        self.memberdata = self.portal.portal_memberdata
+        request = self.app.REQUEST
+        context = getattr(self.portal, 'doc1')
+        self.view = View(context, request)
+        self.portal.portal_workflow.setChainForPortalTypes(
+            ('Discussion Item',), 'comment_review_workflow')
+        self.wf_tool = self.portal.portal_workflow
+
+        # Add a conversation with three comments
+
+        conversation = IConversation(self.portal.doc1)
+
+        comment1 = createObject('plone.Comment')
+        comment1.title = 'Comment 1'
+        comment1.text = 'Comment text'
+        comment1.Creator = 'Jim'
+        new_id_1 = conversation.addComment(comment1)
+        self.comment1 = self.portal.doc1.restrictedTraverse(\
+                            '++conversation++default/%s' % new_id_1)
+
+        comment2 = createObject('plone.Comment')
+        comment2.title = 'Comment 2'
+        comment2.text = 'Comment text'
+        comment2.Creator = 'Joe'
+        new_id_2 = conversation.addComment(comment2)
+        self.comment2 = self.portal.doc1.restrictedTraverse(\
+                            '++conversation++default/%s' % new_id_2)
+
+        comment3 = createObject('plone.Comment')
+        comment3.title = 'Comment 3'
+        comment3.text = 'Comment text'
+        comment3.Creator = 'Emma'
+        new_id_3 = conversation.addComment(comment3)
+        self.comment3 = self.portal.doc1.restrictedTraverse(\
+                            '++conversation++default/%s' % new_id_3)
+
+    def test_moderation_enabled(self):
+        self.assertEquals(self.view.moderation_enabled(), True)
+        self.wf_tool.setChainForPortalTypes(('Discussion Item',),
+                                            ('simple_publication_workflow,'))
+        self.assertEquals(self.view.moderation_enabled(), False)
+
+    def test_comments_all(self):
+        self.failUnless(self.view.comments_all())
+        self.assertEquals(len(self.view.comments_all()), 3)
+
+    def test_comments_pending(self):
+        self.wf_tool.getInfoFor(self.comment1, 'review_state')
+        self.failUnless(self.view.comments_pending())
+        self.assertEquals(len(self.view.comments_pending()), 3)
+        self.portal.portal_workflow.doActionFor(self.comment1,
+                                                action='publish')
+        #self.comment1.reindexObject()
+        #self.assertEquals(len(self.view.comments_pending()), 2)
+
+    def test_comments_published(self):
+        self.assertEquals(len(self.view.comments_published()), 0)
+        self.wf_tool.doActionFor(self.comment1, action='publish')
+        #self.assertEquals(len(self.view.comments_published()), 1)
+
+    def test_comments_spam(self):
+        self.view.comments_spam()
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
---
+++ packages/plone.app.discussion/plone.app.discussion-1.0a2.tar.gz/plone.app.discussion-1.0a2/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a2'
+
+setup(name='plone.app.discussion',
+      version=version,
+      description="Enhanced discussion support for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Timo Stollenwerk',
+      author_email='timo@zmag.de',
+      url='http://pypi.python.org/pypi/plone.app.discussion',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.autopermission',
+          'collective.monkeypatcher',
+          'plone.app.registry',
+          'plone.app.z3cform',
+          'plone.indexer',
+          'plone.registry',
+          'plone.z3cform',
+          'ZODB3',
+          'zope.interface',
+          'zope.component',
+          'zope.annotation',
+          'zope.event',
+          'zope.container',
+          'zope.lifecycleevent',
+          'zope.site',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
