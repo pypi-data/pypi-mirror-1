2b332d07557645aa5fbbffe0f660396e0ac6ffee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:30 +0000

    Add package cssutils/0.9.7a2/cssutils-0.9.7a2.zip

---
+++ packages/cssutils/cssutils-0.9.7a2.zip/cssutils-0.9.7a2/src/tests/test_cssrulelist.py
@@ -0,0 +1,22 @@
+"""Testcases for cssutils.css.CSSRuleList"""
+__version__ = '$Id: test_cssrulelist.py 1116 2008-03-05 13:52:23Z cthedot $'
+
+import basetest
+import cssutils
+
+class CSSRuleListTestCase(basetest.BaseTestCase):
+
+    def test_init(self):
+        "CSSRuleList init"
+        r = cssutils.css.CSSRuleList()
+        self.assertEqual(0, r.length)
+        self.assertEqual(None, r.item(2))
+        
+        # subclasses list but all setting options like append, extend etc
+        # need to be added to an instance of this class by a using class!
+        self.assertRaises(NotImplementedError, r.append, 1)
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
