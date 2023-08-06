e8283bbc93c2a501190fb34f1f714950a4474ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:46 +0000

    Add package zope.schema/3.5.4/zope.schema-3.5.4.tar.gz

---
+++ packages/zope.schema/zope.schema-3.5.4.tar.gz/zope.schema-3.5.4/setup.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.schema package
+
+$Id: setup.py 98362 2009-03-25 13:20:28Z icemac $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.schema',
+      version = '3.5.4',
+      url='http://pypi.python.org/pypi/zope.schema',
+      license='ZPL 2.1',
+      description='zope.interface extension for defining data schemas',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(read('src', 'zope', 'schema', 'README.txt')
+                        + '\n\n' +
+                        read('src', 'zope', 'schema', 'fields.txt')
+                        + '\n\n' +
+                        read('src', 'zope', 'schema', 'sources.txt')
+                        + '\n\n' +
+                        read('src', 'zope', 'schema', 'validation.txt')
+                        + '\n\n' +
+                        read('CHANGES.txt')),
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope',],
+      extras_require={'test': ['zope.testing'],
+                      'docs': ['z3c.recipe.sphinxdoc']},
+      install_requires=['setuptools',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.event',
+                       ],
+      include_package_data = True,
+      zip_safe = False,
+      )
