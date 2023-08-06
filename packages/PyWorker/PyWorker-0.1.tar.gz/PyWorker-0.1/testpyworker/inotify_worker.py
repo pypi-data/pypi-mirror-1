8ff99d4efe01318e37d450d12640fe2d6e0e7490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:44 +0000

    Add package PyWorker/0.1/PyWorker-0.1.tar.gz

---
+++ packages/PyWorker/PyWorker-0.1.tar.gz/PyWorker-0.1/testpyworker/inotify_worker.py
@@ -0,0 +1,12 @@
+from amqpqueue import Worker, WorkerResponse, QueueFactory
+
+class MyWorker(Worker):
+    def endtask(self, msg, response):
+        print "message: %s" % (msg)
+        self.queue_stdin.task_done()
+
+qf = QueueFactory()
+
+worker = MyWorker(qf.Subscriber('logger', 'inotify'))
+
+worker.run()
