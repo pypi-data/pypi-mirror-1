c4dbbb50fcdc6951dcd327399bf3f1ae2461bf67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:33 +0000

    Add package chameleon.genshi/1.0b2/chameleon.genshi-1.0b2.tar.gz

---
+++ packages/chameleon.genshi/chameleon.genshi-1.0b2.tar.gz/chameleon.genshi-1.0b2/README.txt
@@ -0,0 +1,25 @@
+Overview
+========
+
+This package provides a fast Genshi template implementation based on
+the Chameleon template compiler. It's largely compatible with
+``genshi.template``.
+
+Compiler notes
+--------------
+
+1. Match templates defined in included templates are not applied to
+the calling document.
+
+Development
+-----------
+
+If you want to use the code directly from trunk (recommended only for
+development and testing usage), provide ``chameleon.genshi==dev`` as your
+dependency.
+
+svn://svn.zope.org/repos/main/Sandbox/malthe/chameleon.genshi/trunk#egg=chameleon.genshi-dev
+
+Want to contribute? Join #repoze on Freenode IRC.
+
+
---
+++ packages/chameleon.genshi/chameleon.genshi-1.0b2.tar.gz/chameleon.genshi-1.0b2/src/chameleon/genshi/tests/xinclude2.html.py
@@ -0,0 +1,27 @@
+def bind():
+	from cPickle import loads as _loads
+	_init_stream = _loads('cchameleon.core.generation\ninitialize_stream\np1\n.')
+	_lookup_attr = _loads('cchameleon.core.codegen\nlookup_attr\np1\n.')
+	_init_scope = _loads('cchameleon.core.utils\necontext\np1\n.')
+	_init_tal = _loads('cchameleon.core.generation\ninitialize_tal\np1\n.')
+	_lookup_name = _loads('cchameleon.core.codegen\nlookup_name\np1\n.')
+	def render(_domain=None, xincludes=None, econtext=None, _out=None, _write=None, target_language=None):
+		(_attributes, repeat) = _init_tal()
+		u'None'
+		_domain = None
+		_write('\n  ')
+		def greeting(name):
+			_write('<p class="greeting">\n    Hello, ')
+			_tmp1 = name
+			_tmp = _tmp1
+			if _tmp is not None:
+				if not (isinstance(_tmp, basestring)):
+					_tmp = str(_tmp)
+				_write(_tmp)
+			_write('!\n  ')
+			_write('</p>')
+		u'greeting'
+		econtext['greeting'] = greeting
+		_write('\n')
+		return None
+	return render
