a05133ad0413aa3ea0fc1d9ad630e435ce8dcfb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:38 +0000

    Add package sflib/1.0dev-BZR-r40-panta-elasticworld.org-20090226145945-5jo0t55igg503f28/sflib-1.0dev_BZR_r40_panta_elasticworld.org_20090226145945_5jo0t55igg503f28-py2.6.egg

---
+++ packages/sflib/sflib-1.0dev_BZR_r40_panta_elasticworld.org_20090226145945_5jo0t55igg503f28-py2.6.egg/sflib/bzrcachedrev.py
@@ -0,0 +1,4 @@
+info    = {'revinfo': (40, 'panta@elasticworld.org-20090226145945-5jo0t55igg503f28'), 'basepath': '/arc/bzr/python-sflib.bzr/trunk/', 'revno': 40, 'revname': 'panta@elasticworld.org-20090226145945-5jo0t55igg503f28', 'inpath': u'sflib/version.py'}
+revno   = 40
+revname = 'panta@elasticworld.org-20090226145945-5jo0t55igg503f28'
+revinfo = (40, 'panta@elasticworld.org-20090226145945-5jo0t55igg503f28')
---
+++ packages/sflib/sflib-1.0dev_BZR_r40_panta_elasticworld.org_20090226145945_5jo0t55igg503f28-py2.6.egg/sflib/fileconf/__init__.py
@@ -0,0 +1,11 @@
+from configparser import *
+
+def _test():
+    """
+    Test entry point
+    """
+    import doctest
+    doctest.testmod()
+
+if __name__ == '__main__':
+    _test()
