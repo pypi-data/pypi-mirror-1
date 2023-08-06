93efd08d35d972c47287bb7ec0815205f6ed4cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:05 +0000

    Add package glasnaegel/0.2.0/glasnaegel-0.2.0.tar.gz

---
+++ packages/glasnaegel/glasnaegel-0.2.0.tar.gz/glasnaegel-0.2.0/glasnaegel/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2010 Andrey Mikhaylenko and contributors
+#
+#  This file is part of Glasnägel.
+#
+#  Glasnägel is free software under terms of the GNU Lesser General Public
+#  License version 3 (LGPLv3) as published by the Free Software Foundation.
+#  See the file README for copying conditions.
+#
+
+from decorators import *
+
+__version__ = '0.2.0'
