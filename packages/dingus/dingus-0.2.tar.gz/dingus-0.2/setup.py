969bba5cc33852d3304c341b046feca750840528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:25 +0000

    Add package dingus/0.2/dingus-0.2.tar.gz

---
+++ packages/dingus/dingus-0.2.tar.gz/dingus-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='dingus',
+      version='0.2',
+      description='A record-then-assert mocking library',
+      author='Gary Bernhardt',
+      author_email='gary.bernhardt@gmail.com',
+      py_modules=['dingus'],
+      license='MIT',
+      url='http://bitbucket.org/garybernhardt/dingus',
+      keywords='testing test mocking mock double stub fake record assert',
+      classifiers=["Development Status :: 2 - Pre-Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Testing",
+                   ],
+     )
+
