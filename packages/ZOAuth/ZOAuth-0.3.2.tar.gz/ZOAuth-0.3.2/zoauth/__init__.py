2ac0bc901af40e9307cba0e59e9f818b68fbf6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:52 +0000

    Add package ZOAuth/0.3.2/ZOAuth-0.3.2.tar.gz

---
+++ packages/ZOAuth/ZOAuth-0.3.2.tar.gz/ZOAuth-0.3.2/zoauth/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from zoauth import consumer
+from zoauth import meta
+from zoauth import net
+from zoauth import provider
+from zoauth import request
+from zoauth import tokens
+
+__all__ = ['consumer', 'net', 'provider', 'request', 'tokens']
