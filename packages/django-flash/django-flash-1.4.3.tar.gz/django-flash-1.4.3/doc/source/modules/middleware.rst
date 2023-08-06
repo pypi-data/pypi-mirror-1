942af078931a5c3b2ae0ccdc5f4f1e24089a3783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:33 +0000

    Add package django-flash/1.4.3/django-flash-1.4.3.tar.gz

---
+++ packages/django-flash/django-flash-1.4.3.tar.gz/django-flash-1.4.3/doc/source/modules/middleware.rst
@@ -0,0 +1,16 @@
+:mod:`djangoflash.middleware` --- Django-flash middleware
+=========================================================
+
+.. automodule:: djangoflash.middleware
+   :synopsis: Django-flash middleware
+
+:class:`FlashMiddleware` Class
+``````````````````````````````
+
+.. autoclass:: FlashMiddleware
+   :show-inheritance:
+   :members:
+
+
+.. seealso::
+   :ref:`modulesindex`
---
+++ packages/django-flash/django-flash-1.4.3.tar.gz/django-flash-1.4.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-flash',
+    version = '1.4.3',
+    author = 'Daniel Fernandes Martins',
+    author_email = 'daniel.tritone@gmail.com',
+    description = 'Rails-like flash support for Django.',
+    license = 'LGPL',
+    platforms = ['Any'],
+    keywords = ['django', 'flash', 'session', 'scope', 'context', 'contrib'],
+    url = 'http://djangoflash.destaquenet.com/',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+    install_requires = ['Django>=1.0_final'],
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    include_package_data=True,
+    zip_safe = False,
+    test_suite = 'djangoflash.tests.suite',
+)
+
