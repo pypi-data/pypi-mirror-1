199aec16f251c4e19a601ca4b73df7a803e3d88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:21 +0000

    Add package ploneconf-demo/1.0/ploneconf-demo-1.0.tar.gz

---
+++ packages/ploneconf-demo/ploneconf-demo-1.0.tar.gz/ploneconf-demo-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ploneconf-demo',
+      version='1.0',
+      author='tarek',
+      author_email='tarek@ziade.org',
+      url='http://example.com',
+      description=('demo of distutils for the plone conf -- '
+                   'will be removed from PyPI after the tutorial'))
+
