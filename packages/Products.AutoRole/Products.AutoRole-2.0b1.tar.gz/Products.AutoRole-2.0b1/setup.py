8d4d4e71b1cacbae7163d65560ce6df1bdd6c3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:13 +0000

    Add package Products.AutoRole/2.0b1/Products.AutoRole-2.0b1.tar.gz

---
+++ packages/Products.AutoRole/Products.AutoRole-2.0b1.tar.gz/Products.AutoRole-2.0b1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0b1'
+
+setup(name='Products.AutoRole',
+      version=version,
+      description="Add roles to (anonymous or logged-in) "
+                  "visitors based on their IP address.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='autorole pas plugin roles groups',
+      author='Jarn',
+      author_email='info@jarn.com',
+      url='http://plone.org/products/autorole',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
