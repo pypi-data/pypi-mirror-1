1431a797b9e72cbd85dce302b71f137e8440fb0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:32 +0000

    Add package django-feedback/0.4/django-feedback-0.4.tar.gz

---
+++ packages/django-feedback/django-feedback-0.4.tar.gz/django-feedback-0.4/setup.py
@@ -0,0 +1,30 @@
+import os
+#from setuptools import setup
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-feedback',
+    version='0.4',
+    description='A pluggable user feedback app',
+    author='Jared Forsyth',
+    author_email='jabapyth@gmail.com',
+    license='BSD',
+    url='http://github.com/jabapyth/django-feedback/',
+    keywords = ['blog', 'django', 'feedback', 'ajax', 'user', 'customer', 'comment'],
+    package_data = {'feedback':['media/*.js','media/*.css','media/images/*','README.rst','CREDITS']},
+    packages=[
+        'feedback',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
