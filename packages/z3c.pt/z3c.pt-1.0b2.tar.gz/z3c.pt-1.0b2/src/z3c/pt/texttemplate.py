de8655dfa6dbe577284c919305356bf94b33a6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:24 +0000

    Add package z3c.pt/1.0b2/z3c.pt-1.0b2.tar.gz

---
+++ packages/z3c.pt/z3c.pt-1.0b2.tar.gz/z3c.pt-1.0b2/src/z3c/pt/texttemplate.py
@@ -0,0 +1,17 @@
+import pagetemplate
+
+class TextTemplate(pagetemplate.PageTemplate):
+    __doc__ = pagetemplate.PageTemplate.__doc__ # for Sphinx autodoc
+    format = 'text'
+
+class TextTemplateFile(pagetemplate.PageTemplateFile):
+    __doc__ = pagetemplate.PageTemplateFile.__doc__ # for Sphinx autodoc
+    format = 'text'
+
+class ViewTextTemplate(pagetemplate.ViewPageTemplate):
+    __doc__ = pagetemplate.ViewPageTemplate.__doc__ # for Sphinx autodoc
+    template_class = TextTemplate
+        
+class ViewTextTemplateFile(pagetemplate.ViewPageTemplateFile):
+    __doc__ = pagetemplate.ViewPageTemplateFile.__doc__ # for Sphinx autodoc
+    template_class = TextTemplateFile
