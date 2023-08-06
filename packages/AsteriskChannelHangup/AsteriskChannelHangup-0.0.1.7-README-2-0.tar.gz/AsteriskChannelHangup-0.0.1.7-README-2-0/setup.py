ed8c33323579e38347484743a58565d09f9635b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:22 +0000

    Add package AsteriskChannelHangup/0.0.1.7-README-2-0/AsteriskChannelHangup-0.0.1.7-README-2-0.tar.gz

---
+++ packages/AsteriskChannelHangup/AsteriskChannelHangup-0.0.1.7-README-2-0.tar.gz/AsteriskChannelHangup-0.0.1.7-README-2-0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+'''
+Created on 18/12/2009
+Programa que permite colgar llamadas de un Asterisk
+con mas de X tiempo.
+
+@version: 0.0.1
+@organization: Sapian SA
+@author: Sebastian Rojo
+'''
+from ez_setup import use_setuptools
+from setuptools import setup, find_packages
+
+use_setuptools()
+setup(name="AsteriskChannelHangup",
+    version="0.0.1.7-README-2-0",
+    description="Programa utilizado para prevenir canales colgados en una planta asterisk",
+    author="Sebastian rojo",
+    author_email="srojo@sapian.com.co",
+    url="http://www.sapian.com.co",
+    license="GPL",
+    package_data = {'':["conf/*"],'':["html/*"]},
+    packages=["AsteriskChannelHangup"],
+    scripts=['AsteriskSoftHangupOldChannels.py'],
+    data_files=[('conf',['conf/py-asterisk.conf.sample'])],
+    install_requires = ["py-asterisk"]
+)
