b2c699c319eb99d3ecd409e6a7d00a39cb6fcb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:46 +0000

    Add package gocept.cache/0.4/gocept.cache-0.4.tar.gz

---
+++ packages/gocept.cache/gocept.cache-0.4.tar.gz/gocept.cache-0.4/setup.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id: setup.py 29843 2009-06-18 12:48:05Z wosc $
+
+import os.path
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'gocept.cache',
+    version = '0.4',
+    author = "Christian Zagrodnick",
+    author_email = "cz@gocept.com",
+    description = "Cache descriptors for Python and Zope",
+    long_description = (
+        file(os.path.join(os.path.dirname(__file__),
+                          'src', 'gocept', 'cache', 'README.txt')).read() +
+        '\n\n' +
+        file(os.path.join(os.path.dirname(__file__), 'CHANGES.txt')).read()),
+    license = "ZPL 2.1",
+    url='http://pypi.python.org/pypi/gocept.cache',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+    include_package_data = True,
+    zip_safe = False,
+
+    namespace_packages = ['gocept'],
+    install_requires = [
+        'ZODB3',
+        'decorator',
+        'setuptools',
+        'zope.testing',
+    ],
+)
