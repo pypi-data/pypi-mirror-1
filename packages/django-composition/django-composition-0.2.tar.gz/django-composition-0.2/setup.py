560369464e005188443e4e88ba79e15938f92b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:54 +0000

    Add package django-composition/0.2/django-composition-0.2.tar.gz

---
+++ packages/django-composition/django-composition-0.2.tar.gz/django-composition-0.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+def get_description():
+    import os
+    return "".join(
+        file(
+            os.path.join(os.path.dirname(os.path.normpath(__file__)),
+            'README'
+        ), 'r').readlines()[1:] # strip markdown marker line
+    )
+
+setup(
+    name="django-composition",
+    version="0.2",
+
+    license="New BSD License",
+
+    author='Alex Koshelev',
+    author_email="daevaorn@gmail.com",
+
+    url="http://bitbucket.org/daevaorn/django-composition/",
+
+    packages=[
+        "composition",
+        "composition.shortcuts",
+        "composition.tests"
+    ],
+
+    description='`django-composition` provides the abstract way to denormalize data from your models in simple declarative way through special generic model field called `CompositionField`',
+    long_description=get_description(),
+
+    classifiers=[
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+    ]
+)
