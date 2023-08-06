5f34aa6c7016c639c822a16db8ee61f0ec1500f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:48 +0000

    Add package alchemist.traversal/0.3.1/alchemist.traversal-0.3.1.tar.gz

---
+++ packages/alchemist.traversal/alchemist.traversal-0.3.1.tar.gz/alchemist.traversal-0.3.1/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="alchemist.traversal",
+    version="0.3.1",
+    install_requires=['setuptools', 'ore.alchemist', 'z3c.traverser'],
+    packages=find_packages(exclude=["*.tests"]),
+#    package_dir= {'':'src'},
+    namespace_packages=['alchemist'],
+    package_data = {
+      '': ['*.txt', '*.zcml', '*.pt'],
+    },
+    zip_safe=False,
+    author='Kapil Thangavelu',
+    author_email='kapil.foss@gmail.com',
+    description="""traversal mechanisms for alchemist containers and domain objects.""",
+    long_description="Traversal of objects by foreign keys"+'\n\n'+read('changes.txt'),
+    license='LGPL',
+    keywords="zope zope3",
+    )
