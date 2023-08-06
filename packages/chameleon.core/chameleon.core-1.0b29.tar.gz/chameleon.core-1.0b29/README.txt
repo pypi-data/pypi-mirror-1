8db019207c32e5beba7bacc47e2745bb15b4450c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:04 +0000

    Add package chameleon.core/1.0b29/chameleon.core-1.0b29.tar.gz

---
+++ packages/chameleon.core/chameleon.core-1.0b29.tar.gz/chameleon.core-1.0b29/README.txt
@@ -0,0 +1,24 @@
+Overview
+--------
+
+Chameleon is a generic byte-code compiler for attribute template
+languages; it supports macros, includes, interpolation and
+internationalization.
+
+The ``chameleon.core`` package implements a language parser API, the
+byte-code compiler itself and abstract template classes. It should
+only be used directly when implementing a language dialect.
+
+Development
+-----------
+
+If you want to use the code directly from trunk (recommended only for
+development and testing usage), provide ``chameleon.core==dev`` as your
+dependency.
+
+The code is maintained in a subversion repository::
+
+  svn://svn.repoze.org/svn/chameleon.core
+
+Want to contribute? Join #repoze on Freenode IRC.
+
---
+++ packages/chameleon.core/chameleon.core-1.0b29.tar.gz/chameleon.core-1.0b29/src/chameleon/core/filecache.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import config
+import logging
+import cPickle as pickle
+
+logger = logging.getLogger("Chameleon")
+
+class TemplateCache(object):
+    def __init__(self, filename, version):
+        self.filename = filename
+        self.registry = {}
+        self.version = version
+        self.load()
+        
+    def __getitem__(self, key):
+        return self.registry[key]
+
+    def __setitem__(self, key, template):
+        self.registry[key] = template
+        self.save()
+
+    def __len__(self):
+        return len(self.registry)
+    
+    def get(self, key, default=None):
+        return self.registry.get(key, default)
+    
+    @property
+    def module_filename(self):
+        return self.filename + os.extsep + config.CACHE_EXTENSION
+    
+    def load(self):
+        try:
+            module_filename = self.module_filename
+            f = open(module_filename, 'rb')
+        except IOError:
+            return
+
+        try:
+            try:
+                try:
+                    version, version_info, registry = pickle.load(f)
+                except EOFError:
+                    pass
+                
+                if version != self.version:
+                    raise ValueError("Version mismatch: %s != %s" % (
+                        version, self.version))
+
+                if sys.version_info != version_info:
+                    raise ValueError("Python version mismatch: %s != %s" % (
+                        version_info, self.version_info))
+
+                self.registry.update(registry)
+            except (ValueError, TypeError), e:
+                logger.warn(
+                    "Error loading cache for %s (%s)." % (self.filename, str(e)))
+        finally:
+            f.close()
+        
+    def save(self):
+        try:
+            f = open(self.module_filename, 'wb')
+        except IOError:
+            return
+
+        try:
+            data = self.version, sys.version_info, self.registry
+            pickle.dump(data, f, protocol=2)
+        finally:
+            f.close()
+
+    def clear(self):
+        self.registry.clear()
+                
+    def purge(self):
+        self.clear()
+        self.save()
