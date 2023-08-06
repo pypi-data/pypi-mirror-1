23ca081d0f17daa1d138de3d3ad177451c2e16a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:52 +0000

    Add package redturtle.portletpage.views/0.3.0/redturtle.portletpage.views-0.3.0.tar.gz

---
+++ packages/redturtle.portletpage.views/redturtle.portletpage.views-0.3.0.tar.gz/redturtle.portletpage.views-0.3.0/README.txt
@@ -0,0 +1,7 @@
+Introduction
+============
+
+A simple new view for `collective.portletpage`__ that shows only portlets, not
+stuff like title and rich text.
+
+__ http://pypi.python.org/pypi/collective.portletpage
---
+++ packages/redturtle.portletpage.views/redturtle.portletpage.views-0.3.0.tar.gz/redturtle.portletpage.views-0.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.3.0'
+
+setup(name='redturtle.portletpage.views',
+      version=version,
+      description="Additional views for Plone collective.portletpage product",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='portlet view portletpage',
+      author='Massimo Azzolini',
+      author_email='massimo@redturtle.net',
+      url='https://code.redturtle.it/svn/redturtle/redturtle.net/redturtle.portletpage.views/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['redturtle', 'redturtle.portletpage'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.portletpage',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
