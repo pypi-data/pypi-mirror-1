aed9315eb432ddbf2de94c956ad1b367d733886d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:41 +0000

    Add package mypy/0.230/mypy-0.230.tar.gz

---
+++ packages/mypy/mypy-0.230.tar.gz/mypy-0.230/mypy/project_template/myconf/local_config.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+
+#是否启用在线调试
+DEBUG = True
+
+
+#是否python由提供静态文件的服务
+SERVER_STATIC_FILE = True
+
+#是否使用优化过的css，js
+ORG_CSS_JS = True
+
+#Javascript文件服务器的域名
+JS_FILE_HOST = ""
+
+#静态文件服务器的域名
+FILE_HOST = ""
+
+#数据库配置
+WTREE_HOST = "localhost:3306:zuroc_wtree:zuroc:zuroc##"
+
+SMTP_PASSWORD = "kanrssstdyun"
