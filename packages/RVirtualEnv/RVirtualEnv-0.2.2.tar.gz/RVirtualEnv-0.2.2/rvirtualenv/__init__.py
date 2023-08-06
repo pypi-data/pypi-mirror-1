2a61da0e723acc8246e1bab1e0878258617f5e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:53 +0000

    Add package RVirtualEnv/0.2.2/RVirtualEnv-0.2.2.tar.gz

---
+++ packages/RVirtualEnv/RVirtualEnv-0.2.2.tar.gz/RVirtualEnv-0.2.2/rvirtualenv/__init__.py
@@ -0,0 +1,27 @@
+
+import sys
+
+from rvirtualenv.copy import copy
+from rvirtualenv.generate import generate
+
+
+VERSION = (0, 2, 2)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+
+def main(argv=None):
+    '''
+    main call for rvirtualenv command
+    '''
+    if argv is None:
+        argv = sys.argv
+
+    if len(argv) < 2:
+        raise NotImplementedError
+
+    venv = argv[1]
+
+    copy(venv)
+    generate(venv)
+
