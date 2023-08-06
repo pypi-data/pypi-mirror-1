cbca01c3fb242bda44f8b8b4531d1b3a91c38dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:27 +0000

    Add package django-sproutcore/0.5/django-sproutcore-0.5.tar.gz

---
+++ packages/django-sproutcore/django-sproutcore-0.5.tar.gz/django-sproutcore-0.5/setup.py
@@ -0,0 +1,14 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "django-sproutcore",
+    version = "0.5",
+    packages = find_packages(),
+    author = "Taavi Taijala, Richard Silver, Eric Ocean",
+    author_email = "taavi@taijala.com, corn13read@gmail.com, eric.ocean@me.com",
+    description = "A package to help automate creation of sproutcore apps for django",
+    url = "http://github.com/erichocean/django-sproutcore/tree/master",
+    include_package_data = True
+)
+
