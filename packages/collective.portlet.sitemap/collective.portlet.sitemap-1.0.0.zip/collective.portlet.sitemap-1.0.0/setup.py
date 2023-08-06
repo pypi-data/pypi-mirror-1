d80ee6e52c56f96fb8b15cae298be5d001446381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:13 +0000

    Add package collective.portlet.sitemap/1.0.0/collective.portlet.sitemap-1.0.0.zip

---
+++ packages/collective.portlet.sitemap/collective.portlet.sitemap-1.0.0.zip/collective.portlet.sitemap-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0'
+
+setup(name='collective.portlet.sitemap',
+      version=version,
+      description="Plone3 navigation portlet extended",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone portlet navigation',
+      author='Ingeniweb',
+      author_email='support@ingeniweb.com',
+      url='http://plone.org',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
