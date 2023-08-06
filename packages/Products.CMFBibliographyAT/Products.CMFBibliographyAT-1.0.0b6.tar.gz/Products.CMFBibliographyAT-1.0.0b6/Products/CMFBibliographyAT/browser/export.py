beadca085182b7bd9e3b015f43e18f057f8790c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:07 +0000

    Add package Products.CMFBibliographyAT/1.0.0b6/Products.CMFBibliographyAT-1.0.0b6.tar.gz

---
+++ packages/Products.CMFBibliographyAT/Products.CMFBibliographyAT-1.0.0b6.tar.gz/Products.CMFBibliographyAT-1.0.0b6/Products/CMFBibliographyAT/browser/export.py
@@ -0,0 +1,40 @@
+from zope import component
+
+from bibliograph.rendering.interfaces import IBibliographyRenderer
+
+
+
+class BibliographyExportView(object):
+    """ """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        """ """
+
+        output_encoding = self.request.get('output_encoding', 'utf-8')
+        eol_style = self.request.get('eol_style', 0)
+        format = self.request.get('format', 'bibtex')
+        response = self.request.response
+        renderer = self._getRenderer(format)
+        response.setHeader('Content-Type', 'application/octet-stream')
+        response.setHeader('Content-Disposition',
+                           'attachment; filename=%s.%s' % (self.__name__, renderer.target_format))
+        return self.export(format, output_encoding, eol_style)
+
+    def export(self, format, output_encoding='utf-8', eol_style=0):
+        """ """
+        renderer = self._getRenderer(format)
+        return renderer.render(self.context, output_encoding=output_encoding, msdos_eol_style=eol_style)
+
+    def _getRenderer(self, format):
+        utils = component.getAllUtilitiesRegisteredFor(IBibliographyRenderer)
+        for renderer in utils:
+            if renderer.available and renderer.enabled:
+                if format.lower() == renderer.__name__.lower():
+                    return renderer
+                elif format.lower() == renderer.target_format.lower():
+                    return renderer
+        return None
