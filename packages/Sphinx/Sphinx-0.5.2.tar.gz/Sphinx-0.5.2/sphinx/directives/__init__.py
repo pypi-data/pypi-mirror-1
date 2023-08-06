f978c264ddb476fe26b6a3705abc373617ce420d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:36 +0000

    Add package Sphinx/0.5.2/Sphinx-0.5.2.tar.gz

---
+++ packages/Sphinx/Sphinx-0.5.2.tar.gz/Sphinx-0.5.2/sphinx/directives/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    sphinx.directives
+    ~~~~~~~~~~~~~~~~~
+
+    Handlers for additional ReST directives.
+
+    :copyright: Copyright 2007-2009 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from sphinx.directives.desc import *
+from sphinx.directives.code import *
+from sphinx.directives.other import *
