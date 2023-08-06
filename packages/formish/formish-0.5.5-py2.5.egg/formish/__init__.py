ac3af335fd1f4216b84548c0c65fb07119cb2e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:02 +0000

    Add package formish/0.5.5/formish-0.5.5-py2.5.egg

---
+++ packages/formish/formish-0.5.5-py2.5.egg/formish/__init__.py
@@ -0,0 +1,8 @@
+"""
+Base package to import top level modules
+"""
+from formish.forms import Form
+from formish.validation import FieldError, FormError, FormishError
+from formish.widgets import *
+from formish.util import form_in_request
+
