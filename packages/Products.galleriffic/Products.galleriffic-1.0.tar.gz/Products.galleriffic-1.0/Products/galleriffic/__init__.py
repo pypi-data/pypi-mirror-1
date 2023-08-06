5e0bf4cc8cddc93e2600a99242e94e15d0a91ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:02 +0000

    Add package Products.galleriffic/1.0/Products.galleriffic-1.0.tar.gz

---
+++ packages/Products.galleriffic/Products.galleriffic-1.0.tar.gz/Products.galleriffic-1.0/Products/galleriffic/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18n import MessageFactory
+AbstractGallerifficMessageFactory = MessageFactory('Products.galleriffic')
+# 
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
+    pass
