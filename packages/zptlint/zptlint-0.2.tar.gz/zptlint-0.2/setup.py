cb759ab1dd454e85460a543f498e785bfa9bc666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:16 +0000

    Add package zptlint/0.2/zptlint-0.2.tar.gz

---
+++ packages/zptlint/zptlint-0.2.tar.gz/zptlint-0.2/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+import os
+
+
+version = '0.2'
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+long_description = (
+        read('README.txt')
+        + '\n' +
+        read('CHANGES.txt'))
+
+setup(name='zptlint',
+      version=version,
+      description="Utility to debug Zope Page Templates",
+      long_description=long_description,
+      classifiers=[
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+      ],
+      keywords='zope',
+      author='Balazs Ree',
+      author_email='ree@ree.hu',
+      url='http://trac.gotcha.python-hosting.com/file/bubblenet/pythoncode/'
+          'zptlint/README.txt?format=txt',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'zope.pagetemplate',
+          'zope.contentprovider',
+          # mentioned here because not mentioned in zope.pagetemplate <= 3.5.0
+          'zope.traversing',
+          'RestrictedPython',
+      ],
+      entry_points={
+        'console_scripts': [
+            'zptlint = zptlint:run',
+            ],
+        },
+      )
