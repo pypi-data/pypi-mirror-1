f2deee7fcc8e9ceefc56f9004fcc2b5de3a4b47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:49 +0000

    Add package timebook/1.0.2/timebook-1.0.2.tar.gz

---
+++ packages/timebook/timebook-1.0.2.tar.gz/timebook-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+from timebook import __version__
+
+setup(
+    name='timebook',
+    version='.'.join(str(bit) for bit in __version__),
+    url='http://bitbucket.org/trevor/timebook/',
+    description='track what you spend time on',
+    author='Trevor Caira',
+    author_email='trevor@caira.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['timebook'],
+    entry_points={'console_scripts': [
+        't = timebook.cmdline:run_from_cmdline']},
+)
