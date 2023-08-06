e9d26b70bee6b0f89a1c2b1e3743d79f3d1bf746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:00 +0000

    Add package tw2.core/2.0a1/tw2.core-2.0a1.tar.gz

---
+++ packages/tw2.core/tw2.core-2.0a1.tar.gz/tw2.core-2.0a1/tw2/core/__init__.py
@@ -0,0 +1,23 @@
+"""This file defines the ToscaWidgets public API.
+"""
+from core import (WidgetError)
+
+from template import (EngineError)
+
+from params import (Param, ChildParam, Variable, ChildVariable, Required,
+    Deferred, ParameterError, Auto)
+
+from widgets import (Widget, CompoundWidget, RepeatingWidget,
+    DisplayOnlyWidget, Page)
+
+from resources import (Link, JSLink, CSSLink, JSSource, JSFuncCall,
+    inject_resources, DirLink)
+
+from validation import (Validator, LengthValidator,
+    RegexValidator, IntValidator, OneOfValidator, DateValidator,
+    DateTimeValidator, ValidationError, Invalid, EmailValidator,
+    UrlValidator, IpAddressValidator, StringLengthValidator,
+    ListLengthValidator, RangeValidator, MatchValidator,
+    BoolValidator, safe_validate, EmptyField)
+
+from middleware import (make_middleware, dev_server)
---
+++ packages/tw2.core/tw2.core-2.0a1.tar.gz/tw2.core-2.0a1/tw2/core/mako_util.py
@@ -0,0 +1,26 @@
+import unicodedata
+from mako.runtime import Undefined
+
+from cgi import escape
+#from mako.filters import xml_escape
+
+__all__ = ["attrs"]
+
+_BOOLEAN_ATTRS = frozenset(['selected', 'checked', 'compact', 'declare',
+                            'defer', 'disabled', 'ismap', 'multiple',
+                            'nohref', 'noresize', 'noshade', 'nowrap'])
+
+def attrs(context, args=None, attrs=None):
+    # Emulates Genshi's AttrsDirective (poorly)
+    if isinstance(args, dict):
+        args = args.items()
+    if not args:
+        args = []
+    else:
+        args = args[:]
+    if attrs:
+        args.extend(attrs.items())
+    bools = _BOOLEAN_ATTRS
+
+    return u" ".join([u'%s="%s"' % (k, escape(unicode(k in bools and k or v), True))
+                      for k,v in args if (k not in bools and v is not None) or (k in bools and v)])
