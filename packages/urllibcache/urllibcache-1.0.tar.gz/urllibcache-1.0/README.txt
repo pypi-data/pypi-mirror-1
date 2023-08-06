ed497d3829a954cf969d0f394bca47628286c95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:05 +0000

    Add package urllibcache/1.0/urllibcache-1.0.tar.gz

---
+++ packages/urllibcache/urllibcache-1.0.tar.gz/urllibcache-1.0/README.txt
@@ -0,0 +1,28 @@
+urllibcache
+===========
+
+This is a simple caching handler for urllib2.
+
+Usage
+-----
+
+Either use the build_opener function which takes the path to the cache folder
+and returns an OpenerDirector instance. You can then use it's open method to
+open urls normally. If the file was already downloaded, then it will be taken
+from the cache.
+
+You can also use the CachedHandler wherever it's useful within urllib2. This
+also takes the path to cache folder as an argument of the constructor.
+
+Be aware that the caching is very simple. It detects incomplete downloads but
+doesn't take any caching headers into account. To invalidate the cache you
+have to delete the files in the cache folder.
+
+Changelog
+---------
+
+1.0 - 2008-07-22
+----------------
+
+* Initial release.
+  [fschulze]
---
+++ packages/urllibcache/urllibcache-1.0.tar.gz/urllibcache-1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+import os
+
+version = '1.0'
+
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+setup(
+    name='urllibcache',
+    version=version,
+    description="Simple urllib2 caching handler",
+    long_description=long_description[long_description.find('\n\n'):],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='urllib cache',
+    author='Florian Schulze',
+    author_email='florian.schulze@gmx.net',
+    url='http://svn.plone.org/svn/collective/urllibcache',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+    ],
+)
