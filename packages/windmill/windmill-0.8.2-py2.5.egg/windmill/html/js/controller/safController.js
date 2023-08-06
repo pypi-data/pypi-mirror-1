6d85dfee32cc4b5c536aa61a6fc6e1b32b392c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package windmill/0.8.2/windmill-0.8.2-py2.5.egg

---
+++ packages/windmill/windmill-0.8.2-py2.5.egg/windmill/html/js/controller/safController.js
@@ -0,0 +1,129 @@
+/*
+Copyright 2006-2007, Open Source Applications Foundation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+//Safari specific controller functions
+
+windmill.controller.what = function() {
+    alert('Safari');
+} 
+
+/**
+ * In non-IE browsers, getElementById() does not search by name.  Instead, we
+ * we search separately by id and name.
+ */
+windmill.controller.locateElementByIdentifier = function(identifier, inDocument, inWindow) {
+    return PageBot.prototype._locateElementById(identifier, inDocument, inWindow)
+            || PageBot.prototype._locateElementByName(identifier, inDocument, inWindow)
+            || null;
+};
+
+//there is a problem with checking via click in safari
+windmill.controller.check = function(param_object){
+   return windmill.controller.click(param_object);    
+}
+
+//Radio buttons are even WIERDER in safari
+windmill.controller.radio = function(param_object){
+    var element = this._lookupDispatch(param_object);
+    
+    element.checked = true;
+    
+    return true;
+}
+
+//Safari Click function
+windmill.controller.click = function(param_object){
+    var element = this._lookupDispatch(param_object);
+    if (!element){ return false; }
+    windmill.events.triggerEvent(element, 'focus', false);
+      
+      // For form element it is simple.
+      if (element['click']) {
+          element['click']();
+      }
+      else{
+        // And since the DOM order that these actually happen is as follows when a user clicks, we replicate.
+        if (element.nodeName != 'SELECT'){
+          windmill.events.triggerMouseEvent(element, 'mousedown', true);
+          windmill.events.triggerMouseEvent(element, 'mouseup', true);
+        }
+        windmill.events.triggerMouseEvent(element, 'click', true);
+      }
+   return true;
+};
+
+//Double click for Safari
+windmill.controller.doubleClick = function(param_object) {
+
+    var element = this._lookupDispatch(param_object);
+    if (!element){
+           return false;
+    }
+    
+    windmill.events.triggerEvent(element, 'focus', false);
+
+    // Trigger the mouse event.
+    windmill.events.triggerMouseEvent(element, 'dblclick', true);
+
+   /* if (this._windowClosed()) {
+      return;
+      }
+   */
+    windmill.events.triggerEvent(element, 'blur', false);
+    
+    return true;
+};
+
+//Type Function
+windmill.controller.type = function (param_object){
+
+  var element = this._lookupDispatch(param_object);
+  if (!element){
+    return false;
+  }
+
+  //clear the box
+  element.value = '';
+  //Get the focus on to the item to be typed in, or selected
+  windmill.events.triggerEvent(element, 'focus', false);
+  windmill.events.triggerEvent(element, 'select', true);
+
+  //Make sure text fits in the textbox
+  var maxLengthAttr = element.getAttribute("maxLength");
+  var actualValue = param_object.text;
+  var stringValue = param_object.text;
+   
+  if (maxLengthAttr != null) {
+    var maxLength = parseInt(maxLengthAttr);
+    if (stringValue.length > maxLength) {
+      //truncate it to fit
+      actualValue = stringValue.substr(0, maxLength);
+    }
+  }
+  
+  var s = actualValue;
+  for (var c = 0; c < s.length; c++){
+     element.value += s.charAt(c);
+     windmill.events.triggerKeyEvent(element, 'keydown', s.charAt(c), true, false,false, false,false);
+     windmill.events.triggerKeyEvent(element, 'keypress', s.charAt(c), true, false,false, false,false); 
+     windmill.events.triggerKeyEvent(element, 'keyup', s.charAt(c), true, false,false, false,false);
+  }
+  // DGF this used to be skipped in chrome URLs, but no longer.  Is xpcnativewrappers to blame?
+  //Another wierd chrome thing?
+  windmill.events.triggerEvent(element, 'change', true);
+   
+  return true;
+};
---
+++ packages/windmill/windmill-0.8.2-py2.5.egg/windmill/html/js/wm/ide/dx.js
@@ -0,0 +1,212 @@
+/*
+Copyright 2006-2007, Open Source Applications Foundation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+//DOM Explorer Functions
+windmill.ui.domexplorer = new function() {
+  var exploreState = false;
+  this.setExploreState = function() {
+    if (this.exploreState == true) { this.domExplorerOn(); }
+  }
+  //Reset the border to what it was before the mouse over
+  this.resetBorder = function(e) { e.target.style.border = ''; }
+  
+  //Display the id in the remote
+  this.setIdInRemote = function(e) {
+    if (windmill.ui.remote.selectedElement != null) {
+      $("domExp").style.display = 'none';
+    }
+    if (windmill.ui.remote.selectedElementOption != null) {
+        $("domExp").style.display = 'none';
+    }
+    
+    //if absolute xpath is not wanted try our best to get a better locater
+    if ($('useXpath').checked == false) {
+        if (e.target.id != "") {
+          $("domExp").innerHTML = "ID: " + e.target.id;
+        }
+        else if ((e.target.name != "") && (typeof(e.target.name) != "undefined")) {
+          $("domExp").innerHTML = "Name: " + e.target.name;
+        }
+        else if ((e.target.nodeName.toUpperCase() == "A")|| (e.target.parentNode.nodeName.toUpperCase() == "A")) {
+          $("domExp").innerHTML = "Link: " + e.target.innerHTML;
+        }
+        //if not just use the xpath
+        else {
+          var stringXpath = getXSPath(e.target);
+          $("domExp").innerHTML = 'XPath: ' + stringXpath;
+        }
+      }
+      else {
+        var stringXpath = getXSPath(e.target);
+        $("domExp").innerHTML = 'XPath: ' + stringXpath;
+      }
+
+      e.target.style.border = "1px solid yellow";
+      this.explorerUpdate(e);
+  };
+
+  this.explorerUpdate = function(e) {
+    e.cancelBubble = true;
+    if (windmill.browser.isIE == false) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+    if (windmill.ui.remote.selectedElementOption != null && e.altKey == false) {
+        var id = windmill.ui.remote.selectedElementOption.replace('option', '');
+        //Incase if that node has been removed somehow
+        try {
+          var a = $("domExp").innerHTML.split(': ');
+          //If the element is a link, get rid of the all the garbage
+          if (a[0] == 'link') {
+              a[1] = a[1].replace(/(<([^>]+)>)/ig, "");
+              a[1] = a[1].replace(/\n/g, "");
+          }
+          $(id + 'optionType').value = 'opt'+a[0].toLowerCase();
+          $(id + 'option').value = a[1];
+          $(id + 'option').focus();
+        }
+        catch(error) {
+          windmill.ui.results.writeResult('Error in dom explorer');
+        }
+    }
+    
+    if (windmill.ui.remote.selectedElement != null) {
+        var id = windmill.ui.remote.selectedElement.replace('locator', '');
+        //Incase if that node has been removed somehow
+        try {
+          var a = $("domExp").innerHTML.split(': ');
+          //If the element is a link, get rid of the all the garbage
+          if (a[0] == 'link') {
+              a[1] = a[1].replace(/(<([^>]+)>)/ig, "");
+              a[1] = a[1].replace(/\n/g, "");
+          }
+          $(id + 'locatorType').value = a[0].toLowerCase();
+          $(id + 'locator').value = a[1];
+          $(id + 'locator').focus();
+        }
+        catch(error) {
+          windmill.ui.results.writeResult('Error in dom explorer');
+        }
+    }
+    
+  };
+  
+  this.showMouseCoords = function(e){
+    $('mouseExp').innerHTML = '('+e.clientX + ',' + e.clientY+')';
+  }
+  
+  this.explorerClick = function(e) { 
+    var optId = windmill.ui.remote.selectedElementOption;
+    //if an option section is selected and the altKey is down append the mouse coords
+    if ((optId != null) && (e.altKey)){
+      if ($(optId).value == ""){
+        $(optId).value += '('+e.clientX+','+e.clientY+'),';
+      }
+      else{
+        $(optId).value += '('+e.clientX+','+e.clientY+')';
+      }
+    }
+    else{ window.focus();}
+  };
+  //Set the listeners for the dom explorer
+  this.domExplorerOn = function() {
+    //Display the mouse coords in the IDE
+    fleegix.event.listen(opener.document.body, 'onmousemove', windmill.ui.domexplorer, 'showMouseCoords');
+    
+    this.exploreState = true;
+    try {
+      $('explorer').src = 'img/xoff.png';
+      $('domExp').style.display = 'block';
+      $('domExp').innerHTML = '';
+      this.dxRecursiveBind(opener);
+    }
+    catch(error) {
+      windmill.ui.results.writeResult('You must not have set your URL correctly when launching Windmill, we are getting cross domain exceptions.');
+      $('explorer').src = 'img/xon.png';
+      this.exploreState = false;
+    }
+  };
+
+  //Remove the listeners for the dom explorer
+  this.domExplorerOff = function() {
+    //Mouse coords display off
+    fleegix.event.unlisten(opener.document.body, 'onmousemove', windmill.ui.domexplorer, 'showMouseCoords');
+    $('mouseExp').innerHTML = "";
+    this.exploreState = false;
+
+    try {
+      //Reset the selected element
+      windmill.ui.remote.selectedElement = null;
+      windmill.ui.remote.selectedElementOption = null;
+      
+      $('explorer').src = 'img/xon.png';
+      $('domExp').style.display = 'none';
+      $('domExp').innerHTML = '';
+      this.dxRecursiveUnBind(opener);
+    }
+    catch(error) {
+      windmill.ui.results.writeResult('You must not have set your URL correctly when launching Windmill, we are getting cross domain exceptions.');
+      $('explorer').src = 'img/xon.png';
+      this.exploreState = false;
+    }
+  };
+
+  //Recursively bind to all the iframes and frames within
+  this.dxRecursiveBind = function(frame) {
+    this.dxRecursiveUnBind(frame);
+
+    fleegix.event.listen(frame.document, 'onmouseover', this, 'setIdInRemote');
+    fleegix.event.listen(frame.document, 'onmouseout', this, 'resetBorder');
+    fleegix.event.listen(frame.document, 'onclick', this, 'explorerClick');
+
+    var iframeCount = frame.window.frames.length;
+    var iframeArray = frame.window.frames;
+
+    for (var i = 0; i < iframeCount; i++){
+      try {
+        fleegix.event.listen(iframeArray[i].document, 'onmouseover', this, 'setIdInRemote');
+        fleegix.event.listen(iframeArray[i].document, 'onmouseout', this, 'resetBorder');
+        fleegix.event.listen(iframeArray[i].document, 'onclick', this, 'explorerClick');
+        this.dxRecursiveBind(iframeArray[i]);
+      }
+      catch(error) {
+        windmill.ui.results.writeResult('There was a problem binding to one of your iframes, is it cross domain? Binding to all others.' + error);
+      }
+    }
+  };
+
+  this.dxRecursiveUnBind = function(frame) {
+    fleegix.event.unlisten(frame.document, 'onmouseover', this, 'setIdInRemote');
+    fleegix.event.unlisten(frame.document, 'onmouseout', this, 'resetBorder');
+    fleegix.event.unlisten(frame.document, 'onclick', this, 'explorerClick');
+
+    var iframeCount = frame.window.frames.length;
+    var iframeArray = frame.window.frames;
+
+    for (var i = 0; i < iframeCount; i++){
+      try {
+        fleegix.event.unlisten(iframeArray[i].document, 'onmouseover', this, 'setIdInRemote');
+        fleegix.event.unlisten(iframeArray[i].document, 'onmouseout', this, 'resetBorder');
+        fleegix.event.unlisten(iframeArray[i].document, 'onclick', this, 'explorerClick');
+        this.dxRecursiveUnBind(iframeArray[i]);
+      }
+      catch(error) {
+        windmill.ui.results.writeResult('There was a problem binding to one of your iframes, is it cross domain? Binding to all others.' + error);
+      }
+    }
+  };
+
+};
