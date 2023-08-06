a690cffdcb9533617f492f8ad58a1009880c6579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:53 +0000

    Add package redomino.autodelete/0.1r3/redomino.autodelete-0.1r3-py2.5.egg

---
+++ packages/redomino.autodelete/redomino.autodelete-0.1r3-py2.5.egg/redomino/autodelete/interfaces.py
@@ -0,0 +1,28 @@
+from zope.interface import Interface
+
+from zope.schema import Date
+from zope.annotation.interfaces import IAttributeAnnotatable
+
+from redomino.autodelete import autodeleteMessageFactory as _
+
+class IExpires(Interface):
+    """ Expirable adapter for auto-delete objects enabled """
+    delete_date = Date(title=_(u'Absolute delete date'),
+                       description=_(u'Put here the delete date (format: YYYY-MM-DD)'),
+                       required=True)
+    def to_be_deleted():
+        """ Returns True if the element is expired """
+
+    def flush():
+        """ Flush and reindex metadata attributes """
+
+
+class IExpirable(IAttributeAnnotatable):
+    """ Marker interface for expirable objects (objects that could be deleted automatically) and can be annotable """
+
+class IAutodeleteControlPanel(Interface):
+    """ Marker interface for the AutodeleteControlPanel """
+
+    def delete():
+        """ Delete action """
+
