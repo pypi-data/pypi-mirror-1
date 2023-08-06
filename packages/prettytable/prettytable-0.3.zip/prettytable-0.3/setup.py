283802051f6809dbff6a23bfa0ad235079622b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:57 +0000

    Add package prettytable/0.3/prettytable-0.3.zip

---
+++ packages/prettytable/prettytable-0.3.zip/prettytable-0.3/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='prettytable',
+    version='0.3',
+    description='A simple Python library for easily displaying tabular data in a visually appealing ASCII table format',
+    author='Luke Maurits',
+    author_email='luke@maurits.id.au',
+    url='http://www.luke.maurits.id.au/software/prettytable',
+    py_modules=['prettytable']
+)
