776d0ee8e07d5af00a9ec516cf5aa6b3d3e410aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:07 +0000

    Add package tw.dojo/0.8/tw.dojo-0.8-py2.5.egg

---
+++ packages/tw.dojo/tw.dojo-0.8-py2.5.egg/tw/dojo/__init__.py
@@ -0,0 +1,2 @@
+from dojo import dojo_js, dojo, Dojo, DojoInlineEditFarm, DojoButton, DojoItemFileReadStore, DojoFilteringSelect, DojoGridColumn, DojoGrid, DojoItemFileWriteStore
+__all__ = ["dojo_js", "dojo", "Dojo", "DojoInlineEditFarm", "DojoButton", "DojoItemFileReadStore", "DojoFilteringSelect", "DojoGridColumn", "DojoGrid", "DojoItemFileWriteStore"]
