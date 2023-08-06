03b2fc1df8d4e4f9542387606cf1009c3aefe5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package Products.plonehrm/2.1.1/Products.plonehrm-2.1.1.tar.gz

---
+++ packages/Products.plonehrm/Products.plonehrm-2.1.1.tar.gz/Products.plonehrm-2.1.1/Products/plonehrm/validator.py
@@ -0,0 +1,41 @@
+from Products.validation.interfaces import ivalidator
+from DateTime import DateTime
+from zope.i18n import translate
+from Products.plonehrm import PloneHrmMessageFactory as _
+
+class DateValidator:
+    __implements__ = (ivalidator,)
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def __call__(self, value, *args, **kwargs):
+       now = DateTime()
+       value = DateTime(value)
+       if value > now:
+          if value.year() > now.year():
+              error = _("Date is supposed to be in the past, please enter a valid year.")  
+          elif value.month() > now.month():
+              error = _("Date is supposed to be in the past, please enter a valid month.")
+          elif value.day() > now.day():
+              error = _("Date is supposed to be in the past, please enter a valid day.")
+          else:
+              error = ''
+          if error:
+              return translate(error)
+       return True
+              
+class AgeValidator:
+    __implements__ = (ivalidator,)
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def __call__(self, value, *args, **kwargs):
+       now = DateTime()
+       value = DateTime(value)
+      
+       if value.year() > now.year()-12:
+          error = _("You are too young")
+          return translate(error)
+       return True
