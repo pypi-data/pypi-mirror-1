2e35931c16b88f96e144ca7259a6d2165b86e714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:09 +0000

    Add package quills.remoteblogging/1.7.0b1/quills.remoteblogging-1.7.0b1.zip

---
+++ packages/quills.remoteblogging/quills.remoteblogging-1.7.0b1.zip/quills.remoteblogging-1.7.0b1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '1.7.0b1'
+
+setup(name='quills.remoteblogging',
+      version=version,
+      description="Interfaces and implementation of remote blogging for Zope3/Plone.",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Weblog',
+      author='Tim Hicks',
+      author_email='tim@sitefusion.co.uk',
+      url='http://svn.plone.org/svn/collective/quills.remoteblogging/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quills'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'quills.core>=1.7.0b1,<=1.7.99',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
