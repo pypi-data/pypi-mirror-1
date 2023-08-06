60818fc88a726f35d054acf180839e67250e8145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:29 +0000

    Add package z3c.pt/1.0a7/z3c.pt-1.0a7.tar.gz

---
+++ packages/z3c.pt/z3c.pt-1.0a7.tar.gz/z3c.pt-1.0a7/src/z3c/pt/__init__.py
@@ -0,0 +1,9 @@
+from pagetemplate import PageTemplate
+from pagetemplate import PageTemplateFile
+from pagetemplate import ViewPageTemplate
+from pagetemplate import ViewPageTemplateFile
+
+from texttemplate import TextTemplate
+from texttemplate import TextTemplateFile
+from texttemplate import ViewTextTemplate
+from texttemplate import ViewTextTemplateFile
