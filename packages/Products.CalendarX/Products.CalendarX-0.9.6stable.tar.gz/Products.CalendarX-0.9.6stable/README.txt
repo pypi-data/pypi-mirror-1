81ad39a9049f2d6974660ba0bf17ef3cc74c932b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:50 +0000

    Add package Products.CalendarX/0.9.6stable/Products.CalendarX-0.9.6stable.tar.gz

---
+++ packages/Products.CalendarX/Products.CalendarX-0.9.6stable.tar.gz/Products.CalendarX-0.9.6stable/README.txt
@@ -0,0 +1,26 @@
+==================
+Products.CalendarX
+==================
+
+This product restructured from http://svn.plone.org/svn/collective/CalendarX
+to follow the buildout structure.
+
+The new repository for Products.CalendarX is
+http://svn.plone.org/svn/collective/Products.CalendarX
+
+This version is identical to 0.9.2 but packaged as an egg to leverage
+zc.buildout and setuptools.
+
+Mode documentation is available in Products/CalendarX/README.txt and
+Products/CalendarX/docs/*
+
+A more Plone 3ish version is actually at work in
+http://svn.plone.org/svn/collective/Products.CalendarX/branches/plone3refactoring
+and will include :
+
+* ZCA views in place of CMF skin layers
+* More unit tests
+* Using jQuery where possible to keep JS small.
+
+Contributors are welcome.
+
---
+++ packages/Products.CalendarX/Products.CalendarX-0.9.6stable.tar.gz/Products.CalendarX-0.9.6stable/setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+import os
+
+def _textFromPath(*names):
+    here = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(here, *names)
+    return open(path, 'r').read().strip()
+
+version = _textFromPath('Products', 'CalendarX', 'version.txt')
+long_description = '\n\n'.join(
+    (_textFromPath('README.txt'),
+     _textFromPath('docs', 'HISTORY.txt')
+     ))
+
+setup(name='Products.CalendarX',
+      version=version,
+      description=("CalendarX is a customizable, open source metacalendar "
+                   "application written for the Plone content management "
+                   "system on top of Zope and Python."),
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "Programming Language :: Python",
+          "Operating System :: OS Independent",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Office/Business :: Scheduling",
+          "Natural Language :: Catalan",
+          "Natural Language :: Czech",
+          "Natural Language :: Danish",
+          "Natural Language :: English",
+          "Natural Language :: German",
+          "Natural Language :: French",
+          "Natural Language :: Italian",
+          "Natural Language :: Japanese",
+          "Natural Language :: Dutch",
+          "Natural Language :: Portuguese (Brazilian)",
+          "Natural Language :: Swedish",
+        ],
+      keywords='plone calendar',
+      author='Lupa Zurven',
+      author_email='lupa at zurven dot com',
+      url='http://svn.plone.org/svn/collective/Products.CalendarX/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
