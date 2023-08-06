ae1926cb4b069c845dfa020cc040ad4e8efe6034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:54 +0000

    Add package zope.formlib/3.5.2/zope.formlib-3.5.2.zip

---
+++ packages/zope.formlib/zope.formlib-3.5.2.zip/zope.formlib-3.5.2/setup.py
@@ -0,0 +1,67 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.formlib package
+
+$Id: setup.py 96899 2009-02-21 12:20:51Z hannosch $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.formlib',
+      version = '3.5.2',
+      url='http://pypi.python.org/pypi/zope.formlib',
+      license='ZPL 2.1',
+      description='Form generation and validation library for Zope',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(read('README.txt')
+                        + '\n\n' +
+                        read('CHANGES.txt')
+                        + '\n\n' +
+                        read('src', 'zope', 'formlib', 'form.txt')
+                        + '\n\n' +
+                        read('src', 'zope', 'formlib', 'errors.txt')
+                        + '\n\n' +
+                        read('src', 'zope', 'formlib', 'namedtemplate.txt')
+                        ),
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope',],
+      extras_require=dict(
+          test=['zope.app.testing', 'zope.app.zcmlfiles']
+          ),
+      install_requires=['setuptools',
+                        'pytz',
+                        'zope.app.container',
+                        'zope.app.form',
+                        'zope.app.pagetemplate',
+                        'zope.component',
+                        'zope.deferredimport',
+                        'zope.event',
+                        'zope.i18n',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.lifecycleevent',
+                        'zope.publisher',
+                        'zope.schema>=3.5.1',
+                        'zope.security',
+                        'zope.traversing',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
