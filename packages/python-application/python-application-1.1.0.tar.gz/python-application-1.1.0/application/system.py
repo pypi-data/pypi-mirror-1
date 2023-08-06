d4f8e175a373f955f1982fddf99562d53898c47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:02 +0000

    Add package python-application/1.1.0/python-application-1.1.0.tar.gz

---
+++ packages/python-application/python-application-1.1.0.tar.gz/python-application-1.1.0/application/system.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2006-2007 Dan Pascu. See LICENSE for details.
+#
+
+"""Interaction with the underlying operating system"""
+
+__all__ = ['default_host_ip', 'unlink']
+
+## System variables
+
+# The default IP address of this system. This is the IP address of the network
+# interface that has the default route assigned to it, or in other words the
+# IP address that will be used when making connections to the internet.
+import socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('1.2.3.4', 56))
+        default_host_ip = s.getsockname()[0]
+    finally:
+        s.close()
+        del s
+except socket.error:
+    default_host_ip = None
+del socket
+
+## Functions
+
+def unlink(path):
+    """Remove a file ignoring errors"""
+    from os import unlink as os_unlink
+    try:    os_unlink(path)
+    except: pass
+
+
---
+++ packages/python-application/python-application-1.1.0.tar.gz/python-application-1.1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from distutils.core import setup, Extension
+from application import __version__
+
+setup(name         = "python-application",
+      version      = __version__,
+      author       = "Dan Pascu",
+      author_email = "dan@ag-projects.com",
+      url          = "http://ag-projects.com/",
+      download_url = "http://cheeseshop.python.org/pypi/python-application/%s" % __version__,
+      description  = "Basic building blocks for python applications",
+      long_description = open('README').read(),
+      license      = "LGPL",
+      platforms    = ["Platform Independent"],
+      classifiers  = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+      ],
+      packages     = ['application', 'application.configuration', 'application.debug', 'application.python'])
