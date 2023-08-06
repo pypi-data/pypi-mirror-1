447d1d373b3d3ebf4d842ef5ef003b1010bffd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:35 +0000

    Add package z3c.jbot/0.5.1/z3c.jbot-0.5.1.tar.gz

---
+++ packages/z3c.jbot/z3c.jbot-0.5.1.tar.gz/z3c.jbot-0.5.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.5.1'
+
+setup(name='z3c.jbot',
+      version=version,
+      description="Drop-in template overrides.",
+      long_description="\n".join((open("README.txt").read(), open("CHANGES.txt").read())),
+      classifiers=[
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='page template override',
+      author='Malthe Borch',
+      author_email='mborch@gmail.com',
+      url='',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['z3c'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.pagetemplate',
+          'zope.component',
+          'zope.configuration',
+          'zope.security',
+          'zope.publisher',
+          'zope.app.component',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ packages/z3c.jbot/z3c.jbot-0.5.1.tar.gz/z3c.jbot-0.5.1/z3c/jbot/__init__.py
@@ -0,0 +1,101 @@
+from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+
+try:
+    from Acquisition.interfaces import IAcquirer
+except ImportError:
+    IAcquirer = None
+
+import utility
+import logging
+
+logger = logging.getLogger('jbot')
+
+PT_CLASSES = [PageTemplateFile]
+
+try:
+    import Products.PageTemplates.PageTemplateFile
+    PT_CLASSES.append(Products.PageTemplates.PageTemplateFile.PageTemplateFile)
+except:
+    pass
+
+registry = {}
+
+def get(template, view=None, cls=None):
+    layer = utility.getLayer()
+    key = layer, template
+    inst = registry.get(key)
+    if inst is None:
+        cls = type(template)
+        inst = registry[key] = cls.__new__(cls)
+        inst.__dict__ = template.__dict__.copy()
+
+    for manager in utility.getManagers():
+        # register template; this call returns ``True`` if the
+        # template was invalidated (changed filename)
+        if manager.registerTemplate(inst, template):
+            inst._v_last_read = False
+            break
+
+    if view is not None and IAcquirer is not None:
+        if IAcquirer.providedBy(inst):
+            return inst.__of__(view)
+
+    return inst
+
+try:
+    import five.pt.pagetemplate
+except ImportError:
+    pass
+else:
+    pt_class = five.pt.pagetemplate.ViewPageTemplateFile
+    bind = pt_class.__get__
+
+    def get_and_bind(template, view=None, cls=None):
+        inst = get(template, view, cls)
+        if inst._v_last_read is False:
+            inst.registry.purge()
+            inst.read()
+        return bind(inst, view, cls)
+
+    pt_class.__get__ = get_and_bind
+    logger.info(repr(pt_class))
+
+for pt_class in PT_CLASSES:
+    pt_class.__get__ = get
+    logger.info(repr(pt_class))
+
+try:
+    import Products.CMFCore.FSObject
+except ImportError:
+    pass
+else:
+    fs_class = Products.CMFCore.FSObject.FSObject
+
+    def get_skin_obj(obj, view=None, cls=None):
+        layer = utility.getLayer()
+        key = layer, obj
+        inst = registry.get(key)
+        if inst is None:
+            cls = type(obj)
+            inst = registry[key] = cls.__new__(cls)
+            inst.__dict__ = obj.__dict__.copy()
+
+        for manager in utility.getManagers():
+            # register template; this call returns ``True`` if the
+            # template was invalidated (changed filename)
+            if manager.registerTemplate(inst, obj):
+                inst._parsed = False
+                inst.getObjectFSPath()
+
+        return inst
+
+    def get_filename(obj, *args):
+        return obj._filepath
+
+    def set_filename(obj, value, *args):
+        obj._filepath = value
+
+    fs_class.__get__ = get_skin_obj
+    fs_class.filename = property(get_filename, set_filename)
+
+    logger.info(repr(fs_class))
