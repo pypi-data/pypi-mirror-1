a53b7e9b648d18b43502273aaf28702dca7d5abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:49 +0000

    Add package pyth/0.5.3/pyth-0.5.3.tar.gz

---
+++ packages/pyth/pyth-0.5.3.tar.gz/pyth-0.5.3/examples/reading/rtf15.py
@@ -0,0 +1,8 @@
+from pyth.plugins.rtf15.reader import Rtf15Reader
+from pyth.plugins.xhtml.writer import XHTMLWriter
+
+doc = Rtf15Reader.read(open('sample.rtf'))
+
+print XHTMLWriter.write(doc, pretty=True).getvalue()
+
+
