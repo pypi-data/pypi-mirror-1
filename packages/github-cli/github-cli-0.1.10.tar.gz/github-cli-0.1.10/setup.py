ebe9a2b1e4bb490b0bb711405498a0332499d71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:35 +0000

    Add package github-cli/0.1.10/github-cli-0.1.10.tar.gz

---
+++ packages/github-cli/github-cli-0.1.10.tar.gz/github-cli-0.1.10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '0.1.10'
+
+setup(
+    name = "github-cli",
+    version = version,
+    url = 'http://github.com/jsmits/github-cli',
+    license = 'BSD',
+    description = "A command-line interface to Github's API.",
+    long_description = """for now, `github-cli <http://github.com/jsmits/github-cli/>`_ 
+provides a script called ``gh-i``, that can be used to access all of `GitHub 
+<http://www.github.com/>`_'s documented `Issues API <http://develop.github.com/p/issues.html>`_ 
+(v2) functionality from your command-line""",
+    author = 'Sander Smits',
+    author_email = 'jhmsmits@gmail.com',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires = ['setuptools', 'simplejson'],
+    entry_points="""
+    [console_scripts]
+    gh-i = github.issues:main
+    """,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Bug Tracking',
+    ],
+)
