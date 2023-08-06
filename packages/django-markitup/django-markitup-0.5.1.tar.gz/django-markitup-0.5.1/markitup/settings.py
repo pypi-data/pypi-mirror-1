d7806ee74fbcbe6636985f0b878bdd4e76e4f2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:24 +0000

    Add package django-markitup/0.5.1/django-markitup-0.5.1.tar.gz

---
+++ packages/django-markitup/django-markitup-0.5.1.tar.gz/django-markitup-0.5.1/markitup/settings.py
@@ -0,0 +1,18 @@
+"""
+settings for django-markitup
+
+Time-stamp: <2009-11-11 12:28:49 carljm settings.py>
+
+"""
+from django.conf import settings
+import posixpath
+
+
+MARKITUP_PREVIEW_FILTER = getattr(settings, 'MARKITUP_PREVIEW_FILTER',
+                                  getattr(settings, 'MARKITUP_FILTER', None))
+MARKITUP_MEDIA_URL = getattr(settings, 'MARKITUP_MEDIA_URL', settings.MEDIA_URL)
+MARKITUP_SET = getattr(settings, 'MARKITUP_SET', 'markitup/sets/default')
+MARKITUP_SKIN = getattr(settings, 'MARKITUP_SKIN', 'markitup/skins/simple')
+JQUERY_URL = getattr(
+    settings, 'JQUERY_URL',
+    'http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js')
