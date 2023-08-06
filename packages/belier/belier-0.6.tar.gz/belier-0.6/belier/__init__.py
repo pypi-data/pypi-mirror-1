b9bdf34f703b3e36011577fffb9c75617e580770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:45 +0000

    Add package belier/0.6/belier-0.6.tar.gz

---
+++ packages/belier/belier-0.6.tar.gz/belier-0.6/belier/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) [2008] [Carl Chenet]
+# Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le
+# modifier au titre des clauses de la Licence Publique Générale GNU, telle que
+# publiée par la Free Software Foundation ; soit la version 2 de la Licence,
+# ou (à votre discrétion) une version ultérieure quelconque.
+#
+# Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE
+# GARANTIE ; sans même une garantie implicite de COMMERCIABILITE ou DE
+# CONFORMITE A UNE UTILISATION PARTICULIERE.
+
+# Voir la Licence Publique Générale GNU pour plus de détails. Vous devriez
+# avoir reçu un exemplaire de la Licence Publique Générale GNU avec ce
+# programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+# Fichier vide pour le paquet
+"""Module belier"""
