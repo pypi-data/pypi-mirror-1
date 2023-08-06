aa724aa2f7c0c271e11ff7053191479009495a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:29 +0000

    Add package improviser/0.5.6.4/improviser-0.5.6.4.tar.gz

---
+++ packages/improviser/improviser-0.5.6.4.tar.gz/improviser-0.5.6.4/improviser/Musicians/Accompaniment/SimpleChordInstrument.py
@@ -0,0 +1,18 @@
+from Instrument import Instrument
+from mingus.containers.NoteContainer import NoteContainer
+from random import random
+
+class SimpleChordInstrument(Instrument):
+
+	def __init__(self, params):
+		Instrument.__init__(self, params)
+
+	def generate_note(self, state):
+		wild = state['wild']
+
+		if state["tick"] % state["resolution"] == 0 and \
+				random() < 1.0 * wild:
+			return NoteContainer(state["chord"])
+		else:
+			return None
+
