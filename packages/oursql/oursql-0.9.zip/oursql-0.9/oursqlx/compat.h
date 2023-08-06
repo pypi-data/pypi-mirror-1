b5bc4442112ea9d50904675e798566f5a876f378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:05 +0000

    Add package oursql/0.9/oursql-0.9.zip

---
+++ packages/oursql/oursql-0.9.zip/oursql-0.9/oursqlx/compat.h
@@ -0,0 +1,40 @@
+/* I really don't understand the thought process behind most of mysql. */
+#ifdef _WIN32
+#  include "my_global.h"
+#endif
+#if defined(FREEBSD) || defined(__FreeBSD__)
+#  include <stdint.h>
+#endif
+#include "mysql.h"
+#include "mysqld_error.h"
+#include "errmsg.h"
+#include "Python.h"
+#if PY_VERSION_HEX < 0x02050000
+#  define PyBaseExceptionObject PyObject
+#  define PyErr_WarnEx(exc, msg, _) PyErr_Warn(exc, msg)
+  typedef int Py_ssize_t;
+#endif
+
+#if MYSQL_VERSION_ID < 40102
+#error "This driver requires at least MySQL 4.1.2."
+#endif
+
+enum _oursqlx_exception_type {
+    _oursqlx_InterfaceError,
+    _oursqlx_DataError,
+    _oursqlx_OperationalError,
+    _oursqlx_IntegrityError,
+    _oursqlx_InternalError,
+    _oursqlx_ProgrammingError,
+    _oursqlx_NotSupportedError,
+    _oursqlx_PermissionsError,
+    _oursqlx_UnknownError,
+};
+
+my_bool _oursqlx_init_stmt_cursor(MYSQL_STMT *, unsigned long *);
+my_bool _oursqlx_stmt_set_prefetch_rows(MYSQL_STMT *, unsigned long *);
+int _oursqlx_stmt_cursor_prefetch(MYSQL_STMT *, int *);
+int _oursqlx_PyObject_AsReadBuffer(PyObject *, void **, Py_ssize_t *);
+enum _oursqlx_exception_type _oursqlx_exc_from_errno(int);
+PyObject *_oursqlx_escape_string(MYSQL *, PyObject *);
+PyObject *_oursqlx_generate_errno_dict(void);
