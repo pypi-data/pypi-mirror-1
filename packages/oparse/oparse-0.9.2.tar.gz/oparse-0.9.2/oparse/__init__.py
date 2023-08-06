fa6b4467b1acd64db0c2fbaac00b404a6979d4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:25 +0000

    Add package oparse/0.9.2/oparse-0.9.2.tar.gz

---
+++ packages/oparse/oparse-0.9.2.tar.gz/oparse-0.9.2/oparse/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Olli Wang (olliwang@ollix.com) on 2009-11-17.
+Copyright (c) 2009 Ollix. All rights reserved.
+"""
+
+from oparse.command import Command
+from oparse.parser import OptionParser
+from oparse.exceptions import CommandError
---
+++ packages/oparse/oparse-0.9.2.tar.gz/oparse-0.9.2/oparse/exceptions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+exceptions.py
+
+Created by Olli Wang (olliwang@ollix.com) on 2009-11-17.
+Copyright (c) 2009 Ollix. All rights reserved.
+"""
+
+class CommandError(Exception):
+    pass
