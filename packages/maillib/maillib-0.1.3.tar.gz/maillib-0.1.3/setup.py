b3ca43c80149e29830cc05c59ef348275b6bb5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:48 +0000

    Add package maillib/0.1.3/maillib-0.1.3.tar.gz

---
+++ packages/maillib/maillib-0.1.3.tar.gz/maillib-0.1.3/setup.py
@@ -0,0 +1,28 @@
+import os
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README')).read()
+
+setup(
+    name = "maillib",
+    version = "0.1.3",
+    description = "A simplified interface to Python's email package.",
+    long_description = README,
+    author = 'Joseph Kocherhans',
+    author_email = 'jkocherhans@gmail.com',
+    license='BSD',
+    url='http://github.com/jkocherhans/maillib',
+    packages = ['maillib'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email'
+    ],
+    test_suite='maillib.tests.all',
+    install_requires=['chardet']
+)
