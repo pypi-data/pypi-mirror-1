7693250f5e9d7ac9921fdfdc88cfa1bdc46592a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:49 +0000

    Add package pydelta/1.5.0/pydelta-1.5.0.tar.gz

---
+++ packages/pydelta/pydelta-1.5.0.tar.gz/pydelta-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='pydelta',
+      version='1.5.0',
+      description='Library for accessing data in the DELTA taxonomy description format',
+      author='Mauro Cavalcanti, Thomas Kluyver',
+      author_email='maurobio@gmail.com, takowl@gmail.com',
+      url='http://freedelta.sourceforge.net/',
+      py_modules=['pydelta'],
+      license='GPL'
+      )
+
