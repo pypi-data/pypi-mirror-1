b75d0f1f2001253926ed0b04a60e2b73ac076bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:33 +0000

    Add package gocept.mochikit/1.4.2/gocept.mochikit-1.4.2.tar.gz

---
+++ packages/gocept.mochikit/gocept.mochikit-1.4.2.tar.gz/gocept.mochikit-1.4.2/src/gocept/mochikit/tests.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import os
+import unittest
+
+from zope.testing import doctest
+
+import zope.app.testing.functional
+
+
+Layer = zope.app.testing.functional.ZCMLLayer(
+    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
+    __name__, 'gocept.mochikit', allow_teardown=True)
+
+
+def FunctionalDocFileSuite(*paths, **kw):
+    try:
+        layer = kw['layer']
+    except KeyError:
+        layer = Layer
+    else:
+        del kw['layer']
+    kw['package'] = doctest._normalize_module(kw.get('package'))
+    test = zope.app.testing.functional.FunctionalDocFileSuite(
+        *paths, **kw)
+    test.layer = layer
+    return test
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(FunctionalDocFileSuite(
+        'viewletmanager.txt'))
+    return suite
