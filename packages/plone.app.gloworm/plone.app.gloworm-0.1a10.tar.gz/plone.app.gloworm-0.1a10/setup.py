2aec8bb67c37380e0fb31b0f4ffd28e351b051e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:32 +0000

    Add package plone.app.gloworm/0.1a10/plone.app.gloworm-0.1a10.tar.gz

---
+++ packages/plone.app.gloworm/plone.app.gloworm-0.1a10.tar.gz/plone.app.gloworm-0.1a10/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1a10'
+
+setup(name='plone.app.gloworm',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='WebLion Group, Penn State University',
+      author_email='support@weblion.psu.edu',
+      url='https://weblion.psu.edu/svn/weblion/weblion/Products.Gloworm/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'Products.Gloworm'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
