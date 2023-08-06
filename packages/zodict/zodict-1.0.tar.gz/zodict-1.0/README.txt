8899310ae3d255cc87f81e0e65b12d92011f66c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:01 +0000

    Add package zodict/1.0/zodict-1.0.tar.gz

---
+++ packages/zodict/zodict-1.0.tar.gz/zodict-1.0/README.txt
@@ -0,0 +1,11 @@
+
+Ordered dictionary which simply implements the corresponding
+``zope.interface.common.mapping`` interfaces.
+
+  >>> from zodict import zodict
+  >>> zod = zodict()
+  
+Credits
+-------
+
+  -Written by ``rnix`` (2009-03-17)
---
+++ packages/zodict/zodict-1.0.tar.gz/zodict-1.0/setup.py
@@ -0,0 +1,39 @@
+# Copyright 2003-2009, BlueDynamics Alliance - http://bluedynamics.com
+# GNU General Public License Version 2 or later
+
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+shortdesc = 'zope.interface compliant ordered dictionary.'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+setup(name='zodict',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Framework :: Zope3',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python', 
+            'Topic :: Software Development',       
+      ],
+      keywords='odict',
+      author='bearophile, rnix',
+      author_email='rnix@squarewave.at',
+      url=u'https://svn.plone.org/svn/archetypes/AGX/odict',
+      license='GNU General Public Licence',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          'odict',
+          'zope.interface',
+      ],
+      extras_require = {},
+      )
