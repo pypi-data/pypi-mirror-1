cc60e2570123b0030194445889198bc69c596b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package mingus/0.2.0.1/mingus-0.2.0.1.tar.gz

---
+++ packages/mingus/mingus-0.2.0.1.tar.gz/mingus-0.2.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name= "mingus",
+	  version = "0.2.0.1",
+	  description = "Music theory and notation package",
+	  author = "Bart Spaans",
+	  author_email = "onderstekop@gmail.com",
+	  url = "http://mingus.googlecode.com/",
+	  packages = ['mingus', 'mingus.core', 'mingus.containers', 'mingus.extra'],
+	  )
