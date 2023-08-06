2fcf78c6c98d0693a65a6381808ff77faf558637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:42 +0000

    Add package redturtle.portlet.lightreviewlist/0.1.0/redturtle.portlet.lightreviewlist-0.1.0.tar.gz

---
+++ packages/redturtle.portlet.lightreviewlist/redturtle.portlet.lightreviewlist-0.1.0.tar.gz/redturtle.portlet.lightreviewlist-0.1.0/README.txt
@@ -0,0 +1,8 @@
+Introduction
+============
+
+A very simple and static portlet (right now) done to substitute the basic Plone
+**review list** ones.
+
+It only display a link to the **full_revew_list** view.
+
---
+++ packages/redturtle.portlet.lightreviewlist/redturtle.portlet.lightreviewlist-0.1.0.tar.gz/redturtle.portlet.lightreviewlist-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='redturtle.portlet.lightreviewlist',
+      version=version,
+      description="A light version of the Plone review_list portlet",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone portlet review_list',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://svn.plone.org/svn/collective/redturtle.portlet.lightreviewlist',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['redturtle', 'redturtle.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
