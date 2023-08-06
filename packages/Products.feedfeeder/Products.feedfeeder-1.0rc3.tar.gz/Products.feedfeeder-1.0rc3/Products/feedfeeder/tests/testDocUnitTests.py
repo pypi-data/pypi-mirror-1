e10d40cdf5587420b285bb7f7680fb5fbfd3313a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:09 +0000

    Add package Products.feedfeeder/1.0rc3/Products.feedfeeder-1.0rc3.tar.gz

---
+++ packages/Products.feedfeeder/Products.feedfeeder-1.0rc3.tar.gz/Products.feedfeeder-1.0rc3/Products/feedfeeder/tests/testDocUnitTests.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import unittest
+from zope.testing import doctestunit
+
+
+def test_suite():
+    return unittest.TestSuite((
+        doctestunit.DocFileSuite('feedconsuming.txt',
+                                 package='Products.feedfeeder.doc'),
+        ))
