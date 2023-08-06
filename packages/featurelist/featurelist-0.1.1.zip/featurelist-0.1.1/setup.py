2eac20524b5d1872760d80315d99928fe6773453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:53 +0000

    Add package featurelist/0.1.1/featurelist-0.1.1.zip

---
+++ packages/featurelist/featurelist-0.1.1.zip/featurelist-0.1.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='featurelist',
+      version='0.1.1',
+      description='Submit and query feature requests from featurelist.org',
+      author='Matthew from featurelist.org',
+      author_email='support@featurelist.org',
+      packages=['featurelist'],
+      py_modules = ['sitecustomize'],
+      url='http://featurelist.org',
+     )
