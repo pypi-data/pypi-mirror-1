8b1493242b8b354fe7e02797a62bbe402e7bb656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:58 +0000

    Add package gocept.nginx/0.9.2/gocept.nginx-0.9.2.tar.gz

---
+++ packages/gocept.nginx/gocept.nginx-0.9.2.tar.gz/gocept.nginx-0.9.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+name = "gocept.nginx"
+setup(
+    name = name,
+    version = "0.9.2",
+    author = "Christian Theune",
+    author_email = "ct@gocept.com",
+    description = "zc.buildout recipe for configuring an nginx server",
+    long_description=file('README.txt').read(),
+    license = "ZPL 2.1",
+    keywords = "zope3 buildout nginx",
+    classifiers = ["Framework :: Buildout"],
+
+    zip_safe=False,
+    packages = find_packages('.'),
+    include_package_data = True,
+    namespace_packages = ['gocept'],
+    install_requires = [
+        'setuptools',
+    ],
+    entry_points = {
+        'zc.buildout': [
+             'default = %s.recipe:Recipe' % name,
+             ]
+        },
+    )
