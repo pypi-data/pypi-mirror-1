b9afe6175d3dc507290efb370df7627ab36e11d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:56 +0000

    Add package AMQPQueue/0.3.3/AMQPQueue-0.3.3.tar.gz

---
+++ packages/AMQPQueue/AMQPQueue-0.3.3.tar.gz/AMQPQueue-0.3.3/testamqpqueue/testWorker.py
@@ -0,0 +1,43 @@
+from amqpqueue import Worker, QueueFactory
+
+import threading
+
+import time
+
+ADDR = 'localhost:5672'
+
+class echoWorker(Worker):
+    def endtask(self, msg, response):
+        """Simple task end, ack'ing the message but printing out which thread consumed it."""
+        if self.context.get('thread_id', False):
+            print "From worker thread #%s\n------------------------\n%s" % (self.context.get('thread_id'), msg)
+        self.queue_stdin.task_done()
+       
+class echoWorkerThread(threading.Thread):
+    def __init__(self, this_id, queue_in, queue_out=None):
+        self.worker = echoWorker(queue_in, queue_out, thread_id=this_id)
+    
+    def run(self):
+        # Won't bother sync'ing stdout access with a threading.Lock() atm
+        self.worker.run()
+        
+qf = QueueFactory(addr=ADDR)
+
+qp = qf.Producer("stdin")
+
+threads = []
+for index in xrange(0,5):
+    thread = echoWorkerThread(index, qf.Consumer("stdin"))
+    thread.start()
+    threads.append(thread)
+
+from uuid import uuid4
+
+
+for i in xrange(0, 100000):
+    qp.put(uuid4)
+
+for thread in threads:
+    thread.join()
+
+print "Main worker loop exited"
