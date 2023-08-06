ae0fd6bd137acdf3191673e74907726faa5845ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package Graphine/0.0a1/Graphine-0.0.tar.gz

---
+++ packages/Graphine/Graphine-0.0.tar.gz/Graphine-0.0/setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3.0
+
+from distutils.core import setup
+
+setup(name='Graphine',
+      version='0.0',
+      description='Pythonic Graph Library',
+      author='Geremy Condra',
+      author_email='CTO@OpenMigration.Net',
+      url='http://www.graphine.org',
+      packages=['graph']
+     )
+
