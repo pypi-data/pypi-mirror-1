28b2720e4ab7fab70cb5f5cb33882a119643e929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:32 +0000

    Add package mwlib/0.6.2/mwlib-0.6.2.tar.gz

---
+++ packages/mwlib/mwlib-0.6.2.tar.gz/mwlib-0.6.2/sandbox/xmlserver/xml-server.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+import urllib
+import cgi
+import traceback
+import StringIO
+import BaseHTTPServer
+import SimpleHTTPServer
+from mwlib import mwapidb
+from mwlib import xhtmlwriter
+from mwlib import advtree
+
+default_baseurl = "en.wikipedia.org/w"
+default_shared_baseurl = "commons.wikimedia.org/w/"
+default_debug = 1
+default_imgwidth = 200
+imagesrcresolver = "/imageresolver/IMAGENAME"
+version = "0.1"
+
+class XMLHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    """
+    mwlib.xml-server version %s using xhtmlwriter.py version (%s)
+    
+    This server offers two services:
+    /mwxml/         : returns MediaWiki documents as XHTML1.0 transitional
+    /imageresolver/ : resolves imagenames to full urls
+
+    Note this service relies on MediaWiki API http://en.wikipedia.org/w/api.php
+    Son only Mediawikis with an approriate version are supported.
+
+
+    usage /mwxml/ _____________________________________________________:
+
+    /mwxml/<wiki_base_url>/<article><?option_arg=option_value>
+    e.g.
+    /mwxml/www.mediawiki.org/w/API
+    
+    wiki_base_url defaults to %s
+    so one can also use /mwxml/Article to access Article from the above site
+    
+    options (query args):
+
+    debug
+    whether verbose debug info is included as XML-comments
+    0 or 1, defaults to %d
+    
+    imageresolver
+    img src tags can be set to a redirector service, in order to get them 
+    resolved and displayed in browsers.
+    value can be any  string, in which every occurence of IMAGENAME is 
+    substituted by the image name (e.g. 'Picture1.jpg).
+    This defaults to '%s'
+
+    
+    usage /imageresolver/ ___________________________________________:   
+    
+    /imageresolver/<IMAGENAME><?baseurl=url&sharedbaseurl=url2>
+    
+    baseurl and sharedbasurl are used to retrieve images
+
+    baseurl defaults to %s
+    sharedbaseurl defaults to %s
+    imgwidth defaults to %d
+    
+    """ 
+
+    documentation = __doc__ % (version, xhtmlwriter.version, default_baseurl, 
+                               default_debug, imagesrcresolver, default_baseurl, 
+                               default_shared_baseurl, default_imgwidth)
+       
+
+
+    def do_GET(self):
+        path, query = urllib.splitquery(self.path)
+        path = [urllib.unquote_plus(x) for x in path.split("/")]
+        query = cgi.parse_qs(query or "")
+        print path, query
+        app = path[1]
+        args = path[2:]
+        print args, query
+        if app == "mwxml":
+            self._dosafe(self._servXML, args, query)
+        elif app == "imageresolver":
+            self._dosafe(self._resolveImage, args, query)
+        else:
+            self._doc()
+        
+
+
+    def _dosafe(self, method, query, args):
+        try:
+            method(query, args)
+        except Exception, e:
+            s = StringIO.StringIO()
+            traceback.print_exc(file=s)
+            self._doc(error = s.getvalue())
+
+
+    def _doc(self, error=""):
+        if error:
+            response = "An error occurred:\n\n%s\n\n%s" %( error, self.documentation)
+            self.send_response(500)
+        else:
+            self.send_response(200)
+            response = self.documentation
+        
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", str(len(response)))
+        self.end_headers()
+        self.wfile.write(response)
+
+
+    def _servXML(self, args, query):
+        if not len(args):
+            self._doc(error="require articlename")
+            return
+        unknown = [k for k in query if k not in ("debug", "imageresolver")]
+        if unknown:
+            return self._doc(error="unknown option %r" % unknown)
+        title = args.pop()
+        base_url = "http://%s/" % ("/".join(args) or default_baseurl)
+        debug = bool(query.setdefault("debug", [default_debug])[0])
+
+        language = "en" # FIXME
+        namespace="en.wikipedia.org" # FIXME
+
+        print "_servXML", title, base_url, debug
+
+        db = mwapidb.WikiDB(base_url, license=None)
+        db.print_template = None # deactivate print template lookups
+        tree = db.getParsedArticle(title, revision=None)
+        advtree.buildAdvancedTree(tree)
+        dbw = xhtmlwriter.MWXHTMLWriter(language=language, namespace=namespace, 
+                                        imagesrcresolver=imagesrcresolver,
+                                        debug=debug)
+        #dbw = xhtmlwriter.XMLWriter() # another XML-writer implementation
+        dbw.write(tree)
+        if debug:
+            dbw.writeparsetree(tree)
+
+        response = dbw.asstring()
+        
+        self.send_response(200)
+        self.send_header("Content-type", "text/xml")
+        self.send_header("Content-length", str(len(response)))
+        self.end_headers()
+        self.wfile.write(response)
+        
+        # shut down the connection
+        self.wfile.flush()
+        
+
+    def _resolveImage(self, args, query):
+        if not len(args):
+            self._doc(error="require imagename")
+            return
+        unknown = [k for k in query if k not in ("baseurl", "sharedbaseurl", "imgwidth")]
+        if unknown:
+            return self._doc(error="unknown option %r" % unknown)
+        title = args.pop()
+        base_url = "http://" + query.setdefault("baseurl", [default_baseurl])[0] 
+        shared_base_url = "http://" + query.setdefault("sharedbaseurl", [default_shared_baseurl])[0] 
+        imgwidth = int(query.setdefault("imgwidth", [default_imgwidth])[0] )
+
+        print "_resolveImage", title, base_url, shared_base_url, imgwidth
+
+        db = mwapidb.ImageDB(base_url, shared_base_url)
+        url = db.getURL(title.decode("utf8"), size=imgwidth)
+        if not url:
+            self.send_response(404)
+            self.end_headers()
+        else:
+            self.send_response(301)
+            self.send_header("Location", url)
+            self.end_headers()
+
+
+
+
+
+
+def run(port=8000):
+    server_address = ('', port)
+    httpd = BaseHTTPServer.HTTPServer(server_address, XMLHandler)
+    print "listening on port", port
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/mwlib/mwlib-0.6.2.tar.gz/mwlib-0.6.2/tests/test_expander.py
@@ -0,0 +1,331 @@
+#! /usr/bin/env py.test
+
+from mwlib import expander
+from mwlib.expander import expandstr, DictDB
+
+def parse_and_show(s):
+    res=expander.parse(s)
+    print "PARSE:", repr(s)    
+    expander.show(res)
+    return res
+
+def test_noexpansion_inside_pre():
+    db = DictDB(Art="<pre>A{{Pipe}}B</pre>",
+                Pipe="C")
+
+    res = expandstr("<pre>A{{Pipe}}B</pre>", "<pre>A{{Pipe}}B</pre>", wikidb=DictDB(Pipe="C"))
+
+
+def test_undefined_variable():
+    db = DictDB(Art="{{Pipe}}",
+                Pipe="{{{undefined_variable}}}")
+    
+    te = expander.Expander(db.getRawArticle("Art"), pagename="thispage", wikidb=db)
+    res = te.expandTemplates()
+    print "EXPANDED:", repr(res)
+    assert u"{{{undefined_variable}}}" in res, "wrong expansion for undefined variable"
+
+def test_birth_date_and_age():
+    db = DictDB({
+            "birth date and age": '[[{{MONTHNAME|{{{2|{{{month|{{{2}}}}}}}}}}} {{{3|{{{day|{{{3}}}}}}}}}]] [[{{{1|{{{year|{{{1}}}}}}}}}]]<font class="noprint"> (age&nbsp;{{age | {{{1|{{{year|{{{1}}}}}}}}} | {{{2|{{{month|{{{2}}}}}}}}} | {{{3|{{{day|{{{3}}}}}}}}} }})</font>',
+            
+            "age" : '<includeonly>{{#expr:({{{4|{{CURRENTYEAR}}}}})-({{{1}}})-(({{{5|{{CURRENTMONTH}}}}})<({{{2}}})or({{{5|{{CURRENTMONTH}}}}})=({{{2}}})and({{{6|{{CURRENTDAY}}}}})<({{{3}}}))}}</includeonly>',
+            })
+    res=expandstr('{{birth date and age|1960|02|8}}', wikidb=db)
+
+    print "EXPANDED:", repr(res)
+    import datetime
+    now=datetime.datetime.now()
+    b=datetime.datetime(1960,2,8)
+    age = now.year-b.year
+    if now.month*32+now.day < b.month*32+b.day:
+        age -= 1
+    
+    expected = u"age&nbsp;%s" % age
+    assert expected in res
+
+    assert u"February" in res
+    
+def test_five():
+    txt = "text of the tnext template"
+    db=DictDB(
+        t1="{{{{{1}}}}}",
+        tnext=txt)
+    expandstr("{{t1|tnext}}", expected=txt, wikidb=db)
+
+def test_five_parser():
+    n=parse_and_show("{{{{{1}}}}}")
+    assert isinstance(n, expander.Template)
+
+def test_five_two_three():
+    n=parse_and_show("{{{{{1}} }}}")
+    assert isinstance(n, expander.Variable)
+
+def test_five_three_two():
+    n=parse_and_show("{{{{{1}}} }}")
+    assert isinstance(n, expander.Template)
+
+    
+def test_alfred():
+    """I start to hate that Alfred_Gusenbauer"""
+    db = DictDB(
+        a="{{ibox2|birth_date=1960}}",
+        ibox2="{{{birth{{#if:{{{birthdate|}}}||_}}date}}}"
+        )
+    te = expander.Expander(db.getRawArticle("a"), pagename="thispage", wikidb=db)
+    res = te.expandTemplates()
+    print "EXPANDED:", repr(res)
+    assert "1960" in res
+
+def test_switch_numeric_comparison():
+    expandstr("{{ #switch: +07 | 7 = Yes | 007 = Bond | No }}", "Yes")
+
+def test_switch_case_sensitive1():
+    expandstr("{{ #switch: A | a=lower | A=UPPER }}", "UPPER")
+
+def test_switch_case_sensitive2():
+    expandstr("{{ #switch: A | a=lower | UPPER }}", "UPPER")
+
+def test_switch_case_sensitive3():
+    expandstr("{{ #switch: a | a=lower | UPPER }}", "lower")
+
+def test_names_insensitive():
+    expandstr("{{ #SWItch: A | a=lower | UPPER }}", "UPPER")
+
+def test_ifeq_numeric_comparison():
+    expandstr("{{ #ifeq: +07 | 007 | 1 | 0 }}", "1")
+
+def test_ifeq_numeric_comparison2():
+    expandstr('{{ #ifeq: "+07" | "007" | 1 | 0 }}', '0')
+
+def test_ifeq_case_sensitive():
+    expandstr("{{ #ifeq: A | a | 1 | 0 }}", "0")
+
+def test_ifexpr():
+    expandstr("{{ #ifexpr: 10 > 9 | yes | no }}", "yes")
+ 
+def test_expr_round():
+    """round creates integers if it can"""
+    expandstr("{{#expr: 10.0443 round -1}}", "10")
+
+def test_expr_round2():
+    expandstr("{{#expr: 10.0443 round 2}}", "10.04")
+
+def test_too_many_args():
+    expandstr("{{LC:AB|CD}}", "ab")
+
+def test_lc_named_arg():
+    expandstr("{{LC:a=AB|CD}}", "a=ab")
+
+def test_named_variable_whitespace():
+    """http://code.pediapress.com/wiki/ticket/23"""
+
+    expandstr("{{doit|notable roles=these are the notable roles}}",
+              "these are the notable roles",
+              wikidb=DictDB(doit="{{{notable roles}}}"))
+
+def test_monthname():
+    expandstr("{{MONTHNAME|1}}", "January")
+    expandstr("{{MONTHNAME|2}}", "February")
+    expandstr("{{MONTHNAME|12}}", "December")
+    expandstr("{{MONTHNAME|13}}", "January")
+
+    expandstr("{{MONTHNAME|0}}", "December")
+
+def test_pipe_inside_imagemap():
+    """pipes inside image maps should not separate template arguments
+    well, they do not separate arguments with the version running on en.wikipedia.org.
+    they do separate arguments with the version running on pediapress.com:8080.
+    (which is hopefully a newer version)
+    """
+
+    db = DictDB(
+        sp="""{{#ifeq: {{{1}}} | 1 
+| <imagemap>
+ Image:Padlock-silver-medium.svg|20px
+ rect 0 0 1000 1000 [[Wikipedia:Protection policy|This page has been semi-protected from editing]]
+ desc none
+ </imagemap> 
+|bla
+}}
+""")
+    result=expandstr("{{sp|1}}", wikidb=db)
+    assert "</imagemap>" in result
+
+
+def test_expand_comment():
+    s="foo\n     <!-- comment --->     \nbar"
+    e="foo\nbar"
+    expandstr(s, e)
+
+
+def test_tokenize_gallery():
+    gall = """
+<gallery caption="Sample gallery" widths="100px" heights="100px" perrow="6">
+Image:Drenthe-Position.png|[[w:Drenthe|Drenthe]], the least crowded province
+Image:Friesland-Position.png|[[w:Friesland|Friesland]] has many lakes
+</gallery>
+"""
+    tokens = expander.tokenize(gall)
+    g = tokens[1][1]
+    assert g.startswith("<gallery")
+    assert g.endswith("</gallery>")
+    
+def test_template_name_colon():    
+    """http://code.pediapress.com/wiki/ticket/36
+    """
+    p=parse_and_show("{{Template:foobar}}")
+    assert isinstance(p, expander.Template), 'expected a template'
+    assert len(p.children)==1, 'expected exactly one child'
+    
+
+
+def test_expand_parser_func_name():
+    expandstr("{{ {{NZ}}expr: 1+1}}", "2",
+              wikidb=DictDB(NZ="#"))
+
+def test_expand_name_with_colon():
+    wikidb = DictDB()
+    wikidb.d['bla:blubb'] = 'foo'
+    expandstr("{{bla:blubb}}", "foo", wikidb=wikidb)
+
+def test_parser_func_from_template():
+    expandstr("{{ {{bla}} 1 + 1}}", "2", wikidb=DictDB(bla="#expr:"))
+
+def test_bad_expr_name():
+    s=expandstr("{{expr:1+1}}")  # '#' missing
+    assert s!='2', "bad result"
+
+def test_parmpart():
+    parmpart = """{{#ifeq:/{{{2|}}}
+|{{#titleparts:/{{{2|}}}|1|{{#expr:1+{{{1|1}}}}}}}
+|
+|{{#titleparts:/{{{2|}}}|1|{{#expr:1+{{{1|1}}}}}}}
+}}"""
+    expandstr("{{ParmPart|0|a/b}}", "", wikidb=DictDB(ParmPart=parmpart))
+    expandstr("{{ParmPart|1|a/b}}", "a", wikidb=DictDB(ParmPart=parmpart))
+    expandstr("{{ParmPart|2|a/b}}", "b", wikidb=DictDB(ParmPart=parmpart))
+    expandstr("{{ParmPart|3|a/b}}", "", wikidb=DictDB(ParmPart=parmpart))
+              
+def test_titleparts():
+    expandstr("{{#titleparts:Help:Link/a/b|0|}}", "Help:Link/a/b")
+    expandstr("{{#titleparts:Help:Link/a/b|1|}}", "Help:Link")
+    expandstr("{{#titleparts:Help:Link/a/b|2|}}", "Help:Link/a") 
+    expandstr("{{#titleparts:Help:Link/a/b|3|}}", "Help:Link/a/b")
+    expandstr("{{#titleparts:Help:Link/a/b|4|}}", "Help:Link/a/b")
+
+def test_titleparts_2params():
+    expandstr("{{#titleparts:Help:Link/a/b|2|2}}", "a/b")
+    expandstr("{{#titleparts:Help:Link/a/b|1|2}}", "a")
+    expandstr("{{#titleparts:Help:Link/a/b|1|3}}", "b")
+
+def test_titleparts_negative():
+    expandstr("{{#titleparts:Help:Link/a/b|-1|}}", "Help:Link/a")
+    expandstr("{{#titleparts:Help:Link/a/b|1|-1|}}", "b")
+
+
+def test_iferror():
+    expandstr("{{#iferror:{{#expr:1+1}}|bad input|valid expression}}", "valid expression")
+    expandstr("{{#iferror:{{#expr:1+Z}}|bad input|valid expression}}", "bad input")
+    expandstr("{{#iferror:{{#expr:1+1}}|bad input}}", "2")
+    expandstr("{{#iferror:{{#expr:1+Z}}|bad input}}", "bad input")
+    expandstr("{{#iferror:{{#expr:1+1}}}}", "2")
+    expandstr("{{#iferror:{{#expr:1+Z}}}}", "")
+
+def test_implicit_newline_begintable():
+    expandstr("foo {{tt}}", "foo \n{|", wikidb=DictDB(tt="{|"))
+    
+def test_implicit_newline_colon():
+    expandstr("foo {{tt}}", "foo \n:", wikidb=DictDB(tt=":"))
+
+def test_implicit_newline_semicolon():
+    expandstr("foo {{tt}}", "foo \n;", wikidb=DictDB(tt=";"))
+
+def test_empty_template():
+    """test for http://code.pediapress.com/wiki/ticket/126"""
+    expandstr("{{}}", "")
+    
+def test_implicit_newline_magic():
+    expandstr("foo {{#if: 1 | :xxx }} bar", "foo \n:xxx bar")
+    expandstr("foo {{#ifexpr: 1 | #xxx }} bar", "foo \n#xxx bar")
+    
+def test_expand_after_named():
+    db = DictDB(
+        show="{{{1}}}",
+        a="a=bc")
+    expandstr("{{show|{{a}}}}", "a=bc",  wikidb=db)
+    
+def test_padleft():
+    yield expandstr, "{{padleft:7|3|0}}", "007"
+    yield expandstr, "{{padleft:0|3|0}}", "000"
+    yield expandstr, "{{padleft:bcd|6|a}}", "aaabcd"
+    # {{padleft:cafe|8|-}} = ----cafe 
+
+def test_padright():   
+    yield expandstr, "{{padright:bcd|6|a}}", "bcdaaa"
+    yield expandstr, "{{padright:0|6|a}}", "0aaaaa"
+    
+def test_urlencode():
+    expandstr('{{urlencode:x y @}}', 'x+y+%40')
+    
+def test_urlencode_non_ascii():
+    expandstr(u'{{urlencode:L\xe9onie}}', 'L%C3%A9onie')
+
+def test_anchorencode():
+    expandstr('{{anchorencode:x #y @}}', 'x_.23y_.40')
+    
+def test_fullurl():
+    expandstr('{{fullurl:x y @}}', 'http://en.wikipedia.org/wiki/X_y_%40')
+
+def test_fullurl_nonascii():
+    expandstr(u'{{fullurl:L\xe9onie}}', 'http://en.wikipedia.org/wiki/L%C3%A9onie')
+              
+def test_server():
+    expandstr('{{server}}', 'http://en.wikipedia.org')
+    
+def test_servername():
+    expandstr('{{servername}}', 'en.wikipedia.org')
+
+    
+def test_1x_newline_and_spaces():
+    # see http://en.wikipedia.org/wiki/Help:Newlines_and_spaces#Spaces_and.2For_newlines_as_value_of_an_unnamed_parameter
+    wikidb=DictDB()
+    wikidb.d['1x'] = '{{{1}}}'
+    def e(a,b):
+        return expandstr(a,b,wikidb=wikidb)
+
+    yield e, 'a{{#if:1|\n}}b', 'ab'
+    yield e, 'a{{#if:1|b\n}}c', 'abc'
+    yield e, 'a{{#if:1|\nb}}c', 'abc'
+    
+            
+    yield e, 'a{{1x|\n}}b', 'a\nb'
+    yield e, 'a{{1x|b\n}}c', 'ab\nc'
+    yield e, 'a{{1x|\nb}}c', 'a\nbc'
+
+    yield e, 'a{{1x|1=\n}}b', 'ab'
+
+    yield e, 'a{{1x|1=b\n}}c', 'abc'
+    yield e, 'a{{1x|1=\nb}}c', 'abc'
+
+
+
+def test_variable_alternative():
+    wikidb=DictDB(t1='{{{var|undefined}}}')
+    expandstr('{{t1|var=}}', '', wikidb=wikidb)
+    
+def test_pagename_non_ascii():
+    def e(a,b):
+        return expandstr(a,b,pagename=u'L\xe9onie s')
+    yield e, '{{PAGENAME}}', u'L\xe9onie s'
+    yield e, '{{PAGENAMEE}}', 'L%C3%A9onie_s'
+
+    yield e, '{{BASEPAGENAME}}', u'L\xe9onie s'
+    yield e, '{{BASEPAGENAMEE}}', 'L%C3%A9onie_s'
+
+    
+    yield e, '{{FULLPAGENAME}}', u'L\xe9onie s'
+    yield e, '{{FULLPAGENAMEE}}', 'L%C3%A9onie_s'
+    
+    yield e, '{{SUBPAGENAME}}', u'L\xe9onie s'
+    yield e, '{{SUBPAGENAMEE}}', 'L%C3%A9onie_s'
