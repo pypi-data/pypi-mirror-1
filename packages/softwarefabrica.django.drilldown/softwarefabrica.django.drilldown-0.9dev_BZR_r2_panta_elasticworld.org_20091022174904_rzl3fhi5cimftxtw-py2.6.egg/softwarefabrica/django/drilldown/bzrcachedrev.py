01cf9beed1e4dc1428d53d8213833d651779cc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:04 +0000

    Add package softwarefabrica.django.drilldown/0.9dev-BZR-r2-panta-elasticworld.org-20091022174904-rzl3fhi5cimftxtw/softwarefabrica.django.drilldown-0.9dev_BZR_r2_panta_elasticworld.org_20091022174904_rzl3fhi5cimftxtw-py2.6.egg

---
+++ packages/softwarefabrica.django.drilldown/softwarefabrica.django.drilldown-0.9dev_BZR_r2_panta_elasticworld.org_20091022174904_rzl3fhi5cimftxtw-py2.6.egg/softwarefabrica/django/drilldown/bzrcachedrev.py
@@ -0,0 +1,4 @@
+info    = {'revinfo': (2, 'panta@elasticworld.org-20091022174904-rzl3fhi5cimftxtw'), 'basepath': '/arc/bzr/softwarefabrica-drilldown.bzr/trunk/', 'revno': 2, 'revname': 'panta@elasticworld.org-20091022174904-rzl3fhi5cimftxtw', 'inpath': u'softwarefabrica/django/drilldown/version.py'}
+revno   = 2
+revname = 'panta@elasticworld.org-20091022174904-rzl3fhi5cimftxtw'
+revinfo = (2, 'panta@elasticworld.org-20091022174904-rzl3fhi5cimftxtw')
