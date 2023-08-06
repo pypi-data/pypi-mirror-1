2198d65d9dcdcb8d898bfd5e9459629ba1470402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:30 +0000

    Add package collective.project/0.7/collective.project-0.7.zip

---
+++ packages/collective.project/collective.project-0.7.zip/collective.project-0.7/collective/project/browser/projects_view.py
@@ -0,0 +1,123 @@
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+import datetime
+
+class ProjectsView(BrowserView):
+
+    #__call__ = ViewPageTemplateFile('projects_view.pt')
+
+    # Based on http://code.activestate.com/recipes/52306/
+    def sortedDictValues3(self,adict):
+        keys = adict.keys()
+        keys.sort()
+        return map(adict.get, keys)
+
+    def getProjects(self,sort):
+        results = {}
+        projects = self.context.portal_catalog(review_state='active',
+            path='/'.join(self.context.getPhysicalPath()),
+            portal_type='project')
+        for p in projects:
+            if sort == 'sort-on-clients':
+                results[self.breadcrumbs(p,'sort-on-clients','with-id')] = p
+            else:
+                results[self.breadcrumbs(p,'sort-on-projects','with-id')] = p
+        return self.sortedDictValues3(results)
+
+    def project_totals(self,project):
+        proj = project.getObject()
+        rate = proj.rate
+        title = proj.Title()
+        td = datetime.timedelta
+        hours, start, stop = td(0), td(0), td(0)
+
+        if proj.flat:
+            start = proj.start
+            stop = proj.stop
+            diff = stop - start
+            months = diff.days/30
+            amort = rate / months
+            total = amort
+
+        projects = proj.objectValues()
+        for iter in projects:
+            start = iter.start
+            stop = iter.stop
+            tasks = iter.objectValues()
+            for task in tasks:
+                hours += task.stop - task.start
+                days = self.total_hours_billable(iter).days
+                seconds = days * 86400
+                hours = float((self.total_hours_billable(iter).seconds + seconds) / 3600)
+                total = hours * rate
+
+        if not proj.billable:
+            total = 0.0
+
+        ff = self.format_float
+        hours, rate, total = ff(hours), ff(rate), ff(total)
+
+        return ([title, hours, rate, start, stop, total])
+
+    def format_float(self,f):
+        try:
+            f = '%.2f' % f
+            return f
+        except:
+            return None
+
+    def format_date(self,d):
+        try:
+            d = d.strftime('%Y-%m-%d')
+            return d
+        except:
+            return None
+
+    def project_url(self,project):
+        return project.getObject().absolute_url()
+
+    def project_edit(self,project):
+        return project.getObject().absolute_url() + '/edit'
+
+    def breadcrumbs(self,project,sort,type):
+        results = []
+        path = list(project.getObject().getPhysicalPath())[2:]
+        for i in range(len(path)):
+            if type == 'with-id':
+                results.append(self.context.restrictedTraverse('/'.join(path)).getId())
+            else:
+                results.append(self.context.restrictedTraverse('/'.join(path)).Title())
+            path.pop()
+        if sort == 'sort-on-clients':
+            results.reverse()
+        return ' &rarr; '.join(results)
+
+    def projects_total(self,projects):
+        total = 0.0
+        for project in projects:
+            if self.project_totals(project)[5] is not None:
+                total += float(self.project_totals(project)[5])
+        total='%.2f' % total
+        return total
+
+    def getOddEven(self,counter):
+        if counter % 2 == 0:
+            return 'even'
+        else:
+            return 'odd'
+
+    def getIterTitle(self):
+        try:
+            return self.context.portal_properties.project_properties.iteration
+        except:
+            return 'Active Projects'
+
+    def total_hours_billable(self,iter):
+        hours = datetime.timedelta(0)
+        tasks = iter.objectValues()
+        for task in tasks:
+            if task.billable:
+                hours += task.stop - task.start
+        return hours
+
---
+++ packages/collective.project/collective.project-0.7.zip/collective.project-0.7/collective/project/task.py
@@ -0,0 +1,147 @@
+from five import grok
+from zope import schema
+from plone.directives import form
+from collective.project import projectMessageFactory as _
+import datetime
+from BTrees.Length import Length
+
+
+class ITask(form.Schema):
+    form.mode(id='hidden')
+    id = schema.TextLine(
+            title=_(u"Id"),
+        )
+
+    title = schema.TextLine(
+            title=_(u"Title"),
+        )
+
+    summary = schema.Text(
+            title=_(u"Summary"),
+            description=_(u"A short summary of the content."),
+            required=False,
+        )
+
+    start = schema.Datetime(
+            title=_(u"Start time"),
+            required=False,
+        )
+
+    stop = schema.Datetime(
+            title=_(u"Stop time"),
+            required=False,
+        )
+
+    billable = schema.Bool(
+            title=_(u"Billable?"),
+            required=True,
+            default=True,
+        )
+
+
+class View(grok.View):
+    grok.context(ITask)
+    grok.require('zope2.View')
+
+    def getIterTitle(self, project):
+        client_breadcrumbs = self.client_breadcrumbs(project)
+        try:
+            iter = self.context.portal_properties.project_properties.iteration
+            return iter + ' &rarr; ' + client_breadcrumbs
+        except:
+            return 'Active Projects &rarr; ' + client_breadcrumbs
+
+    def client_breadcrumbs(self, project):
+        results = []
+        path = list(project.getPhysicalPath())[2:]
+        for i in range(len(path)):
+            title = self.context.restrictedTraverse('/'.join(path)).Title()
+            results.append(title)
+            path.pop()
+        results.reverse()
+        return ' &rarr; '.join(results)
+
+    def total_hours(self):
+        try:
+            hours = 0.0
+            tasks = self.context.aq_inner.objectValues()
+            for task in tasks:
+                hours += task.hours
+        except:
+            hours = 0.0
+
+        return self.format_float(hours)
+
+    def format_float(self, f):
+        try:
+            f = '%.2f' % f
+            return f
+        except:
+            return None
+
+    def getRate(self):
+        return self.format_float(self.context.aq_inner.rate)
+
+    def total_income(self):
+        try:
+            return self.format_float(self.calculate_billable() *
+                self.context.aq_inner.rate)
+        except:
+            return self.format_float(self.calculate_billable() * 0.0)
+
+    def calculate_billable(self):
+        try:
+            hours = 0.0
+            tasks = self.context.aq_inner.objectValues()
+            for task in tasks:
+                if task.billable:
+                    hours += task.hours
+        except:
+            hours = 0.0
+
+        return hours
+
+    def getOddEven(self, counter):
+        if counter % 2 == 0:
+            return 'even'
+        else:
+            return 'odd'
+
+    def getStartDate(self):
+        return self.format_date(self.context.start)
+
+    def getStopDate(self):
+        return self.format_date(self.context.stop)
+
+    def format_date(self, d):
+        try:
+            d = d.strftime('%Y-%m-%d %I:%M')
+            return d
+        except:
+            return None
+
+    def getHours(self):
+        return str(self.context.stop - self.context.start)
+
+
+@form.default_value(field=ITask['start'])
+def startDate(data):
+    return datetime.datetime.today()
+
+
+@form.default_value(field=ITask['stop'])
+def stopDate(data):
+    # stop in one hour
+    return datetime.datetime.today() + datetime.timedelta(hours=1)
+
+
+@form.default_value(field=ITask['id'])
+def getNextCounter(self): # Some (slightly modified) Joel Burton Fu,
+    # from SimpleCollector.
+        """Get next ID. Lazily creates counter if neccessary.
+        """
+        self = self.context
+        if not hasattr(self, '_counter'):
+            self._counter = Length(0)
+        self._counter.change(+1)
+        return self._counter()
