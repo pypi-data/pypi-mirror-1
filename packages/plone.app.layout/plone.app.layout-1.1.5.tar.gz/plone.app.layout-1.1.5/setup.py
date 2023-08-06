071152c040c2e4198e44f5ae928197d7bd964040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:59 +0000

    Add package plone.app.layout/1.1.5/plone.app.layout-1.1.5.tar.gz

---
+++ packages/plone.app.layout/plone.app.layout-1.1.5.tar.gz/plone.app.layout-1.1.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.5'
+
+setup(name='plone.app.layout',
+      version=version,
+      description="Layout mechanisms for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        ],
+      keywords='plone layout viewlet',
+      author='Martin Aspeli',
+      author_email='optilude@gmx.net',
+      url='http://svn.plone.org/svn/plone/plone.app.layout',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.app.viewletmanager>=1.2dev',
+        'plone.memoize',
+        'plone.portlets',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
