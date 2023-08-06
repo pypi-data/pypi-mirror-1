ebc05e7662fcd855d21968acb0694cc084a1fd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:24 +0000

    Add package collective.realestatebroker/2.0.3.rc2/collective.realestatebroker-2.0.3.rc2.tar.gz

---
+++ packages/collective.realestatebroker/collective.realestatebroker-2.0.3.rc2.tar.gz/collective.realestatebroker-2.0.3.rc2/collective/realestatebroker/browser/commercial.py
@@ -0,0 +1,24 @@
+from zope.interface import implements
+from Acquisition import aq_inner
+from plone.memoize.instance import memoize
+from interfaces import ICommercialListing
+from interfaces import ICommercialView
+from base import RealEstateListing
+from base import RealEstateView
+
+
+class CommercialListing(RealEstateListing):
+    """Default view for a folder which contains Commercial real estate. This 
+    view displays a search form and a listing of the search results.
+    """
+
+    implements(ICommercialListing)
+
+    pass
+
+
+class CommercialView(RealEstateView):
+    
+    implements(ICommercialView)
+
+    pass
