92191b590c592f70ed0d110fe53e0d204728e5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:56 +0000

    Add package bitarray/0.3.0/bitarray-0.3.0.tar.gz

---
+++ packages/bitarray/bitarray-0.3.0.tar.gz/bitarray-0.3.0/setup.py
@@ -0,0 +1,36 @@
+import os, sys
+from distutils.core import setup, Extension
+
+if sys.version_info[:2] < (2, 5):
+    raise Exception('bitarray requires Python 2.5 or greater.')
+
+kwds = {}
+
+# Read the long description from README
+thisdir = os.path.abspath(os.path.dirname(__file__))
+f = open(os.path.join(thisdir, 'README'))
+kwds['long_description'] = f.read()
+f.close()
+
+setup(
+    name = "bitarray",
+    version = "0.3.0",
+    author = "Ilan Schnell",
+    author_email = "ilanschnell@gmail.com",
+    url = "http://pypi.python.org/pypi/bitarray/",
+    license = "PSF",
+    classifiers = [
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: C",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
+    description = "bitarray: efficient arrays of boolean values",
+    packages = ["bitarray"],
+    ext_modules = [Extension(name = "bitarray._bitarray",
+                             sources = ["bitarray/_bitarray.c"])],
+    **kwds
+    )
