b4118c009041b3b4dfb876f99cda1182a59f4e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:40 +0000

    Add package django-markitup/0.2.9/django-markitup-0.2.9.tar.gz

---
+++ packages/django-markitup/django-markitup-0.2.9.tar.gz/django-markitup-0.2.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-markitup',
+    version='0.2.9',
+    description='Django integration with the MarkItUp universal markup editor',
+    long_description=open('README.txt').read(),
+    author='Carl Meyer',
+    author_email='carl@dirtcircle.com',
+    url='http://bitbucket.org/carljm/django-markitup/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+    package_data={'markitup': ['templates/markitup/*.html',
+                               'media/markitup/*.*',
+                               'media/markitup/sets/*/*.*',
+                               'media/markitup/sets/*/images/*.png',
+                               'media/markitup/skins/*/*.*',
+                               'media/markitup/skins/*/images/*.png',
+                               'media/markitup/templates/*.*']}
+)
