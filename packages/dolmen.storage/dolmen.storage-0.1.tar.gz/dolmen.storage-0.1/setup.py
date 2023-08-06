e7fe753607406cfec3b678499f1e2ef14b6eaefb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:36 +0000

    Add package dolmen.storage/0.1/dolmen.storage-0.1.tar.gz

---
+++ packages/dolmen.storage/dolmen.storage-0.1.tar.gz/dolmen.storage-0.1/setup.py
@@ -0,0 +1,52 @@
+from os.path import join
+from setuptools import setup, find_packages
+
+name = 'dolmen.storage'
+version = '0.1'
+readme = open(join('src', 'dolmen', 'storage', 'README.txt')).read()
+history = open(join('docs', 'HISTORY.txt')).read()
+
+install_requires = [
+    'setuptools',
+    'zope.schema',
+    'zope.interface',
+    'zope.annotation',
+    'zope.app.container',
+    'grokcore.component',
+    ]
+
+tests_require = install_requires + [
+    'zope.testing',
+    'zope.app.testing',
+    'zope.app.zcmlfiles',
+    ]
+
+setup(name = name,
+      version = version,
+      description = 'Dolmen Zope3 Grok Storage Annotation',
+      long_description = readme + '\n\n' + history,
+      keywords = 'Grok Zope3 CMS Dolmen',
+      author = 'Souheil Chelfouh',
+      author_email = 'trollfot@gmail.com',
+      url = 'http://gitweb.dolmen-project.org',
+      download_url = '',
+      license = 'GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages = ['dolmen'],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = True,
+      tests_require = tests_require,
+      install_requires = install_requires,
+      extras_require = {'test': tests_require},
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Zope3',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+)
---
+++ packages/dolmen.storage/dolmen.storage-0.1.tar.gz/dolmen.storage-0.1/src/dolmen/storage/__init__.py
@@ -0,0 +1,3 @@
+from interfaces import IStorage, IDelegatedStorage
+from container import OOBTreeStorage, IOBTreeStorage, DelegatedStorage
+from annotations import AnnotationStorage, AnnotationProperty
