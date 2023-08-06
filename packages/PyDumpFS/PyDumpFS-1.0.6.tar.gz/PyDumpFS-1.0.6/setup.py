fe01cdd151d0656bc10654c35482ae6907ac4315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:07 +0000

    Add package PyDumpFS/1.0.6/PyDumpFS-1.0.6.tar.gz

---
+++ packages/PyDumpFS/PyDumpFS-1.0.6.tar.gz/PyDumpFS-1.0.6/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+from setuptools import setup, find_packages
+
+setup(name='PyDumpFS',
+      version='1.0.6',
+      author='MASA.H',
+      author_email='hasegawa@mapse.eng.osaka-u.ac.jp',
+      url='http://www.masahase.mydns.jp/hg/PydumpFS/',
+      scripts=['src/pdumpfs.py'],
+      description="Python base pseudo plan9's dump file system",
+      license="New-style BSD License",
+      platforms=["Posix"],
+      long_description="Python base pseudo plan9's dump file system",
+      classifiers=[
+#          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: Japanese',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: System :: Archiving :: Backup',
+          ],
+      data_files=[
+          ('share/locale/ja_JP/LC_MESSAGES',['src/locale/ja_JP/PyDumpFS.mo'])
+          ],
+      )
