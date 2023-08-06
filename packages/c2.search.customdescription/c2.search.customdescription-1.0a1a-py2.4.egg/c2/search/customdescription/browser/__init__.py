abd273408ebcdd872a4bc628cec37e2c5e6d4190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:22 +0000

    Add package c2.search.customdescription/1.0a1a/c2.search.customdescription-1.0a1a-py2.4.egg

---
+++ packages/c2.search.customdescription/c2.search.customdescription-1.0a1a-py2.4.egg/c2/search/customdescription/browser/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Manabu Terada on 2009-11-11.
+Copyright (c) 2009 CMScom. All rights reserved.
+"""
+
+
---
+++ packages/c2.search.customdescription/c2.search.customdescription-1.0a1a-py2.4.egg/c2/search/customdescription/tests/base.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+base.py
+
+Created by Manabu Terada on 2009-11-11.
+Copyright (c) 2009 CMScom. All rights reserved.
+"""
+
+from Testing import ZopeTestCase
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+from Products.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.layer import onsetup
+
+@onsetup
+def setup_product():
+    fiveconfigure.debug_mode = True
+    import c2.search.customdescription
+    zcml.load_config('configure.zcml', c2.search.customdescription)
+    fiveconfigure.debug_mode = False
+    
+    ZopeTestCase.installPackage('c2.search.customdescription')
+
+setup_product()
+
+PRODUCTS = []
+PloneTestCase.setupPloneSite(products=PRODUCTS)
+
+
+class ProductTestCase(PloneTestCase.PloneTestCase):
+    class Session(dict):
+        def set(self, key, value):
+            self[key] = value
+    
+    def _setup(self):
+        PloneTestCase.PloneTestCase._setup(self)
+        self.app.REQUEST['SESSION'] = self.Session()
