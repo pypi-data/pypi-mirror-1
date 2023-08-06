1041d43bf4c37e03c66d6bfcec9e3b4c1196dbd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:52 +0000

    Add package alchemist.audit/0.3.0/alchemist.audit-0.3.0.tar.gz

---
+++ packages/alchemist.audit/alchemist.audit-0.3.0.tar.gz/alchemist.audit-0.3.0/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    
+setup(
+    name="alchemist.audit",
+    version="0.3.0",
+    install_requires=['setuptools',
+                      'ore.alchemist',
+                      'zope.formlib',
+                      'zc.table'],
+    packages=find_packages(),
+    namespace_packages=['alchemist'],
+    package_data = {
+    '': ['*.txt', '*.zcml'],
+    },
+    zip_safe=False,
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Framework :: Zope3'
+        ],
+    url="http://code.google.com/p/zope-alchemist",
+    keywords="zope3 audit zope",
+    author='Kapil Thangavelu',
+    author_email='kapil.foss@gmail.com',
+    description="Alchemist Auditing Components ( Event Subscribers, Change Recorders, UI ) for Relational Applications",
+    long_description=(
+        read('alchemist','audit','readme.txt')
+        + '\n\n' +
+        read('changes.txt')
+        + '\n\n'
+        ),
+    license='ZPL',
+    keywords="zope zope3",
+    )
+
