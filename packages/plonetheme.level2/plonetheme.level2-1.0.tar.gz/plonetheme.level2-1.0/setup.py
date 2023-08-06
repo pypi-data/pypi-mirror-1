cf351dfecbe3c7956a6313736a8f2495c15ca2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:31 +0000

    Add package plonetheme.level2/1.0/plonetheme.level2-1.0.tar.gz

---
+++ packages/plonetheme.level2/plonetheme.level2-1.0.tar.gz/plonetheme.level2-1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='plonetheme.level2',
+      version=version,
+      description="An installable theme for Plone 3.0",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Niels Steurer',
+      author_email='niels.steurer@d9t.de',
+      url='http://d9t.de/os',
+      license='Creative Commons Attributions 2.5',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
