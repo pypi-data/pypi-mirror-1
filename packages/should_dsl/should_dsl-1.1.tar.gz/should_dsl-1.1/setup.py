ffea7601b373dd5a9b53add4e2891bfdc8ff40d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:56 +0000

    Add package should_dsl/1.1/should_dsl-1.1.tar.gz

---
+++ packages/should_dsl/should_dsl-1.1.tar.gz/should_dsl-1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.1'
+
+readme_fd = open('README.rst')
+readme = readme_fd.read()
+readme_fd.close()
+
+setup(name='should_dsl',
+      version=version,
+      description='Should assertions in Python as clear and readable as possible',
+      long_description=readme,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Documentation',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Testing',
+      ],
+      keywords='should dsl assertion bdd python',
+      author='Hugo Lopes Tavares',
+      author_email='hltbra@gmail.com',
+      url='http://github.com/hugobr/should-dsl',
+      license='MIT License',
+      packages=['should_dsl'],
+      package_dir={'should_dsl': 'src'},
+      package_data={'':['LICENSE', 'INSTALL', '*.rst', '*.txt',]},
+      install_requires=['setuptools'],
+      entry_points="",
+      )
