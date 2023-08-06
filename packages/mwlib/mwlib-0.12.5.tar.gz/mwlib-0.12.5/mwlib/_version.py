1bc38ce11f517ec633264aa8f090e17f82fb5533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:53 +0000

    Add package mwlib/0.12.5/mwlib-0.12.5.tar.gz

---
+++ packages/mwlib/mwlib-0.12.5.tar.gz/mwlib-0.12.5/mwlib/_version.py
@@ -0,0 +1,14 @@
+class _Version(tuple):
+    """internal version object, subclass of C{tuple},
+    but implements a fancier __str__ representation
+    """
+    def __str__(self):
+        return '.'.join([str(x) for x in self])
+
+version = _Version((0,12,5))
+del _Version
+
+try:
+    from mwlib._gitversion import gitid, gitversion
+except ImportError:
+    gitid = gitversion = ""
