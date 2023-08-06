de6af9b3a551b8bf43a5f467d7f1c896dbc2c359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:02 +0000

    Add package plonetheme.greencommunity/0.12dev-r60731/plonetheme.greencommunity-0.12dev-r60731.tar.gz

---
+++ packages/plonetheme.greencommunity/plonetheme.greencommunity-0.12dev-r60731.tar.gz/plonetheme.greencommunity-0.12dev-r60731/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.12'
+
+setup(name='plonetheme.greencommunity',
+      version=version,
+      description="An installable theme for Plone 3.0.",
+      long_description="""\
+This is an installable theme for Plone 3.0 that is based on the website template by t-3k.  The original can be found at <http://www.oswd.org/design/preview/id/2265>.""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Jonathan Wilde',
+      author_email='product-developers@lists.plone.org',
+      url='http://www.speedbreeze.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
