8d631d397e3c48b79fa94694096a8b348de63977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:43 +0000

    Add package Products.ATTalesExpressionCriterion/1.0dev-r69725/Products.ATTalesExpressionCriterion-1.0dev-r69725.tar.gz

---
+++ packages/Products.ATTalesExpressionCriterion/Products.ATTalesExpressionCriterion-1.0dev-r69725.tar.gz/Products.ATTalesExpressionCriterion-1.0dev-r69725/Products/ATTalesExpressionCriterion/README.txt
@@ -0,0 +1,11 @@
+ATTalesExpressionCriterion
+==========================
+
+Smart folder criterion that stores TALES Expressions. 
+Especially useful when you want to use a smart folder 
+as a view on any folder in your site with contextual
+based filter expressions.
+
+
+Andreas Gabriel <gabriel at hrz uni-marbug de> - http://www.uni-marburg.de/hrz
+
---
+++ packages/Products.ATTalesExpressionCriterion/Products.ATTalesExpressionCriterion-1.0dev-r69725.tar.gz/Products.ATTalesExpressionCriterion-1.0dev-r69725/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='Products.ATTalesExpressionCriterion',
+      version=version,
+      description="Smart folder criterion that stores TALES Expressions.",
+      long_description=open("Products/ATTalesExpressionCriterion/README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        ],
+      keywords='ATContentTypes Collection Topic Criterion',
+      author='Andreas Gabriel',
+      author_email='gabriel@hrz.uni-marbug.de',
+      url='http://svn.plone.org/svn/collective/Products.ATTalesExpressionCriterion',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'Products.TALESField',
+          'Products.TemplateFields',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
