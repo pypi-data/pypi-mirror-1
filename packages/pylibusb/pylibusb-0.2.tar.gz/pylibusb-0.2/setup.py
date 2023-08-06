86e5fcb9210a6f635b87c67a28d002922130e197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:13 +0000

    Add package pylibusb/0.2/pylibusb-0.2.tar.gz

---
+++ packages/pylibusb/pylibusb-0.2.tar.gz/pylibusb-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pylibusb',
+      description='libusb wrapper (ctypes based)',
+      url='https://code.astraw.com/projects/pylibusb',
+      version='0.2',
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
+      license='BSD',
+      packages = ['pylibusb'],
+      )
