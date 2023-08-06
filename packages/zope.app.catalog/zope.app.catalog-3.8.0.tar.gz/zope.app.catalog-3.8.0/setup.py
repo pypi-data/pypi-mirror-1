be6cfe3c91148d87176e2a574d7dd5f0833483cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:21 +0000

    Add package zope.app.catalog/3.8.0/zope.app.catalog-3.8.0.tar.gz

---
+++ packages/zope.app.catalog/zope.app.catalog-3.8.0.tar.gz/zope.app.catalog-3.8.0/setup.py
@@ -0,0 +1,67 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.catalog package
+
+$Id: setup.py 95900 2009-02-01 17:52:17Z nadako $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name = 'zope.app.catalog',
+      version = '3.8.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Management pages for Zope Catalog',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 catalog index",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.app.catalog',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(
+          test=['zope.testing',
+                'zope.site',
+                'zope.app.securitypolicy',
+                'zope.app.testing',
+                'zope.app.zcmlfiles',
+                'zope.app.zptpage',
+                ]),
+      install_requires = [
+          'setuptools',
+          'zope.catalog',
+          'zope.app.intid',
+          'zope.app.publisher',
+          'zope.app.form',
+          ],
+      include_package_data = True,
+      zip_safe = False,
+      )
