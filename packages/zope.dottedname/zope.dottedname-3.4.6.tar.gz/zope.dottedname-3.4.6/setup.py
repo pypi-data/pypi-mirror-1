0ae9bdfb23044e7f5dad1c9d2dad9962f2f3a904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:45 +0000

    Add package zope.dottedname/3.4.6/zope.dottedname-3.4.6.tar.gz

---
+++ packages/zope.dottedname/zope.dottedname-3.4.6.tar.gz/zope.dottedname-3.4.6/setup.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""
+$Id: setup.py 104061 2009-09-15 13:04:15Z janjaapdriessen $
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name="zope.dottedname",
+      version = '3.4.6',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Resolver for Python dotted names.',
+      long_description='\n\n'.join((
+          read('src', 'zope', 'dottedname', 'README.txt'),
+          read('CHANGES.txt'),
+          )),
+      keywords = 'resolve dotted name',
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.dottedname',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.testing'],
+          ),
+      install_requires = ['setuptools'],
+      include_package_data=True,
+      zip_safe = False
+      )
+
