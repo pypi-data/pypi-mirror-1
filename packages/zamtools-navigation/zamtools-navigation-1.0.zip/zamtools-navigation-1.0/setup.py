1f197c104f08b1c39df8c2a6b1e16ae10ccb36a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:29 +0000

    Add package zamtools-navigation/1.0/zamtools-navigation-1.0.zip

---
+++ packages/zamtools-navigation/zamtools-navigation-1.0.zip/zamtools-navigation-1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+    name = "zamtools-navigation",
+    version = '1.0',
+    description = "Django application for displaying and tracking site navigation",
+    author = "Ian Zamojc",
+    author_email = "zamtools@zamtools.com",
+    url = "http://code.google.com/p/zamtools-navigation/",
+    packages = [
+        'navigation',
+    ],
+    zip_safe = False,
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+)
