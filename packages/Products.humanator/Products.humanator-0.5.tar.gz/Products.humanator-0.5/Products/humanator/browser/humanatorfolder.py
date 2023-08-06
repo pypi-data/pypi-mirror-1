e9d7e154001925b9c47df285b48436188ebf1ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:25 +0000

    Add package Products.humanator/0.5/Products.humanator-0.5.tar.gz

---
+++ packages/Products.humanator/Products.humanator-0.5.tar.gz/Products.humanator-0.5/Products/humanator/browser/humanatorfolder.py
@@ -0,0 +1,34 @@
+"""Define a browser view for the 'Todo folder' content type. In the FTI 
+configured in profiles/default/types/*.xml, this is being set as the default
+view of that content type.
+"""
+
+from Acquisition import aq_inner
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.app.content.browser.foldercontents import FolderContentsView
+
+class Humanator_folderView(FolderContentsView):
+    """Default view of a Humanator folder
+    """
+
+    __call__ = ViewPageTemplateFile('humanatorfolder_view.pt')
+
+
+    def folder_listing(self):
+        results = ''
+        for i in self.context.objectIds():
+            obj = self.context.unrestrictedTraverse('/'.join(self.context.getPhysicalPath()) + '/' + i)
+            url = obj.absolute_url()
+            title = obj.Title()
+            answer = obj.getAnswer()           
+            wftool = self.context.portal_workflow
+            state = wftool.getInfoFor(obj, 'review_state')
+            if state == 'published':
+                results += ("<li><p><a href='%s'>%s</a></p><p>%s</p></li>" % (url,title, answer))
+            else:
+                results += ("<li><p><a href='%s'><strike>%s</strike></a></p><p><strike>%s</strike></p></li>" %
+                        (url,title, answer))
+        return results 
---
+++ packages/Products.humanator/Products.humanator-0.5.tar.gz/Products.humanator-0.5/Products/humanator/config.py
@@ -0,0 +1,10 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'Products.humanator'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'Humanator question': 'Products.humanator: Add Humanator question',
+    'Humanator folder': 'Products.humanator: Add Humanator folder',
+}
