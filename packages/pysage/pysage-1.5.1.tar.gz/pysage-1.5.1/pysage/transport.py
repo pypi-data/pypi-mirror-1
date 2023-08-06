accfadf49f2c1e537bae891adc249c5350668941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:39 +0000

    Add package pysage/1.5.1/pysage-1.5.1.tar.gz

---
+++ packages/pysage/pysage-1.5.1.tar.gz/pysage-1.5.1/pysage/transport.py
@@ -0,0 +1,99 @@
+# transport.py
+import process as processing
+try:
+    import pyraknet
+except ImportError:
+    RAKNET_AVAILABLE = False
+else:
+    RAKNET_AVAILABLE = True
+
+class Transport(object):
+    '''an interface that all transports must implement'''
+    def connect(self, host, port):
+        '''connects to a server implementing the same interface'''
+        pass
+    def listen(self, port, connection_handler):
+        '''listens for connections, and calls connection_handler upon new connections'''
+        pass
+    def send(self, data, id=-1, broadcast=False):
+        '''send data to another transport specified by "id"'''
+        pass
+    def poll(self, packet_handler):
+        '''polls network data, pass any packet to the packet_handler'''
+        pass
+    def packet_type_info(self, packet_type_id):
+        '''returns information about the packet type'''
+        pass
+    @property
+    def address(self):
+        '''returns the address this transport is bound to'''
+        pass
+    
+class IPCPacket(object):
+    def __init__(self, data):
+        self.data = data
+
+class IPCTransport(Transport):
+    def __init__(self):
+        self._connection = None
+        self.peers = {}
+    def listen(self):
+        self._connection = processing.connection.Listener()
+    def connect(self, address):
+        self._connection = processing.connection.Client(address)
+        self.peers[address] = self._connection
+    @property
+    def address(self):
+        return self._connection.address
+    def accept(self):
+        c = self._connection.accept()
+        _clientid = self._connection.last_accepted
+        if not _clientid:
+            _clientid = c.fileno()
+        self.peers[_clientid] = c
+        return _clientid
+    def disconnect(self, _id):
+        del self.peers[_id]
+    def send(self, data, id=-1, broadcast=False):
+        return processing.send_bytes(self.peers[id], data)
+    def poll(self, packet_handler):
+        processed = False
+        for _id, conn in self.peers.items():
+            if conn.poll():
+                packet = IPCPacket(processing.recv_bytes(conn))
+                packet_handler(packet)
+                processed = True
+        return processed
+
+class RakNetTransport(Transport):
+    def __init__(self):
+        self.net = pyraknet.Peer()
+        self.connection_handler = None
+        self.id_map = {}
+        for t in dir(pyraknet.PacketTypes):
+            if t.startswith('ID_'):
+                self.id_map[getattr(pyraknet.PacketTypes, t)] = t
+    def packet_type_info(self, packet_type_id):
+        return self.id_map[packet_type_id]
+    def connect(self, host, port):
+        self.net.init(peers=1, thread_sleep_timer=10)
+        self.net.connect(host=host, port=port)
+    def listen(self, port, connection_handler):
+        self.net.init(peers=8, port=port, thread_sleep_timer=10)
+        self.net.set_max_connections(8)
+        self.connection_handler = connection_handler
+    def send(self, data, id=-1, broadcast=False):
+        if id >= 0:
+            address = self.net.get_address_from_id(id)
+        elif broadcast:
+            address = pyraknet.PlayerAddress()
+        self.net.send(data, len(data), pyraknet.PacketPriority.LOW_PRIORITY, pyraknet.PacketReliability.RELIABLE_ORDERED, 0, address, broadcast)
+    def poll(self, packet_handler):
+        processed = False
+        packet = self.net.receive()
+        if packet:
+            packet_handler(packet)
+            processed = True
+        return processed
+        
+        
---
+++ packages/pysage/pysage-1.5.1.tar.gz/pysage-1.5.1/pysage/util.py
@@ -0,0 +1,43 @@
+# util.py
+import sys
+import time
+import threading
+import os
+
+class Singleton(object):
+    def __new__(cls, *args, **kwds):
+        return cls.get_singleton(*args, **kwds)
+    @classmethod
+    def get_singleton(cls, *args, **kwds):
+        it = cls.__dict__.get("__it__")
+        if it is not None:
+            return it
+        cls.__it__ = it = object.__new__(cls)
+        it.init(*args, **kwds)
+        return it      
+    def init(self, *args, **kwds):
+        pass
+
+class ProcessLocalSingleton(object):
+    '''fork safe'''
+    def __new__(cls, *args, **kwds):
+        return cls.get_singleton(*args, **kwds)
+    @classmethod
+    def get_singleton(cls, *args, **kwds):
+        it = cls.__dict__.get("__it__")
+        if it is not None and it[0] == os.getpid():
+            return it[1]
+        cls.__it__ = it = (os.getpid(), object.__new__(cls))
+        it[1].init(*args, **kwds)
+        return it[1]
+    def init(self, *args, **kwds):
+        pass
+    @classmethod
+    def _clear_singleton(cls):
+        cls.__it__ = None
+    
+if sys.platform.startswith("win"):
+    get_time = time.clock
+else:
+    get_time = time.time
+    
