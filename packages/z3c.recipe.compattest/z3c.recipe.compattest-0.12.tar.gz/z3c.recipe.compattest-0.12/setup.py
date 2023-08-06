70a6639c2096fff8d4b6aadef649d5af7268ddf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:19 +0000

    Add package z3c.recipe.compattest/0.12/z3c.recipe.compattest-0.12.tar.gz

---
+++ packages/z3c.recipe.compattest/z3c.recipe.compattest-0.12.tar.gz/z3c.recipe.compattest-0.12/setup.py
@@ -0,0 +1,45 @@
+version = '0.12'
+
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name='z3c.recipe.compattest',
+    version = version,
+    author='Grok Contributors',
+    author_email='grok-dev@zope.org',
+    description='Buildout recipe to create testrunners for testing compatibility with other packages',
+    url='http://pypi.python.org/pypi/z3c.recipe.compattest',
+    long_description= (
+        open(os.path.join('src', 'z3c', 'recipe', 'compattest', 'README.txt')).read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    keywords = "zope3 setuptools egg kgs",
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Framework :: Zope3'],
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['z3c', 'z3c.recipe'],
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+        'zc.recipe.testrunner',
+        ],
+    # zope.dottedname is just used as a dummy package to demonstrate things
+    # with, it's not actually imported
+    extras_require=dict(test=[
+          'zope.dottedname',
+          'zope.testing',
+        ]),
+    entry_points = {
+        'zc.buildout': ['default = z3c.recipe.compattest.recipe:Recipe'],
+        },
+    include_package_data = True,
+    zip_safe = False,
+)
