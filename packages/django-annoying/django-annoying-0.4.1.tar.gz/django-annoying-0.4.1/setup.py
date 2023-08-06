703b7616e65221f0547c88fb3a840dd10f960ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:10 +0000

    Add package django-annoying/0.4.1/django-annoying-0.4.1.tar.gz

---
+++ packages/django-annoying/django-annoying-0.4.1.tar.gz/django-annoying-0.4.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-annoying",
+    version = "0.4.1",
+    packages = find_packages(),
+    author = "Anderson",
+    author_email = "self.anderson@gmail.com",
+    description = "This is django application that try to eliminate annoying things in Django framework.",
+    long_description = """
+        **Features:**
+
+            - render_to decorator - reduce typing in django views.
+            - signals decorator - allow use signals as decorators.
+            - ajax_request decorator - returns JsonResponse with this dict as content.
+            - get_object_or_None function - similar to get_object_or_404, but returns None if object not found.
+            - AutoOneToOne field - creates related object on first call if it doesnt exists yet.
+
+                     
+
+        **Installation instruction:**
+
+             - Copy annoying directory to your django project or put in on PYTHONPATH
+             - Also you can run sudo python setup.py install or sudo easy_install django-annoying
+
+                              
+
+        **Download:**
+
+            - hg clone http://hg.assembla.com/django-annoying
+
+    """,
+    license = "BSD",
+    keywords = "django",
+    url = "http://www.assembla.com/spaces/django-annoying",
+)
+
