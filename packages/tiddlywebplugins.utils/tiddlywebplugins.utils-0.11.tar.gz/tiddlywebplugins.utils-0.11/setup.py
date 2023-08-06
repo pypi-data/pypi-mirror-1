684bc0c4494eb68a5c48ca7f9f82870e3d21da2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:10 +0000

    Add package tiddlywebplugins.utils/0.11/tiddlywebplugins.utils-0.11.tar.gz

---
+++ packages/tiddlywebplugins.utils/tiddlywebplugins.utils-0.11.tar.gz/tiddlywebplugins.utils-0.11/setup.py
@@ -0,0 +1,22 @@
+"""
+Setup.py for tiddlywebplugins
+"""
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.11'
+
+setup(
+        namespace_packages = ['tiddlywebplugins'],
+        name = 'tiddlywebplugins.utils',
+        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        version = VERSION,
+        description = 'Tools and methods for managing TiddlyWeb plugins',
+        author = 'Chris Dent',
+        author_email = 'cdent@peermore.com',
+        url = 'http://pypi.python.org/pypi/tiddlywebplugins.utils',
+        packages = find_packages(),
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['setuptools', 'tiddlyweb'],
+        )
