e7801009f1aeb0539c41ab0be0cc4546a94f3e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:40 +0000

    Add package zgeo.plone.kml/0.2/zgeo.plone.kml-0.2.tar.gz

---
+++ packages/zgeo.plone.kml/zgeo.plone.kml-0.2.tar.gz/zgeo.plone.kml-0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '0.2'
+
+setup(name='zgeo.plone.kml',
+      version=version,
+      description="KML for Plone content",
+      long_description="Provides KML views of georeferenced objects, allowing Plone containers and collections to be visualized in Google Earth.",
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        ],
+      keywords='plone geo gis kml google earth',
+      author='Sean Gillies',
+      author_email='sgillies@frii.com',
+      url='http://trac.gispython.org/projects/PrimaGIS/wiki/zgeo.plone.kml',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zgeo', 'zgeo.plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zgeo.kml>=0.4'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
