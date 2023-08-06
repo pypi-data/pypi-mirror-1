bdb16223e17433e817a5edd2da5f3633344b2127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:10 +0000

    Add package zope.app.locales/3.6.0/zope.app.locales-3.6.0.tar.gz

---
+++ packages/zope.app.locales/zope.app.locales-3.6.0.tar.gz/zope.app.locales-3.6.0/src/zope/app/locales/tests.py
@@ -0,0 +1,29 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests for the message string extraction tool.
+
+$Id: tests.py 106918 2009-12-22 19:27:32Z hannosch $
+"""
+import doctest
+import unittest
+from zope.testing.doctest import DocTestSuite
+
+def test_suite():
+    return unittest.TestSuite((
+        DocTestSuite('zope.app.locales.extract',
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,),
+        ))
+
+if __name__ == '__main__':
+    unittest.main()
