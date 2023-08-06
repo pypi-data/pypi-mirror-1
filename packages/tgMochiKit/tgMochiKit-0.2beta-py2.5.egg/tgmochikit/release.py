58a7bfc35e3033e4c204659d3ca06b10221356db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:00 +0000

    Add package tgMochiKit/0.2beta/tgMochiKit-0.2beta-py2.5.egg

---
+++ packages/tgMochiKit/tgMochiKit-0.2beta-py2.5.egg/tgmochikit/release.py
@@ -0,0 +1,12 @@
+# Release information about tgMochikit
+
+version = "0.2beta"
+
+description = "MochiKit packaged as TurboGears widgets."
+author = "Diez B. Roggisch"
+email = "deets@web.de"
+copyright = "Copyright 2008"
+
+url = "http://docs.turbogears.org/tgMochiKit"
+download_url = "http://pypi.python.org/pypi/tgMochiKit"
+license = "MIT"
