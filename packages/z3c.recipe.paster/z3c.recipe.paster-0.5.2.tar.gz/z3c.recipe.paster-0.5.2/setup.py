739e7220e54a4a896ad57a21177ad404ad86197b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:06 +0000

    Add package z3c.recipe.paster/0.5.2/z3c.recipe.paster-0.5.2.tar.gz

---
+++ packages/z3c.recipe.paster/z3c.recipe.paster-0.5.2.tar.gz/z3c.recipe.paster-0.5.2/setup.py
@@ -0,0 +1,122 @@
+##############################################################################
+#
+# Copyright (c) 2007-2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 82497 2007-12-28 14:59:22Z rogerineichen $
+"""
+import os
+import xml.sax.saxutils
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name = 'z3c.recipe.paster',
+    version = '0.5.2',
+    author = 'Roger Ineichen and the Zope Community',
+    author_email = 'zope-dev@zope.org',
+    description = 'Zope3 paste deploy setup recipe',
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n\n' +
+        read('src', 'z3c', 'recipe', 'paster', 'README.txt')
+        + '\n\n' +
+        read('src', 'z3c', 'recipe', 'paster', 'paster.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license = 'ZPL 2.1',
+    keywords = 'zope zope3 z3c paste deploy recipe',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.recipe.paster',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c', 'z3c.recipe'],
+    extras_require = dict(
+        test = [
+            'transaction',
+            'ZODB3',
+            'pytz',
+            'zc.lockfile',
+            'zc.recipe.filestorage',
+            'zdaemon',
+            'zope.annotation',
+            'zope.app.appsetup',
+            'zope.authentication',
+            'zope.app.publication',
+            'zope.browser',
+            'zope.broken',
+            'zope.cachedescriptors',
+            'zope.component',
+            'zope.configuration',
+            'zope.container',
+            'zope.copy',
+            'zope.deferredimport',
+            'zope.dottedname',
+            'zope.error',
+            'zope.event',
+            'zope.exceptions',
+            'zope.filerepresentation',
+            'zope.i18n',
+            'zope.i18nmessageid',
+            'zope.processlifetime',
+            'zope.lifecycleevent',
+            'zope.location',
+            'zope.minmax',
+            'zope.proxy',
+            'zope.publisher',
+            'zope.session',
+            'zope.site',
+            'zope.size',
+            'zope.testing',
+            'zc.buildout',
+            'zope.traversing',
+            'zope.security',
+            ],
+        ),
+    install_requires = [
+        'Paste',
+        'PasteDeploy',
+        'PasteScript',
+        'ZConfig >=2.4a5',
+        'setuptools',
+        'zc.recipe.egg',
+        'zope.app.wsgi',
+        'zope.app.debug',
+        'zope.schema',
+        'zope.interface',
+        ],
+    entry_points = {
+        'zc.buildout': [
+             'serve = z3c.recipe.paster.serve:ServeSetup',
+             'paster = z3c.recipe.paster.paster:PasterSetup',
+             'debug = z3c.recipe.paster.debug:DebugSetup',
+         ]
+    },
+)
---
+++ packages/z3c.recipe.paster/z3c.recipe.paster-0.5.2.tar.gz/z3c.recipe.paster-0.5.2/src/z3c/recipe/paster/tests.py
@@ -0,0 +1,109 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests
+
+$Id:$
+"""
+
+import re
+import unittest
+from zope.testing import doctest
+from zope.testing import renormalizing
+import zc.buildout.testing
+
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install('Paste', test)
+    zc.buildout.testing.install('PasteDeploy', test)
+    zc.buildout.testing.install('PasteScript', test)
+    zc.buildout.testing.install('ZConfig', test)
+    zc.buildout.testing.install('ZODB3', test)
+    zc.buildout.testing.install('pytz', test)
+    zc.buildout.testing.install('transaction', test)
+    zc.buildout.testing.install('zc.lockfile', test)
+    zc.buildout.testing.install('zc.recipe.egg', test)
+    zc.buildout.testing.install('zc.recipe.filestorage', test)
+    zc.buildout.testing.install('zdaemon', test)
+    zc.buildout.testing.install('zope.annotation', test)
+    zc.buildout.testing.install('zope.app.appsetup', test)
+    zc.buildout.testing.install('zope.app.publication', test)
+    zc.buildout.testing.install('zope.app.wsgi', test)
+    zc.buildout.testing.install('zope.authentication', test)
+    zc.buildout.testing.install('zope.broken', test)
+    zc.buildout.testing.install('zope.browser', test)
+    zc.buildout.testing.install('zope.cachedescriptors', test)
+    zc.buildout.testing.install('zope.component', test)
+    zc.buildout.testing.install('zope.configuration', test)
+    zc.buildout.testing.install('zope.container', test)
+    zc.buildout.testing.install('zope.copy', test)
+    zc.buildout.testing.install('zope.deferredimport', test)
+    zc.buildout.testing.install('zope.dottedname', test)
+    zc.buildout.testing.install('zope.error', test)
+    zc.buildout.testing.install('zope.event', test)
+    zc.buildout.testing.install('zope.exceptions', test)
+    zc.buildout.testing.install('zope.filerepresentation', test)
+    zc.buildout.testing.install('zope.i18n', test)
+    zc.buildout.testing.install('zope.i18nmessageid', test)
+    zc.buildout.testing.install('zope.interface', test)
+    zc.buildout.testing.install('zope.lifecycleevent', test)
+    zc.buildout.testing.install('zope.location', test)
+    zc.buildout.testing.install('zope.minmax', test)
+    zc.buildout.testing.install('zope.processlifetime', test)
+    zc.buildout.testing.install('zope.proxy', test)
+    zc.buildout.testing.install('zope.publisher', test)
+    zc.buildout.testing.install('zope.schema', test)
+    zc.buildout.testing.install('zope.security', test)
+    zc.buildout.testing.install('zope.session', test)
+    zc.buildout.testing.install('zope.site', test)
+    zc.buildout.testing.install('zope.size', test)
+    zc.buildout.testing.install('zope.testing', test)
+    zc.buildout.testing.install('zope.traversing', test)
+    zc.buildout.testing.install('zope.contenttype', test)
+    zc.buildout.testing.install('zope.app.debug', test)
+    zc.buildout.testing.install_develop('z3c.recipe.paster', test)
+
+checker = renormalizing.RENormalizing([
+    zc.buildout.testing.normalize_path,
+    # note sure if misspelled?) has \n at the end on linux?
+    (re.compile("Couldn't find index page for '[a-zA-Z0-9.]+' "
+     "\(maybe misspelled\?\)\n"), ''),
+    # windows doesn't have \n at the end of misspelled?)
+    (re.compile("Couldn't find index page for '[a-zA-Z0-9.]+' "
+     "\(maybe misspelled\?\)"), ''),
+    (re.compile("""['"][^\n"']+z3c.recipe.paster[^\n"']*['"],"""),
+     "'/z3c.recipe.paster',"),
+    (re.compile('#![^\n]+\n'), ''),
+    (re.compile('-\S+-py\d[.]\d(-\S+)?.egg'), '-pyN.N.egg'),
+    # the following are for compatibility with Windows
+    (re.compile('-  .*\.exe\n'), ''),
+    (re.compile('-script.py'), ''),
+    ])
+
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('README.txt',
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            checker=checker),
+        doctest.DocFileSuite('paster.txt',
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            checker=checker),
+        doctest.DocFileSuite('debug.txt',
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            checker=checker),
+        ))
