0c5c9e74b94645ad3b183cf9649368dba919236c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:51 +0000

    Add package shoutcast_search/0.3.1/shoutcast_search-0.3.1.tar.gz

---
+++ packages/shoutcast_search/shoutcast_search-0.3.1.tar.gz/shoutcast_search-0.3.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'shoutcast_search',
+    version = '0.3.1',
+    url = 'http://code.k2h.se',
+    author = 'Henrik Hallberg',
+    author_email = 'halhen@k2h.se',
+    license = 'GPL',
+    packages = find_packages(),
+    install_requires = [],
+    description = 'Search shoutcast.com web radio stations',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        ],
+    )
