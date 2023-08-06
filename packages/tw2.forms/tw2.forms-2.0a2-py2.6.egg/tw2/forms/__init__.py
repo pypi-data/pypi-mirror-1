c9eeecd1a2320223cdb1cb11afa3e877e8561dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:29 +0000

    Add package tw2.forms/2.0a2/tw2.forms-2.0a2-py2.6.egg

---
+++ packages/tw2.forms/tw2.forms-2.0a2-py2.6.egg/tw2/forms/__init__.py
@@ -0,0 +1,14 @@
+"""
+This package contains the basic form widgets.
+"""
+
+from widgets import (Button, CheckBox, PostlabeledCheckBox,
+    FieldSet, FileField, Form, HiddenField, ImageButton,
+    Label, Spacer, ListLayout, TableLayout, PasswordField,
+    RadioButton, ResetButton, SubmitButton, TextField, TextArea,
+    SingleSelectField, MultipleSelectField, RadioButtonList, CheckBoxList,
+    RadioButtonTable, CheckBoxTable, GridLayout, RowLayout, TableForm, ListForm,
+    VerticalRadioButtonTable, VerticalCheckBoxTable,
+    TableFieldSet, ListFieldSet, FormPage, FileValidator,
+    LabelField, LinkField)
+from calendars import CalendarDatePicker, CalendarDateTimePicker
