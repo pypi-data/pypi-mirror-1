58bcd9c08c2a2ba8b21cc576c0f30835b85515da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:38 +0000

    Add package pypgoptimizator/1.4/pypgoptimizator-1.4.tar.gz

---
+++ packages/pypgoptimizator/pypgoptimizator-1.4.tar.gz/pypgoptimizator-1.4/setup.py
@@ -0,0 +1,26 @@
+from  setuptools import setup, find_packages
+
+
+readme = open('README.txt').read()
+
+setup(
+    name='pypgoptimizator',
+    version="1.4",
+    description='Postgresql configuration optimizator',
+    packages=find_packages('src'),
+    url="http://git.minitage.org/git/others/pypgoptimizator/",
+    package_dir = {'': 'src'},
+    license='BSD',
+    include_package_data=True,
+    long_description=readme,
+    namespace_packages=['pypgoptimizator'],
+    install_requires=[
+    'setuptools'
+    ],
+    entry_points = {
+    'console_scripts': [
+    'pypgoptimizator = pypgoptimizator.pypgoptimizator:main',
+    ],
+    }
+
+)
