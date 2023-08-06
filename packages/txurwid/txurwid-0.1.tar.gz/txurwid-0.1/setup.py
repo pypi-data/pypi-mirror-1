b3f2517e78b33550fdb15174f70992a4523ee64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:16 +0000

    Add package txurwid/0.1/txurwid-0.1.tar.gz

---
+++ packages/txurwid/txurwid-0.1.tar.gz/txurwid-0.1/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'txurwid',
+    version = '0.1',
+    description = 'Serve Urwid applications remotely over SSH',
+    author='Ali Afshar',
+    author_email='aafshar@gmail.com',
+    url='http://bitbucket.org/aafshar/txurwid-main/',
+    py_modules=['txurwid'],
+)
+
