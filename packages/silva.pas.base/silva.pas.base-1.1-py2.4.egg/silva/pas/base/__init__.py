7a0e1be2d948cae6cfb6a68bb1b0eff3297bb946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package silva.pas.base/1.1/silva.pas.base-1.1-py2.4.egg

---
+++ packages/silva.pas.base/silva.pas.base-1.1-py2.4.egg/silva/pas/base/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: __init__.py 28978 2008-06-03 15:53:31Z sylvain $
+
+import install
+import Membership
+
+from Products.Silva.ExtensionRegistry import extensionRegistry
+from Products.PluggableAuthService.PluggableAuthService import registerMultiPlugin
+from AccessControl.Permissions import manage_users as ManageUsers
+from plugins import cookie
+
+registerMultiPlugin(cookie.SilvaCookieAuthHelper.meta_type)
+
+def initialize(context):
+    extensionRegistry.register(
+        'silva.pas.base', 'Silva Pluggable Auth Service Support', context, [],
+        install, depends_on='Silva')
+    context.registerClass(
+        Membership.MemberService,
+        constructors = (Membership.manage_addMemberServiceForm,
+                        Membership.manage_addMemberService),
+        )
+    context.registerClass(
+        cookie.SilvaCookieAuthHelper,
+        permission=ManageUsers,
+        constructors=
+        (cookie.manage_addSilvaCookieAuthHelperForm,
+         cookie.manage_addSilvaCookieAuthHelper),
+        visibility=None)
+
+
