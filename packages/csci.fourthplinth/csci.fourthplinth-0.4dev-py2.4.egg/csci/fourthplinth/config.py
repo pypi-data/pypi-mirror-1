9f063b5234ef2d76228c3f01c1a133896dd6cf04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:44 +0000

    Add package csci.fourthplinth/0.1dev/csci.fourthplinth-0.4dev-py2.4.egg

---
+++ packages/csci.fourthplinth/csci.fourthplinth-0.4dev-py2.4.egg/csci/fourthplinth/config.py
@@ -0,0 +1,9 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'csci.fourthplinth'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'fourthplinth': 'csci.fourthplinth: Add fourthplinth',
+}
---
+++ packages/csci.fourthplinth/csci.fourthplinth-0.4dev-py2.4.egg/csci/fourthplinth/interfaces/fourthplinth.py
@@ -0,0 +1,42 @@
+from zope import schema
+from zope.interface import Interface
+
+from zope.app.container.constraints import contains
+from zope.app.container.constraints import containers
+
+from csci.fourthplinth import fourthplinthMessageFactory as _
+
+class Ifourthplinth(Interface):
+    """Fourth Plinth"""
+    
+    # -*- schema definition goes here -*-
+    endtag = schema.TextLine(
+        title=_(u"End tag for post"), 
+        required=False,
+        description=_(u"enter some text to add to the end of the post"),
+    )
+
+    debugmode = schema.Bool(
+        title=_(u"Debug Mode"), 
+        required=False,
+        description=_(u"enter debug mode"),
+    )
+
+    tschedule = schema.Text(
+        title=_(u"Generated schedule for today"), 
+        required=False,
+        description=_(u"Field description"),
+    )
+
+    tpassword = schema.TextLine(
+        title=_(u"Twitter Password"), 
+        required=True,
+        description=_(u"Password"),
+    )
+
+    tusername = schema.TextLine(
+        title=_(u"New Field"), 
+        required=False,
+        description=_(u"Field description"),
+    )
+
