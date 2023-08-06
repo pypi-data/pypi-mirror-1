17b74493bbd365ef52635b760a7a5992ef5fdfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:31 +0000

    Add package Editra/0.5.0/Editra-0.5.0-py2.6.egg

---
+++ packages/Editra/Editra-0.5.0-py2.6.egg/Editra/src/info.py
@@ -0,0 +1,22 @@
+###############################################################################
+# Name: info.py                                                               #
+# Purpose: Global project information                                         #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2008 Cody Precord <staff@editra.org>                         #
+# License: wxWindows License                                                  #
+###############################################################################
+
+"""Editra Project information module"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id: info.py 61721 2009-08-20 23:13:12Z CJP $"
+__revision__ = "$Revision: 61721 $"
+
+#-----------------------------------------------------------------------------#
+
+AUTHOR = 'Cody Precord'
+VERSION = '0.5.0'
+PROG_NAME = 'Editra'
+HOME_PAGE = "http://editra.org"
+I18N_PAGE = "https://translations.launchpad.net/editra/trunk/+pots/editra"
+CONTACT_MAIL = "staff@editra.org"
