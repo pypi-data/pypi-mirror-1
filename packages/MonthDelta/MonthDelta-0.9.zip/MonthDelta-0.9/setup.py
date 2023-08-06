5b848a3d2213dc5e511c88447cd56bc1f45d905c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package MonthDelta/0.9/MonthDelta-0.9.zip

---
+++ packages/MonthDelta/MonthDelta-0.9.zip/MonthDelta-0.9/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MonthDelta',
+      version='0.9',
+      description='date computations with months',
+      author='Jess Austin',
+      author_email='jess.austin@gmail.com',
+      url='http://monocular.org/code/monthdelta',
+      py_modules=['monthdelta'])
