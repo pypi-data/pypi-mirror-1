813895c5a2c6bf602ff49b4bdb87070f03638def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:19 +0000

    Add package notefinder/0.9/notefinder.tar.gz

---
+++ packages/notefinder/notefinder.tar.gz/notefinder/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='notefinder',
+	version='0.9',
+	description='Package for easy notes management',
+       	author='GFORGX',
+        author_email='gforgx@gmail.com',
+        url='http://notefinder.googlecode.com',
+    	py_modules=['notefinder'],
+       )
