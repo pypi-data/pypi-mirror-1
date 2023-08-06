1d33d0d813326441bf7e3f2cfb513e7fabb12adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:33 +0000

    Add package Products.Andreas09Theme/0.1-step05/Products.Andreas09Theme-0.1-step05.tar.gz

---
+++ packages/Products.Andreas09Theme/Products.Andreas09Theme-0.1-step05.tar.gz/Products.Andreas09Theme-0.1-step05/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1-step05'
+
+setup(name='Products.Andreas09Theme',
+      version=version,
+      description="An example theme for Plone 3.0",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme example tutorial how-to',
+      author='David Convent',
+      author_email='product-developers@lists.plone.org',
+      url='http://pypi.python.org/pypi/Products.Andreas09Theme',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
