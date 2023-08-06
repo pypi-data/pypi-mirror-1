cff1bbf4120388d4cde5f82f9fbfbee3e3c84080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:35 +0000

    Add package themetweaker.themeswitcher/0.8.6a1/themetweaker.themeswitcher-0.8.6a1.tar.gz

---
+++ packages/themetweaker.themeswitcher/themetweaker.themeswitcher-0.8.6a1.tar.gz/themetweaker.themeswitcher-0.8.6a1/themetweaker/themeswitcher/traverser.py
@@ -0,0 +1,83 @@
+from zope.interface import implements, noLongerProvides, directlyProvides, directlyProvidedBy
+from zope.component import adapts
+
+from zope.publisher.interfaces.http import IHTTPRequest
+from zope.publisher.interfaces.browser import IBrowserPublisher
+from Products.ATContentTypes.interface.folder import IATFolder
+from Products.CMFCore.interfaces import IContentish
+from zope.annotation.interfaces import IAnnotations
+
+from themetweaker.themeswitcher.interfaces import ISwitchSetter
+# from Products.CMFPlone.interfaces import IPloneSiteRoot
+from themetweaker.themeswitcher.interfaces import IThemeSwitcher
+from ZPublisher.BaseRequest import DefaultPublishTraverse
+from zope.traversing.interfaces import ITraversable
+
+from zope.component import getUtilitiesFor
+from plone.browserlayer.interfaces import ILocalBrowserLayerType
+
+class SwitchSetter(object):
+
+    adapts(IHTTPRequest)
+    implements(ISwitchSetter)
+
+    key = u'themeswitcher.marked'
+    
+    def __init__(self, request):
+        self.request = request
+
+    def _provide(self, iface):
+        annotation = IAnnotations(self.request)
+        previous = annotation.get(self.key, None)
+        if previous is None:
+            annotation[self.key] = iface
+            return iface
+        else:
+            annotation[self.key] = iface
+            noLongerProvides(self.request, previous)
+            return previous
+
+    def mark(self, iface):
+        old = self._provide(iface)
+        if not iface.providedBy(self.request):
+            directlyProvides(self.request, iface)
+        return self.request
+
+class Traverser(DefaultPublishTraverse):
+    """Provide traverse features
+    """
+    adapts(IContentish, IHTTPRequest)
+    implements(IBrowserPublisher)
+    
+    key = u'themeswitcher_iface'
+    
+    def publishTraverse(self, request, name):
+        print "traversing %s and asking for %s" % (self.context, name)
+        # request = ISwitchSetter(request).mark(Interface)
+        if IThemeSwitcher.providedBy(self.context):
+            # Apply the prefered browser layer rather than the
+            layers = [ x for x in getUtilitiesFor(ILocalBrowserLayerType) ]
+            annotation = IAnnotations(self.context)
+            iface = annotation.get(self.key, None)['themeswitcher_skin']
+            skin_name = None
+            # Unfortuanately we need the skin name for the changeSkin method
+            for layer in layers:
+                if layer[1] == iface:
+                    skin_name = layer[0]
+                    break
+            if skin_name is None:
+                # FIXME do something meaningful here
+                self.context.changeSkin('Plone Default', request)
+            elif skin_name == self.context.getCurrentSkinName():
+                # If the skin is already the current one, don't do anything
+                # because we have already traversed
+                print "Same name? Around here we like people with the same name, pass right on through :)"
+                return super(Traverser, self).publishTraverse(request, name)
+            else:
+                print "IThemeSwitcher!!!"
+                self.context.changeSkin(skin_name, request)
+                request = ISwitchSetter(request).mark(iface)
+
+        # Then restore normal traversing
+        return super(Traverser, self).publishTraverse(request, name)
+        
