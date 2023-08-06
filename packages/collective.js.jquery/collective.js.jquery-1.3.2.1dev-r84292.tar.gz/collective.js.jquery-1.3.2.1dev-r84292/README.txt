3b4e4baa48322a76568aeb6090aa9f5311df5e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:51 +0000

    Add package collective.js.jquery/1.3.2.1dev-r84292/collective.js.jquery-1.3.2.1dev-r84292.tar.gz

---
+++ packages/collective.js.jquery/collective.js.jquery-1.3.2.1dev-r84292.tar.gz/collective.js.jquery-1.3.2.1dev-r84292/README.txt
@@ -0,0 +1,4 @@
+Introduction
+============
+
+collective.js.jquery is a simple packaging of the jQuery library that uses setuptools for version selection and adds as a Zope browser resource.  If CMF/Plone is available it is also added to the JavaScript registry.
---
+++ packages/collective.js.jquery/collective.js.jquery-1.3.2.1dev-r84292.tar.gz/collective.js.jquery-1.3.2.1dev-r84292/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3.2.1'
+
+setup(name='collective.js.jquery',
+      version=version,
+      description="A packaging of the jQuery library for Zope",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
