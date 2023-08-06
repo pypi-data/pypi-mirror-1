55fb12a46541e5a461292644866e7f12bdccbf56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:25 +0000

    Add package zope.formlib/4.0/zope.formlib-4.0.tar.gz

---
+++ packages/zope.formlib/zope.formlib-4.0.tar.gz/zope.formlib-4.0/src/zope/formlib/namedtemplate.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2005-2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id: namedtemplate.py 106944 2009-12-22 21:49:19Z hannosch $
+"""
+
+# BBB
+from zope.browserpage.namedtemplate import (
+    INamedTemplate, NamedTemplateImplementation, implementation,
+    NamedTemplate, NamedTemplatePathAdapter)
