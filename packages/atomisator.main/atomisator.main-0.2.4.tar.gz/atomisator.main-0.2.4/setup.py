1debf521ec86f967573c9c7df96aa5692c6f558e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:37 +0000

    Add package atomisator.main/0.2.4/atomisator.main-0.2.4.tar.gz

---
+++ packages/atomisator.main/atomisator.main-0.2.4.tar.gz/atomisator.main-0.2.4/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+from atomisator.main import __version__ as VERSION
+
+long_description = open("README.txt").read()
+classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+
+entry_points = {
+    "console_scripts": [
+        "atomisator = atomisator.main.run:atomisator"
+    ]
+}
+
+setup(name='atomisator.main',
+      version=VERSION,
+      description="Expert Python Programming - Main package for Atomisator app",
+      long_description=long_description,
+      classifiers=classifiers,
+      keywords='',
+      author='Tarek Ziade',
+      author_email='tarek@ziade.org',
+      url='http://atomisator.ziade.org',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['atomisator'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'atomisator.db>=0.2.4',
+          'atomisator.feed>=0.2.5',
+          # default plugin
+          'atomisator.parser>=0.2.2',
+          # default filters
+          'atomisator.filters>=0.1.2'
+      ],
+      tests_require=['nose'],
+      test_suite='atomisator.main.tests.test_docs.test_suite',
+      entry_points=entry_points
+      )
+
