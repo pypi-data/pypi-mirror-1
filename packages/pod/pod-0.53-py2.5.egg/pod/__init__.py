a68c21c7c4c098601492c3c9e578e47c2ae63c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:48 +0000

    Add package pod/0.53/pod-0.53-py2.5.egg

---
+++ packages/pod/pod-0.53-py2.5.egg/pod/__init__.py
@@ -0,0 +1,12 @@
+from core   import Meta, Object, NoSave, Undefined, Deleted
+from db     import Db
+from query  import Query, RawQuery
+
+import types
+import fn
+
+from collection import List, Dict, Set
+
+
+
+        
