47344f2bdbfa2539f6aec47595aae80088dbb249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:19 +0000

    Add package huTools/0.39p3/huTools-0.39p3.tar.gz

---
+++ packages/huTools/huTools-0.39p3.tar.gz/huTools-0.39p3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='huTools',
+      maintainer='Maximillian Dornseif',
+      maintainer_email='md@hudora.de',
+      url='https://cybernetics.hudora.biz/projects/wiki/huTools/',
+      version='0.39p3',
+      description='Various tiny tools and toys to make Python coding less work more fun.',
+      long_description='''huTools is a collection of many totally non earth shattering modules:
+      Various tiny tools and toys to make Python coding less work more fun. 
+      See https://cybernetics.hudora.biz/projects/wiki/huTools/ for further Information.''',
+      license='BSD',
+      classifiers=['Intended Audience :: Developers',
+                   'Programming Language :: Python'],
+      packages = find_packages(),
+      zip_safe = False,
+      install_requires=['decorator', 'simplejson>2.0'], 
+      )
