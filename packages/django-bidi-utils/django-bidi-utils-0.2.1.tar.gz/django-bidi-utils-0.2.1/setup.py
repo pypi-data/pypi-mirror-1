4f3d2b0363bc6e1b2313bc2548439ca565d2af52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:11 +0000

    Add package django-bidi-utils/0.2.1/django-bidi-utils-0.2.1.tar.gz

---
+++ packages/django-bidi-utils/django-bidi-utils-0.2.1.tar.gz/django-bidi-utils-0.2.1/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-bidi-utils",
+    version = "0.2.1",
+    url = 'http://github.com/mksoft/django-bidi-utils',
+    license = 'MIT',
+    description = "context processors and helpers for BIDI in django templates",
+    long_description = read('README.rst') + read('CHANGELOG.rst'),
+
+    author = 'Meir Kriheli',
+    author_email = 'meir@mksoft.co.il',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+
+    install_requires = ['setuptools'],
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
