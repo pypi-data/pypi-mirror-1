b837890cf5d9d92ac3cfa7bd4893bf56b12c615d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package json_schema/0.2/json_schema-0.2.tar.gz

---
+++ packages/json_schema/json_schema-0.2.tar.gz/json_schema-0.2/json_schema/__init__.py
@@ -0,0 +1,28 @@
+"""JSON schema defintion & validation
+
+Example:
+
+>>> from json_schema import SchemaCollection, AnyString, AnyInteger
+>>> class Foo(SchemaCollection):
+...     abc = [u"hello", {u"world": AnyString}]
+...     bla = [u"hello", {u"world": AnyInteger}]
+...     bli = [u"foo", AnyInteger, u"bar"]
+... 
+>>> Foo.abc  # doctest: +ELLIPSIS
+<json_schema.schema.Schema object at ...
+>>> Foo.match_to_schema([u"hello", {u"world": u"Hey"}])
+'abc'
+>>> Foo.match_to_schema([u"hello", {u"world": 123}])
+'bla'
+>>> Foo.match_to_schema([u"hey there", {u"world": 123}])
+>>> Foo.match_to_schema([u"foo", 1337, u"bar"])
+'bli'
+"""
+
+from json_schema.defs import *
+from json_schema.validator import validate as validate_json_schema
+from json_schema.schema import SchemaCollection, Schema
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ packages/json_schema/json_schema-0.2.tar.gz/json_schema-0.2/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="json_schema", version="0.2",
+    url="http://lericson.se/",
+    author="Ludvig Ericson", author_email="ludvig@blogg.se",
+    description="JSON schema definition and validation library",
+    packages=["json_schema"])
