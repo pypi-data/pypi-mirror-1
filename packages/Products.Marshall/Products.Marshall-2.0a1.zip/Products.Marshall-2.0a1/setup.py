6ae3ea6b639adbefe736bcb74c54dbabcb4676ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:32 +0000

    Add package Products.Marshall/2.0a1/Products.Marshall-2.0a1.zip

---
+++ packages/Products.Marshall/Products.Marshall-2.0a1.zip/Products.Marshall-2.0a1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+version = '2.0a1'
+
+
+setup(name='Products.Marshall',
+      version=version,
+      description="Configurable Marshallers for Archetypes",
+      long_description=open("README.txt").read() + "\n" + \
+              open("CHANGES.txt").read(),
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: File Transfer Protocol (FTP)",
+        ],
+      keywords='web zope application server webdav ftp',
+      license="GPL",
+      author='Sidnei da Silve and others',
+      author_email='plone-developers@lists.sourceforge.net',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'zope.schema',
+            'Products.ATContentTypes',
+        ]
+      ),
+      install_requires=[
+          'setuptools',
+          'zope.contenttype',
+          'zope.interface',
+          'Products.Archetypes',
+          'Products.CMFCore',
+          'Products.GenericSetup',
+          'Acquisition',
+          'DateTime',
+          'ExtensionClass',
+          'Zope2',
+      ],
+      )
