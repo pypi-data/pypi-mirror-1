df7dd052602a4398d0ae744183ced970e1529d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package p4a.common/1.0.3/p4a.common-1.0.3.tar.gz

---
+++ packages/p4a.common/p4a.common-1.0.3.tar.gz/p4a.common-1.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '1.0.3'
+
+setup(name='p4a.common',
+      version=version,
+      description="Reusable code-bits for Zope 3 and Plone",
+      long_description="""""",
+      classifiers=[
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Rocky Burt',
+      author_email='rocky@serverzen.com',
+      url='http://www.plone4artists.org/svn/projects/p4a.common/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['p4a'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "setuptools",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
