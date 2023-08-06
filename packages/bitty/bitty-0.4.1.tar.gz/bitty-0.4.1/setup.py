7cbe3ccb1c7344c9e20c7ad42195610909fa064e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:23 +0000

    Add package bitty/0.4.1/bitty-0.4.1.tar.gz

---
+++ packages/bitty/bitty-0.4.1.tar.gz/bitty-0.4.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='bitty',
+    version='0.4.1',
+    description='A tiny database layer. Serious Python Programmers(tm) with Enterprise Requirements need not apply.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='http://github.com/toastdriven/bitty',
+    py_modules=['bitty'],
+    license='BSD',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
