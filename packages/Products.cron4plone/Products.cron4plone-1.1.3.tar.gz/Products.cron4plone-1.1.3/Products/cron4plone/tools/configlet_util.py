05f2d2d61f91b4934ef05a4dec35cb0339922bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:40 +0000

    Add package Products.cron4plone/1.1.3/Products.cron4plone-1.1.3.tar.gz

---
+++ packages/Products.cron4plone/Products.cron4plone-1.1.3.tar.gz/Products.cron4plone-1.1.3/Products/cron4plone/tools/configlet_util.py
@@ -0,0 +1,20 @@
+from zope.component import getUtility
+from zope.interface import implements
+from zope.schema.fieldproperty import FieldProperty
+from OFS.SimpleItem import SimpleItem
+
+from Products.cron4plone.interfaces import ICronConfiguration
+
+
+def form_adapter(context):
+    return getUtility(ICronConfiguration, name='cron4plone_config', context=context)
+
+
+class ConfigletUtil(SimpleItem):
+    implements(ICronConfiguration)
+
+    def __call__(self):
+        print self.cronjobs
+
+    cronjobs = FieldProperty(ICronConfiguration['cronjobs'])
+
