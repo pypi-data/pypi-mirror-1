801fdad7228f9975d55c40431301ea320dece6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:55 +0000

    Add package Products.validation/1.6.1/Products.validation-1.6.1.tar.gz

---
+++ packages/Products.validation/Products.validation-1.6.1.tar.gz/Products.validation-1.6.1/Products/validation/validators/RangeValidator.py
@@ -0,0 +1,35 @@
+from Products.validation.interfaces.IValidator import IValidator
+
+
+class RangeValidator:
+    __implements__ = IValidator
+
+    def __init__(self, name, minval=0.0, maxval=0.0, title='', description=''):
+        self.name = name
+        self.minval = minval
+        self.maxval = maxval
+        self.title = title or name
+        self.description = description
+
+    def __call__(self, value, *args, **kwargs):
+        if len(args)>=1:
+            minval=args[0]
+        else:
+            minval=self.minval
+            
+        if len(args)>=2:
+            maxval=args[1]
+        else:
+            maxval=self.maxval
+
+        assert(minval <= maxval)
+        try:
+            nval = float(value)
+        except ValueError:
+            return ("Validation failed(%(name)s): could not convert '%(value)r' to number" %
+                    { 'name' : self.name, 'value': value})
+        if minval <= nval < maxval:
+            return 1
+
+        return ("Validation failed(%(name)s): '%(value)s' out of range(%(min)s, %(max)s)" %
+                { 'name' : self.name, 'value': value, 'min' : minval, 'max' : maxval,})
