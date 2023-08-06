92ec37f2b68a8dc7d579e02c9e9d683bbc2f1e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:34 +0000

    Add package itertools_recipes/0.1/itertools_recipes-0.1.tar.gz

---
+++ packages/itertools_recipes/itertools_recipes-0.1.tar.gz/itertools_recipes-0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='itertools_recipes',
+    version='0.1',
+    author='Eddy Mulyono',
+    author_email='eddymul@gmail.com',
+    license='PSF',
+    description='Pasted from the "Recipes" section of the "itertools" documentations: An extended toolset using the existing itertools as building blocks.',
+    py_modules=['itertools_recipes'],
+    )
