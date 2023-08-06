67a02729ffa988e5fd1df8ef1008e8e060178881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:36 +0000

    Add package Phon/0.1a/Phon-0.1a.tar.gz

---
+++ packages/Phon/Phon-0.1a.tar.gz/Phon-0.1a/setup.py
@@ -0,0 +1,18 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
+setup(
+    name = "Phon",
+    description = "Unix filter for naive phonological analysis of words in wordlists.",
+    version = "0.1a",
+    scripts = ['phon'],
+    author = 'kaleissin',
+    author_email = 'kaleissin@gmail.com',
+    license = 'MIT',
+    keywords = "phonology linguistics analysis unix",
+    long_description = """Given a list of words, one word per line, on
+stdin, phon will dump onsets, codas, consonants between vowels
+(middles) and all consonant groupings (pieces)."""
+)
+
