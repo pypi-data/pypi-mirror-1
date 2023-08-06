140927d4fcd26e2b46089770337b168fb4ede20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:02 +0000

    Add package softwarefabrica.django.crud/1.0dev-BZR-r61-panta-elasticworld.org-20081216105417-7eujtvxt76r11345/softwarefabrica.django.crud-1.0dev_BZR_r61_panta_elasticworld.org_20081216105417_7eujtvxt76r11345-py2.6.egg

---
+++ packages/softwarefabrica.django.crud/softwarefabrica.django.crud-1.0dev_BZR_r61_panta_elasticworld.org_20081216105417_7eujtvxt76r11345-py2.6.egg/softwarefabrica/django/crud/bzrcachedrev.py
@@ -0,0 +1,4 @@
+info    = {'revinfo': (61, 'panta@elasticworld.org-20081216105417-7eujtvxt76r11345'), 'basepath': '/arc/bzr/softwarefabrica-crud.bzr/trunk/', 'revno': 61, 'revname': 'panta@elasticworld.org-20081216105417-7eujtvxt76r11345', 'inpath': u'softwarefabrica/django/crud/version.py'}
+revno   = 61
+revname = 'panta@elasticworld.org-20081216105417-7eujtvxt76r11345'
+revinfo = (61, 'panta@elasticworld.org-20081216105417-7eujtvxt76r11345')
