7fc213d0f2b9331170a39bfebd3358bcae1f98e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:59 +0000

    Add package collective.formcriteria/0.9/collective.formcriteria-0.9.tar.gz

---
+++ packages/collective.formcriteria/collective.formcriteria-0.9.tar.gz/collective.formcriteria-0.9/collective/formcriteria/form.py
@@ -0,0 +1,72 @@
+"""Form criteria search form"""
+
+from plone.memoize import view
+
+def makeFormKey(self, crit_id, field_name):
+    return 'form_%s_%s' % (crit_id, field_name)
+
+class SearchFormView(object):
+
+    def render(self, *args, **kw):
+        if self.formCriteria():
+            return super(SearchFormView, self).render(*args, **kw)
+        return ''
+
+    @view.memoize
+    def formCriteria(self):
+        return [
+            criterion for criterion in self.context.listCriteria()
+            if getattr(criterion, 'getFormFields', lambda : False)()]
+
+    @view.memoize
+    def criteriaFields(self):
+        results = []
+        for criterion in self.formCriteria():
+            crit_id = criterion.getId()
+            field = criterion.Field()
+            index = self.context.portal_atct.getIndex(field)
+
+            fields = []
+            for field_name in criterion.getFormFields():
+                field = criterion.getField(field_name)
+                new_field = field.copy()
+                new_field.write_permission = field.read_permission
+                fields.append(new_field)
+
+            result = {
+                'id': crit_id,
+                'field': field,
+                'friendlyName': index.friendlyName or index.index,
+                'description': index.description,
+                'fields': fields,
+                'widget': criterion.widget,
+                }
+
+            results.append(result)
+        return results
+
+    def fields(self):
+        results = []
+        for criterion in self.criteriaFields():
+            results.extend(criterion['fields'])
+        return results
+
+    def ifHead(self):
+        return (
+            getattr(self._parent, '_data', {}).get(
+                'template_id') != 'criterion_edit_form'
+            and self.formCriteria())
+
+    makeFormKey = makeFormKey
+
+    def action(self):
+        name = self._parent.__name__
+        if name == 'plone':
+            name = self._parent._data['template_id']
+
+        if name == 'criteria_form':
+            return '%s/%s' % (
+                self.context.absolute_url(),
+                self.context.getFormLayout())
+
+        return name
---
+++ packages/collective.formcriteria/collective.formcriteria-0.9.tar.gz/collective.formcriteria-0.9/collective/formcriteria/testing.py
@@ -0,0 +1,60 @@
+import DateTime
+from Products.CMFPlone import utils as plone_utils
+
+from Testing import ZopeTestCase
+from Products.PloneTestCase import ptc
+
+from Products.Five import zcml, fiveconfigure
+
+from collective.testcaselayer import ptc as tcl_ptc
+
+from collective import formcriteria
+
+ptc.setupPloneSite()
+
+class FormCriteriaLayer(tcl_ptc.BasePTCLayer):
+    """Install collective.formcriteria"""
+
+    def afterSetUp(self):
+        fiveconfigure.debug_mode = True
+        zcml.load_config('testing.zcml', package=formcriteria)
+        fiveconfigure.debug_mode = False
+
+        ZopeTestCase.installPackage('collective.formcriteria')
+
+        self.addProfile('collective.formcriteria:default')
+        self.addProfile('collective.formcriteria:testing')
+
+        self.login()
+        home = self.portal.portal_membership.getHomeFolder(
+            ptc.default_user)
+        plone_utils._createObjectByType(
+            container=home, type_name='Topic',
+            id='foo-topic-title', title='Foo Topic Title')
+
+        self.now = DateTime.DateTime('Jan 15, 2009')
+        home.invokeFactory(
+            type_name='Event', effectiveDate=self.now-3,
+            id='foo-event-title', title='Foo Event Title')
+        home.invokeFactory(
+            type_name='Document', effectiveDate=self.now-2,
+            id='bar-document-title', title='Bar Document Title',
+            description='blah', subject=['bah', 'qux'])
+        home.invokeFactory(
+            type_name='Event', effectiveDate=self.now,
+            id='baz-event-title', title='Baz Event Title',
+            # More relevant for a "blah" search
+            description='blah blah',
+            # For Events eventType == subject
+            eventType=['qux', 'quux'])
+
+        self.loginAsPortalOwner()
+        self.portal.portal_workflow.doActionFor(
+            home['foo-topic-title'], 'publish')
+        self.portal.portal_workflow.doActionFor(
+            home['bar-document-title'], 'publish')
+        self.portal.portal_workflow.doActionFor(
+            home['baz-event-title'], 'publish')
+        self.login()
+
+formcriteria_layer = FormCriteriaLayer([tcl_ptc.ptc_layer])
