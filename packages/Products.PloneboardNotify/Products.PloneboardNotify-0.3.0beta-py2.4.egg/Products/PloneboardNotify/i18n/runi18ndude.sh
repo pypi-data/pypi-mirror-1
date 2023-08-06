42ef94746b3f6b02fefa0bdb286caa73c23dd90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:14 +0000

    Add package Products.PloneboardNotify/0.3.0beta/Products.PloneboardNotify-0.3.0beta-py2.4.egg

---
+++ packages/Products.PloneboardNotify/Products.PloneboardNotify-0.3.0beta-py2.4.egg/Products/PloneboardNotify/i18n/runi18ndude.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+TEMPLATES="find .. -name '*.*pt'"
+
+i18ndude rebuild-pot --pot Products.PloneboardNotify.pot --create Products.PloneboardNotify $TEMPLATES 
+i18ndude sync --pot Products.PloneboardNotify.pot Products.PloneboardNotify-it.po
+
