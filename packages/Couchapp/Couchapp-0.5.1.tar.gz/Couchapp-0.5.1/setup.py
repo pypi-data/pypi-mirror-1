152e69e5c7a90ce7364d1935bce895fc57f1a7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:11 +0000

    Add package Couchapp/0.5.1/Couchapp-0.5.1.tar.gz

---
+++ packages/Couchapp/Couchapp-0.5.1.tar.gz/Couchapp-0.5.1/setup.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008,2009  Benoit Chesneau <benoitc@e-engura.org>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at#
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+import sys
+
+if not hasattr(sys, 'version_info') or sys.version_info < (2, 5, 0, 'final'):
+    raise SystemExit("Couchapp requires Python 2.5 or later.")
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+    
+extra = {}
+data_files = []
+
+for dir, dirs, files in os.walk('templates'):
+    data_files.append((os.path.join('couchapp', dir), 
+        [os.path.join(dir, file_) for file_ in files]))
+
+for dir, dirs, files in os.walk('vendor'):
+    data_files.append((os.path.join('couchapp', dir), 
+        [os.path.join(dir, file_) for file_ in files]))
+    
+
+scripts = ['bin/couchapp']
+if os.name == 'nt':
+    scripts.append('contrib/win32/couchapp.bat')
+    
+packages = ['couchapp', 'couchapp.simplejson', 'couchappext', 'couchappext.compress',]
+
+
+# py2exe needs to be installed to work
+try:
+    import py2exe
+
+    # Help py2exe to find win32com.shell
+    try:
+        import modulefinder
+        import win32com
+        for p in win32com.__path__[1:]: # Take the path to win32comext
+            modulefinder.AddPackagePath("win32com", p)
+        pn = "win32com.shell"
+        __import__(pn)
+        m = sys.modules[pn]
+        for p in m.__path__[1:]:
+            modulefinder.AddPackagePath(pn, p)
+    except ImportError:
+        pass
+
+    extra['console'] = ['bin/couchapp']
+    
+except ImportError:
+    pass
+ 
+setup(
+    name = 'Couchapp',
+    version = '0.5.1',
+    url = 'http://github.com/couchapp/couchapp/tree/master',
+    license =  'Apache License 2',
+    author = 'Benoit Chesneau',
+    author_email = 'benoitc@e-engura.org',
+    description = 'Standalone CouchDB Application Development Made Simple.',
+    long_description = """CouchApp is a set of helpers and a jQuery plugin
+    that conspire to get you up and running on CouchDB quickly and
+    correctly. It brings clarity and order to the freedom of CouchDB's
+    document-based approach.""",
+    keywords = 'couchdb couchapp',
+    platforms = ['any'],
+
+    packages=packages,
+    data_files = data_files,
+    include_package_data = True,
+    
+    install_requires = [],
+    scripts = scripts,
+    options = dict(py2exe=dict(packages=['couchappext']),
+                   bdist_mpkg=dict(zipdist=True,
+                                   license='LICENSE',
+                                   readme='contrib/macosx/Readme.html',
+                                   welcome='contrib/macosx/Welcome.html')),
+    classifiers = [
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Database',
+        'Topic :: Utilities',
+    ],
+    test_suite='tests',
+    **extra
+)
