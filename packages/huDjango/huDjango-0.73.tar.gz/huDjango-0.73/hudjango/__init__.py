5c93fc89b37fdd9af76b3d3c64c678346e498157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:30 +0000

    Add package huDjango/0.73/huDjango-0.73.tar.gz

---
+++ packages/huDjango/huDjango-0.73.tar.gz/huDjango-0.73/hudjango/__init__.py
@@ -0,0 +1,4 @@
+"""This module collects several extensions to  Django we found useful at HUDORA.
+See http://www.djangoproject.com/ and https://cybernetics.hudora.biz/projects/wiki/huDjango
+
+"""
---
+++ packages/huDjango/huDjango-0.73.tar.gz/huDjango-0.73/setup.py
@@ -0,0 +1,38 @@
+long_description = """
+huDjango contains a collection of small extensions th the Django Web framework.
+
+ * auth.backends.EmailBackend - authentication with E-Mail adress
+ * auth.backends.ZimbraBackend - authentication against a Zimbra LDAP server
+ * fields.scalingimagefield - images with automatic scaling
+ * fields.defaulting - Django fields with more elaborate default values
+ * templatetags.hudjango - Various template tags to make life more fun.
+ * middleware.threadlocals - Thread local storage
+"""
+
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+# patch distutils if it can't cope with the "classifiers" or
+# "download_url" keywords
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+            
+hubarcode = setup(name='huDjango',
+      maintainer='Maximillian Dornseif',
+      maintainer_email='md@hudora.de',
+      url='https://cybernetics.hudora.biz/projects/wiki/huDjango',
+      version='0.73',
+      description='various snippets for use with Django',
+      long_description=long_description,
+      classifiers=['License :: OSI Approved :: BSD License',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python'],
+      # download_url
+      #packages=['hudjango'],
+      packages = find_packages(),
+)
+
