b54b8348fdef1d1a7aa06df6a835ef89651caaab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:46 +0000

    Add package c2.transform.msoffice/0.1.1dev/c2.transform.msoffice-0.1.1dev.tar.gz

---
+++ packages/c2.transform.msoffice/c2.transform.msoffice-0.1.1dev.tar.gz/c2.transform.msoffice-0.1.1dev/README.txt
@@ -0,0 +1,28 @@
+Introduction
+============
+
+Description
+-------------
+
+This product enables to search MS-Offile files on Plone.
+
+A files
+--------
+- MS-Offile files
+
+  - MS-Word 95-2000, XP, 2007
+  - MS-Excle 95-2000, XP, 2007
+  - MS-PowerPoint 95-2000, XP, 2007
+
+Requirement
+-------------
+
+- Plone
+
+  - Plone 3.0-3.3 (tested by 3.3.2 on CentOS 5.3 64bit)
+  - May be able to use on Plone 4 (but not tested)
+
+- Java
+
+  - JRE 1.6 (tested by 1.6.0)
+  - May be able to use JRE 1.5 (but not tested)
---
+++ packages/c2.transform.msoffice/c2.transform.msoffice-0.1.1dev.tar.gz/c2.transform.msoffice-0.1.1dev/c2/transform/msoffice/transform.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+transform.py
+
+Created by Manabu Terada on 2009-12-02.
+Copyright (c) 2009 CMScom. All rights reserved.
+"""
+import os
+import time
+import subprocess, select
+from cStringIO import StringIO
+import mimetypes
+from Products.PortalTransforms.interfaces import itransform
+from config import SITE_CHARSET, TRANSFORM_NAME
+from c2.transform.msoffice import logger
+
+scpath = os.path.join(os.path.dirname(__file__), 'msdoc2txt.jar')
+
+def msdoc2txt(data, timeout=60): #default 60 sec
+    assert isinstance(data, str)
+    p = subprocess.Popen(("java", '-jar', '-Xmx512m', scpath),
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         stdin=subprocess.PIPE)
+    p.stdin.write(data)
+    p.stdin.close()
+    outs = select.select([p.stdout],(),(),timeout)[0]
+    errs = select.select([p.stderr],(),(),0)[0]
+    if errs:
+        err = errs[0]
+        return False, err.read() # 失敗したら(False, エラーメッセージ)
+    elif outs:
+        out = outs[0]
+        return True, out.read() # 成功したら(True, 抽出文字列)
+    else:
+        return None, "timeout" # タイムアウトしたら(None, "timeout")
+
+class msoffice_to_text:
+    __implements__ = itransform
+    __name__ = TRANSFORM_NAME
+
+    inputs = (
+        # MS-Word formats
+        'application/msword',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'application/vnd.ms-word.document.macroEnabled.12',
+        # MS-Excle formats
+        'application/vnd.ms-excel',
+        'application/msexcel',
+        'application/x-msexcel',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+        'application/vnd.ms-excel.sheet.macroEnabled.12',
+        # MS-PowerPoint formats
+        'application/powerpoint',
+        'application/mspowerpoint',
+        'application/x-mspowerpoint',
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+        )
+
+    output = 'text/plain'
+
+    output_encoding = SITE_CHARSET
+
+    def __init__(self,name=None):
+        if name:
+            self.__name__=name
+        return
+
+    def name(self):
+        return self.__name__
+
+    def convert(self, orig, data, **kwargs):
+        #orig_file = kwargs.get('filename') or 'unknown.xxx'
+        mimetype = kwargs.get('mimetype')
+        filename = kwargs.get('filename') or 'unknown.xxx'
+        if mimetype is None:
+            mimetype = mimetypes.guess_type(filename)[0]
+        try:
+            status, text = msdoc2txt(orig)
+        except Exception, e:
+            logger.error(e, exc_info=True)
+            data.setData('')
+            return data
+        if status:
+            data.setData(text)
+        else:
+            logger.error(text, exc_info=True)
+            data.setData('')
+        return data
+
+def register():
+    return msoffice_to_text()
