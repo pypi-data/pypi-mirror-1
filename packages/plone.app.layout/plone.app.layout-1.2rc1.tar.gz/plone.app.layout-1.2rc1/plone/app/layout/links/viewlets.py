e621a49596d94b11df3d26ecb56abbff047803a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:14 +0000

    Add package plone.app.layout/1.2rc1/plone.app.layout-1.2rc1.tar.gz

---
+++ packages/plone.app.layout/plone.app.layout-1.2rc1.tar.gz/plone.app.layout-1.2rc1/plone/app/layout/links/viewlets.py
@@ -0,0 +1,76 @@
+from StringIO import StringIO
+
+from plone.memoize import ram
+from plone.memoize.compress import xhtml_compress
+
+from zope.component import getMultiAdapter
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+
+from Acquisition import aq_inner
+
+from plone.app.layout.viewlets import ViewletBase
+
+from Products.CMFCore.utils import getToolByName
+
+
+def get_language(context, request):
+    portal_state = getMultiAdapter((context, request),
+                                   name=u'plone_portal_state')
+    return portal_state.locale().getLocaleID()
+
+
+def render_cachekey(fun, self):
+    key = StringIO()
+    # Include the name of the viewlet as the underlying cache key only
+    # takes the module and function name into account, but not the class
+    print >> key, self.__name__
+    print >> key, self.site_url
+    print >> key, get_language(aq_inner(self.context), self.request)
+
+    return key.getvalue()
+
+
+class FaviconViewlet(ViewletBase):
+
+    _template = ViewPageTemplateFile('favicon.pt')
+
+    @ram.cache(render_cachekey)
+    def render(self):
+        return xhtml_compress(self._template())
+
+
+class SearchViewlet(ViewletBase):
+
+    _template = ViewPageTemplateFile('search.pt')
+
+    @ram.cache(render_cachekey)
+    def render(self):
+        return xhtml_compress(self._template())
+
+
+class AuthorViewlet(ViewletBase):
+
+    render = ViewPageTemplateFile('author.pt')
+
+
+class NavigationViewlet(ViewletBase):
+
+    _template = ViewPageTemplateFile('navigation.pt')
+
+    @ram.cache(render_cachekey)
+    def render(self):
+        return xhtml_compress(self._template())
+
+class RSSViewlet(ViewletBase):
+    def update(self):
+        super(RSSViewlet, self).update()
+        syntool = getToolByName(self.context, 'portal_syndication')
+        if syntool.isSyndicationAllowed(self.context):
+            self.allowed = True
+            context_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_context_state')
+            self.url = '%s/RSS' % context_state.object_url()
+        else:
+            self.allowed = False
+
+    render = ViewPageTemplateFile('rsslink.pt')
