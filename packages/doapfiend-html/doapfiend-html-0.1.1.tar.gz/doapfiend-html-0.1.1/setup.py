248d8a569fe45a8204b8dcccf922f78c3972feeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:07 +0000

    Add package doapfiend-html/0.1.1/doapfiend-html-0.1.1.tar.gz

---
+++ packages/doapfiend-html/doapfiend-html-0.1.1.tar.gz/doapfiend-html-0.1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name="doapfiend-html",
+    license="BSD-2",
+    version="0.1.1",
+    description="Doapfiend plugin. Serializes DOAP as HTML/CSS.",
+    maintainer="Rob Cakebread",
+    author="Rob Cakebread",
+    author_email="rob@doapspace.org",
+    url="http://trac.doapspace.org/doapfiend/wiki/DfHtml",
+    keywords="doapfiend.plugin doap rdf doapfiend semantic web",
+    classifiers=["Development Status :: 2 - Pre-Alpha",
+                 "Intended Audience :: Developers",
+                 "Programming Language :: Python",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 ],
+    install_requires = ['lxml>=2.0.2', 'doapfiend>=0.3.0'],
+    py_modules = ['doapfiend_html'],
+    entry_points = {
+        'doapfiend.plugins': [
+        'htmlplugin = doapfiend_html:HtmlPlugin'
+        ]
+    },
+)
+
