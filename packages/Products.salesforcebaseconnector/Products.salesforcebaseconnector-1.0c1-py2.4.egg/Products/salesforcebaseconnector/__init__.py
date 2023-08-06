a98c7428adf34f281f18f8ae3966d8faec2d6a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:31 +0000

    Add package Products.salesforcebaseconnector/1.0c1/Products.salesforcebaseconnector-1.0c1-py2.4.egg

---
+++ packages/Products.salesforcebaseconnector/Products.salesforcebaseconnector-1.0c1-py2.4.egg/Products/salesforcebaseconnector/__init__.py
@@ -0,0 +1,17 @@
+import sys
+from Products.CMFCore import utils as cmf_utils
+
+this_module = sys.modules[ __name__ ]
+ 
+product_globals = globals()
+ 
+import salesforcebaseconnector
+tools = ( salesforcebaseconnector.SalesforceBaseConnector,
+          )
+
+def initialize(context):
+    cmf_utils.ToolInit('Salesforce Base Connector',
+                    tools = tools,
+                    product_name = 'salesforcebaseconnector',
+                    icon='www/salesforce.gif'
+                    ).initialize( context )
