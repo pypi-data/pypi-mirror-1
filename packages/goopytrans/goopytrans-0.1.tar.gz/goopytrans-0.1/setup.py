34e474a7af9c1b92466e26913ae8424407fb3913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:30 +0000

    Add package goopytrans/0.1/goopytrans-0.1.tar.gz

---
+++ packages/goopytrans/goopytrans-0.1.tar.gz/goopytrans-0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name='goopytrans',
+      version='0.1',
+      description='Python Client Library for Google\'s translation API',
+      author='James Dennis',
+      author_email='jd@j2labs.net',
+      url='http://github.com/j2labs/goopytrans',
+      packages=['goopytrans'])
