46dbdefeaf1d263ac38af6f5cb3574b9b14fe1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:55 +0000

    Add package clixevolve/0.2dev/clixevolve-0.2dev.tar.gz

---
+++ packages/clixevolve/clixevolve-0.2dev.tar.gz/clixevolve-0.2dev/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name="clixevolve",
+    version="0.2dev",
+    description="ClixEvolve -- the righteous schema evolver",
+    long_description="""ClixEvolver will evolve any MySQL schema, 'guaranteed'.""",
+    author="Silas Snider",
+    author_email="silas.snider@showclix.com",
+    url="http://silassnider.com",
+    packages=find_packages(exclude='tests'),
+    package_data={'': ['*.dist']},
+    install_requires=['argparse', 'pyyaml'],
+    entry_points={
+        'console_scripts': [
+#            'clixevolve = clixevolve.migrate:main()',
+            'clixextract = clixevolve.extract_structure:main',
+            'clixdiff = clixevolve.diff:main'
+        ]
+    }
+)
