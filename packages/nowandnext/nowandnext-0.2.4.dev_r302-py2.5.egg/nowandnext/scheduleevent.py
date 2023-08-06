14dab7571239d6760fa7153f45849526d1cfacc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:48 +0000

    Add package nowandnext/0.2.4.dev-r302/nowandnext-0.2.4.dev_r302-py2.5.egg

---
+++ packages/nowandnext/nowandnext-0.2.4.dev_r302-py2.5.egg/nowandnext/scheduleevent.py
@@ -0,0 +1,51 @@
+import datetime
+import time
+import logging
+
+log = logging.getLogger( __name__ )
+
+from nowandnext.utc import utc
+from nowandnext.textparser import textparser
+from schedulething import schedulething
+from schedulegap import schedulegap
+
+class scheduleevent( schedulething ):
+    """
+    A thing in the schedule that we might need to notify some other service of
+    """
+    EVENT_TYPE="SHOW"
+    
+    def __init__( self, eventinstance, default_artist="Unknown Artist", default_website="" ):
+        self._eventinstance = eventinstance
+        self._default_artist = default_artist
+        self._createtime = datetime.datetime.now( utc )
+        self._default_website = default_website
+        # log.info("New Event: %s" % eventinstance.getEvent().getTitle() )
+
+    def getStartTime(self):
+        return self._eventinstance.getStart()
+    
+    def getEndTime(self):
+        return self._eventinstance.getEnd()
+
+    def getTitle(self):
+        return self._eventinstance.getEvent().getTitle()
+
+    def ping(self, simplecast ):
+        query = {}
+        now = datetime.datetime.now( utc )
+        # How long till finish?
+        delta_to_end = self.getTimeToEnd( now )
+        seconds_to_end = self.timedeltaToSeconds( delta_to_end )
+
+        description = self._eventinstance.getEvent().getDescription()
+        tp = textparser()
+        descdict = tp.translate( tp.parsetodict( description ) )
+        
+        query['duration'] = seconds_to_end
+        query['title'] = self._eventinstance.getEvent().getTitle()
+        query['artist'] = descdict.get( 'artist', self._default_artist )
+        query['website'] = descdict.get( 'web', self._default_website )
+        query['songtype'] = 'S'
+
+        return simplecast.ping( query )
