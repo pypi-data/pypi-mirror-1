20ffd505e6a193badc656c28e637170ded300c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:59 +0000

    Add package manuel/1.0.0b2/manuel-1.0.0b2.tar.gz

---
+++ packages/manuel/manuel-1.0.0b2.tar.gz/manuel-1.0.0b2/src/manuel/codeblock.py
@@ -0,0 +1,33 @@
+import re
+import manuel
+import textwrap
+
+CODEBLOCK_START = re.compile(r'^\.\.\s*(invisible-)?code-block::?\s*python\b', re.MULTILINE)
+CODEBLOCK_END = re.compile(r'(\n\Z|\n(?=\S))')
+
+
+class CodeBlock(object):
+    def __init__(self, code):
+        self.code = code
+
+
+def find_code_blocks(document):
+    for region in document.find_regions(CODEBLOCK_START, CODEBLOCK_END):
+        source = textwrap.dedent('\n'.join(region.source.splitlines()[1:]))
+        source_location = '%s:%d' % (document.location, region.lineno)
+        code = compile(source, source_location, 'exec', 0, True)
+        document.claim_region(region)
+        region.parsed = CodeBlock(code)
+
+
+def execute_code_block(region, document, globs):
+    if not isinstance(region.parsed, CodeBlock):
+        return
+
+    exec region.parsed.code in globs
+    del globs['__builtins__'] # exec adds __builtins__, we don't want it
+
+
+class Manuel(manuel.Manuel):
+    def __init__(self):
+        manuel.Manuel.__init__(self, [find_code_blocks], [execute_code_block])
