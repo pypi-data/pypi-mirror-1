e7c0508679250f50ebcc67e24039af73231e15d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package scrubber/1.3.4/scrubber-1.3.4.tar.gz

---
+++ packages/scrubber/scrubber-1.3.4.tar.gz/scrubber-1.3.4/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'scrubber',
+    version = '1.3.4',
+    description = 'A whitelisting HTML scrubber',
+    author = 'Samuel Stauffer',
+    author_email = 'samuel@descolada.com',
+    url = 'http://github.com/samuel/python-scrubber/tree/master',
+    packages = ['scrubber'],
+)
