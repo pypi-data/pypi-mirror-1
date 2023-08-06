6ebbf32df93f675f807520ca0507d2fc1237e179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:25 +0000

    Add package django-ratings/0.2.1/django-ratings-0.2.1.tar.gz

---
+++ packages/django-ratings/django-ratings-0.2.1.tar.gz/django-ratings-0.2.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-ratings',
+    version='0.2.1',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    description='Generic Ratings in Django',
+    url='http://github.com/dcramer/django-ratings',
+    install_requires=['django'],
+    packages=find_packages(),
+    include_package_data=True,
+)
