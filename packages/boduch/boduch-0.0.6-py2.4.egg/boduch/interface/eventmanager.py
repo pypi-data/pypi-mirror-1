8e4e007754ec7f2257fc2c1f95143fd7ea025229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:18 +0000

    Add package boduch/0.0.6/boduch-0.0.6-py2.4.egg

---
+++ packages/boduch/boduch-0.0.6-py2.4.egg/boduch/interface/eventmanager.py
@@ -0,0 +1,65 @@
+#boduch - Simple Python tool library.
+#   Copyright (C) 2008  Adam Boduch
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This module defines the IEventManager interface."""
+
+from zope.interface import Interface, Attribute
+
+class IEventManager(Interface):
+    slock=Attribute("""The semaphore lock used for threading.""")
+    subscriptions=Attribute("""Stores all event subscriptions.""")
+    threaded=Attribute("""True is the event manager is in threaded mode.""")
+    
+    def get_slock(cls):
+        """Return the event manager Semaphore lock."""
+        
+    def get_subscriptions(cls, evt=None):
+        """Return the handles for the specified event."""
+        
+    def get_threaded(cls):
+        """Return true if the event manager is running in threaded mode."""
+        
+    def publish(cls, evt):
+        """Publish the specified event."""
+        
+    def subscribe(cls, evt, handle):
+        """Subscribe the specified handle to the specified event."""
+        
+    def unsubscribe(cls, evt, handle):
+        """Remove the subscription of the specified handle to the specified
+        event."""
+        
+    def prioritize(cls, evt):
+        """Sort the list of subscribers to the specified event,
+        making the largest handles first."""
+        
+    def lock(cls):
+        """Acquire the Semaphore lock that is owned by the EventManager
+        class."""
+        
+    def unlock(cls):
+        """Release the Semaphore lock that is owned by the EventManager
+        class."""
+        
+    def build_handlers(cls, evt, params={}):
+        """Build the Handle instances that are currently subcribers to
+        the specified event.  The instance is also passed the specified
+        keyword parameters."""
+        
+    def start_event_thread(cls, handles):
+        """Start the thread of execution for the specified event handlers."""
+    
+__all__=['IEventManager']
