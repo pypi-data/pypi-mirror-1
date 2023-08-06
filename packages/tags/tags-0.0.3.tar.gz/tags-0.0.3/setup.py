94e17bd7d09eef074285c045e44d775b01cbd896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:26 +0000

    Add package tags/0.0.3/tags-0.0.3.tar.gz

---
+++ packages/tags/tags-0.0.3.tar.gz/tags-0.0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(
+    name = "tags",
+    version = "0.0.3",
+    author = "Patrick Sabin",
+    author_email = "patricksabin@gmx.at",
+    url = "http://pypi.python.org/pypi/tags",
+    packages = ["tags"],
+    description = "A toolkit to create HTML code with python",
+    long_description = open('description.rst', 'r').read(),
+    classifiers = [
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup :: HTML" ,
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Development Status :: 2 - Pre-Alpha"
+    ],
+)
