0efa4f7ea1c0600a52947ffc853f2ef7c8a53b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:26 +0000

    Add package manuel/1.0.0a8/manuel-1.0.0a8.tar.gz

---
+++ packages/manuel/manuel-1.0.0a8.tar.gz/manuel-1.0.0a8/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup, find_packages
+
+long_description = (
+    open(os.path.join('src', 'manuel', 'README.txt')).read()
+    #+ '\n\n'
+    #+ open(os.path.join('src', 'manuel', 'table-example.txt')).read()
+    + '\n\n'
+    + open('CHANGES.txt').read()
+    )
+
+setup(
+    name='manuel',
+    version='1.0.0a8',
+    url = 'http://pypi.python.org/pypi/manuel',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    zip_safe=False,
+    author='Benji York',
+    author_email='benji@benjiyork.com',
+    description=
+        'Manuel lets you combine traditional doctests with new test syntax '
+        'that you build yourself or is inlcuded in Manuel.',
+    license='ZPL',
+    install_requires=[
+        'setuptools',
+        'zope.testing',
+        ],
+    include_package_data=True,
+    long_description = long_description,
+    )
