603d9f013700284f88e440041d362206143c6b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:25 +0000

    Add package collective.js.s3slider/1.0/collective.js.s3slider-1.0.zip

---
+++ packages/collective.js.s3slider/collective.js.s3slider-1.0.zip/collective.js.s3slider-1.0/README.txt
@@ -0,0 +1,4 @@
+Introduction
+============
+
+Plone integration of jquery plugin s3Slider: http://www.serie3.info/s3slider/
---
+++ packages/collective.js.s3slider/collective.js.s3slider-1.0.zip/collective.js.s3slider-1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.js.s3slider',
+      version=version,
+      description="The s3Slider jQuery plugin is jd`s smooth slide show implementation with jquery",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='jeanmichel.francois@makina-corpus.com',
+      url='https://svn.plone.org/svn/collective/collective.js.s3slider',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
