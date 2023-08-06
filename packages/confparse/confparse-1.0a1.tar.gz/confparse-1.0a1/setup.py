f09f68c9ff42c4d9b94b7c15728b0d610fc1b445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:48 +0000

    Add package confparse/1.0a1/confparse-1.0a1.tar.gz

---
+++ packages/confparse/confparse-1.0a1.tar.gz/confparse-1.0a1/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='confparse',
+      version='1.0a1',
+      description='Read and write configuration files. Keep comments and layout. Add or delete options.',
+      author='Jean Daniel Browne',
+      url='http://launchpad.net/confparse.py',
+      py_modules=['confparse'],
+      )
