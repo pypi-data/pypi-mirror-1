ed12583e54015b46492085e35194bd324e1ae73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:08 +0000

    Add package plone.i18n/1.0.5/plone.i18n-1.0.5.tar.gz

---
+++ packages/plone.i18n/plone.i18n-1.0.5.tar.gz/plone.i18n-1.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0.5'
+
+setup(name='plone.i18n',
+      version=version,
+      description="Advanced i18n/l10n features",
+      long_description="""\
+""",
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Zope2',
+        'Framework :: Zope3'
+      ],
+      keywords='i18n l10n Plone',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.i18n',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      )
