61d92c80fea19597e9039be6904837745c7d5f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:45 +0000

    Add package experimental.catalogqueryplan/1.1/experimental.catalogqueryplan-1.1.zip

---
+++ packages/experimental.catalogqueryplan/experimental.catalogqueryplan-1.1.zip/experimental.catalogqueryplan-1.1/experimental/catalogqueryplan/__init__.py
@@ -0,0 +1,10 @@
+import setpatches
+setpatches.apply()
+
+import patches
+patches.apply()
+
+# The set patches are only temporary
+# to avoid zc.relationship trying to persist
+# the BTree functions
+setpatches.unapply()
