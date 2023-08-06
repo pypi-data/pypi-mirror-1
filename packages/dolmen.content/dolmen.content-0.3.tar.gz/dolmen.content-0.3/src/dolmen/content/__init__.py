ef7a3e46934304258b15836221b7cee74d42bee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:44 +0000

    Add package dolmen.content/0.3/dolmen.content-0.3.tar.gz

---
+++ packages/dolmen.content/dolmen.content-0.3.tar.gz/dolmen.content-0.3/src/dolmen/content/__init__.py
@@ -0,0 +1,8 @@
+from grokcore.component import name, context
+from grokcore.security import require
+
+from dolmen.content.interfaces import *
+from dolmen.content.directives import schema, factory, nofactory
+from dolmen.content.factoring import Factory
+from dolmen.content.base import BaseContent, Content
+from dolmen.content.base import Container, OrderedContainer
