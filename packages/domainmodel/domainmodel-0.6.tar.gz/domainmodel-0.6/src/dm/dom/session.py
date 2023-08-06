71b396bbe5bba0057b1418ad113692b7afc1b878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:02 +0000

    Add package domainmodel/0.6/domainmodel-0.6.tar.gz

---
+++ packages/domainmodel/domainmodel-0.6.tar.gz/domainmodel-0.6/src/dm/dom/session.py
@@ -0,0 +1,45 @@
+from dm.dom.stateful import *
+from dm.ioc import *
+import md5
+import random
+import sys
+import mx.DateTime
+from dm.dictionarywords import WEBKIT_NAME
+from dm.dictionarywords import DJANGO_SECRET_KEY
+from dm.dictionarywords import PYLONS_SECRET_KEY
+from dm.exceptions import WebkitError
+
+class Session(SimpleDatedObject):
+    "Visitor session."
+
+    registerKeyName = 'key'
+    isUnique        = False
+    key             = String(default='', isRequired=False)
+    person          = HasA('Person', default=None)
+    lastVisited     = DateTime(default=mx.DateTime.now)
+
+    def initialise(self, register):
+        super(Session, self).initialise(register)
+        if not self.key:
+            self.key = self.createKey()
+            self.isChanged = True
+
+    def createKey(self):
+        "Returns new session key."
+        webkitName = self.dictionary[WEBKIT_NAME]
+        if webkitName == 'django':
+            secretKey = self.dictionary[DJANGO_SECRET_KEY] 
+        elif webkitName == 'pylons':
+            secretKey = self.dictionary[PYLONS_SECRET_KEY] 
+        else:
+            secretKey = 'soso' 
+        randomStr = str(random.randint(0, sys.maxint - 1)) + secretKey
+        return md5.new(randomStr).hexdigest()
+
+    def updateLastVisited(self):
+        timeVisited = mx.DateTime.now()
+        # Operator works for mix of datetime.datetime and mx.DateTime.
+        if (timeVisited > self.lastVisited):
+            self.lastVisited = timeVisited
+            self.save()
+
---
+++ packages/domainmodel/domainmodel-0.6.tar.gz/domainmodel-0.6/src/dm/webkit/__init__.py
@@ -0,0 +1,112 @@
+# Todo: Make this switch between Django, Pylons, etc.
+from dm.exceptions import WebkitError
+from dm.dictionarywords import WEBKIT_NAME
+from dm.ioc import RequiredFeature
+
+dictionary = RequiredFeature('SystemDictionary')
+webkitName = dictionary[WEBKIT_NAME]
+
+if not webkitName:
+    pass
+elif webkitName == 'django':
+    import django
+    import django.forms
+    if hasattr(django.forms, 'BaseForm'): # Django 1.0
+        webkitVersion = '1.0'
+        from django.forms.forms import BaseForm
+        class Manipulator(BaseForm): pass
+        from django.forms.util import ValidationError
+        from django.utils.html import escape as htmlescape
+        from django.http import HttpRequest
+        from django.http import HttpResponse
+        from django.http import HttpResponseRedirect
+        from django.template import Context
+        from django.core import template_loader
+        from django.forms import Field
+        from django.forms import EmailField
+        from django.forms import ChoiceField
+        from django.forms import DateTimeField
+        from django.forms import widgets
+        from django.forms import fields
+        from django.utils.datastructures import SortedDict
+    else:                             # Django 0.96
+        webkitVersion = '0.96'
+        from django.utils.html import escape as htmlescape
+        from django.forms import validators
+        from django.forms import Manipulator
+        from django.forms import SelectField, SelectMultipleField, TextField
+        from django.forms import TextField, LargeTextField
+        from django.forms import PasswordField, IntegerField, SmallIntegerField
+        from django.forms import PositiveIntegerField, URLField, CheckboxField
+        from django.forms import DatetimeField, DateField, TimeField
+        from django.forms import FileUploadField, EmailField, RadioSelectField
+        from django.core.validators import ValidationError
+        from django.template import Context
+        from django.core import template_loader
+        from django.http import HttpRequest
+        from django.http import HttpResponse
+        from django.http import HttpResponseRedirect
+
+elif webkitName == 'pylons':
+    import pylons
+    from webhelpers import *
+    from cgi import escape as htmlescape
+    class Manipulator(object): pass
+    SelectField = select
+    SelectMultipleField = None
+    TextField = text_field
+    LargeTextField = text_area
+    PasswordField = text_field
+    IntegerField = text_field
+    class URLField(object): pass
+    class CheckboxField(object): pass
+    class DatetimeField(object): pass
+    class DateField(object): pass
+    class FileUploadField(object): pass
+    EmailField = text_field
+    class ValidationError(object): pass
+    class HttpRequest(object): pass
+    class HttpResponse(object): pass
+    class HttpResponseRedirect(object): pass
+    class Context(object): pass
+    class template_loader(object): pass
+
+else:
+    raise WebkitError, "No support available for '%s' webkit." % webkitName
+
+
+
+
+## Django extensions, without a home. Perhaps push back into ScanBooker?
+#
+
+if webkitName == 'django' and webkitVersion == '0.96':
+
+    import sre
+    _rdatere = r'((?:0?[1-9])|(?:[12][0-9])|(?:3[0-1]))-((?:0?[1-9])|(?:1[0-2]))-(19|2\d)\d{2}'
+    ransi_date_re = sre.compile('^%s$' % _rdatere)
+
+    class RDateField(DateField):
+        """Automatically converts its data to a datetime.date object.
+        The data should be in the format DD-MM-YYYY"""
+    
+        def isValidDate(self, field_data, all_data):
+            try:
+                if not ransi_date_re.search(field_data):
+                    raise ValidationError(
+                        'Enter a valid date in DD-MM-YYYY format.'
+                    )
+            except ValidationError, e:
+                raise validators.CriticalValidationError, e.messages
+    
+        def html2python(data):
+            "Converts the field into a datetime.date object"
+            import time, datetime
+            try:
+                t = time.strptime(data, '%d-%m-%Y')
+                return datetime.date(t[0], t[1], t[2])
+            except (ValueError, TypeError):
+                return None
+                
+        html2python = staticmethod(html2python)
+
