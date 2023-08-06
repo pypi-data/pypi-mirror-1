db1631b6ba082519edcc07fb44b01afb261cc11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:37 +0000

    Add package collective.contentrules.runscript/0.1.1beta/collective.contentrules.runscript-0.1.1beta.zip

---
+++ packages/collective.contentrules.runscript/collective.contentrules.runscript-0.1.1beta.zip/collective.contentrules.runscript-0.1.1beta/collective/contentrules/runscript/actions/interfaces.py
@@ -0,0 +1,19 @@
+from zope.interface import Interface
+from zope import schema
+
+from collective.contentrules.runscript import runscriptMessageFactory as _
+
+class IRunScriptAction(Interface):
+    """Definition of the configuration available for a runscript action"""
+    
+    script = schema.TextLine(
+        title=_(u"Script"),
+        description=_(u"The script"),
+        required=True
+    )
+                              
+    fail_on_script_not_found = schema.Bool(
+        title=_("Fail on script not found"),
+        description=_("Raise exception if script can't be traversed to from object."),
+        required=False
+    )
