d367c69f331444a5f5db70bdcd57d3cc7ee564e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:07 +0000

    Add package ll-orasql/1.26/ll_orasql-1.26-py2.5.egg

---
+++ packages/ll-orasql/ll_orasql-1.26-py2.5.egg/EGG-INFO/scripts/oramerge.py
@@ -0,0 +1,10 @@
+#!/System/Library/Frameworks/Python.framework/Versions/2.5/Resources/Python.app/Contents/MacOS/Python
+# -*- coding: iso-8859-1 -*-
+
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.orasql.scripts import oramerge
+	sys.exit(oramerge.main())
