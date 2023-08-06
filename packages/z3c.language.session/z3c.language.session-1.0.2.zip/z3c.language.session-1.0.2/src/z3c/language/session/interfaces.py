d06d8b9b63d731340da967785df72ac80d89f979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:45 +0000

    Add package z3c.language.session/1.0.2/z3c.language.session-1.0.2.zip

---
+++ packages/z3c.language.session/z3c.language.session-1.0.2.zip/z3c.language.session-1.0.2/src/z3c/language/session/interfaces.py
@@ -0,0 +1,62 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+"""
+$Id: interfaces.py 106089 2009-11-29 03:56:22Z rogerineichen $
+"""
+__docformat__ = 'restructuredtext'
+
+import zope.interface
+from zope.session.interfaces import ISession
+
+sessionPkgDataId = 'z3c.language.session.SessionDataContainer'
+
+
+
+class ILanguageSession(ISession):
+    """Session containing the language for a session."""
+
+
+
+class IHasLanguage(zope.interface.Interface):
+    """Has language API"""
+
+    def hasLanguage():
+        """View for to check if a session has a i18n language value."""
+
+
+
+class IGetLanguage(zope.interface.Interface):
+    """Get language API"""
+
+    def getLanguage():
+        """View for to check if a session has a i18n language value."""
+
+
+
+class ISetLanguage(zope.interface.Interface):
+    """Set language API"""
+
+    def setLanguage():
+        """Set the given language in the request to the session.
+        
+        You can do it via the javascript sessionlanguage.js with the
+        javascript method setLanguage:
+        
+        javascript:setLanguage('@@setLanguage','de')
+        
+        Or send the request to the view '@@setLanguage'. There has to be a 
+        variable 'language' and 'nextURL' in the request like
+        """
+
