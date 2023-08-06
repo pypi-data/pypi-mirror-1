cf63e5fe11b1eb3884990d6b0fe650eda746248e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:40 +0000

    Add package couchdbkit/0.2.2/couchdbkit-0.2.2.tar.gz

---
+++ packages/couchdbkit/couchdbkit-0.2.2.tar.gz/couchdbkit-0.2.2/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2008-2009 Benoit Chesneau <benoitc@e-engura.com> 
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+import os
+import sys
+
+setup(
+    name = 'couchdbkit',
+    version = '0.2.2',
+
+    description = 'Python couchdb kit',
+    long_description = \
+"""CouchDB is document oriented database. Couchdbkit framework try
+to keep its simplicity when you manage it in python""",
+    author = 'Benoit Chesneau',
+    author_email = 'benoitc@e-engura.com',
+    license = 'Apache License 2',
+    url = 'http://hg.e-engura.org/couchdbkit',
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages = find_packages(),
+        
+    zip_safe = False,
+
+    install_requires = [
+        'restkit==0.8.4',
+        'anyjson'
+    ],
+    
+    test_suite='tests',
+
+)
