9bba3d9c4885c44847ab53a22acc3e14ff4e9909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:46 +0000

    Add package wsgiheaders/0.0.1/wsgiheaders-0.0.1.zip

---
+++ packages/wsgiheaders/wsgiheaders-0.0.1.zip/wsgiheaders-0.0.1/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(name='wsgiheaders',
+      version=version,
+      description="Adding and replacing response headers",
+      long_description="""\
+Adding and replacing response headers
+
+ >>> @replaceheader([(r'.*\.html', [('Content-type', 'text/html')])])
+ ... def app(environ, start_response):
+ ...     start_response('200 OK',
+ ...                    [('Content-type', 'text/plain')])
+ ...     return ['Hello, world!']
+ >>> import webtest
+ >>> app = webtest.TestApp(app)
+ >>> res = app.get('/a.txt')
+ >>> res.content_type
+ 'text/plain'
+ >>> res = app.get('/a.html')
+ >>> res.content_type
+ 'text/html'
+ >>> @addheader([('.*', [('X-XRDS', 'http://localhost/services.xrds')])])
+ ... def app(environ, start_response):
+ ...     start_response('200 OK',
+ ...                    [('Content-type', 'text/plain')])
+ ...     return ['Hello, world!']
+ >>> app = webtest.TestApp(app)
+ >>> res = app.get('/')
+ >>> res.headers['X-XRDS']
+ 'http://localhost/services.xrds'
+
+""",
+      classifiers=[
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 2 - Pre-Alpha"
+        ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Atsushi Odagiri',
+      author_email='aodagx@gmail.com',
+      url='http://www.bitbucket.org/aodag/wsgiheaders/',
+      license='MIT License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          "WebOb",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
