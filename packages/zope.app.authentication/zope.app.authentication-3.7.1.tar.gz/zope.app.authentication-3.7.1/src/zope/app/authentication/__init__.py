5e3a02e5187bb7f4565efb288a5458f1b1daea33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:55 +0000

    Add package zope.app.authentication/3.7.1/zope.app.authentication-3.7.1.tar.gz

---
+++ packages/zope.app.authentication/zope.app.authentication-3.7.1.tar.gz/zope.app.authentication-3.7.1/src/zope/app/authentication/__init__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Pluggable Authentication Utility
+
+$Id: __init__.py 108827 2010-02-06 11:16:44Z trollfot $
+"""
+
+import interfaces
+from zope.pluggableauth.authentication import PluggableAuthentication
