758635b36df383f3702eefc564434d02c4c85e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package ZOEmbed/0.1.2/ZOEmbed-0.1.2.tar.gz

---
+++ packages/ZOEmbed/ZOEmbed-0.1.2.tar.gz/ZOEmbed-0.1.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='ZOEmbed',
+    version='0.1.2',
+    description="Zack's OEmbed client library.",
+    author='Zachary Voase',
+    author_email='disturbyte@gmail.com',
+    url='http://github.com/disturbyte/zoembed',
+    packages=['zoembed', 'zoembed.consumer'],
+    package_dir={'zoembed': 'zoembed'},
+    package_data={'zoembed': ['providers.yaml']},
+    include_package_data=True,
+)
