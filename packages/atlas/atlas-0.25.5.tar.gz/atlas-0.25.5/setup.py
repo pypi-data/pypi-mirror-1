3bdd2e5f95965606a5b34c39cdd5d12a47cec405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:42 +0000

    Add package atlas/0.25.5/atlas-0.25.5.tar.gz

---
+++ packages/atlas/atlas-0.25.5.tar.gz/atlas-0.25.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#
+# setup.py
+#
+
+from setuptools import setup, find_packages
+
+
+setup(
+  name = "atlas",
+  version = "0.25.5",
+  packages = find_packages(exclude=["*_test.py"]),
+  scripts = ['atlas'],
+
+  install_requires = ['threadpool>=1.2.4'],
+
+  author = "Mike Parent",
+  description = "Atlas Build Tool - simple, extensible build system.",
+  license = "Apache 2.0",
+  keywords = "build test",
+  url = "http://code.google.com/p/atlas-build-tool",
+  zip_safe=True,
+)
