4aea50c582cdc11835220b7f3402493b6200e0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:01 +0000

    Add package twitsilver/0.1.2dev/twitsilver-0.1.2dev.tar.gz

---
+++ packages/twitsilver/twitsilver-0.1.2dev.tar.gz/twitsilver-0.1.2dev/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from os import path
+
+DEST_DIR = path.join(path.expanduser("~"), 
+    "Desktop/")
+
+version = '0.1.2'
+
+setup(name='twitsilver',
+    version=version,
+    description="A Quicksilver client for twitter",
+    long_description=open("README.txt").read() + "\n" +
+        open(path.join("README.txt")).read(),
+    classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Environment :: MacOS X",
+    "Operating System :: MacOS :: MacOS X",
+    "Intended Audience :: End Users/Desktop",
+    "License :: OSI Approved :: BSD License",
+    "Topic :: Communications",
+    ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='Quicksilver twitter OSX',
+    author='Tom Lazar',
+    author_email='tom@tomster.org',
+    url='http://tomster.org/',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      # -*- Extra requirements: -*-
+      "py_Growl",
+      "twitter",
+    ],
+    data_files=[
+        (DEST_DIR, ['twitsilver/bin/install-twitsilver-action.sh',],),
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [console_scripts]
+    tweet=twitsilver.tweet:main
+    """,
+    )
+
