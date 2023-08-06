912c8b7825d23b53b64f8bb9b11361c1f38cdfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:35 +0000

    Add package amqplib_thrift/0.1/amqplib_thrift-0.1.tar.gz

---
+++ packages/amqplib_thrift/amqplib_thrift-0.1.tar.gz/amqplib_thrift-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='amqplib_thrift',
+    version='0.1',
+    description='Python library to use thrift services (client and servers) over AMQP with pyamqplib',
+    author='Jonathan Stoppani',
+    author_email='jonathan.stoppani@edu.hefr.ch',
+    url='http://github.com/garetjax/pyamqplib-thrift',
+    packages=['amqplib_thrift'],
+    license='MIT',
+    requires=['amqplib'],
+    provides=('amqplib_trift',),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    )
+)
