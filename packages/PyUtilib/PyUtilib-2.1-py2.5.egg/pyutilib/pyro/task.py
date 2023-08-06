d9528338d8a629bb92e80470f23665533d4a869a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:03 +0000

    Add package PyUtilib/2.1/PyUtilib-2.1-py2.5.egg

---
+++ packages/PyUtilib/PyUtilib-2.1-py2.5.egg/pyutilib/pyro/task.py
@@ -0,0 +1,22 @@
+#  _________________________________________________________________________
+#
+#  PyUtilib: A Python utility library.
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  For more information, see the PyUtilib README.txt file.
+#  _________________________________________________________________________
+
+class Task(object):
+
+    def __init__(self, id=None, data=None):
+        self.id=id
+        self.data=data
+        self.result=None
+        self.processedBy=None
+        self.type=None
+
+    def __str__(self):
+        return "<Task id=%s>" % str(self.id)
+
