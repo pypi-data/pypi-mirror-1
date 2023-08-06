dac691beba813938c2eba4d83e1ab6f36090e31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:25 +0000

    Add package zope.app.form/4.0/zope.app.form-4.0.tar.gz

---
+++ packages/zope.app.form/zope.app.form-4.0.tar.gz/zope.app.form-4.0/src/zope/app/form/browser/widget.py
@@ -0,0 +1,30 @@
+##############################################################################
+#
+# Copyright (c) 2001-2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Browser Widget Definitions
+
+$Id: widget.py 107385 2009-12-30 20:25:24Z faassen $
+"""
+__docformat__ = 'restructuredtext'
+
+# BBB
+from zope.formlib.widget import (
+    quoteattr,
+    BrowserWidget,
+    SimpleInputWidget,
+    DisplayWidget,
+    UnicodeDisplayWidget,
+    renderTag,
+    renderElement,
+    setUp,
+    tearDown)
