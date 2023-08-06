4997a1576f7d31090410595f49aa71f1e618be18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:26 +0000

    Add package HypnoAPNSWrapper/0.3-2/HypnoAPNSWrapper-0.3-2.tar.gz

---
+++ packages/HypnoAPNSWrapper/HypnoAPNSWrapper-0.3-2.tar.gz/HypnoAPNSWrapper-0.3-2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+setup(
+    name = "HypnoAPNSWrapper",
+    version = "0.3-2",
+    packages = find_packages('.'),
+    classifiers = ["Intended Audience :: Customer Service", "Topic :: Internet" ],
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['docutils>=0.3'],
+
+    package_data = {
+        '': ['*.dat'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Max Klymyshyn, Sonettic, Jan Huelsbergen",
+    author_email = "klymyshyn@gmail.com",
+    description = "This is wrapper for Apple Push Notification Service.",
+    license = "ALv2",
+    keywords = "apns push notification service wrapper apple",
+    url = "http://code.google.com/p/apns-python-wrapper/",
+	long_description = """
+		The Wrapper support for Alerts, Badges, Sounds and Custom arguments.		
+		Feedback Service wrapper support for iterations through feedback tuples.
+
+        This version has been modified to be easily installable with python 2.6.
+	
+	"""
+)
