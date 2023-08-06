f3ade10287c29c3bd61236ad160f42f51442de79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:45 +0000

    Add package sprox/0.6.6/sprox-0.6.6-py2.5.egg

---
+++ packages/sprox/sprox-0.6.6-py2.5.egg/sprox/widgets/static/dojo_grid.js
@@ -0,0 +1,11 @@
+//alert('here');
+dojo.require("dojo.parser");
+dojo.require("dojox.data.QueryReadStore");
+dojo.require("dojox.grid.DataGrid");
+dojo.require("dojo._base.xhr");
+dojo.__XhrArgs = {preventCache:true}
+//dojo.addOnLoad(
+//    function(){
+//        dojo.parser.parse();
+//    });
+
