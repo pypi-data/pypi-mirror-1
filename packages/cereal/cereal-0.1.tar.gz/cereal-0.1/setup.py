e54086a159e396fa30328efe0b0344f072e95ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:13 +0000

    Add package cereal/0.1/cereal-0.1.tar.gz

---
+++ packages/cereal/cereal-0.1.tar.gz/cereal-0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name = 'cereal',
+    version = '0.1',
+    description = 'Simple class-based DSL for serialization.',
+    url = 'http://code.zacharyvoase.com/cereal/',
+    license = 'MIT',
+    
+    author = 'Zachary Voase',
+    author_email = 'zacharyvoase@me.com',
+    
+    py_modules = ['cereal'],
+)
