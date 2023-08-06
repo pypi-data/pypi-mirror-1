4ee2a0375556c3456353f06dc70b889296d9e18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:04 +0000

    Add package bda.cache/1.1.2/bda.cache-1.1.2.tar.gz

---
+++ packages/bda.cache/bda.cache-1.1.2.tar.gz/bda.cache-1.1.2/src/bda/cache/__init__.py
@@ -0,0 +1,23 @@
+#
+# Copyright 2009, Blue Dynamics Alliance, Austria - http://bluedynamics.com
+#
+# GNU General Public Licence Version 2 or later
+
+__author__ = """Robert Niederreiter <rnix@squarewave.at>"""
+__docformat__ = 'plaintext'
+
+from interfaces import ICacheVary
+
+from interfaces import ICacheProvider
+from interfaces import ICacheManager
+
+from nullcache import NullCache
+from nullcache import NullCacheManager
+
+from fscache import FSCache
+from fscache import FSCacheManager
+from fscache import FSCacheException
+
+from memcached import Memcached
+from memcached import MemcachedManager
+from memcached import MemcachedException
---
+++ packages/bda.cache/bda.cache-1.1.2.tar.gz/bda.cache-1.1.2/src/bda/cache/tests/test_cache.py
@@ -0,0 +1,45 @@
+__author__ = """Robert Niederreiter <rnix@squarewave.at>"""
+__docformat__ = 'plaintext'
+
+import os
+import unittest
+import interlude
+import zope.app.component
+from pprint import pprint
+from zope.testing import doctest
+from zope.app.testing.placelesssetup import setUp, tearDown
+from zope.configuration.xmlconfig import XMLConfig
+import logging
+logger = logging.getLogger('bda.cache')
+
+optionflags = doctest.NORMALIZE_WHITESPACE | \
+              doctest.ELLIPSIS | \
+              doctest.REPORT_ONLY_FIRST_FAILURE
+
+TESTFILES = [
+    '../nullcache.txt',
+    '../fscache.txt',
+    '../cachemanager.txt',
+]
+
+if os.environ.get('MEMCACHEDBIN', None):
+    TESTFILES += ['../memcached.txt',]
+else:
+    logger.info('Can not test memcached module. No path to memcached binary '
+                'given in test environment (MEMCACHEDBIN).')
+
+def test_suite():
+    setUp()
+    XMLConfig('meta.zcml', zope.app.component)()
+    return unittest.TestSuite([
+        doctest.DocFileSuite(
+            file, 
+            optionflags=optionflags,
+            globs={'interact': interlude.interact,
+                   'pprint': pprint},
+        ) for file in TESTFILES
+    ])
+    tearDown()
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite') 
