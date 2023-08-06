1d52f91521cddff5a03c85a3f35db8a974a6c8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:16 +0000

    Add package pudb/0.91.5/pudb-0.91.5.tar.gz

---
+++ packages/pudb/pudb-0.91.5.tar.gz/pudb-0.91.5/pudb/__init__.py
@@ -0,0 +1,38 @@
+VERSION = "0.91.5"
+
+
+
+
+CURRENT_DEBUGGER = [None]
+def set_trace():
+    if CURRENT_DEBUGGER[0] is None:
+        from pudb.debugger import Debugger
+        dbg = Debugger()
+        CURRENT_DEBUGGER[0] = dbg
+
+        import sys
+        dbg.set_trace(sys._getframe().f_back)
+
+
+
+
+def post_mortem(t):
+    p = Debugger()
+    p.reset()
+    while t.tb_next is not None:
+        t = t.tb_next
+    p.interaction(t.tb_frame, t)
+
+
+
+
+def pm():
+    import sys
+    post_mortem(sys.last_traceback)
+
+
+
+
+if __name__ == "__main__":
+    print "To keep Python 2.6 happy, you now need to type 'python -m pudb.run'."
+    print "Sorry for the inconvenience."
