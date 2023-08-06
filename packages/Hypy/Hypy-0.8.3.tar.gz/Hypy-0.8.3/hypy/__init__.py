9eae5acadfb1356cb3eee682ee2426a74b6369b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package Hypy/0.8.3/Hypy-0.8.3.tar.gz

---
+++ packages/Hypy/Hypy-0.8.3.tar.gz/Hypy-0.8.3/hypy/__init__.py
@@ -0,0 +1,12 @@
+"""Hypy - Pythonic Hyper Estraier"""
+from .lib import (PutFailed,
+    OpenFailed,
+    CloseFailed,
+    EditFailed,
+    FlushFailed,
+    HCondition,
+    HDatabase,
+    HResults,
+    HDocument,
+    HHit,
+    )
