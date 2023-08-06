49e398962a1af4def7612255501d6f82e38962e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:42 +0000

    Add package bleach/0.2.2/bleach-0.2.2.tar.gz

---
+++ packages/bleach/bleach-0.2.2.tar.gz/bleach-0.2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='bleach',
+    version='0.2.2',
+    description='An easy whitelist-based HTML-sanitizing tool.',
+    long_description=open('README.rst').read(),
+    author='James Socol',
+    author_email='james@mozilla.com',
+    url='http://github.com/jsocol/bleach',
+    license='BSD',
+    packages=['bleach'],
+    include_package_data=True,
+    package_data = { '': ['README.rst'] },
+    zip_safe=False,
+    install_requires=['html5lib', 'django'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: Mozilla',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
