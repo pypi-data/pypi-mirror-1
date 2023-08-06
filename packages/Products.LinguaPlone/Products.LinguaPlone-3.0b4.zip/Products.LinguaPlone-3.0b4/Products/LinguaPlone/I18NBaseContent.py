5858912df5b214d9081b496d52d978ac7bdf4854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:11 +0000

    Add package Products.LinguaPlone/3.0b4/Products.LinguaPlone-3.0b4.zip

---
+++ packages/Products.LinguaPlone/Products.LinguaPlone-3.0b4.zip/Products.LinguaPlone-3.0b4/Products/LinguaPlone/I18NBaseContent.py
@@ -0,0 +1,15 @@
+from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.atapi import BaseContent
+from Products.LinguaPlone.I18NBaseObject import I18NBaseObject
+
+class I18NBaseContent(I18NBaseObject, BaseContent):
+    """Overrides BaseContent for *i18n* content."""
+
+    security = ClassSecurityInfo()
+
+    def manage_beforeDelete(self, item, container):
+        I18NBaseObject.manage_beforeDelete(self, item, container)
+        BaseContent.manage_beforeDelete(self, item, container)
+
+InitializeClass(I18NBaseContent)
---
+++ packages/Products.LinguaPlone/Products.LinguaPlone-3.0b4.zip/Products.LinguaPlone-3.0b4/Products/LinguaPlone/I18NBaseFolder.py
@@ -0,0 +1,27 @@
+from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
+from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.atapi import BaseFolder
+from Products.LinguaPlone.I18NBaseObject import I18NBaseObject
+
+class I18NBaseFolder(I18NBaseObject, BaseFolder):
+    """ Base class for translatable objects """
+
+    security = ClassSecurityInfo()
+
+    def __nonzero__(self):
+        return 1
+
+    def manage_beforeDelete(self, item, container):
+        I18NBaseObject.manage_beforeDelete(self, item, container)
+        BaseFolder.manage_beforeDelete(self, item, container)
+
+    def __browser_default__(self, request):
+        """Set default so we can return whatever we want instead of index_html.
+
+        Make sure we use the I18N aware one.
+        """
+        return getToolByName(self, 'plone_utils').browserDefault(self)
+
+
+InitializeClass(I18NBaseFolder)
