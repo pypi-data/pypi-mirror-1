647628cc394e2e3c7597e51f4daee6c83378b532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:35 +0000

    Add package texttree/1.0.1/texttree-1.0.1.zip

---
+++ packages/texttree/texttree-1.0.1.zip/texttree-1.0.1/setup.py
@@ -0,0 +1,11 @@
+import texttree
+
+from distutils.core import setup
+setup(name='texttree',
+    version='1.0.1',
+    description=texttree.__doc__.splitlines()[0],
+    author='Tom Wright',
+    author_email="tat.wright@googlemail.com",
+    url="http://tat.wright.name/texttree/",
+    classifiers=["License :: OSI Approved :: BSD License"],
+    py_modules=['texttree'])
