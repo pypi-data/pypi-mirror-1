7ba8c70d839b471c85304603b0e5402ffdb91a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:03 +0000

    Add package pydap.handlers.netcdf/0.4.3/pydap.handlers.netcdf-0.4.3.tar.gz

---
+++ packages/pydap.handlers.netcdf/pydap.handlers.netcdf-0.4.3.tar.gz/pydap.handlers.netcdf-0.4.3/pavement.py
@@ -0,0 +1,71 @@
+import string
+
+from paver.runtime import *
+from paver.setuputils import *
+import paver.doctools
+
+__version__ = (0,4,3)
+
+options(
+    setup=Bunch(
+        name='pydap.handlers.netcdf',
+        version='.'.join(str(d) for d in __version__),
+        description='NetCDF handler for Pydap',
+        long_description='''
+Pydap is an implementation of the Opendap/DODS protocol, written from
+scratch. This handler enables Pydap to serve NetCDF files on the
+network for Opendap clients.
+        ''',
+        keywords='netcdf opendap dods dap data science climate oceanography meteorology',
+        classifiers=filter(None, map(string.strip, '''
+            Development Status :: 5 - Production/Stable
+            Environment :: Console
+            Environment :: Web Environment
+            Framework :: Paste
+            Intended Audience :: Developers
+            Intended Audience :: Science/Research
+            License :: OSI Approved :: MIT License
+            Operating System :: OS Independent
+            Programming Language :: Python
+            Topic :: Internet
+            Topic :: Internet :: WWW/HTTP :: WSGI
+            Topic :: Scientific/Engineering
+            Topic :: Software Development :: Libraries :: Python Modules
+        '''.split('\n'))),
+        author='Roberto De Almeida',
+        author_email='rob@pydap.org',
+        url='http://pydap.org/3.0/handlers.html#netcdf',
+        license='MIT',
+
+        packages=find_packages(),
+        package_data=find_package_data("pydap", package="pydap",
+                only_in_packages=False),
+        include_package_data=True,
+        zip_safe=False,
+        namespace_packages=['pydap', 'pydap.handlers'],
+
+        test_suite='nose.collector',
+
+        dependency_links=[],
+        install_requires=[
+            'Pydap',
+            'pupynere>=1.0.8',
+            'arrayterator>=1.0',
+        ],
+        entry_points="""
+            [pydap.handler]
+            netcdf = pydap.handlers.netcdf:Handler
+        """,
+    ),
+    minilib=Bunch(
+        extra_files=['doctools', 'virtual']
+    ),
+)
+
+
+@task
+@needs(['generate_setup', 'minilib', 'setuptools.command.sdist'])
+def sdist():
+    """Overrides sdist to make sure that our setup.py is generated."""
+    pass
+
