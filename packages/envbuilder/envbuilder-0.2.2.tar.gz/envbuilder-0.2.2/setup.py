996d1e19e103c64891e48525c6d13074b73bf681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:40 +0000

    Add package envbuilder/0.2.2/envbuilder-0.2.2.tar.gz

---
+++ packages/envbuilder/envbuilder-0.2.2.tar.gz/envbuilder-0.2.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+GITHUB_ALERT = """**NOTE**:  These are the docs for the version of envbuilder in git.  For
+documentation on the last release, see the `pypi_page <http://pypi.python.org/pypi/envbuilder/>`_."""
+readme = open('README.rst', 'r')
+
+unsplit_readme_text = readme.read()
+split_text = [x for x in unsplit_readme_text.split('.. split here')
+              if x]
+README_TEXT = split_text[-1]
+readme.close()
+
+setup(
+    name='envbuilder',
+    author='Jason Baker',
+    author_email='amnorvend@gmail.com',
+    version='0.2.2',
+    packages=find_packages(),
+    setup_requires=['nose'],
+    install_requires=['ConfigObj>=4.7.2', 'argparse', 'pip', 'virtualenv'],
+    zip_safe=False,
+    include_package_data=True,
+    entry_points = {
+        'console_scripts' : [
+            'envb = envbuilder.run:main',
+            'envbuilder = envbuilder.run:main'            
+            ]
+        },
+    description = "A package for automatic generation of virtualenvs",
+    long_description = README_TEXT,
+    url='http://github.com/jasonbaker/envbuilder',
+    )
