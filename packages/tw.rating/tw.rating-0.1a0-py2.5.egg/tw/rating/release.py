59ea94dae051ae6c47f1bcbc5038f7bb015d5f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:05 +0000

    Add package tw.rating/0.1a0/tw.rating-0.1a0-py2.5.egg

---
+++ packages/tw.rating/tw.rating-0.1a0-py2.5.egg/tw/rating/release.py
@@ -0,0 +1,11 @@
+# Metadata for the widget EGG. Should leave it for the (unimplemented) widget
+# tracker to track it
+
+__PROJECT__ = 'tw.rating'
+__DESCRIPTION__ = 'Star Rater plugin'
+__URL__ = 'http://twtools.googlecode.com'
+__VERSION__ = '0.1a0'
+__AUTHOR__ = "Alberto Valverde, Sean O'Donnell, Christopher Perkins"
+__EMAIL__ = 'chris@percious.com'
+__COPYRIGHT__ = "Copyright 2008 Alberto Valverde, Sean O'Donnell, Christopher Perins"
+__LICENSE__ = 'MIT'
