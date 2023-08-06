b1b4eeb5fb90a683eefd42cef7ac98e86a7fa086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:26 +0000

    Add package py-epoll/1.2/py-epoll-1.2.zip

---
+++ packages/py-epoll/py-epoll-1.2.zip/py-epoll-1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.core import Extension
+
+module1 = Extension('epoll',
+                    sources = ['epollmodule.c'])
+
+setup (name = 'py-epoll',
+       version = '1.2',
+       description = 'A Python interface to epoll(4)',
+       author='Ben Woolley', author_email='user ben at host tautology.org',
+       ext_modules = [module1])
