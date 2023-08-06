90177b9ea20f1fef551674dddd69409471bc2625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:44 +0000

    Add package collective.remove.rules/0.1/collective.remove.rules-0.1.zip

---
+++ packages/collective.remove.rules/collective.remove.rules-0.1.zip/collective.remove.rules-0.1/README.txt
@@ -0,0 +1,5 @@
+Introduction
+============
+
+Remove 'rules' tab action.
+
---
+++ packages/collective.remove.rules/collective.remove.rules-0.1.zip/collective.remove.rules-0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.remove.rules',
+      version=version,
+      description="Remove rules system from plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS',
+      author_email='jeanmichel.francois@makina-corpus.org',
+      url='http://svn.plone.org/svn/collective/collective.remove/rules/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.remove'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
