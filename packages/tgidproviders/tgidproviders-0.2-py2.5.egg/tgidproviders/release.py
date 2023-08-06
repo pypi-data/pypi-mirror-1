e1eb81575a8dd2ea0a24f910ea39a5e94bae2748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:43 +0000

    Add package tgidproviders/0.2/tgidproviders-0.2-py2.5.egg

---
+++ packages/tgidproviders/tgidproviders-0.2-py2.5.egg/tgidproviders/release.py
@@ -0,0 +1,17 @@
+# Release information about tgidproviders
+version = "0.2"
+
+description = "A collection of Identity providers for Turbogears"
+long_description = """TGIDProviders is a collection of Identity providers that help you get up and running quickly when you want to validate passwords against an LDAP directory, a UNIX password file, or a SMB provider under Windows.  It takes advantage of TurboGears' entry points (provided by SetupTools) to extend the capabilities of the default identity providers to support other authentication methods.
+
+OpenID is not yet supported, but patches are welcome!
+"""
+author = "Ken Kuhlman and contributors"
+email = "tgidproviders@redlagoon.net"
+copyright = "Copyright 2007 - Ken Kuhlman, et al"
+
+# if it's open source, you might want to specify these
+url = "http://code.google.com/p/tgidproviders"
+download_url = "http://code.google.com/p/tgidproviders/downloads/list"
+license = "MIT"
+
