aec17745b13f6d979c9cdf9ed0fc83571ea7a644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:15 +0000

    Add package pyunv/0.2.1/pyunv-0.2.1.tar.gz

---
+++ packages/pyunv/pyunv-0.2.1.tar.gz/pyunv-0.2.1/pyunv/tests/test_reader.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+test_reader.py
+
+Created by David Peckham on 2009-09-20.
+Copyright (c) 2009 David Peckham. All rights reserved.
+"""
+
+import datetime
+import os
+import sys
+import unittest
+
+from pyunv.universe import Universe
+from pyunv.reader import Reader
+from pyunv.manifest import Manifest
+
+
+class ReaderTests(unittest.TestCase):
+    def setUp(self):
+        super(ReaderTests, self).setUp()
+    
+    def tearDown(self):
+        super(ReaderTests, self).tearDown()
+            
+    def test_date_from_dateindex1(self):
+        date = datetime.date(1976,7,4)
+        self.assertEqual(Reader.date_from_dateindex(2442964), date)
+        
+    def test_date_from_dateindex2(self):
+        date = datetime.date(1976,7,5)
+        self.assertEqual(Reader.date_from_dateindex(2442965), date)
+        
+    def test_date_from_dateindex3(self):
+        date = datetime.date(2009,9,15)
+        self.assertEqual(Reader.date_from_dateindex(2455090), date)
+        
+
+class SampleUniverseXIR2(unittest.TestCase):
+    def setUp(self):
+        super(SampleUniverseXIR2, self).setUp()
+        self.filename = 'pyunv/tests/universes/universe_xir2.unv'
+        self.reader = Reader(open(self.filename, 'rb'))
+    
+    def tearDown(self):
+        super(SampleUniverseXIR2, self).tearDown()
+        del self.reader
+            
+    def test_manifest(self):
+        universe = self.reader.universe
+        f = open(self.filename+'.txt', 'w')
+        Manifest().save(f, universe)
+        f.close()
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ packages/pyunv/pyunv-0.2.1.tar.gz/pyunv-0.2.1/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+# python setup.py register sdist upload
+
+
+SUMMARY = """
+PyUnv reads SAP BusinessObjects universe (.unv) files. PyUnv can extract 
+universe settings, classes, objects, conditions, and source tables and columns
+from the universe file.
+
+PyUnv requires Mako to produce manifests. I include a sample template for a
+text manifest. If you come up with your own HTML, PDF, or RST manifests,
+let me know.
+"""
+
+CLASSIFIERS = """
+Development Status :: 2 - Pre-Alpha
+Environment :: Other Environment
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+
+setup(
+    name = 'pyunv',
+    version = '0.2.1',
+    
+    author='David Peckham',
+    author_email = 'dave.peckham@me.com',
+    classifiers = filter(None, CLASSIFIERS.split("\n")),
+    description = 'Python parser for SAP BusinessObjects universe (*.unv) files',
+    download_url = 'http://code.google.com/p/pyunv/downloads/list',
+    include_package_data = True,
+    install_requires = ['Mako'],
+    keywords = ['encoding', 'BusinessObjects', 'SAP', 'universe', 'unv'],
+    license = 'LGPL',
+    # long_description = SUMMARY.strip() + '\n\n' + file('CHANGES.txt').read(),
+    long_description = SUMMARY.strip(),
+    #packages = find_packages(exclude=['pyunv.tests']),
+    packages = find_packages(),
+    platforms = ['Many'],
+    provides = ['pyunv'],
+    test_suite = 'pyunv.tests',
+    url = 'http://code.google.com/p/pyunv/',
+    zip_safe = False,
+    
+    # package_data = {
+    #     # If any package contains *.txt or *.rst files, include them:
+    #     '': ['*.txt', '*.rst', '*.mako'],
+    #     # And include any *.unv files found in the 'tests' package:
+    #     'tests': ['*.unv'],
+    # },
+    
+    )
