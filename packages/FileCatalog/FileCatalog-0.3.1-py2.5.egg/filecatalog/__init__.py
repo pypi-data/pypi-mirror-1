56b7a5e762a54474bc3e2733327108379f2af704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:56 +0000

    Add package FileCatalog/0.3.1/FileCatalog-0.3.1-py2.5.egg

---
+++ packages/FileCatalog/FileCatalog-0.3.1-py2.5.egg/filecatalog/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+File Catalog
+~~~~~~~~~~~~
+
+This package contains tools to create, view and transform hierachical
+YAML_ representations (using PyYAML_) of directories and files below
+a specified path:
+
+- The **Indexer** is a command line application to traverse and index
+  a directory structure and store the result as YAML.
+
+- The **Viewer** is a graphical user interface that visualizes such a
+  document as a tree.  It makes use of the wxPython_ toolkit.
+
+- The **XHTMLizer** transforms tree data into XHTML representation.
+
+.. _YAML:       http://www.yaml.org/
+.. _wxPython:   http://www.wxpython.org/
+.. _PyYAML:     http://www.pyyaml.org/
+
+:copyright: 2006-2008 Jochen Kupperschmidt
+:license: GPLv2 `<http://www.opensource.org/licenses/gpl-license.php>`_
+"""
+
+__author__ = 'Jochen Kupperschmidt <homework at nwsnet dot de>'
+__date__ = '2008-06-18'
+__docformat__ = 'restructuredtext'
+__license__ = 'GPL <http://www.opensource.org/licenses/gpl-license.php>'
+__url__ = 'http://homework.nwsnet.de/'
+__version__ = '0.3.1'
