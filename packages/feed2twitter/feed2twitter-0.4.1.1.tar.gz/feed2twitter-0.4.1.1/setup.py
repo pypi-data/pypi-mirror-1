74e4fc766b751729c1af5369b10510f134c0eee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:37 +0000

    Add package feed2twitter/0.4.1.1/feed2twitter-0.4.1.1.tar.gz

---
+++ packages/feed2twitter/feed2twitter-0.4.1.1.tar.gz/feed2twitter-0.4.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pkg_resources import resource_string
+VERSION = resource_string(__name__, 'VERSION').strip()
+
+setup(name='feed2twitter',
+      version=VERSION,
+      description="Publish your feed items to twitter",
+      long_description=open('README').read(),
+      classifiers=[
+      "Intended Audience :: Developers",
+      "Programming Language :: Python",
+      "Environment :: Console",
+      "Development Status :: 3 - Alpha"
+      ],
+      keywords='twitter feed',
+      author='Walter Cruz',
+      author_email='walter@waltercruz.com',
+      url='http://www.assembla.com/spaces/feed2twitter',
+      packages=find_packages(),
+      package_data = {'feed2twitter':['docs/*']},
+      include_package_data=True,
+      license='AGPL',
+      zip_safe=False,
+      scripts=['scripts/feed2twitter'],
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
