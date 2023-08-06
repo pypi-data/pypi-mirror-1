2e58f0679160745a7cf0c9399f38ff08714715a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:32 +0000

    Add package pyspacewar/0.9.7/pyspacewar-0.9.7.zip

---
+++ packages/pyspacewar/pyspacewar-0.9.7.zip/pyspacewar-0.9.7/src/pyspacewar/version.py
@@ -0,0 +1,23 @@
+"""
+PySpaceWar version number tracker.
+"""
+
+version = "0.9.7"
+
+
+def get_bzr_revision(unknown='', format='+r%s'):
+    """Return the latest revision number of the files in the package."""
+    import os, subprocess, pyspacewar
+    package_root = os.path.dirname(__file__)
+    try:
+        p = subprocess.Popen(['bzr', 'revno', package_root],
+                             stdout=subprocess.PIPE)
+    except OSError:
+        return unknown
+    else:
+        return format % p.communicate()[0].strip()
+
+
+if version.endswith('dev'):
+    version += get_bzr_revision()
+
