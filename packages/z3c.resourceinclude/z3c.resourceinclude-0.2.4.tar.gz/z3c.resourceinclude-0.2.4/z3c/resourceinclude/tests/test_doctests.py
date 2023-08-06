13633f49dc11c3448ceba5890c4221370efae4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:02 +0000

    Add package z3c.resourceinclude/0.2.4/z3c.resourceinclude-0.2.4.tar.gz

---
+++ packages/z3c.resourceinclude/z3c.resourceinclude-0.2.4.tar.gz/z3c.resourceinclude-0.2.4/z3c/resourceinclude/tests/test_doctests.py
@@ -0,0 +1,30 @@
+import zope.testing
+import unittest
+
+OPTIONFLAGS = (zope.testing.doctest.ELLIPSIS |
+               zope.testing.doctest.NORMALIZE_WHITESPACE)
+
+import zope.component
+import zope.component.testing
+
+import z3c.resourceinclude.testing
+
+def setUp(suite):
+    zope.component.testing.setUp(suite)
+    z3c.resourceinclude.testing.setSite()
+    zope.component.provideAdapter(z3c.resourceinclude.testing.MockSiteURL)
+
+def test_suite():
+    doctests = ('README.txt', 'zcml.txt')
+
+    return unittest.TestSuite(
+        [zope.testing.doctest.DocFileSuite(
+                doctest,
+                optionflags=OPTIONFLAGS,
+                setUp=setUp,
+                tearDown=zope.component.testing.tearDown,
+                package="z3c.resourceinclude") for doctest in doctests]
+        )
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
