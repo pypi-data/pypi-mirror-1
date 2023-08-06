9eb473107d5adaa99baeaaf07c5718041ed13dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:20 +0000

    Add package silva.pas.base/1.5.1/silva.pas.base-1.5.1.tar.gz

---
+++ packages/silva.pas.base/silva.pas.base-1.5.1.tar.gz/silva.pas.base-1.5.1/setup.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: setup.py 34193 2009-03-20 09:09:01Z sylvain $
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.5.1'
+
+setup(name='silva.pas.base',
+      version=version,
+      description="Base PluggableAuthService support for Silva",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        ],
+      keywords='pas silva',
+      author='Sylvain Viollon',
+      author_email='info@infrae.com',
+      url='https://svn.infrae.com/silva.pas.base/trunk',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup',]),
+      namespace_packages=['silva', 'silva.pas'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=["Products.PluggableAuthService >= 1.5.0",
+                        "Products.GenericSetup >= 1.3.0, < 1.3.999",
+                        "setuptools"],
+      )
+
+
---
+++ packages/silva.pas.base/silva.pas.base-1.5.1.tar.gz/silva.pas.base-1.5.1/silva/pas/base/plugins/cookie.py
@@ -0,0 +1,133 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: cookie.py 34193 2009-03-20 09:09:01Z sylvain $
+
+from Products.PluggableAuthService.PluggableAuthService import \
+    _SWALLOWABLE_PLUGIN_EXCEPTIONS
+from Products.PluggableAuthService.interfaces.plugins import \
+    IAuthenticationPlugin
+from Products.PluggableAuthService.plugins.CookieAuthHelper import \
+    CookieAuthHelper
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from Products.Silva import mangle
+from AccessControl.SecurityInfo import ClassSecurityInfo
+from Globals import InitializeClass
+
+def manage_addSilvaCookieAuthHelper(self, id, title='',
+                                    REQUEST=None, **kw):
+    """Create an instance of an Silva cookie auth helper.
+    """
+
+    o = SilvaCookieAuthHelper(id, title, **kw)
+    self._setObject(o.getId(), o)
+
+    if REQUEST is not None:
+        REQUEST["RESPONSE"].redirect("%s/manage_workspace"
+                "?manage_tabs_message=Silva+cookie+auth+helper+plugin+added." %
+                self.absolute_url())
+
+
+
+manage_addSilvaCookieAuthHelperForm =  PageTemplateFile("../www/cookieAddForm",
+                globals(), __name__="manage_addSilvaCookieHelperForm")
+
+
+class SilvaCookieAuthHelper(CookieAuthHelper):
+
+    meta_type = 'Silva Cookie Auth Helper'
+    security = ClassSecurityInfo()
+
+    # Customize configuration
+    cookie_name='__ac_silva'
+    login_path = 'silva_login_form.html'
+
+
+    security.declarePrivate('manage_afterAdd')
+    def manage_afterAdd(self, item, container):
+        """ Setup tasks upon instantiation """
+        pass                    # Do nothing
+
+
+    def unauthorized(self, login_status=None):
+        req = self.REQUEST
+        resp = req['RESPONSE']
+
+        # If we set the auth cookie before, delete it now.
+        if resp.cookies.has_key(self.cookie_name):
+            del resp.cookies[self.cookie_name]
+
+        # Redirect if desired.
+        url = self.getLoginURL()
+        if url is not None:
+            came_from = req.get('came_from', None)
+
+            if came_from is None:
+                came_from = req.get('URL', '')
+                query = req.form.copy()
+                if query:
+                    for bad in ['login_status', '-C']:
+                        if bad in query:
+                            del query[bad]
+                    if query:
+                        keys, values = zip(*query.items())
+                        query = dict(zip(keys, map(lambda v: v.encode('ascii', 'xmlcharrefreplace'), values)))
+                        came_from = mangle.urlencode(came_from, **query)
+            else:
+                req_url = req.get('URL', '')
+
+                if req_url and req_url == url:
+                    return 0
+
+            options = {}
+            options['came_from'] = came_from
+            if login_status is None:
+                login_status = req.form.get('login_status', None)
+            if login_status is not None:
+                options['login_status'] = login_status
+            url = mangle.urlencode(url, **options)
+            resp.redirect(url, lock=1)
+            return 1
+
+        # Could not challenge.
+        return 0
+
+
+    security.declarePublic('login')
+    def login(self):
+        """ Set a cookie and redirect to the url that we tried to
+        authenticate against originally.
+        """
+        request = self.REQUEST
+        response = request['RESPONSE']
+
+        login = request.get('__ac_name', '')
+        password = request.get('__ac_password', '')
+
+        if (not login) or (not password):
+            return self.unauthorized(
+                login_status=u"You need to type a login and a password.")
+        else:
+            creds = {'login': login, 'password': password,}
+            pas_instance = self._getPAS()
+            if pas_instance is not None:
+                plugins = pas_instance.plugins
+                authenticators = plugins.listPlugins(IAuthenticationPlugin)
+                for auth_id, auth in authenticators:
+                    try:
+                        uid_and_info = auth.authenticateCredentials(creds)
+                        if uid_and_info is None:
+                            continue
+                        user_id, info = uid_and_info
+                        if user_id is not None:
+                            pas_instance.updateCredentials(request, response,
+                                                           login, password)
+                            break
+                    except _SWALLOWABLE_PLUGIN_EXCEPTIONS:
+                        continue
+
+            came_from = request.form['came_from']
+            return response.redirect(came_from)
+
+
+InitializeClass(SilvaCookieAuthHelper)
+
