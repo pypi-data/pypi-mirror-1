735b91ff879c25a88de70d31961dae9dd688b627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:24 +0000

    Add package Products.validation/1.6.2/Products.validation-1.6.2.tar.gz

---
+++ packages/Products.validation/Products.validation-1.6.2.tar.gz/Products.validation-1.6.2/Products/validation/validators/RangeValidator.py
@@ -0,0 +1,38 @@
+from Products.validation.interfaces.IValidator import IValidator
+from Products.validation.i18n import PloneMessageFactory as _
+from Products.validation.i18n import recursiveTranslate
+
+class RangeValidator:
+    __implements__ = IValidator
+
+    def __init__(self, name, minval=0.0, maxval=0.0, title='', description=''):
+        self.name = name
+        self.minval = minval
+        self.maxval = maxval
+        self.title = title or name
+        self.description = description
+
+    def __call__(self, value, *args, **kwargs):
+        if len(args)>=1:
+            minval=args[0]
+        else:
+            minval=self.minval
+
+        if len(args)>=2:
+            maxval=args[1]
+        else:
+            maxval=self.maxval
+
+        assert(minval <= maxval)
+        try:
+            nval = float(value)
+        except ValueError:
+            msg = _(u"Validation failed($name): could not convert '$value' to number",
+                    mapping = { 'name' : self.name, 'value': value})
+            return recursiveTranslage(msg, **kwargs)
+        if minval <= nval < maxval:
+            return 1
+
+        msg = _(u"Validation failed($name): '$value' out of range($min, $max)",
+                mapping = { 'name' : self.name, 'value': value, 'min' : minval, 'max' : maxval,})
+        return recursiveTranslate(msg, **kwargs)
