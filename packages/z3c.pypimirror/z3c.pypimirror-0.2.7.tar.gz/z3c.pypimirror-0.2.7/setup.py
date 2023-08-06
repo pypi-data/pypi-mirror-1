f016cf610e980e26709671235eafd0accc9a7da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:59 +0000

    Add package z3c.pypimirror/0.2.7/z3c.pypimirror-0.2.7.tar.gz

---
+++ packages/z3c.pypimirror/z3c.pypimirror-0.2.7.tar.gz/z3c.pypimirror-0.2.7/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages, find_packages
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Zope Public License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+]
+
+desc = open('README.txt').read().strip()
+changes = open('CHANGES.txt').read().strip()
+long_description = desc + '\n\nChanges\n=======\n\n'  + changes
+
+setup(
+    name='z3c.pypimirror',
+    version='0.2.7',
+    author='Daniel Kraft et al.',
+    author_email='dk@d9t.de',
+    description='A module for building a complete or a partial PyPI mirror',
+    long_description=long_description,
+    maintainer='Andreas Jung',
+    maintainer_email='lists@zopyx.com',
+    classifiers=CLASSIFIERS,
+    package_dir = {'': 'src'},
+    namespace_packages=['z3c'] ,
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_data={
+        '' : ['*.txt', '*.sample'],
+    },
+    zip_safe=False,
+    install_requires = ['setuptools',
+                        'zc.lockfile',
+                        'BeautifulSoup'],
+    extras_require = {
+        'test': [ 'zc.buildout' ],
+    },
+    entry_points = dict(console_scripts=[
+        'pypimirror = z3c.pypimirror.mirror:run',
+        ])
+    )
