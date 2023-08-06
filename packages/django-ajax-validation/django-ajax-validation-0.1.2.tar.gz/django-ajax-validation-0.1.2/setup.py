d1f9a7346c9027d6f07830e44c37ae09aefc9356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:03 +0000

    Add package django-ajax-validation/0.1.2/django-ajax-validation-0.1.2.tar.gz

---
+++ packages/django-ajax-validation/django-ajax-validation-0.1.2.tar.gz/django-ajax-validation-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-ajax-validation',
+    version='0.1.2',
+    description='Provides support for doing validation using Ajax(currently with jQuery) using your existing Django forms.',
+    author='Alex Gaynor',
+    author_email='alex.gaynor@gmail.com',
+    url='http://github.com/alex/django-ajax-validation/tree/master',
+    packages=find_packages(),
+    data_files=[
+        ('ajax_validation/media/ajax_validation/js', ['ajax_validation/media/ajax_validation/js/jquery-ajax-validation.js']),
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
