dfd88ac9a9b1a89c372474b8b1b4c02813bd5afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:58 +0000

    Add package tw.rating/0.8/tw.rating-0.8.tar.gz

---
+++ packages/tw.rating/tw.rating-0.8.tar.gz/tw.rating-0.8/tw/rating/release.py
@@ -0,0 +1,11 @@
+# Metadata for the widget EGG. Should leave it for the (unimplemented) widget
+# tracker to track it
+
+__PROJECT__ = 'tw.rating'
+__DESCRIPTION__ = 'Star Rater plugin'
+__URL__ = 'http://toscawidgets.org/'
+__VERSION__ = '0.8'
+__AUTHOR__ = "Christopher Perkins, Alberto Valverde, Sean O'Donnell"
+__EMAIL__ = 'chris@percious.com'
+__COPYRIGHT__ = "Copyright 2008 Alberto Valverde, Sean O'Donnell, Christopher Perins"
+__LICENSE__ = 'MIT'
