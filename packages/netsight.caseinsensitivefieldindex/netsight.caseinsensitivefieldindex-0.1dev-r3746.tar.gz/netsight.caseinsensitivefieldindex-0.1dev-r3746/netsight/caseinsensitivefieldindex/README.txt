2d5e113cc2f0bd38cd554f12296e6ca52c812ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:32 +0000

    Add package netsight.caseinsensitivefieldindex/0.1dev-r3746/netsight.caseinsensitivefieldindex-0.1dev-r3746.tar.gz

---
+++ packages/netsight.caseinsensitivefieldindex/netsight.caseinsensitivefieldindex-0.1dev-r3746.tar.gz/netsight.caseinsensitivefieldindex-0.1dev-r3746/netsight/caseinsensitivefieldindex/README.txt
@@ -0,0 +1,14 @@
+netsight.caseinsensitivefieldindex Package Readme
+=========================
+
+Overview
+--------
+
+A case insensitive version of Zope's FieldIndex
+
+
+Your tests here
+---------------
+
+    >>> 1 + 1
+    3
---
+++ packages/netsight.caseinsensitivefieldindex/netsight.caseinsensitivefieldindex-0.1dev-r3746.tar.gz/netsight.caseinsensitivefieldindex-0.1dev-r3746/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='netsight.caseinsensitivefieldindex',
+      version=version,
+      description="A case insensitive version of Zope's FieldIndex",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zope indexing fieldindex',
+      author='Matt Hamilton',
+      author_email='matth@netsight.co.uk',
+      url='http://www.netsight.co.uk/',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['netsight'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
