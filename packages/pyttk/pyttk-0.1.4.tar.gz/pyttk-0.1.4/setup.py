79fc5e324723332be74a3582d86cce71796722eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:09 +0000

    Add package pyttk/0.1.4/pyttk-0.1.4.tar.gz

---
+++ packages/pyttk/pyttk-0.1.4.tar.gz/pyttk-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyttk',
+    version='0.1.4',
+    author='Guilherme Polo',
+    author_email='ggpolo@gmail.com',
+    url='http://code.google.com/p/python-ttk',
+    description='Ttk Python wrapper',
+    package_dir={'': 'src'},
+    packages=['']
+)
