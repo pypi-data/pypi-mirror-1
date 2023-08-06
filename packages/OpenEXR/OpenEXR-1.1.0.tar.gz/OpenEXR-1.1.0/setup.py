80feb183b6afad4eed943029802edc7f3cf9600d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:35 +0000

    Add package OpenEXR/1.1.0/OpenEXR-1.1.0.tar.gz

---
+++ packages/OpenEXR/OpenEXR-1.1.0.tar.gz/OpenEXR-1.1.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from distutils.command.build_py import build_py as _build_py
+
+from os import system
+
+from distutils.core import setup, Extension
+setup(name='OpenEXR',
+  author = 'James Bowman',
+  author_email = 'jamesb@excamera.com',
+  url = 'http://excamera.com/articles/26/openexr.html',
+  description = "Python bindings for ILM's OpenEXR image file format",
+  long_description = "Python bindings for ILM's OpenEXR image file format",
+  version='1.1.0',
+  data_files=['test-exr.py'],
+  ext_modules=[ 
+    Extension('OpenEXR',
+              ['OpenEXR.cpp'],
+              include_dirs=['/usr/include/OpenEXR', '/usr/local/include/OpenEXR', '/opt/local/include/OpenEXR'],
+              library_dirs=['/usr/local/lib', '/opt/local/lib'],
+              libraries=['Iex', 'Half', 'Imath', 'IlmImf', 'z'],
+              extra_compile_args=['-g'])
+  ],
+  py_modules=['Imath'],
+  # scripts=['test-exr.py']
+)
