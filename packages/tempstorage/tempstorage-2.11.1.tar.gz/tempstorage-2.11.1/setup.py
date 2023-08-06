e585b8bbac66e9de63f97076f9d08051fdb37ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:11 +0000

    Add package tempstorage/2.11.1/tempstorage-2.11.1.tar.gz

---
+++ packages/tempstorage/tempstorage-2.11.1.tar.gz/tempstorage-2.11.1/setup.py
@@ -0,0 +1,38 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for the Acquisition egg package
+"""
+import os
+from setuptools import setup, find_packages, Extension
+
+setup(name='tempstorage',
+      version = '2.11.1',
+      url='http://pypi.python.org/pypi/tempstorage',
+      license='ZPL 2.1',
+      description='A RAM-based storage for ZODB',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description="""\
+A storage implementation which uses RAM to persist objects, much like
+MappingStorage.  Unlike MappingStorage, it needs not be packed to get rid of
+non-cyclic garbage and it does rudimentary conflict resolution.  This is a
+ripoff of Jim's Packless bsddb3 storage.""",
+      
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+
+      install_requires=['ZODB3'],
+      include_package_data=True,
+      zip_safe=False,
+      )
