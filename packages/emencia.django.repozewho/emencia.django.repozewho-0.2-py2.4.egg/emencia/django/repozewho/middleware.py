daacd9fac821c1124a2112b3e184f6ce9e2c818e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:29 +0000

    Add package emencia.django.repozewho/0.2/emencia.django.repozewho-0.2-py2.4.egg

---
+++ packages/emencia.django.repozewho/emencia.django.repozewho-0.2-py2.4.egg/emencia/django/repozewho/middleware.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import User
+from django.conf import settings
+
+
+class AuthMiddleware(object):
+
+    def process_request(self, request):
+        if request.environ.get('repoze.who.identity') is not None:
+            repoze_who_user = None
+            identity = request.environ['repoze.who.identity']
+            if identity is not None:
+                repoze_who_user = identity['repoze.who.userid']
+            if repoze_who_user is not None:
+                user = User.objects.get(**{settings.LOGIN_FIELD:
+                                           repoze_who_user})
+                request.user = user
---
+++ packages/emencia.django.repozewho/emencia.django.repozewho-0.2-py2.4.egg/emencia/django/repozewho/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+class AppTestCase(TestCase):
+    """
+    Populate this class with unit tests for your application
+    """
+    
+    urls = 'emencia.django.repozewho.test_urls'
+    
+    def testApp():
+        pass
