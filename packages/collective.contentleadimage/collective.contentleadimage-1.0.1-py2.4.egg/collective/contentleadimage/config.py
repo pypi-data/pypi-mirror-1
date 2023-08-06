1df4f5062dfda2734f72d1e92f727e94d5322dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:39 +0000

    Add package collective.contentleadimage/1.0.1/collective.contentleadimage-1.0.1-py2.4.egg

---
+++ packages/collective.contentleadimage/collective.contentleadimage-1.0.1-py2.4.egg/collective/contentleadimage/config.py
@@ -0,0 +1,9 @@
+# obsolete - used for migration from version < 1.0
+CONTENT_LEADIMAGE_ANNOTATION_KEY = 'collective.contentleadimage'
+
+# Please note, templates in browser directory uses field name directly
+IMAGE_FIELD_NAME = 'leadImage'
+
+# All upload images will be scaled to this size. 
+# Thumbnail will be created to value set in the preferences - (81,67) by default
+IMAGE_MAX_SIZE   = (640, 480)
