df51d707765aae3e2dc8c55955782b84c16a354e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:48 +0000

    Add package pyunv/0.2.0/pyunv-0.2.0.tar.gz

---
+++ packages/pyunv/pyunv-0.2.0.tar.gz/pyunv-0.2.0/README.txt
@@ -0,0 +1,70 @@
+`pyunv` is a Python parser for SAP BusinessObjects universe (.unv) files.
+
+Introduction
+============
+
+PyUnv reads SAP BusinessObjects universe (.unv) files. In BusinessObjects, a 
+universe provides a metadata layer above enterprise databases, expressed in 
+language that is easier for business users to understand than the underlying 
+data structures. Universes are edited with the BusinessObjects Designer and 
+saved to universe files in an undocumented binary format. PyUnv can extract 
+most of this metadata from the universe file, enabling you to use it outside 
+BusinessObjects, or to create tools to streamline your BusinessObjects 
+development process.
+
+PyUnv requires Mako to produce manifests. I include a sample template for a
+text manifest. If you come up with your own manifests in RST, HTML, or other,
+let me know.
+
+Installation
+============
+
+Install PyUnv with easy_install::
+
+    easy_install pyunv
+
+Sample Usage
+============
+
+With PyUnv installed, this should work::
+
+    >>> from pyunv.reader import Reader
+    >>> from pyunv.manifest import Manifest
+    >>> universe = Reader(open('sample.unv', 'rb')).universe
+    >>> manifest = Manifest()
+    >>> manifest.save(open('manifest.txt', 'w'), universe)
+
+This will create a text manifest of the tables, columns, classes, objects, 
+and conditions in your universe.
+
+Applications
+============
+
+I wrote the earliest versions of PyUnv to extract descriptions for classes, 
+objects, and conditions from the universe file. After reverse-engineering more 
+of the universe file format, I saw PyUnv as a way to workaround limitations of 
+the BusinessObjects development tools. For example, BusinessObjects Designer 
+provides no support for change tracking. If you want to know what changed 
+between two versions of a universe, you open the first universe in one 
+Designer window and the second in another window, and then compare them 
+visually. That is impractical for all but the simplest universes.
+
+With PyUnv, you can export all of the universe metadata to a text manifest file 
+and use your favorite file comparison tool (diff, p4diff, FileMerge, or even 
+Microsoft Word) to highlight the differences. To track changes over time, just 
+store the manifest with your universe in a version control system.
+
+Roadmap
+=======
+
+At this point, PyUnv reads basic universe information and classes, objects, 
+conditions, tables, and virtual tables from a universe file. The Python objects 
+defined in PyUnv mirror the entities from the universe file, and are arranged in 
+memory in a tree structure as you would see them in Designer. For objects and 
+conditions, you can get the description, select statement, where statement, 
+and more.
+
+I'm testing PyUnv with universes created on BusinessObjects XI R2. I haven't
+tested it with earlier or later versions. I am still reverse-engineering other 
+metadata in the universe file. If you have questions, or would like to help, 
+just drop me a line.
---
+++ packages/pyunv/pyunv-0.2.0.tar.gz/pyunv-0.2.0/pyunv/universe.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+pyunv.py
+
+Created by David Peckham on 2009-09-01.
+Copyright (c) 2009 David Peckham. All rights reserved.
+"""
+
+import os
+import re
+import sys
+from repr import repr
+
+
+class Universe(object):
+    
+    """docstring for Universe"""
+    def __init__(self, id_=None, name=None, description=None):
+        super(Universe, self).__init__()
+        self.id_ = id_
+        self.name = name
+        self.description = description
+        self.schema = None
+        self.tables = []
+        self.columns = []
+        self.classes = []
+        self.joins = []
+        self.parameters = None
+
+    def table_name(self, table_id):
+        try:
+            tablename = self.table_map[table_id].name
+        except KeyError:
+            tablename = 'UNKNOWN TABLE (%d)' % table_id
+        return tablename
+
+
+class Parameters(object):
+    """docstring for Parameters"""
+    def __init__(self):
+        super(Parameters, self).__init__()
+        self.universe_filename = None
+        self.universe_name = None
+        self.revision = 0
+        self.description = None
+        self.created_by = None
+        self.modified_by = None
+        self.created_date = None
+        self.modified_date = None
+        self.query_time_limit = 0
+        self.row_limit = 0
+        self.object_strategy = None
+        self.cost_estimate_warning_limit = 0
+        self.long_text_limit = 0
+        self.comments = None
+        self.domain = None
+        self.dbms_engine = None
+        self.network_layer = None
+
+
+class Class(object):
+    
+    """docstring for Class"""
+    def __init__(self, universe, id_, parent, name, description):
+        super(Class, self).__init__()
+        self.universe = universe
+        self.id_ = id_
+        self.parent = parent
+        self.name = name
+        self.description = description
+        self.objects = []
+        self.conditions = []
+        self.subclasses = []
+
+
+class Join(object):
+    
+    """docstring for Join"""
+    def __init__(self, universe):
+        super(Join, self).__init__()
+        self.universe = universe
+        self.expression = None
+        self.term_count = 0
+        self.terms = []
+    
+    @property
+    def statement(self):
+        #TODO: add table or object names to the statement
+        if self.term_count == 2:
+            s = self.fullterm(self.terms[0]) + self.expression + self.fullterm(self.terms[1])
+        else:
+            format = self.expression.replace(chr(1), '%s')
+            s = format % tuple([self.fullterm(t) for t in self.terms])
+        return s
+    
+    def fullterm(self, term):
+        """return the fully qualified term with table and column names"""
+        column_name, table_id = term
+        return '%s.%s' % (self.universe.table_name(table_id), column_name)
+
+
+class ObjectBase(object):
+    
+    """docstring for ObjectBase"""
+    def __init__(self, universe, id_, parent, name, description):
+        super(ObjectBase, self).__init__()
+        assert(universe)
+        self.universe = universe
+        self.id_ = id_
+        self.parent = parent
+        self.name = name
+        self.description = description
+        self.select_table_refs = []
+        self.where_table_refs = []
+        self.select = None
+        self.where = None
+
+    def lookup_table(self, match):
+        index = int(match.groups()[0])
+        try:
+            table = self.universe.table_map[index]
+            return table.name
+        except KeyError:
+            return 'UNKNOWN (%d)' % index
+    
+    def expand_sql(self, sql):
+        """Return the SQL with table names instead of table IDs"""
+        if sql:
+            p = re.compile(r'(?:'+chr(3)+')([0-9]{1,4})')
+            return p.sub(self.lookup_table, sql)
+        else:
+            return None
+        
+    @property
+    def select_sql(self):
+        return self.expand_sql(self.select)
+
+    @property
+    def where_sql(self):
+        return self.expand_sql(self.where)
+
+    def __str__(self):
+        return '%s id=%d, name=%s, select=%s' % (type(self), 
+            self.id_, self.name, self.select) 
+
+       
+class Object(ObjectBase):
+    
+    """docstring for Object"""
+    def __init__(self, universe, id_, parent, name, description):
+        super(Object, self).__init__(universe, id_, parent, name, description)
+        self.format = None
+        self.lov_name = None
+        
+
+class Condition(ObjectBase):
+    
+    """docstring for Condition"""
+    def __init__(self, universe, id_, parent, name, description):
+        super(Condition, self).__init__(universe, id_, parent, name, description)
+
+
+class Table(object):
+
+    """docstring for Table"""
+    def __init__(self, id_, name, schema):
+        super(Table, self).__init__()
+        self.id_ = id_
+        self.name = name
+        self.schema = schema
+
+    def __str__(self):
+        return '%s id=%d, schema=%s name=%s' % (type(self), 
+            self.id_, self.schema, self.name) 
+        
+
+class VirtualTable(object):
+
+    """docstring for VirtualTable"""
+    def __init__(self, table_id=None, select=None):
+        super(VirtualTable, self).__init__()
+        self.table_id = table_id
+        self.select = select
+
+    def __str__(self):
+        return '%s table_id=%d, select=%s' % (type(self), 
+            self.table_id, self.select) 
+
+
+class Column(object):
+
+    """docstring for Column"""
+    def __init__(self, id_=None, name=None, parent=None, universe=None):
+        super(Column, self).__init__()
+        self.id_ = id_
+        self.name = name
+        self.parent = parent
+        self.universe = universe
+
+    @property
+    def table_name(self):
+        return self.universe.table_name(self.parent)
+
+        
+    def __cmp__(self, other):
+        return cmp(self.id_, other.id_)
+        
+    def __str__(self):
+        return '%s id=%d, name=%s parent=%d' % (type(self), 
+            self.id_, self.name, self.parent) 
+        
+
