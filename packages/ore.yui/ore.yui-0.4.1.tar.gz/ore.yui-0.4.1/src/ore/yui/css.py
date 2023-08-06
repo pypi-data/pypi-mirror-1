c164edc8f93a6a3cb08349b8615ee8940421a925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:50 +0000

    Add package ore.yui/0.4.1/ore.yui-0.4.1.tar.gz

---
+++ packages/ore.yui/ore.yui-0.4.1.tar.gz/ore.yui-0.4.1/src/ore/yui/css.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (c) 2008 Kapil Thangavelu
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: $
+"""
+
+from zope.viewlet.viewlet import CSSViewlet
+
+YahooDataTable = CSSViewlet('yui/datatable/assets/datatable.css')
+YahooDataTableSkin = CSSViewlet('yui/assets/skins/sam/datatable.css')
+YahooTreeView = CSSViewlet('yui/assets/skins/sam/treeview.css')
+YahooLogger = CSSViewlet('yui/logger/assets/skins/sam/logger.css')
+
