56bf43bd12535be39a674bc0a644cd6b2613bed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:39 +0000

    Add package bluebream/0.1.1/bluebream-0.1.1.tar.gz

---
+++ packages/bluebream/bluebream-0.1.1.tar.gz/bluebream-0.1.1/src/bluebream/template.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from paste.script import templates
+from paste.script.templates import var
+
+
+class BlueBream(templates.Template):
+
+    _template_dir = 'project_template'
+    summary = "A Zope project"
+    vars = [var('namespace_package', '')]
+
