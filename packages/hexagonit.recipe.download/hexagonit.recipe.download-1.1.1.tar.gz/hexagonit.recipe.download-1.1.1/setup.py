28d32459ddb20d5e6098cbdbd44a06f2af483b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:30 +0000

    Add package hexagonit.recipe.download/1.1.1/hexagonit.recipe.download-1.1.1.tar.gz

---
+++ packages/hexagonit.recipe.download/hexagonit.recipe.download-1.1.1.tar.gz/hexagonit.recipe.download-1.1.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.1'
+name = 'hexagonit.recipe.download'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name=name,
+      version=version,
+      description="zc.buildout recipe for downloading and extracting packages",
+      long_description= (
+        read('README.rst')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('hexagonit','recipe','download','README.txt')
+        + '\n' +
+        'Download\n'
+        '***********************\n'
+        ),
+      classifiers=[
+       'Framework :: Buildout',
+       'Intended Audience :: Developers',
+       'License :: OSI Approved :: GNU General Public License (GPL)',
+       'Topic :: Software Development :: Build Tools',
+       'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      keywords='development buildout recipe',
+      author='Kai Lautaportti',
+      author_email='kai.lautaportti@hexagonit.fi',
+      url='http://cheeseshop.python.org/pypi/%s' % name,
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['hexagonit', 'hexagonit.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = ['zc.buildout', 'setuptools'],
+      tests_require = ['zope.testing'],
+      test_suite = '%s.tests.test_suite' % name,
+      entry_points = { 'zc.buildout' : ['default = hexagonit.recipe.download:Recipe'] },
+      )
