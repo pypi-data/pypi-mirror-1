3e5dad91ef33f682ea27db8d8f42aa6a385cfc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:01 +0000

    Add package clhash/1.000/clhash-1.000.tar.gz

---
+++ packages/clhash/clhash-1.000.tar.gz/clhash-1.000/setup.py
@@ -0,0 +1,28 @@
+#E:\profile
+#VS90COMNTOOLS D:\setup\vs8\Common7\Tools\
+
+from distutils.core import setup, Extension
+
+setup(
+        name = 'clhash',
+        version = '1.000',
+        ext_modules = [
+            Extension('clhash', ['clhash.cpp'],),
+        ],
+        description="merge crc32 and lookup",
+        long_description="""
+Merge hash function from crc32 and lookup
+
+c version by jingmi , python wrapper by zsp:
+
+    from clhash import get_unsigned_hash,get_hash
+
+    print get_unsigned_hash("stdyun.com zsp007@gmail.com")
+
+    print get_hash("stdyun.com zsp007@gmail.com")
+
+
+Reference:
+http://burtleburtle.net/bob/hash/doobs.html
+"""
+)
