60f829ef234711de883b0c48b5153ba74ab2bd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package visionegg/1.1/visionegg-1.1-py2.5-win32.egg

---
+++ packages/visionegg/visionegg-1.1-py2.5-win32.egg/VisionEgg/PyroApps/__init__.py
@@ -0,0 +1,16 @@
+#
+# The Vision Egg: PyroApps
+#
+# Copyright (C) 2001-2003 Andrew Straw.
+# Author: Andrew Straw <astraw@users.sourceforge.net>
+# URL: <http://www.visionegg.org/>
+#
+# Distributed under the terms of the GNU Lesser General Public License
+# (LGPL). See LICENSE.TXT that came with this file.
+#
+# $Id: __init__.py 1224 2003-09-18 13:12:50Z astraw $
+
+"""
+VisionEgg subpackage providing support for demo applications based on Pyro
+
+"""
