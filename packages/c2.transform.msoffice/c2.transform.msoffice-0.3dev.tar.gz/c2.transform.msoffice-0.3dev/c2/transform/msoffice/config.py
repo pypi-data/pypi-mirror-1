4e50e874e125952a7f8815d21ab3f240d10042d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:48 +0000

    Add package c2.transform.msoffice/0.3dev/c2.transform.msoffice-0.3dev.tar.gz

---
+++ packages/c2.transform.msoffice/c2.transform.msoffice-0.3dev.tar.gz/c2.transform.msoffice-0.3dev/c2/transform/msoffice/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+config.py
+
+Created by Manabu Terada on 2009-12-02.
+Copyright (c) 2009 CMScom. All rights reserved.
+"""
+PROJECTNAME = 'c2.transform.msoffice'
+PROJECT_GLOBALS = globals()
+SITE_CHARSET = 'utf-8'
+TRANSFORM_NAME = 'msoffice_to_text'
+
+
