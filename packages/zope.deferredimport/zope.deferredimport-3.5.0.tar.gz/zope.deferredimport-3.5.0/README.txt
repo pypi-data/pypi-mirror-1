0ca3ed4907dd387723ca5c4f46742ca23bd80f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:40 +0000

    Add package zope.deferredimport/3.5.0/zope.deferredimport-3.5.0.tar.gz

---
+++ packages/zope.deferredimport/zope.deferredimport-3.5.0.tar.gz/zope.deferredimport-3.5.0/README.txt
@@ -0,0 +1,31 @@
+Often, especially for package modules, you want to import names for
+convenience, but not actually perform the imports until necessary.
+The zope.deferredimport package provided facilities for defining names
+in modules that will be imported from somewhere else when used.  You
+can also cause deprecation warnings to be issued when a variable is
+used.
+
+.. contents::
+
+Changes
+=======
+
+3.5.0 (2009-02-04)
+------------------
+
+- Added support to bootstrap on Jython.
+
+- Added reference documentation.
+
+
+3.4.0 (2007/07/19)
+------------------
+
+- Finished release of zope.deferredimport.
+
+
+3.4.0b1 (2007/07/09)
+--------------------
+
+- Initial release as a separate project, corresponds to zope.deferredimport
+  from Zope 3.4.0b1.
---
+++ packages/zope.deferredimport/zope.deferredimport-3.5.0.tar.gz/zope.deferredimport-3.5.0/setup.py
@@ -0,0 +1,49 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.deferredimport package
+
+$Id: setup.py 96097 2009-02-04 22:29:46Z shane $
+"""
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='zope.deferredimport',
+    version = '3.5.0',
+    url='http://pypi.python.org/pypi/zope.deferredimport',
+    license='ZPL 2.1',
+    description='zope.deferredimport allows you to perform imports names '
+    'that will only be resolved when used in the code.',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+    long_description=(
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '======================\n'
+        + '\n' +
+        read('src', 'zope', 'deferredimport', 'README.txt')
+        ),
+      
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope',],
+      install_requires=['zope.proxy', 'setuptools'],
+      include_package_data = True,
+      zip_safe = False,
+      )
