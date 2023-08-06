3b82bc03d4cd06eb70c1e3aaa0aed66037b61703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:24 +0000

    Add package atomisator.readers/0.1.2/atomisator.readers-0.1.2.tar.gz

---
+++ packages/atomisator.readers/atomisator.readers-0.1.2.tar.gz/atomisator.readers-0.1.2/atomisator/readers/tests/test_plugins.py
@@ -0,0 +1,49 @@
+from nose.tools import *
+
+import os
+from atomisator.readers.html import HTML 
+from atomisator.readers.folder import Folder
+
+test_dir = os.path.dirname(__file__)
+
+def test_html():
+
+    web_page = os.path.join(test_dir, 'page.html') 
+    parser = HTML()
+    content = parser('file://'+web_page)
+    
+    assert_equals(len(content), 1)
+    content = content[0]
+    assert_equals(content['title'], 'the title')
+    assert content['summary'].startswith('The content')
+
+    extractor = (r'<div class="entry">.*?<h1>(.*?)</h1>'
+                  '(.*?)</div>')
+
+    content = parser('file://'+web_page, 
+                     extractor)
+
+    assert_equals(len(content), 2)
+    assert_equals(content[1]['title'], 'title 2')
+    assert_equals(content[1]['summary'], 'this is entry two')
+
+    extractor = (r'<div class="entry">.*?<h1>.*?</h1>'
+                  '(.*?)</div>')
+
+    content = parser('file://'+web_page, 
+                     extractor)
+
+    assert_equals(len(content), 2)
+    assert_equals(content[1]['title'], 'the title')
+    assert_equals(content[1]['summary'], 'this is entry two')
+
+def test_folder():
+    
+    content_folder = os.path.join(test_dir, 'data')   
+    parser = Folder()
+    result = parser(content_folder)
+
+    
+    assert_equals(len(result), 2)
+    assert_equals(result[1]['summary'], 'some things\n\n')
+
