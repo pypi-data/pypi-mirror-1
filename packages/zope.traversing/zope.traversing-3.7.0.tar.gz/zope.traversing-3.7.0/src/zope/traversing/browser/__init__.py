65ed06e73804eb664bb6fb5ee3cc9dd8f05786a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:48 +0000

    Add package zope.traversing/3.7.0/zope.traversing-3.7.0.tar.gz

---
+++ packages/zope.traversing/zope.traversing-3.7.0.tar.gz/zope.traversing-3.7.0/src/zope/traversing/browser/__init__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+"""Absolute URL View components
+
+$Id: __init__.py 100317 2009-05-23 22:05:12Z shane $
+"""
+from zope.traversing.browser.absoluteurl import absoluteURL
+from zope.traversing.browser.absoluteurl import AbsoluteURL
+from zope.traversing.browser.absoluteurl import SiteAbsoluteURL
