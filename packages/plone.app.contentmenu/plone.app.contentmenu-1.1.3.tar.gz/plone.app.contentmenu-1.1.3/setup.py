4d11864bbae49be2fa7558d4ef76263d1a337786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:13 +0000

    Add package plone.app.contentmenu/1.1.3/plone.app.contentmenu-1.1.3.tar.gz

---
+++ packages/plone.app.contentmenu/plone.app.contentmenu-1.1.3.tar.gz/plone.app.contentmenu-1.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.3'
+
+setup(name='plone.app.contentmenu',
+      version=version,
+      description="Plone's content menu implementation",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='plone contentmenu menu',
+      author='Martin Aspeli',
+      author_email='optilude@gmx.net',
+      url='http://svn.plone.org/svn/plone/plone.app.contentmenu',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.locking',
+        'plone.memoize',
+        'plone.app.content',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
