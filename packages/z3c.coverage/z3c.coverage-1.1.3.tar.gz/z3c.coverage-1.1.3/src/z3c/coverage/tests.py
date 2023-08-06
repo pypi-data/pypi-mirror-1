869c333632ff1d6f7acd08a25faa1ad62b94c5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:41 +0000

    Add package z3c.coverage/1.1.3/z3c.coverage-1.1.3.tar.gz

---
+++ packages/z3c.coverage/z3c.coverage-1.1.3.tar.gz/z3c.coverage-1.1.3/src/z3c/coverage/tests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+Test suite for z3c.coverage
+"""
+
+import unittest, re
+from zope.testing import doctest, renormalizing
+
+
+def test_suite():
+    checker = renormalizing.RENormalizing([
+                # optparse in Python 2.4 prints "usage:" and "options:",
+                # in 2.5 it prints "Usage:" and "Options:".
+                (re.compile('^usage:'), 'Usage:'),
+                (re.compile('^options:', re.MULTILINE), 'Options:'),
+                                           ])
+    return unittest.TestSuite([
+        doctest.DocFileSuite(
+            'README.txt', checker=checker,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            ),
+        doctest.DocFileSuite(
+            'coveragediff.txt', checker=checker,
+            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+            ),
+        doctest.DocTestSuite(
+            'z3c.coverage.coveragediff'),
+        doctest.DocTestSuite(
+            'z3c.coverage.coveragereport'),
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
