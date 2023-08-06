ffc89bf227765edef8c2d18555906808946b98ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:27 +0000

    Add package unicodescript/0.2/unicodescript-0.2.tar.gz

---
+++ packages/unicodescript/unicodescript-0.2.tar.gz/unicodescript-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (name='unicodescript',
+      version='0.2',
+      description='Complements the unicodedata module by allowing lookup of the Script attribute of characters. Based on Unicode 5.2.1',
+      author='Conrad Irwin',
+      author_email='conrad.irwin+py@gmail.com',
+      license='Python',
+      packages=['unicodescript'],
+      package_data={'unicodescript':['*']}
+)
+
