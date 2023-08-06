8c1328166e3ff61fae1015e7207cfd45900c3ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:07 +0000

    Add package collective.portlet.googleapps/0.1/collective.portlet.googleapps-0.1-py2.4.egg

---
+++ packages/collective.portlet.googleapps/collective.portlet.googleapps-0.1-py2.4.egg/collective/portlet/googleapps/portlets/__init__.py
@@ -0,0 +1,9 @@
+from Products.CMFCore.permissions import setDefaultRoles 
+setDefaultRoles('collective.portlet.googleapps: Add Google Docs portlet', ('Manager', 'Owner',))
+setDefaultRoles('collective.portlet.googleapps: Add Google Talk portlet', ('Manager', 'Owner',))
+setDefaultRoles('collective.portlet.googleapps: Add Google Tasks portlet', ('Manager', 'Owner',))
+setDefaultRoles('collective.portlet.googleapps: Add Google Contacts portlet', ('Manager', 'Owner',))
+setDefaultRoles('collective.portlet.googleapps: Add Google Calendar portlet', ('Manager', 'Owner',))
+setDefaultRoles('collective.portlet.googleapps: Add Google Mail portlet', ('Manager', 'Owner',))
+
+
