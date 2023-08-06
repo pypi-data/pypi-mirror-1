7fb11817eca2afd0a109f03da4e36eddbc634580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:26 +0000

    Add package SaladeDeFruits/0.5/SaladeDeFruits-0.5.tar.gz

---
+++ packages/SaladeDeFruits/SaladeDeFruits-0.5.tar.gz/SaladeDeFruits-0.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.5'
+
+setup(name='SaladeDeFruits',
+      version=version,
+      description="A skinning middleware",
+      long_description=open('README.txt').read(),
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Operating System :: OS Independent',
+         ],
+      keywords='wsgi middleware skin lxml pyquery',
+      author='Gael Pasgrimaud',
+      author_email='gael@gawel.org',
+      url='http://www.gawel.org/docs/SaladeDeFruits/index.html',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'WebOb',
+          'Paste',
+          'WSGIProxy',
+          'pyquery',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [paste.filter_app_factory]
+      main = saladedefruits.saladier:make_salade
+      """,
+      )
