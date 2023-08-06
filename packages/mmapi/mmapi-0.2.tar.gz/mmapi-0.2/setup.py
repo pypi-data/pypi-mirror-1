07268e754c03bccd3dad5244840ecd2502ad5b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:02 +0000

    Add package mmapi/0.2/mmapi-0.2.tar.gz

---
+++ packages/mmapi/mmapi-0.2.tar.gz/mmapi-0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'mmapi',
+    version = '0.2',
+    description = 'Client library for the Multimap Web Service API',
+    author = 'Andy Hume',
+    author_email = 'andyhume@gmail.com',
+    url = 'http://github.com/ahume/mmapi',
+    download_url = 'http://github.com/ahume/mmapi/tarball/master',
+    keywords = ['geocoder', 'routing', 'spatial', 'multimap'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        ]
+)
