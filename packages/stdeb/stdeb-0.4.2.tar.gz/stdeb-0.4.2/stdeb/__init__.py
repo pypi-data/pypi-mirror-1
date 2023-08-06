d0a62aa7b872952d924d3e6ac1ab0f375d40126f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:22 +0000

    Add package stdeb/0.4.2/stdeb-0.4.2.tar.gz

---
+++ packages/stdeb/stdeb-0.4.2.tar.gz/stdeb-0.4.2/stdeb/__init__.py
@@ -0,0 +1,12 @@
+# setuptools is required for distutils.commands plugin we use
+import logging
+import setuptools
+__version__ = '0.4.2'
+
+log = logging.getLogger('stdeb')
+log.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
